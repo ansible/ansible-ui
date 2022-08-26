120364832b68e59d2956660b1268f3303c1d6488
James Talton <jtalton@redhat.com>
2022-08-26 14:18:25 -0400

    pwa
    Signed-off-by: James Talton <jtalton@redhat.com>

--- scripts/icons.sh
+++ scripts/icons.sh
@@ -2,6 +2,15 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../frontend/icons
 convert -density 1920 -background none -resize 144x144 -border 24 -bordercolor transparent ansible.svg 192.png
+convert -density 2560 -background none -resize 192x192 -border 32 -bordercolor transparent ansible.svg 256.png
+convert -density 3840 -background none -resize 288x288 -border 48 -bordercolor transparent ansible.svg 384.png
 convert -density 5120 -background none -resize 384x384 -border 64 -bordercolor transparent ansible.svg 512.png
 convert -density 960 -background none -resize 96x96 ansible.svg ansible.png
 convert -density 480 -background none -resize 48x48 favicon.svg favicon.png
+
+optipng -o7 ansible.png
+optipng -o7 favicon.png
+optipng -o7 192.png
+optipng -o7 256.png
+optipng -o7 384.png
+optipng -o7 512.png
