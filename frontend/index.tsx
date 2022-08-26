67ebfc700f6090b4d8ac2eeef2854fdde6cc8963
James Talton <jtalton@redhat.com>
2022-08-26 13:37:00 -0400

    pwa
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -20,3 +20,16 @@ render(
     </ThemeProvider>,
     container
 )
+
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then((registration) => {
+                console.log('SW registered: ', registration)
+            })
+            .catch((registrationError) => {
+                console.log('SW registration failed: ', registrationError)
+            })
+    })
+}
