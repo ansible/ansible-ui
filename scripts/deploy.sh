b9ea1ef4a58b18dc05b64f221a1e1fac84d26eff
James Talton <jtalton@redhat.com>
2022-09-06 10:52:09 -0400

    cleanup
    Signed-off-by: James Talton <jtalton@redhat.com>

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-docker build --tag ansible .
-docker save ansible -o ansible.tar
-scp ansible.tar root@137.184.52.198:~
-rm -f *.tar
-ssh root@137.184.52.198 "docker load -i ansible.tar; docker rm -f ansible || true; docker run --name ansible --restart always -d -e PORT=443 -p 443:443 -v /root/certs:/home/node/certs ansible"
