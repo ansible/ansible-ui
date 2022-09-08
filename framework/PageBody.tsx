f4c4984f99844f6e91841373453838c23c750f98
James Talton <jtalton@redhat.com>
2022-09-08 13:48:54 -0400

    theme fixes
    Signed-off-by: James Talton <jtalton@redhat.com>

--- framework/PageBody.tsx
+++ framework/PageBody.tsx
@@ -14,14 +14,24 @@ export function PageBody(props: { children: ReactNode }) {
                     flexDirection: 'column',
                     height: '100%',
                     maxHeight: '100%',
-                    margin: lg ? 24 : 0,
                     overflow: 'hidden',
-                    // backgroundColor: 'var(--pf-c-page__main-section--BackgroundColor)',
-                    border: settings.borders && lg ? 'thin solid var(--pf-global--BorderColor--100)' : undefined,
-                    backgroundColor: 'var(--pf-global--BackgroundColor--100)',
+                    backgroundColor: 'var(--pf-c-page__main-section--BackgroundColor)',
                 }}
             >
-                {props.children}
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: '100%',
+                        maxHeight: '100%',
+                        margin: lg ? 24 : 0,
+                        overflow: 'hidden',
+                        border: settings.borders && lg ? 'thin solid var(--pf-global--BorderColor--100)' : undefined,
+                        backgroundColor: 'var(--pf-global--BackgroundColor--100)',
+                    }}
+                >
+                    {props.children}
+                </div>
             </div>
         </ErrorBoundary>
     )
