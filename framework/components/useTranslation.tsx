f0750782b7a8136e4dabbe8fb9ba03467e4a117a
James Talton <jtalton@redhat.com>
2022-09-22 17:10:12 -0400

    translation support
    Signed-off-by: James Talton <jtalton@redhat.com>

--- framework/components/useTranslation.tsx
+++ framework/components/useTranslation.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Button } from '@patternfly/react-core'
 import { createContext, ReactNode, useContext, useState } from 'react'
+export * from 'react-i18next'
 
 const TranslationContext = createContext(false)
 const SetTranslationContext = createContext<(dev: boolean) => void>(() => {
@@ -16,22 +17,22 @@ export function TranslationProvider(props: { children: ReactNode }) {
     )
 }
 
-export function useTranslation() {
-    const devMode = useContext(TranslationContext)
-    if (devMode) {
-        return {
-            t: (text: string, options?: { count?: number }) => text.toUpperCase(),
-        }
-    }
-    return {
-        t: (text: string, options?: { count?: number }) => {
-            if (options?.count !== undefined) {
-                text = text.replace('{{count}}', options.count.toString())
-            }
-            return text
-        },
-    }
-}
+// export function useTranslation() {
+//     const devMode = useContext(TranslationContext)
+//     if (devMode) {
+//         return {
+//             t: (text: string, options?: { count?: number }) => text.toUpperCase(),
+//         }
+//     }
+//     return {
+//         t: (text: string, options?: { count?: number }) => {
+//             if (options?.count !== undefined) {
+//                 text = text.replace('{{count}}', options.count.toString())
+//             }
+//             return text
+//         },
+//     }
+// }
 
 export function TranslationSwitcher() {
     const devMode = useContext(TranslationContext)
--- frontend/index.tsx
+++ frontend/index.tsx
@@ -2,7 +2,12 @@
 import '@patternfly/patternfly/patternfly-base.css'
 import '@patternfly/patternfly/patternfly-charts-theme-dark.css'
 
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
+import { Suspense } from 'react'
 import { render } from 'react-dom'
+import { initReactI18next } from 'react-i18next'
 import { TranslationProvider } from '../framework/components/useTranslation'
 import Demo from './Main'
 
@@ -13,10 +18,24 @@ container.style.height = '100%'
 container.style.overflow = 'hidden'
 document.body.appendChild(container)
 
+void i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        fallbackLng: 'en',
+        debug: true,
+        interpolation: {
+            escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+        },
+    })
+
 render(
-    <TranslationProvider>
-        <Demo />
-    </TranslationProvider>,
+    <Suspense fallback="div">
+        <TranslationProvider>
+            <Demo />
+        </TranslationProvider>
+    </Suspense>,
     container
 )
 
