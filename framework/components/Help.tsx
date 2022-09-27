4dfbe5e9969392b8eb78dabf7dba05e3361054ca
James Talton <jtalton@redhat.com>
2022-09-27 11:52:40 -0400

    removeFindDomNode
    Signed-off-by: James Talton <jtalton@redhat.com>

--- framework/components/Help.tsx
+++ framework/components/Help.tsx
@@ -6,7 +6,7 @@ export function Help(props: { help?: ReactNode; title?: string }) {
     const { help, title } = props
     if (!help) return <></>
     return (
-        <Popover headerContent={title} bodyContent={help}>
+        <Popover headerContent={title} bodyContent={help} removeFindDomNode>
             <Button variant="link" style={{ padding: 0, marginLeft: '8px', verticalAlign: 'middle' }}>
                 <OutlinedQuestionCircleIcon />
             </Button>
