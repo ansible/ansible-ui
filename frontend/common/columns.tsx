d8348e65540114b439bf2bc7ec0e90d08a2653a4
James Talton <jtalton@redhat.com>
2022-10-03 09:54:38 -0400

    linting
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/common/columns.tsx
+++ frontend/common/columns.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react'
+import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 import { ITableColumn, SinceCell, TextCell } from '../../framework'
