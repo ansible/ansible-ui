09aef058b1998d3adc4f6a2a2d34f6e4e69e6acf
James Talton <jtalton@redhat.com>
2022-10-10 13:53:57 -0400

    convert typebox definitions to typescript interfaces (#14)
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/controller/access/organizations/Organization.tsx
+++ frontend/controller/access/organizations/Organization.tsx
@@ -1,39 +1,27 @@
-import { Static, Type } from '@sinclair/typebox'
-
-export const OrganizationType = Type.Object({
-    name: Type.String({ minLength: 1 }),
-    description: Type.Optional(Type.String()),
-    organization: Type.Number(),
-    type: Type.Literal('organization'),
-    id: Type.Number(),
-    created: Type.String(),
-    modified: Type.String(),
-    summary_fields: Type.Optional(
-        Type.Object({
-            related_field_counts: Type.Optional(
-                Type.Object({
-                    users: Type.Number(),
-                    teams: Type.Number(),
-                })
-            ),
-            created_by: Type.Optional(
-                Type.Object({
-                    id: Type.Number(),
-                    username: Type.String(),
-                    first_name: Type.String(),
-                    last_name: Type.String(),
-                })
-            ),
-            modified_by: Type.Optional(
-                Type.Object({
-                    id: Type.Number(),
-                    username: Type.String(),
-                    first_name: Type.String(),
-                    last_name: Type.String(),
-                })
-            ),
-        })
-    ),
-})
-
-export type Organization = Static<typeof OrganizationType>
+export interface Organization {
+    type: 'organization'
+    id: number
+    name: string
+    description?: string
+    organization: number
+    created: string
+    modified: string
+    summary_fields?: {
+        related_field_counts?: {
+            users: number
+            teams: number
+        }
+        created_by?: {
+            id: number
+            username: string
+            first_name: string
+            last_name: string
+        }
+        modified_by?: {
+            id: number
+            username: string
+            first_name: string
+            last_name: string
+        }
+    }
+}
--- frontend/controller/access/users/User.tsx
+++ frontend/controller/access/users/User.tsx
@@ -1,47 +1,36 @@
-import { Static, Type } from '@sinclair/typebox'
+export interface User {
+    type: 'user'
+    id: number
+    modified: string
+    created: string
+    username: string
+    password?: string
+    first_name?: string
+    last_name?: string
+    email?: string
+    is_superuser: boolean
+    is_system_auditor: boolean
+    summary_fields?: {
+        organization?: {
+            id: number
+            name: string
+        }
 
-export const UserType = Type.Object({
-    type: Type.Literal('user'),
-    id: Type.Number(),
-    modified: Type.String(),
-    created: Type.String(),
-    username: Type.String(),
-    password: Type.Optional(Type.String()),
-    first_name: Type.Optional(Type.String()),
-    last_name: Type.Optional(Type.String()),
-    email: Type.Optional(Type.String()),
-    is_superuser: Type.Boolean(),
-    is_system_auditor: Type.Boolean(),
-    summary_fields: Type.Optional(
-        Type.Object({
-            organization: Type.Optional(
-                Type.Object({
-                    id: Type.Number(),
-                    name: Type.String(),
-                })
-            ),
-            user_capabilities: Type.Object({
-                delete: Type.Number(),
-                edit: Type.String(),
-            }),
-            direct_access: Type.Array(Type.Object({})),
-            indirect_access: Type.Optional(
-                Type.Array(
-                    Type.Object({
-                        descendant_roles: Type.Array(Type.String()),
-                        role: Type.Object({
-                            id: Type.Number(),
-                            name: Type.String(),
-                            description: Type.String(),
-                            user_capabilities: Type.Object({
-                                unattach: Type.Boolean(),
-                            }),
-                        }),
-                    })
-                )
-            ),
-        })
-    ),
-})
-
-export type User = Static<typeof UserType>
+        user_capabilities?: {
+            delete: number
+            edit: string
+        }
+        // direct_access?: {}
+        indirect_access?: {
+            descendant_roles: string[]
+            role: {
+                id: number
+                name: string
+                description: string
+                user_capabilities: {
+                    unattach: boolean
+                }
+            }
+        }[]
+    }
+}
