063d62118359486e1bfefa7c27b90db2e24181d6
James Talton <jtalton@redhat.com>
2022-10-13 11:34:18 -0400

    initial support for tasks (#20)
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    Signed-off-by: James Talton <jtalton@redhat.com>

---
+++ frontend/hub/collections/task-management/Task.tsx
@@ -0,0 +1,7 @@
+export interface Task {
+    name: string
+    pulp_created: string
+    started_at: string
+    finished_at: string
+    state: 'completed' | 'failed'
+}
---
+++ frontend/hub/collections/task-management/Tasks.tsx
@@ -0,0 +1,55 @@
+import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+import { ITableColumn, IToolbarFilter, PageBody, PageHeader, PageLayout, PageTable, SinceCell, TextCell } from '../../../../framework'
+import { StatusCell } from '../../../common/status'
+import { nameKeyFn } from '../../useHubView'
+import { usePulpView } from '../../usePulpView'
+import { Task } from './Task'
+
+export function Tasks() {
+    const { t } = useTranslation()
+    const toolbarFilters = useTaskFilters()
+    const tableColumns = useTasksColumns()
+    const view = usePulpView<Task>('/api/automation-hub/pulp/api/v3/tasks/', nameKeyFn, toolbarFilters, tableColumns)
+    return (
+        <PageLayout>
+            <PageHeader title={t('Tasks')} />
+            <PageBody>
+                <PageTable<Task>
+                    toolbarFilters={toolbarFilters}
+                    tableColumns={tableColumns}
+                    errorStateTitle={t('Error loading tasks')}
+                    emptyStateTitle={t('No tasks yet')}
+                    {...view}
+                />
+            </PageBody>
+        </PageLayout>
+    )
+}
+
+export function useTasksColumns(_options?: { disableSort?: boolean; disableLinks?: boolean }) {
+    const { t } = useTranslation()
+    const tableColumns = useMemo<ITableColumn<Task>[]>(
+        () => [
+            { header: t('Name'), cell: (task) => <TextCell text={task.name} />, sort: 'name' },
+            { header: t('Created'), cell: (task) => <SinceCell value={task.pulp_created} /> },
+            { header: t('Started'), cell: (task) => <SinceCell value={task.started_at} /> },
+            { header: t('Finished'), cell: (task) => <SinceCell value={task.finished_at} /> },
+            { header: t('Status'), cell: (task) => <StatusCell status={task.state} /> },
+        ],
+        [t]
+    )
+    return tableColumns
+}
+
+export function useTaskFilters() {
+    const { t } = useTranslation()
+    const toolbarFilters = useMemo<IToolbarFilter[]>(
+        () => [
+            { key: 'name', label: t('Task name'), type: 'string', query: 'name__contains' },
+            { key: 'status', label: t('Status'), type: 'string', query: 'state' },
+        ],
+        [t]
+    )
+    return toolbarFilters
+}
