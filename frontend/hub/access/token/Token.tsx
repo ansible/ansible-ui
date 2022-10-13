abaa33795189a2150c22c0774b88cc0cc76d7e10
James Talton <jtalton@redhat.com>
2022-10-13 15:14:04 -0400

    cleanup API token page
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/hub/access/token/Token.tsx
+++ frontend/hub/access/token/Token.tsx
@@ -1,23 +1,59 @@
-import { ActionGroup, Alert, Button, PageSection, Stack, StackItem } from '@patternfly/react-core'
+import { ActionGroup, Alert, Button, PageSection, Stack } from '@patternfly/react-core'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { PageBody, PageHeader, PageLayout } from '../../../../framework'
+import { CopyCell, PageBody, PageHeader, PageLayout } from '../../../../framework'
+import { requestPost } from '../../../Data'
 
 export function Token() {
     const { t } = useTranslation()
+    const [working, setWorking] = useState(false)
+    const [token, setToken] = useState('')
+    const [error, setError] = useState('')
+    const onClick = async () => {
+        try {
+            setWorking(true)
+            setError('')
+            const result = await requestPost<{ token: string }>('/api/automation-hub/v3/auth/token/', {})
+            setToken(result.token)
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err.message)
+            } else {
+                setError(t('An unknown error occured.'))
+            }
+        } finally {
+            setWorking(false)
+        }
+    }
     return (
         <PageLayout>
-            <PageHeader title={t('API Token')} />
+            <PageHeader title={t('API Token')} description={t('An API token can be used to authenticate the ansible-galaxy client.')} />
             <PageBody>
                 <PageSection variant="light">
-                    <Stack hasGutter>
-                        <StackItem>{t('Use this token to authenticate the ansible-galaxy client.')}</StackItem>
-                        <Alert variant="warning" isInline title="Warning">
-                            {t('Loading a new token will delete your old token.')}
-                        </Alert>
-                        <ActionGroup>
-                            <Button>{t('Load Token')}</Button>
-                        </ActionGroup>
-                    </Stack>
+                    {token ? (
+                        <Stack hasGutter>
+                            <Alert
+                                variant="warning"
+                                isInline
+                                title={t('Copy this token now. This is the only time you will ever see it.')}
+                            />
+                            <CopyCell text={token} />
+                        </Stack>
+                    ) : (
+                        <Stack hasGutter>
+                            <Alert variant="warning" isInline title={t('Loading a new token will delete your old token.')} />
+                            <ActionGroup>
+                                <Button
+                                    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                                    onClick={onClick}
+                                    isDisabled={working}
+                                >
+                                    {t('Load Token')}
+                                </Button>
+                            </ActionGroup>
+                            {error && <Alert variant="danger" isInline title={error} />}
+                        </Stack>
+                    )}
                 </PageSection>
             </PageBody>
         </PageLayout>
