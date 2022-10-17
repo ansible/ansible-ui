703e4a996f6113d98f70a9b5d1f07eeeec4de670
James Talton <jtalton@redhat.com>
2022-10-17 14:37:44 -0400

    add filter
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/hub/collections/collections/Collections.tsx
+++ frontend/hub/collections/collections/Collections.tsx
@@ -142,6 +142,7 @@ export function useCollectionFilters() {
     const toolbarFilters = useMemo<IToolbarFilter[]>(
         () => [
             { key: 'keywords', label: t('Keywords'), type: 'string', query: 'keywords' },
+            { key: 'namespace', label: t('Namespace'), type: 'string', query: 'namespace' },
             { key: 'tags', label: t('Tags'), type: 'string', query: 'tags' },
             { key: 'sign-state', label: t('Sign state'), type: 'string', query: 'sign_state' },
         ],
