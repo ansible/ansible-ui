4bbb821edb386a0b0b36ab04f9419249f6e6dd2f
James Talton <jtalton@redhat.com>
2022-10-26 09:57:43 -0400

    update dependencies
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/controller/ControllerRouter.tsx
+++ frontend/controller/ControllerRouter.tsx
@@ -1,3 +1,5 @@
+import { Bullseye, Spinner } from '@patternfly/react-core'
+import { lazy, Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Debug from '../common/Debug'
 import { RouteE } from '../Routes'
@@ -16,7 +18,6 @@ import { InstanceGroups } from './administration/instance-groups/InstanceGroups'
 import { EditInstance } from './administration/instances/EditInstance'
 import { InstanceDetails } from './administration/instances/InstanceDetails'
 import { Instances } from './administration/instances/Instances'
-import { Topology } from './administration/topology/Topology'
 import { Credentials } from './resources/credentials/Credentials'
 import { Hosts } from './resources/hosts/Hosts'
 import { Inventories } from './resources/inventories/Inventories'
@@ -24,86 +25,100 @@ import { Projects } from './resources/projects/Projects'
 import Settings from './settings/Settings'
 import Dashboard from './views/Dashboard'
 
+// Lasy loading Topology so that pf-topology and it dependencies can be bundled into a chunk
+const Topology = lazy(() => import('./administration/topology/Topology'))
+
 export function ControllerRouter() {
   return (
-    <Routes>
-      <Route path={RouteE.Dashboard.replace(RouteE.Controller, '')} element={<Dashboard />} />
-      {/* <Route path={RouteE.Jobs} element={JobsPage} /> */}
-      {/* <Route path={RouteE.Schedules} element={SchedulesPage} /> */}
-      {/* <Route path={RouteE.ActivityStream} element={ActivityStreeam} /> */}
-      {/* <Route path={RouteE.WorkflowApprovals} element={WorkflowApprovalsPage} /> */}
+    <Suspense
+      fallback={
+        <Bullseye>
+          <Spinner />
+        </Bullseye>
+      }
+    >
+      <Routes>
+        <Route path={RouteE.Dashboard.replace(RouteE.Controller, '')} element={<Dashboard />} />
+        {/* <Route path={RouteE.Jobs} element={JobsPage} /> */}
+        {/* <Route path={RouteE.Schedules} element={SchedulesPage} /> */}
+        {/* <Route path={RouteE.ActivityStream} element={ActivityStreeam} /> */}
+        {/* <Route path={RouteE.WorkflowApprovals} element={WorkflowApprovalsPage} /> */}
 
-      {/* <Route path={RouteE.Templates} element={TemplatesPage} /> */}
+        {/* <Route path={RouteE.Templates} element={TemplatesPage} /> */}
 
-      <Route path={RouteE.Credentials.replace(RouteE.Controller, '')} element={<Credentials />} />
+        <Route path={RouteE.Credentials.replace(RouteE.Controller, '')} element={<Credentials />} />
 
-      <Route path={RouteE.Projects.replace(RouteE.Controller, '')} element={<Projects />} />
-      {/* <Route path={RouteE.ProjectDetails} element={ProjectsDetailsPage} /> */}
-      {/* <Route path={RouteE.ProjectEdit} element={ProjectEditPage} /> */}
+        <Route path={RouteE.Projects.replace(RouteE.Controller, '')} element={<Projects />} />
+        {/* <Route path={RouteE.ProjectDetails} element={ProjectsDetailsPage} /> */}
+        {/* <Route path={RouteE.ProjectEdit} element={ProjectEditPage} /> */}
 
-      <Route path={RouteE.Inventories.replace(RouteE.Controller, '')} element={<Inventories />} />
+        <Route path={RouteE.Inventories.replace(RouteE.Controller, '')} element={<Inventories />} />
 
-      <Route path={RouteE.Hosts.replace(RouteE.Controller, '')} element={<Hosts />} />
+        <Route path={RouteE.Hosts.replace(RouteE.Controller, '')} element={<Hosts />} />
 
-      <Route
-        path={RouteE.Organizations.replace(RouteE.Controller, '')}
-        element={<Organizations />}
-      />
-      <Route
-        path={RouteE.OrganizationDetails.replace(RouteE.Controller, '')}
-        element={<OrganizationDetails />}
-      />
-      <Route
-        path={RouteE.CreateOrganization.replace(RouteE.Controller, '')}
-        element={<EditOrganization />}
-      />
-      <Route
-        path={RouteE.EditOrganization.replace(RouteE.Controller, '')}
-        element={<EditOrganization />}
-      />
+        <Route
+          path={RouteE.Organizations.replace(RouteE.Controller, '')}
+          element={<Organizations />}
+        />
+        <Route
+          path={RouteE.OrganizationDetails.replace(RouteE.Controller, '')}
+          element={<OrganizationDetails />}
+        />
+        <Route
+          path={RouteE.CreateOrganization.replace(RouteE.Controller, '')}
+          element={<EditOrganization />}
+        />
+        <Route
+          path={RouteE.EditOrganization.replace(RouteE.Controller, '')}
+          element={<EditOrganization />}
+        />
 
-      <Route path={RouteE.Users.replace(RouteE.Controller, '')} element={<Users />} />
-      <Route
-        path={RouteE.UserDetails.replace(RouteE.Controller, '')}
-        element={<UserDetailsPage />}
-      />
-      <Route path={RouteE.CreateUser.replace(RouteE.Controller, '')} element={<CreateUser />} />
-      <Route path={RouteE.EditUser.replace(RouteE.Controller, '')} element={<EditUser />} />
+        <Route path={RouteE.Users.replace(RouteE.Controller, '')} element={<Users />} />
+        <Route
+          path={RouteE.UserDetails.replace(RouteE.Controller, '')}
+          element={<UserDetailsPage />}
+        />
+        <Route path={RouteE.CreateUser.replace(RouteE.Controller, '')} element={<CreateUser />} />
+        <Route path={RouteE.EditUser.replace(RouteE.Controller, '')} element={<EditUser />} />
 
-      <Route path={RouteE.Teams.replace(RouteE.Controller, '')} element={<Teams />} />
-      <Route path={RouteE.TeamDetails.replace(RouteE.Controller, '')} element={<TeamDetails />} />
-      <Route path={RouteE.CreateTeam.replace(RouteE.Controller, '')} element={<EditTeam />} />
-      <Route path={RouteE.EditTeam.replace(RouteE.Controller, '')} element={<EditTeam />} />
+        <Route path={RouteE.Teams.replace(RouteE.Controller, '')} element={<Teams />} />
+        <Route path={RouteE.TeamDetails.replace(RouteE.Controller, '')} element={<TeamDetails />} />
+        <Route path={RouteE.CreateTeam.replace(RouteE.Controller, '')} element={<EditTeam />} />
+        <Route path={RouteE.EditTeam.replace(RouteE.Controller, '')} element={<EditTeam />} />
 
-      {/* <Route path={RouteE.CredentialTypes} element={CredentialTypesPage} /> */}
-      {/* <Route path={RouteE.Notifications} element={NotificationsPage} /> */}
-      {/* <Route path={RouteE.ManagementJobs} element={ManagementJobsPage} /> */}
+        {/* <Route path={RouteE.CredentialTypes} element={CredentialTypesPage} /> */}
+        {/* <Route path={RouteE.Notifications} element={NotificationsPage} /> */}
+        {/* <Route path={RouteE.ManagementJobs} element={ManagementJobsPage} /> */}
 
-      <Route
-        path={RouteE.InstanceGroups.replace(RouteE.Controller, '')}
-        element={<InstanceGroups />}
-      />
+        <Route
+          path={RouteE.InstanceGroups.replace(RouteE.Controller, '')}
+          element={<InstanceGroups />}
+        />
 
-      <Route path={RouteE.Instances.replace(RouteE.Controller, '')} element={<Instances />} />
-      <Route
-        path={RouteE.InstanceDetails.replace(RouteE.Controller, '')}
-        element={<InstanceDetails />}
-      />
-      <Route path={RouteE.EditInstance.replace(RouteE.Controller, '')} element={<EditInstance />} />
+        <Route path={RouteE.Instances.replace(RouteE.Controller, '')} element={<Instances />} />
+        <Route
+          path={RouteE.InstanceDetails.replace(RouteE.Controller, '')}
+          element={<InstanceDetails />}
+        />
+        <Route
+          path={RouteE.EditInstance.replace(RouteE.Controller, '')}
+          element={<EditInstance />}
+        />
 
-      {/* <Route path={RouteE.Applications} element={ApplicationsPage} /> */}
-      <Route
-        path={RouteE.ExecutionEnvironments.replace(RouteE.Controller, '')}
-        element={<ExecutionEnvironments />}
-      />
+        {/* <Route path={RouteE.Applications} element={ApplicationsPage} /> */}
+        <Route
+          path={RouteE.ExecutionEnvironments.replace(RouteE.Controller, '')}
+          element={<ExecutionEnvironments />}
+        />
 
-      <Route path={RouteE.TopologyView.replace(RouteE.Controller, '')} element={<Topology />} />
+        <Route path={RouteE.TopologyView.replace(RouteE.Controller, '')} element={<Topology />} />
 
-      <Route path={RouteE.Settings.replace(RouteE.Controller, '')} element={<Settings />} />
+        <Route path={RouteE.Settings.replace(RouteE.Controller, '')} element={<Settings />} />
 
-      <Route path={RouteE.ControllerDebug.replace(RouteE.Controller, '')} element={<Debug />} />
+        <Route path={RouteE.ControllerDebug.replace(RouteE.Controller, '')} element={<Debug />} />
 
-      <Route path="*" element={<Navigate to={RouteE.Login} replace />} />
-    </Routes>
+        <Route path="*" element={<Navigate to={RouteE.Login} replace />} />
+      </Routes>
+    </Suspense>
   )
 }
