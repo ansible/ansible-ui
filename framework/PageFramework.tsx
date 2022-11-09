7bc3e7eef25979900169ffa35ab0a3a5f2015f85
James Talton <jtalton@redhat.com>
2022-11-09 11:53:05 -0500

    Alert toaster support (#59)
    * alert toaster support
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * typescript build fixes
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- framework/PageFramework.tsx
+++ framework/PageFramework.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import { PageAlertToasterProvider } from './PageAlertToaster'
 import { PageDialogProvider } from './PageDialog'
 import { SettingsProvider } from './Settings'
 import { FrameworkTranslationsProvider } from './useFrameworkTranslations'
@@ -7,7 +8,9 @@ export function PageFrameworkProvider(props: { children: ReactNode }) {
   return (
     <FrameworkTranslationsProvider>
       <SettingsProvider>
-        <PageDialogProvider>{props.children}</PageDialogProvider>
+        <PageDialogProvider>
+          <PageAlertToasterProvider>{props.children}</PageAlertToasterProvider>
+        </PageDialogProvider>
       </SettingsProvider>
     </FrameworkTranslationsProvider>
   )
