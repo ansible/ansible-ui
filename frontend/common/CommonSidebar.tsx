519b3f7f691f2e9f26c9a32296f1ccf23ae4d17b
James Talton <jtalton@redhat.com>
2022-11-11 16:52:03 -0500

    common sidebar (#64)
    * common sidebar
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * formatting
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * linting
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    Signed-off-by: James Talton <jtalton@redhat.com>

---
+++ frontend/common/CommonSidebar.tsx
@@ -0,0 +1,78 @@
+import { Nav, NavItem, NavItemSeparator, NavList, PageSidebar } from '@patternfly/react-core'
+import { ReactNode, useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useBreakpoint } from '../../framework'
+import { useSettings } from '../../framework/Settings'
+import { useAutomationServers } from '../automation-servers/AutomationServerProvider'
+import { AutomationServerSwitcher } from '../automation-servers/AutomationServerSwitcher'
+import { RouteE } from '../Routes'
+import { isRouteActive } from './Masthead'
+
+export function CommonSidebar(props: {
+  isNavOpen: boolean
+  setNavOpen: (open: boolean) => void
+  children?: ReactNode
+}) {
+  const { t } = useTranslation()
+  const location = useLocation()
+  const navigate = useNavigate()
+  const settings = useSettings()
+  const { automationServer } = useAutomationServers()
+
+  const isXl = useBreakpoint('xl')
+  const { isNavOpen, setNavOpen } = props
+  const onClick = useCallback(
+    (route: RouteE) => {
+      navigate(route)
+      if (!isXl) {
+        setNavOpen(false)
+      }
+    },
+    [navigate, isXl, setNavOpen]
+  )
+  return (
+    <PageSidebar
+      isNavOpen={isNavOpen}
+      style={{
+        backgroundColor:
+          settings.theme === 'dark' ? 'var(--pf-global--BackgroundColor--300)' : undefined,
+      }}
+      nav={
+        <Nav>
+          <NavList>
+            <NavItemSeparator style={{ margin: 0 }} />
+            <NavItem
+              isActive={isRouteActive(
+                automationServer?.type === 'controller'
+                  ? RouteE.ControllerAutomationServers
+                  : automationServer?.type === 'hub'
+                  ? RouteE.HubAutomationServers
+                  : automationServer?.type === 'eda'
+                  ? RouteE.EdaAutomationServers
+                  : RouteE.AutomationServers,
+                location
+              )}
+              onClick={() =>
+                onClick(
+                  automationServer?.type === 'controller'
+                    ? RouteE.ControllerAutomationServers
+                    : automationServer?.type === 'hub'
+                    ? RouteE.HubAutomationServers
+                    : automationServer?.type === 'eda'
+                    ? RouteE.EdaAutomationServers
+                    : RouteE.AutomationServers
+                )
+              }
+            >
+              {t('Automation Servers')}
+            </NavItem>
+            <AutomationServerSwitcher />
+            <NavItemSeparator style={{ margin: 0 }} />
+            {props.children}
+          </NavList>
+        </Nav>
+      }
+    />
+  )
+}
--- frontend/controller/ControllerSidebar.tsx
+++ frontend/controller/ControllerSidebar.tsx
@@ -1,17 +1,9 @@
-import {
-  Nav,
-  NavExpandable,
-  NavItem,
-  NavItemSeparator,
-  NavList,
-  PageSidebar,
-} from '@patternfly/react-core'
+import { NavExpandable, NavItem } from '@patternfly/react-core'
 import { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useBreakpoint } from '../../framework'
-import { useSettings } from '../../framework/Settings'
-import { AutomationServerSwitcher } from '../automation-servers/AutomationServerSwitcher'
+import { CommonSidebar } from '../common/CommonSidebar'
 import { isRouteActive } from '../common/Masthead'
 import { RouteE } from '../Routes'
 
@@ -19,50 +11,28 @@ export function ControllerSidebar(props: {
   isNavOpen: boolean
   setNavOpen: (open: boolean) => void
 }) {
+  const { isNavOpen, setNavOpen } = props
   const { t } = useTranslation()
   const location = useLocation()
   const navigate = useNavigate()
-  const settings = useSettings()
-
   const isXl = useBreakpoint('xl')
-  const { isNavOpen, setNavOpen } = props
   const onClick = useCallback(
     (route: RouteE) => {
       navigate(route)
-      if (!isXl) {
-        setNavOpen(false)
-      }
+      if (!isXl) setNavOpen(false)
     },
     [navigate, isXl, setNavOpen]
   )
   return (
-    <PageSidebar
-      isNavOpen={isNavOpen}
-      style={{
-        backgroundColor:
-          settings.theme === 'dark' ? 'var(--pf-global--BackgroundColor--300)' : undefined,
-      }}
-      nav={
-        <>
-          <Nav>
-            <NavList>
-              <NavItemSeparator style={{ margin: 0 }} />
-              <NavItem
-                isActive={isRouteActive(RouteE.ControllerAutomationServers, location)}
-                onClick={() => onClick(RouteE.ControllerAutomationServers)}
-              >
-                {t('Automation Servers')}
-              </NavItem>
-              <AutomationServerSwitcher />
-              <NavItemSeparator style={{ margin: 0 }} />
-              <NavItem
-                isActive={isRouteActive(RouteE.Dashboard, location)}
-                onClick={() => onClick(RouteE.Dashboard)}
-              >
-                {t('Dashboard')}
-              </NavItem>
+    <CommonSidebar isNavOpen={isNavOpen} setNavOpen={setNavOpen}>
+      <NavItem
+        isActive={isRouteActive(RouteE.Dashboard, location)}
+        onClick={() => onClick(RouteE.Dashboard)}
+      >
+        {t('Dashboard')}
+      </NavItem>
 
-              {/* <NavExpandable
+      {/* <NavExpandable
                             key="controller"
                             title="Controller"
                             isExpanded
@@ -71,7 +41,7 @@ export function ControllerSidebar(props: {
                                 location
                             )}
                         > */}
-              {/* <NavExpandable
+      {/* <NavExpandable
                             key="views"
                             title="Views"
                             isExpanded
@@ -96,153 +66,140 @@ export function ControllerSidebar(props: {
                                 <Link to={RouteE.WorkflowApprovals}>Workflow approvals</Link>
                             </NavItem>
                         </NavExpandable> */}
-              <NavExpandable
-                key="resources"
-                title={t('Resources')}
-                isExpanded
-                isActive={isRouteActive(
-                  [
-                    RouteE.Templates,
-                    RouteE.Credentials,
-                    RouteE.Projects,
-                    RouteE.Inventories,
-                    RouteE.Hosts,
-                  ],
-                  location
-                )}
-              >
-                <NavItem
-                  isActive={isRouteActive(RouteE.Templates, location)}
-                  onClick={() => onClick(RouteE.Templates)}
-                >
-                  {t('Templates')}
-                </NavItem>
-                <NavItem
-                  isActive={isRouteActive(RouteE.Credentials, location)}
-                  onClick={() => onClick(RouteE.Credentials)}
-                >
-                  {t('Credentials')}
-                </NavItem>
-                <NavItem
-                  isActive={isRouteActive(RouteE.Projects, location)}
-                  onClick={() => onClick(RouteE.Projects)}
-                >
-                  {t('Projects')}
-                </NavItem>
-                <NavItem
-                  isActive={isRouteActive(RouteE.Inventories, location)}
-                  onClick={() => onClick(RouteE.Inventories)}
-                >
-                  {t('Inventories')}
-                </NavItem>
-                <NavItem
-                  isActive={isRouteActive(RouteE.Hosts, location)}
-                  onClick={() => onClick(RouteE.Hosts)}
-                >
-                  {t('Hosts')}
-                </NavItem>
-              </NavExpandable>
-              <NavExpandable
-                key="access"
-                title={t('Access')}
-                isExpanded
-                isActive={isRouteActive(
-                  [RouteE.Organizations, RouteE.Users, RouteE.Teams],
-                  location
-                )}
-              >
-                <NavItem
-                  isActive={isRouteActive(RouteE.Organizations, location)}
-                  onClick={() => onClick(RouteE.Organizations)}
-                >
-                  {t('Organizations')}
-                </NavItem>
-                <NavItem
-                  isActive={isRouteActive(RouteE.Teams, location)}
-                  onClick={() => onClick(RouteE.Teams)}
-                >
-                  {t('Teams')}
-                </NavItem>
-                <NavItem
-                  isActive={isRouteActive(RouteE.Users, location)}
-                  onClick={() => onClick(RouteE.Users)}
-                >
-                  {t('Users')}
-                </NavItem>
-              </NavExpandable>
-              <NavExpandable
-                key="administration"
-                title="Administration"
-                isExpanded
-                isActive={isRouteActive(
-                  [
-                    RouteE.CredentialTypes,
-                    RouteE.Notifications,
-                    RouteE.ManagementJobs,
-                    RouteE.InstanceGroups,
-                    RouteE.Instances,
-                    RouteE.Applications,
-                    RouteE.ExecutionEnvironments,
-                    RouteE.TopologyView,
-                  ],
-                  location
-                )}
-              >
-                {/* <NavItem isActive={isRouteActive(RouteE.CredentialTypes, location)}>
+      <NavExpandable
+        key="resources"
+        title={t('Resources')}
+        isExpanded
+        isActive={isRouteActive(
+          [RouteE.Templates, RouteE.Credentials, RouteE.Projects, RouteE.Inventories, RouteE.Hosts],
+          location
+        )}
+      >
+        <NavItem
+          isActive={isRouteActive(RouteE.Templates, location)}
+          onClick={() => onClick(RouteE.Templates)}
+        >
+          {t('Templates')}
+        </NavItem>
+        <NavItem
+          isActive={isRouteActive(RouteE.Credentials, location)}
+          onClick={() => onClick(RouteE.Credentials)}
+        >
+          {t('Credentials')}
+        </NavItem>
+        <NavItem
+          isActive={isRouteActive(RouteE.Projects, location)}
+          onClick={() => onClick(RouteE.Projects)}
+        >
+          {t('Projects')}
+        </NavItem>
+        <NavItem
+          isActive={isRouteActive(RouteE.Inventories, location)}
+          onClick={() => onClick(RouteE.Inventories)}
+        >
+          {t('Inventories')}
+        </NavItem>
+        <NavItem
+          isActive={isRouteActive(RouteE.Hosts, location)}
+          onClick={() => onClick(RouteE.Hosts)}
+        >
+          {t('Hosts')}
+        </NavItem>
+      </NavExpandable>
+      <NavExpandable
+        key="access"
+        title={t('Access')}
+        isExpanded
+        isActive={isRouteActive([RouteE.Organizations, RouteE.Users, RouteE.Teams], location)}
+      >
+        <NavItem
+          isActive={isRouteActive(RouteE.Organizations, location)}
+          onClick={() => onClick(RouteE.Organizations)}
+        >
+          {t('Organizations')}
+        </NavItem>
+        <NavItem
+          isActive={isRouteActive(RouteE.Teams, location)}
+          onClick={() => onClick(RouteE.Teams)}
+        >
+          {t('Teams')}
+        </NavItem>
+        <NavItem
+          isActive={isRouteActive(RouteE.Users, location)}
+          onClick={() => onClick(RouteE.Users)}
+        >
+          {t('Users')}
+        </NavItem>
+      </NavExpandable>
+      <NavExpandable
+        key="administration"
+        title="Administration"
+        isExpanded
+        isActive={isRouteActive(
+          [
+            RouteE.CredentialTypes,
+            RouteE.Notifications,
+            RouteE.ManagementJobs,
+            RouteE.InstanceGroups,
+            RouteE.Instances,
+            RouteE.Applications,
+            RouteE.ExecutionEnvironments,
+            RouteE.TopologyView,
+          ],
+          location
+        )}
+      >
+        {/* <NavItem isActive={isRouteActive(RouteE.CredentialTypes, location)}>
                                 <Link to={RouteE.CredentialTypes}>Credential types</Link>
                             </NavItem> */}
-                {/* <NavItem isActive={isRouteActive(RouteE.Notifications, location)}>
+        {/* <NavItem isActive={isRouteActive(RouteE.Notifications, location)}>
                                 <Link to={RouteE.Notifications}>Notifications</Link>
                             </NavItem> */}
-                {/* <NavItem isActive={isRouteActive(RouteE.ManagementJobs, location)}>
+        {/* <NavItem isActive={isRouteActive(RouteE.ManagementJobs, location)}>
                                 <Link to={RouteE.ManagementJobs}>Management jobs</Link>
                             </NavItem> */}
-                <NavItem
-                  isActive={isRouteActive(RouteE.InstanceGroups, location)}
-                  onClick={() => onClick(RouteE.InstanceGroups)}
-                >
-                  {t('Instance groups')}
-                </NavItem>
-                <NavItem
-                  isActive={isRouteActive(RouteE.Instances, location)}
-                  onClick={() => onClick(RouteE.Instances)}
-                >
-                  {t('Instances')}
-                </NavItem>
-                {/* <NavItem isActive={isRouteActive(RouteE.Applications, location)}>
+        <NavItem
+          isActive={isRouteActive(RouteE.InstanceGroups, location)}
+          onClick={() => onClick(RouteE.InstanceGroups)}
+        >
+          {t('Instance groups')}
+        </NavItem>
+        <NavItem
+          isActive={isRouteActive(RouteE.Instances, location)}
+          onClick={() => onClick(RouteE.Instances)}
+        >
+          {t('Instances')}
+        </NavItem>
+        {/* <NavItem isActive={isRouteActive(RouteE.Applications, location)}>
                                 <Link to={RouteE.Applications}>Applications</Link>
                             </NavItem> */}
-                <NavItem
-                  isActive={isRouteActive(RouteE.ExecutionEnvironments, location)}
-                  onClick={() => onClick(RouteE.ExecutionEnvironments)}
-                >
-                  {t('Execution Environments')}
-                </NavItem>
-                <NavItem
-                  isActive={isRouteActive(RouteE.TopologyView, location)}
-                  onClick={() => onClick(RouteE.TopologyView)}
-                >
-                  {t('Topology view')}
-                </NavItem>
-              </NavExpandable>
-              {/* <NavGroup>
+        <NavItem
+          isActive={isRouteActive(RouteE.ExecutionEnvironments, location)}
+          onClick={() => onClick(RouteE.ExecutionEnvironments)}
+        >
+          {t('Execution Environments')}
+        </NavItem>
+        <NavItem
+          isActive={isRouteActive(RouteE.TopologyView, location)}
+          onClick={() => onClick(RouteE.TopologyView)}
+        >
+          {t('Topology view')}
+        </NavItem>
+      </NavExpandable>
+      {/* <NavGroup>
                             <NavItem isActive={isRouteActive(RouteE.Settings, location)}>
                                 <Link to={RouteE.Settings}>Settings</Link>
                             </NavItem>
                         </NavGroup> */}
-              {/* </NavExpandable> */}
-              {process.env.NODE_ENV === 'development' && (
-                <NavItem
-                  isActive={isRouteActive(RouteE.ControllerDebug, location)}
-                  onClick={() => onClick(RouteE.ControllerDebug)}
-                >
-                  {t('Debug')}
-                </NavItem>
-              )}
-            </NavList>
-          </Nav>
-        </>
-      }
-    />
+      {/* </NavExpandable> */}
+      {process.env.NODE_ENV === 'development' && (
+        <NavItem
+          isActive={isRouteActive(RouteE.ControllerDebug, location)}
+          onClick={() => onClick(RouteE.ControllerDebug)}
+        >
+          {t('Debug')}
+        </NavItem>
+      )}
+    </CommonSidebar>
   )
 }
