4e63b68aaf2b247dca6f265dabfe8570502a8407
James Talton <jtalton@redhat.com>
2022-11-15 12:53:14 -0500

    Remove react router dep from framework and cleanup text cell (#70)
    * remove react-router dep from framework and cleanup TextCell
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix TextCell navigation
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix tests
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- framework/cells/PageCells.tsx
+++ framework/cells/PageCells.tsx
@@ -7,16 +7,12 @@ import {
   LabelGroup,
   Split,
   SplitItem,
-  Tooltip,
   Truncate,
 } from '@patternfly/react-core'
 import { DateTime } from 'luxon'
-import { ReactNode, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { IconWrapper } from './components/IconWrapper'
-import { getPatternflyColor, PatternFlyColor } from './components/patternfly-colors'
-import { useSettings } from './Settings'
-import { useFrameworkTranslations } from './useFrameworkTranslations'
+import { useEffect, useState } from 'react'
+import { useSettings } from '../Settings'
+import { useFrameworkTranslations } from '../useFrameworkTranslations'
 
 export function LabelsCell(props: { labels: string[] }) {
   return (
@@ -34,75 +30,6 @@ export function DateCell(props: { value: number | string }) {
     <Split hasGutter>
       <SplitItem>{date.toLocaleDateString()}</SplitItem>
       <SplitItem>{date.toLocaleTimeString()}</SplitItem>
-    </Split>
-  )
-}
-
-export function TextCell(props: {
-  icon?: ReactNode
-  text?: string
-  iconSize?: 'sm' | 'md' | 'lg'
-  to?: string
-  onClick?: () => void
-  textColor?: PatternFlyColor
-}) {
-  return (
-    <Split>
-      {props.icon && (
-        <SplitItem>
-          <IconWrapper size={props.iconSize ?? 'md'}>{props.icon}</IconWrapper>
-        </SplitItem>
-      )}
-      {props.to ? (
-        <SplitItem>
-          {/* <Link to={props.to}>
-                        <Truncate position="middle" content={props.text ?? ''} />
-                    </Link> */}
-          <Tooltip content={props.text ?? ''}>
-            <div
-              style={{
-                maxWidth: 300,
-                whiteSpace: 'nowrap',
-                textOverflow: 'ellipsis',
-                overflow: 'hidden',
-              }}
-            >
-              <Link to={props.to}>{props.text ?? ''}</Link>
-            </div>
-          </Tooltip>
-        </SplitItem>
-      ) : props.onClick !== undefined ? (
-        <SplitItem onClick={props.onClick}>
-          <Button
-            variant="link"
-            isInline
-            style={{
-              whiteSpace: 'nowrap',
-            }}
-          >
-            {props.text}
-          </Button>
-        </SplitItem>
-      ) : (
-        <SplitItem
-          style={{
-            color: props.textColor ? getPatternflyColor(props.textColor) : undefined,
-          }}
-        >
-          <Tooltip content={props.text ?? ''}>
-            <div
-              style={{
-                maxWidth: 300,
-                whiteSpace: 'nowrap',
-                textOverflow: 'ellipsis',
-                overflow: 'hidden',
-              }}
-            >
-              {props.text ?? ''}
-            </div>
-          </Tooltip>
-        </SplitItem>
-      )}
     </Split>
   )
 }
