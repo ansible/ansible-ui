51c98de4890ccfe56703ef1063261d119e80ca93
James Talton <jtalton@redhat.com>
2022-11-16 08:55:58 -0500

    Framework page navigation (#73)
    * framework page navigation
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * support PageFramework navigation
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- framework/components/usePageNavigate.tsx
+++ framework/components/usePageNavigate.tsx
@@ -1,12 +1,33 @@
-import { useCallback } from 'react'
+import { createContext, ReactNode, useCallback, useContext } from 'react'
+
+const PageNavigateCallbackContext = createContext<((url: string) => void) | undefined>(undefined)
+
+function usePageNavigateCallback() {
+  return useContext(PageNavigateCallbackContext)
+}
+
+export function PageNavigateCallbackContextProvider(props: {
+  callback?: (url: string) => void
+  children: ReactNode
+}) {
+  return (
+    <PageNavigateCallbackContext.Provider value={props.callback}>
+      {props.children}
+    </PageNavigateCallbackContext.Provider>
+  )
+}
 
 export function usePageNavigate() {
-  const navigate = useCallback((to?: string) => {
-    if (to?.startsWith('http')) {
-      open(to, '_blank')
-    } else {
-      open(to, '_self')
-    }
-  }, [])
+  const pageNavigateCallback = usePageNavigateCallback()
+  const navigate = useCallback(
+    (to?: string) => {
+      if (to?.startsWith('http')) {
+        open(to, '_blank')
+      } else {
+        pageNavigateCallback ? pageNavigateCallback(to ?? '') : open(to, '_self')
+      }
+    },
+    [pageNavigateCallback]
+  )
   return navigate
 }
