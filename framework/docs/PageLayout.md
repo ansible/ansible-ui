c1971873babc9d49ac1b462ce97b29db9fa6c60f
James Talton <jtalton@redhat.com>
2022-11-18 16:05:44 -0500

    generate docs
    Signed-off-by: James Talton <jtalton@redhat.com>

---
+++ framework/docs/PageLayout.md
@@ -0,0 +1,14 @@
+## PageLayout()
+
+PageLayout enables the layout of the page to be responsive.
+
+**Example**
+
+```js
+<Page>
+  <PageLayout>
+    <PageHeader />
+    <PageBody />...</PageBody>
+  </PageLayout>
+<Page>
+```
---
+++ scripts/gendocs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR/..
+
+OUT=./framework/docs
+
+components=("PageHeader PageLayout BulkActionDialog")
+for component in ${components}
+do
+  echo $component
+  ./node_modules/.bin/jsdoc2md ./framework/${component}.tsx --configure ./jsdoc2md.json > $OUT/${component}.md
+  sed -i -e "s/<code>/\`/" "$OUT/${component}.md"
+  sed -i -e "s/<\/code>/\`/" "$OUT/${component}.md"
+  sed -i -e "s/<\/code>/\`/" "$OUT/${component}.md"
+  sed -i -e "s/<p>//" "$OUT/${component}.md"
+  sed -i -e "s/<\/p>//" "$OUT/${component}.md"
+  sed -i -e '/^\*\*Kind\*\*/d' "$OUT/${component}.md"
+  sed -i -e '/^<a/d' "$OUT/${component}.md"
+  sed -i -e "s/&lt;/</" "$OUT/${component}.md"
+  sed -i -e "s/&gt;/>/" "$OUT/${component}.md"
+done
+
+rm -f $OUT/*.md-e
+./node_modules/.bin/prettier --write $OUT/*.md
