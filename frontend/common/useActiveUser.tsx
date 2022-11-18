9fcee25cb482a57080d042d504c2d638ab703f64
James Talton <jtalton@redhat.com>
2022-11-18 10:52:27 -0500

    useActiveUser
    Signed-off-by: James Talton <jtalton@redhat.com>

---
+++ frontend/common/useActiveUser.tsx
@@ -0,0 +1,18 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react'
+import { useGet } from '../common/useItem'
+import { User } from '../controller/interfaces/User'
+
+const ActiveUserContext = createContext<User | null | undefined>(undefined)
+
+export function useActiveUser() {
+  return useContext(ActiveUserContext)
+}
+
+export function ActiveUserProvider(props: { children?: ReactNode }) {
+  const [activeUser, setActiveUser] = useState<User | null | undefined>(undefined)
+  const userResponse = useGet<User>('/api/v2/me/')
+  useEffect(() => setActiveUser(userResponse.data ?? null), [userResponse.data])
+  return (
+    <ActiveUserContext.Provider value={activeUser}>{props.children}</ActiveUserContext.Provider>
+  )
+}
