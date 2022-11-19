30a6db7243fdf4c219975fbf0d0e2321f07c6777
James Talton <jtalton@redhat.com>
2022-11-19 09:35:30 -0500

    Framework docs 2 (#80)
    * framework documentation
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * prettier
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- framework/PageLayout.tsx
+++ framework/PageLayout.tsx
@@ -3,13 +3,15 @@ import ErrorBoundary from './components/ErrorBoundary'
 import { useFrameworkTranslations } from './useFrameworkTranslations'
 
 /**
- * PageLayout enables the layout of the page to be responsive.
+ * The PageLayout is used as the container for the contents of the page.
+ * It enables page components to leverage full page layout and scrolling of sub content.
+ * An example is a full page table that the page header, toolbar, column headers, and pagination stay fixed, but the rows of the table can scroll.
  *
  * @example
  * <Page>
  *   <PageLayout>
  *     <PageHeader />
- *     <PageBody />...</PageBody>
+ *     ...
  *   </PageLayout>
  * <Page>
  */
