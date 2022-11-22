40ae1662d8ea511b448b82411221b917f7dfd43c
James Talton <jtalton@redhat.com>
2022-11-22 08:44:29 -0500

    useActiveUser (#78)
    * useActiveUser
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * jsdoc
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * jsdoc
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    Signed-off-by: James Talton <jtalton@redhat.com>

---
+++ frontend/common/useActiveUser.tsx
@@ -0,0 +1,22 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react'
+import { useGet } from '../common/useItem'
+import { User } from '../controller/interfaces/User'
+
+const ActiveUserContext = createContext<User | null | undefined>(undefined)
+
+/**
+ * Get the active logged in user
+ * @returns undefined while querying, null if user not logged in, otherwise the User.
+ */
+export function useActiveUser() {
+  return useContext(ActiveUserContext)
+}
+
+export function ActiveUserProvider(props: { children?: ReactNode }) {
+  const [activeUser, setActiveUser] = useState<User | null | undefined>(undefined)
+  const userResponse = useGet<User>('/api/v2/me/')
+  useEffect(() => setActiveUser(userResponse.data ?? null), [userResponse.data])
+  return (
+    <ActiveUserContext.Provider value={activeUser}>{props.children}</ActiveUserContext.Provider>
+  )
+}
