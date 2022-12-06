57b2f292a89dab3e4bd3aeb6b4940b8105b7693e
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2022-12-06 16:30:56 -0500

    Relaunch a job from the Jobs list (#105)
    * relaunch
    
    * hooks for toolbar and row actions

--- frontend/controller/views/jobs/jobUtils.tsx
+++ frontend/controller/views/jobs/jobUtils.tsx
@@ -1,3 +1,5 @@
+import { UnifiedJob } from '../../interfaces/UnifiedJob'
+
 /** Returns the jobs API endpoint based on the job type */
 export function getJobsAPIUrl(type: string) {
   switch (type) {
@@ -18,4 +20,21 @@ export function getJobsAPIUrl(type: string) {
 
 export function isJobRunning(status: string) {
   return ['new', 'pending', 'waiting', 'running'].includes(status)
+}
+
+/** Returns the jobs relaunch endpoint based on the job type */
+export function getRelaunchEndpoint(job: UnifiedJob) {
+  const baseUrl = getJobsAPIUrl(job.type)
+  switch (job.type) {
+    case 'ad_hoc_command':
+    case 'workflow_job':
+    case 'job':
+      return `${baseUrl}${job.id}/relaunch/`
+    case 'inventory_update':
+      return job.inventory_source
+        ? `/api/v2/inventory_sources/${job.inventory_source}/update/`
+        : undefined
+    case 'project_update':
+      return job.project ? `/api/v2/projects/${job.project}/update/` : undefined
+  }
 }
