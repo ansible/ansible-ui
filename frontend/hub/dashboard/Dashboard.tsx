02b31b373e360400501580ae59ffed892eacb36f
James Talton <jtalton@redhat.com>
2022-12-08 13:12:42 -0500

    Jtalton   page dashboard work (#111)
    * page dashboard
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * dashboard work
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * dashboard
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/hub/dashboard/Dashboard.tsx
+++ frontend/hub/dashboard/Dashboard.tsx
@@ -5,17 +5,24 @@ import {
   CardBody,
   CardHeader,
   CardTitle,
+  Gallery,
   PageSection,
   Stack,
   StackItem,
 } from '@patternfly/react-core';
-import { ExternalLinkAltIcon } from '@patternfly/react-icons';
-import { PageHeader, PageLayout } from '../../../framework';
+import { useTranslation } from 'react-i18next';
+import { PageHeader, PageLayout, pfSuccess } from '../../../framework';
 import { LoadingPage } from '../../../framework/components/LoadingPage';
+import { PageDashboardDonutCard } from '../../../framework/PageDashboard/PageDonutChart';
+import { RouteE } from '../../Routes';
+import { FeaturedCollections } from '../automation-content/collections/FeaturedCollections';
+import { useCollections } from '../automation-content/collections/hooks/useCollections';
 import { useNamespaces } from '../automation-content/namespaces/hooks/useNamespaces';
 
 export function HubDashboard() {
+  const { t } = useTranslation();
   const namespaces = useNamespaces();
+  const collections = useCollections();
 
   if (!namespaces) {
     return <LoadingPage />;
@@ -24,7 +31,7 @@ export function HubDashboard() {
   return (
     <PageLayout>
       <PageHeader
-        title="Welcome to Private Automation Hub"
+        title="Welcome to Automation Hub"
         description="Find and use content that is supported by Redhat and out partners to devliver reassurance for the most demanding environments. Get started by exploring the options below."
       />
       <PageSection>
@@ -44,31 +51,21 @@ export function HubDashboard() {
               </CardBody>
             </Card>
           )}
-          <Card isRounded isFlat>
-            <CardHeader>
-              <CardTitle>Featured collections</CardTitle>
-            </CardHeader>
-            <CardBody>
-              <Stack hasGutter>
-                <StackItem>Sync from the featured collections below or add your own.</StackItem>
-                <StackItem>
-                  <Button>Add your own collection</Button>
-                </StackItem>
-                <StackItem>
-                  <Button
-                    icon={
-                      <span style={{ marginRight: 4 }}>
-                        <ExternalLinkAltIcon />
-                      </span>
-                    }
-                    variant="link"
-                  >
-                    Browse all certified collections
-                  </Button>
-                </StackItem>
-              </Stack>
-            </CardBody>
-          </Card>
+          <FeaturedCollections />
+          <Gallery hasGutter minWidths={{ default: '300px' }}>
+            <PageDashboardDonutCard
+              title={t('Collections')}
+              items={[
+                { label: t('Collections'), count: collections?.length ?? 0, color: pfSuccess },
+              ]}
+              to={RouteE.Collections}
+            />
+            <PageDashboardDonutCard
+              title={t('Namespaces')}
+              items={[{ label: t('Namespaces'), count: namespaces?.length ?? 0, color: pfSuccess }]}
+              to={RouteE.Namespaces}
+            />
+          </Gallery>
         </Stack>
       </PageSection>
     </PageLayout>
