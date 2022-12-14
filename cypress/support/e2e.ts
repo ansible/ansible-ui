7ec3961a0870d7cbb17b942101dc1dd91d6f5cf2
James Talton <jtalton@redhat.com>
2022-12-14 14:28:25 -0500

    support running cypress tests against both a live server and mocks
    Signed-off-by: James Talton <jtalton@redhat.com>

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,39 +1,52 @@
+/// <reference types="cypress" />
 // import 'cypress-axe';
 import './commands';
 import { mockController } from './mock-controller';
 
 before(() => {
+  const server = Cypress.env('server') ? (Cypress.env('server') as string) : 'mock';
+  const username = Cypress.env('username') ? (Cypress.env('username') as string) : 'admin';
+  const password = Cypress.env('password') ? (Cypress.env('password') as string) : 'password';
+
+  // cy.injectAxe()
+
+  Cypress.Cookies.defaults({
+    preserve: ['awx_sessionid', 'useLoggedIn', 'csrftoken'],
+  });
+
   window.localStorage.setItem('access', 'true');
   window.localStorage.setItem('theme', 'light');
 
-  if (Cypress.env('mock')) {
+  if (!Cypress.env('server')) {
     mockController();
   }
 
-  cy.visit(`/controller/debug`);
-  // cy.injectAxe()
+  cy.visit(`/automation-servers`, { retryOnStatusCodeFailure: true, retryOnNetworkFailure: true });
 
-  // cy.visit(`/login`, { retryOnStatusCodeFailure: true, retryOnNetworkFailure: true })
-  // cy.get('#server').type('https://localhost:8043')
-  // cy.get('#username').type('test')
-  // cy.get('#password').type('test')
-  // cy.get('button[type=submit]').click()
+  cy.clickButton(/^Add automation server$/);
+  cy.typeByLabel(/^Name$/, 'Controller');
+  cy.typeByLabel(/^Url$/, server);
+  cy.get('button[type=submit]').click();
 
-  // Cypress.Cookies.preserveOnce(names...)
+  cy.contains('a', /^Controller$/).click();
+  cy.typeByLabel(/^Username$/, username);
+  cy.typeByLabel(/^Password$/, password);
+  cy.get('button[type=submit]').click();
+
+  cy.contains(/^Welcome to Automation Controller$/);
+
+  // Cypress.Cookies.
 
   // Cypress.Cookies.defaults({
   //     preserve: ['_csrf', '_oauth_proxy', 'acm-access-token-cookie'],
   // })
-  // cy.login()
-  // cy.visit(`/`, { retryOnStatusCodeFailure: true, retryOnNetworkFailure: true })
-  // cy.get('.pf-c-page__main').contains('Red Hat', { timeout: 5 * 60 * 1000 })
 });
 
 beforeEach(() => {
   window.localStorage.setItem('access', 'true');
   window.localStorage.setItem('theme', 'light');
 
-  if (Cypress.env('mock')) {
+  if (!Cypress.env('server')) {
     mockController();
   }
 });
