574697cbacaf8b2a07aa05956bb7c3c70eff85f9
James Talton <jtalton@redhat.com>
2023-01-11 10:42:43 -0500

    fix proxy
    Signed-off-by: James Talton <jtalton@redhat.com>

--- proxy/request-handler.ts
+++ proxy/request-handler.ts
@@ -7,15 +7,19 @@ import { serve } from './serve';
 
 export function requestHandler(req: Http2ServerRequest, res: Http2ServerResponse): void {
   try {
-    const cookieHeader = req.headers[HTTP2_HEADER_COOKIE];
-    let cookies: Record<string, string> | undefined;
-    if (typeof cookieHeader === 'string') {
-      cookies = cookie.parse(cookieHeader);
+    let proxyRequest = false;
+    if (req.url.startsWith('/api/')) {
+      const cookieHeader = req.headers[HTTP2_HEADER_COOKIE];
+      let cookies: Record<string, string> | undefined;
+      if (typeof cookieHeader === 'string') {
+        cookies = cookie.parse(cookieHeader);
+      }
+
+      const target = cookies?.['server'];
+      proxyRequest = !!target;
     }
 
-    const target = cookies?.['server'];
-
-    if (target) {
+    if (proxyRequest) {
       proxyHandler(req, res);
     } else {
       void serve(req, res);
