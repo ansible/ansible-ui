bfd2ad0b2501d9da70595dfe7920205980e230d8
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2023-01-11 11:38:20 -0500

    Setup for unit tests with Cypress component testing (#153)
    * Setup for unit tests with Cypress component testing
    
    Basic hello world spec
    
    linter + prettier
    
    revert change to tsconfig
    
    Test render of Jobs component
    
    linter fix
    
    npm script to run component tests
    
    Test functions and move test file next to the component
    
    additional test scenarios
    
    code coverage
    
    updated md file
    
    * remove redundant file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,67 @@
-import codeCoverage from '@cypress/code-coverage/task'
-import { defineConfig } from 'cypress'
+import codeCoverage from '@cypress/code-coverage/task';
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-    viewportWidth: 1600,
-    viewportHeight: 1120,
-    pageLoadTimeout: 120000,
-    defaultCommandTimeout: 30000,
-    e2e: {
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
-            codeCoverage(on, config)
-            return config
-        },
-        baseUrl: 'https://localhost:3002/',
+  viewportWidth: 1600,
+  viewportHeight: 1120,
+  pageLoadTimeout: 120000,
+  defaultCommandTimeout: 30000,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
+      codeCoverage(on, config);
+      return config;
     },
-})
+    baseUrl: 'https://localhost:3002/',
+  },
+
+  component: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
+      codeCoverage(on, config);
+      return config;
+    },
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+      webpackConfig: {
+        mode: 'development',
+        devtool: false,
+        resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+        module: {
+          rules: [
+            { test: /\.(hbs|yaml)$/, type: 'asset/source' },
+            { test: /\.(svg)$/, use: '@svgr/webpack' },
+            { test: /\.(jpg|jpeg|png|gif|ttf|eot|woff|woff2)$/, type: 'asset/resource' },
+            {
+              test: /\.css$/,
+              use: ['style-loader', 'css-loader'],
+            },
+            {
+              test: /\.(ts|tsx|js|jsx)$/,
+              exclude: /node_modules/,
+              use: [
+                'coverage-istanbul-loader',
+                {
+                  loader: 'babel-loader',
+                  options: {
+                    plugins: [
+                      'istanbul',
+                      ['@babel/plugin-transform-modules-commonjs', { loose: true }],
+                    ],
+                  },
+                },
+              ],
+              type: 'javascript/auto',
+            },
+          ],
+        },
+      },
+    },
+    specPattern: 'frontend/controller/**/*.cy.tsx',
+    supportFile: 'cypress/support/component.ts',
+  },
+});
