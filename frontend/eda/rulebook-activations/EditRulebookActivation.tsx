15099335bfeef7b2e4b9c756c674013f3887613b
lgalis <lgalis@redhat.com>
2023-01-16 11:58:58 -0500

    Updated rulebook activation New/Edit page. Update PageForm Switch parameters
    Updated rulebook activation New/Edit page. Update PageForm Switch parameters

--- frontend/eda/rulebook-activations/EditRulebookActivation.tsx
+++ frontend/eda/rulebook-activations/EditRulebookActivation.tsx
@@ -3,12 +3,23 @@ import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSWRConfig } from 'swr';
-import { PageForm, PageFormSubmitHandler, PageHeader, PageLayout } from '../../../framework';
+import {
+  PageForm,
+  PageFormSelectOption,
+  PageFormSubmitHandler,
+  PageHeader,
+  PageLayout,
+} from '../../../framework';
 import { PageFormSchema } from '../../../framework/PageForm/PageFormSchema';
 import { useGet } from '../../common/useItem';
 import { requestPatch, requestPost } from '../../Data';
 import { RouteE } from '../../Routes';
 import { EdaRulebookActivation } from '../interfaces/EdaRulebookActivation';
+import { PageFormTextInput } from '../../../framework/PageForm/Inputs/PageFormTextInput';
+import { EdaRulebook } from '../interfaces/EdaRulebook';
+import { EdaProject } from '../interfaces/EdaProject';
+import { EdaInventory } from '../interfaces/EdaInventory';
+import { PageFormSwitch } from '../../../framework/PageForm/Inputs/PageFormSwitch';
 
 export function EditRulebookActivation() {
   const { t } = useTranslation();
@@ -18,6 +29,11 @@ export function EditRulebookActivation() {
   const { data: rulebookActivation } = useGet<EdaRulebookActivation>(
     `/api/activations/${id.toString()}`
   );
+
+  const { data: rulebooks } = useGet<EdaRulebook[]>('/api/rulebooks');
+  const { data: projects } = useGet<EdaProject[]>('/api/projects');
+  const { data: inventories } = useGet<EdaInventory[]>('/api/inventories');
+  const { data: extra_vars } = useGet<EdaInventory[]>('/api/extra_vars');
 
   const RulebookActivationSchemaType = useMemo(
     () =>
@@ -85,7 +101,6 @@ export function EditRulebookActivation() {
             ]}
           />
           <PageForm
-            schema={RulebookActivationSchemaType}
             submitText={t('Save rulebook activation')}
             onSubmit={onSubmit}
             cancelText={t('Cancel')}
@@ -114,7 +129,101 @@ export function EditRulebookActivation() {
           cancelText={t('Cancel')}
           onCancel={onCancel}
         >
-          <PageFormSchema schema={RulebookActivationSchemaType} />
+          <PageFormTextInput
+            name={'name'}
+            label={t('Name')}
+            id={'name'}
+            isRequired={true}
+            placeholder={t('Insert name here')}
+          />
+          <PageFormTextInput
+            name={'description'}
+            label={t('Description')}
+            id={'description'}
+            placeholder={t('Insert description here')}
+          />
+          <PageFormSelectOption
+            name={'inventory_id'}
+            label={t('Inventory')}
+            placeholderText={t('Select inventory')}
+            options={
+              inventories
+                ? inventories.map((item: { name: string; id: number }) => ({
+                    label: item.name,
+                    value: item.id,
+                  }))
+                : []
+            }
+          />
+          <PageFormTextInput
+            name={'execution_environment'}
+            label={t('Execution environment')}
+            id={'execution_environment'}
+            placeholder={t('Insert execution environment here')}
+          />
+          <PageFormSelectOption
+            name={'rulebook_id'}
+            label={t('Rulebook')}
+            placeholderText={t('Select rulebook')}
+            options={
+              rulebooks
+                ? rulebooks.map((item: { name: string; id: number }) => ({
+                    label: item.name,
+                    value: item.id,
+                  }))
+                : []
+            }
+          />
+          <PageFormSelectOption
+            name={'restart_policy'}
+            label={t('Restart policy')}
+            placeholderText={t('Select  a restart policy')}
+            options={[]}
+          />
+          <PageFormSelectOption
+            name={'project_id'}
+            label={t('Project')}
+            placeholderText={t('Select project')}
+            options={
+              projects
+                ? projects.map((item: { name: string; id: number }) => ({
+                    label: item.name,
+                    value: item.id,
+                  }))
+                : []
+            }
+          />
+          <PageFormTextInput
+            name={'working_directory'}
+            label={t('Working directory')}
+            id={'working_directory'}
+            placeholder={t('Insert working directory here')}
+          />
+          <PageFormSwitch
+            label={t('Rulebook activation enabled?')}
+            labelOn={'Enabled'}
+            labelOff={'Disabled'}
+            name="is_enabled"
+          />
+          <PageFormSwitch
+            label={t('Throttle')}
+            labelOn={'Enabled'}
+            labelOff={'Disabled'}
+            name="throttle"
+          />
+          <PageFormSelectOption
+            name={'extra_var_id'}
+            label={t('Extra vars')}
+            placeholderText={t('Select extra vars')}
+            options={
+              extra_vars
+                ? extra_vars.map((item: { name: string; id: number }) => ({
+                    label: item.name,
+                    value: item.id,
+                  }))
+                : []
+            }
+          />
         </PageForm>
       </PageLayout>
     );
