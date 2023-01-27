a428c39bc0233f509de492dc7017920718ae918c
lgalis <lgalis@redhat.com>
2023-01-27 15:00:34 -0500

    Actions pages

---
+++ frontend/eda/views/actions/hooks/useHostsColumns.tsx
@@ -0,0 +1,50 @@
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { ITableColumn, TextCell } from '../../../../../framework';
+import { RouteE } from '../../../../Routes';
+import { EdaHost } from '../../../interfaces/EdaHost';
+import { formatDateString } from '../../../../../framework/utils/formatDateString';
+
+export function useHostsColumns() {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  return useMemo<ITableColumn<EdaHost>[]>(
+    () => [
+      {
+        header: t('Name'),
+        cell: (host) => (
+          <TextCell
+            text={host.host}
+            onClick={() => navigate(RouteE.EdaRulesetDetails.replace(':id', host.id.toString()))}
+          />
+        ),
+        sort: 'ost',
+        defaultSort: true,
+        card: 'name',
+        list: 'name',
+      },
+      {
+        header: t('Rule'),
+        cell: (host) => (
+          <TextCell
+            text={host?.rule.name}
+            onClick={() =>
+              navigate(RouteE.EdaRulesetDetails.replace(':id', (host?.rule?.id || '').toString()))
+            }
+          />
+        ),
+        sort: 'rule',
+        defaultSort: true,
+      },
+      {
+        header: t('Last fired date'),
+        cell: (host) => (
+          <TextCell text={host?.fired_date ? formatDateString(new Date(host.fired_date)) : ''} />
+        ),
+        sort: 'fired_at',
+      },
+    ],
+    [navigate, t]
+  );
+}
