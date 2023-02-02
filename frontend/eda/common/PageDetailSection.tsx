eb04f2bbc8060fe486d66c2c407efd1454ca2aeb
James Talton <jtalton@redhat.com>
2023-02-02 10:14:35 -0500

    cleanup theming (#202)
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/eda/common/PageDetailSection.tsx
+++ frontend/eda/common/PageDetailSection.tsx
@@ -1,21 +1,11 @@
 import { DescriptionList, PageSection } from '@patternfly/react-core';
 import { ReactNode } from 'react';
-import { useSettings } from '../../../framework/Settings';
 
 export function PageDetailsSection(props: { children?: ReactNode; disablePadding?: boolean }) {
   const { disablePadding } = props;
-  const settings = useSettings();
   const isCompact = false;
   return (
-    <PageSection
-      padding={{ default: 'noPadding' }}
-      style={{
-        backgroundColor:
-          settings.activeTheme === 'dark'
-            ? 'var(--pf-global--BackgroundColor--300)'
-            : 'var(--pf-global--BackgroundColor--100)',
-      }}
-    >
+    <PageSection padding={{ default: 'noPadding' }} className="dark-2">
       <DescriptionList
         style={{ maxWidth: 1200, padding: disablePadding ? undefined : 24 }}
         isCompact={isCompact}
