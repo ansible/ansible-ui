5fe92b79e0706c1f43472f3e9b337c0d7743774b
lgalis <lgalis@users.noreply.github.com>
2023-02-07 14:19:49 -0500

    WIP - [AAP-7495]  EDA dashboard - rules over time chart  (#206)
    * Dashboard page
    
    * Add rules chart

--- frontend/eda/dashboard/Dashboard.tsx
+++ frontend/eda/dashboard/Dashboard.tsx
@@ -5,7 +5,8 @@ import {
   CardBody,
   CardHeader,
   CardTitle,
-  Gallery,
+  Grid,
+  GridItem,
   PageSection,
   Stack,
   StackItem,
@@ -15,6 +16,7 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { PageHeader } from '../../../framework';
 import { RouteE } from '../../Routes';
+import ActionsChartCard from './ActionsChartCard';
 
 export default function Dashboard() {
   const navigate = useNavigate();
@@ -28,53 +30,66 @@ export default function Dashboard() {
         )}
       />
       <PageSection>
-        <Gallery hasGutter minWidths={{ default: '300px' }}>
-          <Card isRounded isFlat>
-            <CardHeader>
-              <CardTitle>Create a project</CardTitle>
-            </CardHeader>
-            <CardBody>
-              <Stack hasGutter>
-                <StackItem>To get started, create a project.</StackItem>
-                <StackItem>
-                  <Button onClick={() => navigate(RouteE.CreateEdaProject)}>Create project</Button>
-                </StackItem>
-              </Stack>
-            </CardBody>
-          </Card>
-
-          <Card isRounded isFlat>
-            <CardHeader>
-              <CardTitle>Create a rulebook</CardTitle>
-            </CardHeader>
-            <CardBody>
-              <Stack hasGutter>
-                <StackItem>To get started, create a rulebook.</StackItem>
-                <StackItem>
-                  <Button onClick={() => navigate(RouteE.CreateEdaRulebook)}>
-                    Create rulebook
-                  </Button>
-                </StackItem>
-              </Stack>
-            </CardBody>
-          </Card>
-
-          <Card isRounded isFlat>
-            <CardHeader>
-              <CardTitle>Create an inventory</CardTitle>
-            </CardHeader>
-            <CardBody>
-              <Stack hasGutter>
-                <StackItem>To get started, create an inventory.</StackItem>
-                <StackItem>
-                  <Button onClick={() => navigate(RouteE.CreateEdaInventory)}>
-                    Create inventory
-                  </Button>
-                </StackItem>
-              </Stack>
-            </CardBody>
-          </Card>
-        </Gallery>
+        <Stack hasGutter>
+          <StackItem>
+            <ActionsChartCard />
+          </StackItem>
+          <StackItem>
+            <Grid hasGutter>
+              <GridItem span={6}>
+                <Card isRounded isFlat>
+                  <CardHeader>
+                    <CardTitle>Create a project</CardTitle>
+                  </CardHeader>
+                  <CardBody>
+                    <Stack hasGutter>
+                      <StackItem>To get started, create a project.</StackItem>
+                      <StackItem>
+                        <Button onClick={() => navigate(RouteE.CreateEdaProject)}>
+                          Create project
+                        </Button>
+                      </StackItem>
+                    </Stack>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem span={6}>
+                <Card isRounded isFlat>
+                  <CardHeader>
+                    <CardTitle>Create a rulebook</CardTitle>
+                  </CardHeader>
+                  <CardBody>
+                    <Stack hasGutter>
+                      <StackItem>To get started, create a rulebook.</StackItem>
+                      <StackItem>
+                        <Button onClick={() => navigate(RouteE.CreateEdaRulebook)}>
+                          Create rulebook
+                        </Button>
+                      </StackItem>
+                    </Stack>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem span={6}>
+                <Card isRounded isFlat>
+                  <CardHeader>
+                    <CardTitle>Create an inventory</CardTitle>
+                  </CardHeader>
+                  <CardBody>
+                    <Stack hasGutter>
+                      <StackItem>To get started, create an inventory.</StackItem>
+                      <StackItem>
+                        <Button onClick={() => navigate(RouteE.CreateEdaInventory)}>
+                          Create inventory
+                        </Button>
+                      </StackItem>
+                    </Stack>
+                  </CardBody>
+                </Card>
+              </GridItem>
+            </Grid>
+          </StackItem>
+        </Stack>
       </PageSection>
     </Fragment>
   );
