20d90c6c5c29391425bfb6631800e08b6d58ec3b
Vidya Nambiar <vnambiar@redhat.com>
2023-02-08 17:00:27 -0500

    Delete roles from Team Access list

--- frontend/controller/access/teams/TeamPage/TeamAccess.tsx
+++ frontend/controller/access/teams/TeamPage/TeamAccess.tsx
@@ -1,14 +1,23 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { ButtonVariant, Label, LabelGroup } from '@patternfly/react-core';
+import {
+  ButtonVariant,
+  Chip,
+  ChipGroup,
+  DescriptionList,
+  DescriptionListDescription,
+  DescriptionListGroup,
+  DescriptionListTerm,
+} from '@patternfly/react-core';
 import { MinusCircleIcon, PlusIcon } from '@patternfly/react-icons';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { IPageAction, PageActionType, PageTable } from '../../../../../framework';
 import { RouteE } from '../../../../Routes';
 import { Team } from '../../../interfaces/Team';
-import { User } from '../../../interfaces/User';
+import { AccessRole, User } from '../../../interfaces/User';
 import { useControllerView } from '../../../useControllerView';
+import { useDeleteRoleConfirmationDialog } from '../../common/DeleteRoleConfirmation';
 import { useRemoveUsersFromTeams } from '../../users/hooks/useRemoveUsersFromTeams';
 import { useSelectUsersAddTeams } from '../../users/hooks/useSelectUsersAddTeams';
 import { useUsersColumns } from '../../users/hooks/useUsersColumns';
@@ -20,21 +29,73 @@ export function TeamAccess(props: { team: Team }) {
 
   const toolbarFilters = useUsersFilters();
 
+  const openDeleteRoleConfirmationDialog = useDeleteRoleConfirmationDialog();
+  const deleteRole = (role: AccessRole, user: User) => {
+    openDeleteRoleConfirmationDialog({
+      role,
+      username: user.username,
+      onConfirm: () => {
+        console.log('Confirm deletion');
+      },
+    });
+  };
+
   const tableColumns = useUsersColumns();
-  tableColumns.splice(1, 0, {
+  tableColumns.splice(1, 1);
+  tableColumns.splice(4, 1);
+  tableColumns.push({
     header: t('Roles'),
-    cell: (user) => (
-      <LabelGroup>
-        {user.summary_fields?.indirect_access?.map((access) => (
-          <Label
-            key={access.role.id}
-            color={user.is_superuser || user.is_system_auditor ? 'orange' : undefined}
-          >
-            {access.role.name}
-          </Label>
-        ))}
-      </LabelGroup>
-    ),
+    cell: (user) => {
+      return (
+        <DescriptionList
+          isHorizontal
+          horizontalTermWidthModifier={{
+            default: '8ch',
+          }}
+        >
+          {user?.user_roles?.length ? (
+            <DescriptionListGroup>
+              <DescriptionListTerm>{t('User roles')}</DescriptionListTerm>
+              <DescriptionListDescription>
+                <ChipGroup>
+                  {user.user_roles.map((role) => (
+                    <Chip
+                      key={role.id}
+                      onClick={() => deleteRole(role, user)}
+                      isReadOnly={!role.user_capabilities.unattach}
+                      ouiaId={`${role.name}-${role.id}`}
+                      closeBtnAriaLabel={t`Remove ${role.name} chip`}
+                    >
+                      {role.name}
+                    </Chip>
+                  ))}
+                </ChipGroup>
+              </DescriptionListDescription>
+            </DescriptionListGroup>
+          ) : null}
+          {user?.team_roles?.length ? (
+            <DescriptionListGroup>
+              <DescriptionListTerm>{t('Team roles')}</DescriptionListTerm>
+              <DescriptionListDescription>
+                <ChipGroup>
+                  {user.team_roles.map((role) => (
+                    <Chip
+                      key={role.id}
+                      onClick={() => deleteRole(role, user)}
+                      isReadOnly={!role.user_capabilities.unattach}
+                      ouiaId={`${role.name}-${role.id}`}
+                      closeBtnAriaLabel={t`Remove ${role.name} chip`}
+                    >
+                      {role.name}
+                    </Chip>
+                  ))}
+                </ChipGroup>
+              </DescriptionListDescription>
+            </DescriptionListGroup>
+          ) : null}
+        </DescriptionList>
+      );
+    },
   });
 
   const view = useControllerView<User>({
@@ -43,6 +104,32 @@ export function TeamAccess(props: { team: Team }) {
     tableColumns,
     disableQueryString: true,
   });
+
+  type Access = {
+    descendant_roles: string[];
+    role: AccessRole;
+  };
+
+  useEffect(() => {
+    const users = view.pageItems as User[];
+    function sortRoles(access: Access, user: User) {
+      const { role } = access;
+      if (role.team_id) {
+        user.team_roles?.push(role);
+      } else {
+        user.user_roles?.push(role);
+      }
+    }
+
+    if (users?.length > 0) {
+      for (const user of users) {
+        user.team_roles = [];
+        user.user_roles = [];
+        user.summary_fields?.direct_access?.forEach((access) => sortRoles(access, user));
+        user.summary_fields?.indirect_access?.forEach((access) => sortRoles(access, user));
+      }
+    }
+  }, [view.pageItems]);
 
   const selectUsersAddTeams = useSelectUsersAddTeams(() => void view.refresh());
   const removeUsersFromTeams = useRemoveUsersFromTeams();
