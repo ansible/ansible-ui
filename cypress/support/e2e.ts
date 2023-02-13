f66e23b8c5d48ebe4c29111e62b9a0bcb89691e2
James Talton <jtalton@redhat.com>
2023-02-13 17:12:09 -0500

    cypress switch to version 12 and add live e2e github action (#214)
    * cypress switch to version 12 and add live e2e github action
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * github action name
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * e2e fixes
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * unit test tweak
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix e2e tests
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix tests
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix e2e live action
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fixed
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix e2e live
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix e2e tests
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * cleanup e2e and support e2e cleanup
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * rename variables
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * streamline tests
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * strreamline login
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * add support for cleaning up e2e orgs older than 2 hours
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * disable project e2e tests that cannot be run against a live server
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * cleanup code checks
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * minor tweaks
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * update projects e2e tests to handle delete
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * Add Docker Build and Archive step
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * Add comments to make clear what is going on in cleanup code
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * docker tweak
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * test using docker image
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * make cypress deleteRequest have optional param
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * Add code coverage to actions
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * merge
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix docker image job
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix docker build job
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix docker image build
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix e2e testing
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * text coverage action
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * cleanup cypress package scripts
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix job
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * adjust code coverage minimum
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    ---------
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,52 +1,11 @@
 /// <reference types="cypress" />
 // import 'cypress-axe';
 import './commands';
-import { mockController } from './mock-controller';
 
 before(() => {
-  // cy.injectAxe()
-
-  window.localStorage.setItem('theme', 'light');
-
-  if (Cypress.env('server')) {
-    const server = Cypress.env('server') ? (Cypress.env('server') as string) : 'mock';
-    const username = Cypress.env('username') ? (Cypress.env('username') as string) : 'admin';
-    const password = Cypress.env('password') ? (Cypress.env('password') as string) : 'password';
-
-    Cypress.Cookies.defaults({
-      preserve: ['awx_sessionid', 'useLoggedIn', 'csrftoken'],
-    });
-
-    cy.visit(`/automation-servers`, {
-      retryOnStatusCodeFailure: true,
-      retryOnNetworkFailure: true,
-    });
-
-    cy.clickButton(/^Add automation server$/);
-    cy.typeByLabel(/^Name$/, 'Controller');
-    cy.typeByLabel(/^Url$/, server);
-    cy.get('button[type=submit]').click();
-
-    cy.contains('a', /^AWX Ausible server$/).click();
-    cy.typeByLabel(/^Username$/, username);
-    cy.typeByLabel(/^Password$/, password);
-    cy.get('button[type=submit]').click();
-
-    cy.contains(/^Welcome to/);
-  } else {
-    mockController();
-    cy.visit(`/controller/debug`, { retryOnStatusCodeFailure: true, retryOnNetworkFailure: true });
-  }
+  cy.login();
 });
 
-beforeEach(() => {
-  window.localStorage.setItem('theme', 'light');
-
-  if (!Cypress.env('server')) {
-    mockController();
-  }
-});
-
-afterEach(() => {
-  // cy.checkA11y()
-});
+// afterEach(() => {
+//   // cy.checkA11y()
+// });
