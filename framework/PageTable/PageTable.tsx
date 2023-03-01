088f3c1ed1550c733bc21dabd91c8df4b7d9ac87
Vidya Nambiar <vnambiar@redhat.com>
2023-03-01 14:00:32 -0500

    Make table view width match list and card views

--- framework/PageTable/PageTable.tsx
+++ framework/PageTable/PageTable.tsx
@@ -218,7 +218,7 @@ export function PageTable<T extends object>(props: PageTableProps<T>) {
   }
 
   return (
-    <>
+    <PageSection>
       <PageTableToolbar
         {...props}
         openColumnModal={openColumnModal}
@@ -258,7 +258,7 @@ export function PageTable<T extends object>(props: PageTableProps<T>) {
         <PagePagination {...props} />
       )}
       {columnModal}
-    </>
+    </PageSection>
   );
 }
 
