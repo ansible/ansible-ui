455aa02365de5c0b886f063a59c90e2cff260c6f
lgalis <lgalis@redhat.com>
2023-03-03 14:20:53 -0500

    Update Rulebook activations and projects columns

--- frontend/eda/rulebook-activations/hooks/useRulebookActivationColumns.tsx
+++ frontend/eda/rulebook-activations/hooks/useRulebookActivationColumns.tsx
@@ -4,20 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import { ITableColumn, TextCell } from '../../../../framework';
 import { RouteObj } from '../../../Routes';
 import { EdaRulebookActivation } from '../../interfaces/EdaRulebookActivation';
+import { SwitchCell } from '../../common/SwitchCell';
+import { StatusLabelCell } from '../../common/StatusLabelCell';
 
 export function useRulebookActivationColumns() {
   const { t } = useTranslation();
   const navigate = useNavigate();
   return useMemo<ITableColumn<EdaRulebookActivation>[]>(
     () => [
-      {
-        header: t('ID'),
-        cell: (inventory) => inventory.id,
-        sort: 'id',
-        card: 'hidden',
-        list: 'hidden',
-        isIdColumn: true,
-      },
       {
         header: t('Name'),
         cell: (rulebookActivation) => (
@@ -37,6 +31,39 @@ export function useRulebookActivationColumns() {
         card: 'name',
         list: 'name',
         defaultSort: true,
+      },
+      {
+        header: t('Activation status'),
+        cell: (activation) => <StatusLabelCell status={activation.status} />,
+        sort: 'status',
+        defaultSort: true,
+      },
+      {
+        header: t('Number of rules'),
+        cell: (activation) => (
+          <TextCell text={`${activation?.rules_count ? activation.rules_count : 0}`} />
+        ),
+        sort: 'rules_count',
+      },
+      {
+        header: t('Fire count'),
+        cell: (activation) => (
+          <TextCell text={`${activation?.fired_count ? activation.fired_count : 0}`} />
+        ),
+        sort: 'fired_count',
+      },
+      {
+        header: t('Activation instances'),
+        cell: (activation) => (
+          <TextCell text={`${activation?.instances_count ? activation.instances_count : 0}`} />
+        ),
+        sort: 'instance_count',
+      },
+      {
+        header: '',
+        style: 'maxWidth: 0',
+
+        cell: (activation) => <SwitchCell state={activation.is_enabled || false} />,
       },
     ],
     [navigate, t]
