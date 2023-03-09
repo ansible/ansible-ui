e0f0d651b6b4083e35d7d7eed24999fc6f5583b6
lgalis <lgalis@users.noreply.github.com>
2023-03-09 10:22:59 -0500

    [AAP-7594] [AAP-7592] Added Groups and Roles pages (#233)
    * Add Groups and Roles pages
    
    * Update user page
    
    * Merge path changes
    
    * Update create/edit user page

---
+++ frontend/eda/UserAccess/Roles/hooks/useDeleteRole.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { compareStrings, useBulkConfirmation } from '../../../../../framework';
+import { requestDelete } from '../../../../Data';
+import { idKeyFn } from '../../../../hub/useHubView';
+import { EdaRole } from '../../../interfaces/EdaRole';
+import { useRoleColumns } from './useRoleColumns';
+import { API_PREFIX } from '../../../constants';
+
+export function useDeleteRoles(onComplete: (roles: EdaRole[]) => void) {
+  const { t } = useTranslation();
+  const confirmationColumns = useRoleColumns();
+  const actionColumns = useMemo(() => [confirmationColumns[0]], [confirmationColumns]);
+  const bulkAction = useBulkConfirmation<EdaRole>();
+  return useCallback(
+    (roles: EdaRole[]) => {
+      bulkAction({
+        title: t('Permanently delete Roles', { count: roles.length }),
+        confirmText: t('Yes, I confirm that I want to delete these {{count}} Roles.', {
+          count: roles.length,
+        }),
+        actionButtonText: t('Delete Roles', { count: roles.length }),
+        items: roles.sort((l, r) => compareStrings(l.name, r.name)),
+        keyFn: idKeyFn,
+        isDanger: true,
+        confirmationColumns,
+        actionColumns,
+        onComplete,
+        actionFn: (role: EdaRole) => requestDelete(`${API_PREFIX}/roles/${role.id}/`),
+      });
+    },
+    [actionColumns, bulkAction, confirmationColumns, onComplete, t]
+  );
+}
