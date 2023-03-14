9746d867ee987d440b4c0c35d4bea5a27571944e
James Talton <jtalton@redhat.com>
2023-03-14 15:28:37 -0400

    patternfly code editor
    Signed-off-by: James Talton <jtalton@redhat.com>

---
+++ frontend/awx/access/teams/PageCodeEditor.tsx
@@ -0,0 +1,54 @@
+import { CodeEditor, Language } from '@patternfly/react-code-editor';
+import * as monacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
+import { CSSProperties, useRef } from 'react';
+import { ChangeHandler } from 'react-monaco-editor';
+import { useSettings } from '../../../../framework';
+import './PageCodeEditor.css';
+
+export function PageCodeEditor(props: {
+  id?: string;
+  value?: string | undefined;
+  onChange?: ChangeHandler;
+  style?: CSSProperties;
+  className?: string;
+  invalid?: boolean;
+  isReadOnly?: boolean;
+  showLineNumbers?: boolean;
+}) {
+  const editorRef = useRef<{
+    editor?: monacoEditor.editor.IStandaloneCodeEditor;
+    monaco?: typeof monacoEditor;
+  }>({});
+  const onEditorDidMount = (
+    editor: monacoEditor.editor.IStandaloneCodeEditor,
+    monaco: typeof monacoEditor
+  ) => {
+    editorRef.current.editor = editor;
+    editorRef.current.monaco = monaco;
+    // editor.layout();
+    // editor.focus();
+    // monaco.editor.getModels()[0].updateOptions({ tabSize: 5 });
+  };
+  const settings = useSettings();
+  return (
+    <div
+      id={props.id}
+      className={props.className}
+      style={{ ...props.style, flexGrow: 1, position: 'relative', padding: 0 }}
+      aria-invalid={props.invalid ? 'true' : undefined}
+    >
+      <CodeEditor
+        isDarkTheme={settings.activeTheme === 'dark'}
+        isLineNumbersVisible={props.showLineNumbers === true}
+        isReadOnly={props.isReadOnly}
+        isMinimapVisible={false}
+        isLanguageLabelVisible={false}
+        code={props.value}
+        onChange={props.onChange}
+        language={Language.yaml}
+        onEditorDidMount={onEditorDidMount}
+        // height={`${height}px`}
+      />
+    </div>
+  );
+}
---
+++ frontend/awx/access/teams/PageFormCodeEditor.tsx
@@ -0,0 +1,68 @@
+import { CSSProperties } from 'react';
+import { Controller, FieldPath, FieldValues, useFormContext, Validate } from 'react-hook-form';
+import { FormGroupTextInputProps } from '../../../../framework';
+import { PageFormGroup } from '../../../../framework/PageForm/Inputs/PageFormGroup';
+import { capitalizeFirstLetter } from '../../../../framework/utils/capitalize';
+import { PageCodeEditor } from './PageCodeEditor';
+import './PageFormCodeEditor.css';
+
+export type PageFormTextInputProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TFieldName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = {
+  name: TFieldName;
+  validate?: Validate<string, TFieldValues> | Record<string, Validate<string, TFieldValues>>;
+  style: CSSProperties;
+} & Omit<FormGroupTextInputProps, 'onChange' | 'value'>;
+
+/** PatternFly TextInput wrapper for use with react-hook-form */
+export function PageFormCodeEditor<
+  TFieldValues extends FieldValues = FieldValues,
+  TFieldName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>(props: PageFormTextInputProps<TFieldValues, TFieldName>) {
+  const { isReadOnly, validate, ...formGroupInputProps } = props;
+  const { label, name, isRequired } = props;
+  const {
+    control,
+    formState: { isSubmitting, isValidating },
+  } = useFormContext<TFieldValues>();
+
+  const id = props.id ?? name.split('.').join('-');
+
+  return (
+    <Controller<TFieldValues, TFieldName>
+      name={name}
+      control={control}
+      shouldUnregister
+      render={({ field: { onChange, value }, fieldState: { error } }) => {
+        return (
+          <PageFormGroup
+            {...formGroupInputProps}
+            id={id}
+            helperTextInvalid={!(validate && isValidating) && error?.message}
+          >
+            <PageCodeEditor
+              id={id}
+              // aria-describedby={id ? `${id}-form-group` : undefined}
+              value={value as unknown as string}
+              onChange={onChange}
+              isReadOnly={isReadOnly || isSubmitting}
+              style={{ ...props.style }}
+              className="pf-c-form-control"
+              invalid={!(validate && isValidating) && error?.message !== undefined}
+            />
+          </PageFormGroup>
+        );
+      }}
+      rules={{
+        required:
+          typeof label === 'string' && typeof isRequired === 'boolean'
+            ? {
+                value: true,
+                message: `${capitalizeFirstLetter(label.toLocaleLowerCase())} is required.`,
+              }
+            : isRequired,
+      }}
+    />
+  );
+}
