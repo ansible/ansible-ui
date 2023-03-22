c1270d7dc330952f32b15313eff57657982daac8
James Talton <jtalton@redhat.com>
2023-03-22 15:32:50 -0400

    fix
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/common/crud/delay.ts
+++ frontend/common/crud/delay.ts
@@ -1,9 +1,9 @@
-const delay =
-  process.env.DELAY && Number.isInteger(Number(process.env.DELAY))
-    ? Number(process.env.DELAY)
-    : undefined;
-
 export async function Delay() {
+  const delay =
+    process.env.DELAY && Number.isInteger(Number(process.env.DELAY))
+      ? Number(process.env.DELAY)
+      : undefined;
+
   if (delay) {
     await new Promise((resolve) => setTimeout(resolve, delay));
   }
