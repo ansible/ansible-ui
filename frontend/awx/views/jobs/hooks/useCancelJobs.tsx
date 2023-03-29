ab6b5d970c5cf8ddcc43ef6e58e6420905afa64f
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2023-03-29 09:25:16 -0400

    Bulk dialog message for jobs that cannot be canceled (#346)
    * messaging for jobs that cannot be cancelled
    
    * unit test
    
    * rebase
    
    * remove dup
    
    * update

--- frontend/awx/views/jobs/hooks/useCancelJobs.tsx
+++ frontend/awx/views/jobs/hooks/useCancelJobs.tsx
@@ -4,7 +4,7 @@ import { compareStrings, useBulkConfirmation } from '../../../../../framework';
 import { useNameColumn } from '../../../../common/columns';
 import { getItemKey, requestPost } from '../../../../common/crud/Data';
 import { UnifiedJob } from '../../../interfaces/UnifiedJob';
-import { getJobsAPIUrl } from '../jobUtils';
+import { getJobsAPIUrl, isJobRunning } from '../jobUtils';
 import { useJobsColumns } from './useJobsColumns';
 
 export function useCancelJobs(onComplete: (jobs: UnifiedJob[]) => void) {
@@ -13,14 +13,62 @@ export function useCancelJobs(onComplete: (jobs: UnifiedJob[]) => void) {
   const cancelActionNameColumn = useNameColumn({ disableLinks: true, disableSort: true });
   const actionColumns = useMemo(() => [cancelActionNameColumn], [cancelActionNameColumn]);
   const bulkAction = useBulkConfirmation<UnifiedJob>();
+  const cannotCancelJobDueToPermissions = (job: UnifiedJob) => {
+    if (!job.summary_fields.user_capabilities.start && isJobRunning(job.status))
+      return t(`The job cannot be canceled due to insufficient permission`);
+    return '';
+  };
+  const cannotCancelJobDueToStatus = (job: UnifiedJob) => {
+    if (!isJobRunning(job.status)) return t(`The job cannot be canceled because it is not running`);
+    return '';
+  };
+
   const cancelUnifiedJobs = (jobs: UnifiedJob[]) => {
+    const uncancellableJobsDueToStatus: UnifiedJob[] = jobs.filter(cannotCancelJobDueToStatus);
+    const runningJobs: UnifiedJob[] = jobs.filter((job) => isJobRunning(job.status));
+    const uncancellableJobsDueToPermissions: UnifiedJob[] = runningJobs.filter(
+      cannotCancelJobDueToPermissions
+    );
+
     bulkAction({
       title: t('Cancel jobs', { count: jobs.length }),
       confirmText: t('Yes, I confirm that I want to cancel these {{count}} jobs.', {
-        count: jobs.length,
+        count:
+          jobs.length -
+          uncancellableJobsDueToPermissions.length -
+          uncancellableJobsDueToStatus.length,
       }),
       actionButtonText: t('Cancel jobs', { count: jobs.length }),
       items: jobs.sort((l, r) => compareStrings(l.name, r.name)),
+      alertPrompts:
+        uncancellableJobsDueToStatus.length || uncancellableJobsDueToPermissions.length
+          ? [
+              ...(uncancellableJobsDueToStatus.length
+                ? [
+                    t(
+                      '{{count}} of the selected jobs cannot be canceled because they are not running.',
+                      {
+                        count: uncancellableJobsDueToStatus.length,
+                      }
+                    ),
+                  ]
+                : []),
+              ...(uncancellableJobsDueToPermissions.length
+                ? [
+                    t(
+                      '{{count}} of the selected jobs cannot be cancelled due to insufficient permissions.',
+                      {
+                        count: uncancellableJobsDueToPermissions.length,
+                      }
+                    ),
+                  ]
+                : []),
+            ]
+          : undefined,
+      isItemNonActionable: (item: UnifiedJob) =>
+        cannotCancelJobDueToStatus(item)
+          ? cannotCancelJobDueToStatus(item)
+          : cannotCancelJobDueToPermissions(item),
       keyFn: getItemKey,
       confirmationColumns,
       actionColumns,
