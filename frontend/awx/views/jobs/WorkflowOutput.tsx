3170631dfbea39d67523dfc742f9828c8ee297d6
Keith J. Grant <keithjgrant@redhat.com>
2023-03-30 15:30:37 -0700

    route workflow jobs to separate view

---
+++ frontend/awx/views/jobs/WorkflowOutput.tsx
@@ -0,0 +1,6 @@
+import { Job } from '../../interfaces/Job';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function WorkflowOutput(props: { job: Job }) {
+  return null;
+}
