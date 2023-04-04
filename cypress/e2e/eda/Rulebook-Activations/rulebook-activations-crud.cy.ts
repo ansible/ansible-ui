536eb63b61b4a1a05d6dfb145d7a806d746bf8b9
akus062381 <sarah.akus@gmail.com>
2023-04-04 15:09:31 -0400

    adding a CRUD test

--- cypress/e2e/eda/Rulebook-Activations/rulebook-activations-crud.cy.ts
+++ cypress/e2e/eda/Rulebook-Activations/rulebook-activations-crud.cy.ts
@@ -1,52 +1,58 @@
 //Tests a user's ability to create, edit, and delete Rulebook Activations in the EDA UI.
-// import { EdaRulebookActivation } from '../../../../frontend/eda/interfaces/EdaRulebookActivation';
 import { randomString } from '../../../../framework/utils/random-string';
-import { EdaRulebook } from '../../../../frontend/eda/interfaces/EdaRulebook';
-import { EdaResult } from '../../../../frontend/eda/interfaces/EdaResult';
-import { EdaProject } from '../../../../frontend/eda/interfaces/EdaProject';
 
 describe('EDA Rulebook Activations- Create, Edit, Delete', () => {
-  // let edarulebookactivation: EdaRulebookActivation;
-  let project: EdaProject;
-
   before(() => {
     cy.edaLogin();
-
-    // cy.createEdaRulebookActivation().then((rulebookactivation) => {
-    //   edarulebookactivation = rulebookactivation;
-    // });
-  });
-
-  after(() => {
-    cy.deleteEdaProject(project);
   });
 
   it.only('can create a Rulebook Activation including custom variables, enable it, and assert the information showing on the details page', () => {
-    cy.createEdaProject().then((proj) => {
-      project = proj;
-      cy.requestGet<EdaResult<EdaRulebook>>('/api/eda/v1/rulebooks/').then((rulebooks) => {
+    cy.createEdaProject().then((edaProject) => {
+      cy.optionsWait(2000);
+      cy.getEdaRulebooks(edaProject).then((edaRuleBooks) => {
+        const edaRulebook = edaRuleBooks[0];
         const name = 'E2E Rulebook Activation ' + randomString(4);
-        cy.navigateTo(/^Rulebook activations$/);
+        cy.visit('/eda/rulebook-activations/create');
         cy.clickButton(/^Add rulebook activation$/);
         cy.get('h1').should('contain', 'Create rulebook activation');
         cy.typeByLabel(/^Name$/, name);
         cy.typeByLabel(/^Description$/, 'This is a new rulebook activation.');
-        cy.selectFromDropdown(/^Rulebook$/, rulebooks?.results[0]?.name);
+        cy.selectByLabel(/^Rulebook$/, edaRulebook.name, { disableSearch: true });
         cy.selectFromDropdown(/^Restart policy$/, 'Always');
-        cy.selectFromDropdown(/^Project$/, project.name);
+        cy.selectFromDropdown(/^Project$/, edaProject.name);
         cy.clickButton(/^Add rulebook activation$/);
         cy.get('h1').should('contain', name);
-        cy.clickPageAction(/^Delete rulebookActivation$/);
-        cy.confirmModalAction('Delete rulebookActivation');
+        cy.getEdaRulebookActivation(name).then((edaRulebookActivation) => {
+          cy.deleteEdaRulebookActivation(edaRulebookActivation);
+        });
       });
+      cy.deleteEdaProject(edaProject);
     });
   });
 
   it.skip('can edit a Rulebook Activation and then disable it', () => {
-    //write test here
+    cy.createEdaProject().then((edaProject) => {
+      cy.getEdaRulebooks(edaProject).then((edaRuleBooks) => {
+        const edaRulebook = edaRuleBooks[0];
+        cy.createEdaRulebookActivation(edaRulebook).then((edaRulebookActivation) => {
+          // Verify
+          cy.deleteEdaRulebookActivation(edaRulebookActivation);
+        });
+      });
+      cy.deleteEdaProject(edaProject);
+    });
   });
 
   it.skip('can delete a Rulebook Activation from the details view', () => {
-    //write test here
+    cy.createEdaProject().then((edaProject) => {
+      cy.getEdaRulebooks(edaProject).then((edaRuleBooks) => {
+        const edaRulebook = edaRuleBooks[0];
+        cy.createEdaRulebookActivation(edaRulebook).then((edaRulebookActivation) => {
+          // Verify
+          cy.deleteEdaRulebookActivation(edaRulebookActivation);
+        });
+      });
+      cy.deleteEdaProject(edaProject);
+    });
   });
 });
