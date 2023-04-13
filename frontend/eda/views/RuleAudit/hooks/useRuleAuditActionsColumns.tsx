f5edb1e9ca2e5a604d9290c6a6d25e7b43848a6a
lgalis <lgalis@redhat.com>
2023-04-13 17:40:18 -0400

    Add status and fired date to the actions list

--- frontend/eda/views/RuleAudit/hooks/useRuleAuditActionsColumns.tsx
+++ frontend/eda/views/RuleAudit/hooks/useRuleAuditActionsColumns.tsx
@@ -2,6 +2,8 @@ import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ITableColumn, TextCell } from '../../../../../framework';
 import { EdaRuleAuditAction } from '../../../interfaces/EdaRuleAuditAction';
+import { StatusCell } from '../../../../common/StatusCell';
+import { formatDateString } from '../../../../../framework/utils/formatDateString';
 
 export function useRuleAuditActionsColumns() {
   const { t } = useTranslation();
@@ -9,11 +11,40 @@ export function useRuleAuditActionsColumns() {
     () => [
       {
         header: t('Name'),
-        cell: (ruleAuditAction) => <TextCell text={ruleAuditAction?.name} />,
+        cell: (ruleAuditAction) =>
+          ruleAuditAction?.url ? (
+            <TextCell
+              text={ruleAuditAction?.name}
+              onClick={() => {
+                open(ruleAuditAction?.url, '_blank');
+              }}
+            />
+          ) : (
+            <TextCell text={ruleAuditAction?.name} />
+          ),
         sort: 'name',
         defaultSort: true,
         card: 'name',
         list: 'name',
+      },
+      {
+        header: t('Status'),
+        cell: (ruleAuditAction) => <StatusCell status={ruleAuditAction?.status} />,
+        sort: 'status',
+        defaultSort: true,
+        card: 'name',
+        list: 'name',
+      },
+      {
+        header: t('Last fired date'),
+        cell: (ruleAuditAction) => (
+          <TextCell
+            text={
+              ruleAuditAction?.fired_at ? formatDateString(new Date(ruleAuditAction.fired_at)) : ''
+            }
+          />
+        ),
+        sort: 'fired_at',
       },
     ],
     [t]
