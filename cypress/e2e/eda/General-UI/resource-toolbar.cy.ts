00ad5349fb8307a2bdefd225a55931e87f6f2cde
akus062381 <sarah.akus@gmail.com>
2023-04-14 15:55:07 -0400

    resource toolbar tests

--- cypress/e2e/eda/General-UI/resource-toolbar.cy.ts
+++ cypress/e2e/eda/General-UI/resource-toolbar.cy.ts
@@ -5,39 +5,64 @@ describe('EDA Resource Toolbar Functionality', () => {
     cy.edaLogin();
   });
 
-  it.skip('can visit the pages in the Projects section and assert the data there', () => {
-    //change test stub name to stipulate what the specific criteria is
+  it('can visit the dashboard page and assert the data there', () => {
+    cy.get('.pf-c-nav__item').contains('Dashboard').click();
+    cy.get('h1').should('contain', 'Welcome to EDA Server');
+    cy.contains('button', 'Create project').should('exist');
+    cy.contains('button', 'Create rulebook activation').should('exist');
   });
 
-  it.skip('can visit the pages in the Credentials section and assert the data there', () => {
-    //change test stub name to stipulate what the specific criteria is
+  it('can visit the projects page and assert the data there', () => {
+    cy.get('.pf-c-nav__item').contains('Projects').click();
+    cy.get('h1').should('contain', 'Projects');
+    cy.contains('button', 'Create project').should('exist');
   });
 
-  it.skip('can visit the pages in the Decision Environments section and assert the data there', () => {
-    //change test stub name to stipulate what the specific criteria is
+  it('can visit the credentials page and assert the data there', () => {
+    cy.get('.pf-c-nav__item').contains('Credentials').click();
+    cy.get('h1').should('contain', 'Credentials');
+    cy.contains('button', 'Create credential').should('exist');
   });
 
-  it.skip('can visit the pages in the Rule Audits section and assert the data there', () => {
-    //change test stub name to stipulate what the specific criteria is
+  it('can visit the decision environments page and assert the data there', () => {
+    cy.get('.pf-c-nav__item').contains('Decision environments').click();
+    cy.get('h1').should('contain', 'DecisionEnvironments');
+    cy.contains('button', 'Create decision environment').should('exist');
   });
 
-  it.skip('can visit the pages in the Rulebook Activations section and assert the data there', () => {
-    //change test stub name to stipulate what the specific criteria is
+  it('can visit the rule audits page and assert the data there', () => {
+    cy.get('.pf-c-nav__link').contains('Rule audit').click();
+    cy.get('h1').should('contain', 'Rule Audit');
   });
 
-  it.skip('can visit the pages in the Rulebooks section and assert the data there', () => {
-    //change test stub name to stipulate what the specific criteria is
+  it('can visit the rulebook activations page and assert the data there', () => {
+    cy.get('.pf-c-nav__link').contains('Rulebook activations').click();
+    cy.get('h1').should('contain', 'Rulebook activations');
+    cy.contains('button', 'Create rulebook activation').should('exist');
   });
 
-  it.skip('can visit the pages in the Rules section and assert the data there', () => {
-    //change test stub name to stipulate what the specific criteria is
+  it('can visit the rulebooks page and assert the data there', () => {
+    cy.get('.pf-c-nav__item').contains('Rulebooks').click();
+    cy.get('h1').should('contain', 'Rulebooks');
+    cy.contains('button', 'Create project').should('exist');
   });
 
-  it.skip('can visit the pages in the Users section and assert the data there', () => {
-    //change test stub name to stipulate what the specific criteria is
+  it('can visit the rules page and assert the data there', () => {
+    cy.get('.pf-c-nav__item').contains('Rules').click();
+    cy.get('h1').should('contain', 'Rules');
+    cy.contains('button', 'Create project').should('exist');
   });
 
-  it.skip('can visit the pages in the Roles section and assert the data there', () => {
-    //change test stub name to stipulate what the specific criteria is
+  it.skip('can visit the users page and assert the data there', () => {
+    // TODO: needs further work when Users page is functional
+    cy.get('.pf-c-nav__item').contains('Users').click();
+    cy.get('h1').should('contain', 'Users');
+    cy.contains('button', 'Create user').should('exist');
+  });
+
+  it('can visit the roles page and assert the data there', () => {
+    cy.get('.pf-c-nav__item').contains('Roles').click();
+    cy.get('h1').should('contain', 'Roles');
+    cy.contains('button', 'Create role').should('exist');
   });
 });
