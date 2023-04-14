9fbdd16785d67d720f0741e66d933786265a2a0c
Sarah Akus <sarah.akus@gmail.com>
2023-04-14 15:28:31 -0400

    [EDA] Login/Logout tests (#399)
    * adding new tests
    
    * remove .only
    
    * writing tests for logout
    
    * change command name
    
    * rename command
    
    * rename command
    
    * fixing errors
    
    * fixing error
    
    * fix error
    
    * fix errors
    
    * fix errors
    
    * fix error
    
    * fix error
    
    * fix error
    
    * fix error
    
    * change code
    
    * fix
    
    * fix test failure
    
    * fix error

--- cypress/e2e/eda/General-UI/login-logout.cy.ts
+++ cypress/e2e/eda/General-UI/login-logout.cy.ts
@@ -1,12 +1,96 @@
 //Tests a user's ability to log into and out of the EDA UI.
 //Note that EDA Actions do not have any CRUD functionality.
+import { randomString } from '../../../../framework/utils/random-string';
 
 describe('EDA Login / Logoff', () => {
-  it.skip('can log into the UI and view username in the top right of the Dashboard toolbar', () => {
-    //change test stub name to stipulate what the specific criteria is
+  before(() => {
+    cy.edaLogin();
+  });
+
+  it('can log into the UI and view username in the top right of the Dashboard toolbar', () => {
+    cy.getEdaUser().then((edaUser) => {
+      if (Cypress.env('TEST_STANDALONE') === true) {
+        cy.intercept('GET', '/api/logout/').as('loggedOut');
+        cy.edaLogout();
+        cy.wait('@loggedOut').then((result) => {
+          expect(result?.response?.statusCode).to.eql(200);
+        });
+      } else {
+        cy.intercept('POST', '/api/eda/v1/auth/session/logout/').as('loggedOut');
+        cy.edaLogout();
+        cy.wait('@loggedOut').then((result) => {
+          expect(result?.response?.statusCode).to.eql(204);
+        });
+      }
+      if (edaUser && Cypress.env('TEST_STANDALONE') === true) {
+        cy.typeInputByLabel(/^Username$/, edaUser.username);
+        cy.typeInputByLabel(/^Password$/, Cypress.env('EDA_PASSWORD') as string);
+        cy.clickModalButton('Log in');
+        cy.get('.pf-c-dropdown__toggle').eq(1).should('contain', edaUser.username);
+      } else if (edaUser && !Cypress.env('TEST_STANDALONE')) {
+        cy.get('#E2E-title').then(() => {
+          cy.contains('E2E').click();
+        });
+        cy.typeInputByLabel(/^Username$/, edaUser.username);
+        cy.typeInputByLabel(/^Password$/, Cypress.env('EDA_PASSWORD') as string);
+        cy.clickModalButton('Log in');
+        cy.get('.pf-c-dropdown__toggle').eq(1).should('contain', edaUser.username);
+      }
+    });
   });
 
   it.skip('can log out and login as a different user', () => {
-    //need to use mock data to test the search feature
+    // TODO: needs further work when Users page is functional
+    const userDetails = {
+      Username: `E2E User ${randomString(4)}`,
+      FirstName: 'Firstname',
+      LastName: 'Lastname',
+      Email: 'first.last@redhat.com',
+      Password: `${randomString(12)}`,
+    };
+    cy.navigateTo(/^Users$/);
+    cy.contains('h1', 'Users');
+    cy.clickButton(/^Create user$/);
+    cy.typeInputByLabel(/^Username$/, userDetails.Username);
+    cy.typeInputByLabel(/^First name$/, userDetails.FirstName);
+    cy.typeInputByLabel(/^Last name$/, userDetails.LastName);
+    cy.typeInputByLabel(/^Email$/, userDetails.Email);
+    cy.typeInputByLabel(/^Password$/, userDetails.Password);
+    cy.typeInputByLabel(/^Password confirmation$/, userDetails.Password);
+    /*Roles selection*/
+    cy.selectDropdownOptionByLabel(/^User type$/, 'Super user');
+    cy.selectDropdownOptionByLabel(/^Roles(s)$/, 'User Experience');
+    cy.clickButton(/^Create user$/);
+    cy.hasDetail('First name', userDetails.FirstName);
+    cy.hasDetail('Last name', userDetails.LastName);
+    cy.hasDetail('Email', userDetails.Email);
+    cy.hasDetail('Username', userDetails.Username);
+    if (Cypress.env('TEST_STANDALONE') === true) {
+      cy.intercept('GET', '/api/logout/').as('loggedOut');
+      cy.edaLogout();
+      cy.wait('@loggedOut').then((result) => {
+        expect(result?.response?.statusCode).to.eql(200);
+      });
+    } else {
+      cy.intercept('POST', '/api/eda/v1/auth/session/logout/').as('loggedOut');
+      cy.edaLogout();
+      cy.wait('@loggedOut').then((result) => {
+        expect(result?.response?.statusCode).to.eql(204);
+      });
+    }
+    if (Cypress.env('TEST_STANDALONE') === true) {
+      cy.typeInputByLabel(/^Username$/, userDetails.Username);
+      cy.typeInputByLabel(/^Password$/, userDetails.Password);
+      cy.clickModalButton('Log in');
+      cy.get('.pf-c-dropdown__toggle').eq(1).should('contain', userDetails.Username);
+    } else {
+      cy.get('#E2E-title').then(() => {
+        cy.contains('E2E').click();
+      });
+      cy.typeInputByLabel(/^Username$/, userDetails.Username);
+      cy.typeInputByLabel(/^Password$/, userDetails.Password);
+      cy.clickModalButton('Log in');
+      cy.get('.pf-c-dropdown__toggle').eq(1).should('contain', userDetails.Username);
+    }
   });
 });
