2614c5988f72c733b08cd7257d2330d5ffbce105
Vishali Sanghishetty <95239214+vishsanghishetty@users.noreply.github.com>
2023-04-18 10:41:12 -0400

    resolves conflicts

--- cypress/e2e/eda/General-UI/dashboard.cy.ts
+++ cypress/e2e/eda/General-UI/dashboard.cy.ts
@@ -1,19 +1,17 @@
 //Tests a user's ability to perform certain actions on the Dashboard of the EDA UI.
 //Implementation of Visual Tests makes sense here at some point
+import { EdaProject } from '../../../../frontend/eda/interfaces/EdaProject';
 
 describe('EDA Dashboard', () => {
+  let edaProject: EdaProject;
+
   before(() => {
     cy.edaLogin();
-    cy.createEdaProject().then((project) => {
-      cy.getEdaRulebooks(project).then((edaRulebooksArray) => {
-        cy.createEdaDecisionEnvironment().then((edaDecisionEnvironment) => {
-          cy.createEdaRulebookActivation({
-            rulebook_id: edaRulebooksArray[0].id,
-            decision_environment_id: edaDecisionEnvironment.id,
-          });
-        });
-      });
-    });
+    cy.createEdaProject().then((project) => (edaProject = project));
+  });
+
+  after(() => {
+    cy.deleteEdaProject(edaProject);
   });
 
   it('checks the dashboard landing page titles ', () => {
@@ -86,8 +84,19 @@ describe('EDA Dashboard', () => {
   });
 
   it('shows the user a Rulebook Activation card with a list of Rulebook Activations visible including working links', () => {
-    cy.navigateTo(/^Dashboard$/);
-    cy.get('[data-label="Name"] div > a').contains('E2E Rulebook Activation').click();
-    cy.url().should('match', new RegExp('eda/rulebook-activations/details/[0-9]*'));
+    cy.getEdaRulebooks(edaProject).then((edaRulebooksArray) => {
+      cy.createEdaDecisionEnvironment().then((edaDecisionEnvironment) => {
+        cy.createEdaRulebookActivation({
+          rulebook_id: edaRulebooksArray[0].id,
+          decision_environment_id: edaDecisionEnvironment.id,
+        }).then((activation) => {
+          cy.navigateTo(/^Dashboard$/);
+          cy.get('[data-label="Name"] div > a').contains(activation.name).click();
+          cy.url().should('match', new RegExp('eda/rulebook-activations/details/[0-9]*'));
+          cy.deleteEdaRulebookActivation(activation);
+        });
+        cy.deleteEdaDecisionEnvironment(edaDecisionEnvironment);
+      });
+    });
   });
 });
