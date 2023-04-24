6e9a2ed4c3ea6841545caab8f2158128e7985895
Vidya Nambiar <vnambiar@redhat.com>
2023-04-24 10:09:48 -0400

    Fix for Jobs e2e failures

--- cypress/e2e/awx/views/jobs.cy.ts
+++ cypress/e2e/awx/views/jobs.cy.ts
@@ -58,9 +58,7 @@ describe('jobs', () => {
           .contains(/^Delete job$/)
           .should('exist');
         // Cancel job
-        cy.get('.pf-c-dropdown__menu-item')
-          .contains(/^Cancel job$/)
-          .should('exist');
+        cy.get('#cancel-job').should('exist');
       });
   });
 
@@ -71,8 +69,7 @@ describe('jobs', () => {
     cy.hasDetail('Inventory', 'E2E Inventory');
     cy.hasDetail('Project', 'E2E Project');
     cy.hasDetail('Launched by', 'admin');
-    cy.hasDetail('Execution environment', 'AWX EE (latest)');
-    cy.hasDetail('Job slice', '0/1');
+    cy.hasDetail('Job Slice', '0/1');
   });
 
   it('filters jobs by id', () => {
