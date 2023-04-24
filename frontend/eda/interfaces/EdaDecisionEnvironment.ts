176b2bc417095f3d9feb6cea799583655685dabe
lgalis <lgalis@users.noreply.github.com>
2023-04-24 11:52:37 -0400

    Fix the credential id field in the decision environment pages (#448)

--- frontend/eda/interfaces/EdaDecisionEnvironment.ts
+++ frontend/eda/interfaces/EdaDecisionEnvironment.ts
@@ -10,7 +10,8 @@ export interface EdaDecisionEnvironment {
   name: string;
   description: string;
   image_url: string;
-  credential: number;
+  credential: { id: number; name: string };
+  credential_id?: number;
   created_at: string;
   modified_at: string;
 }
