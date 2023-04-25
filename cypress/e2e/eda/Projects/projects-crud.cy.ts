868ebc99b508bcaaefc2851f37dcca587928c97c
James Talton <jtalton@redhat.com>
2023-04-25 12:56:36 -0400

    fix
    Signed-off-by: James Talton <jtalton@redhat.com>

--- cypress/e2e/eda/Projects/projects-crud.cy.ts
+++ cypress/e2e/eda/Projects/projects-crud.cy.ts
@@ -14,7 +14,7 @@ describe('EDA Projects CRUD', () => {
     cy.get('h1').should('contain', 'Projects');
     cy.clickButton(/^Create project$/);
     cy.typeInputByLabel(/^Name$/, name);
-    cy.typeInputByLabel(/^SCM URL$/, 'https://github.com/Alex-Izquierdo/eda-sample-project');
+    cy.typeInputByLabel(/^Git URL$/, 'https://github.com/Alex-Izquierdo/eda-sample-project');
     cy.clickButton(/^Create project$/);
     cy.hasTitle(name);
     cy.getEdaProjectByName(name).then((project) => {
