d5a676d8cf971e4cfe44e75a4a4a0b189001e6d7
James Talton <jtalton@redhat.com>
2023-04-27 15:54:46 -0400

    Page Table Toolbar Sorting Support (#474)
    * PageTableToolbarSort
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix toolbar wrapping
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * translations
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * tweak icon
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * Fix z-index issues and cleanup toolbar view controls
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * cleanup
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix component tests
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    ---------
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- framework/PageTable/PageToolbarFilter.tsx
+++ framework/PageTable/PageToolbarFilter.tsx
@@ -6,6 +6,8 @@ import {
   SelectOption,
   SelectOptionObject,
   SelectVariant,
+  Split,
+  SplitItem,
   TextInputGroup,
   TextInputGroupMain,
   TextInputGroupUtilities,
@@ -16,8 +18,8 @@ import {
 } from '@patternfly/react-core';
 import { ArrowRightIcon, FilterIcon, TimesIcon } from '@patternfly/react-icons';
 import { Dispatch, SetStateAction, useCallback, useState } from 'react';
-import styled from 'styled-components';
 import { FormGroupSelect } from '../PageForm/Inputs/FormGroupSelect';
+import { useBreakpoint } from '../components/useBreakPoint';
 import { useFrameworkTranslations } from '../useFrameworkTranslations';
 
 export const enum PageFilterTypeE {
@@ -50,18 +52,22 @@ export type IToolbarFilter = IToolbarStringFilter | IToolbarSelectFilter;
 
 export type IFilterState = Record<string, string[] | undefined>;
 
-export type PageTableToolbarFiltersProps = {
+export type PageToolbarFiltersProps = {
   toolbarFilters?: IToolbarFilter[];
   filters?: Record<string, string[]>;
   setFilters?: Dispatch<SetStateAction<Record<string, string[]>>>;
 };
 
-export function PageTableToolbarFilters(props: PageTableToolbarFiltersProps) {
+export function PageToolbarFilters(props: PageToolbarFiltersProps) {
   const { toolbarFilters, filters, setFilters } = props;
 
   const [selectedFilter, setSeletedFilter] = useState(() =>
     toolbarFilters ? (toolbarFilters?.length > 0 ? toolbarFilters[0].key : '') : ''
   );
+
+  const [translations] = useFrameworkTranslations();
+
+  const showFilterLabel = !useBreakpoint('md');
 
   if (!toolbarFilters) return <></>;
   if (toolbarFilters.length === 0) return <></>;
@@ -69,72 +75,77 @@ export function PageTableToolbarFilters(props: PageTableToolbarFiltersProps) {
   return (
     <ToolbarToggleGroup toggleIcon={<FilterIcon />} breakpoint="md">
       <ToolbarGroup variant="filter-group">
+        {showFilterLabel && <ToolbarItem variant="label">{translations.filter}</ToolbarItem>}
         <ToolbarItem>
-          <InputGroup style={{ zIndex: 400 }}>
-            {toolbarFilters.length === 1 ? (
-              <>
-                <InputGroupText
-                  style={{
-                    border: 0,
-                    paddingLeft: 12,
-                    paddingRight: 2,
-                    color: 'inherit',
-                    borderRadius: '4px 0px 0px 4px',
-                  }}
-                >
-                  <FilterIcon />
-                </InputGroupText>
-                <InputGroupText style={{ border: 0, padding: '6px 8px', color: 'inherit' }}>
-                  {toolbarFilters[0].label}
-                </InputGroupText>
-              </>
-            ) : (
-              <>
-                <InputGroupText
-                  style={{
-                    border: 0,
-                    paddingLeft: 12,
-                    paddingRight: 12,
-                    color: 'inherit',
-                    borderRadius: '4px 0px 0px 4px',
-                  }}
-                >
-                  <FilterIcon />
-                </InputGroupText>
-                <FormGroupSelect
-                  id="filter"
-                  onSelect={(_, v) => setSeletedFilter(v.toString())}
-                  value={selectedFilter}
-                  placeholderText=""
-                >
-                  {toolbarFilters.map((filter) => (
-                    <SelectOption key={filter.key} value={filter.key}>
-                      {filter.label}
-                    </SelectOption>
-                  ))}
-                </FormGroupSelect>
-              </>
-            )}
-          </InputGroup>
-        </ToolbarItem>
-        <ToolbarItem>
-          <ToolbarFilterInput
-            id="filter-input"
-            filter={toolbarFilters.find((filter) => filter.key === selectedFilter)}
-            addFilter={(value: string) => {
-              let values = filters?.[selectedFilter];
-              if (!values) values = [];
-              if (!values.includes(value)) values.push(value);
-              setFilters?.({ ...filters, [selectedFilter]: values });
-            }}
-            removeFilter={(value: string) => {
-              let values = filters?.[selectedFilter];
-              if (!values) values = [];
-              values = values.filter((v) => v !== value);
-              setFilters?.({ ...filters, [selectedFilter]: values });
-            }}
-            values={filters?.[selectedFilter] ?? []}
-          />
+          <Split style={{ zIndex: 400 }}>
+            <SplitItem>
+              <InputGroup>
+                {toolbarFilters.length === 1 ? (
+                  <>
+                    <InputGroupText
+                      style={{
+                        border: 0,
+                        paddingLeft: 12,
+                        paddingRight: 2,
+                        color: 'inherit',
+                        borderRadius: '4px 0px 0px 4px',
+                      }}
+                    >
+                      <FilterIcon />
+                    </InputGroupText>
+                    <InputGroupText style={{ border: 0, padding: '6px 8px', color: 'inherit' }}>
+                      {toolbarFilters[0].label}
+                    </InputGroupText>
+                  </>
+                ) : (
+                  <>
+                    <InputGroupText
+                      style={{
+                        border: 0,
+                        paddingLeft: 12,
+                        paddingRight: 12,
+                        color: 'inherit',
+                        borderRadius: '4px 0px 0px 4px',
+                      }}
+                    >
+                      <FilterIcon />
+                    </InputGroupText>
+                    <FormGroupSelect
+                      id="filter"
+                      onSelect={(_, v) => setSeletedFilter(v.toString())}
+                      value={selectedFilter}
+                      placeholderText=""
+                    >
+                      {toolbarFilters.map((filter) => (
+                        <SelectOption key={filter.key} value={filter.key}>
+                          {filter.label}
+                        </SelectOption>
+                      ))}
+                    </FormGroupSelect>
+                  </>
+                )}
+              </InputGroup>
+            </SplitItem>
+            <SplitItem isFilled>
+              <ToolbarFilterInput
+                id="filter-input"
+                filter={toolbarFilters.find((filter) => filter.key === selectedFilter)}
+                addFilter={(value: string) => {
+                  let values = filters?.[selectedFilter];
+                  if (!values) values = [];
+                  if (!values.includes(value)) values.push(value);
+                  setFilters?.({ ...filters, [selectedFilter]: values });
+                }}
+                removeFilter={(value: string) => {
+                  let values = filters?.[selectedFilter];
+                  if (!values) values = [];
+                  values = values.filter((v) => v !== value);
+                  setFilters?.({ ...filters, [selectedFilter]: values });
+                }}
+                values={filters?.[selectedFilter] ?? []}
+              />
+            </SplitItem>
+          </Split>
         </ToolbarItem>
         {toolbarFilters.map((filter) => {
           const values = filters?.[filter.key] ?? [];
@@ -285,15 +296,10 @@ function ToolbarSelectFilter(props: {
         onToggle={setOpen}
         selections={selections}
         onSelect={onSelect}
-        placeholderText={
-          values.length ? (
-            translations.selectedText
-          ) : (
-            <SelectionSpan>{props.placeholder}</SelectionSpan>
-          )
-        }
+        placeholderText={values.length ? translations.selectedText : props.placeholder}
         // ZIndex 400 is needed for PF table stick headers
         style={{ zIndex: open ? 400 : 0 }}
+        hasPlaceholderStyle
       >
         {options.map((option) => (
           <SelectOption id={option.value} key={option.value} value={option.value}>
@@ -304,7 +310,3 @@ function ToolbarSelectFilter(props: {
     </>
   );
 }
-
-const SelectionSpan = styled.span`
-  opacity: 0.7;
-`;
---
+++ framework/PageTable/PageToolbarSort.tsx
@@ -0,0 +1,163 @@
+import {
+  Button,
+  Select,
+  SelectOption,
+  SelectOptionObject,
+  SelectVariant,
+  Split,
+  ToolbarGroup,
+  ToolbarItem,
+  ToolbarToggleGroup,
+} from '@patternfly/react-core';
+import {
+  SortAlphaDownAltIcon,
+  SortAlphaUpIcon,
+  SortAmountDownIcon,
+  SortAmountUpIcon,
+  SortNumericDownIcon,
+  SortNumericUpIcon,
+} from '@patternfly/react-icons';
+import { useCallback, useMemo, useState } from 'react';
+import { useFrameworkTranslations } from '../useFrameworkTranslations';
+import { ITableColumn } from './PageTableColumn';
+
+export type PageToolbarSortProps = {
+  sort?: string;
+  setSort?: (sort: string) => void;
+  sortDirection?: 'asc' | 'desc';
+  setSortDirection?: (sortDirection: 'asc' | 'desc') => void;
+  sortOptions?: PageTableSortOption[];
+};
+
+export interface PageTableSortOption {
+  label: string;
+  value: string;
+  type?: 'text' | 'number' | undefined;
+}
+
+export function PageToolbarSort(props: PageToolbarSortProps) {
+  const { sort, setSort, sortDirection, setSortDirection, sortOptions } = props;
+
+  const sortOption = sortOptions?.find((sortOption) => sortOption.value === sort);
+  const sortType = sortOption ? sortOption.type : undefined;
+
+  const [translations] = useFrameworkTranslations();
+
+  const sortDirectionIcon = useMemo(() => {
+    switch (sortType) {
+      case 'text':
+        switch (sortDirection) {
+          case 'asc':
+            return <SortAlphaUpIcon />;
+          case 'desc':
+            return <SortAlphaDownAltIcon />;
+        }
+        break;
+      case 'number':
+        switch (sortDirection) {
+          case 'asc':
+            return <SortNumericUpIcon />;
+          case 'desc':
+            return <SortNumericDownIcon />;
+        }
+        break;
+      default:
+        switch (sortDirection) {
+          case 'asc':
+            return <SortAmountUpIcon />;
+          case 'desc':
+            return <SortAmountDownIcon />;
+        }
+        break;
+    }
+    return <></>;
+  }, [sortDirection, sortType]);
+
+  if (!sortOptions || sortOptions.length <= 1) return <></>;
+
+  return (
+    <ToolbarToggleGroup breakpoint="md" toggleIcon={undefined}>
+      <ToolbarGroup variant="filter-group">
+        <ToolbarItem variant="label">{translations.sort}</ToolbarItem>
+        <ToolbarItem>
+          <Split>
+            <ToolbarSortSelect sortOptions={sortOptions} sort={sort} setSort={setSort} />
+            <Button
+              variant="control"
+              icon={sortDirectionIcon}
+              onClick={() => {
+                if (sortDirection === 'asc') setSortDirection?.('desc');
+                else setSortDirection?.('asc');
+              }}
+            />
+          </Split>
+        </ToolbarItem>
+      </ToolbarGroup>
+    </ToolbarToggleGroup>
+  );
+}
+
+function ToolbarSortSelect(props: {
+  sort?: string;
+  setSort?: (sort: string) => void;
+  sortOptions: PageTableSortOption[];
+}) {
+  const { sortOptions: options, sort, setSort } = props;
+  const [open, setOpen] = useState(false);
+  const onSelect = useCallback(
+    (e: unknown, value: string | SelectOptionObject) => {
+      setSort?.(value.toString());
+      setOpen(false);
+    },
+    [setSort]
+  );
+  return (
+    <Select
+      variant={SelectVariant.single}
+      isOpen={open}
+      onToggle={setOpen}
+      selections={sort}
+      onSelect={onSelect}
+      // ZIndex 400 is needed for PF table sticky headers
+      style={{ zIndex: open ? 400 : 0 }}
+      hasPlaceholderStyle
+    >
+      {options.map((option) => (
+        <SelectOption id={option.value} key={option.value} value={option.value}>
+          {option.label}
+        </SelectOption>
+      ))}
+    </Select>
+  );
+}
+
+export function usePageToolbarSortOptionsFromColumns<T extends object>(
+  tableColumns: ITableColumn<T>[]
+) {
+  const sortOptions = useMemo(() => {
+    const sortOptions: PageTableSortOption[] = [];
+    for (const column of tableColumns) {
+      if (column.sort) {
+        if (column.defaultSort) {
+          // Assumes the defauilt sort is a text column
+          sortOptions.push({ label: column.header, value: column.sort, type: 'text' });
+          continue;
+        }
+
+        switch (column.type) {
+          case 'count':
+            sortOptions.push({ label: column.header, value: column.sort, type: 'number' });
+            break;
+          case 'text':
+            sortOptions.push({ label: column.header, value: column.sort, type: 'text' });
+            break;
+          default:
+            sortOptions.push({ label: column.header, value: column.sort });
+            break;
+        }
+      }
+    }
+    return sortOptions;
+  }, [tableColumns]);
+  return sortOptions;
+}
