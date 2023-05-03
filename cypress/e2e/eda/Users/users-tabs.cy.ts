c0d4a50b3071c8216c198eead5f51fbcbe66e589
James Talton <jtalton@redhat.com>
2023-05-03 16:15:02 -0400

    Cypress eda add support for getting awx tokan and creating in eda (#501)
    * Cypress EDA add support for getting AWX tokan and creating in EDA
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * linting
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    ---------
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- cypress/e2e/eda/Users/users-tabs.cy.ts
+++ cypress/e2e/eda/Users/users-tabs.cy.ts
@@ -1,12 +1,29 @@
 //Tests the actions a user can perform on the tabs inside of a User in the EDA UI.
+import { randomString } from '../../../../framework/utils/random-string';
+import { AwxToken } from '../../../../frontend/awx/interfaces/AwxToken';
 
 describe('EDA User Tokens Tab', () => {
+  let awxToken: AwxToken;
   before(() => {
+    cy.createAwxToken().then((token) => {
+      awxToken = token;
+    });
     cy.edaLogin();
   });
 
-  it.skip('can add a new Token', () => {
-    //disable ruleset
+  it('can add a new Token', () => {
+    const newTokenName = 'E2E Token ' + randomString(8);
+    cy.getEdaUser().then((activeUser) => {
+      cy.navigateTo('Users');
+      cy.clickTableRow(activeUser?.username ?? '');
+      cy.clickTab('Controller Tokens');
+      cy.clickButton('Create controller token');
+      cy.hasTitle('Create Controller Token');
+      cy.typeInputByLabel('Name', newTokenName);
+      cy.typeInputByLabel('Token', awxToken.token as string);
+      cy.clickButton('Create controller token');
+      cy.contains('td', newTokenName);
+    });
   });
 
   it.skip('can verify the functionality of items in the kebab menu on the Tokens list view', () => {
