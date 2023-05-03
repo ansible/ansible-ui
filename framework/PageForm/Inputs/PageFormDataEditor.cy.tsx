459cfe3be13ac735aaa4b3b200e0a5a048f8c337
Alex Corey <acorey@redhat.com>
2023-05-03 10:00:31 -0400

    Aap 7561 variables form field (#483)
    * Switching languages, and formatting code properly
    
    * Refactors variables field
    
    * Finishing touches on the code editor

---
+++ framework/PageForm/Inputs/PageFormDataEditor.cy.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable i18next/no-literal-string */
+import { PageForm } from '../PageForm';
+import { PageFormDataEditor } from './PageFormDataEditor';
+
+describe('PageFormCodeEditor', () => {
+  interface CodeEditor {
+    extra_vars: string;
+  }
+  const yaml = 'one: 1\ntwo: two';
+
+  it('should mount properly', () => {
+    cy.mount(
+      <PageForm<CodeEditor>
+        onSubmit={() => Promise.resolve()}
+        onCancel={() => null}
+        submitText="Submit"
+        defaultValue={{ extra_vars: yaml }}
+      >
+        <PageFormDataEditor
+          label="Extra Variables"
+          name="extra_vars"
+          defaultExpanded={false}
+          isExpandable
+        />
+      </PageForm>
+    );
+    expect(cy.get('label').contains('Extra Variables'));
+    expect(cy.get('div#wrapper').should('not.exist'));
+  });
+  it('should mount expanded, with copy button and 2 toggle language buttons', () => {
+    cy.mount(
+      <PageForm<CodeEditor>
+        submitText="submit"
+        onSubmit={() => Promise.resolve()}
+        defaultValue={{ extra_vars: '' }}
+        onCancel={() => null}
+      >
+        <PageFormDataEditor
+          label="Extra Variables"
+          name="extra_vars"
+          defaultExpanded
+          toggleLanguages={['json', 'yaml']}
+          isExpandable
+        />
+      </PageForm>
+    );
+    expect(cy.get('div#copy-button').should('be.visible'));
+    expect(cy.get('div#toggle-json').should('be.visible'));
+    expect(cy.get('div#toggle-yaml').should('be.visible'));
+    expect(cy.get('div#wrapper').should('be.visible'));
+  });
+  it('Invalid syntax should make toggle language buttons disabled', () => {
+    cy.mount(
+      <PageForm<CodeEditor>
+        onSubmit={() => Promise.resolve()}
+        onCancel={() => null}
+        submitText="submit"
+        defaultValue={{
+          extra_vars:
+            'playing_song_artist: Playing song, {{ song_name }} by {{ artist }}playing_playlist: {{ action }} playlist {{ playlist_name }}',
+        }}
+      >
+        <PageFormDataEditor
+          label="Extra Variables"
+          name="extra_vars"
+          defaultExpanded
+          toggleLanguages={['json', 'yaml']}
+          isExpandable
+        />
+      </PageForm>
+    );
+    cy.get('div#toggle-json').get('button.pf-c-toggle-group__button').should('be.disabled');
+    cy.get('div#toggle-yaml').get('button.pf-c-toggle-group__button').should('be.disabled');
+  });
+});
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -109,6 +109,16 @@ module.exports = function (env, argv) {
       }),
       new MonacoWebpackPlugin({
         languages: ['json', 'yaml', 'shell'],
+        customLanguages: [
+          {
+            label: 'yaml',
+            entry: 'monaco-yaml',
+            worker: {
+              id: 'monaco-yaml/yamlWorker',
+              entry: 'monaco-yaml/yaml.worker',
+            },
+          },
+        ],
       }),
     ].filter(Boolean),
     output: {
