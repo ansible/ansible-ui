d3049765d2f4130ed2bc837f3108ebb8a4e102ac
lgalis <lgalis@redhat.com>
2023-05-05 19:57:51 -0400

    Add permissions to the Role details page

--- frontend/eda/UserAccess/Roles/RoleDetails.tsx
+++ frontend/eda/UserAccess/Roles/RoleDetails.tsx
@@ -1,10 +1,25 @@
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
-import { PageDetail, PageDetails, PageHeader, PageLayout, Scrollable } from '../../../../framework';
+import {
+  DateTimeCell,
+  PageDetail,
+  PageDetails,
+  PageHeader,
+  PageLayout,
+  Scrollable,
+} from '../../../../framework';
 import { RouteObj } from '../../../Routes';
 import { useGet } from '../../../common/crud/useGet';
 import { API_PREFIX, SWR_REFRESH_INTERVAL } from '../../constants';
 import { EdaRole } from '../../interfaces/EdaRole';
+import {
+  DescriptionList,
+  DescriptionListDescription,
+  DescriptionListGroup,
+  DescriptionListTerm,
+  Label,
+  LabelGroup,
+} from '@patternfly/react-core';
 
 export function RoleDetails() {
   const { t } = useTranslation();
@@ -25,6 +40,38 @@ export function RoleDetails() {
         <PageDetails>
           <PageDetail label={t('Name')}>{role?.name || ''}</PageDetail>
           <PageDetail label={t('Description')}>{role?.description || ''}</PageDetail>
+          <PageDetail label={t('Created')}>
+            <DateTimeCell format="date-time" value={role?.created_at} />
+          </PageDetail>
+        </PageDetails>
+        <PageDetails numberOfColumns={'single'}>
+          <PageDetail label={t('Permissions')}>
+            <DescriptionList
+              isCompact
+              isHorizontal
+              horizontalTermWidthModifier={{
+                default: '16ch',
+              }}
+            >
+              {role?.permissions.map((permission) => (
+                <DescriptionListGroup key={permission?.resource_type}>
+                  <DescriptionListTerm
+                    style={{ fontWeight: 'normal' }}
+                    key={permission?.resource_type}
+                  >
+                    {permission?.resource_type}
+                  </DescriptionListTerm>
+                  <DescriptionListDescription>
+                    <LabelGroup numLabels={5}>
+                      {permission?.action.map((action) => (
+                        <Label key={action}>{action}</Label>
+                      ))}
+                    </LabelGroup>
+                  </DescriptionListDescription>
+                </DescriptionListGroup>
+              ))}
+            </DescriptionList>
+          </PageDetail>
         </PageDetails>
       </Scrollable>
     </PageLayout>
