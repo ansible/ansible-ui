fc5198df1b79d0b0d00bab4ba4658e0c154761b9
James Talton <jtalton@redhat.com>
2023-05-08 16:45:59 -0400

    final proxy fix
    Signed-off-by: James Talton <jtalton@redhat.com>

--- proxy/proxy-handler.ts
+++ proxy/proxy-handler.ts
@@ -54,10 +54,8 @@ export function proxyHandler(req: Http2ServerRequest, res: Http2ServerResponse):
   headers[HTTP2_HEADER_REFERER] = `${proxyUrl.protocol}//${proxyUrl.hostname}:${proxyUrl.port}`;
 
   // Remove x-forwarded-proto header
-  // fixes django - django.security.csrf Forbidden (Referer checking failed - Referer is insecure while host is secure.
+  // fixes django - django.security.csrf Forbidden (Referer checking failed - Referer is insecure while host is secure.)
   delete headers['x-forwarded-proto'];
-
-  logger.info({ msg: 'headers', headers });
 
   const requestOptions: RequestOptions = {
     protocol: proxyUrl.protocol,
