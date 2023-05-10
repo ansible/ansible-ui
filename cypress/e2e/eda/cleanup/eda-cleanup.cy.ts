dfc6530c8b236e461ebb27ba1d38944a2f1f4e34
James Talton <jtalton@redhat.com>
2023-05-10 13:37:34 -0400

    fixes
    Signed-off-by: James Talton <jtalton@redhat.com>

--- cypress/e2e/eda/cleanup/eda-cleanup.cy.ts
+++ cypress/e2e/eda/cleanup/eda-cleanup.cy.ts
@@ -11,9 +11,12 @@ describe('EDA Cleanup', () => {
         const beforeTime = new Date(
           Date.now() - (30 + new Date().getTimezoneOffset()) * 60 * 60 * 1000
         ).toLocaleString();
-        for (const token of tokens) {
-          if (token.created_at > beforeTime) continue;
-          if (token.name.startsWith('E2E Token') || token.name.startsWith('AWX Token')) {
+        for (const token of tokens ?? []) {
+          if (!token.created_at || token.created_at > beforeTime) continue;
+          if (
+            token.name &&
+            (token.name.startsWith('E2E Token') || token.name.startsWith('AWX Token'))
+          ) {
             cy.deleteEdaCurrentUserAwxToken(token);
           }
         }
