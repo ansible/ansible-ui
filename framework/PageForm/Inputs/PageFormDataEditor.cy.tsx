4a9d1b6d5d91da9d4ca711ead31c768bc4346787
James Talton <jtalton@redhat.com>
2023-05-11 11:52:50 -0400

    disable randomly failing test until dev can take a closer look (#548)
    Signed-off-by: James Talton <jtalton@redhat.com>

--- framework/PageForm/Inputs/PageFormDataEditor.cy.tsx
+++ framework/PageForm/Inputs/PageFormDataEditor.cy.tsx
@@ -27,6 +27,7 @@ describe('PageFormCodeEditor', () => {
     expect(cy.get('label').contains('Extra Variables'));
     expect(cy.get('div#wrapper').should('not.exist'));
   });
+
   it('should mount expanded, with copy button and 2 toggle language buttons', () => {
     cy.mount(
       <PageForm<CodeEditor>
@@ -49,27 +50,31 @@ describe('PageFormCodeEditor', () => {
     expect(cy.get('div#toggle-yaml').should('be.visible'));
     expect(cy.get('div#wrapper').should('be.visible'));
   });
-  it('Invalid syntax should make toggle language buttons disabled', () => {
-    cy.mount(
-      <PageForm<CodeEditor>
-        onSubmit={() => Promise.resolve()}
-        onCancel={() => null}
-        submitText="submit"
-        defaultValue={{
-          extra_vars:
-            'playing_song_artist: Playing song, {{ song_name }} by {{ artist }}playing_playlist: {{ action }} playlist {{ playlist_name }}',
-        }}
-      >
-        <PageFormDataEditor
-          label="Extra Variables"
-          name="extra_vars"
-          defaultExpanded
-          toggleLanguages={['json', 'yaml']}
-          isExpandable
-        />
-      </PageForm>
-    );
-    cy.get('div#toggle-json').get('button.pf-c-toggle-group__button').should('be.disabled');
-    cy.get('div#toggle-yaml').get('button.pf-c-toggle-group__button').should('be.disabled');
-  });
+
+  // RANDOM FAILING TEST
+  // > Uncaught NetworkError: Failed to execute 'importScripts' on 'WorkerGlobalScope': The script at 'http://localhost:8080/__cypress/src/vendors-node_modules_yaml_browser_index_js.js' failed to load.
+  //
+  // it('Invalid syntax should make toggle language buttons disabled', () => {
+  //   cy.mount(
+  //     <PageForm<CodeEditor>
+  //       onSubmit={() => Promise.resolve()}
+  //       onCancel={() => null}
+  //       submitText="submit"
+  //       defaultValue={{
+  //         extra_vars:
+  //           'playing_song_artist: Playing song, {{ song_name }} by {{ artist }}playing_playlist: {{ action }} playlist {{ playlist_name }}',
+  //       }}
+  //     >
+  //       <PageFormDataEditor
+  //         label="Extra Variables"
+  //         name="extra_vars"
+  //         defaultExpanded
+  //         toggleLanguages={['json', 'yaml']}
+  //         isExpandable
+  //       />
+  //     </PageForm>
+  //   );
+  //   cy.get('div#toggle-json').get('button.pf-c-toggle-group__button').should('be.disabled');
+  //   cy.get('div#toggle-yaml').get('button.pf-c-toggle-group__button').should('be.disabled');
+  // });
 });
