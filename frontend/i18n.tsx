39cc52c17fcc5ad8b8278f97cbcf86b79dbb6b09
Michael Abashian <mabashia@redhat.com>
2023-05-23 10:04:50 -0400

    Leverage i18next-browser-languagedetector to allow users to specify the lang query param to switch languages

--- frontend/i18n.tsx
+++ frontend/i18n.tsx
@@ -2,11 +2,9 @@ import i18n from 'i18next';
 import detector from 'i18next-browser-languagedetector';
 import backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
-import { getLanguageWithoutRegionCode } from './common/utils/language';
 
 const params = new URLSearchParams(window.location.search);
 const pseudolocalizationEnabled = params.get('pseudolocalization') === 'true';
-const language = params.get('lang') || getLanguageWithoutRegionCode(navigator) || 'en';
 
 void i18n
   .use({
@@ -27,7 +25,11 @@ void i18n
   .use(detector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    lng: language,
+    detection: {
+      lookupQuerystring: 'lang',
+      lookupCookie: 'lang',
+      lookupLocalStorage: 'lang',
+    },
     fallbackLng: 'en', // use en if detected lng is not available
     supportedLngs: ['en', 'es', 'fr', 'ja', 'ko', 'nl', 'zh', 'zu'],
     interpolation: {
