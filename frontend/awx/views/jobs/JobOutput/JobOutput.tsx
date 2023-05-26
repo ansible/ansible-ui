b79d58549c8e93b515899d39af5533798baec8a0
Keith J. Grant <keithjgrant@redhat.com>
2023-05-26 16:03:26 -0700

    start job output search bar, fix toolbar styles

--- frontend/awx/views/jobs/JobOutput/JobOutput.tsx
+++ frontend/awx/views/jobs/JobOutput/JobOutput.tsx
@@ -1,14 +1,5 @@
-import {
-  PageSection,
-  Label,
-  SearchInput,
-  Skeleton,
-  Toolbar,
-  ToolbarContent,
-  ToolbarItem,
-} from '@patternfly/react-core';
+import { PageSection, Skeleton } from '@patternfly/react-core';
 import styled from 'styled-components';
-import { useTranslation } from 'react-i18next';
 import { Job } from '../../../interfaces/Job';
 import './JobOutput.css';
 import { JobStatusBar } from './JobStatusBar';
@@ -24,7 +15,6 @@ const Section = styled(PageSection)`
 
 export function JobOutput(props: { job: Job }) {
   const { job } = props;
-  const { t } = useTranslation();
 
   if (!job) {
     return <Skeleton />;
@@ -34,32 +24,6 @@ export function JobOutput(props: { job: Job }) {
       <JobStatusBar job={job} />
       <HostStatusBar counts={job.host_status_counts || {}} />
       <JobOutputToolbar job={job} />
-      <Toolbar className="dark-2 border-bottom">
-        <ToolbarContent>
-          <ToolbarItem>
-            <SearchInput />
-          </ToolbarItem>
-          <div style={{ flexGrow: 1 }} />
-          <ToolbarItem>
-            {t('Plays')} <Label>{job.playbook_counts?.play_count ?? 0}</Label>
-          </ToolbarItem>
-          <ToolbarItem>
-            {t('Tasks')} <Label>{job.playbook_counts?.task_count ?? 0}</Label>
-          </ToolbarItem>
-          <ToolbarItem>
-            {t('Hosts')}{' '}
-            <Label>
-              {job.host_status_counts?.ok ?? 0 + (job.host_status_counts?.failures || 0)}
-            </Label>
-          </ToolbarItem>
-          <ToolbarItem>
-            {t('Failed')} <Label>{job.host_status_counts?.failures ?? 0}</Label>
-          </ToolbarItem>
-          <ToolbarItem>
-            {t('Elapsed')} <Label>{job.elapsed}</Label>
-          </ToolbarItem>
-        </ToolbarContent>
-      </Toolbar>
       <JobOutputEvents job={job} />
     </Section>
   );
--- frontend/awx/views/jobs/JobOutput/JobOutputToolbar.tsx
+++ frontend/awx/views/jobs/JobOutput/JobOutputToolbar.tsx
@@ -1,6 +1,40 @@
+import { useMemo } from 'react';
+import { Toolbar, ToolbarContent } from '@patternfly/react-core';
+import { useTranslation } from 'react-i18next';
+import { PageToolbarFilters } from '../../../../../framework/PageTable/PageToolbarFilter';
+import { IToolbarFilter } from '../../../../../framework';
 import { Job } from '../../../interfaces/Job';
 
 export function JobOutputToolbar(props: { job: Job }) {
   const { job } = props;
-  return <div />;
+  const toolbarFilters = useOutputFilters();
+
+  return (
+    <Toolbar>
+      <ToolbarContent>
+        <PageToolbarFilters
+          toolbarFilters={toolbarFilters}
+          // filters={filters}
+          // setFilters={setFilters}
+        />
+      </ToolbarContent>
+    </Toolbar>
+  );
+}
+
+function useOutputFilters() {
+  const { t } = useTranslation();
+
+  return useMemo<IToolbarFilter[]>(
+    () => [
+      {
+        key: 'stdout',
+        label: t('the label'),
+        type: 'string',
+        query: 'stdout__icontains',
+        placeholder: t('Filter by keyword'),
+      },
+    ],
+    [t]
+  );
 }
