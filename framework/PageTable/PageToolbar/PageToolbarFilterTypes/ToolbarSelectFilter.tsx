c2d44ee036c688b3b93f312195cf3c4ce591a90e
ZitaNemeckova <znemecko@redhat.com>
2023-06-05 15:02:50 +0200

    Add optional search field to ToolbarSelectFilter (#593)

--- framework/PageTable/PageToolbar/PageToolbarFilterTypes/ToolbarSelectFilter.tsx
+++ framework/PageTable/PageToolbar/PageToolbarFilterTypes/ToolbarSelectFilter.tsx
@@ -1,23 +1,24 @@
 import { Select, SelectOption, SelectOptionObject, SelectVariant } from '@patternfly/react-core';
-import { useCallback, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useFrameworkTranslations } from '../../../useFrameworkTranslations';
 import { ToolbarFilterCommon } from './ToolbarFilterCommon';
+export interface IToolbarSelectFilterOption {
+  /** The label to show for the option. */
+  label: string;
+
+  /** The description to show for the option. */
+  description?: string;
+
+  /** The value to use for the option. */
+  value: string;
+}
 /** Filter for filtering by user selection of option. */
 export interface IToolbarSelectFilter extends ToolbarFilterCommon {
   /** Filter for filtering by user selection of option. */
   type: 'select';
 
   /** The options to show in the select. */
-  options: {
-    /** The label to show for the option. */
-    label: string;
-
-    /** The description to show for the option. */
-    description?: string;
-
-    /** The value to use for the option. */
-    value: string;
-  }[];
+  options: IToolbarSelectFilterOption[];
 }
 
 export function ToolbarSelectFilter(props: {
@@ -26,6 +27,7 @@ export function ToolbarSelectFilter(props: {
   options: { label: string; value: string }[];
   values: string[];
   placeholder?: string;
+  hasSearch?: boolean;
 }) {
   const [translations] = useFrameworkTranslations();
   const { addFilter, removeFilter, options, values } = props;
@@ -40,6 +42,21 @@ export function ToolbarSelectFilter(props: {
     },
     [addFilter, removeFilter, values]
   );
+  const onFilter = (_: unknown, textInput: string) => {
+    if (textInput === '') return renderValues(options);
+    return renderValues(
+      options.filter((option) =>
+        option.label.toString().toLowerCase().includes(textInput.toLowerCase())
+      )
+    );
+  };
+  const renderValues = (options: IToolbarSelectFilterOption[]) =>
+    options &&
+    options.map((option) => (
+      <SelectOption id={option.value} key={option.value} value={option.value}>
+        {option.label}
+      </SelectOption>
+    ));
   const selections = values;
   return (
     <>
@@ -53,12 +70,10 @@ export function ToolbarSelectFilter(props: {
         // ZIndex 400 is needed for PF table stick headers
         style={{ zIndex: open ? 400 : 0 }}
         hasPlaceholderStyle
+        onFilter={onFilter}
+        hasInlineFilter={props.hasSearch}
       >
-        {options.map((option) => (
-          <SelectOption id={option.value} key={option.value} value={option.value}>
-            {option.label}
-          </SelectOption>
-        ))}
+        {renderValues(options)}
       </Select>
     </>
   );
