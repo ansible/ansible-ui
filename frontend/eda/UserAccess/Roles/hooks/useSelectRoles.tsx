5527982c5ff755b1e48cb2a4d0b39d6cf26c8e3a
Marliana Lara <mlara@redhat.com>
2023-06-08 14:02:10 -0400

    [EDA] Fix broken settings modal in eda user create (#583)

--- frontend/eda/UserAccess/Roles/hooks/useSelectRoles.tsx
+++ frontend/eda/UserAccess/Roles/hooks/useSelectRoles.tsx
@@ -1,20 +1,22 @@
+import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useEdaView } from '../../../useEventDrivenView';
 import { useRoleColumns } from './useRoleColumns';
 import { API_PREFIX } from '../../../constants';
 import { EdaRole } from '../../../interfaces/EdaRole';
-import { useSelectDialog } from '../../../../../framework';
+import { IToolbarFilter, useSelectDialog } from '../../../../../framework';
 
 export function useSelectRoles() {
   const { t } = useTranslation();
   const tableColumns = useRoleColumns(false);
+  const toolbarFilters = useRef<IToolbarFilter[]>([]);
   const view = useEdaView<EdaRole>({
     url: `${API_PREFIX}/roles/`,
     tableColumns,
     disableQueryString: true,
   });
   return useSelectDialog<EdaRole, true>({
-    toolbarFilters: [],
+    toolbarFilters: toolbarFilters.current,
     tableColumns,
     view,
     confirm: t('Confirm'),
