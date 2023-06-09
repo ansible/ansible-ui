d7e9253b3f091bf0567a1d60ef53b9040562e4fb
James Talton <jtalton@redhat.com>
2023-06-09 15:36:20 -0400

    HUB: Support running 'npm run hub' (#623)
    Signed-off-by: James Talton <jtalton@redhat.com>

---
+++ webpack.awx.cjs
@@ -0,0 +1,11 @@
+const webpackConfig = require('./webpack.config');
+module.exports = function (_env, argv) {
+  const config = webpackConfig(_env, argv);
+  config.devServer.proxy = {
+    '/api': {
+      target: 'http://localhost:5001',
+      secure: false,
+    },
+  };
+  return config;
+};
