9bef6b01f713b5612e08560b82c25c2270ccdd3b
James Talton <jtalton@redhat.com>
2023-06-09 15:28:08 -0400

    Enhance PR GitHub Action to only run tests for what has changed (#620)
    Signed-off-by: James Talton <jtalton@redhat.com>

---
+++ cypress/e2e/hub/hub-dashboard.cy.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/// <reference types="cypress" />
+
+describe('hub dashboard', () => {
+  it.skip('renders', () => {
+    // TODO
+  });
+});
