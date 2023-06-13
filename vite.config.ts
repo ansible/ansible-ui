7a25a6f71d573d13f1e0c7346568f6198ae97659
James Talton <jtalton@redhat.com>
2023-06-13 16:33:22 -0400

    disable PWA as it seems to be messing up e2e tests
    Signed-off-by: James Talton <jtalton@redhat.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
@@ -8,45 +8,45 @@ export default defineConfig({
   plugins: [
     svgr({ exportAsDefault: true }),
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}', 'assets/**/*', 'locales/**/*'],
-        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
-      },
-      manifest: {
-        name: 'Ansible',
-        short_name: 'Ansible',
-        description: 'Ansible',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'maskable-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: 'maskable-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   workbox: {
+    //     globPatterns: ['**/*.{js,css,html,ico,png,svg}', 'assets/**/*', 'locales/**/*'],
+    //     maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
+    //   },
+    //   manifest: {
+    //     name: 'Ansible',
+    //     short_name: 'Ansible',
+    //     description: 'Ansible',
+    //     theme_color: '#000000',
+    //     icons: [
+    //       {
+    //         src: 'icon-192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //         purpose: 'any',
+    //       },
+    //       {
+    //         src: 'icon-512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any',
+    //       },
+    //       {
+    //         src: 'maskable-192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //         purpose: 'maskable',
+    //       },
+    //       {
+    //         src: 'maskable-512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'maskable',
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   server: {
     open: true,
