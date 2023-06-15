1eb6b4d7dccdc32a493f2f97353eb32e60e6e53e
Keith J. Grant <keithjgrant@redhat.com>
2023-06-15 16:06:36 -0700

    add live job events from ws

--- frontend/awx/views/jobs/JobOutput/useJobOutput.tsx
+++ frontend/awx/views/jobs/JobOutput/useJobOutput.tsx
@@ -1,8 +1,16 @@
 import { useCallback, useRef, useState, useEffect } from 'react';
 import { ItemsResponse, requestGet } from '../../../../common/crud/Data';
+import { useAwxWebSocketSubscription } from '../../../common/useAwxWebSocket';
 import { Job } from '../../../interfaces/Job';
 import { JobEvent } from '../../../interfaces/JobEvent';
 import type { IToolbarFilter } from '../../../../../framework';
+
+type WebSocketMessage = {
+  group_name?: string;
+  type?: string;
+  status?: string;
+  inventory_id?: number;
+};
 
 export function useJobOutput(
   job: Job,
@@ -16,9 +24,9 @@ export function useJobOutput(
 
   const getJobOutputEvent = useCallback((counter: number) => jobEvents[counter + 1], [jobEvents]);
 
+  const isFiltered = Object.keys(filters).length > 0;
   const queryJobOutputEvent = useCallback(
     (counter: number) => {
-      const isFiltered = Object.keys(filters).length > 0;
       const jobEvent = jobEvents[counter + 1];
       if (!jobEvent && !isQuerying.current.querying) {
         isQuerying.current.querying = true;
@@ -57,7 +65,64 @@ export function useJobOutput(
       }
       return jobEvent;
     },
-    [job.id, job.type, jobEvents, pageSize, filters, toolbarFilters]
+    [job.id, job.type, jobEvents, pageSize, filters, toolbarFilters, isFiltered]
+  );
+
+  const batchedEvents = useRef([] as JobEvent[]);
+  const batchTimeout = useRef(undefined as ReturnType<typeof setTimeout> | undefined);
+  const addBatchedEvents = useCallback(() => {
+    if (isFiltered) {
+      console.log('is filtered');
+      return;
+    }
+    console.log('adding', batchedEvents.current);
+    const newCount = jobEventCount + batchedEvents.current.length;
+    setJobEvents((jobEvents) => {
+      batchedEvents.current.forEach((message: JobEvent) => {
+        jobEvents[message.counter] = message;
+      });
+      batchedEvents.current = [];
+      return jobEvents;
+    });
+    setJobEventCount(newCount);
+  }, [isFiltered, jobEventCount]);
+
+  const eventGroup = `${job.type}_events`;
+  const handleWebSocketMessage = useCallback(
+    (message?: WebSocketMessage) => {
+      if (message?.group_name === eventGroup) {
+        const jobEvent = message as JobEvent;
+        // setJobEvents((jobEvents) => {
+        //   jobEvents = { ...jobEvents };
+        //   let i = Object.keys(jobEvents).length + 1;
+        //   if (isFiltered) {
+        //     jobEvents[i] = jobEvent;
+        //     i++;
+        //   } else {
+        //     jobEvents[jobEvent.counter] = jobEvent;
+        //   }
+        //   return jobEvents;
+        // });
+        // setJobEventCount((jobEventCount) => Math.max(jobEventCount, jobEvent.counter));
+        batchedEvents.current.push(jobEvent);
+        clearTimeout(batchTimeout.current);
+        if (batchedEvents.current.length >= 10) {
+          console.log('10 events batched, adding...');
+          addBatchedEvents();
+        } else {
+          batchTimeout.current = setTimeout(addBatchedEvents, 500);
+        }
+      }
+    },
+    [addBatchedEvents, eventGroup]
+  );
+  useAwxWebSocketSubscription(
+    {
+      control: ['limit_reached_1'],
+      jobs: ['summary', 'status_changed'],
+      [eventGroup]: [job.id],
+    },
+    handleWebSocketMessage as (message: unknown) => void
   );
 
   useEffect(() => {
