ded827a0ff834bbd4d2e1700a27ca08530df3bce
Martin Hradil <mhradil@redhat.com>
2023-06-16 18:30:51 +0000

    frontend/hub/api.tsx: export pulpAPI, hubAPI
        pulpAPI`/tasks/` === hubAPI`/pulp/api/v3/tasks/` === `/api/automation-hub/pulp/api/v3/tasks/`
    
    but understands process.env.HUB_API_BASE_PATH
    
    also urlencodes any interpolated variables
    
        hubAPI`/foo/${'bar/baz'}/` === '/api/automation-hub/foo/bar%2Fbaz/'

---
+++ frontend/hub/api.tsx
@@ -0,0 +1,23 @@
+function apiTag(strings: TemplateStringsArray, ...values: string[]) {
+  if (strings[0]?.[0] !== '/') {
+    throw 'Invalid URL';
+  }
+
+  let url = '';
+  strings.forEach((fragment, index) => {
+    url += fragment;
+    if (index !== strings.length - 1) {
+      url += encodeURIComponent(`${values.shift() ?? ''}`);
+    }
+  });
+
+  return url;
+}
+
+export function hubAPI(strings: TemplateStringsArray, ...values: string[]) {
+  return process.env.HUB_API_BASE_PATH + apiTag(strings, ...values);
+}
+
+export function pulpAPI(strings: TemplateStringsArray, ...values: string[]) {
+  return process.env.HUB_API_BASE_PATH + '/pulp/api/v3' + apiTag(strings, ...values);
+}
