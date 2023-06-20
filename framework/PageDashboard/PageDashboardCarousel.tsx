51c07803d1afb53b6f034a1302a9a79519998bb7
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2023-06-20 09:06:04 -0400

    Page carousel component (#632)

---
+++ framework/PageDashboard/PageDashboardCarousel.tsx
@@ -0,0 +1,21 @@
+import { CardBody } from '@patternfly/react-core';
+import { PageDashboardCard, PageDashboardCardWidth } from './PageDashboardCard';
+import { PageCarousel } from '../PageCarousel/PageCarousel';
+import { ReactNode } from 'react';
+
+export function PageDashboardCarousel(props: {
+  title: string;
+  linkText?: string;
+  width?: PageDashboardCardWidth;
+  children: ReactNode;
+}) {
+  return (
+    <PageDashboardCard title={props.title} linkText={props.linkText} width={props.width}>
+      <CardBody>
+        <PageCarousel carouselId={props.title.replace(/\s+/g, '-').toLowerCase()}>
+          {props.children}
+        </PageCarousel>
+      </CardBody>
+    </PageDashboardCard>
+  );
+}
