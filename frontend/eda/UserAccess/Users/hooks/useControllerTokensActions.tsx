fc3aae748cc4034160e9eeb90a2535d834668222
lgalis <lgalis@users.noreply.github.com>
2023-06-21 10:09:29 -0400

    [AAP-12838] Remove the Create Token button from the Controller Tokens list page (#603)

--- frontend/eda/UserAccess/Users/hooks/useControllerTokensActions.tsx
+++ frontend/eda/UserAccess/Users/hooks/useControllerTokensActions.tsx
@@ -1,29 +1,16 @@
-import { ButtonVariant } from '@patternfly/react-core';
-import { PlusIcon, TrashIcon } from '@patternfly/react-icons';
+import { TrashIcon } from '@patternfly/react-icons';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
 import { IPageAction, PageActionSelection, PageActionType } from '../../../../../framework';
-import { RouteObj } from '../../../../Routes';
 import { EdaControllerToken } from '../../../interfaces/EdaControllerToken';
 import { IEdaView } from '../../../useEventDrivenView';
 import { useDeleteControllerTokens } from './useDeleteControllerTokens';
 
 export function useControllerTokensActions(view: IEdaView<EdaControllerToken>) {
   const { t } = useTranslation();
-  const navigate = useNavigate();
   const deleteControllerTokens = useDeleteControllerTokens(view.unselectItemsAndRefresh);
   return useMemo<IPageAction<EdaControllerToken>[]>(
     () => [
-      {
-        type: PageActionType.Button,
-        selection: PageActionSelection.None,
-        variant: ButtonVariant.primary,
-        isPinned: true,
-        icon: PlusIcon,
-        label: t('Create controller token'),
-        onClick: () => navigate(RouteObj.CreateEdaControllerToken),
-      },
       {
         type: PageActionType.Button,
         selection: PageActionSelection.Multiple,
@@ -34,6 +21,6 @@ export function useControllerTokensActions(view: IEdaView<EdaControllerToken>) {
         isDanger: true,
       },
     ],
-    [deleteControllerTokens, navigate, t]
+    [deleteControllerTokens, t]
   );
 }
