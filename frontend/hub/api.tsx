4a23cdfa7fccf6b566c802683fec741e507ed41f
Martin Hradil <mhradil@redhat.com>
2023-06-23 00:14:27 +0000

    fixups

--- frontend/hub/api.tsx
+++ frontend/hub/api.tsx
@@ -34,7 +34,7 @@ export function getQueryString(queryParams: QueryParams) {
 
 const UUIDRegEx = /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/i;
 
-export function parsePulpIDFromURL(url: string): string {
+export function parsePulpIDFromURL(url: string): string | null {
   for (const section of url.split('/')) {
     if (section.match(UUIDRegEx)) {
       return section;
