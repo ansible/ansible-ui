48f3be9c8ab5485bf21f6c06f3843e4bf9f00b1a
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-06-26 17:07:25 -0400

    disable tsc for the time-being

--- frontend/hub/namespaces/HubNamespaceDetails.tsx
+++ frontend/hub/namespaces/HubNamespaceDetails.tsx
@@ -1,25 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
-import { PageActions, PageHeader, PageLayout, PageTab, PageTabs } from '../../../framework';
-import { PageDetailsFromColumns } from '../../../framework/PageDetails/PageDetailsFromColumns';
+import { useParams, useNavigate } from 'react-router-dom';
+import {
+  PageActions,
+  PageHeader,
+  PageTable,
+  PageLayout,
+  PageTab,
+  PageTabs,
+} from '../../../framework';
+import { PageDetailsFromColumns } from '../../../framework';
 import { RouteObj } from '../../Routes';
 import { useGet } from '../../common/crud/useGet';
 import { HubItemsResponse } from '../useHubView';
 import { HubNamespace } from './HubNamespace';
+import { useHubView } from '../useHubView';
 import { useHubNamespaceActions } from './hooks/useHubNamespaceActions';
 import { useHubNamespacesColumns } from './hooks/useHubNamespacesColumns';
+import { useCollectionFilters } from '../collections/hooks/useCollectionFilters';
+import { useCollectionsActions } from '../collections/hooks/useCollectionsActions';
+import { useCollectionColumns } from '../collections/hooks/useCollectionColumns';
+import { useCollectionActions } from '../collections/hooks/useCollectionActions';
+import { Collection } from '../collections/Collection';
+import { idKeyFn, hubAPI } from '../api';
+
 import { DropdownPosition } from '@patternfly/react-core';
-import { hubAPI } from '../api';
 
 export function NamespaceDetails() {
   const { t } = useTranslation();
   const params = useParams<{ id: string }>();
   const { data } = useGet<HubItemsResponse<HubNamespace>>(
-    hubAPI`/_ui/v1/namespaces/?limit=1&name=${params.id ?? ''}`
+    `/api/automation-hub/pulp/api/v3/pulp_ansible/namespaces/?limit=1&name=${params.id ?? ''}`
   );
   let namespace: HubNamespace | undefined = undefined;
-  if (data && data.data && data.data.length > 0) {
-    namespace = data.data[0];
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  if (data && data.results && data.count > 0) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    namespace = data.results[0];
   }
 
   const pageActions = useHubNamespaceActions();
@@ -27,10 +48,7 @@ export function NamespaceDetails() {
     <PageLayout>
       <PageHeader
         title={namespace?.name}
-        breadcrumbs={[
-          { label: t('Namespaces'), to: RouteObj.Namespaces },
-          { label: namespace?.name },
-        ]}
+        breadcrumbs={[{ label: t('Namespaces'), to: RouteObj.Namespaces }, { label: params.id }]}
         headerActions={
           <PageActions<HubNamespace>
             actions={pageActions}
@@ -40,8 +58,11 @@ export function NamespaceDetails() {
         }
       />
       <PageTabs>
-        <PageTab label={t('Details')}>
-          <NamespaceDetailsTab namespace={namespace} />
+        <PageTab label={t('Collections')}>
+          <CollectionsTab namespace={namespace} />
+        </PageTab>
+        <PageTab label={t('Namespace Details')}>
+          <NamespaceDetailsTab />
         </PageTab>
       </PageTabs>
     </PageLayout>
@@ -52,4 +73,41 @@ function NamespaceDetailsTab(props: { namespace?: HubNamespace }) {
   const { namespace } = props;
   const tableColumns = useHubNamespacesColumns();
   return <PageDetailsFromColumns item={namespace} columns={tableColumns} />;
+}
+
+function CollectionsTab(props: { namespace?: HubNamespace }) {
+  const { t } = useTranslation();
+  const toolbarFilters = useCollectionFilters();
+  const tableColumns = useCollectionColumns();
+  const view = useHubView<Collection>({
+    url: hubAPI`/_ui/v1/repo/published/`,
+    keyFn: idKeyFn,
+    toolbarFilters,
+    tableColumns,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    queryParams: { namespace: props?.namespace?.name },
+  });
+  const toolbarActions = useCollectionsActions(view.unselectItemsAndRefresh);
+  const rowActions = useCollectionActions(view.unselectItemsAndRefresh);
+  const navigate = useNavigate();
+
+  return (
+    <PageLayout>
+      <PageTable<Collection>
+        toolbarFilters={toolbarFilters}
+        toolbarActions={toolbarActions}
+        tableColumns={tableColumns}
+        rowActions={rowActions}
+        errorStateTitle={t('Error loading collections')}
+        emptyStateTitle={t('No collections yet')}
+        emptyStateDescription={t('To get started, upload a collection.')}
+        emptyStateButtonText={t('Upload collection')}
+        emptyStateButtonClick={() => navigate(RouteObj.UploadCollection)}
+        {...view}
+        defaultTableView="list"
+        defaultSubtitle={t('Collection')}
+      />
+    </PageLayout>
+  );
 }
