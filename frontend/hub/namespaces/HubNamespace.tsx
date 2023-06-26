e509333363605d057294bd87b0dd033953c828d8
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-06-26 10:07:21 -0400

    update ns type interface

--- frontend/hub/namespaces/HubNamespace.tsx
+++ frontend/hub/namespaces/HubNamespace.tsx
@@ -1,17 +1,44 @@
+export interface LinksType {
+  name: string;
+  url: string;
+}
+
+export interface LatestMetadataType {
+  pulp_href: string;
+  name: string;
+  company: string;
+  email: string;
+  description: string;
+  resources: string;
+  links: LinksType[];
+  avatar_sha256: string | null;
+  avatar_url: string | null;
+  metadata_sha256: string;
+  groups: string[];
+  task: string | null;
+}
+
 export interface HubNamespace {
   pulp_href: string;
+  pulp_created: string;
   id: number;
   name: string;
   company: string;
   email: string;
   avatar_url: string;
   description: string;
+  num_collections: number;
   groups: [
     {
       id: number;
       name: string;
-      object_roles: string[];
+      object_permissions: string[];
+      pulp_href?: string;
     }
   ];
-  related_fields: object;
+  resources: string;
+  owners: string[];
+  links: LinksType[];
+  my_permissions: string[];
+  latest_metadata: LatestMetadataType;
 }
