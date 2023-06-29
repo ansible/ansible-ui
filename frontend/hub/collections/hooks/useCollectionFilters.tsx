658d9236a198e727ab686f573809006566135f4f
MilanPospisil <arkanus@seznam.cz>
2023-06-29 11:54:32 +0200

    useMemo correct usage

--- frontend/hub/collections/hooks/useCollectionFilters.tsx
+++ frontend/hub/collections/hooks/useCollectionFilters.tsx
@@ -18,43 +18,46 @@ export function useCollectionFilters() {
 
   const repoList: any = repositories.pageItems || [];
 
-  return [
-    {
-      key: 'keywords',
-      label: t('Name'),
-      type: 'string',
-      query: 'keywords',
-      comparison: 'equals',
-    },
-    {
-      key: 'namespace',
-      label: t('Namespace'),
-      type: 'string',
-      query: 'namespace',
-      comparison: 'equals',
-    },
-    {
-      key: 'is_signed',
-      label: t('Signature'),
-      type: 'select',
-      query: 'is_signed',
-      options: [
-        { label: t('Signed'), value: 'true' },
-        { label: t('Unsigned'), value: 'false' },
-      ],
-      placeholder: t('Select signatures'),
-    },
-    {
-      key: 'repository_name',
-      label: t('Repository'),
-      type: 'select',
-      query: 'repository_name',
-      options: repoList.map((repo: Repository) => {
-        return { label: repo.name, value: repo.name };
-      }),
-      placeholder: t('Select repositories'),
-    },
-  ];
+  return useMemo<IToolbarFilter[]>(
+    () => [
+      {
+        key: 'keywords',
+        label: t('Name'),
+        type: 'string',
+        query: 'keywords',
+        comparison: 'equals',
+      },
+      {
+        key: 'namespace',
+        label: t('Namespace'),
+        type: 'string',
+        query: 'namespace',
+        comparison: 'equals',
+      },
+      {
+        key: 'is_signed',
+        label: t('Signature'),
+        type: 'select',
+        query: 'is_signed',
+        options: [
+          { label: t('Signed'), value: 'true' },
+          { label: t('Unsigned'), value: 'false' },
+        ],
+        placeholder: t('Select signatures'),
+      },
+      {
+        key: 'repository_name',
+        label: t('Repository'),
+        type: 'select',
+        query: 'repository_name',
+        options: repoList.map((repo: Repository) => {
+          return { label: repo.name, value: repo.name };
+        }),
+        placeholder: t('Select repositories'),
+      },
+    ],
+    [t, repoList]
+  );
 }
 
 /*
