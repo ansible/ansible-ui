203627824508233098f961af97f19f8bb50d9048
MilanPospisil <arkanus@seznam.cz>
2023-06-29 11:18:25 +0200

    Repo filter

--- frontend/hub/collections/hooks/useCollectionFilters.tsx
+++ frontend/hub/collections/hooks/useCollectionFilters.tsx
@@ -1,39 +1,60 @@
-import { useMemo } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { IToolbarFilter } from '../../../../framework';
+import { usePulpView } from '../../usePulpView';
+import { pulpAPI } from '../../api';
 
 export function useCollectionFilters() {
   const { t } = useTranslation();
-  return useMemo<IToolbarFilter[]>(
-    () => [
-      {
-        key: 'keywords',
-        label: t('Name'),
-        type: 'string',
-        query: 'keywords',
-        comparison: 'equals',
-      },
-      {
-        key: 'namespace',
-        label: t('Namespace'),
-        type: 'string',
-        query: 'namespace',
-        comparison: 'equals',
-      },
-      {
-        key: 'is_signed',
-        label: t('Signature'),
-        type: 'select',
-        query: 'is_signed',
-        options: [
-          { label: t('Signed'), value: 'true' },
-          { label: t('Unsigned'), value: 'false' },
-        ],
-        placeholder: t('Select signatures'),
-      },
-    ],
-    [t]
-  );
+  //const [repositories, setRepositories] = useState([]);
+
+  const repositories = usePulpView<Repository>({
+    url: pulpAPI`/repositories/ansible/ansible/`,
+    keyFn: (item) => item.name,
+    queryParams: {
+      pulp_label_select: '!hide_from_search',
+    },
+  });
+
+  const repoList: any = repositories.pageItems || [];
+
+  return [
+    {
+      key: 'keywords',
+      label: t('Name'),
+      type: 'string',
+      query: 'keywords',
+      comparison: 'equals',
+    },
+    {
+      key: 'namespace',
+      label: t('Namespace'),
+      type: 'string',
+      query: 'namespace',
+      comparison: 'equals',
+    },
+    {
+      key: 'is_signed',
+      label: t('Signature'),
+      type: 'select',
+      query: 'is_signed',
+      options: [
+        { label: t('Signed'), value: 'true' },
+        { label: t('Unsigned'), value: 'false' },
+      ],
+      placeholder: t('Select signatures'),
+    },
+    {
+      key: 'repository_name',
+      label: t('Repository'),
+      type: 'select',
+      query: 'repository_name',
+      options: repoList.map((repo: Repository) => {
+        return { label: repo.name, value: repo.name };
+      }),
+      placeholder: t('Select repositories'),
+    },
+  ];
 }
 
 /*
@@ -102,3 +123,7 @@ export function useCollectionFilters() {
   );
 }
 */
+
+interface Repository {
+  name: string;
+}
