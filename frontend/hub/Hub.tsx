d293ae649f8312f77e0bfc0ca95998ccbd93a5ad
Martin Hradil <mhradil@redhat.com>
2023-06-30 16:13:55 +0200

    Hub - add hook for settings, feature flags & permissions (#655)

--- frontend/hub/Hub.tsx
+++ frontend/hub/Hub.tsx
@@ -2,11 +2,14 @@ import { Page } from '@patternfly/react-core';
 import { AnsibleMasthead } from '../common/Masthead';
 import { HubRouter } from './HubRouter';
 import { HubSidebar } from './HubSidebar';
+import { HubContextProvider } from './useHubContext';
 
 export function Hub() {
   return (
     <Page header={<AnsibleMasthead />} sidebar={<HubSidebar />}>
-      <HubRouter />
+      <HubContextProvider>
+        <HubRouter />
+      </HubContextProvider>
     </Page>
   );
 }
