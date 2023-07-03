da728ab2de83be46bb683829a5e33b50f95bb310
lgalis <lgalis@users.noreply.github.com>
2023-07-03 14:11:26 -0400

    [AAP-]12160] Add expanded row to the users list, containing the user's roles (#670)

--- frontend/eda/UserAccess/Users/hooks/useUserColumns.tsx
+++ frontend/eda/UserAccess/Users/hooks/useUserColumns.tsx
@@ -1,7 +1,13 @@
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
-import { ITableColumn, TextCell } from '../../../../../framework';
+import {
+  ColumnModalOption,
+  ColumnTableOption,
+  ITableColumn,
+  LabelsCell,
+  TextCell,
+} from '../../../../../framework';
 import { RouteObj } from '../../../../Routes';
 import { EdaUser } from '../../../interfaces/EdaUser';
 
@@ -28,6 +34,16 @@ export function useUserColumns() {
       {
         header: t('Last name'),
         cell: (user) => user.last_name && <TextCell text={user.last_name} />,
+      },
+      {
+        header: t('Role(s)'),
+        cell: (user) =>
+          user.roles &&
+          user.roles.length > 0 && <LabelsCell labels={user.roles.map((role) => role?.name)} />,
+        table: ColumnTableOption.Expanded,
+        card: 'hidden',
+        list: 'secondary',
+        modal: ColumnModalOption.Hidden,
       },
     ],
     [navigate, t]
