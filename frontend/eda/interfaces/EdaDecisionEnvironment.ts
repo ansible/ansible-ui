d0ba513e93b5f60ddbbae545e984171ed0bf6c1b
lgalis <lgalis@users.noreply.github.com>
2023-07-07 14:12:17 -0400

    [AAP-12502][AAP-13133] Fixed the credential id in the Project and Decision Environment edit pages (#684)

--- frontend/eda/interfaces/EdaDecisionEnvironment.ts
+++ frontend/eda/interfaces/EdaDecisionEnvironment.ts
@@ -1,2 +1,3 @@
-import { DecisionEnvironment } from './generated/eda-api';
+import { DecisionEnvironment, DecisionEnvironmentRead } from './generated/eda-api';
 export type EdaDecisionEnvironment = DecisionEnvironment;
+export type EdaDecisionEnvironmentRead = DecisionEnvironmentRead;
