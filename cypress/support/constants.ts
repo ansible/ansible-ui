33a0e3c1054685608722f1b97d587860bfacc711
nixocio <nixocio@gmail.com>
2023-07-07 14:13:33 -0400

    Add initial cypress tests to HUB
    Add initial cypress tests to HUB and command to add `hubLogin`.

---
+++ cypress/support/constants.ts
@@ -0,0 +1,6 @@
+export enum SERVER_NAME {
+  AWX_SERVER = 'AWX Ansible Server',
+  EDA_SERVER = 'Event Driven Automation Server',
+  HUB_SERVER = 'Automation Hub',
+  GALAXY_SERVER = 'Galaxy Server',
+}
