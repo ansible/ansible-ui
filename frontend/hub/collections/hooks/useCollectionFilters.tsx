9596b0464f695c4c6ee51847a9f0fb42a59bcbf2
MilanPospisil <arkanus@seznam.cz>
2023-07-12 11:41:05 +0200

    Delete repo selector for now - waiting for type search ahead PR

--- frontend/hub/collections/hooks/useCollectionFilters.tsx
+++ frontend/hub/collections/hooks/useCollectionFilters.tsx
@@ -1,22 +1,9 @@
-import { useMemo, useState, useEffect } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { IToolbarFilter } from '../../../../framework';
-import { usePulpView } from '../../usePulpView';
-import { pulpAPI } from '../../api';
 
 export function useCollectionFilters() {
   const { t } = useTranslation();
-  //const [repositories, setRepositories] = useState([]);
-
-  const repositories = usePulpView<Repository>({
-    url: pulpAPI`/repositories/ansible/ansible/`,
-    keyFn: (item) => item.name,
-    queryParams: {
-      pulp_label_select: '!hide_from_search',
-    },
-  });
-
-  const repoList: any = repositories.pageItems || [];
 
   return useMemo<IToolbarFilter[]>(
     () => [
@@ -64,22 +51,7 @@ export function useCollectionFilters() {
           { label: t('windows'), value: 'windows' },
         ],
       },
-      {
-        key: 'repository_name',
-        label: t('Repository'),
-        type: 'select',
-        query: 'repository_name',
-        options: repoList.map((repo: Repository) => {
-          return { label: repo.name, value: repo.name };
-        }),
-        placeholder: t('Select repositories'),
-        hasSearch: true,
-      },
     ],
-    [t, repoList]
+    [t]
   );
-}
-
-interface Repository {
-  name: string;
 }
