476dd9ba5a69edcd6519d68b270e50abef5b348b
Alex Corey <alex.swansboro@gmail.com>
2023-07-17 11:51:04 -0400

    Adds Prompt on Launch Functionality for Job Template and Workflow Job Templates schedules

--- frontend/awx/interfaces/ScheduleFormFields.ts
+++ frontend/awx/interfaces/ScheduleFormFields.ts
@@ -1,4 +1,4 @@
-import { RegularInventory } from './Inventory';
+import { InstanceGroup } from './InstanceGroup';
 import { InventorySource } from './InventorySource';
 import { JobTemplate } from './JobTemplate';
 import { Project } from './Project';
@@ -6,15 +6,18 @@ import { Schedule } from './Schedule';
 import { WorkflowJobTemplate } from './WorkflowJobTemplate';
 
 export interface ScheduleFormFields
-  extends Omit<Schedule, 'unified_job_template' | 'timezone' | 'inventory'> {
-  unified_job_template: JobTemplate | WorkflowJobTemplate | Project | InventorySource;
+  extends Omit<
+    Schedule,
+    'unified_job_template' | 'timezone' | 'inventory' | 'execution_environment'
+  > {
+  unified_job_template_object: JobTemplate | WorkflowJobTemplate | Project | InventorySource;
+  unified_job_template: number;
   resourceName: string;
   name: string;
   description?: string;
   resource_type: string;
   startDateTime: { startDate: string; startTime: string };
   timezone: string;
-  inventory: RegularInventory | null;
   frequencies: string[];
   freq: number;
   interval: number;
@@ -30,4 +33,52 @@ export interface ScheduleFormFields
   endTime: string;
   count: number;
   endingType: 'never';
+  organizationId?: number;
+  extra_vars?: string;
+  defaultInstanceGroups: InstanceGroup[];
+  newInstanceGroups: InstanceGroup[];
+  arrayedSkipTags: { name: string }[];
+  arrayedJobTags: { name: string }[];
+  inventory: {
+    name: string;
+    id: number;
+  };
+  inventoryId: number;
+  labels: { name: string; id: number }[];
+  newLabels: { id: number; name: string }[];
+  limit: string;
+  scm_branch: string;
+  job_tags: string;
+  skip_tags: string;
+  diff_mode: boolean;
+  job_type: 'run' | 'check';
+  verbosity: '0' | '1' | '2' | '3' | '4' | '5';
+  credentials?: [
+    {
+      id: number;
+      name: string;
+      credential_type: number;
+      passwords_needed: string[];
+    }
+  ];
+  newCredentials: [
+    {
+      id: number;
+      name: string;
+      credential_type: number;
+      passwords_needed: string[];
+    }
+  ];
+  execution_environment: { id: number; name: string };
+  executionEnvironmentId: number;
+  forks: number;
+  job_slice_count: number;
+  timeout: number;
+  instance_groups: [];
+
+  job_template_data: {
+    name: string;
+    id: number;
+    description: string;
+  };
 }
