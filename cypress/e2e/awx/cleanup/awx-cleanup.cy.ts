bfbd159298c2e94d5923c54272ed7bd93d9245e4
James Talton <jtalton@redhat.com>
2023-07-18 10:37:38 -0400

    Rename ItemsResponse to AwxItemsResponse (#706)
    Signed-off-by: James Talton <talton@gmail.com>

--- cypress/e2e/awx/cleanup/awx-cleanup.cy.ts
+++ cypress/e2e/awx/cleanup/awx-cleanup.cy.ts
@@ -1,3 +1,4 @@
+import { AwxItemsResponse } from '../../../../frontend/awx/common/AwxItemsResponse';
 import { Inventory } from '../../../../frontend/awx/interfaces/Inventory';
 import { Job } from '../../../../frontend/awx/interfaces/Job';
 import { JobTemplate } from '../../../../frontend/awx/interfaces/JobTemplate';
@@ -5,13 +6,12 @@ import { Organization } from '../../../../frontend/awx/interfaces/Organization';
 import { Project } from '../../../../frontend/awx/interfaces/Project';
 import { User } from '../../../../frontend/awx/interfaces/User';
 import { getJobsAPIUrl } from '../../../../frontend/awx/views/jobs/jobUtils';
-import { ItemsResponse } from '../../../../frontend/common/crud/Data';
 
 const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000).toISOString();
 
 describe('AWX Cleanup', () => {
   it('cleanup projects', () => {
-    cy.awxRequestGet<ItemsResponse<Project>>(
+    cy.awxRequestGet<AwxItemsResponse<Project>>(
       `/api/v2/projects?name__startswith=E2E&page=1&page_size=200&created__lt=${tenMinutesAgo}`
     ).then((result) => {
       for (const resource of result.results ?? []) {
@@ -21,7 +21,7 @@ describe('AWX Cleanup', () => {
   });
 
   it('cleanup inventories', () => {
-    cy.awxRequestGet<ItemsResponse<Inventory>>(
+    cy.awxRequestGet<AwxItemsResponse<Inventory>>(
       `/api/v2/inventories?name__startswith=E2E&page=1&page_size=200&created__lt=${tenMinutesAgo}`
     ).then((result) => {
       for (const resource of result.results ?? []) {
@@ -31,7 +31,7 @@ describe('AWX Cleanup', () => {
   });
 
   it('cleanup organizations', () => {
-    cy.awxRequestGet<ItemsResponse<Organization>>(
+    cy.awxRequestGet<AwxItemsResponse<Organization>>(
       `/api/v2/organizations?name__startswith=E2E&page=1&page_size=200&created__lt=${tenMinutesAgo}`
     ).then((result) => {
       for (const resource of result.results ?? []) {
@@ -41,7 +41,7 @@ describe('AWX Cleanup', () => {
   });
 
   it('cleanup users', () => {
-    cy.awxRequestGet<ItemsResponse<User>>(
+    cy.awxRequestGet<AwxItemsResponse<User>>(
       `/api/v2/users?username__startswith=e2e&page=1&page_size=200&created__lt=${tenMinutesAgo}`
     ).then((result) => {
       for (const resource of result.results ?? []) {
@@ -51,7 +51,7 @@ describe('AWX Cleanup', () => {
   });
 
   it('cleanup templates', () => {
-    cy.awxRequestGet<ItemsResponse<JobTemplate>>(
+    cy.awxRequestGet<AwxItemsResponse<JobTemplate>>(
       `/api/v2/unified_job_templates/?name__startswith=E2E&page=1&page_size=200&created__lt=${tenMinutesAgo}`
     ).then((result) => {
       for (const resource of result.results ?? []) {
@@ -61,7 +61,7 @@ describe('AWX Cleanup', () => {
   });
 
   it('cleanup jobs', () => {
-    cy.awxRequestGet<ItemsResponse<Job>>(
+    cy.awxRequestGet<AwxItemsResponse<Job>>(
       `/api/v2/unified_jobs/?name__startswith=E2E&page=1&page_size=200&created__lt=${tenMinutesAgo}`
     ).then((result) => {
       for (const resource of result.results ?? []) {
