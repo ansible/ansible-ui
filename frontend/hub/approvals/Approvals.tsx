6bffaa96a8852efd9bff880efaaca51bdb17af75
MilanPospisil <arkanus@seznam.cz>
2023-08-10 12:39:13 +0200

    WIP - action menu item reject

--- frontend/hub/approvals/Approvals.tsx
+++ frontend/hub/approvals/Approvals.tsx
@@ -5,11 +5,14 @@ import { useHubView } from '../useHubView';
 import { CollectionVersionSearch } from './Approval';
 import { useApprovalFilters } from './hooks/useApprovalFilters';
 import { useApprovalsColumns } from './hooks/useApprovalsColumns';
+import { useApprovalActions } from './hooks/useApprovalActions';
 
 export function Approvals() {
   const { t } = useTranslation();
   const toolbarFilters = useApprovalFilters();
   const tableColumns = useApprovalsColumns();
+  const rowActions = useApprovalActions();
+
   const view = useHubView<CollectionVersionSearch>({
     url: hubAPI`/v3/plugin/ansible/search/collection-versions/`,
     keyFn: collectionKeyFn,
@@ -25,6 +28,7 @@ export function Approvals() {
       <PageTable<CollectionVersionSearch>
         toolbarFilters={toolbarFilters}
         tableColumns={tableColumns}
+        rowActions={rowActions}
         errorStateTitle={t('Error loading approvals')}
         emptyStateTitle={t('No approvals yet')}
         {...view}
