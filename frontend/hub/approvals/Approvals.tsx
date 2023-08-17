01ff4e344e937508a97bfceadd7830ea78a8e354
MilanPospisil <arkanus@seznam.cz>
2023-08-17 16:31:07 +0200

    Approvals reject (#789)

--- frontend/hub/approvals/Approvals.tsx
+++ frontend/hub/approvals/Approvals.tsx
@@ -5,11 +5,14 @@ import { useHubView } from '../useHubView';
 import { CollectionVersionSearch } from './Approval';
 import { useApprovalFilters } from './hooks/useApprovalFilters';
 import { useApprovalsColumns } from './hooks/useApprovalsColumns';
+import { useApprovalActions } from './hooks/useApprovalActions';
+import { useApprovalsActions } from './hooks/useApprovalsActions';
 
 export function Approvals() {
   const { t } = useTranslation();
   const toolbarFilters = useApprovalFilters();
   const tableColumns = useApprovalsColumns();
+
   const view = useHubView<CollectionVersionSearch>({
     url: hubAPI`/v3/plugin/ansible/search/collection-versions/`,
     keyFn: collectionKeyFn,
@@ -19,12 +22,17 @@ export function Approvals() {
     defaultFilters: { status: ['pipeline=staging'] },
   });
 
+  const rowActions = useApprovalActions(view.unselectItemsAndRefresh);
+  const toolbarActions = useApprovalsActions(view.unselectItemsAndRefresh);
+
   return (
     <PageLayout>
       <PageHeader title={t('Collection Approvals')} />
       <PageTable<CollectionVersionSearch>
         toolbarFilters={toolbarFilters}
         tableColumns={tableColumns}
+        rowActions={rowActions}
+        toolbarActions={toolbarActions}
         errorStateTitle={t('Error loading approvals')}
         emptyStateTitle={t('No approvals yet')}
         {...view}
