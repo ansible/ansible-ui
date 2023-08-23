6c1d9346c7b7bf2462baf6546f47296d373bb0fe
MilanPospisil <arkanus@seznam.cz>
2023-08-23 12:18:05 +0200

    First changes

---
+++ framework/PageToolbar/PageToolbarFilters/ToolbarAsyncMultiSelectFilter.tsx
@@ -0,0 +1,65 @@
+import { ToolbarFilterType } from '../PageToolbarFilter';
+import { PageAsyncMultiSelectOptionsFn } from '../../PageInputs/PageAsyncMultiSelect';
+
+import { PageAsyncQueryErrorTextType } from '../../PageInputs/PageAsyncSingleSelect';
+
+import { ToolbarFilterCommon } from './ToolbarFilterCommon';
+
+/** A function to open a single selection browse modal for a toolbar filter. */
+type ToolbarOpenMultiSelectBrowse = (
+  onSelect: (values: string[]) => void,
+  defaultSelections?: string[]
+) => void;
+
+export interface IToolbarAsyncMultiSelectFilter extends ToolbarFilterCommon {
+  type: ToolbarFilterType.AsyncSingleSelect;
+
+  /** The function to query for options. */
+  queryOptions: PageAsyncMultiSelectOptionsFn<string>;
+
+  /** The placeholder to show while querying. */
+  queryPlaceholder?: string;
+
+  /** The placeholder to show if the query fails. */
+  queryErrorText?: PageAsyncQueryErrorTextType;
+
+  /** The function to open the browse modal. */
+  openBrowse?: ToolbarOpenMultiSelectBrowse;
+
+  /**
+   * Whether the select required an option to be selected.
+   *
+   * If true, the select will autoselect the first option,
+   * else the select will contain a clear button.
+   */
+  isRequired?: boolean;
+}
+
+/**
+ * Helper function to adapt an object based select dialog to a string based select dialog for use in the toolbar.
+ *
+ * Toolbars use string based filters, but many select dialog uses objects.
+ * The toolbar filter uses strings because the values are synced to the URL query string.
+ *
+ * @param selectFn Function to open the original select dialog that uses objects.
+ * @param keyFn Function to get a unique key from the object. Used as the string value in the toolbar filter values and query string.
+ * @param objectFn Function to create an object from the key. Used for default selection in the dialog.
+ * @returns A function to open the select dialog.
+ */
+export function toolbarMultiSelectBrowseAdapter<T>(
+  /** The function to open the original select dialog that uses objects. */
+  selectFn: (onItemsSelect: (itemValue: T[]) => void, itemDefaultSelections?: T[]) => void,
+  /** The function to get a unique key from the object. Used as the string value in the toolbar filter values and query string. */
+  keyFn: (item: T) => string,
+  /** The function to create an object from the key. Used for default selection in the dialog. */
+  objectFn: (name: string) => object
+): ToolbarOpenMultiSelectBrowse {
+  return (onSelect: (value: string[]) => void, defaultSelections?: string[]) => {
+    selectFn(
+      (items: T[]) => onSelect(items.map((item) => keyFn(item))),
+      defaultSelections
+        ? defaultSelections.map((defaultSelection) => objectFn(defaultSelection) as T)
+        : []
+    );
+  };
+}
