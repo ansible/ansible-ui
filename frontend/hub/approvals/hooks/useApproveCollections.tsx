ce442383f3a388c5433da1fb49c69dac14832881
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-08-24 09:26:12 -0400

    create useApproveCollections hook

---
+++ frontend/hub/approvals/hooks/useApproveCollections.tsx
@@ -0,0 +1,78 @@
+import { useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { compareStrings, useBulkConfirmation } from '../../../../framework';
+import { postRequest } from '../../../common/crud/Data';
+import { useGetRequest } from '../../../common/crud/useGet';
+import { collectionKeyFn, parsePulpIDFromURL, pulpAPI } from '../../api/utils';
+import { PulpItemsResponse } from '../../usePulpView';
+import { CollectionVersionSearch } from '../Approval';
+import { useApprovalsColumns } from './useApprovalsColumns';
+
+export function useApproveCollections(
+  onComplete?: (collections: CollectionVersionSearch[]) => void
+) {
+  const { t } = useTranslation();
+  const confirmationColumns = useApprovalsColumns();
+  const actionColumns = useMemo(() => [confirmationColumns[0]], [confirmationColumns]);
+
+  const bulkAction = useBulkConfirmation<CollectionVersionSearch>();
+
+  const getRequest = useGetRequest();
+
+  return useCallback(
+    (collections: CollectionVersionSearch[]) => {
+      bulkAction({
+        title: t('Approve collections', { count: collections.length }),
+        confirmText: t('Yes, I confirm that I want to approve these {{count}} collections.', {
+          count: collections.length,
+        }),
+        actionButtonText: t('Approve collections', { count: collections.length }),
+        items: collections.sort((l, r) =>
+          compareStrings(
+            l.collection_version.pulp_href + l.repository.name,
+            r.collection_version.pulp_href + r.repository.name
+          )
+        ),
+        keyFn: collectionKeyFn,
+        isDanger: true,
+        confirmationColumns,
+        actionColumns,
+        onComplete,
+        actionFn: (collection: CollectionVersionSearch) =>
+          approveCollection(collection, getRequest),
+      });
+    },
+    [actionColumns, bulkAction, confirmationColumns, onComplete, t, getRequest]
+  );
+}
+
+export function approveCollection(
+  collection: CollectionVersionSearch,
+  getRequest: ReturnType<typeof useGetRequest>
+) {
+  let approvedRepo = '';
+
+  async function innerAsync() {
+    const repoRes = (await getRequest(
+      pulpAPI`/repositories/ansible/ansible/?name=published`
+    )) as PulpItemsResponse<Repository>;
+    approvedRepo = repoRes.results[0].pulp_href;
+
+    return postRequest(
+      pulpAPI`/repositories/ansible/ansible/${
+        parsePulpIDFromURL(collection.repository.pulp_href) || ''
+      }/move_collection_version/`,
+      {
+        collection_versions: [`${collection.collection_version.pulp_href}`],
+        destination_repositories: [approvedRepo],
+      }
+    );
+  }
+
+  return innerAsync();
+}
+
+export interface Repository {
+  name: string;
+  pulp_href: string;
+}
