74d85d394928c61b6b19d46a61aa421e427997fe
Vidya Nambiar <vnambiar@redhat.com>
2023-08-30 11:34:25 -0400

    updated regex
    replaceAll

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -27,7 +27,7 @@ export function escapeForShellCommand(args: string[]) {
 
 function escapeString(s: string) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unnecessary-type-assertion
-  const newString = s.replaceAll(/[!\\]/g, '\\$&') as string;
+  const newString = s.replaceAll(/[\\]/g, '\\$&');
   return `"${newString}"`;
 }
 
