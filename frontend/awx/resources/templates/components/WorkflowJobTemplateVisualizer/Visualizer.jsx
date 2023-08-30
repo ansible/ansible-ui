548dc4b50debb5560dcd7a3ca73613842eb8e5d3
James Talton <talton@gmail.com>
2023-08-30 12:37:43 -0400

    eslint fix
    Signed-off-by: James Talton <talton@gmail.com>

--- frontend/awx/resources/templates/components/WorkflowJobTemplateVisualizer/Visualizer.jsx
+++ frontend/awx/resources/templates/components/WorkflowJobTemplateVisualizer/Visualizer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable i18next/no-literal-string */
 /* eslint-disable react/prop-types */
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-floating-promises */
