75354c51ec89853c246b56cb82eba74dafe3abbb
Shaiah Emigh-Doyle <64337863+ShaiahWren@users.noreply.github.com>
2023-08-30 14:11:46 -0400

    Shaiahwren approve collections (#816)

--- frontend/hub/approvals/hooks/useApprovalActions.tsx
+++ frontend/hub/approvals/hooks/useApprovalActions.tsx
@@ -1,14 +1,16 @@
-import { TrashIcon } from '@patternfly/react-icons';
+import { ThumbsDownIcon, ThumbsUpIcon } from '@patternfly/react-icons';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { IPageAction, PageActionSelection, PageActionType } from '../../../../framework';
 import { CollectionVersionSearch } from '../Approval';
 import { useRejectCollections } from './useRejectCollections';
+import { useApproveCollections } from './useApproveCollections';
 
 export function useApprovalActions(callback?: (collections: CollectionVersionSearch[]) => void) {
   const { t } = useTranslation();
 
   const rejectCollections = useRejectCollections(callback);
+  const approveCollections = useApproveCollections(callback);
 
   return useMemo<IPageAction<CollectionVersionSearch>[]>(
     () => [
@@ -17,12 +19,22 @@ export function useApprovalActions(callback?: (collections: CollectionVersionSea
         type: PageActionType.Button,
         selection: PageActionSelection.Single,
         isPinned: true,
-        icon: TrashIcon,
+        icon: ThumbsUpIcon,
+        label: t('Approve'),
+        onClick: (collection) => approveCollections([collection]),
+        isDanger: false,
+      },
+      { type: PageActionType.Seperator },
+      {
+        type: PageActionType.Button,
+        selection: PageActionSelection.Single,
+        isPinned: true,
+        icon: ThumbsDownIcon,
         label: t('Reject'),
         onClick: (collection) => rejectCollections([collection]),
         isDanger: true,
       },
     ],
-    [t, rejectCollections]
+    [t, rejectCollections, approveCollections]
   );
 }
