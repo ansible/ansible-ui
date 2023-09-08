1d9633665785d3180505ef7b12714cb5272ac27e
James Talton <talton@gmail.com>
2023-09-08 14:53:06 -0400

    fixes
    Signed-off-by: James Talton <talton@gmail.com>

--- frontend/common/Login.tsx
+++ frontend/common/Login.tsx
@@ -1,16 +1,13 @@
 import { useCallback, useEffect } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import background from '../../node_modules/@patternfly/patternfly/assets/images/pfbg_1200.jpg';
 import { useLoginModal } from './LoginModal';
 
 export function Login() {
   const navigate = useNavigate();
-  const [searchparams] = useSearchParams();
   const navigateBack = useCallback(() => {
-    if (searchparams.get('navigate-back') === 'true') {
-      navigate(-2);
-    }
-  }, [navigate, searchparams]);
+    navigate('/');
+  }, [navigate]);
   const openLoginModal = useLoginModal(navigateBack);
   useEffect(() => openLoginModal(), [openLoginModal]);
   return (
