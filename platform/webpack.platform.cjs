8946d0900d9f251ebf1627b24c99d9decd120ac3
James Talton <talton@gmail.com>
2023-09-13 17:35:01 -0400

    add platform
    Signed-off-by: James Talton <talton@gmail.com>

---
+++ platform/webpack.platform.cjs
@@ -0,0 +1,53 @@
+const webpackConfig = require('../webpack/webpack.config');
+
+const awxServer = process.env.AWX_HOST
+  ? process.env.AWX_PROTOCOL + '://' + process.env.AWX_HOST
+  : 'http://localhost:8043';
+
+const proxyUrl = new URL(awxServer);
+
+module.exports = function (env, argv) {
+  const config = webpackConfig(env, argv);
+
+  config.entry = './platform/Platform.tsx';
+
+  config.devServer.proxy = {
+    '/api': {
+      target: awxServer,
+      secure: false,
+      bypass: (req) => {
+        req.headers.host = proxyUrl.host;
+        req.headers.origin = proxyUrl.origin;
+        req.headers.referer = proxyUrl.href;
+      },
+    },
+    '/sso': {
+      target: awxServer,
+      secure: false,
+      bypass: (req, res, options) => {
+        req.headers.origin = proxyUrl.origin;
+        req.headers.host = getRawHeader(req.rawHeaders, 'Host') || proxyUrl.host;
+        req.referrer = getRawHeader(req.rawHeaders, 'Referer') || proxyUrl.href;
+      },
+    },
+    '/websocket': {
+      target: awxServer,
+      ws: true,
+      secure: false,
+      bypass: (req) => {
+        req.headers.host = proxyUrl.host;
+        req.headers.origin = proxyUrl.origin;
+        req.headers.referer = proxyUrl.href;
+      },
+    },
+  };
+  return config;
+};
+
+function getRawHeader(rawHeaders, headerName) {
+  const index = rawHeaders.indexOf(headerName);
+  if (index === -1) {
+    return null;
+  }
+  return rawHeaders[index + 1];
+}
