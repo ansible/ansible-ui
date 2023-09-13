04361e8ce26760203d7d1d3810253a34b7fbe385
MilanPospisil <arkanus@seznam.cz>
2023-09-13 13:40:18 +0200

    Expand repository

---
+++ frontend/hub/api-schemas/expanded/RepositoryResponse.ts
@@ -0,0 +1,13 @@
+// URL of interface:
+// /api/pulp/api/v3/repositories/
+// Part of response collection PulpItemsResponse
+
+import './../generated/RepositoryResponse';
+
+declare module './../generated/RepositoryResponse' {
+  interface RepositoryResponse {
+    pulp_labels?: {
+      pipeline?: string;
+    };
+  }
+}
