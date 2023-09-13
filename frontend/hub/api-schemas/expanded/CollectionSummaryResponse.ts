7a59e7d8b67750a8de521a0aa6ac9251212db782
MilanPospisil <arkanus@seznam.cz>
2023-09-13 14:22:24 +0200

    WIP - collection detail

--- frontend/hub/api-schemas/expanded/CollectionSummaryResponse.ts
+++ frontend/hub/api-schemas/expanded/CollectionSummaryResponse.ts
@@ -4,15 +4,14 @@ declare module '../generated/CollectionSummaryResponse' {
   interface CollectionSummaryResponse {
     // A JSON field with data about the contents.
     contents?: ContentSummaryType[];
-
     tags?: {
       name: string;
     }[];
-
     // A dict declaring Collections that this collection requires to be installed for it to be usable.
     dependencies?: {
       [collection: string]: string;
     };
+    require_ansible: string;
   }
 }
 
