5c445e4fa6a9484f83a5264460a5151dfc41b24f
James Talton <talton@gmail.com>
2023-09-13 17:35:01 -0400

    add platform
    Signed-off-by: James Talton <talton@gmail.com>

---
+++ platform/Platform.tsx
@@ -0,0 +1,6 @@
+import { lazy } from 'react';
+import { createRoot } from 'react-dom/client';
+const Main = lazy(() => import('./PlatformMain'));
+document.body.innerHTML = '<div id="app"></div>';
+const root = createRoot(document.getElementById('app')!);
+root.render(<Main />);
---
+++ platform/usePlatformNavigation.tsx
@@ -0,0 +1,44 @@
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { PageNavigationItem } from '../framework/PageNavigation/PageNavigation';
+import { useAwxNavigation } from '../frontend/awx/useAwxNavigation';
+import { useEdaNavigation } from '../frontend/eda/useEdaNavigation';
+import { useHubNavigation } from '../frontend/hub/useHubNavigation';
+import { PlatformDashboard } from './dashboard/PlatformDashboard';
+
+export function usePlatformNavigation() {
+  const { t } = useTranslation();
+  const awxNavigation = useAwxNavigation();
+  const hubNavigation = useHubNavigation();
+  const edaNavigation = useEdaNavigation();
+  const pageNavigationItems = useMemo<PageNavigationItem[]>(() => {
+    const navigationItems: PageNavigationItem[] = [
+      {
+        id: 'platform-dashboard',
+        label: t('Dashboard'),
+        path: 'dashboard',
+        element: <PlatformDashboard />,
+      },
+      {
+        id: 'platform-awx',
+        label: t('Automation Controller'),
+        path: 'awx',
+        children: awxNavigation,
+      },
+      {
+        id: 'platform-hub',
+        label: t('Automation Hub'),
+        path: 'hub',
+        children: hubNavigation,
+      },
+      {
+        id: 'platform-eda',
+        label: t('Event Driven Automation'),
+        path: 'eda',
+        children: edaNavigation,
+      },
+    ];
+    return navigationItems;
+  }, [awxNavigation, edaNavigation, hubNavigation, t]);
+  return pageNavigationItems;
+}
