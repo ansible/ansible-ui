94ab719611370ccd29b737b0e90c936083fbd04d
James Talton <jtalton@redhat.com>
2023-09-19 16:14:25 -0400

    Make page masthead more composable for downstream (#923)
    Signed-off-by: James Talton <talton@gmail.com>

---
+++ framework/PageMasthead/PageMasthead.tsx
@@ -0,0 +1,68 @@
+import {
+  Masthead,
+  MastheadBrand,
+  MastheadContent,
+  MastheadMain,
+  MastheadToggle,
+  PageToggleButton,
+  Stack,
+  Text,
+  Title,
+  Toolbar,
+  ToolbarContent,
+  ToolbarItem,
+  Truncate,
+} from '@patternfly/react-core';
+import { BarsIcon } from '@patternfly/react-icons';
+import { ReactNode } from 'react';
+import { usePageNavSideBar } from '../PageNavigation/PageNavSidebar';
+import { useBreakpoint } from '../components/useBreakPoint';
+
+export function PageMasthead(props: {
+  icon?: ReactNode;
+  brand?: string;
+  title: string;
+  children?: ReactNode;
+}) {
+  const isSmallOrLarger = useBreakpoint('sm');
+  return (
+    <Masthead display={{ default: 'inline' }}>
+      <PageMastheadToggle />
+      <MastheadMain>
+        <MastheadBrand>{props.icon}</MastheadBrand>
+      </MastheadMain>
+      <MastheadContent style={{ marginLeft: 0 }}>
+        <Toolbar id="toolbar" inset={{ default: 'insetNone' }}>
+          <ToolbarContent>
+            {isSmallOrLarger && (
+              <ToolbarItem>
+                <Stack style={{ color: 'white', cursor: 'default' }}>
+                  {props.brand && (
+                    <Text>
+                      <Truncate content={props.brand} style={{ minWidth: 0 }} />
+                    </Text>
+                  )}
+                  <Title headingLevel="h1" style={{ lineHeight: 1 }}>
+                    <Truncate content={props.title} style={{ minWidth: 0 }} />
+                  </Title>
+                </Stack>
+              </ToolbarItem>
+            )}
+            {props.children}
+          </ToolbarContent>
+        </Toolbar>
+      </MastheadContent>
+    </Masthead>
+  );
+}
+
+export function PageMastheadToggle() {
+  const navBar = usePageNavSideBar();
+  return (
+    <MastheadToggle onClick={() => navBar.setState({ isOpen: !navBar.isOpen })}>
+      <PageToggleButton variant="plain" aria-label="Global navigation">
+        <BarsIcon />
+      </PageToggleButton>
+    </MastheadToggle>
+  );
+}
