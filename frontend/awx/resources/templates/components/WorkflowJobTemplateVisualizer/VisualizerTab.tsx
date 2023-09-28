0280a4a7f624ccb95a2a44ef0986315ec0ec94a3
Alex Corey <acorey@redhat.com>
2023-09-28 13:50:18 -0400

    Removes unused workflow visualizer code (#965)

--- frontend/awx/resources/templates/components/WorkflowJobTemplateVisualizer/VisualizerTab.tsx
+++
@@ -1,6 +0,0 @@
-import { WorkflowJobTemplate } from '../../../../interfaces/WorkflowJobTemplate';
-import Visualizer from './Visualizer.jsx';
-
-export function VisualizerTab(props: { template: WorkflowJobTemplate }) {
-  return <Visualizer template={props.template} />;
-}
