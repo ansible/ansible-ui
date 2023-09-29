45cea77d6c1592af7aee77f79c821fde37e7d2b6
akus062381 <sarah.akus@gmail.com>
2023-09-29 13:16:41 -0400

    try to fix error

--- cypress/support/auth.ts
+++ cypress/support/auth.ts
@@ -42,7 +42,7 @@ Cypress.Commands.add('awxLogin', () => {
         Cypress.env('AWX_USERNAME') as string,
         Cypress.env('AWX_PASSWORD') as string
       );
-      window.localStorage.setItem('hide-welcome-message', 'true');
+      // window.localStorage.setItem('hide-welcome-message', 'true');
       cy.verifyPageTitle('Welcome to');
     },
     {
