587fab5da9cb8c20cffc481839a28b06c770b6d1
Marliana Lara <mlara@redhat.com>
2023-10-03 15:48:00 -0400

    Hookup launch to new navigation framework

--- frontend/awx/useAwxNavigation.tsx
+++ frontend/awx/useAwxNavigation.tsx
@@ -82,6 +82,7 @@ import { ScheduleDetails } from './views/schedules/SchedulePage/ScheduleDetails'
 import { SchedulePage } from './views/schedules/SchedulePage/SchedulePage';
 import { ScheduleRules } from './views/schedules/SchedulePage/ScheduleRules';
 import { Schedules } from './views/schedules/Schedules';
+import { TemplateLaunchWizard } from './resources/templates/TemplatePage/TemplateLaunchWizard';
 
 export function useAwxNavigation() {
   const { t } = useTranslation();
