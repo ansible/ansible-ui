3ddb2c5db27c311f330544db1bf5da0c2409e3f1
MilanPospisil <arkanus@seznam.cz>
2023-10-04 16:06:35 +0200

    Finish functionality, only loading missing

--- frontend/hub/collections/hooks/useCopyToRepository.tsx
+++ frontend/hub/collections/hooks/useCopyToRepository.tsx
@@ -2,7 +2,7 @@ import { CollectionVersionSearch } from '../Collection';
 import { usePageDialog } from './../../../../framework';
 import { Button, Modal, ModalVariant } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
-import { useState, useEffect, Dispatch, SetStateAction, useRef } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import {
   useRepositoryColumns,
   useRepositoryFilters,
@@ -14,51 +14,30 @@ import { pulpAPI, hubAPI } from '../../api/utils';
 import { useGetRequest } from './../../../common/crud/useGet';
 import { HubItemsResponse } from '../../useHubView';
 import { PulpItemsResponse } from '../../usePulpView';
-import { parsePulpIDFromURL } from '../../api/utils';
-import { useHubContext } from './../../useHubContext';
+import { parsePulpIDFromURL, parseTaskResponse } from '../../api/utils';
+import { useHubContext, HubContext } from './../../useHubContext';
 import { SigningServiceResponse } from '../../api-schemas/generated/SigningServiceResponse';
+import { usePostRequest } from '../../../common/crud/usePostRequest';
+import { TaskResponse } from '../../tasks/Task';
+import { LoadingPage } from '../../../../framework/components/LoadingPage';
 
 export function useCopyToRepository() {
   const [_, setDialog] = usePageDialog();
-  const { t } = useTranslation();
-  const  onClick  = useRef<boolean>(false);
 
-
-  if (onClick.current == true)
-  {
-    debugger;
-    onClick.current = false;
-  }
+  const context = useHubContext();
+  const onClose = useCallback(() => setDialog(undefined), [setDialog]);
 
   return (collection: CollectionVersionSearch) => {
     setDialog(
-      <Modal
-        title={t(`Select repositories`)}
-        aria-label={t(`Select repositories`)}
-        isOpen
-        onClose={() => {}}
-        variant={ModalVariant.large}
-        tabIndex={0}
-        actions={[
-          <Button key="select" variant="primary" id="select" onClick={() => {debugger; onClick.current = true;}}>
-            {t('Select')}
-          </Button>,
-          <Button key="cancel" variant="link" onClick={() => {}}></Button>,
-        ]}
-        hasNoBodyWrapper
-      >
-        <CopyToRepositoryTable
-          collection={collection}
-          onClick={onClick.current}
-        />
-      </Modal>
+      <CopyToRepositoryModal collection={collection} context={context} onClose={onClose} />
     );
   };
 }
 
-function CopyToRepositoryTable(props: {
+function CopyToRepositoryModal(props: {
   collection: CollectionVersionSearch;
-  onClick : boolean;
+  context: HubContext;
+  onClose: () => void;
 }) {
   const toolbarFilters = useRepositoryFilters();
   const tableColumns = useRepositoryColumns();
@@ -66,104 +45,86 @@ function CopyToRepositoryTable(props: {
   const { collection } = props;
   const request = useGetRequest<HubItemsResponse<CollectionVersionSearch>>();
   const pulpRequest = useGetRequest<PulpItemsResponse<SigningServiceResponse>>();
-  const context = useHubContext();
+  const postRequest = usePostRequest();
 
   const [selectedRepositories, setSelectedRepositories] = useState<Repository[]>([]);
 
   const [fixedRepositories, setFixedRepositories] = useState<Repository[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const copyToRepositories = () => {
-    try{
+    setIsLoading(true);
+    try {
       void (async () => {
-        const { collection_version, repository } = props.collection;
+        const { repository } = props.collection;
         if (!repository) {
           return;
         }
 
         const pulpId = parsePulpIDFromURL(repository?.pulp_href);
 
-        const signingServiceName = context.settings.GALAXY_COLLECTION_SIGNING_SERVICE;
+        const signingServiceName = props.context.settings.GALAXY_COLLECTION_SIGNING_SERVICE;
 
-        const signingService = await pulpRequest(pulpAPI`signing-services/?name=${signingServiceName}`);
+        const url = pulpAPI`/signing-services/?name=${signingServiceName}`;
+        const signingServiceList = await pulpRequest(url);
 
-        /* let signingService = null;
-        try {
-          const signingList = await SigningServiceAPI.list({
-            name: signingServiceName,
-          });
-          signingService = signingList.data.results[0].pulp_href;
-        } catch {
-          setLoading(false);
-          props.addAlert({
-            title: t`Failed to copy collection version.`,
-            variant: 'danger',
-            description: t`Signing service ${signingServiceName} not found`,
-          });
-          return;
+        const signingService = signingServiceList?.results?.[0].pulp_href;
+
+        const repoHrefs = selectedRepositories.map((repo) => repo.pulp_href);
+
+        const params: {
+          collection_versions: string[];
+          destination_repositories: string[];
+          signing_service?: string;
+        } = {
+          collection_versions: collection.collection_version?.pulp_href
+            ? [collection.collection_version?.pulp_href]
+            : [],
+          destination_repositories: repoHrefs,
+        };
+
+        if (signingService) {
+          params.signing_service = signingService;
         }
 
-        const repoHrefs = repositoryList
-          .filter((repo) => selectedRepos.includes(repo.name))
-          .map((repo) => repo.pulp_href);
+        const taskResponse = (await postRequest(
+          pulpAPI`/repositories/ansible/ansible/${pulpId || ''}/copy_collection_version/`,
+          params
+        )) as TaskResponse;
+        await parseTaskResponse(taskResponse);
 
-        Repositories.copyCollectionVersion(
-          pulpId,
-          [collection_version.pulp_href],
-          repoHrefs,
-          signingService,
-        )
-          .then(({ data }) => {
-            selectedRepos.map((repo) => {
-              props.addAlert(
-                taskAlert(
-                  data.task,
-                  t`Started adding ${collection_version.namespace}.${collection_version.name} v${collection_version.version} from "${repository.name}" to repository "${repo}".`,
-                ),
-              );
-            });
-          })
-          .catch((e) => {
-          
-          });*/
+        setIsLoading(false);
+        props.onClose();
       })();
-    }catch(error)
-    {
-      
+    } catch (error) {
+      // TODO
+      setIsLoading(false);
+    } finally {
     }
+  };
+
+  useEffect(() => {
+    async function getSelected() {
+      const repos = await request(
+        hubAPI`/v3/plugin/ansible/search/collection-versions?name=${
+          collection.collection_version?.name || ''
+        } &&version=${collection.collection_version?.version || ''}`
+      );
+
+      if (repos.data?.length > 0) {
+        const reposMapped = repos.data.map((item) => item.repository || ({} as Repository));
+        setSelectedRepositories(reposMapped);
+        setFixedRepositories(reposMapped);
+      }
     }
 
-
-    useEffect(() => {
-      debugger;
-      if (props.onClick == true)
-      {
-        copyToRepositories();
+    void (async () => {
+      try {
+        await getSelected();
+      } catch (error) {
+        // TODO - error handling
       }
-
-    }, [props.onClick]);
-
-    useEffect(() => {
-      async function getSelected() {
-        const repos = await request(
-          hubAPI`/v3/plugin/ansible/search/collection-versions?name=${
-            collection.collection_version?.name || ''
-          } &&version=${collection.collection_version?.version || ''}`
-        );
-
-        if (repos.data?.length > 0) {
-          const reposMapped = repos.data.map((item) => item.repository || ({} as Repository));
-          setSelectedRepositories(reposMapped);
-          setFixedRepositories(reposMapped);
-        }
-      }
-
-      void (async () => {
-        try {
-          await getSelected();
-        } catch (error) {
-          // TODO - error handling
-        }
-      })();
+    })();
   }, []);
 
   const view = usePulpView({
@@ -175,47 +136,84 @@ function CopyToRepositoryTable(props: {
   });
 
   return (
-    <PageTable<AnsibleAnsibleRepositoryResponse>
-      id="hub-copy-to-repository-table"
-      toolbarFilters={toolbarFilters}
-      tableColumns={tableColumns}
-      errorStateTitle={t('Error loading collections')}
-      emptyStateTitle={t('No collections yet')}
-      emptyStateDescription={t('To get started, upload a collection.')}
-      emptyStateButtonText={t('Upload collection')}
-      {...view}
-      defaultTableView="list"
-      defaultSubtitle={t('Repository')}
-      showSelect={true}
-      compact={true}
-      isSelectMultiple={true}
-      isSelected={(item) => (selectedRepositories.find((i) => i.name == item.name) ? true : false)}
-      isSelectionDisabled={(item) =>
-        fixedRepositories.find((i) => i.name == item.name) ? true : false
-      }
-      selectItem={(item) => {
-        const newItems = [...selectedRepositories, item];
-        setSelectedRepositories(newItems);
+    <Modal
+      title={t(`Select repositories`)}
+      aria-label={t(`Select repositories`)}
+      isOpen
+      onClose={() => {
+        props.onClose();
       }}
-      selectItems={(items) => {
-        let newItems = [...selectedRepositories];
-        for (const item of items) {
-          if (!selectedRepositories.find((item2) => item.name == item2.name)) {
-            newItems.push(item);
+      variant={ModalVariant.large}
+      tabIndex={0}
+      actions={[
+        <Button
+          key="select"
+          variant="primary"
+          id="select"
+          onClick={() => {
+            copyToRepositories();
+          }}
+          isDisabled={selectedRepositories.length <= fixedRepositories.length}
+        >
+          {t('Select')}
+          {isLoading && <LoadingPage />}
+        </Button>,
+        <Button
+          key="cancel"
+          variant="link"
+          onClick={() => {
+            props.onClose();
+          }}
+        ></Button>,
+      ]}
+      hasNoBodyWrapper
+    >
+      <PageTable<AnsibleAnsibleRepositoryResponse>
+        id="hub-copy-to-repository-table"
+        toolbarFilters={toolbarFilters}
+        tableColumns={tableColumns}
+        errorStateTitle={t('Error loading collections')}
+        emptyStateTitle={t('No collections yet')}
+        emptyStateDescription={t('To get started, upload a collection.')}
+        emptyStateButtonText={t('Upload collection')}
+        {...view}
+        defaultTableView="list"
+        defaultSubtitle={t('Repository')}
+        showSelect={true}
+        compact={true}
+        isSelectMultiple={true}
+        isSelected={(item) =>
+          selectedRepositories.find((i) => i.name == item.name) ? true : false
+        }
+        isSelectionDisabled={(item) =>
+          fixedRepositories.find((i) => i.name == item.name) ? true : false
+        }
+        selectItem={(item) => {
+          const newItems = [...selectedRepositories, item];
+          setSelectedRepositories(newItems);
+        }}
+        selectItems={(items) => {
+          let newItems = [...selectedRepositories];
+          for (const item of items) {
+            if (!selectedRepositories.find((item2) => item.name == item2.name)) {
+              newItems.push(item);
+            }
           }
-        }
 
-        setSelectedRepositories(newItems);
-      }}
-      unselectItem={(item) => {
-        if (!fixedRepositories.find((item2) => item.name == item2.name)) {
-          setSelectedRepositories(selectedRepositories.filter((item2) => item2.name != item.name));
-        }
-      }}
-      unselectAll={() => {
-        setSelectedRepositories(fixedRepositories);
-      }}
-    />
+          setSelectedRepositories(newItems);
+        }}
+        unselectItem={(item) => {
+          if (!fixedRepositories.find((item2) => item.name == item2.name)) {
+            setSelectedRepositories(
+              selectedRepositories.filter((item2) => item2.name != item.name)
+            );
+          }
+        }}
+        unselectAll={() => {
+          setSelectedRepositories(fixedRepositories);
+        }}
+      />
+    </Modal>
   );
 }
 
