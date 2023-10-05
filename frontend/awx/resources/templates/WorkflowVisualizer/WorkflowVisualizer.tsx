7da3ca522598278928e37f9c6c88cc25e53df3d8
Alex Corey <alex.swansboro@gmail.com>
2023-10-05 16:09:49 -0400

    Adds unsaved changes modal

--- frontend/awx/resources/templates/WorkflowVisualizer/WorkflowVisualizer.tsx
+++ frontend/awx/resources/templates/WorkflowVisualizer/WorkflowVisualizer.tsx
@@ -1,10 +1,25 @@
 import { TopologyView } from '@patternfly/react-topology';
 import { useWorkflowVisualizerToolbarActions } from '../hooks/useWorkflowVisualizerToolbarActions';
 import { useState } from 'react';
+import { UnsavedChangesAlertModal } from './UnsavedChangesAlertModal';
 
 export function WorkflowJobTemplateVisualizer() {
-  const [_isSidePanelFormOpen, setIsSidePanelFormOpen] = useState(false);
-  const toolbarActions = useWorkflowVisualizerToolbarActions(setIsSidePanelFormOpen);
+  const toggleSidePanel = useState(false);
+  const [visualizerHasUnsavedChanges, _setVisualizerHasUnsavedChanges] = useState(false);
+  const [isUnsaveChangesModalOpen, setToggleUnsaveChangesModal] = useState(false);
+  const toolbarActions = useWorkflowVisualizerToolbarActions(
+    toggleSidePanel,
+    setToggleUnsaveChangesModal,
+    visualizerHasUnsavedChanges
+  );
 
-  return <TopologyView contextToolbar={toolbarActions}></TopologyView>;
+  return (
+    <TopologyView contextToolbar={toolbarActions}>
+      {isUnsaveChangesModalOpen && (
+        <UnsavedChangesAlertModal
+          modalState={[isUnsaveChangesModalOpen, setToggleUnsaveChangesModal]}
+        />
+      )}
+    </TopologyView>
+  );
 }
