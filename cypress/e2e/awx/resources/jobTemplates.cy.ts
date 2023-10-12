e4283aa87787e9e95616e941a274bbb8fc76be5f
James Talton <talton@gmail.com>
2023-10-12 09:41:11 -0400

    test fixes
    Signed-off-by: James Talton <talton@gmail.com>

--- cypress/e2e/awx/resources/jobTemplates.cy.ts
+++ cypress/e2e/awx/resources/jobTemplates.cy.ts
@@ -180,11 +180,11 @@ describe('Job templates form Create, Edit, Delete', () => {
         cy.clickButton(/^Launch template$/);
         cy.selectDropdownOptionByResourceName('inventory', inventory.name);
         cy.clickButton(/^Next/);
-        cy.selectItemFromLookupModal('credentials', machineCredential);
+        cy.selectItemFromLookupModal('credential-select', machineCredential);
         cy.clickButton(/^Next/);
-        cy.selectItemFromLookupModal('execution-environment', executionEnvironment);
+        cy.selectItemFromLookupModal('execution-environment-select', executionEnvironment);
         cy.clickButton(/^Next/);
-        cy.selectItemFromLookupModal('instance_groups', instanceGroup);
+        cy.selectItemFromLookupModal('instance-group-select', instanceGroup);
         cy.clickButton(/^Next/);
         cy.intercept('POST', `api/v2/job_templates/${id}/launch/`).as('postLaunch');
         cy.clickButton(/^Finish/);
