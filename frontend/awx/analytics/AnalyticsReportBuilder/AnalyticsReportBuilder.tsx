6641a2965bea071c92264cd29859b8111f006e21
MilanPospisil <arkanus@seznam.cz>
2023-10-13 11:00:01 +0200

    Move some functions into the utils

--- frontend/awx/analytics/AnalyticsReportBuilder/AnalyticsReportBuilder.tsx
+++ frontend/awx/analytics/AnalyticsReportBuilder/AnalyticsReportBuilder.tsx
@@ -71,6 +71,8 @@ import { useSearchParams } from '../../../../framework/components/useSearchParam
 import { ChartFunctions } from '@ansible/react-json-chart-builder';
 import { ChartSchemaElement } from '@ansible/react-json-chart-builder';
 
+import { getDateFormatByGranularity, formattedValue } from './AnalyticsReportBuilderUtils';
+
 type KeyValue = { key: string; value: string };
 
 // definition of main data returned from main request
@@ -650,41 +652,3 @@ function buildTableColumns(params: AnalyticsReportColumnBuilderProps) {
   return columns;
 }
 
-// another function for chart
-const formattedValue = (key: string, value: number) => {
-  let val;
-  switch (key) {
-    case 'elapsed':
-      val = value.toFixed(2) + ' seconds';
-      break;
-    case 'template_automation_percentage':
-      val = value.toFixed(2) + '%';
-      break;
-    case 'successful_hosts_savings':
-    case 'failed_hosts_costs':
-    case 'monetary_gain':
-      val = currencyFormatter(value);
-      break;
-    default:
-      val = value.toFixed(2);
-  }
-  return val;
-};
-
-// another function for chart
-const currencyFormatter = (n: number): string => {
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  });
-
-  return formatter.format(n); /* $2,500.00 */
-};
-
-// another function for chart
-const getDateFormatByGranularity = (granularity: string): string => {
-  if (granularity === 'yearly') return 'formatAsYear';
-  if (granularity === 'monthly') return 'formatAsMonth';
-  if (granularity === 'daily') return 'formatDateAsDayMonth';
-  return '';
-};
