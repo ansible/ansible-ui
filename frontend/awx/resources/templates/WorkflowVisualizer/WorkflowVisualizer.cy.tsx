9e1ac62b8c5b8079f12a81f7c71136c8489a4bfe
Alex Corey <acorey@redhat.com>
2023-10-18 15:41:02 -0400

    Adds Save button to workflow visualizer toolbar (#1065)

--- frontend/awx/resources/templates/WorkflowVisualizer/WorkflowVisualizer.cy.tsx
+++ frontend/awx/resources/templates/WorkflowVisualizer/WorkflowVisualizer.cy.tsx
@@ -3,6 +3,7 @@ import { WorkflowVisualizer } from './WorkflowVisualizer';
 describe('WorkflowVisualizer', () => {
   it('Should show the WorkflowVisualizer toolbar with Add and Cancel buttons', () => {
     cy.mount(<WorkflowVisualizer />);
+    cy.contains('button:not(:disabled):not(:hidden)', 'Save').should('be.visible');
     cy.contains('button:not(:disabled):not(:hidden)', 'Add node').should('be.visible');
     cy.get('button[data-cy="workflow-visualizer-toolbar-close"]').should('be.visible');
   });
