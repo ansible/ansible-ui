0d8c909bb2aae4a5ee87ece77d7ade1fc541396c
akus062381 <sarah.akus@gmail.com>
2023-10-18 10:56:57 -0400

    fix failing tests

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -7,7 +7,13 @@ import { createGlobalProject } from './global-project';
 before(() => {
   const devBaseUrl = Cypress.config().baseUrl?.split(':').slice(-1).toString();
   cy.log('PORT', devBaseUrl);
-  if (devBaseUrl && (devBaseUrl === '4201' || devBaseUrl === '4202' || devBaseUrl === '4203')) {
+  if (
+    devBaseUrl &&
+    (devBaseUrl === '4201' ||
+      devBaseUrl === '4201' ||
+      devBaseUrl === '4202' ||
+      devBaseUrl === '4203')
+  ) {
     return;
   } else {
     createGlobalProject();
