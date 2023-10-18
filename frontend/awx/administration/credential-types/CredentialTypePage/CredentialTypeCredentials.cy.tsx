507bc732bc6ceb019c5bada687c8c6b2af82daec
Keith Grant <kegrant@redhat.com>
2023-10-18 16:34:16 -0400

    simplify routes in test

--- frontend/awx/administration/credential-types/CredentialTypePage/CredentialTypeCredentials.cy.tsx
+++ frontend/awx/administration/credential-types/CredentialTypePage/CredentialTypeCredentials.cy.tsx
@@ -12,16 +12,13 @@ describe('CredentialTypeCredentials', () => {
     cy.intercept(
       {
         method: 'GET',
-        url: '/api/v2/credential_types/*/credentials/*',
+        url: '/api/v2/credential_types/1/credentials/*',
       },
       {
         fixture: 'credentials.json',
       }
     ).as('credentials');
-    cy.mount(<CredentialTypeCredentials />, {
-      path: '/credential_types/:id/credentials',
-      initialEntries: ['/credential_types/1/credentials'],
-    });
+    cy.mount(<CredentialTypeCredentials />);
     cy.wait('@credentials').then((interception) => {
       expect(interception.response?.statusCode).to.equal(200);
     });
