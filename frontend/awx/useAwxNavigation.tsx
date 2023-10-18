fa87f85e79e76ea56096e0b3139fb8ff61473360
Alex Corey <alex.swansboro@gmail.com>
2023-10-18 11:57:40 -0400

    POC- moves invntory routing out of useGetAwxRoutes file because inventory routing gets complex

--- frontend/awx/useAwxNavigation.tsx
+++ frontend/awx/useAwxNavigation.tsx
@@ -53,10 +53,6 @@ import { CredentialPage } from './resources/credentials/CredentialPage/Credentia
 import { Credentials } from './resources/credentials/Credentials';
 import { HostPage } from './resources/hosts/HostPage/HostPage';
 import { Hosts } from './resources/hosts/Hosts';
-import { Inventories } from './resources/inventories/Inventories';
-import { CreateInventory, EditInventory } from './resources/inventories/InventoryForm';
-import { InventoryDetails } from './resources/inventories/InventoryPage/InventoryDetails';
-import { InventoryPage } from './resources/inventories/InventoryPage/InventoryPage';
 import { ProjectDetails } from './resources/projects/ProjectPage/ProjectDetails';
 import { CreateProject, EditProject } from './resources/projects/ProjectPage/ProjectForm';
 import { ProjectPage } from './resources/projects/ProjectPage/ProjectPage';
@@ -83,9 +79,11 @@ import { SchedulePage } from './views/schedules/SchedulePage/SchedulePage';
 import { ScheduleRules } from './views/schedules/SchedulePage/ScheduleRules';
 import { Schedules } from './views/schedules/Schedules';
 import { TemplateLaunchWizard } from './resources/templates/TemplatePage/TemplateLaunchWizard';
+import { useGetAwxInventoryRoutes } from './useGetAwxInventoryRoutes';
 
 export function useAwxNavigation() {
   const { t } = useTranslation();
+  const awxInventoryRoutes: PageNavigationItem = useGetAwxInventoryRoutes();
   const pageNavigationItems = useMemo<PageNavigationItem[]>(() => {
     const navigationItems: PageNavigationItem[] = [
       {
@@ -546,114 +544,7 @@ export function useAwxNavigation() {
                   },
                 ],
               },
-              {
-                id: AwxRoute.Inventories,
-                label: t('Inventories'),
-                path: 'inventories',
-                children: [
-                  {
-                    id: AwxRoute.CreateInventory,
-                    path: 'create',
-                    element: <CreateInventory />,
-                  },
-                  {
-                    id: AwxRoute.InventorySourceScheduleCreate,
-                    path: ':id/sources/:source_id/schedules/create',
-                    element: <CreateSchedule />,
-                  },
-                  {
-                    id: AwxRoute.InventorySourceScheduleEdit,
-                    path: ':id/sources/:source_id/schedules/edit',
-                    element: <PageNotImplemented />,
-                  },
-                  {
-                    id: AwxRoute.InventorySourceSchedulePage,
-                    path: ':id/inventory_source/:source_id/schedules/:schedule_id/*',
-                    element: (
-                      <SchedulePage
-                        backTab={{
-                          label: t('Back to Schedules'),
-                          page: AwxRoute.InventorySourceSchedules,
-                          persistentFilterKey: 'inventory-schedules',
-                        }}
-                        tabs={[
-                          {
-                            label: t('Details'),
-                            page: AwxRoute.InventorySourceScheduleDetails,
-                          },
-                          {
-                            label: t('Rules'),
-                            page: AwxRoute.InventorySourceScheduleRrules,
-                          },
-                        ]}
-                      />
-                    ),
-                    children: [
-                      {
-                        id: AwxRoute.InventorySourceScheduleDetails,
-                        path: 'details',
-                        element: <ScheduleDetails />,
-                      },
-                      {
-                        id: AwxRoute.InventorySourceScheduleRrules,
-                        path: 'rrules',
-                        element: <ScheduleRules />,
-                      },
-                    ],
-                  },
-                  {
-                    id: AwxRoute.EditInventory,
-                    path: ':id/edit',
-                    element: <EditInventory />,
-                  },
-                  {
-                    id: AwxRoute.InventoryPage,
-                    path: ':inventory_type/:id/',
-                    element: <InventoryPage />,
-                    children: [
-                      {
-                        id: AwxRoute.InventoryDetails,
-                        path: 'details',
-                        element: <InventoryDetails />,
-                      },
-                      {
-                        id: AwxRoute.InventoryAccess,
-                        path: 'access',
-                        element: <PageNotImplemented />,
-                      },
-                      {
-                        id: AwxRoute.InventoryGroups,
-                        path: 'groups',
-                        element: <PageNotImplemented />,
-                      },
-                      {
-                        id: AwxRoute.InventoryHosts,
-                        path: 'hosts',
-                        element: <PageNotImplemented />,
-                      },
-                      {
-                        id: AwxRoute.InventorySources,
-                        path: 'sources',
-                        element: <PageNotImplemented />,
-                      },
-                      {
-                        id: AwxRoute.InventoryJobs,
-                        path: 'jobs',
-                        element: <PageNotImplemented />,
-                      },
-                      {
-                        id: AwxRoute.InventoryJobTemplates,
-                        path: 'templates',
-                        element: <PageNotImplemented />,
-                      },
-                    ],
-                  },
-                  {
-                    path: '',
-                    element: <Inventories />,
-                  },
-                ],
-              },
+              awxInventoryRoutes,
               {
                 id: AwxRoute.Hosts,
                 label: t('Hosts'),
@@ -1133,7 +1024,7 @@ export function useAwxNavigation() {
       },
     ];
     return navigationItems;
-  }, [t]);
+  }, [t, awxInventoryRoutes]);
 
   return pageNavigationItems;
 }
