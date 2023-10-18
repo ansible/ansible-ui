abeddc964d23d7a18f0b2ae9a9a81aaafe16258d
Marliana Lara <mlara@redhat.com>
2023-10-18 11:50:18 -0400

    Add credential type add form, edit form, and unit tests

--- frontend/awx/interfaces/CredentialType.ts
+++ frontend/awx/interfaces/CredentialType.ts
@@ -1,8 +1,12 @@
 import { CredentialType as SwaggerCredentialType } from './generated-from-swagger/api';
 
-export interface CredentialType extends Omit<SwaggerCredentialType, 'id' | 'name'> {
+export interface CredentialType extends Omit<SwaggerCredentialType, 'id' | 'name' | 'managed'> {
   id: number;
   name: string;
+  description: string;
+  namespace: string;
+  managed: boolean;
+  injectors: { [key: string]: string };
   inputs: {
     fields: {
       id: string;
