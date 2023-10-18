1dd69a7a4f787c865e3ce198e7ff1c37a89fb5f3
akus062381 <sarah.akus@gmail.com>
2023-10-18 10:42:51 -0400

    fix failing component tests

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -5,7 +5,13 @@ import './commands';
 import { createGlobalProject } from './global-project';
 
 before(() => {
-  createGlobalProject();
+  const devBaseUrl = Cypress.config().baseUrl.split(':').slice(-1).toString();
+  cy.log('PORT', devBaseUrl);
+  if (devBaseUrl === '4201' || devBaseUrl === '4202' || devBaseUrl === '4203') {
+    return;
+  } else {
+    createGlobalProject();
+  }
 });
 
 // afterEach(() => {
