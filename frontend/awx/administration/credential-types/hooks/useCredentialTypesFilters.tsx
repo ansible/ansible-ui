201627ef0aa4d2b29cabc4cb340314fc191d337e
Vidya Nambiar <vnambiar@redhat.com>
2023-10-18 13:17:27 -0400

    Credential types list

---
+++ frontend/awx/administration/credential-types/hooks/useCredentialTypesFilters.tsx
@@ -0,0 +1,25 @@
+import { useMemo } from 'react';
+import {
+  useCreatedByToolbarFilter,
+  useDescriptionToolbarFilter,
+  useModifiedByToolbarFilter,
+  useNameToolbarFilter,
+} from '../../../common/awx-toolbar-filters';
+import { IToolbarFilter } from '../../../../../framework';
+
+export function useCredentialTypesFilters() {
+  const nameToolbarFilter = useNameToolbarFilter();
+  const descriptionToolbarFilter = useDescriptionToolbarFilter();
+  const createdByToolbarFilter = useCreatedByToolbarFilter();
+  const modifiedByToolbarFilter = useModifiedByToolbarFilter();
+  const toolbarFilters = useMemo<IToolbarFilter[]>(
+    () => [
+      nameToolbarFilter,
+      descriptionToolbarFilter,
+      createdByToolbarFilter,
+      modifiedByToolbarFilter,
+    ],
+    [nameToolbarFilter, descriptionToolbarFilter, createdByToolbarFilter, modifiedByToolbarFilter]
+  );
+  return toolbarFilters;
+}
--- frontend/awx/interfaces/CredentialType.ts
+++ frontend/awx/interfaces/CredentialType.ts
@@ -1,12 +1,22 @@
 import { CredentialType as SwaggerCredentialType } from './generated-from-swagger/api';
+import { SummaryFieldsByUser } from './summary-fields/summary-fields';
 
-export interface CredentialType extends Omit<SwaggerCredentialType, 'id' | 'name' | 'managed'> {
+export interface CredentialType
+  extends Omit<SwaggerCredentialType, 'id' | 'name' | 'managed' | 'summary_fields'> {
   id: number;
   name: string;
   description: string;
   namespace: string;
   managed: boolean;
   injectors: { [key: string]: string };
+  summary_fields: {
+    created_by?: SummaryFieldsByUser;
+    modified_by?: SummaryFieldsByUser;
+    user_capabilities: {
+      edit: boolean;
+      delete: boolean;
+    };
+  };
   inputs: {
     fields: {
       id: string;
