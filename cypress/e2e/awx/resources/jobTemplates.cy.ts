6a5b27c1eb6accb97a0989be18e14879e8f9a40b
Vishali Sanghishetty <95239214+vishsanghishetty@users.noreply.github.com>
2023-10-19 17:39:48 -0400

    adds project and org

--- cypress/e2e/awx/resources/jobTemplates.cy.ts
+++ cypress/e2e/awx/resources/jobTemplates.cy.ts
@@ -15,11 +15,16 @@ describe('Job templates form Create, Edit, Delete', function () {
   before(function () {
     cy.awxLogin();
 
-    cy.createAwxInventory({ organization: (this.globalProjectOrg as Organization).id }).then(
-      (inv) => {
-        inventory = inv;
-      }
-    );
+    cy.createAwxOrganization().then((o) => {
+      organization = o;
+
+      cy.createAwxProject({ organization: organization.id }).then((p) => {
+        project = p;
+      });
+
+      cy.createAwxInventory({ organization: organization.id }).then((i) => {
+        inventory = i;
+      });
 
     cy.createAWXCredential({
       kind: 'machine',
