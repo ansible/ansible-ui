36f1532d58f3772f0acbc1b228287c277e06310d
Alex Corey <acorey@redhat.com>
2023-10-24 16:09:20 -0400

    Adds expand and compress button to Workflow Visualizer toolbar (#1066)

--- frontend/awx/resources/templates/WorkflowVisualizer/hooks/useWorkflowVisualizerToolbarActions.tsx
+++ frontend/awx/resources/templates/WorkflowVisualizer/hooks/useWorkflowVisualizerToolbarActions.tsx
@@ -1,12 +1,20 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Button, ToolbarGroup, ToolbarItem } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
-import { CheckCircleIcon, CloseIcon, PlusCircleIcon } from '@patternfly/react-icons';
+import {
+  CheckCircleIcon,
+  CloseIcon,
+  CompressAltIcon,
+  ExpandAltIcon,
+  PlusCircleIcon,
+} from '@patternfly/react-icons';
+
 import { usePageNavigate } from '../../../../../../framework';
 import { AwxRoute } from '../../../../AwxRoutes';
 
 export function useWorkflowVisualizerToolbarActions() {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const { t } = useTranslation();
   const pageNavigate = usePageNavigate();
   const params = useParams<{ id: string }>();
@@ -34,15 +42,37 @@ export function useWorkflowVisualizerToolbarActions() {
           </Button>
         </ToolbarItem>
       </ToolbarGroup>
-      <ToolbarItem align={{ default: 'alignRight' }}>
-        <Button
-          data-cy="workflow-visualizer-toolbar-close"
-          variant="plain"
-          icon={<CloseIcon />}
-          aria-label={t('Close')}
-          onClick={handleCancel}
-        />
-      </ToolbarItem>
+      <ToolbarGroup align={{ default: 'alignRight' }}>
+        <ToolbarItem>
+          <Button
+            data-cy="workflow-visualizer-toolbar-expand-compress"
+            variant="plain"
+            aria-label={isExpanded ? t('Compress') : t('Expand')}
+            onClick={() => setIsExpanded(!isExpanded)}
+          >
+            {isExpanded ? (
+              <CompressAltIcon
+                data-cy="workflow-visualizer-toolbar-compress"
+                aria-label={t('Compress')}
+              />
+            ) : (
+              <ExpandAltIcon
+                data-cy="workflow-visualizer-toolbar-expand"
+                aria-label={t('Expand')}
+              />
+            )}
+          </Button>
+        </ToolbarItem>
+        <ToolbarItem>
+          <Button
+            data-cy="workflow-visualizer-toolbar-close"
+            variant="plain"
+            icon={<CloseIcon />}
+            aria-label={t('Close')}
+            onClick={handleCancel}
+          />
+        </ToolbarItem>
+      </ToolbarGroup>
     </>
   );
 }
