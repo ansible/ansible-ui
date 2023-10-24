6d7670cdaf20dec2536baef60fa8bb5a62e2ada0
Alex Corey <acorey@redhat.com>
2023-10-24 13:44:29 -0400

    Adds inventory source page and inventory source details with tests (#1049)

---
+++ frontend/awx/resources/inventories/hooks/useInventorySourceActions.tsx
@@ -0,0 +1,110 @@
+import { ButtonVariant } from '@patternfly/react-core';
+import { EditIcon, RocketIcon, TrashIcon } from '@patternfly/react-icons';
+import { useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  IPageAction,
+  PageActionSelection,
+  PageActionType,
+  usePageAlertToaster,
+  usePageNavigate,
+} from '../../../../../framework';
+import { AwxRoute } from '../../../AwxRoutes';
+import { InventorySource } from '../../../interfaces/InventorySource';
+import { useDeleteInventorySources } from './useDeleteInventorySources';
+import { useActiveUser } from '../../../../common/useActiveUser';
+import { usePostRequest } from '../../../../common/crud/usePostRequest';
+
+type InventorySourceActionOptions = {
+  onInventorySourcesDeleted: (inventorySources: InventorySource[]) => void;
+};
+
+export function useInventorySourceActions({
+  onInventorySourcesDeleted,
+}: InventorySourceActionOptions) {
+  const { t } = useTranslation();
+  const pageNavigate = usePageNavigate();
+  const deleteInventorySources = useDeleteInventorySources(onInventorySourcesDeleted);
+
+  const activeUser = useActiveUser();
+  const alertToaster = usePageAlertToaster();
+
+  const postRequest = usePostRequest();
+  const handleUpdate = useCallback(
+    async (invSrc: InventorySource) => {
+      try {
+        await postRequest(`/api/v2/inventory_sources/${invSrc.id}/update/`, {});
+      } catch (error) {
+        alertToaster.addAlert({
+          variant: 'danger',
+          title: t('Failed to update inventory source'),
+          children: error instanceof Error && error.message,
+          timeout: 5000,
+        });
+      }
+    },
+    [alertToaster, postRequest, t]
+  );
+
+  return useMemo<IPageAction<InventorySource>[]>(() => {
+    const cannotDeleteInventorySource = (inventorySource: InventorySource): string =>
+      inventorySource?.summary_fields?.user_capabilities?.delete && !activeUser?.is_system_auditor
+        ? ''
+        : t(`The inventory source cannot be deleted due to insufficient permission`);
+    const cannotEditInventorySource = (inventorySource: InventorySource): string =>
+      inventorySource?.summary_fields?.user_capabilities?.edit && !activeUser?.is_system_auditor
+        ? ''
+        : t(`The inventory source cannot be edited due to insufficient permission`);
+    const cannotLaunchInventorySourceUpdate = (inventorySource: InventorySource): string => {
+      return inventorySource.summary_fields.user_capabilities.start &&
+        !activeUser?.is_system_auditor
+        ? ''
+        : t(`The inventory source cannot be updated due to insufficient permission`);
+    };
+    const itemActions: IPageAction<InventorySource>[] = [
+      {
+        type: PageActionType.Button,
+        selection: PageActionSelection.Single,
+        icon: EditIcon,
+        variant: ButtonVariant.primary,
+        isPinned: true,
+        label: t('Edit inventory source'),
+        isDisabled: (inventorySource: InventorySource) =>
+          cannotEditInventorySource(inventorySource),
+        onClick: (inventorySource) =>
+          pageNavigate(AwxRoute.InventorySourceEdit, {
+            params: {
+              id: inventorySource.inventory,
+              source_id: inventorySource.id,
+            },
+          }),
+      },
+
+      { type: PageActionType.Seperator },
+      {
+        type: PageActionType.Button,
+        selection: PageActionSelection.Single,
+        icon: RocketIcon,
+        label: t('Launch inventory update'),
+        isPinned: true,
+        isHidden: (inventorySource: InventorySource) =>
+          !inventorySource?.summary_fields.user_capabilities.start,
+        // isDisabled: 'yes',
+        isDisabled: (inventorySource: InventorySource) =>
+          cannotLaunchInventorySourceUpdate(inventorySource),
+        onClick: (inventorySource) => handleUpdate(inventorySource),
+      },
+      {
+        type: PageActionType.Button,
+        selection: PageActionSelection.Single,
+        icon: TrashIcon,
+        label: t('Delete inventory source'),
+        isDisabled: (inventorySource: InventorySource) =>
+          cannotDeleteInventorySource(inventorySource),
+        onClick: (inventorySource) => deleteInventorySources([inventorySource]),
+        isDanger: true,
+      },
+    ];
+    return itemActions;
+  }, [deleteInventorySources, pageNavigate, handleUpdate, activeUser, t]);
+}
---
+++ frontend/awx/resources/inventories/inventorySources/InventorySourceDetails.cy.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable i18next/no-literal-string */
+
+import { InventorySourceDetails } from './InventorySourceDetails';
+
+describe('InventorySourceDetails', () => {
+  before(() => {
+    cy.intercept(
+      { method: 'GET', url: '/api/v2/inventory_sources/*' },
+      { fixture: 'inventory_source.json' }
+    );
+    cy.intercept(
+      { method: 'OPTIONS', url: '/api/v2/inventory_sources' },
+      { fixture: 'inventory_source_options.json' }
+    );
+  });
+  it('Component renders and displays project', () => {
+    cy.mount(<InventorySourceDetails />, {
+      path: '/inventories/:id/sources/:source_id/details',
+      initialEntries: ['/inventories/1/sources/1/details'],
+    });
+    cy.get('#name').should('have.text', 'Demo Inventory Source');
+    cy.get('#organization').should('have.text', 'Default');
+    cy.get('#last-job-status').invoke('text').as('Failed');
+    cy.get('#source').should('have.text', 'Sourced from a Project');
+    cy.get('#project').should('have.text', 'Demo Project');
+    cy.get('#inventory-file').should('have.text', '/ (project root)');
+    cy.get('#verbosity').should('have.text', '1 (Verbose)');
+
+    cy.get('#cache-timeout').should('have.text', '0 seconds');
+    cy.get('#source-variables').should('have.text', '---');
+    cy.get('#created > .pf-v5-c-description-list__text > .date-time > .pf-v5-c-button').should(
+      'have.text',
+      'dev'
+    );
+    cy.get('#modified > .pf-v5-c-description-list__text > .date-time > .pf-v5-c-button').should(
+      'have.text',
+      'dev'
+    );
+  });
+});
