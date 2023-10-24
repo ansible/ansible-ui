f71306c2117f47adfa384940b0577795b1ff548b
Alex Corey <acorey@redhat.com>
2023-10-24 16:09:20 -0400

    Adds expand and compress button to Workflow Visualizer toolbar (#1066)

--- frontend/awx/resources/templates/WorkflowVisualizer/WorkflowVisualizer.cy.tsx
+++ frontend/awx/resources/templates/WorkflowVisualizer/WorkflowVisualizer.cy.tsx
@@ -13,6 +13,17 @@ describe('WorkflowVisualizer', () => {
     cy.contains('button:not(:disabled):not(:hidden)', 'Save').should('be.visible');
     cy.contains('button:not(:disabled):not(:hidden)', 'Add node').should('be.visible');
     cy.get('button[data-cy="workflow-visualizer-toolbar-close"]').should('be.visible');
+    cy.get('button[data-cy="workflow-visualizer-toolbar-expand-compress"]').should('be.visible');
+    cy.get('svg[data-cy="workflow-visualizer-toolbar-compress"]').should('not.exist');
+  });
+  it('Should toggle the expand collapse button in the toolbar', () => {
+    cy.mount(<WorkflowVisualizer />);
+    cy.get('button[data-cy="workflow-visualizer-toolbar-expand-compress"]').click();
+    cy.get('svg[data-cy="workflow-visualizer-toolbar-expand"]').should('not.exist');
+    cy.get('[data-cy="workflow-visualizer-toolbar-compress"]').should('be.visible');
+    cy.get('button[data-cy="workflow-visualizer-toolbar-expand-compress"]').click();
+    cy.get('[data-cy="workflow-visualizer-toolbar-expand"]').should('be.visible');
+    cy.get('[data-cy="workflow-visualizer-toolbar-compress"]').should('not.exist');
   });
 
   it('Should show the visualizer screen', () => {
