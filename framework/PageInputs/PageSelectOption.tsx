0b7bf39315a598b2265adaf14a0f10866b9a4ebb
James Talton <jtalton@redhat.com>
2023-10-25 10:16:46 -0400

    select component icons (#1115)
    Signed-off-by: James Talton <talton@gmail.com>

--- framework/PageInputs/PageSelectOption.tsx
+++ framework/PageInputs/PageSelectOption.tsx
@@ -1,6 +1,9 @@
+import { ReactNode } from 'react';
+
 export interface PageSelectOption<ValueT> {
   key?: string | number; // If not provided, will items will be tracked by label
   value: ValueT;
+  icon?: ReactNode;
   label: string;
   description?: string;
 }
