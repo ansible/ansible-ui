4816e13a975142b82ee1233dd8384e5a3b14ca38
Vidya Nambiar <vnambiar@redhat.com>
2023-10-30 13:53:18 -0400

    Base component for UserDetails used in awx & eda
    initial
    
    Base component for UserDetails used in awx & eda

--- frontend/awx/access/users/UserPage/UserDetails.tsx
+++ frontend/awx/access/users/UserPage/UserDetails.tsx
@@ -1,14 +1,10 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
-import { PageDetail, PageDetails, DateTimeCell } from '../../../../../framework';
 import { User } from '../../../interfaces/User';
 import { useGetItem } from '../../../../common/crud/useGet';
-import { AuthenticationType } from '../components/AuthenticationType';
-import { UserType } from '../components/UserType';
+import { UserDetailsBase, UserDetailsType } from '../../../../common/access/UserDetailsBase';
 
 export function UserDetails() {
-  const { t } = useTranslation();
   const params = useParams<{ id: string }>();
   const { data: user } = useGetItem<User>('/api/v2/users', params.id);
 
@@ -18,27 +14,10 @@ export function UserDetails() {
 
   return (
     <>
-      <PageDetails>
-        <PageDetail label={t('Username')}>{user.username}</PageDetail>
-        <PageDetail label={t('First name')}>{user.first_name}</PageDetail>
-        <PageDetail label={t('Last name')}>{user.last_name}</PageDetail>
-        <PageDetail label={t('Email')}>{user.email}</PageDetail>
-        <PageDetail label={t('User type')}>
-          <UserType user={user} />
-        </PageDetail>
-        <PageDetail label={t('Authentication type')}>
-          <AuthenticationType user={user} />
-        </PageDetail>
-        <PageDetail label={t('Created')}>
-          <DateTimeCell format="since" value={user.created} />
-        </PageDetail>
-        <PageDetail label={t('Modified')}>
-          <DateTimeCell format="since" value={user.modified} />
-        </PageDetail>
-        <PageDetail label={t('Last login')}>
-          <DateTimeCell format="since" value={user.last_login} />
-        </PageDetail>
-      </PageDetails>
+      <UserDetailsBase
+        user={user as UserDetailsType}
+        options={{ showAuthType: true, showUserType: true }}
+      />
     </>
   );
 }
