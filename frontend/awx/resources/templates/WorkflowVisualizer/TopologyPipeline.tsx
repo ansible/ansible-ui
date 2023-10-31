d1303a295ef2b730af371cdc1a156094fb377b48
Marliana Lara <mlara@redhat.com>
2023-10-31 08:34:06 -0400

    Add default edges and spacers to visualizer graph (#1135)

--- frontend/awx/resources/templates/WorkflowVisualizer/TopologyPipeline.tsx
+++ frontend/awx/resources/templates/WorkflowVisualizer/TopologyPipeline.tsx
@@ -2,28 +2,38 @@ import * as React from 'react';
 import {
   ComponentFactory,
   Controller,
+  DEFAULT_EDGE_TYPE,
+  DEFAULT_FINALLY_NODE_TYPE,
+  DEFAULT_SPACER_NODE_TYPE,
+  DEFAULT_TASK_NODE_TYPE,
   DefaultNode,
   Graph,
   GraphComponent,
-  graphDropTargetSpec,
   Layout,
   Model,
   ModelKind,
   Node,
-  NodeModel,
   NodeShape,
-  nodeDragSourceSpec,
-  nodeDropTargetSpec,
   PipelineDagreLayout,
+  PipelineNodeModel,
+  FinallyNode,
+  SpacerNode,
+  DefaultTaskGroup,
+  TaskEdge,
   Visualization,
   VisualizationProvider,
   VisualizationSurface,
   WithDndDropProps,
   WithDragNodeProps,
+  WithSelectionProps,
+  getEdgesFromNodes,
+  getSpacerNodes,
+  graphDropTargetSpec,
+  nodeDragSourceSpec,
+  nodeDropTargetSpec,
   withDndDrop,
   withDragNode,
   withPanZoom,
-  WithSelectionProps,
 } from '@patternfly/react-topology';
 import { ClipboardCheckIcon } from '@patternfly/react-icons';
 import type { WorkflowNode } from '../../../interfaces/WorkflowNode';
@@ -51,31 +61,58 @@ const pipelineComponentFactory = (kind: ModelKind, type: string) => {
     return withDndDrop(graphDropTargetSpec())(withPanZoom()(GraphComponent));
   }
   switch (type) {
-    case ModelKind.node:
+    case DEFAULT_TASK_NODE_TYPE:
       return withDndDrop(nodeDropTargetSpec([CONNECTOR_TARGET_DROP]))(
         withDragNode(nodeDragSourceSpec('node', true, true))(CustomNode)
       );
+    case DEFAULT_FINALLY_NODE_TYPE:
+      return FinallyNode;
+    case 'task-group':
+      return DefaultTaskGroup;
+    case 'finally-group':
+      return DefaultTaskGroup;
+    case DEFAULT_SPACER_NODE_TYPE:
+      return SpacerNode;
+    case 'finally-spacer-edge':
+    case DEFAULT_EDGE_TYPE:
+      return TaskEdge;
     default:
       return undefined;
   }
 };
 
+type PipelineWorkflowNode = WorkflowNode & {
+  runAfterTasks?: string[];
+};
+
 type TopologyPipelineProps = {
   data: AwxItemsResponse<WorkflowNode>;
 };
-export const TopologyPipeline = (props: TopologyPipelineProps) => {
-  const testNode = props.data.results[0];
-
+export const TopologyPipeline = ({ data: { results = [] } }: TopologyPipelineProps) => {
   const controllerRef = React.useRef<Controller>();
   const controller = controllerRef.current;
 
-  const [layout, setLayout] = React.useState<WorkflowNode[]>([]);
+  const [layout, setLayout] = React.useState<PipelineWorkflowNode[]>([]);
 
   React.useEffect(() => {
-    return setLayout(() => [testNode]);
-  }, [testNode]);
+    const layoutNodes = results.map((node) => {
+      return {
+        ...node,
+        runAfterTasks: [],
+      };
+    });
+
+    return setLayout(() => [...layoutNodes]);
+  }, [results]);
 
   React.useEffect(() => {
+    const newController = new Visualization();
+    newController.setFitToScreenOnLayout(true);
+    newController.registerComponentFactory(pipelineComponentFactory as ComponentFactory);
+    newController.registerLayoutFactory(
+      (type: string, graph: Graph): Layout | undefined => new PipelineDagreLayout(graph)
+    );
+
     const model: Model = {
       nodes: [],
       edges: [],
@@ -85,36 +122,60 @@ export const TopologyPipeline = (props: TopologyPipelineProps) => {
         layout: 'pipelineLayout',
       },
     };
-
-    const newController = new Visualization();
-    newController.setFitToScreenOnLayout(true);
-
-    newController.registerComponentFactory(pipelineComponentFactory as ComponentFactory);
-    newController.registerLayoutFactory(
-      (type: string, graph: Graph): Layout | undefined => new PipelineDagreLayout(graph)
-    );
-
     newController.fromModel(model, false);
 
     controllerRef.current = newController;
   }, []);
 
   React.useEffect(() => {
-    const nodes: NodeModel[] = layout.map((n) => {
-      const name = n.summary_fields?.unified_job_template?.name || 'no name';
+    const taskNodes: PipelineNodeModel[] = layout.map((n) => {
+      const processNodes = (nodesArray: number[]) => {
+        nodesArray.forEach((id) => {
+          const afterNode = layout.find((afterNode) => afterNode.id.toString() === id.toString());
+          if (afterNode) {
+            if (afterNode.runAfterTasks) {
+              afterNode.runAfterTasks.push(n.id.toString());
+            } else {
+              afterNode.runAfterTasks = [n.id.toString()];
+            }
+          }
+        });
+      };
+      if (n.success_nodes.length > 0) {
+        processNodes(n.success_nodes);
+      }
+      if (n.failure_nodes.length > 0) {
+        processNodes(n.failure_nodes);
+      }
+      if (n.always_nodes.length > 0) {
+        processNodes(n.always_nodes);
+      }
 
-      return {
-        id: n.identifier,
-        type: 'node',
-        label: name,
+      const node: PipelineNodeModel = {
+        id: n.id.toString(),
+        type: DEFAULT_TASK_NODE_TYPE,
+        label: n.summary_fields?.unified_job_template?.name || 'UNDEFINED',
         width: NODE_DIAMETER,
         height: NODE_DIAMETER,
         shape: NodeShape.ellipse,
+        runAfterTasks: n.runAfterTasks,
       };
+
+      return node;
     });
+
+    const spacerNodes = getSpacerNodes(taskNodes);
+    const nodes = [...taskNodes, ...spacerNodes];
+    const edges = getEdgesFromNodes(nodes);
+
     const model: Model = {
-      edges: [],
+      edges,
       nodes,
+      graph: {
+        id: 'g1',
+        type: 'graph',
+        layout: 'pipelineLayout',
+      },
     };
 
     if (!controller) {
