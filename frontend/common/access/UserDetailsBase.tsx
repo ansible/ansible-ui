9100fda1d76133c51d37f8877d15c6ec3128dbb6
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2023-10-31 15:33:08 -0400

    Base component for UserDetails (used in AWX & EDA) (#1155)

---
+++ frontend/common/access/UserDetailsBase.tsx
@@ -0,0 +1,89 @@
+import { useTranslation } from 'react-i18next';
+import { DateTimeCell, PageDetail, PageDetails } from '../../../framework';
+import { UserType } from '../../awx/access/users/components/UserType';
+import { AuthenticationType } from '../../awx/access/users/components/AuthenticationType';
+import { RoleRef } from '../../eda/interfaces/generated/eda-api';
+import { Label, LabelGroup } from '@patternfly/react-core';
+
+export type UserDetailsType = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  username: string;
+} & Partial<{
+  is_superuser: boolean | null;
+  is_system_auditor: boolean | null;
+  ldap_dn: string;
+  auth: string[];
+  created_at: string;
+  created: string;
+  created_on: string;
+  date_joined: string;
+  modified_at: string;
+  modified_on: string;
+  modified: string;
+  last_login: string;
+  roles: RoleRef[];
+}>;
+
+export function UserDetailsBase<T extends UserDetailsType>(props: {
+  user: T;
+  options?: {
+    // Shows the authentication type based on the last login
+    showAuthType?: boolean;
+    // Displays user type: System Administrator/ System Auditor/ Normal User
+    showUserType?: boolean;
+  };
+}) {
+  const { t } = useTranslation();
+  const { user, options } = props;
+
+  return (
+    <>
+      <PageDetails>
+        <PageDetail label={t('First name')}>{user.first_name}</PageDetail>
+        <PageDetail label={t('Last name')}>{user.last_name}</PageDetail>
+        <PageDetail label={t('Email')}>{user.email}</PageDetail>
+        <PageDetail label={t('Username')}>{user.username}</PageDetail>
+        {options?.showUserType && (
+          <PageDetail label={t('User type')}>
+            <UserType user={user} />
+          </PageDetail>
+        )}
+        {options?.showAuthType && (
+          <PageDetail label={t('Authentication type')}>
+            <AuthenticationType user={user} />
+          </PageDetail>
+        )}
+        {user.last_login && (
+          <PageDetail label={t('Last login')}>
+            <DateTimeCell format="since" value={user.last_login} />
+          </PageDetail>
+        )}
+        <PageDetail label={t('Created')}>
+          <DateTimeCell
+            format="date-time"
+            value={user.created ?? user.created_at ?? user.date_joined ?? user.created_on}
+          />
+        </PageDetail>
+        {(user.modified || user.modified_at || user.modified_on) && (
+          <PageDetail label={t('Modified')}>
+            <DateTimeCell
+              format="date-time"
+              value={user.modified ?? user.modified_at ?? user.modified_on}
+            />
+          </PageDetail>
+        )}
+        {user.roles && user.roles.length > 0 && (
+          <PageDetail label={t('Role(s)')}>
+            <LabelGroup>
+              {user.roles.map((role) => (
+                <Label key={role?.id}>{role?.name}</Label>
+              ))}
+            </LabelGroup>
+          </PageDetail>
+        )}
+      </PageDetails>
+    </>
+  );
+}
--- frontend/eda/UserAccess/Users/UserPage/MyDetails.tsx
+++ frontend/eda/UserAccess/Users/UserPage/MyDetails.tsx
@@ -1,41 +1,18 @@
 /* eslint-disable react/prop-types */
-import { Label, LabelGroup } from '@patternfly/react-core';
-import { useTranslation } from 'react-i18next';
-import { DateTimeCell, PageDetail, PageDetails } from '../../../../../framework';
 import { LoadingPage } from '../../../../../framework/components/LoadingPage';
+import { UserDetailsBase, UserDetailsType } from '../../../../common/access/UserDetailsBase';
 import { useGet } from '../../../../common/crud/useGet';
 import { API_PREFIX } from '../../../constants';
 import { EdaUser } from '../../../interfaces/EdaUser';
 
 export function MyDetails() {
-  const { t } = useTranslation();
   const { data: user } = useGet<EdaUser>(`${API_PREFIX}/users/me/`);
 
   if (!user) return <LoadingPage breadcrumbs tabs />;
 
   return (
-    <PageDetails>
-      <PageDetail label={t('Username')}>{user?.username}</PageDetail>
-      <PageDetail label={t('First name')}>{user?.first_name}</PageDetail>
-      <PageDetail label={t('Last name')}>{user?.last_name}</PageDetail>
-      <PageDetail label={t('Email')}>{user?.email}</PageDetail>
-      <PageDetail label={t('Created')}>
-        <DateTimeCell format="date-time" value={user?.created_at} />
-      </PageDetail>
-      <PageDetail label={t('Last modified')}>
-        <DateTimeCell format="date-time" value={user?.modified_at} />
-      </PageDetail>
-      {user?.roles && user.roles.length ? (
-        <PageDetail label={t('Role(s)')}>
-          <LabelGroup>
-            {user.roles.map((role) => (
-              <Label key={role?.id}>{role?.name}</Label>
-            ))}
-          </LabelGroup>
-        </PageDetail>
-      ) : (
-        <></>
-      )}
-    </PageDetails>
+    <>
+      <UserDetailsBase user={user as UserDetailsType} />
+    </>
   );
 }
