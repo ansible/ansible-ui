c78979f6da0dbb05c4cd854df66c07a8f54ab2ff
Vidya Nambiar <vnambiar@redhat.com>
2023-11-01 15:52:24 -0400

    initial

--- platform/access/users/components/UserDetails.tsx
+++ platform/access/users/components/UserDetails.tsx
@@ -1,5 +1,36 @@
-import { PageNotImplemented } from '../../../../framework';
+import { useParams } from 'react-router-dom';
+import { useGet, useGetItem } from '../../../../frontend/common/crud/useGet';
+import {
+  UserDetailsBase,
+  UserDetailsType,
+} from '../../../../frontend/common/access/UserDetailsBase';
+import { User } from '../../../interfaces/User';
+import { PlatformItemsResponse } from '../../../interfaces/PlatformItemsResponse';
+import { Organization } from '../../../interfaces/Organization';
+import { useEffect, useMemo } from 'react';
 
 export function UserDetails() {
-  return <PageNotImplemented />;
+  const params = useParams<{ id: string }>();
+  const { data: user } = useGetItem<User>('/api/gateway/v1/users', params.id);
+  const { data: organizations } = user
+    ? useGet<PlatformItemsResponse<Organization>>(user.related?.organizations)
+    : { data: [] };
+  const organizationNames = useMemo(() => {
+    if (Array.isArray(organizations) && organizations.length > 0) {
+      return organizations.map((organization: Organization) => organization.name);
+    }
+    return [];
+  }, []);
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <UserDetailsBase
+      user={user as UserDetailsType}
+      organizationNames={organizationNames}
+      options={{ showUserType: true }}
+    />
+  );
 }
