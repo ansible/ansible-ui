eb246ef2249ee212b72ea1bd880dd82afb89c545
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2023-11-03 08:59:57 -0400

    Renaming UserDetails base component and its usages (#1175)

--- frontend/eda/UserAccess/Users/UserPage/EdaMyDetails.tsx
+++ frontend/eda/UserAccess/Users/UserPage/EdaMyDetails.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable react/prop-types */
 import { LoadingPage } from '../../../../../framework/components/LoadingPage';
-import { UserDetailsBase, UserDetailsType } from '../../../../common/access/UserDetailsBase';
+import { UserDetails, UserDetailsType } from '../../../../common/access/UserDetails';
 import { useGet } from '../../../../common/crud/useGet';
 import { edaAPI } from '../../../api/eda-utils';
 import { EdaUser } from '../../../interfaces/EdaUser';
 
-export function MyDetails() {
+export function EdaMyDetails() {
   const { data: user } = useGet<EdaUser>(edaAPI`/users/me/`);
 
   if (!user) return <LoadingPage breadcrumbs tabs />;
 
   return (
     <>
-      <UserDetailsBase user={user as UserDetailsType} />
+      <UserDetails user={user as UserDetailsType} />
     </>
   );
 }
