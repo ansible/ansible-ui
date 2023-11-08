e9220543e12f94b6133ee09bb9651e1c43d0444e
James Talton <talton@gmail.com>
2023-11-08 09:35:33 -0500

    PageForm Documentation - Document Hidden, Section, & Watch
    Signed-off-by: James Talton <talton@gmail.com>

--- framework/PageForm/Utils/PageFormSection.tsx
+++ framework/PageForm/Utils/PageFormSection.tsx
@@ -2,6 +2,17 @@ import { FormSection, GridItem } from '@patternfly/react-core';
 import { ReactNode } from 'react';
 import { PageFormGrid } from '../PageForm';
 
+/**
+ * Renders a form section with an optional title and children components.
+ *
+ * This can also be used around an input such as testarea or data editor to ensure the input is rendered in a single column.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} [props.title] - The title of the form section.
+ * @param {ReactNode} props.children - The children components to render within the form section.
+ * @param {boolean} [props.singleColumn] - Whether to render the children components in a single column.
+ * @returns {JSX.Element} - The rendered form section component.
+ */
 export function PageFormSection(props: {
   title?: string;
   children: ReactNode;
--- framework/PageForm/Utils/PageFormWatch.tsx
+++ framework/PageForm/Utils/PageFormWatch.tsx
@@ -1,6 +1,20 @@
 import { ReactNode } from 'react';
 import { useWatch } from 'react-hook-form';
 
+/**
+ * A component that watches for changes in a specific field using `useWatch` from `react-hook-form`.
+ *
+ * @template T The type of the watched field.
+ * @param {Object} props The component props.
+ * @param {string} props.watch The name of the field to watch.
+ * @param {(value: T) => React.ReactNode} props.children A function that returns the ReactNode to render with the watched value.
+ * @returns {JSX.Element} The JSX element representing the component.
+ *
+ * @example
+ * <PageFormWatch<File | undefined> watch="file">
+ *   {(file) => <div>{file?.name}</div>}
+ * </PageFormWatch>
+ */
 export function PageFormWatch<T>(props: { watch: string; children: (value: T) => ReactNode }) {
   const value = useWatch({ name: props.watch }) as T;
   return <>{props.children(value)}</>;
