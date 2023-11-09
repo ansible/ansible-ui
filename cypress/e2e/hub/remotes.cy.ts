9d77ca92d9d6496354c2ac4800a498d08959adf4
Kersom <9053044+nixocio@users.noreply.github.com>
2023-11-09 13:28:49 -0500

    [AAH-2494] Add test to edit remote (#1206)

--- cypress/e2e/hub/remotes.cy.ts
+++ cypress/e2e/hub/remotes.cy.ts
@@ -6,6 +6,9 @@ const remoteName = `test-remote-${randomString(5, undefined, { isLowercase: true
 describe('Remotes', () => {
   before(() => {
     cy.hubLogin();
+  });
+
+  beforeEach(() => {
     cy.navigateTo('hub', 'remotes');
   });
 
@@ -27,5 +30,39 @@ describe('Remotes', () => {
     cy.clickButton(/^Clear all filters$/);
     cy.searchAndDisplayResource(remoteName);
     cy.contains(Remotes.noResults);
+  });
+
+  it('edit a remote', () => {
+    cy.get('[data-cy="create-remote"]').should('be.visible').click();
+    cy.url().should('include', Remotes.urlCreate);
+    cy.get('[data-cy="name"]').type(remoteName);
+    cy.get('[data-cy="url"]').type(Remotes.remoteURL);
+    cy.get('[data-cy="Submit"]').click();
+    cy.searchAndDisplayResource(remoteName);
+    cy.get('[data-cy="actions-column-cell"]').click();
+    cy.get('[data-cy="edit-remote"]').click({ force: true });
+    cy.url().should('include', `remotes/${remoteName}/edit`);
+    cy.get('[data-cy="url"]').clear().type(Remotes.editRemoteURL);
+    cy.get('[data-cy="username"]').type(Remotes.username);
+    cy.get('[data-cy="password"]').type(Remotes.password);
+    cy.get('[data-cy="expandable-section"]').find('button').first().click();
+    cy.get('[data-cy="token"]').type(Remotes.token);
+    cy.get('[data-cy="auth-url"]').type(Remotes.ssoURL);
+    cy.get('[data-cy="proxy-url"]').type(Remotes.proxyURL);
+    cy.get('[data-cy="proxy-username"]').type(Remotes.username);
+    cy.get('[data-cy="proxy-password"]').type(Remotes.password);
+    cy.get('[data-cy="download-concurrency"]').type(Remotes.downloadConcurrency);
+    cy.get('[data-cy="rate-limit"]').type(Remotes.rateLimit);
+    cy.get('[data-cy="tls_validation"]').click();
+    cy.clickButton(/^Edit remote$/);
+    cy.clickButton(/^Clear all filters$/);
+    cy.contains(remoteName).click();
+    cy.url().should('include', `remotes/details/${remoteName}`);
+    cy.get('[data-cy="name"]').should('contain', remoteName);
+    cy.get('[data-cy="url"]').should('contain', Remotes.editRemoteURL);
+    cy.get('[data-cy="proxy-url"]').should('contain', Remotes.proxyURL);
+    cy.get('[data-cy="tls-validation"]').should('contain', Remotes.tlsValidation);
+    cy.get('[data-cy="rate-limit"]').should('contain', Remotes.rateLimit);
+    cy.get('[data-cy="download-concurrency"]').should('contain', Remotes.downloadConcurrency);
   });
 });
