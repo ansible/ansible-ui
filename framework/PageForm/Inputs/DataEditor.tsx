df82ff36455525c4fbd03ad04c2bb1db10b91848
James Talton <jtalton@redhat.com>
2023-11-16 14:20:15 -0500

    Fix DataEditor to support two editors on the same page (credential-type) (#1231)
    Signed-off-by: James Talton <talton@gmail.com>

--- framework/PageForm/Inputs/DataEditor.tsx
+++ framework/PageForm/Inputs/DataEditor.tsx
@@ -117,16 +117,20 @@ export function DataEditor<
 
   useEffect(() => {
     const editor = editorRef?.current?.editor;
-    const uri = monaco.editor.getModels()[0];
-    if (editor && uri && language) {
-      monaco.editor.setModelLanguage(uri, language);
+    if (!editor) return;
+
+    const model = editor.getModel();
+    if (!model) return;
+
+    if (language) {
+      monaco.editor.setModelLanguage(model, language);
     }
-    editorRef.current?.editor?.getModel()?.onDidChangeContent(() => {
-      onChange(editorRef.current?.editor?.getValue() ?? '');
+    const didChangeContentDisposable = model.onDidChangeContent(() => {
+      onChange(editor.getValue() ?? '');
     });
-    const currentValue = editorRef.current?.editor?.getValue();
-    if (editorRef.current?.editor && currentValue !== props.value) {
-      editorRef.current?.editor?.setValue(props.value);
+    const currentValue = editor.getValue();
+    if (currentValue !== props.value) {
+      editor.setValue(props.value);
     }
     const valueArray = props.value.split('\n');
     const element = document.getElementById(idDataEditorElement);
@@ -134,6 +138,10 @@ export function DataEditor<
       element.style.minHeight = '75px';
       element.style.height = `${(valueArray.length + 3) * 19}` + 'px';
     }
+
+    return () => {
+      didChangeContentDisposable.dispose();
+    };
   }, [props.value, language, onChange, idDataEditorElement]);
 
   useResizeObserver(divEl, () => {
@@ -151,9 +159,16 @@ export function DataEditor<
   }, [settings.activeTheme]);
 
   useEffect(() => {
-    monaco.editor.onDidChangeMarkers(() => {
+    const editor = editorRef?.current?.editor;
+    if (!editor) return;
+
+    const model = editor.getModel();
+    if (!model) return;
+
+    const disChangeMarkersDisposable = monaco.editor.onDidChangeMarkers(() => {
       const markers = monaco.editor.getModelMarkers({
-        owner: monaco.editor?.getModels()[0]?.getLanguageId(),
+        owner: model.getLanguageId(),
+        resource: model.uri,
       });
       if (markers.length > 0) {
         setError(name, { message: markers.map((marker) => marker.message).join('\n') });
@@ -161,6 +176,10 @@ export function DataEditor<
         clearErrors(name);
       }
     });
+
+    return () => {
+      disChangeMarkersDisposable.dispose();
+    };
   }, [setError, clearErrors, name]);
 
   return (
