806d0db7eae484c9c90cb200bc17cab5a2bc4878
lgalis <lgalis@users.noreply.github.com>
2023-11-20 09:55:09 -0500

    Update delete url when force flag is required (#1230)

--- frontend/eda/Resources/decision-environments/DecisionEnvironments.cy.tsx
+++ frontend/eda/Resources/decision-environments/DecisionEnvironments.cy.tsx
@@ -79,6 +79,93 @@ describe('DecisionEnvironments.cy.ts', () => {
     cy.contains('Decision Environment');
     cy.contains('Image');
   });
+
+  it('can delete a Decision Environment not in use', () => {
+    cy.mount(<DecisionEnvironments />);
+    cy.intercept(
+      { method: 'DELETE', url: edaAPI`/decision-environments/1/` },
+      {
+        statusCode: 204,
+      }
+    );
+    cy.intercept(
+      { method: 'GET', url: edaAPI`/activations/?decision_environment_id=1` },
+      { count: 0, next: null, previous: null, page_size: 20, page: 1, results: [] }
+    );
+    cy.get('input[id="check-1"]').first().click();
+    cy.get('[data-cy="actions-dropdown"]').first().click();
+    cy.get('[data-cy="delete-selected-decision-environments"]').click();
+    cy.get('div[role="dialog"]').within(() => {
+      cy.get('.pf-v5-c-check__label').should(
+        'contain',
+        `Yes, I confirm that I want to delete these`
+      );
+      cy.contains('EDA Decision Environment 1');
+      cy.get('input[id="confirm"]').click();
+      cy.get('button').contains('Delete decision environments').click();
+    });
+    cy.get('[data-cy="status-column-cell"] > span').contains('Success');
+    cy.clickButton(/^Close$/);
+  });
+
+  it('can delete a Decision Environment in use', () => {
+    cy.mount(<DecisionEnvironments />);
+    cy.intercept(
+      { method: 'DELETE', url: edaAPI`/decision-environments/1/?force=true` },
+      {
+        statusCode: 204,
+      }
+    );
+    cy.intercept(
+      { method: 'GET', url: edaAPI`/activations/?decision_environment_id=1` },
+      {
+        count: 1,
+        next: null,
+        previous: null,
+        page_size: 20,
+        page: 1,
+        results: [
+          {
+            id: 1,
+            name: 'Activation 1',
+            description: '',
+            is_enabled: true,
+            status: 'completed',
+            decision_environment_id: 1,
+            project_id: 4,
+            rulebook_id: 30,
+            extra_var_id: null,
+            restart_policy: 'on-failure',
+            restart_count: 0,
+            rulebook_name: 'hello_echo.yml',
+            current_job_id: null,
+            rules_count: 1,
+            rules_fired_count: 1,
+            created_at: '2023-11-16T18:55:26.833885Z',
+            modified_at: '2023-11-16T18:55:32.928509Z',
+            status_message: 'Activation has completed',
+          },
+        ],
+      }
+    );
+    cy.get('input[id="check-1"]').first().click();
+    cy.get('[data-cy="actions-dropdown"]').first().click();
+    cy.get('[data-cy="delete-selected-decision-environments"]').click();
+    cy.get('div[role="dialog"]').within(() => {
+      cy.get('.pf-v5-c-check__label').should(
+        'contain',
+        `Yes, I confirm that I want to delete these`
+      );
+      cy.contains('EDA Decision Environment 1');
+      cy.get('.pf-v5-c-alert__title').contains(
+        'The following decision environments are in use: EDA Decision Environment 1'
+      );
+      cy.get('input[id="confirm"]').click();
+      cy.get('button').contains('Delete decision environments').click();
+    });
+    cy.get('[data-cy="status-column-cell"] > span').contains('Success');
+    cy.clickButton(/^Close$/);
+  });
 });
 
 describe('Empty list', () => {
