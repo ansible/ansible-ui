1fb764ba1b09005155b3c4b7611410743842c7a1
Marliana Lara <mlara@redhat.com>
2023-11-20 10:05:24 -0500

    Render deleted workflow nodes (#1244)

--- frontend/awx/resources/templates/WorkflowVisualizer/components/index.ts
+++ frontend/awx/resources/templates/WorkflowVisualizer/components/index.ts
@@ -1,6 +1,7 @@
 import { AddNodeButton } from './AddNodeButton';
 import { CustomEdge } from './CustomEdge';
 import { CustomNode } from './CustomNode';
+import { DeletedNode } from './DeletedNode';
 import { NodeContextMenu } from './NodeContextMenu';
 import { SystemJobNodeDetails } from './SystemJobNodeDetails';
 import { VisualizerWrapper } from './VisualizerWrapper';
@@ -10,6 +11,7 @@ export {
   AddNodeButton,
   CustomEdge,
   CustomNode,
+  DeletedNode,
   NodeContextMenu,
   SystemJobNodeDetails,
   VisualizerWrapper,
