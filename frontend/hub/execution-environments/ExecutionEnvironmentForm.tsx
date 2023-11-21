f1dd3ece1605008f5727d3608db681161fa1b8b3
MilanPospisil <arkanus@seznam.cz>
2023-11-21 10:26:34 +0100

    Prettier

--- frontend/hub/execution-environments/ExecutionEnvironmentForm.tsx
+++ frontend/hub/execution-environments/ExecutionEnvironmentForm.tsx
@@ -1,4 +1,5 @@
-import { useTranslation, TFunction } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
+import { TFunction } from 'i18next';
 import { useParams } from 'react-router-dom';
 import {
   PageFormSubmitHandler,
