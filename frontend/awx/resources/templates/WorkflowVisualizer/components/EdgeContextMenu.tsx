3417f3b73e6a2c11c847b67652cded235c1b7d3b
Alex Corey <acorey@redhat.com>
2023-11-27 15:11:15 -0500

    Adds Edge context menu (#1254)

---
+++ frontend/awx/resources/templates/WorkflowVisualizer/components/EdgeContextMenu.tsx
@@ -0,0 +1,110 @@
+import { ReactElement } from 'react';
+import {
+  ContextMenuSeparator,
+  ContextMenuItem,
+  GraphElement,
+  ElementModel,
+} from '@patternfly/react-topology';
+import {
+  CheckCircleIcon,
+  CircleIcon,
+  ExclamationCircleIcon,
+  TrashIcon,
+} from '@patternfly/react-icons';
+import { Icon } from '@patternfly/react-core';
+
+interface MenuItem {
+  key: string;
+  label: string;
+  icon?: ReactElement;
+  isDanger?: boolean;
+  onClick?: (element: GraphElement) => void;
+}
+
+export function useEdgeMenuItems(t: (item: string) => string): MenuItem[] {
+  return [
+    {
+      key: 'success',
+      icon: (
+        <Icon status="success">
+          <CheckCircleIcon />
+        </Icon>
+      ),
+      label: t('Run on success'),
+      onClick: (element: GraphElement) =>
+        element.setData({
+          tag: t('Run on success'),
+          tagStatus: 'success',
+          endTerminalStatus: 'success',
+        }),
+    },
+    {
+      key: 'always',
+      icon: (
+        <Icon status="info">
+          <CircleIcon />
+        </Icon>
+      ),
+      label: t('Run always'),
+      onClick: (element: GraphElement) =>
+        element.setData({ tag: t('Run always'), tagStatus: 'info', endTerminalStatus: 'info' }),
+    },
+    {
+      key: 'fail',
+      icon: (
+        <Icon status="danger">
+          <ExclamationCircleIcon />
+        </Icon>
+      ),
+      label: t('Run on failure'),
+      onClick: (element: GraphElement) =>
+        element.setData({
+          tag: t('Run on failure'),
+          tagStatus: 'danger',
+          endTerminalStatus: 'fail',
+        }),
+    },
+    {
+      key: 'separator',
+      label: '-',
+    },
+    {
+      key: 'remove-link',
+      icon: (
+        <Icon status="danger">
+          <TrashIcon />
+        </Icon>
+      ),
+      isDanger: true,
+      label: t('Remove link'),
+      onClick: (_element) => alert(`Selected: Remove Node`),
+    },
+  ];
+}
+
+export function EdgeContextMenu(
+  element: GraphElement<ElementModel, unknown>,
+  t: (string: string) => string
+) {
+  const data = element.getData() as { tagStatus: string };
+
+  const items = useEdgeMenuItems(t);
+  return items.map((item) => {
+    if (item.label === '-') {
+      return <ContextMenuSeparator component="li" key={`separator:${item.key}`} />;
+    }
+
+    return (
+      <ContextMenuItem
+        data-cy={item.key}
+        isSelected={item.key === data.tagStatus}
+        key={item.key}
+        icon={item.icon}
+        isDanger={item.isDanger}
+        onClick={() => item?.onClick && item.onClick(element)}
+      >
+        {item.label}
+      </ContextMenuItem>
+    );
+  });
+}
