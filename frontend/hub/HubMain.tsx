a919e5d754faa2499921261ebf6d170d34862ad5
James Talton <talton@gmail.com>
2023-11-28 16:01:09 -0500

    Fix login and logout
    Signed-off-by: James Talton <talton@gmail.com>

--- frontend/hub/HubMain.tsx
+++ frontend/hub/HubMain.tsx
@@ -4,18 +4,21 @@ import '@patternfly/patternfly/patternfly-charts.css';
 import '@patternfly/patternfly/patternfly-charts-theme-dark.css';
 
 import { Outlet } from 'react-router-dom';
+import { useGetPageUrl } from '../../framework';
 import { PageApp } from '../../framework/PageNavigation/PageApp';
 import { Login } from '../common/Login';
 import '../common/i18n';
 import { HubMasthead } from './HubMasthead';
+import { hubAPI } from './api/formatPath';
 import { HubContextProvider } from './useHubContext';
 import { useHubNavigation } from './useHubNavigation';
 
 export default function HubMain() {
   const navigation = useHubNavigation();
+  const getPageUrl = useGetPageUrl();
   return (
     <PageApp
-      login={<Login />}
+      login={<Login apiUrl={hubAPI`/_ui/v1/auth/login/`} onLoginUrl="/dashboard" />}
       root={
         <HubContextProvider>
           <Outlet />
