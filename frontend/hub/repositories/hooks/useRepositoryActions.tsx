4ccbd63dd7a695122a472e984ab971243fae913b
ZitaNemeckova <znemecko@redhat.com>
2023-11-28 15:12:49 +0100

    Update Repositories List view to be visually same as old one (#1264)

--- frontend/hub/repositories/hooks/useRepositoryActions.tsx
+++ frontend/hub/repositories/hooks/useRepositoryActions.tsx
@@ -4,6 +4,7 @@ import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { IPageAction, PageActionSelection, PageActionType } from '../../../../framework';
 import { Repository } from '../Repository';
+import { PROTECTED_REPOSITORIES } from '../../common/constants';
 
 export function useRepositoryActions() {
   const { t } = useTranslation();
@@ -22,12 +23,34 @@ export function useRepositoryActions() {
         type: PageActionType.Seperator,
       },
       {
+        label: t('Sync repository'),
+        onClick: () => {},
+        selection: PageActionSelection.Single,
+        type: PageActionType.Button,
+        isDisabled: (repo) => {
+          return repo.remote
+            ? undefined
+            : t('There are no remotes associated with this repository');
+        },
+      },
+      {
+        label: t('Copy CLI configuration'),
+        onClick: () => {},
+        selection: PageActionSelection.Single,
+        type: PageActionType.Button,
+      },
+      {
         icon: TrashIcon,
         label: t('Delete repository'),
         onClick: () => {},
         selection: PageActionSelection.Single,
         type: PageActionType.Button,
         isDanger: true,
+        isDisabled: (repo) => {
+          return PROTECTED_REPOSITORIES.includes(repo.name)
+            ? t('Protected repository cannot be deleted')
+            : undefined;
+        },
       },
     ],
     [t]
