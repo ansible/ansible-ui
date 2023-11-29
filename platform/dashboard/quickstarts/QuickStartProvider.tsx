98de2416a039afbcd37cf26adb767df2514a4586
James Talton <talton@gmail.com>
2023-11-29 14:55:09 -0500

    Finding content in automation hub quick start
    Signed-off-by: James Talton <talton@gmail.com>

--- platform/dashboard/quickstarts/QuickStartProvider.tsx
+++ platform/dashboard/quickstarts/QuickStartProvider.tsx
@@ -1,12 +1,17 @@
 import { AllQuickStartStates, QuickStart, QuickStartContainer } from '@patternfly/quickstarts';
 import { ReactNode, useMemo, useState } from 'react';
 import { useExampleQuickStart } from './useExampleQuickStart';
+import { useFindingContentQuickStart } from './useFindingContentQuickStart';
 
 export function QuickStartProvider(props: { children: ReactNode }) {
   const [activeQuickStartID, setActiveQuickStartID] = useState('');
   const [allQuickStartStates, setAllQuickStartStates] = useState<AllQuickStartStates>({});
   const exampleQuickStart = useExampleQuickStart();
-  const quickStarts = useMemo<QuickStart[]>(() => [exampleQuickStart], [exampleQuickStart]);
+  const findingContentQuickStart = useFindingContentQuickStart();
+  const quickStarts = useMemo<QuickStart[]>(
+    () => [exampleQuickStart, findingContentQuickStart],
+    [exampleQuickStart, findingContentQuickStart]
+  );
   return (
     <QuickStartContainer
       quickStarts={quickStarts}
