43757f716431c3593083759c2d797ea8ddb0277b
Vidya Nambiar <vnambiar@redhat.com>
2023-11-30 12:10:17 -0500

    updated fallback for EDA endpoint

--- cypress/support/formatApiPathForEDA.ts
+++ cypress/support/formatApiPathForEDA.ts
@@ -8,6 +8,6 @@
 import { apiTag } from '../../frontend/hub/api/formatPath';
 
 export function edaAPI(strings: TemplateStringsArray, ...values: string[]) {
-  const base = (Cypress.env('EDA_API_PREFIX') as string) || '/api/eda';
+  const base = (Cypress.env('EDA_API_PREFIX') as string) || '/api/eda/v1';
   return base + apiTag(strings, ...values);
 }
