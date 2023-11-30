9950658103508e7f0bd3b3043a0090f20290d672
Pratyush Bhandari <prbhanda@redhat.com>
2023-11-30 09:26:57 -0500

    update fadeway timeout from default to 2 seconds

--- frontend/awx/views/jobs/hooks/useRelaunchJob.tsx
+++ frontend/awx/views/jobs/hooks/useRelaunchJob.tsx
@@ -90,7 +90,7 @@ export function useRelaunchJob(jobRelaunchParams?: JobRelaunch) {
         variant: 'danger',
         title: t('Failed to relaunch job'),
         children: error instanceof Error && error.message,
-        timeout: true,
+        timeout: 2000,
       });
     }
   };
