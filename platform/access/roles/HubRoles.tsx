e97a9cae167c498b32d422dd28fae433b230ad57
James Talton <talton@gmail.com>
2023-11-30 13:40:15 -0500

    initial roles support
    Signed-off-by: James Talton <talton@gmail.com>

--- platform/access/roles/HubRoles.tsx
+++ platform/access/roles/HubRoles.tsx
@@ -1,19 +1,19 @@
 import { Alert } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
-import { EdaRolesTable } from '../../../frontend/eda/UserAccess/Roles/Roles';
+import { HubRolesTable } from '../../../frontend/hub/access/roles/Roles';
 
-export function EdaRoles() {
+export function HubRoles() {
   const { t } = useTranslation();
   return (
     <>
       <Alert
-        title={t('These roles only apply to resources in the context of automation decisions.')}
+        title={t('These roles only apply to resources in the context of automation content.')}
         variant="info"
         style={{ borderTop: 0 }}
         className="border-bottom"
         isInline
       ></Alert>
-      <EdaRolesTable />
+      <HubRolesTable />
     </>
   );
 }
