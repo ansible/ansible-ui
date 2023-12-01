658e884493f4ba7bda4c549e857976548cd70f96
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-12-01 13:45:13 -0500

    WIP

--- frontend/hub/namespaces/HubNamespacePage/HubNamespaceCLI.tsx
+++ frontend/hub/namespaces/HubNamespacePage/HubNamespaceCLI.tsx
@@ -3,14 +3,13 @@ import React from 'react';
 import { LoadingPage } from '../../../../framework';
 import ExternalLinkAltIcon from '@patternfly/react-icons/dist/esm/icons/external-link-alt-icon';
 import { Trans } from 'react-i18next';
-import { ClipboardCopy, PageSection } from '@patternfly/react-core';
+import { ClipboardCopy } from '@patternfly/react-core';
 import { useGet } from '../../../common/crud/useGet';
 import { HubItemsResponse } from '../../useHubView';
 import { HubNamespace } from '../HubNamespace';
 import { hubAPI } from '../../api/formatPath';
 import { HubError } from '../../common/HubError';
 import { getRepoURL } from '../../api/utils';
-import styled from 'styled-components';
 
 export function HubNamespaceCLI() {
   const repositoryUrl = getRepoURL('published');
@@ -35,19 +34,19 @@ export function HubNamespaceCLI() {
     <div style={{ padding: '24px' }}>
       <ClipboardCopy isReadOnly>{repositoryUrl}</ClipboardCopy>
       <div style={{ paddingTop: '12px' }}>
-      <Trans>
-        <b>Note:</b> Use this URL to configure ansible-galaxy to upload collections to this
-        namespace. More information on ansible-galaxy configurations can be found{' '}
-        <a
-          href="https://docs.ansible.com/ansible/latest/galaxy/user_guide.html#configuring-the-ansible-galaxy-client"
-          target="_blank"
-          rel="noreferrer"
-        >
-          here
-        </a>
-        <span>&nbsp;</span>
-        <ExternalLinkAltIcon />.
-      </Trans>
+        <Trans>
+          <b>Note:</b> Use this URL to configure ansible-galaxy to upload collections to this
+          namespace. More information on ansible-galaxy configurations can be found{' '}
+          <a
+            href="https://docs.ansible.com/ansible/latest/galaxy/user_guide.html#configuring-the-ansible-galaxy-client"
+            target="_blank"
+            rel="noreferrer"
+          >
+            here
+          </a>
+          <span>&nbsp;</span>
+          <ExternalLinkAltIcon />.
+        </Trans>
       </div>
     </div>
   );
