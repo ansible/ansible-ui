04820b3820460c3513938a9dffaf6aff25c6b966
James Talton <talton@gmail.com>
2023-12-01 16:27:21 -0500

    plaform pull in awx roles from upstream
    Signed-off-by: James Talton <talton@gmail.com>

--- platform/access/roles/PlatformAwxRoles.tsx
+++ platform/access/roles/PlatformAwxRoles.tsx
@@ -1,6 +1,6 @@
 import { Alert } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
-import { PageNotImplemented } from '../../../framework';
+import { AwxRolesTable } from '../../../frontend/awx/access/roles/AwxRoles';
 
 export function PlatformAwxRoles() {
   const { t } = useTranslation();
@@ -13,40 +13,8 @@ export function PlatformAwxRoles() {
         style={{ borderTop: 0 }}
         className="border-bottom"
         isInline
-        isExpandable
-      >
-        {t(
-          'Automation execution roles are based on the type of resource access is being granted. When a user or team is granted access to a resource, the roles shown below can be selected. Some roles such a team member or some organization roles grant further access to the users and teams.'
-        )}
-      </Alert>
-      <PageNotImplemented />
-
-      {/* <PageTabs>
-        {Object.keys(awxRoles).map((key) => {
-          const resourceType = awxRoles[key];
-          return (
-            <PageTab key={key} label={resourceType.name}>
-              <PageSection variant="light">
-                <DescriptionList isHorizontal>
-                  <DescriptionListGroup>
-                    <DescriptionListTerm>{t('Role')}</DescriptionListTerm>
-                    <DescriptionListTerm>{t('Description')}</DescriptionListTerm>
-                  </DescriptionListGroup>
-                  <Divider />
-                  {Object.values(resourceType.roles).map((role) => {
-                    return (
-                      <DescriptionListGroup key={role.label}>
-                        <DescriptionListTerm>{role.label}</DescriptionListTerm>
-                        <DescriptionListDescription>{role.description}</DescriptionListDescription>
-                      </DescriptionListGroup>
-                    );
-                  })}
-                </DescriptionList>
-              </PageSection>
-            </PageTab>
-          );
-        })}
-      </PageTabs> */}
+      />
+      <AwxRolesTable />
     </>
   );
 }
