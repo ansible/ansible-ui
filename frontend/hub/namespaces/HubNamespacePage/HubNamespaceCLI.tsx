26898f71214ad98a6503c8e9bf8d7d192fb6fb81
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-12-02 20:40:22 -0500

    use CopyCell component for styling unity

--- frontend/hub/namespaces/HubNamespacePage/HubNamespaceCLI.tsx
+++ frontend/hub/namespaces/HubNamespacePage/HubNamespaceCLI.tsx
@@ -1,14 +1,14 @@
-import { ClipboardCopy } from '@patternfly/react-core';
 import ExternalLinkAltIcon from '@patternfly/react-icons/dist/esm/icons/external-link-alt-icon';
 import { Trans } from 'react-i18next';
 import { getRepoURL } from '../../api/utils';
+import { CopyCell } from '../../../../framework';
 
 export function HubNamespaceCLI() {
   const repositoryUrl = getRepoURL('published');
 
   return (
     <div style={{ padding: '24px' }}>
-      <ClipboardCopy isReadOnly>{repositoryUrl}</ClipboardCopy>
+      <CopyCell text={repositoryUrl} />
       <div style={{ paddingTop: '12px' }}>
         <Trans>
           <b>Note:</b> Use this URL to configure ansible-galaxy to upload collections to this
