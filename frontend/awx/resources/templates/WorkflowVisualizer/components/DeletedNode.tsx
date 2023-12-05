27f9eb50a49fbf3fc43e51d0d7417a0eb7eacf59
Marliana Lara <mlara@redhat.com>
2023-12-05 13:40:06 -0500

    Workflow visualizer remove all nodes and screen state management  (#1294)

--- frontend/awx/resources/templates/WorkflowVisualizer/components/DeletedNode.tsx
+++ frontend/awx/resources/templates/WorkflowVisualizer/components/DeletedNode.tsx
@@ -1,14 +1,25 @@
 import { FC } from 'react';
 import { TrashIcon } from '@patternfly/react-icons';
-import { DefaultNode, WithDragNodeProps } from '@patternfly/react-topology';
+import { DefaultNode, WithDragNodeProps, WithSelectionProps } from '@patternfly/react-topology';
 import type { CustomNodeProps } from '../types';
 
-export const DeletedNode: FC<CustomNodeProps & WithDragNodeProps> = ({ element, ...rest }) => {
+export const DeletedNode: FC<CustomNodeProps & WithDragNodeProps & WithSelectionProps> = ({
+  element,
+  onSelect,
+  selected,
+  ...rest
+}) => {
   const data = element.getData();
   if (!data) return null;
 
   return (
-    <DefaultNode element={element} labelClassName={`${data.id}-node-label`} {...rest}>
+    <DefaultNode
+      element={element}
+      labelClassName={`${data.id}-node-label`}
+      onSelect={onSelect}
+      selected={selected}
+      {...rest}
+    >
       <g transform={`translate(13, 13)`}>
         <TrashIcon style={{ color: '#393F44' }} width={25} height={25} />
       </g>
