4580e41a3157b68495f3e53f8847febfb04df970
Pratyush Bhandari <prbhanda@redhat.com>
2023-12-06 20:50:04 -0500

    Add toolbar filter to application tokens list

--- frontend/awx/administration/applications/ApplicationPage/ApplicationPageTokens.tsx
+++ frontend/awx/administration/applications/ApplicationPage/ApplicationPageTokens.tsx
@@ -7,15 +7,18 @@ import { useTokensColumns } from '../hooks/useTokensColumns';
 import { awxAPI } from '../../../api/awx-utils';
 import { Token } from '../../../interfaces/Token';
 import { useParams } from 'react-router-dom';
+import { useTokensFilters } from '../hooks/useTokensFilters';
 
 export function ApplicationTokens() {
   const { t } = useTranslation();
   const config = useAwxConfig();
   const tableColumns = useTokensColumns();
+  const toolbarFilters = useTokensFilters();
   const params = useParams<{ id: string }>();
   const view = useAwxView<Token>({
     url: awxAPI`/applications/${params.id ?? ''}/tokens`,
     tableColumns,
+    toolbarFilters,
   });
 
   return (
@@ -23,12 +26,13 @@ export function ApplicationTokens() {
       <PageHeader
         title={t('Application Tokens')}
         description={t('List of access tokens associated with this application.')}
-        titleHelpTitle={t('Applications')}
+        titleHelpTitle={t('Application Tokens')}
         titleHelp={t('List of access tokens associated with this application.')}
         titleDocLink={`${getDocsBaseUrl(config)}/html/userguide/applications_auth.html`}
       />
       <PageTable<Token>
         id="awx-applications-token-table"
+        toolbarFilters={toolbarFilters}
         tableColumns={tableColumns}
         errorStateTitle={t('Error loading applications')}
         emptyStateTitle={t('There are currently no tokens associated with this application')}
---
+++ frontend/awx/administration/applications/hooks/useTokensFilters.tsx
@@ -0,0 +1,23 @@
+import { useMemo } from 'react';
+import { IToolbarFilter, ToolbarFilterType } from '../../../../../framework';
+import { useTranslation } from 'react-i18next';
+
+export function useTokensFilters() {
+  const nameToolbarFilter = useTokenUserToolbarFilter();
+  const toolbarFilters = useMemo<IToolbarFilter[]>(() => [nameToolbarFilter], [nameToolbarFilter]);
+  return toolbarFilters;
+}
+
+function useTokenUserToolbarFilter() {
+  const { t } = useTranslation();
+  return useMemo<IToolbarFilter>(
+    () => ({
+      key: 'name',
+      label: t('name'),
+      type: ToolbarFilterType.Text,
+      query: 'user__username__icontains',
+      comparison: 'contains',
+    }),
+    [t]
+  );
+}
