a3ad61df90f0789e00989bc0d23ca14c2942d69a
Sarah Akus <sarah.akus@gmail.com>
2023-12-11 10:25:44 -0500

    add file-upload plugin for cypress (#1328)

--- cypress/e2e/hub/collections.cy.ts
+++ cypress/e2e/hub/collections.cy.ts
@@ -1,6 +1,6 @@
 import { Collections } from './constants';
 
-describe('Collections', () => {
+describe('Collections- List View', () => {
   before(() => {
     cy.hubLogin();
   });
@@ -9,4 +9,62 @@ describe('Collections', () => {
     cy.navigateTo('hub', Collections.url);
     cy.verifyPageTitle(Collections.title);
   });
+
+  it.skip('user can upload a new collection', () => {});
+
+  it.skip('user can delete a collection using the list toolbar', () => {});
+
+  it.skip('user can delete selected entire collections from repository using the list toolbar', () => {});
+
+  it.skip('user can deprecate selected collections using the list toolbar', () => {});
+});
+
+describe('Collections List- Line Item Kebab Menu', () => {
+  before(() => {
+    cy.hubLogin();
+  });
+
+  it.skip('user can upload a new version to an existing collection', () => {});
+
+  it.skip('user can delete entire collection from system', () => {});
+
+  it.skip('user can delete entire collection from repository', () => {});
+
+  it.skip('user can deprecate a collection', () => {});
+
+  it.skip('user can copy a version to repository', () => {});
+});
+
+describe('Collections Details View', () => {
+  before(() => {
+    cy.hubLogin();
+  });
+
+  it.skip('user can upload a new version to an existing collection', () => {});
+
+  it.skip('user can delete entire collection from system', () => {});
+
+  it.skip('user can delete entire collection from repository', () => {});
+
+  it.skip('user can deprecate a collection', () => {});
+
+  it.skip('user can delete version from system', () => {});
+
+  it.skip('user can delete version from repository', () => {});
+
+  it.skip('user can copy a version to repository', () => {});
+
+  it.skip('user can access the Install tab and download a tarball', () => {});
+});
+
+describe('Collection Approvals List', () => {
+  before(() => {
+    cy.hubLogin();
+  });
+
+  it.skip('user can approve a collection', () => {});
+
+  it.skip('user can reject a collection', () => {});
+
+  it.skip('user can upload a signature to a collection', () => {});
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -47,6 +47,7 @@ import './e2e';
 import './eda-commands';
 import './hub-commands';
 import './rest-commands';
+import 'cypress-file-upload';
 
 declare global {
   namespace Cypress {
