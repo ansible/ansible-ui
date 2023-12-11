45843853866c822a3a383a61464ddc6bfbabfa70
MilanPospisil <arkanus@seznam.cz>
2023-12-11 16:48:17 +0100

    WIP

--- frontend/hub/collections/CollectionPage/CollectionDistributions.tsx
+++ frontend/hub/collections/CollectionPage/CollectionDistributions.tsx
@@ -1,7 +1,31 @@
 import { DetailInfo } from '../../../../framework/components/DetailInfo';
 import { useTranslation } from 'react-i18next';
+import { useOutletContext } from 'react-router-dom';
+import { CollectionVersionSearch } from '../Collection';
+//import { useGet } from '../../../common/crud/useGet';
+import { usePulpView } from '../../usePulpView';
+import { pulpAPI } from '../../api/formatPath';
 
 export function CollectionDistributions() {
   const { t } = useTranslation();
+  const { collection } = useOutletContext<{ collection: CollectionVersionSearch }>();
+
+  //const { data, error, refresh} =  useGet('/distributions/ansible/ansible/', { 'repository' : collection.repository?.pulp_href || '' });
+
+  debugger;
+
+  const view = usePulpView<Distribution>({
+    url : pulpAPI`/distributions/ansible/ansible/`,
+    keyFn : (item) => item.base_path,
+  });
+
   return <DetailInfo title={t('This page is under construction')} />;
+}
+
+interface Distribution
+{
+  name : string,
+  pulp_href : string,
+  base_path : string,
+  pulp_created : string,
 }
