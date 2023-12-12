842ad37874e8525f33e7b01c0042c5fcbcdccaa0
MilanPospisil <arkanus@seznam.cz>
2023-12-12 10:37:33 +0100

    collection distributions

--- frontend/hub/collections/CollectionPage/CollectionDistributions.tsx
+++ frontend/hub/collections/CollectionPage/CollectionDistributions.tsx
@@ -1,31 +1,88 @@
-import { DetailInfo } from '../../../../framework/components/DetailInfo';
 import { useTranslation } from 'react-i18next';
 import { useOutletContext } from 'react-router-dom';
 import { CollectionVersionSearch } from '../Collection';
-//import { useGet } from '../../../common/crud/useGet';
 import { usePulpView } from '../../usePulpView';
 import { pulpAPI } from '../../api/formatPath';
+import { useMemo } from 'react';
+import { ITableColumn } from '../../../../framework';
+import { PageTable } from '../../../../framework';
+import { CopyCell } from '../../../../framework';
+
+import { getRepoURL } from '../../api/formatPath';
 
 export function CollectionDistributions() {
   const { t } = useTranslation();
   const { collection } = useOutletContext<{ collection: CollectionVersionSearch }>();
 
-  //const { data, error, refresh} =  useGet('/distributions/ansible/ansible/', { 'repository' : collection.repository?.pulp_href || '' });
-
-  debugger;
+  const tableColumns = useDistributionsColumns();
 
   const view = usePulpView<Distribution>({
-    url : pulpAPI`/distributions/ansible/ansible/`,
-    keyFn : (item) => item.base_path,
+    url: pulpAPI`/distributions/ansible/ansible/`,
+    keyFn: (item) => item.base_path,
+    tableColumns,
+    queryParams: { repository: collection.repository?.pulp_href || '' },
   });
 
-  return <DetailInfo title={t('This page is under construction')} />;
+  return (
+    <>
+      <PageTable<Distribution>
+        id="hub-collection-version-search-table"
+        tableColumns={tableColumns}
+        errorStateTitle={t('Error loading distributions')}
+        emptyStateTitle={t('No distributions yet')}
+        {...view}
+        defaultTableView="list"
+        defaultSubtitle={t('Distributions')}
+      />
+    </>
+  );
 }
 
-interface Distribution
-{
-  name : string,
-  pulp_href : string,
-  base_path : string,
-  pulp_created : string,
+interface Distribution {
+  name: string;
+  pulp_href: string;
+  base_path: string;
+  pulp_created: string;
+}
+
+function useDistributionsColumns(_options?: { disableSort?: boolean; disableLinks?: boolean }) {
+  const { t } = useTranslation();
+
+  return useMemo<ITableColumn<Distribution>[]>(
+    () => [
+      {
+        header: t('Name'),
+        type: 'text',
+        value: (item) => item.name,
+        sort: 'name',
+      },
+      {
+        header: t('Base path'),
+        type: 'text',
+        value: (item) => item.base_path,
+        sort: 'base_path',
+      },
+      {
+        header: t('Created'),
+        type: 'datetime',
+        value: (item) => item.pulp_created,
+        sort: 'pulp_created',
+      },
+      {
+        header: t('	CLI configuration'),
+        cell: (item) => {
+          const copyText = [
+            '[galaxy]',
+            `server_list = ${item.base_path}`,
+            '',
+            `[galaxy_server.${item.base_path}]`,
+            `url=${getRepoURL(item.base_path)}`,
+            'token=<put your token here>',
+          ].join('\n');
+          return <CopyCell text={copyText} />;
+        },
+      },
+    ],
+    [t]
+  );
 }
