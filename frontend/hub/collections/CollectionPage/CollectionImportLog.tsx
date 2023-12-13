99f9012e57aa077b8cf87f7f7a28968df55cf8e5
MilanPospisil <arkanus@seznam.cz>
2023-12-13 16:37:48 +0100

    Scrollable bottom and back- but it not work good quite yet

--- frontend/hub/collections/CollectionPage/CollectionImportLog.tsx
+++ frontend/hub/collections/CollectionPage/CollectionImportLog.tsx
@@ -11,9 +11,10 @@ import {
 } from '../../../../framework';
 import { Alert, CodeBlock, PageSection, Stack, StackItem } from '@patternfly/react-core';
 import { StatusCell } from '../../../common/Status';
-import React from 'react';
 import { useOutletContext } from 'react-router-dom';
 import { CollectionImport, CollectionVersionSearch } from '../Collection';
+import styled from 'styled-components';
+import React, { useRef } from 'react';
 
 export function CollectionImportLog() {
   const { collection } = useOutletContext<{ collection: CollectionVersionSearch }>();
@@ -27,12 +28,40 @@ export function CollectionImportLog() {
         }&sort=-created&limit=1`
       : ''
   );
+  const ref = useRef<HTMLDivElement>(null);
 
   const { data: collectionImport } = useGet<CollectionImport>(
     collectionImportsResponse && collectionImportsResponse.data.length
       ? hubAPI`/_ui/v1/imports/collections/${collectionImportsResponse.data[0].id}/`
       : ''
   );
+
+  const StyledArrowDown = styled.div`
+    color: white;
+    &:hover {
+      cursor: pointer;
+    }
+    position: absolute;
+    right: 0; /* Aligns the icon to the right */
+    top: 0; /* Aligns the icon to the top */
+    margin-top: 10px;
+    margin-right: 10px;
+    font-size: 120%;
+  `;
+
+  const StyledArrowUp = styled.div`
+    color: white;
+    &:hover {
+      cursor: pointer;
+    }
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    margin-bottom: 10px;
+    margin-right: 10px;
+    font-size: 120%;
+  `;
+
   // http://ec2-54-147-146-116.compute-1.amazonaws.com:8002/api/automation-hub/_ui/v1/imports/collections/ef7849bd-17f5-434f-b35a-3c1877884d12/
 
   if (!collection) return <></>;
@@ -40,54 +69,93 @@ export function CollectionImportLog() {
 
   return (
     <Scrollable>
-      <PageSection variant="light">
-        <Stack hasGutter>
-          <PageDetails>
-            <PageDetail label={t('Status')}>
-              <StatusCell status={collectionImport?.state} />
-            </PageDetail>
-            {/* <Detail label={t('Approval Status')}>
+      <div ref={ref}>
+        <PageSection variant="light">
+          <Stack hasGutter>
+            <PageDetails>
+              <PageDetail label={t('Status')}>
+                <StatusCell status={collectionImport?.state} />
+              </PageDetail>
+              {/* <Detail label={t('Approval Status')}>
             </Detail> */}
-            <PageDetail label={t('Version')}>{collectionImport?.version}</PageDetail>
-          </PageDetails>
-          {collectionImport?.error && (
-            <Alert
-              variant="danger"
-              title={
-                <Stack>
-                  {collectionImport?.error?.description
-                    .split('\n')
-                    .map((line, index) => <StackItem key={index}>{line}</StackItem>)}
-                </Stack>
-              }
-              isInline
-              isExpandable
-            >
-              <pre>{collectionImport?.error?.traceback}</pre>
-            </Alert>
-          )}
-          <CodeBlock>
-            {collectionImport?.messages?.map((message, index) => (
-              <div
-                key={index}
+              <PageDetail label={t('Version')}>{collectionImport?.version}</PageDetail>
+            </PageDetails>
+            {collectionImport?.error && (
+              <Alert
+                variant="danger"
+                title={
+                  <Stack>
+                    {collectionImport?.error?.description
+                      .split('\n')
+                      .map((line, index) => <StackItem key={index}>{line}</StackItem>)}
+                  </Stack>
+                }
+                isInline
+                isExpandable
+              >
+                <pre>{collectionImport?.error?.traceback}</pre>
+              </Alert>
+            )}
+            <div>
+              <CodeBlock
                 style={{
-                  color: getPatternflyColor(
-                    message.level === 'INFO'
-                      ? PFColorE.Default
-                      : message.level === 'WARNING'
-                        ? PFColorE.Warning
-                        : message.level === 'ERROR'
-                          ? PFColorE.Danger
-                          : PFColorE.Disabled
-                  ),
+                  backgroundColor: 'var(--pf-v5-global--palette--black-850)',
+                  position: 'relative',
                 }}
               >
-                {message.message}
-              </div>
-            ))}
-          </CodeBlock>
-        </Stack>
-      </PageSection>
+                <StyledArrowDown>
+                  <span
+                    onClick={() => {
+                      ref?.current?.scrollIntoView({ block: 'end', behavior: 'smooth' });
+                    }}
+                    className="fa fa-arrow-circle-down clickable"
+                  />
+                </StyledArrowDown>
+                <StyledArrowUp>
+                  <span
+                    onClick={() => {
+                      ref?.current?.scrollIntoView({ block: 'start', behavior: 'smooth' });
+                    }}
+                    className="fa fa-arrow-circle-up clickable"
+                  />
+                </StyledArrowUp>
+                {collectionImport?.messages?.map((message, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      color: getColor(message.level),
+                    }}
+                  >
+                    {message.message}
+                  </div>
+                ))}
+                <br />
+                <div
+                  key={'done'}
+                  style={{
+                    color: PFColorE.Green,
+                  }}
+                >{t`Done`}</div>
+              </CodeBlock>
+            </div>
+          </Stack>
+        </PageSection>
+      </div>
     </Scrollable>
   );
+}
+
+function getColor(messageLevel: string) {
+  const res = getPatternflyColor(
+    messageLevel === 'WARNING'
+      ? PFColorE.Warning
+      : messageLevel === 'ERROR'
+      ? PFColorE.Danger
+      : PFColorE.Disabled
+  );
+
+  if (messageLevel == 'INFO') {
+    return 'white';
+  }
+  return res;
 }
