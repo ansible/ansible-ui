d9e4ba69d19e10d54e0674a42e072a1c7c6a49ce
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-12-14 14:15:04 -0500

    WIP

--- frontend/hub/collections/CollectionPage/CollectionDocumentation.tsx
+++ frontend/hub/collections/CollectionPage/CollectionDocumentation.tsx
@@ -1,41 +1,55 @@
 import React, { useMemo, useState } from 'react';
 import { useGet } from '../../../common/crud/useGet';
-import { hubAPI } from '../../api/formatPath';
+import { hubAPI, pulpAPI } from '../../api/formatPath';
 import { useBreakpoint } from '../../../../framework';
 import { Drawer, DrawerContent, DrawerContentBody } from '@patternfly/react-core';
 import { useOutletContext } from 'react-router-dom';
 import { IContents, CollectionDocs, CollectionVersionSearch } from '../Collection';
 import { CollectionDocumentationTabPanel } from '../components/CollectionDocumentationTabPanel';
 import { CollectionDocumentationTabContent } from '../components/CollectionDocumentationTabContent';
+import { CollectionVersionListResponse } from '../../api-schemas/generated/CollectionVersionListResponse';
 
 export function CollectionDocumentation() {
   const { collection } = useOutletContext<{ collection: CollectionVersionSearch }>();
 
   const [content, setContent] = useState<IContents>();
 
-  const { data } = useGet<CollectionDocs>(
-    hubAPI`/_ui/v1/repo/published/${collection?.collection_version?.name || ''}/${
-      collection?.collection_version?.name || ''
-    }/?include_related=my_permissions`
+  const { data } = useGet<CollectionVersionsContent>(
+    pulpAPI`/content/ansible/collection_versions/?namespace=${
+      collection?.collection_version?.namespace || ''
+    }&name=${collection?.collection_version?.name || ''}&version=${
+      collection?.collection_version?.version || ''
+    }&offset=0&limit=1`
   );
+
+  type CollectionVersionsContent = {
+    count: number;
+    next: string;
+    previous: string;
+    results: unknown[];
+  };
 
   const groups = useMemo(() => {
     const groups: Record<string, { name: string; contents: IContents[] }> = {};
-    if (data?.latest_version.docs_blob.contents) {
-      for (const content of data.latest_version.docs_blob.contents) {
+    if (data) {
+      for (const content of data.results[0].docs_blob.contents) {
         let group = groups[content.content_type];
-        if (!group) {
-          group = { name: content.content_type, contents: [] };
-          groups[content.content_type] = group;
+        if (!group)  {
+          group = {
+            name: content.content_type.charAt(0).toUpperCase() + content.content_type.slice(1),
+            contents: [],
+          };
         }
-        group.contents.push(content);
+        groups[content.content_type] = group;
+        }
+        groups.contents.push(content);
       }
     }
     for (const group of Object.values(groups)) {
       group.contents = group.contents.sort((l, r) => l.content_name.localeCompare(r.content_name));
     }
     return Object.values(groups);
-  }, [data?.latest_version.docs_blob.contents]);
+  }, [data]);
 
   const [isDrawerOpen, setDrawerOpen] = useState(true);
   const lg = useBreakpoint('lg');
