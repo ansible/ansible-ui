c3d58024287ff947bfbd0407ae905fa767a4ee8c
Shaiah Emigh-Doyle <64337863+ShaiahWren@users.noreply.github.com>
2023-12-15 15:22:52 -0500

    CLI config on ns detail (#1291)

--- frontend/hub/useHubNavigation.tsx
+++ frontend/hub/useHubNavigation.tsx
@@ -24,6 +24,7 @@ import {
 import { ExecutionEnvironments } from './execution-environments/ExecutionEnvironments';
 import { CreateHubNamespace, EditHubNamespace } from './namespaces/HubNamespaceForm';
 import { HubNamespaceDetails } from './namespaces/HubNamespacePage/HubNamespaceDetails';
+import { HubNamespaceCLI } from './namespaces/HubNamespacePage/HubNamespaceCLI';
 import { HubNamespacePage } from './namespaces/HubNamespacePage/HubNamespacePage';
 import { Namespaces } from './namespaces/HubNamespaces';
 import { RemoteRegistries } from './remote-registries/RemoteRegistries';
@@ -71,6 +72,11 @@ export function useHubNavigation() {
               id: HubRoute.NamespaceDetails,
               path: 'details',
               element: <HubNamespaceDetails />,
+            },
+            {
+              id: HubRoute.NamespaceCLI,
+              path: 'cli',
+              element: <HubNamespaceCLI />,
             },
             {
               path: '',
