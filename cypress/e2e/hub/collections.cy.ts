4c5e6314801db0f04445c70b53900fbccd73c380
Sarah Akus <sarah.akus@gmail.com>
2023-12-18 15:40:08 -0500

    Hub upload new collection- custom command (#1365)

--- cypress/e2e/hub/collections.cy.ts
+++ cypress/e2e/hub/collections.cy.ts
@@ -1,25 +1,52 @@
-import { randomString } from '../../../framework/utils/random-string';
 import { Collections } from './constants';
 
 describe('Collections- List View', () => {
-  //Important to know:
+  //**Important to know:
   //**In order to upload a collection, a namespace must first exist containing the first word of the collection file name
   //**The only way to get rid of a collection's artifact is to choose the following option:
   //**Delete entire collection from repository
-  //
+  //**If the artifact isn't deleted when the collection is deleted, and a user tries to create
+  //**a new collection by uploading the same file again, Hub will not allow it.
   before(() => {
-    const newname = 'e2enamespace' + randomString(4);
-    const namespaceName = newname.toLowerCase();
     cy.hubLogin();
-    cy.createNamespace(namespaceName);
+    cy.getNamespace('ibm');
   });
+
+  // after(() => {
+  //   cy.deleteCollectionsInNamespace('ibm');
+  // });
 
   it('it should render the collections page', () => {
     cy.navigateTo('hub', Collections.url);
     cy.verifyPageTitle(Collections.title);
   });
 
-  it.skip('user can upload a new collection', () => {});
+  it.skip('user can upload a new collection', () => {
+    cy.navigateTo('hub', Collections.url);
+    cy.verifyPageTitle(Collections.title);
+    cy.get('[data-cy="upload-collection"]').click();
+
+    cy.uploadHubCollectionFile(
+      'collection-files/ibm-zos_zoau_operator-1.0.3.tar.gz',
+      'ibm-zos_zoau_operator-1.0.3.tar.gz'
+    );
+
+    cy.get('input[id="radio-non-pipeline"]').click();
+    cy.get('[data-cy="row-0"]').within(() => {
+      cy.get('input').click();
+    });
+    cy.intercept(
+      'POST',
+      `/api/galaxy//v3/plugin/ansible/content/community/collections/artifacts/`
+    ).as('collection');
+    cy.get('[data-cy="Submit"]').click();
+    cy.wait('@collection').then((resp) => {
+      expect(resp?.response?.statusCode).to.eql(202);
+    });
+    cy.get('[data-cy="hub-collections"]').click();
+    cy.verifyPageTitle(Collections.title);
+    cy.get('[data-cy="refresh"]').click();
+  });
 
   it.skip('user can delete a collection using the list toolbar', () => {});
 
