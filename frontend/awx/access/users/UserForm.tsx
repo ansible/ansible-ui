942f678c2965000eec7f1f4d579a96306d452a5b
James Talton <jtalton@redhat.com>
2023-12-19 15:11:50 -0500

    Upgrade dependencies (#1349)
    Signed-off-by: James Talton <talton@gmail.com>

--- frontend/awx/access/users/UserForm.tsx
+++ frontend/awx/access/users/UserForm.tsx
@@ -132,8 +132,8 @@ export function EditUser() {
     userType: user.is_superuser
       ? UserType.SystemAdministrator
       : user.is_system_auditor
-      ? UserType.SystemAuditor
-      : UserType.NormalUser,
+        ? UserType.SystemAuditor
+        : UserType.NormalUser,
   };
   return (
     <PageLayout>
--- frontend/awx/resources/templates/WorkflowVisualizer/hooks/useGetDetailComponent.tsx
+++ frontend/awx/resources/templates/WorkflowVisualizer/hooks/useGetDetailComponent.tsx
@@ -1,9 +1,9 @@
 import { useCallback } from 'react';
-import { WorkflowNode } from '../../../../interfaces/WorkflowNode';
+import { UnifiedJobType, WorkflowNode } from '../../../../interfaces/WorkflowNode';
+import { InventorySourceDetails } from '../../../inventories/inventorySources/InventorySourceDetails';
 import { ProjectDetails } from '../../../projects/ProjectPage/ProjectDetails';
 import { TemplateDetails } from '../../TemplatePage/TemplateDetails';
 import { WorkflowJobTemplateDetails } from '../../WorkflowJobTemplatePage/WorkflowJobTemplateDetails';
-import { InventorySourceDetails } from '../../../inventories/inventorySources/InventorySourceDetails';
 import { SystemJobNodeDetails } from '../components/SystemJobNodeDetails';
 import { WorkflowApprovalNodeDetails } from '../components/WorkflowApprovalNodeDetails';
 
@@ -11,14 +11,14 @@ export function useGetDetailComponent(selectedNode: WorkflowNode) {
   const detailsComponent = useCallback(() => {
     let component;
     switch (selectedNode?.summary_fields.unified_job_template.unified_job_type) {
-      case 'project_update':
+      case UnifiedJobType.project_update:
         component = (
           <ProjectDetails
             projectId={selectedNode?.summary_fields.unified_job_template.id.toString()}
           />
         );
         break;
-      case 'job':
+      case UnifiedJobType.job:
         component = (
           <TemplateDetails
             templateId={selectedNode?.summary_fields.unified_job_template.id.toString()}
@@ -26,7 +26,7 @@ export function useGetDetailComponent(selectedNode: WorkflowNode) {
         );
 
         break;
-      case 'workflow_job':
+      case UnifiedJobType.workflow_job:
         component = (
           <WorkflowJobTemplateDetails
             templateId={selectedNode?.summary_fields.unified_job_template.id.toString()}
@@ -34,17 +34,17 @@ export function useGetDetailComponent(selectedNode: WorkflowNode) {
         );
 
         break;
-      case 'inventory_update':
+      case UnifiedJobType.inventory_update:
         component = (
           <InventorySourceDetails
             inventorySourceId={selectedNode?.summary_fields.unified_job_template.id.toString()}
           />
         );
         break;
-      case 'system_job':
+      case UnifiedJobType.system_job:
         component = <SystemJobNodeDetails selectedNode={selectedNode} />;
         break;
-      case 'workflow_approval':
+      case UnifiedJobType.workflow_approval:
         component = <WorkflowApprovalNodeDetails selectedNode={selectedNode} />;
         break;
 
