691facea96db56f9cfe1f1975200e4dceb98bc3b
lgalis <lgalis@redhat.com>
2023-12-20 14:49:40 -0500

    Add args field to the

--- frontend/eda/Resources/event-sources/EventSourcePage/EventSourceDetails.tsx
+++ frontend/eda/Resources/event-sources/EventSourcePage/EventSourceDetails.tsx
@@ -8,6 +8,9 @@ import { SWR_REFRESH_INTERVAL } from '../../../constants';
 import { EdaEventSourceRead } from '../../../interfaces/EdaEventSource';
 import { LastModifiedPageDetail } from '../../../../common/LastModifiedPageDetail';
 import { EdaDecisionEnvironment } from '../../../interfaces/EdaDecisionEnvironment';
+import { PageSection } from '@patternfly/react-core';
+import { PageDetailCodeEditor } from '../../../../../framework/PageDetails/PageDetailCodeEditor';
+import { Fragment } from 'react';
 
 export function EventSourceDetails() {
   const { t } = useTranslation();
@@ -26,26 +29,38 @@ export function EventSourceDetails() {
   const getPageUrl = useGetPageUrl();
 
   return (
-    <PageDetails>
-      <PageDetail label={t('Name')}>{eventSource?.name || ''}</PageDetail>
-      <PageDetail label={t('Type')}>{eventSource?.type || ''}</PageDetail>
-      <PageDetail label={t('Decision environment')} helpText={t('Decision environment.')}>
-        {eventSource && eventSource.decision_environment_id ? (
-          <Link
-            to={getPageUrl(EdaRoute.DecisionEnvironmentPage, {
-              params: { id: eventSource?.decision_environment_id },
-            })}
-          >
-            {decisionEnvironment?.name}
-          </Link>
-        ) : (
-          decisionEnvironment?.name || ''
-        )}
-      </PageDetail>
-      <PageDetail label={t('Created')}>
-        <DateTimeCell format="date-time" value={eventSource?.created_at} />
-      </PageDetail>
-      <LastModifiedPageDetail format="date-time" value={eventSource?.modified_at} />
-    </PageDetails>
+    <Fragment>
+      <PageDetails>
+        <PageDetail label={t('Name')}>{eventSource?.name || ''}</PageDetail>
+        <PageDetail label={t('Type')}>{eventSource?.type || ''}</PageDetail>
+        <PageDetail label={t('Decision environment')} helpText={t('Decision environment.')}>
+          {eventSource && eventSource.decision_environment_id ? (
+            <Link
+              to={getPageUrl(EdaRoute.DecisionEnvironmentPage, {
+                params: { id: eventSource?.decision_environment_id },
+              })}
+            >
+              {decisionEnvironment?.name}
+            </Link>
+          ) : (
+            decisionEnvironment?.name || ''
+          )}
+        </PageDetail>
+        <PageDetail label={t('Created')}>
+          <DateTimeCell format="date-time" value={eventSource?.created_at} />
+        </PageDetail>
+        <LastModifiedPageDetail format="date-time" value={eventSource?.modified_at} />
+      </PageDetails>
+      {eventSource?.args && (
+        <PageSection variant="light">
+          <PageDetailCodeEditor
+            value={eventSource.args}
+            showCopyToClipboard={true}
+            label={t('Args')}
+            helpText={t(`The arguments are in a JSON or YAML format.`)}
+          />
+        </PageSection>
+      )}
+    </Fragment>
   );
 }
