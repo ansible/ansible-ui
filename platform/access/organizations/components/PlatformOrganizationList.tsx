24f6f25cab8e4d3a8ed2418d7616f559fd297da5
James Talton <talton@gmail.com>
2024-01-03 14:26:18 -0500

    Access fix user create adding to teams
    Signed-off-by: James Talton <talton@gmail.com>

--- platform/access/organizations/components/PlatformOrganizationList.tsx
+++ platform/access/organizations/components/PlatformOrganizationList.tsx
@@ -69,6 +69,7 @@ export function PlatformOrganizationList() {
         }
         {...view}
         defaultSubtitle={t('Organization')}
+        defaultTableView="cards"
       />
     </PageLayout>
   );
