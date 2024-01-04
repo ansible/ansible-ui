247ab470890fdf2701c1293b831a1916f70f15ef
Martin Hradil <mhradil@redhat.com>
2024-01-04 01:19:13 +0000

    fixup keys to account for /_ui/v1/imports/collections/

--- frontend/hub/useHubView.tsx
+++ frontend/hub/useHubView.tsx
@@ -67,6 +67,7 @@ const sortKeys = {
   '/pulp/api/v3/pulp_container/namespaces/': 'sort',
   '/v1/imports/': 'order_by',
   '/v1/roles/': 'order_by',
+  '/_ui/v1/': 'sort',
   '/v3/plugin/ansible/search/collection-versions/': 'order_by',
 };
 
@@ -74,7 +75,7 @@ const pageKeys = {
   '/v1/imports/': 'page',
   '/v1/namespaces/': 'page',
   '/v1/roles/': 'page',
-  '/_ui/v1/namespaces/': 'offset',
+  '/_ui/v1/': 'offset',
 };
 
 function url2keys(url: string): { sortKey: string; pageKey: string } {
