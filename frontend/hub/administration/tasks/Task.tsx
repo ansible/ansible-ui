3e8f1ac1c8296b180f9d7c4321516cb92f04e481
Salma Kochay <skochay330@gmail.com>
2024-01-04 12:28:22 -0500

    task details page

--- frontend/hub/administration/tasks/Task.tsx
+++ frontend/hub/administration/tasks/Task.tsx
@@ -2,10 +2,19 @@ export interface Task {
   name: string;
   pulp_href: string;
   pulp_created: string;
+  created_by: string;
   started_at: string;
   finished_at: string;
   state: 'completed' | 'failed' | 'running' | 'waiting';
   error?: TaskError;
+  worker: string;
+  logging_cid: string;
+  task_group: string;
+  parent_task: string;
+  child_tasks: [];
+  progress_reports: [];
+  created_resources: [];
+  reserved_resources_record: [];
 }
 
 interface TaskError {
--- frontend/hub/administration/tasks/TaskDetails.tsx
+++ frontend/hub/administration/tasks/TaskDetails.tsx
@@ -2,6 +2,8 @@ import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import {
   DateTimeCell,
+  PageDashboard,
+  PageDashboardCard,
   PageDetails,
   PageHeader,
   PageLayout,
@@ -15,6 +17,8 @@ import { HubError } from '../../common/HubError';
 import { pulpAPI } from '../../common/api/formatPath';
 import { HubRoute } from '../../main/HubRoutes';
 import { Task } from './Task';
+import { DescriptionList, Divider, capitalize } from '@patternfly/react-core';
+import { EmptyStateNoData } from '../../../framework/components/EmptyStateNoData';
 
 export function TaskDetails() {
   const { t } = useTranslation();
@@ -39,21 +43,105 @@ export function TaskDetails() {
           { label: task?.name },
         ]}
       />
-      <PageDetails>
-        <PageDetail label={t('Name')}>{task?.name}</PageDetail>
-        <PageDetail label={t('Status')}>
-          <StatusCell status={task?.state} />
-        </PageDetail>
-        <PageDetail label={t('Started')}>
-          <DateTimeCell format="since" value={task?.finished_at} />
-        </PageDetail>
-        <PageDetail label={t('Finished')}>
-          <DateTimeCell format="since" value={task?.finished_at} />
-        </PageDetail>
-        <PageDetail label={t('Created')}>
-          <DateTimeCell format="since" value={task?.pulp_created} />
-        </PageDetail>
-      </PageDetails>
+      <PageDashboard>
+        <PageDashboardCard title={t('Task detail')}>
+          <DescriptionList>
+            <PageDetails numberOfColumns="single">
+              <PageDetail label={t('Name')}>{task?.name}</PageDetail>
+              <PageDetail label={t('Status')}>
+                <StatusCell status={task?.state} />
+              </PageDetail>
+              <PageDetail label={t('Started')}>
+                <DateTimeCell format="since" value={task?.finished_at} />
+              </PageDetail>
+              <PageDetail label={t('Finished')}>
+                <DateTimeCell format="since" value={task?.finished_at} />
+              </PageDetail>
+              <PageDetail label={t('Created')}>
+                <DateTimeCell format="since" value={task?.pulp_created} />
+              </PageDetail>
+            </PageDetails>
+          </DescriptionList>
+        </PageDashboardCard>
+        <PageDashboardCard title={t('Task groups')}>
+          <DescriptionList>
+            <PageDetails numberOfColumns="single">
+              <PageDetail label={t('Task group')}>
+                {task.task_group ? task.task_group : t`No task group`}
+              </PageDetail>
+              <PageDetail label={t('Parent task')}>
+                {task.parent_task ? task.parent_task : t`No parent task`}
+              </PageDetail>
+              <PageDetail label={t('Child task')}>
+                {task.child_tasks.length > 0 ? task.child_tasks : t`No child task`}
+              </PageDetail>
+            </PageDetails>
+          </DescriptionList>
+        </PageDashboardCard>
+        <PageDashboardCard title={t('Reserve resources')}>
+          {/* {resources.length ? (
+            <DescriptionList>
+              {resources.map((resource) => {
+                return (
+                  <>
+                    <PageDetails key={''} numberOfColumns="single">
+                      <PageDetail label={t('Type')}>{resource.type}</PageDetail>
+                      {resource.pluginName && (
+                        <PageDetail label={t('Plugin')}>{resource.pluginName}</PageDetail>
+                      )}
+                      {resource.name && <PageDetail label={t('Name')}>{resource.name}</PageDetail>}
+                    </PageDetails>
+                    <Divider inset={{ default: 'insetMd' }} />
+                  </>
+                );
+              })}
+            </DescriptionList>
+          ) : (
+            <EmptyStateNoData
+              title={t`There is no resource record.`}
+              description={t`There is no resource record.`}
+            ></EmptyStateNoData>
+          )} */}
+        </PageDashboardCard>
+        <PageDashboardCard title={t('Progress messages')}>
+          {task.progress_reports.length ? (
+            <DescriptionList>
+              {task.progress_reports.reverse().map((report) => {
+                return (
+                  <>
+                    <PageDetails key={''} numberOfColumns="single">
+                      {Object.keys(report).map((key) => {
+                        return (
+                          !!report[key] && (
+                            <PageDetail
+                              label={
+                                {
+                                  message: t`Message`,
+                                  code: t`Code`,
+                                  state: t`State`,
+                                  done: t`Done`,
+                                }[key] || capitalize(key)
+                              }
+                            >
+                              {report[key]}
+                            </PageDetail>
+                          )
+                        );
+                      })}
+                    </PageDetails>
+                    <Divider inset={{ default: 'insetMd' }} />
+                  </>
+                );
+              })}
+            </DescriptionList>
+          ) : (
+            <EmptyStateNoData
+              title={t`There is no progress message.`}
+              description={t`There is no progress message.`}
+            ></EmptyStateNoData>
+          )}
+        </PageDashboardCard>
+      </PageDashboard>
     </PageLayout>
   );
 }
