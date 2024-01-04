f9b664eb6485af0616f8f7a7dbb7fc563fa8099d
James Talton <jtalton@redhat.com>
2024-01-04 09:01:10 -0500

    Routing cleanup - cleanup more routes (#1406)
    Signed-off-by: James Talton <talton@gmail.com>

--- frontend/common/Routes.ts
+++ frontend/common/Routes.ts
@@ -2,11 +2,7 @@
 export const RouteObj = {
   /** DO NOT ADD NEW VALUES - DEPRECATED */
   JobOutput: `/jobs/:job_type/:id/output`,
-  JobDetails: `/jobs/:job_type/:id/details`,
   CreateSchedule: `/schedules/create`,
-
-  /** DO NOT ADD NEW VALUES - DEPRECATED */
-  JobTemplateDetails: `/templates/job_template/:id/details`,
 
   /** DO NOT ADD NEW VALUES - DEPRECATED */
   JobTemplateSchedules: `/templates/job_template/:id/schedules/`,
@@ -25,20 +21,14 @@ export const RouteObj = {
   WorkflowJobTemplateScheduleRules: `/templates/workflow_job_template/:id/schedules/:schedule_id/rules`,
 
   /** DO NOT ADD NEW VALUES - DEPRECATED */
-  Projects: `/projects`,
-  ProjectDetails: `/projects/:id/details`,
   ProjectSchedules: `/projects/:id/schedules`,
   ProjectSchedulePage: `/projects/:id/schedules/:schedule_id/*`,
   ProjectSchedulesCreate: `/projects/:id/schedules/create`,
   ProjectScheduleDetails: `/projects/:id/schedules/:schedule_id/details`,
   ProjectCreateScheduleRules: `/project/:id/schedules/:schedule_id/rules/create`,
   ProjectScheduleRules: `/project/:id/schedules/:schedule_id/rules`,
-  EditProject: `/projects/:id/edit`,
 
   /** DO NOT ADD NEW VALUES - DEPRECATED */
-  Inventories: `/infrastructure/inventories`,
-  InventoryDetails: `/infrastructure/inventories/:inventory_type/:id/details`,
-  InventorySourcesDetails: `/infrastructure/inventories/:inventory_type/:id/sources/:source_id/details`,
   InventorySourceSchedulesCreate: `/infrastructure/inventories/:inventory_type/:id/sources/:source_id/schedules/create`,
   InventorySourceSchedules: `/infrastructure/inventories/:inventory_type/:id/sources/:source_id/schedules`,
   InventorySourceSchedulePage: `/infrastructure/inventories/:inventory_type/:id/sources/:source_id/schedules/:schedule_id/*`,
@@ -47,12 +37,8 @@ export const RouteObj = {
   InventorySourceScheduleRules: `/infrastructure/inventories/inventory/:id/sources/:source_id/schedules/:schedule_id/rules`,
 
   /** DO NOT ADD NEW VALUES - DEPRECATED */
-  ManagementJobs: `/administration/management_jobs`,
-  ManagementJobSchedulesDetails: `/administration/management_jobs/:id/schedules/:schedule_id/details`,
   InstanceGroupDetails: `/infrastructure/instance-groups/:id`,
   ExecutionEnvironmentDetails: `/administration/execution-environments/:id/details`,
-  CreateExecutionEnvironment: `/administration/execution-environments/create`,
-  EditExecutionEnvironment: `/administration/execution-environments/:id/edit`,
 
   /** DO NOT ADD NEW VALUES - DEPRECATED */
 };
