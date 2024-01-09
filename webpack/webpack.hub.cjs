aa030b70dbf6e5b463c79491f17a0b6b884dc843
James Talton <talton@gmail.com>
2024-01-09 10:44:24 -0500

    Webpack: Fix hosting of static assets based on ROUTE_PREFIX.
    Signed-off-by: James Talton <talton@gmail.com>

--- webpack/webpack.hub.cjs
+++ webpack/webpack.hub.cjs
@@ -4,8 +4,12 @@ const { HUB_SERVER } = env;
 const proxyUrl = new URL(HUB_SERVER);
 module.exports = function (env, argv) {
   const config = webpackConfig(env, argv);
-
   config.entry = './frontend/hub/Hub.tsx';
+
+  // publicPath is the path where the bundle is served from
+  // https://webpack.js.org/guides/public-path/
+  // it nees to match the route prefix of the app
+  config.output.publicPath = process.env.PUBLIC_PATH || process.env.HUB_ROUTE_PREFIX || '/';
 
   config.devServer.proxy = {
     '/api': {
