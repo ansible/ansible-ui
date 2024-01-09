ee8f78fbc4874918c386475bb040139f4ab3170d
MilanPospisil <arkanus@seznam.cz>
2024-01-09 14:49:15 +0100

    Sign collection

--- frontend/hub/collections/hooks/useSignCollection.tsx
+++ frontend/hub/collections/hooks/useSignCollection.tsx
@@ -8,25 +8,49 @@ import { useCollectionColumns } from './useCollectionColumns';
 import { useHubContext } from '../../useHubContext';
 import { getRepositoryBasePath } from '../../api/utils';
 
-export function useSignCollection(version : boolean, onComplete?: (collections: CollectionVersionSearch[]) => void) {
+export function useSignCollection(
+  version: boolean,
+  onComplete?: (collections: CollectionVersionSearch[]) => void
+) {
   const context = useHubContext();
   const signing_service = context.settings.GALAXY_COLLECTION_SIGNING_SERVICE;
   const { t } = useTranslation();
   const confirmationColumns = useCollectionColumns();
   const actionColumns = useMemo(() => [confirmationColumns[0]], [confirmationColumns]);
   const bulkAction = useBulkConfirmation<CollectionVersionSearch>();
+
   return useCallback(
     (collections: CollectionVersionSearch[]) => {
+      const title = version
+        ? t('Sign collection version', { count: collections.length })
+        : t('Sign collection', { count: collections.length });
+
+      const confirmText = version
+        ? t('Yes, I confirm that I want to sign these {{count}} collections versions.', {
+            count: collections.length,
+          })
+        : t('Yes, I confirm that I want to sign these {{count}} collections.', {
+            count: collections.length,
+          });
+
+      const actionButtonText = version
+        ? t('Sign collections versions', { count: collections.length })
+        : t('Sign collections', { count: collections.length });
+
       bulkAction({
-        title: t('Sign collection version', { count: collections.length }),
-        confirmText: t('Yes, I confirm that I want to sign these {{count}} collections versions.', {
-          count: collections.length,
-        }),
-        actionButtonText: t('Sign collections versions', { count: collections.length }),
+        title,
+        confirmText,
+        actionButtonText,
         items: collections.sort((l, r) =>
           compareStrings(
-            l.collection_version?.name || '' + l.repository?.name + l.collection_version?.namespace,
-            r.collection_version?.name || '' + r.repository?.name + r.collection_version?.namespace
+            l.collection_version?.name ||
+              '' + l.repository?.name + l.collection_version?.namespace + version
+              ? l.collection_version?.version
+              : '',
+            r.collection_version?.name ||
+              '' + r.repository?.name + r.collection_version?.namespace + version
+              ? r.collection_version?.version
+              : ''
           )
         ),
         keyFn: collectionKeyFn,
@@ -35,23 +59,33 @@ export function useSignCollection(version : boolean, onComplete?: (collections: 
         actionColumns,
         onComplete,
         actionFn: (collection: CollectionVersionSearch) =>
-          signCollectionVersion(collection, signing_service),
+          signCollectionVersion(version, collection, signing_service),
       });
     },
     [actionColumns, bulkAction, confirmationColumns, onComplete, t, signing_service]
   );
 }
 
-async function signCollectionVersion(collection: CollectionVersionSearch, signing_service: string) {
+async function signCollectionVersion(
+  version: boolean,
+  collection: CollectionVersionSearch,
+  signing_service: string
+) {
   const distro_base_path = await getRepositoryBasePath(
     collection.repository?.name || '',
     collection?.repository?.pulp_href
   );
-  return hubAPIPost(hubAPI`/_ui/v1/collection_signing/`, {
+
+  const postData: Record<string, unknown> = {
     distro_base_path,
     collection: collection.collection_version?.name,
     namespace: collection.collection_version?.namespace,
     signing_service,
-    version: collection.collection_version?.version,
-  });
+  };
+
+  if (version) {
+    postData.version = collection.collection_version?.version;
+  }
+
+  return hubAPIPost(hubAPI`/_ui/v1/collection_signing/`, postData);
 }
