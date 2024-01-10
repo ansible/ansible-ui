d4efa93448a3aed0130e587520cbd02c3b1844e4
James Talton <talton@gmail.com>
2024-01-10 14:06:36 -0500

    Fix merge with upsteam changes
    Signed-off-by: James Talton <talton@gmail.com>

--- platform/api/gateway-api-utils.tsx
+++ platform/api/gateway-api-utils.tsx
@@ -1,4 +1,4 @@
-import { apiTag } from '../../frontend/hub/api/formatPath';
+import { apiTag } from '../../frontend/hub/common/api/formatPath';
 
 export function gatewayAPI(strings: TemplateStringsArray, ...values: string[]) {
   const base = process.env.GATEWAY_API_PREFIX ?? '/api/gateway';
