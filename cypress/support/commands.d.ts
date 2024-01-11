8679ba8dcad64e2d6906f3ae735e82bf14768a00
Vishali Sanghishetty <95239214+vishsanghishetty@users.noreply.github.com>
2024-01-11 09:48:29 -0500

    QA Workflow Visualizer Edit and Delete WIP (#1440)

--- cypress/support/commands.d.ts
+++ cypress/support/commands.d.ts
@@ -162,6 +162,14 @@ declare global {
       singleSelectShouldContainOption(selector: string, label: string | RegExp): Chainable<void>;
       selectSingleSelectOption(selector: string, label: string | RegExp): Chainable<void>;
 
+      editNodeInVisualizer(
+        nodeName: string,
+        newNodeType: string,
+        newNodeName?: string
+      ): Chainable<void>;
+
+      removeAllNodesFromVisualizerToolbar(): Chainable<void>;
+      removeNodeInVisualizer(nodeName: string): Chainable<void>;
       multiSelectShouldHaveSelectedOption(
         selector: string,
         label: string | RegExp
@@ -772,6 +780,11 @@ declare global {
       ): Chainable<WorkflowNode>;
 
       createWorkflowJTFailureNodeLink(
+        firstNode: WorkflowNode,
+        secondNode: WorkflowNode
+      ): Chainable<WorkflowNode>;
+
+      createWorkflowJTAlwaysNodeLink(
         firstNode: WorkflowNode,
         secondNode: WorkflowNode
       ): Chainable<WorkflowNode>;
