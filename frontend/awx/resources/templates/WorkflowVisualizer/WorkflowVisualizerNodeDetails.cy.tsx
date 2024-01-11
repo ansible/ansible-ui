0c7d1af3547a69fbab26ab6bb66f435a67ba7d0b
Alex Corey <acorey@redhat.com>
2024-01-11 15:37:02 -0500

    Unsaved changes modal (#1378)

--- frontend/awx/resources/templates/WorkflowVisualizer/WorkflowVisualizerNodeDetails.cy.tsx
+++ frontend/awx/resources/templates/WorkflowVisualizer/WorkflowVisualizerNodeDetails.cy.tsx
@@ -1,157 +1,265 @@
-import { JobTemplate } from '../../../interfaces/JobTemplate';
-import { WorkflowNode, UnifiedJobType } from '../../../interfaces/WorkflowNode';
-import { WorkflowVisualizerNodeDetails } from './WorkflowVisualizerNodeDetails';
-import { ControllerContext, Controller } from '@patternfly/react-topology';
-import { SummaryFieldUnifiedJobTemplate } from '../../../interfaces/summary-fields/summary-fields';
+import { AwxItemsResponse } from '../../../common/AwxItemsResponse';
+import { WorkflowNode } from '../../../interfaces/WorkflowNode';
+import { WorkflowVisualizer } from './WorkflowVisualizer';
 
-describe('WorkflowVisualizerNodeDetails', () => {
-  const mockNode = (unifiedJobTemplate?: Partial<SummaryFieldUnifiedJobTemplate>) =>
-    ({
-      summary_fields: {
-        unified_job_template: {
-          id: 7,
-          unified_job_type: undefined,
-          ...unifiedJobTemplate,
-        },
+describe('WorkflowVisualizer', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/v2/workflow_job_templates/*/workflow_nodes/*',
       },
-    }) as WorkflowNode;
-  const mockContext = {
-    getState: () => ({
-      workflowTemplate: { summary_fields: { user_capabilities: { edit: true } } },
-    }),
-  } as Controller;
-
-  it('should render job template details with prompted values', () => {
-    cy.fixture('jobTemplate.json').then((jobTemplate: JobTemplate) => {
-      jobTemplate.ask_credential_on_launch = true;
-      cy.intercept('GET', '/api/v2/job_templates/*', jobTemplate);
-    });
-
-    cy.intercept({ method: 'GET', url: '/api/v2/credentials/*/' }, { fixture: 'credential.json' });
+      { fixture: 'workflow_nodes.json' }
+    ).as('getWorkflowNodes');
     cy.intercept(
-      { method: 'GET', url: '/api/v2/job_templates/*/instance_groups/' },
-      { fixture: 'instance_groups.json' }
+      { method: 'GET', url: '/api/v2/workflow_job_templates/*' },
+      { fixture: 'workflowJobTemplate.json' }
     );
-    cy.mount(
-      <ControllerContext.Provider value={mockContext}>
-        <WorkflowVisualizerNodeDetails
-          resource={mockNode({ unified_job_type: UnifiedJobType.job })}
-        />
-      </ControllerContext.Provider>
-    );
+    cy.intercept('/api/v2/job_templates/*', { fixture: 'jobTemplate.json' });
+    cy.intercept('/api/v2/job_templates/*/instance_groups', { fixture: 'instance_groups.json' });
+  });
 
-    cy.get('dd[data-cy="name"]').within(() => {
-      cy.get('a').should('have.text', 'JT with Default Cred');
-      cy.get('a').should('have.attr', 'href');
+  it('Should render nodes and labels', () => {
+    cy.mount(<WorkflowVisualizer />);
+    cy.wait('@getWorkflowNodes')
+      .its('response.body.results')
+      .then((nodes: WorkflowNode[]) => {
+        nodes.forEach((node: WorkflowNode) => {
+          cy.get(`[data-id="${node.id}"]`).should('be.visible');
+          cy.get(`[data-id="${node.id}"] text`).should(
+            'have.text',
+            node.summary_fields.unified_job_template.name
+          );
+        });
+      });
+  });
+
+  it('Should show topology control bar and legend', () => {
+    cy.mount(<WorkflowVisualizer />);
+    cy.get('.pf-topology-control-bar').within(() => {
+      cy.get('button').should('have.length', 5);
+      cy.get('button#reset-view').should('be.visible');
+      cy.get('button#zoom-in').should('be.visible');
+      cy.get('button#zoom-out').should('be.visible');
+      cy.get('button#fit-to-screen').should('be.visible');
+      cy.get('button#legend').should('be.visible');
+      cy.get('button#legend').click();
+    });
+    cy.get('[data-cy="workflow-visualizer-legend"]')
+      .should('exist')
+      .within(() => {
+        cy.get('[data-cy="legend-node-types"]').should((description) => {
+          expect(description).to.contain('Node types');
+          expect(description).to.contain('Job Template');
+          expect(description).to.contain('Workflow Template');
+          expect(description).to.contain('Project Sync');
+          expect(description).to.contain('Approval Node');
+          expect(description).to.contain('Inventory Update');
+          expect(description).to.contain('System Job');
+        });
+        cy.get('[data-cy="legend-node-status-types"]').should((description) => {
+          expect(description).to.contain('Node status types');
+          expect(description).to.contain('Failed');
+          expect(description).to.contain('Warning');
+        });
+        cy.get('[data-cy="legend-run-status-types"]').should((description) => {
+          expect(description).to.contain('Run status types');
+          expect(description).to.contain('Run on success');
+          expect(description).to.contain('Run on fail');
+          expect(description).to.contain('Run always');
+        });
+      });
+  });
+
+  it('Should show sidebar details when a node is selected', () => {
+    cy.mount(<WorkflowVisualizer />);
+    cy.get('[data-id="1510"]').click();
+    cy.get('[data-cy="workflow-topology-sidebar"]').should('be.visible');
+  });
+
+  it('Node label kebab should open context menu dropdown', () => {
+    cy.mount(<WorkflowVisualizer />);
+    cy.get('[data-id="1510"] .pf-topology__node__action-icon').click();
+    cy.get('li[data-cy="edit-node"]').should('be.visible');
+    cy.get('li[data-cy="add-link"]').should('be.visible');
+    cy.get('li[data-cy="add-node-and-link"]').should('be.visible');
+    cy.get('li[data-cy="remove-node"]').should('be.visible');
+  });
+
+  it('Edge label kebab should open context menu dropdown', () => {
+    cy.mount(<WorkflowVisualizer />);
+    cy.get('[data-id="1356-1511"]').within(() => {
+      cy.get('[data-cy="edge-context-menu_kebab"]').click({ force: true });
+    });
+    cy.get('li[data-cy="success"]').should('be.visible');
+    cy.get('li[data-cy="always"]').should('be.visible');
+    cy.get('li[data-cy="fail"]').should('be.visible');
+    cy.get('li[data-cy="remove-link"]').should('be.visible');
+  });
+
+  it('Click on edge context menu option to change link type', () => {
+    cy.mount(<WorkflowVisualizer />);
+    cy.get('[data-id="1356-1511"]').within(() => {
+      cy.get('[data-cy="edge-context-menu_kebab"]').click({ force: true });
+    });
+    cy.get('li[data-cy="fail"]').click();
+    cy.get('[data-id="1356-1511"]').within(() => {
+      cy.get('text').should('have.text', 'Run on fail');
     });
   });
 
-  it('should render workflow job template details with prompted values', () => {
-    cy.fixture('workflowJobTemplate.json').then((jobTemplate: JobTemplate) => {
-      jobTemplate.ask_inventory_on_launch = true;
-      cy.intercept('GET', '/api/v2/workflow_job_templates/*', jobTemplate);
-    });
-
-    cy.intercept({ method: 'GET', url: '/api/v2/inventories/*/' }, { fixture: 'inventories.json' });
-
-    cy.mount(
-      <ControllerContext.Provider value={mockContext}>
-        <WorkflowVisualizerNodeDetails
-          resource={mockNode({ unified_job_type: UnifiedJobType.workflow_job })}
-        />
-      </ControllerContext.Provider>
-    );
-
-    cy.get('dd[data-cy="name"]').within(() => {
-      cy.get('a').should('have.text', 'E2E 6GDe');
-      cy.get('a').should('have.attr', 'href');
-    });
+  it('Should show the WorkflowVisualizer toolbar with Add and Cancel buttons', () => {
+    cy.mount(<WorkflowVisualizer />);
+    cy.contains('button:not(:disabled):not(:hidden)', 'Save').should('be.visible');
+    cy.contains('button:not(:disabled):not(:hidden)', 'Add node').should('be.visible');
+    cy.get('button[data-cy="workflow-visualizer-toolbar-close"]').should('be.visible');
+    cy.get('button[data-cy="workflow-visualizer-toolbar-expand-collapse"]').should('be.visible');
+    cy.get('svg[data-cy="workflow-visualizer-toolbar-collapse"]').should('not.exist');
   });
-  it('Should render project details', () => {
-    cy.intercept({ method: 'GET', url: '/api/v2/projects/*/' }, { fixture: 'project.json' });
-    cy.mount(
-      <ControllerContext.Provider value={mockContext}>
-        <WorkflowVisualizerNodeDetails
-          resource={mockNode({ unified_job_type: UnifiedJobType.project_update })}
-        />
-      </ControllerContext.Provider>
-    );
-    cy.get('dd[data-cy="name"]').within(() => {
-      cy.get('a').should('have.text', 'Demo Project @ 10:44:51');
-      cy.get('a').should('have.attr', 'href');
-    });
+
+  it('Should toggle the expand collapse button in the toolbar', () => {
+    cy.mount(<WorkflowVisualizer />);
+    cy.get('button[data-cy="workflow-visualizer-toolbar-expand-collapse"]').click();
+    cy.get('svg[data-cy="workflow-visualizer-toolbar-expand"]').should('not.exist');
+    cy.get('[data-cy="workflow-visualizer-toolbar-collapse"]').should('be.visible');
+    cy.get('button[data-cy="workflow-visualizer-toolbar-expand-collapse"]').click();
+    cy.get('[data-cy="workflow-visualizer-toolbar-expand"]').should('be.visible');
+    cy.get('[data-cy="workflow-visualizer-toolbar-collapse"]').should('not.exist');
   });
-  it('Should render inventory source details', () => {
+
+  it('Should show the visualizer screen', () => {
+    cy.fixture('workflow_nodes.json').then((workflowNodes: AwxItemsResponse<WorkflowNode>) => {
+      workflowNodes.count = 3;
+      cy.intercept(
+        { method: 'GET', url: '/api/v2/workflow_job_templates/*/workflow_nodes/*' },
+        workflowNodes
+      );
+    });
+
+    cy.mount(<WorkflowVisualizer />);
+    cy.get('[data-cy="workflow-visualizer"]').should('be.visible');
+    cy.get('[data-cy="workflow-visualizer-toolbar-total-nodes"]').should(
+      'have.text',
+      'Total nodes 6'
+    );
+  });
+
+  it('Should show Delete all nodes button', () => {
     cy.intercept(
-      { method: 'GET', url: '/api/v2/inventory_sources/*/' },
-      { fixture: 'inventory_source.json' }
+      { method: 'GET', url: '/api/v2/workflow_job_templates/*/workflow_nodes/*' },
+      { fixture: 'workflow_nodes.json' }
     );
-    cy.mount(
-      <ControllerContext.Provider value={mockContext}>
-        <WorkflowVisualizerNodeDetails
-          resource={mockNode({ unified_job_type: UnifiedJobType.inventory_update })}
-        />
-      </ControllerContext.Provider>
+
+    cy.mount(<WorkflowVisualizer />);
+    cy.get('.toggle-kebab')
+      .click()
+      .get('.pf-v5-c-menu__item-text')
+      .contains('Remove all nodes')
+      .should('be.visible');
+    cy.get('.toggle-kebab').click();
+    cy.get('.toggle-kebab')
+      .click()
+      .get('[data-cy="workflow-documentation"]')
+      .contains('Documentation')
+      .should('have.attr', 'href')
+      .should('include', 'html/userguide/workflow_templates.html#ug-wf-editor');
+    cy.get('.toggle-kebab').click();
+    cy.get('.toggle-kebab')
+      .click()
+      .get('.pf-v5-c-menu__item-text')
+      .contains('Launch workflow')
+      .should('be.visible');
+  });
+});
+
+describe('Empty state', () => {
+  it('Should show empty state view', () => {
+    cy.intercept(
+      { method: 'GET', url: '/api/v2/workflow_job_templates/123/workflow_nodes/?*' },
+      {
+        statusCode: 200,
+        body: {
+          count: 0,
+          next: null,
+          previous: null,
+          results: [],
+        },
+      }
     );
-    cy.get('dd[data-cy="name"]').within(() => {
-      cy.get('a').should('have.text', 'Demo Inventory Source');
-      cy.get('a').should('have.attr', 'href');
+    cy.intercept(
+      { method: 'GET', url: '/api/v2/workflow_job_templates/123' },
+      { fixture: 'workflowJobTemplate.json' }
+    );
+    cy.mount(<WorkflowVisualizer />, {
+      path: '/templates/workflow_job_template/:id/visualizer',
+      initialEntries: ['/templates/workflow_job_template/123/visualizer'],
+    });
+    cy.get('h4.pf-v5-c-empty-state__title-text').should(
+      'have.text',
+      'There are currently no nodes in this workflow'
+    );
+    cy.get('div.pf-v5-c-empty-state__actions').within(() => {
+      cy.get('[data-cy="add-node-button"]').should('be.visible');
     });
   });
-
-  it('Should render system job details', () => {
-    cy.mount(
-      <ControllerContext.Provider value={mockContext}>
-        <WorkflowVisualizerNodeDetails
-          resource={mockNode({ unified_job_type: UnifiedJobType.system_job, name: 'System job' })}
-        />
-      </ControllerContext.Provider>
+  it('Should add a node to an empty workflow visualizer', () => {
+    cy.intercept(
+      { method: 'GET', url: '/api/v2/workflow_job_templates/123/workflow_nodes/?*' },
+      {
+        statusCode: 200,
+        body: {
+          count: 0,
+          next: null,
+          previous: null,
+          results: [],
+        },
+      }
     );
-    cy.get('dd[data-cy="name"]').should('have.text', 'System job');
+    cy.intercept(
+      { method: 'GET', url: '/api/v2/workflow_job_templates/123' },
+      { fixture: 'workflowJobTemplate.json' }
+    );
+    cy.intercept(
+      { method: 'GET', url: '/api/v2/job_templates/*' },
+      { fixture: 'jobTemplates.json' }
+    );
+    cy.mount(<WorkflowVisualizer />, {
+      path: '/templates/workflow_job_template/:id/visualizer',
+      initialEntries: ['/templates/workflow_job_template/123/visualizer'],
+    });
+    cy.get('div.pf-v5-c-empty-state__actions').within(() => {
+      cy.get('[data-cy="add-node-button"]').click();
+    });
+    cy.get('button[data-cy="Submit"]').click();
+    cy.get('button[data-cy="wizard-next"]').click();
+    cy.get('g[data-id="1-unsavedNode"]').should('be.visible');
   });
-  it('Should render workflow approval details', () => {
-    cy.mount(
-      <ControllerContext.Provider value={mockContext}>
-        <WorkflowVisualizerNodeDetails
-          resource={mockNode({
-            unified_job_type: UnifiedJobType.workflow_approval,
-            name: 'Workflow approval',
-          })}
-        />
-      </ControllerContext.Provider>
+});
+
+describe('Should show unsaved changes modal', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/v2/workflow_job_templates/*/workflow_nodes/*',
+      },
+      { fixture: 'workflow_nodes.json' }
+    ).as('getWorkflowNodes');
+    cy.intercept(
+      { method: 'GET', url: '/api/v2/workflow_job_templates/*' },
+      { fixture: 'workflowJobTemplate.json' }
     );
-    cy.get('dd[data-cy="name"]').should('have.text', 'Workflow approval');
+    cy.intercept('/api/v2/job_templates/*', { fixture: 'jobTemplate.json' });
+    cy.intercept('/api/v2/job_templates/*/instance_groups', { fixture: 'instance_groups.json' });
   });
-
-  it('Should show action buttons when user can edit workflow', () => {
-    const readOnlyContext = {
-      getState: () => ({
-        workflowTemplate: { summary_fields: { user_capabilities: { edit: true } } },
-      }),
-    } as Controller;
-
-    cy.mount(
-      <ControllerContext.Provider value={readOnlyContext}>
-        <WorkflowVisualizerNodeDetails resource={mockNode()} />
-      </ControllerContext.Provider>
-    );
-    cy.get('button[data-cy="edit-node"]').should('be.visible');
-    cy.get('button[data-cy="remove-node"]').should('be.visible');
-  });
-
-  it('Should hide action buttons when user cannot edit workflow', () => {
-    const readOnlyContext = {
-      getState: () => ({
-        workflowTemplate: { summary_fields: { user_capabilities: { edit: false } } },
-      }),
-    } as Controller;
-
-    cy.mount(
-      <ControllerContext.Provider value={readOnlyContext}>
-        <WorkflowVisualizerNodeDetails resource={mockNode()} />
-      </ControllerContext.Provider>
-    );
-    cy.get('button[data-cy="edit-node"]').should('not.exist');
-    cy.get('button[data-cy="remove-node"]').should('not.exist');
+  it('Click on edge context menu option to change link type', () => {
+    cy.mount(<WorkflowVisualizer />);
+    cy.get('[data-id="1356-1511"]').within(() => {
+      cy.get('[data-cy="edge-context-menu_kebab"]').click({ force: true });
+    });
+    cy.get('li[data-cy="fail"]').click();
+    cy.get('[data-id="1356-1511"]').should('have.text', 'Run on fail');
+    cy.get('button[data-cy="workflow-visualizer-toolbar-close"]').click();
+    cy.get('div[data-cy="visualizer-unsaved-changes-modal"]').should('be.visible');
   });
 });
