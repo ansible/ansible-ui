f8afa26f8157426ecad42f061247fe0a33fd9a6c
Shaiah Emigh-Doyle <64337863+ShaiahWren@users.noreply.github.com>
2024-01-11 12:43:59 -0500

    test edit namespace (#1442)

--- cypress/e2e/hub/namespaces.cy.ts
+++ cypress/e2e/hub/namespaces.cy.ts
@@ -18,9 +18,27 @@ describe('Namespaces', () => {
     cy.get('[data-cy="create-namespace"]').should('be.visible').click();
     cy.url().should('include', Namespaces.urlCreate);
     cy.get('[data-cy="name"]').type(namespaceName);
-    cy.get('[data-cy="company"]').type(Namespaces.company);
+    cy.get('[data-cy="company"]').type('test company');
     cy.get('[data-cy="Submit"]').click();
     cy.url().should('include', `/namespaces/${namespaceName}/details`);
     cy.selectDetailsPageKebabAction('delete-namespace');
+  });
+
+  it('edit a namespace', () => {
+    cy.navigateTo('hub', Namespaces.url);
+    const namespaceName = `test_namespace_${randomString(5, undefined, { isLowercase: true })}`;
+    cy.get('[data-cy="create-namespace"]').should('be.visible').click();
+    cy.url().should('include', Namespaces.urlCreate);
+    cy.get('[data-cy="name"]').type(namespaceName);
+    cy.get('[data-cy="company"]').type('test company');
+    cy.get('[data-cy="Submit"]').click();
+    cy.url().should('include', `/namespaces/${namespaceName}/details`);
+    cy.get('[data-cy="edit-namespace"]').click();
+    cy.get('[data-cy="name"]').should('be.disabled');
+    cy.get('[data-cy="company"]').clear().type('new company');
+    cy.get('[data-cy="description"]').clear().type('new description');
+    cy.get('[data-cy="Submit"]').click();
+    cy.get('[data-cy="company"]').contains(/^new company$/);
+    cy.get('[data-cy="description"]').contains(/^new description$/);
   });
 });
