f0866dcc8700c3b60a7bc66d7643c4952ade6b93
Zita Nemeckova <znemecko@redhat.com>
2024-01-15 20:30:56 +0100

    Add tests to Signature keys

--- cypress/e2e/hub/signature-keys.cy.ts
+++ cypress/e2e/hub/signature-keys.cy.ts
@@ -5,8 +5,27 @@ describe('Signature Keys', () => {
     cy.hubLogin();
   });
 
-  it('it should render the signature keys page', () => {
+  it('should render the signature keys page', () => {
     cy.navigateTo('hub', SignatureKeys.url);
     cy.verifyPageTitle(SignatureKeys.title);
+  });
+  it('no content', () => {
+    cy.contains('No signature keys yet').should('be.visible');
+  });
+  it.skip('should contain correct table headers', () => {
+    cy.navigateTo('hub', SignatureKeys.url);
+    cy.get('[data-cy="name-column-header"]').should('be.visible');
+    cy.get('[data-cy="fingerprint-column-header"]').should('be.visible');
+    cy.get('[data-cy="public-_key-column-header"]').should('be.visible');
+    cy.get('[data-cy="created-column-header"]').should('be.visible');
+  });
+  it.skip('should contain table with row that contains two CopyCells and download button', () => {
+    cy.navigateTo('hub', SignatureKeys.url);
+    // assuming we always have at least one signature key
+    cy.get('[data-cy="row-0"]').within(() => {
+      cy.get('[data-cy="fingerprint-column-cell"]').should('be.visible');
+      cy.get('[data-cy="public-key-column-cell"]').should('be.visible');
+      cy.get('[data-cy="download-key"]').should('be.visible');
+    });
   });
 });
