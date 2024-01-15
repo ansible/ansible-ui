ff798a69a10543218307baaeb763d9a53fee3222
Vidya Nambiar <vnambiar@redhat.com>
2024-01-15 06:48:29 -0500

    formatApiPathforPlatform cypress support file

---
+++ cypress/support/formatApiPathForPlatform.ts
@@ -0,0 +1,14 @@
+/**
+ * This formatApiPathforPlatform file allows access to the gatewayAPI util for building
+ * API endpoints within Cypress.
+ * The main difference from platform/api/gateway-api-utils.tsx is that this file
+ * references a Cypress env variable for GATEWAY_API_PREFIX instead of a
+ * process.env variable
+ */
+
+import { apiTag } from '../../frontend/hub/common/api/formatPath';
+
+export function gatewayAPI(strings: TemplateStringsArray, ...values: string[]) {
+  const base = (Cypress.env('GATEWAY_API_PREFIX') as string) || '/api/gateway/v1';
+  return base + apiTag(strings, ...values);
+}
