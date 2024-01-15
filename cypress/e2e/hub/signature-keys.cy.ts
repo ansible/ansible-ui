e8d8f4842f5b2f9e03a4ddbdaf65efe927fb136c
Zita Nemeckova <znemecko@redhat.com>
2024-01-15 21:29:52 +0100

    Mock API to get signature keys

--- cypress/e2e/hub/signature-keys.cy.ts
+++ cypress/e2e/hub/signature-keys.cy.ts
@@ -1,4 +1,5 @@
 import { SignatureKeys } from './constants';
+import { pulpAPI } from '../../support/formatApiPathForHub';
 
 describe('Signature Keys', () => {
   before(() => {
@@ -9,19 +10,29 @@ describe('Signature Keys', () => {
     cy.navigateTo('hub', SignatureKeys.url);
     cy.verifyPageTitle(SignatureKeys.title);
   });
-  it('no content', () => {
+  it('should show no content when no data', () => {
     cy.contains('No signature keys yet').should('be.visible');
   });
-  it.skip('should contain correct table headers', () => {
+  it('should contain correct table headers', () => {
+    // backend on CI not supporting signing yet so it's not possible to create signature key, mocking instead
+    cy.intercept(
+      { method: 'GET', url: pulpAPI`/signing-services/*` },
+      { fixture: 'hubSignatureKeys.json' }
+    ).as('signatureKeys');
     cy.navigateTo('hub', SignatureKeys.url);
+    cy.wait('@signatureKeys');
     cy.get('[data-cy="name-column-header"]').should('be.visible');
     cy.get('[data-cy="fingerprint-column-header"]').should('be.visible');
-    cy.get('[data-cy="public-_key-column-header"]').should('be.visible');
+    cy.get('[data-cy="public-key-column-header"]').should('be.visible');
     cy.get('[data-cy="created-column-header"]').should('be.visible');
   });
-  it.skip('should contain table with row that contains two CopyCells and download button', () => {
+  it('should contain table with row that contains two CopyCells and download button', () => {
+    cy.intercept(
+      { method: 'GET', url: pulpAPI`/signing-services/*` },
+      { fixture: 'hubSignatureKeys.json' }
+    ).as('signatureKeys');
     cy.navigateTo('hub', SignatureKeys.url);
-    // assuming we always have at least one signature key
+    cy.wait('@signatureKeys');
     cy.get('[data-cy="row-0"]').within(() => {
       cy.get('[data-cy="fingerprint-column-cell"]').should('be.visible');
       cy.get('[data-cy="public-key-column-cell"]').should('be.visible');
