4b0c7d306427e8a0ca777dfd517187217e7fdd4d
Martin Hradil <mhradil@redhat.com>
2024-01-16 05:58:21 +0000

    undo task changes
    Issue: AAH-2994

--- frontend/hub/administration/tasks/Task.tsx
+++ frontend/hub/administration/tasks/Task.tsx
@@ -2,19 +2,10 @@ export interface Task {
   name: string;
   pulp_href: string;
   pulp_created: string;
-  created_by: string;
   started_at: string;
   finished_at: string;
   state: 'completed' | 'failed' | 'running' | 'waiting';
   error?: TaskError;
-  worker: string;
-  logging_cid: string;
-  task_group: string;
-  parent_task: string;
-  child_tasks: string[];
-  progress_reports: [];
-  created_resources: [];
-  reserved_resources_record: string[];
 }
 
 interface TaskError {
@@ -22,7 +13,6 @@ interface TaskError {
   description: string;
   traceback: string;
 }
-
 export interface TaskResponse {
   task: string;
 }
