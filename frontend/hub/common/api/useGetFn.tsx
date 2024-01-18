cc6b6bb3147dd0c2c988b8eb68ade0ded6903abc
Martin Hradil <mhradil@redhat.com>
2024-01-18 21:50:44 +0000

    useGetFn: add AbortSignal, 401 redirect

--- frontend/hub/common/api/useGetFn.tsx
+++ frontend/hub/common/api/useGetFn.tsx
@@ -1,12 +1,28 @@
-import useSWR, { SWRConfiguration } from 'swr';
 import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useSWR, { SWRConfiguration } from 'swr';
+import { RequestError } from '../../../common/crud/RequestError';
+import { useAbortController } from '../../../common/crud/useAbortController';
 
 export function useGetFn<T>(
   key: string,
-  fetcher: () => Promise<T>,
+  fetcher: (signal?: AbortSignal) => Promise<T>,
   swrConfiguration: SWRConfiguration = {}
 ) {
-  const response = useSWR<T>(key, fetcher, { dedupingInterval: 0, ...swrConfiguration });
+  const abortController = useAbortController();
+  const navigate = useNavigate();
+  const response = useSWR<T>(
+    key,
+    (_id, signal?: AbortSignal) =>
+      fetcher(signal ?? abortController.signal).catch((error) => {
+        if (error instanceof RequestError && error.statusCode === 401) {
+          navigate('/login?navigate-back=true');
+        }
+
+        throw error;
+      }),
+    { dedupingInterval: 0, ...swrConfiguration }
+  );
   const refresh = useCallback(() => void response.mutate(), [response]);
 
   let error = response.error as Error;
