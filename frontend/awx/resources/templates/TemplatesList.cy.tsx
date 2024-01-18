17b8d515e8febef7d92bbdda0c19ce801afe3bc2
Maeve Hoffer <dhoffer@redhat.com>
2024-01-18 13:46:15 -0800

    add fitering tests for TemplateList

--- frontend/awx/resources/templates/TemplatesList.cy.tsx
+++ frontend/awx/resources/templates/TemplatesList.cy.tsx
@@ -1,4 +1,7 @@
+import { contains } from 'cypress/types/jquery';
 import { TemplatesList } from './TemplatesList';
+import { useOptions } from '../../../common/crud/useOptions';
+import { ToolbarFilterType } from '../../../../framework';
 
 describe('TemplatesList', () => {
   describe('ErrorList', () => {
@@ -36,5 +39,82 @@ describe('TemplatesList', () => {
       cy.clickTableRowPinnedAction('Demo Job Template', 'launch-template');
       cy.wait('@launchRequest');
     });
+
+    it('Has filters for Name, Description, Created By, and Modified By', () => {
+      cy.mount(<TemplatesList url={'/api/v2/projects/6/*'} />);
+      cy.openToolbarFilterTypeSelect().within(() => {
+        cy.contains(/^Name$/).should('be.visible');
+        cy.contains(/^Description$/).should('be.visible');
+        cy.contains(/^Created by$/).should('be.visible');
+        cy.contains(/^Modified by$/).should('be.visible');
+      });
+    });
+
+    it('Filter templates by name', () => {
+      cy.mount(<TemplatesList url={'/api/v2/projects/6/*'} />);
+      cy.intercept('api/v2/projects/6/*?name__icontains=foo*').as('nameFilterRequest');
+      cy.filterTableByTypeAndText(/^Name$/, 'foo');
+      cy.wait('@nameFilterRequest');
+      cy.clickButton(/^Clear all filters$/);
+    });
+
+    it('Filter templates by description', () => {
+      cy.mount(<TemplatesList url={'/api/v2/projects/6/*'} />);
+      cy.intercept('api/v2/projects/6/*?description__icontains=bar*').as(
+        'descriptionFilterRequest'
+      );
+      cy.filterTableByTypeAndText(/^Description$/, 'bar');
+      cy.wait('@descriptionFilterRequest');
+      cy.clickButton(/^Clear all filters$/);
+    });
+
+    it('Filter templates by created by', () => {
+      cy.mount(<TemplatesList url={'/api/v2/projects/6/*'} />);
+      cy.intercept('api/v2/projects/6/*?created_by__username__icontains=baz*').as(
+        'createdByFilterRequest'
+      );
+      cy.filterTableByTypeAndText(/^Created by$/, 'baz');
+      cy.wait('@createdByFilterRequest');
+      cy.clickButton(/^Clear all filters$/);
+    });
+
+    it('Filter templates by modified by', () => {
+      cy.mount(<TemplatesList url={'/api/v2/projects/6/*'} />);
+      cy.intercept('api/v2/projects/6/*?modified_by__username__icontains=qux*').as(
+        'modifiedByFilterRequest'
+      );
+      cy.filterTableByTypeAndText(/^Modified by$/, 'qux');
+      cy.wait('@modifiedByFilterRequest');
+      cy.clickButton(/^Clear all filters$/);
+    });
+
+    it('Create Template button is disabled if the user does not have permission to create templates', () => {});
+
+    it('Create Template button renders form if user has permission to create', () => {
+      cy.stub(useOptions, 'useOptions').callsFake(() => ({
+        data: {
+          actions: {
+            POST: {
+              name: {
+                type: ToolbarFilterType.Text,
+                required: true,
+                label: 'Name',
+                max_length: 512,
+                help_text: 'Name of this template.',
+                filterable: true,
+              },
+            },
+          },
+        },
+      }));
+      cy.mount(<TemplatesList url={'/api/v2/projects/6/*'} />);
+      cy.contains('.pf-v5-c-dropdown__toggle', 'Create template').should('not.be.disabled');
+    });
+
+    it('Delete Template button renders delete modal', () => {});
+
+    it('Clicking Sort button changes the order of listed templates', () => {});
+
+    it('Pagination button functions as expected', () => {});
   });
 });
