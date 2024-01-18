e84b0c2bac2fef2e065bb286cf13cb79cb20ed6c
MilanPospisil <arkanus@seznam.cz>
2024-01-18 11:32:26 +0100

    Elements and tree structure with visibility

--- frontend/hub/collections/CollectionPage/documentationComponents/CollectionDocumentationTabContent.tsx
+++ frontend/hub/collections/CollectionPage/documentationComponents/CollectionDocumentationTabContent.tsx
@@ -4,50 +4,71 @@ import { CodeBlock, PageSection, Stack, StackItem, Title } from '@patternfly/rea
 import { Table, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
 import { IContents, IContentsOption } from '../../Collection';
 import { PFColorE } from '../../../../../framework';
+import { useState, useEffect } from 'react';
 
 export function CollectionDocumentationTabContent(props: { content: IContents | undefined }) {
   const { t } = useTranslation();
   const { content } = props;
   const splitString = '- name';
+  const [optionsState, setOptionsState] = useState<OptionRecord[]>([]);
 
-  type OptionRecord = { option: IContentsOption; level: number; path_name: string };
-  const options: OptionRecord[] = [];
+  type OptionRecord = {
+    option: IContentsOption;
+    level: number;
+    path_name: string;
+    parent_path: string;
+    visible: boolean;
+    children: boolean;
+  };
 
-  function fillOptions(
-    local_options: IContentsOption[],
-    level: number,
-    global_options: OptionRecord[],
-    path_name: string
-  ) {
-    if (level === undefined) {
-      return;
+  const contentKey = content?.content_type + ' > ' + content?.content_name;
+  useEffect(() => {
+    const options: OptionRecord[] = [];
+
+    function fillOptions(
+      local_options: IContentsOption[],
+      level: number,
+      global_options: OptionRecord[],
+      path_name: string
+    ) {
+      if (level === undefined) {
+        return;
+      }
+
+      if (!local_options) {
+        return;
+      }
+
+      local_options.forEach((option) => {
+        let new_path = path_name;
+        if (new_path.length > 0) {
+          new_path += '/';
+        }
+        new_path += option.name;
+
+        // fill boolean choices if their are missing
+        if (option.type === 'bool' && !(option.choices && option.choices.length > 0)) {
+          option.choices = [];
+          option.choices.push('true');
+          option.choices.push('false');
+        }
+        global_options?.push({
+          option,
+          level,
+          path_name: new_path,
+          parent_path: path_name,
+          visible: true,
+          children: option.suboptions ? true : false,
+        });
+        if (option.suboptions) {
+          fillOptions(option.suboptions, level + 1, global_options, new_path);
+        }
+      });
     }
 
-    if (!local_options) {
-      return;
-    }
-
-    local_options.forEach((option) => {
-      let new_path = path_name;
-      if (new_path.length > 0) {
-        new_path += '/';
-      }
-      new_path += option.name;
-
-      // fill boolean choices if their are missing
-      if (option.type === 'bool' && !(option.choices && option.choices.length > 0)) {
-        option.choices = [];
-        option.choices.push('true');
-        option.choices.push('false');
-      }
-      global_options?.push({ option, level, path_name: new_path });
-      if (option.suboptions) {
-        fillOptions(option.suboptions, level + 1, global_options, new_path);
-      }
-    });
-  }
-
-  fillOptions(content?.doc_strings?.doc?.options || [], 0, options, '');
+    fillOptions(content?.doc_strings?.doc?.options || [], 0, options, '');
+    setOptionsState(options);
+  }, [contentKey]);
 
   return (
     <>
@@ -67,7 +88,7 @@ export function CollectionDocumentationTabContent(props: { content: IContents | 
           </Stack>
         </PageSection>
       )}
-      {options && options.length > 0 && (
+      {optionsState && optionsState.length > 0 && (
         <>
           <PageSection variant="light" style={{ paddingBottom: 0 }}>
             <Title headingLevel="h2">{t('Parameters')}</Title>
@@ -82,43 +103,46 @@ export function CollectionDocumentationTabContent(props: { content: IContents | 
                 </Tr>
               </Thead>
               <Tbody>
-                {options.map((optionRecord) => (
-                  <Tr key={optionRecord.path_name}>
-                    <Td>
-                      <div style={{ marginLeft: `${optionRecord.level * 30}px` }}>
-                        <div>
-                          {optionRecord.path_name !== optionRecord.option.name &&
-                            optionRecord.path_name}
+                {optionsState
+                  .filter((optionState) => optionState.visible)
+                  .map((optionRecord) => (
+                    <Tr key={optionRecord.path_name}>
+                      <Td>
+                        <div style={{ marginLeft: `${optionRecord.level * 30}px` }}>
+                          <div>
+                            {optionRecord.path_name !== optionRecord.option.name &&
+                              optionRecord.path_name}
+                          </div>
+                          <div style={{ fontWeight: 'bold' }}>{optionRecord.option.name}</div>
+                          <small style={{ opacity: 0.7 }}>
+                            {optionRecord.option.type}
+                            {optionRecord.option.elements && ' / ' }
+                            {optionRecord.option.elements}{' '}
+                            {optionRecord.option.required && (
+                              <span style={{ color: PFColorE.Red }}> / {t('Required')}</span>
+                            )}{' '}
+                          </small>
                         </div>
-                        <div style={{ fontWeight: 'bold' }}>{optionRecord.option.name}</div>
-                        <small style={{ opacity: 0.7 }}>
-                          {optionRecord.option.type}
-                          {optionRecord.option.elements && ' / elements = '}{optionRecord.option.elements}{' '}
-                          {optionRecord.option.required && (
-                            <span style={{ color: PFColorE.Red }}> / {t('Required')}</span>
-                          )}{' '}
-                        </small>
-                      </div>
-                    </Td>
-                    <Td>
-                      {optionRecord.option.choices?.map((choice) => {
-                        let style = {};
-                        let title = '';
-                        if (optionRecord.option.default?.toString() === choice.toString()) {
-                          title = t('Default');
-                          style = { color: PFColorE.Blue };
-                        }
+                      </Td>
+                      <Td>
+                        {optionRecord.option.choices?.map((choice) => {
+                          let style = {};
+                          let title = '';
+                          if (optionRecord.option.default?.toString() === choice.toString()) {
+                            title = t('Default');
+                            style = { color: PFColorE.Blue };
+                          }
 
-                        return (
-                          <p title={title} style={style} key={choice}>
-                            {choice}
-                          </p>
-                        );
-                      })}
-                    </Td>
-                    <Td>{optionRecord.option.description}</Td>
-                  </Tr>
-                ))}
+                          return (
+                            <p title={title} style={style} key={choice}>
+                              {choice}
+                            </p>
+                          );
+                        })}
+                      </Td>
+                      <Td>{optionRecord.option.description}</Td>
+                    </Tr>
+                  ))}
               </Tbody>
             </Table>
           </PageSection>
