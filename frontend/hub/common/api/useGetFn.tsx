98bd969285d81e10e581d6b396ac3c89053e21ae
Martin Hradil <mhradil@redhat.com>
2024-01-23 15:52:43 +0100

    Hub: useGetFn (#1458)
    Co-authored-by: Salma Kochay <skochay330@gmail.com>

---
+++ frontend/hub/common/api/useGetFn.tsx
@@ -0,0 +1,39 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useSWR, { SWRConfiguration } from 'swr';
+import { RequestError } from '../../../common/crud/RequestError';
+import { useAbortController } from '../../../common/crud/useAbortController';
+
+export function useGetFn<T>(
+  key: string,
+  fetcher: (signal?: AbortSignal) => Promise<T>,
+  swrConfiguration: SWRConfiguration = {}
+) {
+  const abortController = useAbortController();
+  const navigate = useNavigate();
+  const response = useSWR<T>(
+    key,
+    (_id, signal?: AbortSignal) =>
+      fetcher(signal ?? abortController.signal).catch((error) => {
+        if (error instanceof RequestError && error.statusCode === 401) {
+          navigate('/login?navigate-back=true');
+        }
+
+        throw error;
+      }),
+    { dedupingInterval: 0, ...swrConfiguration }
+  );
+  const refresh = useCallback(() => void response.mutate(), [response]);
+
+  let error = response.error as Error;
+  if (error && !(error instanceof Error)) {
+    error = new Error('Unknown error');
+  }
+
+  return {
+    data: response.data,
+    error: response.isLoading ? undefined : error,
+    refresh,
+    isLoading: response.isLoading,
+  };
+}
