7ee9cbe8e0456dfa03ebc2db0e5a9da5f30a6f78
Vishali Sanghishetty <95239214+vishsanghishetty@users.noreply.github.com>
2024-01-24 13:28:26 -0500

    wraps up external link check

--- cypress/e2e/platform/light-speed.cy.ts
+++ cypress/e2e/platform/light-speed.cy.ts
@@ -1,17 +1,14 @@
 describe('Ansible Lightspeed external Link Check', () => {
   it('should open external link in a new tab', () => {
+    const ansibleLightSpeed_URL = 'https://developers.redhat.com/products/ansible/lightspeed';
     cy.platformLogin();
     cy.contains('a', 'Ansible Lightspeed').click();
     cy.contains('h1', 'Ansible Lightspeed with IBM watsonx Code Assistant').should('be.visible');
     cy.checkAnchorLinks('Get started');
-    cy.intercept('GET', 'https://developers.redhat.com/products/ansible/lightspeed').as(
-      'externalLink'
-    );
-    cy.contains('a', 'Get started').should('have.attr', 'target', '_blank').click();
-    cy.wait('@externalLink').then((interception) => {
-      expect(interception.request.url).to.include(
-        'https://developers.redhat.com/products/ansible/lightspeed'
-      );
+    cy.contains('a', 'Get started').should('have.attr', 'target', '_blank');
+    cy.get('a[target="_blank"]').invoke('attr', 'href').as('externalLink');
+    cy.get('@externalLink').then((url) => {
+      expect(url).to.include(ansibleLightSpeed_URL);
     });
   });
 });
