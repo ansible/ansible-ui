89215a78bef39381b59ca4c0b75b43a18a892777
Keith Grant <keithjgrant@redhat.com>
2024-01-23 17:14:59 -0800

    authenticator map fields

--- platform/access/authenticators/components/steps/AuthenticatorMappingStep.tsx
+++ platform/access/authenticators/components/steps/AuthenticatorMappingStep.tsx
@@ -1,24 +1,52 @@
 import { useState, MouseEvent } from 'react';
 import { Dropdown, DropdownList, DropdownItem, MenuToggle } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
-// import { usePageWizard } from '../../../../../framework/PageWizard/PageWizardProvider';
-// import type { AuthenticatorPlugins } from '../../../../interfaces/AuthenticatorPlugin';
+import styled from 'styled-components';
+import { usePageWizard } from '../../../../../framework/PageWizard/PageWizardProvider';
 import { AuthenticatorMapType } from '../../../../interfaces/AuthenticatorMap';
+import type { AuthenticatorFormValues, AuthenticatorMapValues } from '../AuthenticatorForm';
+import {
+  PageFormGrid,
+  PageFormCheckbox,
+  PageFormSelect,
+  PageFormTextInput,
+} from '../../../../../framework';
+import { PageFormHidden } from '../../../../../framework/PageForm/Utils/PageFormHidden';
 
-export function AuthenticatorMappingStep(/*props: { plugins: AuthenticatorPlugins }*/) {
-  // const { wizardData, setWizardData } = usePageWizard();
-  // const { mappings = [] } = wizardData;
+const Checkbox = styled(PageFormCheckbox)`
+  margin-block-start: 2em;
 
-  const addMapping = () => {};
-  // const addMapping = (value: AuthenticatorMapType) => {
-  //   setWizardData({
-  //     ...wizardData,
-  //     mappings: [...mappings, { type: value }],
-  //   });
-  // };
+  & > input {
+    align-self: center;
+    margin-block-start: -0.5em;
+  }
+
+  & > label {
+    align-self: center;
+  }
+`;
+
+export function AuthenticatorMappingStep() {
+  const { wizardData, setWizardData } = usePageWizard();
+  const { mappings = [] } = wizardData as AuthenticatorFormValues;
+
+  const addMapping = (value: AuthenticatorMapType) => {
+    const map: AuthenticatorMapValues = {
+      map_type: value,
+      // order: mappings.length + 1,
+      revoke: false,
+    };
+    setWizardData({
+      ...wizardData,
+      mappings: [...mappings, map],
+    });
+  };
 
   return (
     <>
+      {mappings.map((map, i) => (
+        <MapFields key={i} index={i} map={map} />
+      ))}
       <AddMappingDropdown onSelect={addMapping} />
     </>
   );
@@ -55,5 +83,111 @@ function AddMappingDropdown(props: { onSelect: (value: AuthenticatorMapType) => 
         <DropdownItem value="team">{t('Team')}</DropdownItem>
       </DropdownList>
     </Dropdown>
+  );
+}
+
+function MapFields(props: { index: number; map: AuthenticatorMapValues }) {
+  const { index, map } = props;
+  const { t } = useTranslation();
+
+  const label = {
+    allow: t('Allow'),
+    organization: t('Organization'),
+    team: t('Team'),
+  }[map.map_type];
+
+  return (
+    <div>
+      {label}
+      <input type="hidden" name={`mappings[${index}].map_type`} value={map.map_type} />
+      <input type="hidden" name={`mappings[${index}].order`} value={index} />
+      <PageFormGrid>
+        <PageFormTextInput
+          id={`mappings-${index}-name`}
+          name={`mappings[${index}].name`}
+          label={t('Name')}
+        />
+        <PageFormSelect
+          id={`mappings-${index}-trigger`}
+          name={`mappings[${index}].trigger`}
+          label={t('Trigger')}
+          options={[
+            {
+              value: 'always',
+              label: t('Always'),
+            },
+            {
+              value: 'never',
+              label: t('Never'),
+            },
+            {
+              value: 'groups',
+              label: t('Groups'),
+            },
+            {
+              value: 'attributes',
+              label: t('Attributes'),
+            },
+          ]}
+          placeholderText={t('Select trigger')}
+          isRequired
+        />
+        <Checkbox name={`mappings[${index}.revoke]`} label={t('Revoke')} />
+        <PageFormHidden watch={`mappings[${index}].trigger`} hidden={(value) => value !== 'groups'}>
+          <PageFormSelect
+            id={`mappings-${index}-groups-conditional`}
+            name={`mappings[${index}].conditional`}
+            label={t('Conditional')}
+            options={[
+              { value: 'or', label: t('or') },
+              { value: 'and', label: t('and') },
+            ]}
+            placeholderText={t('Select conditional')}
+          />
+          <PageFormTextInput
+            id={`mappings-${index}-groups-value`}
+            name={`mappings[${index}].groups-value`}
+            label={t('Groups')}
+          />
+        </PageFormHidden>
+        <PageFormHidden
+          watch={`mappings[${index}].trigger`}
+          hidden={(value) => value !== 'attributes'}
+        >
+          <PageFormSelect
+            id={`mappings-${index}-attributes-conditional`}
+            name={`mappings[${index}].conditional`}
+            label={t('Conditional')}
+            options={[
+              { value: 'or', label: t('or') },
+              { value: 'and', label: t('and') },
+            ]}
+          />
+          <PageFormTextInput
+            id={`mappings-${index}-attributes-criteria`}
+            name={`mappings[${index}].criteria`}
+            label={t('Criteria')}
+          />
+          <PageFormSelect
+            id={`mappings-${index}-attributes-criteria-conditional`}
+            name={`mappings[${index}].criteria-conditional`}
+            label=""
+            options={[
+              { value: 'contains', label: t('contains') },
+              { value: 'matches', label: t('matches') },
+              { value: 'ends_with', label: t('ends with') },
+              { value: 'equals', label: t('equals') },
+              { value: 'is', label: t('in') },
+            ]}
+            placeholderText={t('Select conditional')}
+          />
+          <PageFormTextInput
+            id={`mappings-${index}-attributes-value`}
+            name={`mappings[${index}].criteria-value`}
+            label=""
+          />
+        </PageFormHidden>
+      </PageFormGrid>
+    </div>
   );
 }
