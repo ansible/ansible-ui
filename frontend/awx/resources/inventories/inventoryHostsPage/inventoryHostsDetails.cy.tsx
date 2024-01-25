4118baf7fcc2aff24ea875229d81906bb9981aeb
Pratyush Bhandari <prbhanda@redhat.com>
2024-01-25 11:29:30 -0500

    Add inventory host details component tests

---
+++ frontend/awx/resources/inventories/inventoryHostsPage/inventoryHostsDetails.cy.tsx
@@ -0,0 +1,30 @@
+import { AwxHost } from '../../../interfaces/AwxHost';
+import { InventoryHostsDetailsInner as InventoryHostsDetails } from './inventoryHostsDetails';
+import mockAwxHost from '../../../../../cypress/fixtures/awxHost.json';
+import { DateTime } from 'luxon';
+
+describe('ApplicationDetails', () => {
+  it('Component renders and displays Application', () => {
+    cy.mount(<InventoryHostsDetails host={mockAwxHost as unknown as AwxHost} />);
+  });
+  it('Render application detail fields', () => {
+    cy.mount(<InventoryHostsDetails host={mockAwxHost as unknown as AwxHost} />);
+    cy.get('[data-cy="name"]').should('have.text', 'test');
+    cy.get('[data-cy="code-block-value"]').should('have.text', '---\ntest: test');
+    cy.get('[data-cy="activity"] > .pf-v5-c-description-list__text').find(
+      'a[href="/jobs/command/1/output"]'
+    );
+    cy.get('[data-cy="activity"] > .pf-v5-c-description-list__text').find(
+      'a[href="/jobs/playbook/2/output"]'
+    );
+    cy.get('[data-cy="code-block-value"]');
+    cy.get('[data-cy="created"]').should(
+      'contain.text',
+      DateTime.fromISO((mockAwxHost as unknown as AwxHost).created).toRelative()
+    );
+    cy.get('[data-cy="last-modified"]').should(
+      'contain.text',
+      DateTime.fromISO((mockAwxHost as unknown as AwxHost).modified).toRelative()
+    );
+  });
+});
