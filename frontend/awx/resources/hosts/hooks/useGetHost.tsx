7c8d954f2604e6bf7b52c1360375477332823801
prat98 <112966709+prat98@users.noreply.github.com>
2024-01-26 18:19:43 -0500

    [AAP-8293] Host Details Tab (#1490)
    Co-authored-by: Alex Corey <alex.swansboro@gmail.com>
    Co-authored-by: Alex Corey <acorey@redhat.com>

---
+++ frontend/awx/resources/hosts/hooks/useGetHost.tsx
@@ -0,0 +1,8 @@
+import { useGetItem } from '../../../../common/crud/useGet';
+import { awxAPI } from '../../../common/api/awx-utils';
+import { AwxHost } from '../../../interfaces/AwxHost';
+
+export function useGetHost(host_id: string) {
+  const { data: host, refresh } = useGetItem<AwxHost>(awxAPI`/hosts`, host_id.toString());
+  return { host, refresh };
+}
