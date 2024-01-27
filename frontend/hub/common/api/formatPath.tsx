2e2b3b1ed56c796c0d3c604aaed846ae50a15c3d
Martin Hradil <mhradil@redhat.com>
2024-01-27 23:16:35 +0000

    hubAPI - reject missing trailing slash, double slash
    throws if:
      HUB_API_PREFIX ends with a trailing slash
      prefix-relative url doesn't start with a slash
      url doesn't end in a slash (end of string or before a ?)
      url contains a double slash
      url contains a double ?

--- frontend/hub/common/api/formatPath.tsx
+++ frontend/hub/common/api/formatPath.tsx
@@ -1,6 +1,6 @@
 export function apiTag(strings: TemplateStringsArray, ...values: string[]) {
   if (strings[0]?.[0] !== '/') {
-    throw new Error('Invalid URL');
+    throw new Error('Invalid URL - must start with a slash');
   }
 
   let url = '';
@@ -11,15 +11,34 @@ export function apiTag(strings: TemplateStringsArray, ...values: string[]) {
     }
   });
 
+  if (url.includes('//')) {
+    throw new Error('Invalid URL - must not contain a double slash');
+  }
+
+  if (!url.includes('/?') && !url.endsWith('/')) {
+    throw new Error(
+      url.includes('?')
+        ? 'Invalid URL - missing slash before "?"'
+        : 'Invalid URL - must end with a slash'
+    );
+  }
+
+  if (url.indexOf('?') !== url.lastIndexOf('?')) {
+    throw new Error('Invalid URL - multiple "?"');
+  }
+
   return url;
 }
 
+const base = process.env.HUB_API_PREFIX;
+if (base.endsWith('/')) {
+  throw new Error('Invalid HUB_API_PREFIX - must NOT end with a slash');
+}
+
 export function hubAPI(strings: TemplateStringsArray, ...values: string[]) {
-  const base = process.env.HUB_API_PREFIX;
   return base + apiTag(strings, ...values);
 }
 
 export function pulpAPI(strings: TemplateStringsArray, ...values: string[]) {
-  const base = process.env.HUB_API_PREFIX;
   return base + '/pulp/api/v3' + apiTag(strings, ...values);
 }
