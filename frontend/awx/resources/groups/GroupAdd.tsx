0c5765b6b15a5e8ad7b5a511a5f3c7bab367b8e9
matoval <mat24c@gmail.com>
2024-01-27 11:51:15 -0800

    move add form into separate file for general use

---
+++ frontend/awx/resources/groups/GroupAdd.tsx
@@ -0,0 +1,66 @@
+import { useTranslation } from 'react-i18next';
+import {
+  PageFormDataEditor,
+  PageFormSubmitHandler,
+  PageFormTextInput,
+  usePageNavigate,
+} from '../../../../framework';
+import { useNavigate } from 'react-router-dom';
+import { InventoryGroupCreate } from '../../interfaces/InventoryGroupCreate';
+import { usePostRequest } from '../../../common/crud/usePostRequest';
+import { AwxPageForm } from '../../common/AwxPageForm';
+import { PageFormSection } from '../../../../framework/PageForm/Utils/PageFormSection';
+import { AwxRoute } from '../../main/AwxRoutes';
+import { awxAPI } from '../../common/api/awx-utils';
+import { InventoryGroup } from '../../interfaces/InventoryGroup';
+import { Inventory } from '../../interfaces/Inventory';
+
+export function GroupAdd(props: { inventory: Inventory }) {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const pageNavigate = usePageNavigate();
+  const postRequest = usePostRequest<InventoryGroupCreate, InventoryGroup>();
+
+  const onSubmit: PageFormSubmitHandler<InventoryGroupCreate> = async (groupInput) => {
+    const { name, description, variables } = groupInput;
+    const createGroup: InventoryGroupCreate = {
+      name,
+      description: description ?? '',
+      inventory: props.inventory?.id ?? 0,
+      variables: variables ?? '',
+    };
+    const newGroup = await postRequest(awxAPI`/groups/`, createGroup);
+    pageNavigate(AwxRoute.InventoryGroupDetails, {
+      params: { inventory_type: 'inventory', id: newGroup.inventory, group_id: newGroup.id },
+    });
+  };
+
+  const onCancel = () => navigate(-1);
+  return (
+    <AwxPageForm<InventoryGroupCreate>
+      submitText={t('Save')}
+      onSubmit={onSubmit}
+      cancelText={t('Cancel')}
+      onCancel={onCancel}
+      defaultValue={{
+        name: '',
+        description: '',
+        variables: '---\n',
+      }}
+    >
+      <PageFormTextInput name="name" label={t('Name')} placeholder={t('Enter name')} isRequired />
+      <PageFormTextInput
+        name="description"
+        label={t('Description')}
+        placeholder={t('Enter description')}
+      />
+      <PageFormSection singleColumn>
+        <PageFormDataEditor
+          name="variables"
+          label={t('Variables')}
+          toggleLanguages={['yaml', 'json']}
+        />
+      </PageFormSection>
+    </AwxPageForm>
+  );
+}
---
+++ frontend/awx/resources/inventories/InventoryGroups/GroupPageAdd.tsx
@@ -0,0 +1,54 @@
+import { useTranslation } from 'react-i18next';
+import { LoadingPage, PageHeader, PageLayout, useGetPageUrl } from '../../../../../framework';
+import { AwxRoute } from '../../../main/AwxRoutes';
+import { useParams } from 'react-router-dom';
+import { useGet } from '../../../../common/crud/useGet';
+import { awxAPI } from '../../../common/api/awx-utils';
+import { AwxError } from '../../../common/AwxError';
+import { Inventory } from '../../../interfaces/Inventory';
+import { GroupAdd } from '../../groups/GroupAdd';
+
+export function GroupPageAdd() {
+  const { t } = useTranslation();
+  const params = useParams<{ id: string }>();
+  const {
+    error,
+    data: inventory,
+    refresh,
+  } = useGet<Inventory>(awxAPI`/inventories/${params.id?.toString() ?? ''}`);
+
+  const getPageUrl = useGetPageUrl();
+
+  if (error) return <AwxError error={error} handleRefresh={refresh} />;
+  if (!inventory) return <LoadingPage breadcrumbs tabs />;
+
+  return (
+    <PageLayout>
+      <PageHeader
+        title={t('Create new group')}
+        breadcrumbs={[
+          { label: t('Inventories'), to: getPageUrl(AwxRoute.Inventories) },
+          {
+            label: `${inventory?.name}`,
+            to: getPageUrl(AwxRoute.InventoryDetails, {
+              params: {
+                id: inventory?.id,
+                inventory_type: 'inventory',
+              },
+            }),
+          },
+          {
+            label: t('Groups'),
+            to: getPageUrl(AwxRoute.InventoryGroups, {
+              params: {
+                id: inventory?.id,
+                inventory_type: 'inventory',
+              },
+            }),
+          },
+        ]}
+      />
+      <GroupAdd inventory={inventory} />
+    </PageLayout>
+  );
+}
