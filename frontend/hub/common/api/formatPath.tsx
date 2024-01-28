93cb9ba5476b46f06eb8c1e5f7e3b2a1d3640e19
Martin Hradil <mhradil@redhat.com>
2024-01-28 00:58:38 +0000

    formatPath - better errors

--- frontend/hub/common/api/formatPath.tsx
+++ frontend/hub/common/api/formatPath.tsx
@@ -2,13 +2,13 @@ import { url2keys } from './query-string';
 
 function checkParam(url: string, good: string, bad: string) {
   if (url.includes(`?${bad}=`) || url.includes(`&${bad}=`)) {
-    throw new Error(`Invalid param - use "${good}", not "${bad}"`);
+    throw new Error(`Invalid param - use "${good}", not "${bad}" (url: ${url})`);
   }
 }
 
 export function apiTag(strings: TemplateStringsArray, ...values: string[]) {
   if (strings[0]?.[0] !== '/') {
-    throw new Error('Invalid URL - must start with a slash');
+    throw new Error(`Invalid URL - must start with a slash`);
   }
 
   let url = '';
@@ -20,19 +20,19 @@ export function apiTag(strings: TemplateStringsArray, ...values: string[]) {
   });
 
   if (url.includes('//')) {
-    throw new Error('Invalid URL - must not contain a double slash');
+    throw new Error(`Invalid URL - must not contain a double slash (url: ${url})`);
   }
 
   if (!url.includes('/?') && !url.endsWith('/')) {
     throw new Error(
       url.includes('?')
-        ? 'Invalid URL - missing slash before "?"'
-        : 'Invalid URL - must end with a slash'
+        ? `Invalid URL - missing slash before "?" (url: ${url})`
+        : `Invalid URL - must end with a slash (url: ${url})`
     );
   }
 
   if (url.indexOf('?') !== url.lastIndexOf('?')) {
-    throw new Error('Invalid URL - multiple "?"');
+    throw new Error(`Invalid URL - multiple "?" (url: ${url})`);
   }
 
   if (url.includes('?')) {
@@ -69,7 +69,7 @@ export function apiTag(strings: TemplateStringsArray, ...values: string[]) {
 
 const base = process.env.HUB_API_PREFIX;
 if (base.endsWith('/')) {
-  throw new Error('Invalid HUB_API_PREFIX - must NOT end with a slash');
+  throw new Error(`Invalid HUB_API_PREFIX - must NOT end with a slash`);
 }
 
 export function hubAPI(strings: TemplateStringsArray, ...values: string[]) {
