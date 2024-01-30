c0ff0160af972a21ed31c335cb78c2dd4551cc29
MilanPospisil <arkanus@seznam.cz>
2024-01-30 14:03:05 +0100

    Error handling

--- frontend/hub/namespaces/hooks/useHubNamespaces.tsx
+++ frontend/hub/namespaces/hooks/useHubNamespaces.tsx
@@ -3,11 +3,9 @@ import { hubAPI } from '../../common/api/formatPath';
 import { HubItemsResponse } from '../../common/useHubView';
 import { HubNamespace } from '../HubNamespace';
 
-export function useHubNamespaces(namespaceName : string | null | undefined) {
-
+export function useHubNamespaces(namespaceName: string | null | undefined) {
   let url = '';
-  if (namespaceName)
-  {
+  if (namespaceName) {
     url = hubAPI`/_ui/v1/namespaces/?limit=1&name=${namespaceName}`;
   }
 
