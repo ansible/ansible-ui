c00d7f77aa38a659fa86b86954f64bd8de8c2487
MilanPospisil <arkanus@seznam.cz>
2024-01-31 15:19:57 +0100

    Repositories tab versions action revert (#1525)

--- frontend/hub/administration/repositories/RepositoryPage/RepositoryVersions.tsx
+++ frontend/hub/administration/repositories/RepositoryPage/RepositoryVersions.tsx
@@ -6,37 +6,19 @@ import { pulpAPI } from '../../../common/api/formatPath';
 import { useHubView } from '../../../common/useHubView';
 import { HubRoute } from '../../../main/HubRoutes';
 import { RepositoryVersion } from '../Repository';
-import { useVersionsActions } from '../hooks/useRepositoryActions';
+import { useHubBulkConfirmation } from '../../../common/useHubBulkConfirmation';
+import { useCallback } from 'react';
+import { parsePulpIDFromURL } from '../../../common/api/hub-api-utils';
+import { PageActionSelection } from '../../../../../framework';
+import { PageActionType } from '../../../../../framework';
+import { IPageAction } from '../../../../../framework';
+import { waitForTask } from '../../../common/api/hub-api-utils';
+import { postRequest } from '../../../../common/crud/Data';
+import { Repository } from '../Repository';
 
 export function RepositoryVersions() {
   const { t } = useTranslation();
-  const getPageUrl = useGetPageUrl();
-  const params = useParams<{ id: string; version: string }>();
-  const { repo_id } = useOutletContext<{ repo_id: string }>();
-  const rowActions = useVersionsActions();
-  const tableColumns = useMemo<ITableColumn<RepositoryVersion>[]>(
-    () => [
-      {
-        header: t('Version number'),
-        cell: (repository) => (
-          <TextCell
-            text={repository?.number?.toString()}
-            to={getPageUrl(HubRoute.RepositoryVersionPage, {
-              params: { id: params.id, version: repository?.number?.toString() },
-            })}
-          />
-        ),
-        sort: 'number',
-      },
-      {
-        header: t('Created date'),
-        type: 'datetime',
-        value: (repository: RepositoryVersion) => repository.pulp_created,
-        sort: 'pulp_created',
-      },
-    ],
-    [t, getPageUrl, params.id]
-  );
+  const { repo_id, repository } = useOutletContext<{ repo_id: string; repository: Repository }>();
 
   const view = useHubView<RepositoryVersion>({
     url: pulpAPI`/repositories/ansible/ansible/${repo_id}/versions/`,
@@ -45,8 +27,15 @@ export function RepositoryVersions() {
       offset: '0',
       limit: '10',
     },
-    defaultSort: 'pulp_created',
+    defaultSort: '-number',
   });
+
+  const rowActions = useVersionsActions(
+    view.unselectItemsAndRefresh,
+    repository.latest_version_href
+  );
+
+  const tableColumns = useRepositoryVersionColumns(repository.latest_version_href);
 
   return (
     <PageTable<RepositoryVersion>
@@ -63,4 +52,105 @@ export function RepositoryVersions() {
       defaultSubtitle={t('Collection')}
     />
   );
+}
+
+export function useRepositoryVersionColumns(latest_href?: string) {
+  const getPageUrl = useGetPageUrl();
+  const params = useParams<{ id: string; version: string }>();
+  const { t } = useTranslation();
+
+  return useMemo<ITableColumn<RepositoryVersion>[]>(
+    () => [
+      {
+        header: t('Version number'),
+        cell: (repository) => (
+          <TextCell
+            text={
+              repository?.number?.toString() +
+              (latest_href === repository?.pulp_href ? ' ' + t('(latest)') : '')
+            }
+            to={getPageUrl(HubRoute.RepositoryVersionPage, {
+              params: { id: params.id, version: repository?.number?.toString() },
+            })}
+          />
+        ),
+        sort: 'number',
+      },
+      {
+        header: t('Created date'),
+        type: 'datetime',
+        value: (repository: RepositoryVersion) => repository.pulp_created,
+        sort: 'pulp_created',
+      },
+    ],
+    [t, getPageUrl, params.id, latest_href]
+  );
+}
+
+export function useRevertToVersion(onComplete?: (items: RepositoryVersion[]) => void) {
+  const { t } = useTranslation();
+  const confirmationColumns = useRepositoryVersionColumns();
+  const actionColumns = useMemo(() => [confirmationColumns[0]], [confirmationColumns]);
+  const bulkAction = useHubBulkConfirmation<RepositoryVersion>();
+
+  return useCallback(
+    (items: RepositoryVersion[]) => {
+      const confirmText = t('Yes, I confirm that I want to revert to this repository version.');
+
+      const title = t('Rever to repository version.');
+
+      const actionButtonText = t('Revert to repository version');
+      bulkAction({
+        title,
+        confirmText,
+        actionButtonText,
+        items: items,
+        keyFn: (item) => item.number,
+        isDanger: true,
+        confirmationColumns,
+        actionColumns,
+        onComplete,
+        actionFn: (item: RepositoryVersion) => {
+          return revertToVersion(item);
+        },
+      });
+    },
+    [actionColumns, bulkAction, confirmationColumns, onComplete, t]
+  );
+}
+
+async function revertToVersion(repositoryVersion: RepositoryVersion) {
+  const res: { task: string } = await postRequest(
+    pulpAPI`/repositories/ansible/ansible/${
+      parsePulpIDFromURL(repositoryVersion.repository) || ''
+    }/modify/`,
+    {
+      base_version: repositoryVersion.pulp_href,
+    }
+  );
+
+  return await waitForTask(parsePulpIDFromURL(res.task));
+}
+
+function useVersionsActions(callback?: (items: RepositoryVersion[]) => void, latest_href?: string) {
+  const { t } = useTranslation();
+  const revert = useRevertToVersion(callback);
+
+  const actions = useMemo<IPageAction<RepositoryVersion>[]>(
+    () => [
+      {
+        label: t('Revert to this version'),
+        onClick: (item) => {
+          revert([item]);
+        },
+        selection: PageActionSelection.Single,
+        type: PageActionType.Button,
+        isDisabled: (item) =>
+          latest_href === item.pulp_href ? t('Already at the highest version') : '',
+      },
+    ],
+    [t, revert, latest_href]
+  );
+
+  return actions;
 }
