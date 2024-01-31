4a096f99ee6e933a9e707230f9baa601b1953a30
Marliana Lara <mlara@redhat.com>
2024-01-31 14:52:07 -0500

    Handle all job types in workflow visualizer node wizard  (#1544)

--- frontend/awx/resources/templates/WorkflowVisualizer/components/Sidebar.tsx
+++ frontend/awx/resources/templates/WorkflowVisualizer/components/Sidebar.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
 import { TopologySideBar as PFTopologySideBar } from '@patternfly/react-topology';
 import { useViewOptions } from '../ViewOptionsProvider';
 import { WorkflowVisualizerNodeDetails } from '../WorkflowVisualizerNodeDetails';
-import { NodeFormInputs } from './NodeFormInputs';
+import { NodeAddWizard } from '../wizard/NodeAddWizard';
+import { NodeEditWizard } from '../wizard/NodeEditWizard';
 import { useSelectedNode } from '../hooks';
 
 const TopologySideBar = styled(PFTopologySideBar)`
@@ -11,19 +13,25 @@ const TopologySideBar = styled(PFTopologySideBar)`
 `;
 
 export function Sidebar() {
+  const { t } = useTranslation();
   const selectedNode = useSelectedNode();
   const { sidebarMode } = useViewOptions();
 
   if (!sidebarMode) return null;
 
   const nodeSidebar = {
-    add: <NodeFormInputs node={undefined} />,
-    edit: <NodeFormInputs node={selectedNode} />,
+    add: <NodeAddWizard />,
+    edit: selectedNode && <NodeEditWizard node={selectedNode} />,
     view: selectedNode && <WorkflowVisualizerNodeDetails node={selectedNode} />,
   };
 
   return (
-    <TopologySideBar data-cy="workflow-topology-sidebar" resizable show>
+    <TopologySideBar
+      aria-label={t('Topology sidebar')}
+      data-cy="workflow-topology-sidebar"
+      resizable
+      show
+    >
       {nodeSidebar[sidebarMode]}
     </TopologySideBar>
   );
