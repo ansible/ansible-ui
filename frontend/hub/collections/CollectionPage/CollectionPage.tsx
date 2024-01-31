ca8663b7a0892159543b734cbccc8ad62475cfc1
MilanPospisil <arkanus@seznam.cz>
2024-01-31 16:20:17 +0100

    fix collection names and approvals links (#1554)

--- frontend/hub/collections/CollectionPage/CollectionPage.tsx
+++ frontend/hub/collections/CollectionPage/CollectionPage.tsx
@@ -138,7 +138,9 @@ export function CollectionPage() {
   return (
     <PageLayout>
       <PageHeader
-        title={collection?.collection_version?.name}
+        title={
+          collection?.collection_version?.namespace + '.' + collection?.collection_version?.name
+        }
         breadcrumbs={[
           { label: t('Collections'), to: getPageUrl(HubRoute.Collections) },
           { label: collection?.collection_version?.name },
