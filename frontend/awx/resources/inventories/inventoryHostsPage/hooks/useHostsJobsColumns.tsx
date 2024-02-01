0e8267b1b2472093f223c23f894dd8c0f25a2306
Pratyush Bhandari <prbhanda@redhat.com>
2024-02-01 17:28:50 -0500

    Add expanded columns to host jobs list

--- frontend/awx/resources/inventories/inventoryHostsPage/hooks/useHostsJobsColumns.tsx
+++ frontend/awx/resources/inventories/inventoryHostsPage/hooks/useHostsJobsColumns.tsx
@@ -1,8 +1,19 @@
 import { useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { usePageNavigate, ITableColumn, DateTimeCell } from '../../../../../../framework';
-import { useNameColumn } from '../../../../../common/columns';
+import {
+  useExecutionEnvColumn,
+  useInventoryNameColumn,
+  useNameColumn,
+  useProjectNameColumn,
+} from '../../../../../common/columns';
 import { StatusCell } from '../../../../../common/Status';
+import {
+  useJobLaunchedByColumn,
+  useJobSliceColumn,
+  useJobTemplateColumn,
+  useSourceWorkflowColumn,
+} from '../../../../common/JobColumns';
 import { UnifiedJob } from '../../../../interfaces/UnifiedJob';
 import { AwxRoute } from '../../../../main/AwxRoutes';
 
@@ -70,9 +81,41 @@ export function useHostsJobsColumns(options?: { disableSort?: boolean; disableLi
     [t]
   );
 
-  const tableColumns = useMemo<ITableColumn<UnifiedJob>[]>(
-    () => [nameColumn, statusColumn, startTimeColumn, finishTimeColumn],
-    [nameColumn, statusColumn, startTimeColumn, finishTimeColumn]
-  );
+  const launchedByColumn = useJobLaunchedByColumn();
+  const inventoryColumn = useInventoryNameColumn(AwxRoute.InventoryDetails);
+  const jobSliceColumn = useJobSliceColumn();
+  const jobTemplateColumn = useJobTemplateColumn();
+  const sourceWorkflowColumn = useSourceWorkflowColumn();
+  const projectNameColumn = useProjectNameColumn(AwxRoute.ProjectDetails);
+  const executionEnvColumn = useExecutionEnvColumn(AwxRoute.ExecutionEnvironmentDetails);
+
+  const tableColumns = useMemo<ITableColumn<UnifiedJob>[]>(() => {
+    const displayColumns = [
+      nameColumn,
+      statusColumn,
+      startTimeColumn,
+      finishTimeColumn,
+      launchedByColumn,
+      jobTemplateColumn,
+      sourceWorkflowColumn,
+      inventoryColumn,
+      projectNameColumn,
+      executionEnvColumn,
+      jobSliceColumn,
+    ];
+    return displayColumns;
+  }, [
+    nameColumn,
+    statusColumn,
+    startTimeColumn,
+    finishTimeColumn,
+    launchedByColumn,
+    jobTemplateColumn,
+    sourceWorkflowColumn,
+    inventoryColumn,
+    projectNameColumn,
+    executionEnvColumn,
+    jobSliceColumn,
+  ]);
   return tableColumns;
 }
