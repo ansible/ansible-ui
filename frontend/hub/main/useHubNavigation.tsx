4035514ced5253b713dda0e5f0127b962eb8608d
MilanPospisil <arkanus@seznam.cz>
2024-02-06 13:20:55 +0100

    Back url

--- frontend/hub/main/useHubNavigation.tsx
+++ frontend/hub/main/useHubNavigation.tsx
@@ -286,7 +286,7 @@ export function useHubNavigation() {
                   element: <RepositoryAccess />,
                 },
                 {
-                  path: 'collection-version-remove',
+                  path: 'collection-version',
                   id: HubRoute.RepositoryCollectionVersion,
                   element: <RepositoryCollectionVersion />,
                 },
