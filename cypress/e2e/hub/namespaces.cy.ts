b370b9f4007070b40f58126de090a57b447d1efb
Shaiah Emigh-Doyle <64337863+ShaiahWren@users.noreply.github.com>
2024-02-06 12:47:13 -0500

    [AAH-2904] shaiah test collections tab (#1509)

--- cypress/e2e/hub/namespaces.cy.ts
+++ cypress/e2e/hub/namespaces.cy.ts
@@ -75,6 +75,28 @@ describe('Namespaces', () => {
     cy.clickButton(/^Delete namespaces$/);
   });
 
+  it('should show collections tab', () => {
+    cy.navigateTo('hub', Namespaces.url);
+    const namespaceName = `test_namespace_${randomString(5, undefined, { isLowercase: true })}`;
+    cy.get('[data-cy="create-namespace"]').should('be.visible').click();
+    cy.url().should('include', Namespaces.urlCreate);
+    cy.get('[data-cy="name"]').type(namespaceName);
+    cy.get('[data-cy="description"]').type('test description');
+    cy.get('[data-cy="company"]').type('test company');
+    cy.get('[data-cy="Submit"]').click();
+    cy.url().should('include', `/namespaces/${namespaceName}/details`);
+    cy.get('[data-cy="collections-tab"]').should('contain', 'Collections');
+    cy.get('[data-cy="collections-tab"]').click();
+    cy.get('[data-cy="empty-state-title"]').should('contain', 'No collections yet');
+    cy.get('[data-cy="upload-collection"]').should('contain', 'Upload collection');
+
+    // Delete the edited namespace
+    cy.get('[data-cy="actions-dropdown"]').click();
+    cy.get('[data-cy="delete-namespace"]').click();
+    cy.get('#confirm').click();
+    cy.clickButton(/^Delete namespaces$/);
+  });
+
   it('edit a namespace', () => {
     cy.navigateTo('hub', Namespaces.url);
     const namespaceName = `test_namespace_${randomString(5, undefined, { isLowercase: true })}`;
@@ -113,13 +135,13 @@ describe('Namespaces', () => {
     cy.contains(nameSpaceName).should('be.visible');
     cy.get('[data-cy="table-view"]').click();
     cy.contains(nameSpaceName).should('be.visible');
-    cy.get('#select-all').click();
-    cy.clickToolbarKebabAction('delete-selected-namespaces');
+    cy.get('[href*="/namespaces/test_pagination_namespace_"]').click();
+
+    // Delete the edited namespace
+    cy.get('[data-cy="actions-dropdown"]').click();
+    cy.get('[data-cy="delete-namespace"]').click();
     cy.get('#confirm').click();
     cy.clickButton(/^Delete namespaces$/);
-    cy.contains(/^Success$/);
-    cy.clickButton(/^Close$/);
-    cy.clickButton(/^Clear all filters$/);
   });
 
   it('user can view import logs', () => {
