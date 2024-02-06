590fee86bd4033d637c49582734930a0292fefae
Vidya Nambiar <vnambiar@redhat.com>
2024-02-06 15:54:24 -0500

    re-enable team details test

--- platform/access/teams/components/PlatformTeamDetails.cy.tsx
+++ platform/access/teams/components/PlatformTeamDetails.cy.tsx
@@ -1,12 +1,12 @@
+import { DateTime } from 'luxon';
 import mockPlatformTeams from '../../../../cypress/fixtures/platformTeams.json';
-import { formatDateString } from '../../../../framework/utils/formatDateString';
 import { gatewayV1API } from '../../../api/gateway-api-utils';
 import { PlatformTeamDetails } from './PlatformTeamDetails';
 
 const mockPlatformTeam = mockPlatformTeams.results[0];
 
 describe('PlatformTeamDetails', () => {
-  it.skip('Component renders and displays team details', () => {
+  it('Component renders and displays team details', () => {
     cy.intercept({ method: 'GET', path: gatewayV1API`/teams/*` }, { body: mockPlatformTeam });
     cy.mount(<PlatformTeamDetails />);
     cy.get('[data-cy="name"]').should('have.text', mockPlatformTeam.name);
@@ -15,14 +15,17 @@ describe('PlatformTeamDetails', () => {
       'contain',
       mockPlatformTeam.summary_fields.organization.name
     );
-    cy.get('[data-cy="created"]').should('contain', formatDateString(mockPlatformTeam.created_on));
+    cy.get('[data-cy="created"]').should(
+      'contain',
+      DateTime.fromISO(mockPlatformTeam.created_on).toRelative()
+    );
     cy.get('[data-cy="created"]').should(
       'contain',
       mockPlatformTeam.summary_fields.created_by.username
     );
     cy.get('[data-cy="modified"]').should(
       'contain',
-      formatDateString(mockPlatformTeam.modified_on)
+      DateTime.fromISO(mockPlatformTeam.modified_on).toRelative()
     );
     cy.get('[data-cy="modified"]').should(
       'contain',
