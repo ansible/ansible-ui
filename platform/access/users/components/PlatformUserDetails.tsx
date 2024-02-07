193035b94978a48a962e50156c55c0ff2678771c
Vidya Nambiar <vnambiar@redhat.com>
2024-02-07 15:28:32 -0500

    Assigning organizations while creating/editing users

--- platform/access/users/components/PlatformUserDetails.tsx
+++ platform/access/users/components/PlatformUserDetails.tsx
@@ -1,14 +1,48 @@
 import { useParams } from 'react-router-dom';
-import { LoadingPage, PageDetailsFromColumns } from '../../../../framework';
-import { useGetItem } from '../../../../frontend/common/crud/useGet';
+import {
+  LabelsCell,
+  LoadingPage,
+  PageDetail,
+  PageDetails,
+  PageDetailsFromColumns,
+  useGetPageUrl,
+} from '../../../../framework';
+import { useGet, useGetItem } from '../../../../frontend/common/crud/useGet';
 import { gatewayV1API } from '../../../api/gateway-api-utils';
 import { PlatformUser } from '../../../interfaces/PlatformUser';
 import { useUsersColumns } from '../hooks/useUserColumns';
+import { PlatformItemsResponse } from '../../../interfaces/PlatformItemsResponse';
+import { PlatformOrganization } from '../../../interfaces/PlatformOrganization';
+import { useTranslation } from 'react-i18next';
+import { PlatformRoute } from '../../../main/PlatformRoutes';
 
 export function PlatformUserDetails() {
   const params = useParams<{ id: string }>();
+  const { t } = useTranslation();
+  const getPageUrl = useGetPageUrl();
   const columns = useUsersColumns();
   const { data: user, isLoading } = useGetItem<PlatformUser>(gatewayV1API`/users`, params.id);
+  const { data: organizationsData } = useGet<PlatformItemsResponse<PlatformOrganization>>(
+    gatewayV1API`/users/${params.id ?? ''}/organizations`
+  );
   if (isLoading) return <LoadingPage />;
-  return <PageDetailsFromColumns columns={columns} item={user} />;
+
+  return (
+    <PageDetails>
+      <PageDetailsFromColumns columns={columns} item={user} />
+      {organizationsData?.results?.length && (
+        <PageDetail label={t('Organization', { count: organizationsData.count })}>
+          <LabelsCell
+            numLabels={3}
+            labelsWithLinks={organizationsData?.results.map((org) => ({
+              name: org.name,
+              link: getPageUrl(PlatformRoute.OrganizationDetails, {
+                params: { id: org.id },
+              }),
+            }))}
+          />
+        </PageDetail>
+      )}
+    </PageDetails>
+  );
 }
