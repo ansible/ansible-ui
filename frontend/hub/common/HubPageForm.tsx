2b5852415ef7aa771483955d47a77fb7bd3c393c
Jiří Jeřábek (Jiri Jerabek) <Jerabekjirka@email.cz>
2024-02-07 15:24:28 +0100

    [Hub > Execution Environment] Fix small EE issues (#1555)

--- frontend/hub/common/HubPageForm.tsx
+++ frontend/hub/common/HubPageForm.tsx
@@ -2,5 +2,5 @@ import { PageForm, PageFormProps } from '../../../framework';
 import { hubErrorAdapter } from './adapters/hubErrorAdapter';
 
 export function HubPageForm<T extends object>(props: PageFormProps<T>) {
-  return <PageForm<T> {...props} errorAdapter={hubErrorAdapter} />;
+  return <PageForm<T> {...props} errorAdapter={props.errorAdapter ?? hubErrorAdapter} />;
 }
