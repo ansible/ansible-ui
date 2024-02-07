159340c033657973bb0a9b98f0a0ae1d1c2bec54
Keith Grant <keithjgrant@redhat.com>
2024-02-07 13:21:09 -0800

    extract CreateAuthenticator out of AuthenticatorForm

--- platform/routes/useGetPlatformAuthenticatorsRoutes.tsx
+++ platform/routes/useGetPlatformAuthenticatorsRoutes.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { PageNavigationItem, PageNotImplemented } from '../../framework';
 import { AuthenticatorPage } from '../access/authenticators/AuthenticatorPage/AuthenticatorPage';
 import { PlatformAuthenticatorDetails } from '../access/authenticators/AuthenticatorPage/PlatformAuthenticatorDetails';
-import { CreateAuthenticator } from '../access/authenticators/components/AuthenticatorForm';
+import { CreateAuthenticator } from '../access/authenticators/CreateAuthenticator';
 import { AuthenticatorsList } from '../access/authenticators/components/AuthenticatorsList';
 import { PlatformRoute } from '../main/PlatformRoutes';
 
