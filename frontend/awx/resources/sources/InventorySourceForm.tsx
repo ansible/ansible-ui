edb8ff8b4a64d6175f1f3dc7905ace33374d542d
resoluteCoder <resolutecoder@gmail.com>
2024-02-08 10:43:06 -0600

    fixed inventory source bug

--- frontend/awx/resources/sources/InventorySourceForm.tsx
+++ frontend/awx/resources/sources/InventorySourceForm.tsx
@@ -1,4 +1,4 @@
-import { FieldValues } from 'react-hook-form';
+import { FieldValues, useFormContext, useWatch } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from 'react-router-dom';
 import {
@@ -16,7 +16,7 @@ import { AwxPageForm } from '../../common/AwxPageForm';
 import { awxAPI } from '../../common/api/awx-utils';
 import { ActionsResponse, OptionsResponse } from '../../interfaces/OptionsResponse';
 import { AwxRoute } from '../../main/AwxRoutes';
-import { ProjectSubForm } from './sourceSubForms/ProjectSubForm';
+import { InventorySourceSubForm } from './InventorySourceSubForm';
 import {
   InventorySource,
   InventorySourceCreate,
@@ -24,6 +24,7 @@ import {
 } from '../../interfaces/InventorySource';
 import { useGet } from '../../../common/crud/useGet';
 import { Inventory } from '../../interfaces/Inventory';
+import { useEffect } from 'react';
 
 export interface SourceFields extends FieldValues {
   project: Omit<InventorySource, 'source'> & {
@@ -51,14 +52,14 @@ export function CreateInventorySource() {
   const postRequest = usePostRequest<InventorySourceCreate, InventorySource>();
   const params = useParams<{ id: string }>();
   const { data: inventory } = useGet<Inventory>(
-    awxAPI`/inventories/${params.id?.toString() ?? ''}`
+    awxAPI`/inventories/${params.id?.toString() ?? ''}/`
   );
 
   const onSubmit: PageFormSubmitHandler<InventorySourceForm> = async (values) => {
     const formValues: InventorySourceCreate = {
       ...values,
-      credential: values.credential.id,
-      source_path: values.source_path.name,
+      credential: values?.credentialIdPath,
+      source_path: values?.source_path?.name,
       inventory: parseInt(params.id ?? ''),
       source_project: values?.source_project?.id,
     };
@@ -114,6 +115,11 @@ export function CreateInventorySource() {
 function InventorySourceInputs() {
   const { t } = useTranslation();
   const { data } = useOptions<OptionsResponse<ActionsResponse>>(awxAPI`/inventory_sources/`);
+  const source = useWatch<InventorySourceForm>({
+    name: 'source',
+  });
+  const formContext = useFormContext();
+
   // remove the filter when mockups/functionality for file and constructed source types are ready
   const scmTypeOptions = data?.actions?.GET?.source?.choices?.filter(([value, label]) => {
     const ignoredSourceTypes = ['file', 'constructed'];
@@ -125,6 +131,10 @@ function InventorySourceInputs() {
     }
   });
 
+  useEffect(() => {
+    formContext.clearErrors();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [source]);
   return (
     <>
       <PageFormTextInput name="name" label={t('Name')} placeholder={t('Enter name')} isRequired />
@@ -148,7 +158,7 @@ function InventorySourceInputs() {
         }
         placeholderText={t('Select a source')}
       />
-      <ProjectSubForm />
+      <InventorySourceSubForm />
     </>
   );
 }
