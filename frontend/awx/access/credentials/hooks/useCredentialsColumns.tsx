276b44104665e2859237df3420e8af3f3253c3e9
Pratyush Bhandari <prbhanda@redhat.com>
2024-02-08 09:50:23 -0500

    Pull all credtype results for cred type map

--- frontend/awx/access/credentials/hooks/useCredentialsColumns.tsx
+++ frontend/awx/access/credentials/hooks/useCredentialsColumns.tsx
@@ -14,6 +14,7 @@ import { awxAPI } from '../../../common/api/awx-utils';
 import { Credential } from '../../../interfaces/Credential';
 import { CredentialType } from '../../../interfaces/CredentialType';
 import { AwxRoute } from '../../../main/AwxRoutes';
+import { useAwxGetAllPages } from '../../../common/useAwxGetAllPages';
 
 export function useCredentialsColumns(options?: { disableSort?: boolean; disableLinks?: boolean }) {
   const { t } = useTranslation();
@@ -28,19 +29,19 @@ export function useCredentialsColumns(options?: { disableSort?: boolean; disable
   const descriptionColumn = useDescriptionColumn();
   const createdColumn = useCreatedColumn(options);
   const modifiedColumn = useModifiedColumn(options);
-  const itemsResponse = useGet<AwxItemsResponse<CredentialType>>(
-    awxAPI`/credential_types/`.concat(`?page=1&page_size=200`)
-  );
+
+  const { items: allCredTypes } = useAwxGetAllPages<CredentialType>(awxAPI`/credential_types/`);
+
   const credentialTypesMap: { [id: number]: string } = useMemo(() => {
-    if (itemsResponse?.data?.results) {
-      return itemsResponse.data.results.reduce((acc, credentialType) => {
+    if (allCredTypes) {
+      return allCredTypes.reduce((acc, credentialType) => {
         const { id, name } = credentialType;
         return { ...acc, [id]: name };
       });
     } else {
       return {};
     }
-  }, [itemsResponse?.data?.results]);
+  }, [allCredTypes]);
   const tableColumns = useMemo<ITableColumn<Credential>[]>(
     () => [
       idColumn,
