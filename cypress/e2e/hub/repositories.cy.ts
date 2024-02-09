d9aab7cabe8c9e81a29fe98299222405d842c022
MilanPospisil <arkanus@seznam.cz>
2024-02-09 11:33:06 +0100

    WIP

--- cypress/e2e/hub/repositories.cy.ts
+++ cypress/e2e/hub/repositories.cy.ts
@@ -1,7 +1,7 @@
 import { Repositories } from './constants';
 import { randomString } from '../../../framework/utils/random-string';
 
-describe('Repositories', () => {
+describe.skip('Repositories', () => {
   before(() => {
     cy.hubLogin();
   });
@@ -21,5 +21,57 @@ describe('Repositories', () => {
     cy.get('[data-cy="copy-cli-configuration"]').click();
     cy.get('[data-cy="alert-toaster"]').should('be.visible');
     cy.galaxykit('-i repository delete ' + repositoryName);
+  });
+});
+
+describe('Repositories Remove collection', () => {
+  let collections : string[];
+  let namespace: string;
+  let repository: string;
+  let numCollections : number;
+
+  afterEach( () => {
+    /*collections?.forEach( (collection) => {
+      cy.galaxykit('-i collection delete ', namespace, collection);
+    });
+
+    cy.galaxykit('task wait all');
+    cy.galaxykit('-i repository remove', repository);
+    cy.galaxykit('-i namespace remove', namespace);*/
+  });
+
+  it('it should add and then remove the collections', () => {    
+    numCollections = 3;
+    collections = [];
+    for (let i = 0; i < numCollections; i++)
+    {
+      collections.push('hub_e2e_collection_' + randomString(5));
+    }
+
+    namespace = 'hub_e2e_appr_namespace' + randomString(5).toLowerCase();
+    repository = 'hub_e2e_appr_repository' + randomString(5);    
+    cy.hubLogin();
+
+    cy.galaxykit('repository create', repository);
+    cy.galaxykit('namespace create', namespace);
+    cy.galaxykit('task wait all');
+
+    /*collections.forEach( (collection) => {
+      debugger;
+      const upload = `-i collection upload ${namespace} ${collection} 1.0.0`;
+      cy.galaxykit(upload);  
+    });
+    cy.galaxykit('task wait all');
+
+    collections.forEach( (collection) => {
+      cy.galaxykit('-i collection move', namespace, collection, '1.0.0', 'staging', repository);  
+    });
+    cy.galaxykit('task wait all');*/
+
+    cy.navigateTo('hub', Repositories.url + `/{repository}/collection-version`);
+    
+    /*collections.forEach( (collection) => {
+      cy.contains(collection);
+    });*/
   });
 });
