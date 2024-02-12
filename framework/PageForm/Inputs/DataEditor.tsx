564b912ab23295a53b65560ad4840efed4096bfa
lgalis <lgalis@redhat.com>
2024-02-12 14:15:17 -0500

    Revert DataEditor custom tags change.

--- framework/PageForm/Inputs/DataEditor.tsx
+++ framework/PageForm/Inputs/DataEditor.tsx
@@ -34,7 +34,6 @@ export function DataEditor<
     configureMonacoYaml(monaco, {
       validate: true,
       format: true,
-      customTags: ['!vault scalar'],
       schemas: [
         {
           uri: '',
