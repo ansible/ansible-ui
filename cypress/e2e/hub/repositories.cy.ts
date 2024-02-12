65aeca8484821c61838b916419888f8022a90461
MilanPospisil <arkanus@seznam.cz>
2024-02-12 12:16:14 +0100

    Revert repo version test
    Manual rebase

--- cypress/e2e/hub/repositories.cy.ts
+++ cypress/e2e/hub/repositories.cy.ts
@@ -124,3 +124,89 @@ describe('Repositories Remove collection', () => {
     cy.contains('No collection versions yet');
   });
 });
+
+describe('Repositories - revert to repository version', () => {
+  let collections: string[];
+  let namespace: string;
+  let repository: string;
+  let numCollections: number;
+
+  before(() => {
+    cy.hubLogin();
+
+    namespace = 'hub_e2e_appr_namespace' + randomString(5).toLowerCase();
+    repository = 'hub_e2e_appr_repository' + randomString(5);
+
+    cy.galaxykit(`repository create ${repository}`);
+    cy.galaxykit('task wait all');
+
+    cy.galaxykit(`distribution create ${repository}`);
+    cy.galaxykit('task wait all');
+
+    cy.galaxykit(`namespace create ${namespace}`);
+    cy.galaxykit('task wait all');
+  });
+
+  after(() => {
+    /*collections?.forEach((collection) => {
+      cy.galaxykit('-i collection delete ', namespace, collection, '1.0.0', repository);
+      cy.galaxykit('task wait all');
+    });
+    cy.galaxykit('task wait all');
+    cy.galaxykit('-i repository delete', repository);
+    cy.galaxykit('task wait all');
+    cy.galaxykit('-i namespace delete', namespace);*/
+  });
+
+  it('it should revert to repository version', () => {
+    numCollections = 1;
+
+    collections = [];
+    for (let i = 0; i < numCollections; i++) {
+      collections.push('hub_e2e_collection_' + randomString(5, undefined, { isLowercase: true }));
+    }
+
+    cy.navigateTo('hub', Repositories.url);
+    cy.filterTableBySingleText(repository);
+    cy.contains(`[data-cy="repository-name-column-cell"] a`, repository).click();
+    cy.contains(`a[role="tab"]`, 'Collection versions').click();
+
+    // can see empty repository
+    cy.contains('No collection versions yet');
+
+    // set retained versions count
+    cy.get(`[data-cy="edit-repository"]`).click();
+    cy.get(`[data-cy="Submit"]`);
+    cy.get(`[data-cy="retain-repo-versions"]`).type('1');
+    cy.get(`[data-cy="Submit"]`).click();
+    cy.get(`[data-cy="Submit"]`).should('not.exist');
+
+    // add collections to repo
+    collections.forEach((collection) => {
+      cy.galaxykit(`collection upload ${namespace} ${collection}`);
+      cy.galaxykit('task wait all');
+      cy.galaxykit(`collection move ${namespace} ${collection} 1.0.0 staging ${repository}`);
+      cy.galaxykit('task wait all');
+    });
+
+    cy.contains('No collection versions yet').should('not.exist');
+
+    // can revert to repository
+    cy.contains(`a[role="tab"]`, 'Versions').click();
+    cy.contains('Version number');
+    cy.get(`[data-cy="actions-dropdown"]`);
+    cy.get(`[data-cy="actions-dropdown"]`).eq(1).click();
+    cy.contains(`a[data-cy="revert-to-this-version"]`, 'Revert to this version').click();
+
+    cy.clickModalConfirmCheckbox();
+    cy.clickModalButton('Revert to repository version');
+    cy.assertModalSuccess();
+    cy.clickModalButton('Close');
+
+    cy.galaxykit('task wait all');
+
+    cy.contains(`a[role="tab"]`, 'Collection versions').click();
+    // can see empty repository again
+    cy.contains('No collection versions yet');
+  });
+});
