6921b24d3136987fe64e4097af566809183f6fc7
Zita Nemeckova <znemecko@redhat.com>
2024-02-13 15:27:41 +0100

    Fix Signature keys tests after updated server settings

--- cypress/e2e/hub/signature-keys.cy.ts
+++ cypress/e2e/hub/signature-keys.cy.ts
@@ -6,61 +6,46 @@ describe('Signature Keys', () => {
     cy.hubLogin();
   });
 
-  // skipping flaky test that assumes server state that is not always true. this should be a component test.
-  it.skip('should render the signature keys page and display empty state when no content when no data', () => {
-    cy.navigateTo('hub', SignatureKeys.url);
-    cy.verifyPageTitle(SignatureKeys.title);
-    cy.get('[data-cy="empty-state-title"]')
-      .should('contain', 'No signature keys yet')
-      .should('be.visible');
-  });
-
-  it('should display name and public key mocked data in the list page', () => {
-    // backend on CI not supporting signing yet so it's not possible to create signature key, mocking instead
+  it('should display name and public key in the list page', () => {
     const resourceValues = ['name', 'public-key'];
-    cy.intercept(
-      { method: 'GET', url: pulpAPI`/signing-services/*` },
-      { fixture: 'hubSignatureKeys.json' }
-    ).as('signatureKeys');
+    cy.intercept({ method: 'GET', url: pulpAPI`/signing-services/*` }).as('signatureKeys');
     cy.navigateTo('hub', SignatureKeys.url);
     cy.wait('@signatureKeys')
       .its(`response.body.results[0].public_key`)
       .then((public_key) => {
-        cy.get(`[data-cy="${resourceValues[1]}-column-cell"]`).within(() => {
-          cy.get('.pf-v5-c-truncate__start').should('contain', public_key as string);
-        });
+        cy.get(`[data-cy="${resourceValues[1]}-column-cell"]`)
+          .first()
+          .within(() => {
+            cy.get('.pf-v5-c-truncate__start').should('contain', public_key as string);
+          });
       });
     cy.wait('@signatureKeys')
       .its(`response.body.results[0].name`)
       .then((name) => {
-        cy.get(`[data-cy="${resourceValues[0]}-column-cell"]`).within(() => {
-          cy.contains(name as string).should('be.visible');
-        });
+        cy.get(`[data-cy="${resourceValues[0]}-column-cell"]`)
+          .first()
+          .within(() => {
+            cy.contains(name as string).should('be.visible');
+          });
       });
   });
 
-  it('should display finger print mocked data in the list page', () => {
-    // backend on CI not supporting signing yet so it's not possible to create signature key, mocking instead
+  it('should display finger print in the list page', () => {
     const resourceValue = 'fingerprint';
-    cy.intercept(
-      { method: 'GET', url: pulpAPI`/signing-services/*` },
-      { fixture: 'hubSignatureKeys.json' }
-    ).as('signatureKeys');
+    cy.intercept({ method: 'GET', url: pulpAPI`/signing-services/*` }).as('signatureKeys');
     cy.navigateTo('hub', SignatureKeys.url);
     cy.wait('@signatureKeys')
       .its(`response.body.results[0].pubkey_fingerprint`)
       .then((pubkey_fingerprint) => {
-        cy.get(`[data-cy="${resourceValue}-column-cell"]`).within(() => {
-          cy.get('.pf-v5-c-truncate__start').should('contain', pubkey_fingerprint as string);
-        });
+        cy.get(`[data-cy="${resourceValue}-column-cell"]`)
+          .first()
+          .within(() => {
+            cy.get('.pf-v5-c-truncate__start').should('contain', pubkey_fingerprint as string);
+          });
       });
   });
   it('should contain correct table headers', () => {
-    // backend on CI not supporting signing yet so it's not possible to create signature key, mocking instead
-    cy.intercept(
-      { method: 'GET', url: pulpAPI`/signing-services/*` },
-      { fixture: 'hubSignatureKeys.json' }
-    ).as('signatureKeys');
+    cy.intercept({ method: 'GET', url: pulpAPI`/signing-services/*` }).as('signatureKeys');
     cy.navigateTo('hub', SignatureKeys.url);
     cy.wait('@signatureKeys');
     cy.get('[data-cy="name-column-header"]').should('be.visible');
@@ -69,10 +54,7 @@ describe('Signature Keys', () => {
     cy.get('[data-cy="created-column-header"]').should('be.visible');
   });
   it('should contain table with row that contains two CopyCells and download button', () => {
-    cy.intercept(
-      { method: 'GET', url: pulpAPI`/signing-services/*` },
-      { fixture: 'hubSignatureKeys.json' }
-    ).as('signatureKeys');
+    cy.intercept({ method: 'GET', url: pulpAPI`/signing-services/*` }).as('signatureKeys');
     cy.navigateTo('hub', SignatureKeys.url);
     cy.wait('@signatureKeys');
     cy.get('[data-cy="row-0"]').within(() => {
