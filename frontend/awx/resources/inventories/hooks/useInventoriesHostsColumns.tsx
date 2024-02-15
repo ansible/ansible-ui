7771af89145debccd766ef9a4de4bb041d24fe11
Matthew Sandoval <mat24c@gmail.com>
2024-02-15 07:16:37 -0800

    [AAP-8170] Add host list to inventory groups (#1604)

--- frontend/awx/resources/inventories/hooks/useInventoriesHostsColumns.tsx
+++ frontend/awx/resources/inventories/hooks/useInventoriesHostsColumns.tsx
@@ -2,15 +2,42 @@ import { Chip, ChipGroup } from '@patternfly/react-core';
 import { useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { usePageNavigate, ITableColumn } from '../../../../../framework';
-import {
-  useNameColumn,
-  useDescriptionColumn,
-  useCreatedColumn,
-  useModifiedColumn,
-} from '../../../../common/columns';
+import { useNameColumn, useCreatedColumn, useModifiedColumn } from '../../../../common/columns';
 import { AwxRoute } from '../../../main/AwxRoutes';
 import { AwxHost } from '../../../interfaces/AwxHost';
 import { useParams } from 'react-router-dom';
+import { Sparkline } from '../../templates/components/Sparkline';
+
+function useActivityColumn() {
+  const { t } = useTranslation();
+  const column: ITableColumn<AwxHost> = useMemo(
+    () => ({
+      header: t('Activity'),
+      cell: (item) => <Sparkline jobs={item.summary_fields?.recent_jobs} />,
+      value: (item) =>
+        item.summary_fields?.recent_jobs && item.summary_fields?.recent_jobs?.length > 0,
+      card: 'hidden',
+      list: 'hidden',
+    }),
+    [t]
+  );
+  return column;
+}
+
+function useDescriptionColumn() {
+  const { t } = useTranslation();
+  const column = useMemo<ITableColumn<AwxHost>>(
+    () => ({
+      header: t('Description'),
+      type: 'description',
+      value: (host: AwxHost) => host.description,
+      list: 'description',
+      card: 'description',
+    }),
+    [t]
+  );
+  return column;
+}
 
 export function useInventoriesHostsColumns(options?: {
   disableSort?: boolean;
@@ -40,6 +67,38 @@ export function useInventoriesHostsColumns(options?: {
   const tableColumns = useMemo<ITableColumn<AwxHost>[]>(
     () => [nameColumn, descriptionColumn, relatedGroupColumn, createdColumn, modifiedColumn],
     [nameColumn, descriptionColumn, relatedGroupColumn, createdColumn, modifiedColumn]
+  );
+  return tableColumns;
+}
+
+export function useInventoriesGroupHostsColumns(options?: {
+  disableSort?: boolean;
+  disableLinks?: boolean;
+}) {
+  const pageNavigate = usePageNavigate();
+  const params = useParams<{ inventory_type: string; id: string }>();
+  const nameClick = useCallback(
+    (host: AwxHost) =>
+      pageNavigate(AwxRoute.InventoryHostDetails, {
+        params: {
+          inventory_type: params.inventory_type,
+          id: params.id,
+          host_id: host.id,
+        },
+      }),
+    [pageNavigate, params.id, params.inventory_type]
+  );
+  const nameColumn = useNameColumn({
+    ...options,
+    onClick: nameClick,
+  });
+  const activityColumn = useActivityColumn();
+  const descriptionColumn = useDescriptionColumn();
+  const createdColumn = useCreatedColumn(options);
+  const modifiedColumn = useModifiedColumn(options);
+  const tableColumns = useMemo<ITableColumn<AwxHost>[]>(
+    () => [nameColumn, descriptionColumn, activityColumn, createdColumn, modifiedColumn],
+    [nameColumn, descriptionColumn, activityColumn, createdColumn, modifiedColumn]
   );
   return tableColumns;
 }
