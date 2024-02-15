2d11e0acc375c2cf5330f7c656bfea54792c3837
James Talton <jtalton@redhat.com>
2024-02-15 17:34:25 -0500

    Eda E2E Role Tests (#1677)
    Signed-off-by: James Talton <talton@gmail.com>
    Co-authored-by: Vishali Sanghishetty <95239214+vishsanghishetty@users.noreply.github.com>
    Co-authored-by: Kersom <9053044+nixocio@users.noreply.github.com>

---
+++ cypress/e2e/eda/Roles/eda-role-details.cy.ts
@@ -0,0 +1,31 @@
+describe('Eda Role Details', () => {
+  before(() => {
+    cy.edaLogin();
+  });
+
+  it('Role Details Pages Should Show', () => {
+    cy.getEdaRoles().then((roles) => {
+      roles.forEach((role) => {
+        cy.navigateTo('eda', 'roles');
+        cy.clickLink(role.name);
+        cy.verifyPageTitle(role.name);
+        cy.get('[data-cy=name]').should('have.text', role.name);
+        cy.get('[data-cy=description]').should('have.text', role.description);
+        cy.getEdaRoleDetail(role.id).then((roleDetail) => {
+          cy.get('[data-cy=permissions-description-list]').within(() => {
+            for (const detail of roleDetail.permissions) {
+              cy.get(`[data-cy=${detail.resource_type}]`).should('be.visible');
+              cy.get(`[data-cy=${detail.resource_type}]`)
+                .parent()
+                .within(() => {
+                  for (const action of detail.action) {
+                    cy.get(`[data-cy=${action}]`).should('be.visible');
+                  }
+                });
+            }
+          });
+        });
+      });
+    });
+  });
+});
---
+++ cypress/e2e/eda/Roles/eda-roles-list.cy.ts
@@ -0,0 +1,22 @@
+//Tests a user's ability to perform certain actions on the Roles list in the EDA UI.
+
+describe('EDA Roles List', () => {
+  before(() => {
+    cy.edaLogin();
+  });
+
+  it('can render the Roles list view and utilize the Roles links to view details', () => {
+    cy.navigateTo('eda', 'roles');
+    cy.verifyPageTitle('Roles');
+    cy.getEdaRoles().then((roles) => {
+      cy.get('tbody').find('tr').should('have.length', roles.length);
+      roles.forEach((role) => {
+        cy.verifyPageTitle('Roles');
+        cy.clickLink(role.name);
+        cy.verifyPageTitle(role.name);
+        cy.get('#description').should('contain', role.description);
+        cy.clickLink(/^Roles$/);
+      });
+    });
+  });
+});
