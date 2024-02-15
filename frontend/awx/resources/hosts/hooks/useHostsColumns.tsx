1bf1129660f8c33e02963650adc1f43665e0c0ad
Pratyush Bhandari <prbhanda@redhat.com>
2024-02-15 18:21:48 -0500

    Add activity column to hosts list

--- frontend/awx/resources/hosts/hooks/useHostsColumns.tsx
+++ frontend/awx/resources/hosts/hooks/useHostsColumns.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useMemo } from 'react';
-import { usePageNavigate, ITableColumn } from '../../../../../framework';
+import { usePageNavigate, ITableColumn, ColumnTableOption } from '../../../../../framework';
 import {
   useNameColumn,
   useDescriptionColumn,
@@ -7,6 +7,34 @@ import {
 } from '../../../../common/columns';
 import { AwxRoute } from '../../../main/AwxRoutes';
 import { AwxHost } from '../../../interfaces/AwxHost';
+import { useTranslation } from 'react-i18next';
+import { Sparkline } from '../../templates/components/Sparkline';
+
+function useActivityColumn() {
+  const { t } = useTranslation();
+  const column: ITableColumn<AwxHost> = useMemo(
+    () => ({
+      header: t('Activity'),
+      cell: (item) => {
+        const recentPlaybookJobs = item.summary_fields.recent_jobs.map((job) => ({
+          ...job,
+          canceled_on: null,
+        }));
+
+        if (item.summary_fields?.recent_jobs && item.summary_fields.recent_jobs.length > 0) {
+          return <Sparkline jobs={recentPlaybookJobs} />;
+        } else {
+          return t('No job data available');
+        }
+      },
+      table: ColumnTableOption.expanded,
+      card: 'hidden',
+      list: 'hidden',
+    }),
+    [t]
+  );
+  return column;
+}
 
 export function useHostsColumns(options?: { disableSort?: boolean; disableLinks?: boolean }) {
   const pageNavigate = usePageNavigate();
@@ -25,9 +53,10 @@ export function useHostsColumns(options?: { disableSort?: boolean; disableLinks?
   const inventoryColumn = useInventoryNameColumn(AwxRoute.InventoryDetails, {
     tableViewOption: undefined,
   });
+  const activityColumn = useActivityColumn();
   const tableColumns = useMemo<ITableColumn<AwxHost>[]>(
-    () => [nameColumn, descriptionColumn, inventoryColumn],
-    [nameColumn, descriptionColumn, inventoryColumn]
+    () => [nameColumn, descriptionColumn, inventoryColumn, activityColumn],
+    [nameColumn, descriptionColumn, inventoryColumn, activityColumn]
   );
   return tableColumns;
 }
