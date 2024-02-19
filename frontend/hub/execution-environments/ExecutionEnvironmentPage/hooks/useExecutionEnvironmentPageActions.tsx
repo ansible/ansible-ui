7508e623216442e1820e2fb40fdeb27cedcc2ec6
jerabekjiri <jerabekjirka@email.cz>
2024-02-19 14:52:23 +0100

    add sign action in EE detail
    Issue: AAH-3097

--- frontend/hub/execution-environments/ExecutionEnvironmentPage/hooks/useExecutionEnvironmentPageActions.tsx
+++ frontend/hub/execution-environments/ExecutionEnvironmentPage/hooks/useExecutionEnvironmentPageActions.tsx
@@ -11,12 +11,19 @@ import {
 } from '../../../../../framework';
 import { ExecutionEnvironment } from '../../ExecutionEnvironment';
 import { useDeleteExecutionEnvironments } from '../../hooks/useExecutionEnvironmentsActions';
+import { useSignExecutionEnvironments } from '../../hooks/useExecutionEnvironmentsActions';
 
-export function useExecutionEnvironmentPageActions() {
+export function useExecutionEnvironmentPageActions(options: { refresh?: () => undefined }) {
   const { t } = useTranslation();
   const pageNavigate = usePageNavigate();
   const deleteExecutionEnvironments = useDeleteExecutionEnvironments(() => {
     pageNavigate(HubRoute.ExecutionEnvironments);
+  });
+
+  const { refresh } = options;
+
+  const signExecutionEnvironments = useSignExecutionEnvironments(() => {
+    void refresh?.();
   });
 
   return useMemo(() => {
@@ -48,9 +55,9 @@ export function useExecutionEnvironmentPageActions() {
         type: PageActionType.Button,
         selection: PageActionSelection.Single,
         label: t('Sign'),
-        onClick: () => {},
+        onClick: (ee) => signExecutionEnvironments([ee]),
       },
     ];
     return actions;
-  }, [pageNavigate, t, deleteExecutionEnvironments]);
+  }, [pageNavigate, t, deleteExecutionEnvironments, signExecutionEnvironments]);
 }
