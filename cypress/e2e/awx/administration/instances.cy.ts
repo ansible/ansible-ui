c71ff6760a6d1e4884cfb8a3bfd9b9e7fc1f4cd0
Adriana Cruz <acruzgon@redhat.com>
2024-02-20 12:24:04 -0600

    Instances > Remove instance

--- cypress/e2e/awx/administration/instances.cy.ts
+++ cypress/e2e/awx/administration/instances.cy.ts
@@ -4,17 +4,26 @@ import { awxAPI } from '../../../support/formatApiPathForAwx';
 
 describe('Instances', () => {
   let instance: Instance;
+  const testSignature: string = randomString(5, undefined, { isLowercase: true });
+  function generateInstanceName(): string {
+    return `test-${testSignature}-instance-${randomString(5, undefined, { isLowercase: true })}`;
+  }
 
   beforeEach(() => {
     cy.awxLogin();
     cy.navigateTo('awx', 'instances');
-    cy.createAwxInstance().then((ins: Instance) => {
+    cy.createAwxInstance('E2EInstanceTest' + randomString(5)).then((ins: Instance) => {
       instance = ins;
     });
   });
 
   afterEach(() => {
-    cy.removeAwxInstance(instance.id.toString(), { failOnStatusCode: false });
+    if (
+      !Cypress.currentTest.title.includes('user can remove') &&
+      Cypress.currentTest.title !== 'user can bulk remove instances'
+    ) {
+      cy.removeAwxInstance(instance.id.toString(), { failOnStatusCode: false });
+    }
   });
 
   it('render the instances list page', () => {
@@ -70,6 +79,82 @@ describe('Instances', () => {
         expect(body.enabled).to.eql(true);
         expect(body.managed_by_policy).to.eql(true);
         expect(body.peers_from_control_nodes).to.eql(true);
+      });
+  });
+
+  it('user can remove an instance from details page', () => {
+    cy.intercept('PATCH', '/api/v2/instances/*').as('removedInstance');
+
+    cy.clickTableRow(instance.hostname);
+    cy.url().then((currentUrl) => {
+      expect(currentUrl.includes('details')).to.be.true;
+    });
+
+    cy.get('[data-cy="remove-instance"]').click();
+    cy.get('[data-ouia-component-type="PF5/ModalContent"]').within(() => {
+      cy.get('header').contains('Permanently remove instances');
+      cy.get('button').contains('Remove instance').should('have.attr', 'aria-disabled', 'true');
+      cy.get('[data-cy="name-column-cell"]').should('have.text', instance.hostname);
+      cy.get('input[id="confirm"]').click();
+      cy.get('button').contains('Remove instance').click();
+    });
+
+    cy.wait('@removedInstance')
+      .its('response')
+      .then((response) => {
+        expect(response?.statusCode).to.eql(200);
+      });
+  });
+
+  it('user can remove an instance from instance page toolbar', () => {
+    cy.intercept('PATCH', '/api/v2/instances/*').as('removedInstance');
+    cy.get('[data-cy="remove-instance"]').should('have.attr', 'aria-disabled', 'true');
+    cy.filterTableBySingleText(instance.hostname + '{enter}');
+    cy.contains('tr', instance.hostname).find('input').check();
+    cy.get('[data-cy="remove-instance"]').should('have.attr', 'aria-disabled', 'false');
+    cy.get('[data-cy="remove-instance"]').click();
+
+    cy.get('[data-ouia-component-type="PF5/ModalContent"]').within(() => {
+      cy.get('header').contains('Permanently remove instances');
+      cy.get('button').contains('Remove instance').should('have.attr', 'aria-disabled', 'true');
+      cy.get('[data-cy="name-column-cell"]').should('have.text', instance.hostname);
+      cy.get('input[id="confirm"]').click();
+      cy.get('button').contains('Remove instance').click();
+    });
+
+    cy.wait('@removedInstance')
+      .its('response')
+      .then((response) => {
+        expect(response?.statusCode).to.eql(200);
+      });
+  });
+
+  it('user can bulk remove instances', () => {
+    for (let i = 0; i < 5; i++) {
+      const instanceName = generateInstanceName();
+      cy.createAwxInstance(instanceName).then((ins: Instance) => {
+        instance = ins;
+      });
+    }
+    cy.intercept('PATCH', '/api/v2/instances/*').as('removedInstance');
+    cy.get('[data-cy="remove-instance"]').should('have.attr', 'aria-disabled', 'true');
+    cy.filterTableBySingleText(testSignature + '{enter}');
+    cy.get('tbody').find('tr').should('have.length', 5);
+    cy.get('[data-cy="select-all"]', { timeout: 30000 }).click();
+    cy.get('[data-cy="remove-instance"]').should('have.attr', 'aria-disabled', 'false');
+    cy.get('[data-cy="remove-instance"]').click();
+
+    cy.get('[data-ouia-component-type="PF5/ModalContent"]').within(() => {
+      cy.get('header').contains('Permanently remove instances');
+      cy.get('button').contains('Remove instance').should('have.attr', 'aria-disabled', 'true');
+      cy.get('input[id="confirm"]').click();
+      cy.get('button').contains('Remove instance').click();
+    });
+
+    cy.wait('@removedInstance')
+      .its('response')
+      .then((response) => {
+        expect(response?.statusCode).to.eql(200);
       });
   });
 });
--- frontend/awx/administration/instances/hooks/useInstanceActions.tsx
+++ frontend/awx/administration/instances/hooks/useInstanceActions.tsx
@@ -1,10 +1,26 @@
 import pDebounce from 'p-debounce';
-import { useEffect, useState } from 'react';
-import { requestGet, requestPatch } from '../../../../common/crud/Data';
+import { useEffect, useMemo, useState } from 'react';
+import { postRequest, requestGet, requestPatch } from '../../../../common/crud/Data';
 import { AwxItemsResponse } from '../../../common/AwxItemsResponse';
 import { awxAPI } from '../../../common/api/awx-utils';
 import { Instance } from '../../../interfaces/Instance';
 import { InstanceGroup } from '../../../interfaces/InstanceGroup';
+import { HeartbeatIcon, PencilAltIcon, TrashIcon } from '@patternfly/react-icons';
+import { useTranslation } from 'react-i18next';
+import {
+  usePageNavigate,
+  IPageAction,
+  PageActionType,
+  PageActionSelection,
+} from '../../../../../framework';
+import { AwxRoute } from '../../../main/AwxRoutes';
+import { useRemoveInstances } from './useRemoveInstances';
+import { ButtonVariant } from '@patternfly/react-core';
+import { useGet, useGetItem } from '../../../../common/crud/useGet';
+import { useAwxActiveUser } from '../../../common/useAwxActiveUser';
+import { Settings } from '../../../interfaces/Settings';
+import { useParams } from 'react-router-dom';
+import { TFunction } from 'i18next';
 
 export function useInstanceActions(instanceId: string) {
   const [instance, setInstance] = useState<Instance>();
@@ -80,4 +96,90 @@ export function useInstanceActions(instanceId: string) {
     handleToggleInstance,
     handleInstanceForksSlider,
   };
+}
+
+export function useInstanceDetailsActions(options: {
+  onInstancesRemoved: (instance: Instance[]) => void;
+  isDetailsPageAction?: boolean;
+}) {
+  const { onInstancesRemoved } = options;
+  const { t } = useTranslation();
+  const params = useParams<{ id: string }>();
+  const pageNavigate = usePageNavigate();
+  const { data: instance } = useGetItem<Instance>(awxAPI`/instances`, params.id);
+
+  const removeInstances = useRemoveInstances(onInstancesRemoved);
+  const activeUser = useAwxActiveUser();
+  const { data } = useGet<Settings>(awxAPI`/settings/system/`);
+  const instancesType = instance?.node_type === 'execution' || instance?.node_type === 'hop';
+  const userAccess = activeUser?.is_superuser || activeUser?.is_system_auditor;
+  const isK8s = data?.IS_K8S;
+  const canEditAndRemoveInstances = instancesType && isK8s && userAccess;
+
+  return useMemo<IPageAction<Instance>[]>(
+    () => [
+      {
+        type: PageActionType.Button,
+        isHidden: () => isK8s === false || !instancesType,
+        selection: PageActionSelection.None,
+        variant: ButtonVariant.primary,
+        isPinned: true,
+        icon: PencilAltIcon,
+        label: t('Edit instance'),
+        onClick: () => pageNavigate(AwxRoute.EditInstance, { params: { id: params.id } }),
+        isDisabled: canEditAndRemoveInstances
+          ? undefined
+          : t(
+              'You do not have permission to edit instances. Please contact your organization administrator if there is an issue with your access.'
+            ),
+      },
+      {
+        type: PageActionType.Button,
+        isHidden: () => isK8s === false || !instancesType,
+        selection: PageActionSelection.Single,
+        isPinned: true,
+        icon: TrashIcon,
+        label: t('Remove instance'),
+        onClick: (instance: Instance) => removeInstances([instance]),
+        isDisabled: () =>
+          canEditAndRemoveInstances
+            ? undefined
+            : t(
+                'You do not have permission to remove instances. Please contact your organization administrator if there is an issue with your access.'
+              ),
+        isDanger: true,
+      },
+      {
+        type: PageActionType.Button,
+        selection: PageActionSelection.Single,
+        icon: HeartbeatIcon,
+        variant: ButtonVariant.secondary,
+        isPinned: true,
+        label: t('Run health check'),
+        onClick: (instance: Instance) => {
+          void postRequest(awxAPI`/instances/${instance?.id.toString() ?? ''}/health_check/`, {});
+        },
+      },
+    ],
+    [t, canEditAndRemoveInstances, isK8s, instancesType, pageNavigate, params.id, removeInstances]
+  );
+}
+
+export function cannotRemoveInstance(instance: Instance, t: (string: string) => string) {
+  if (instance.node_type === 'execution' || instance.node_type === 'hop') {
+    return '';
+  }
+  return t(`This cannot be deleted due to insufficient permissions.`);
+}
+
+export function cannotRemoveInstances(
+  instances: Instance[],
+  t: TFunction<'translation', undefined>
+) {
+  if (instances.length === 0) {
+    return t(`Select at least one item from the list.`);
+  } else if (instances.find((instance: Instance) => cannotRemoveInstance(instance, t))) {
+    return t(`Cannot delete due to insufficient permissions with one or many items.`);
+  }
+  return '';
 }
