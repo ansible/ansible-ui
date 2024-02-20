37a04e385946886b7ffccf03501f14c7ea3b521b
Vidya Nambiar <vnambiar@redhat.com>
2024-02-20 12:04:00 -0500

    Add auth type to user details

--- platform/access/users/components/PlatformUserDetails.tsx
+++ platform/access/users/components/PlatformUserDetails.tsx
@@ -15,6 +15,8 @@ import { PlatformOrganization } from '../../../interfaces/PlatformOrganization';
 import { PlatformUser } from '../../../interfaces/PlatformUser';
 import { PlatformRoute } from '../../../main/PlatformRoutes';
 import { useUsersColumns } from '../hooks/useUserColumns';
+import { Authenticator } from '../../../interfaces/Authenticator';
+import { useReadableAuthenticatorTypes } from '../../authenticators/hooks/useReadableAuthenticatorTypes';
 
 export function PlatformUserDetails() {
   const params = useParams<{ id: string }>();
@@ -25,6 +27,10 @@ export function PlatformUserDetails() {
   const { data: organizationsData } = useGet<PlatformItemsResponse<PlatformOrganization>>(
     gatewayV1API`/users/${params.id ?? ''}/organizations/`
   );
+  const { data: authenticators } = useGet<PlatformItemsResponse<Authenticator>>(
+    gatewayV1API`/users/${params.id ?? ''}/authenticators/`
+  );
+  const readableAuthenticatorTypes = useReadableAuthenticatorTypes(authenticators?.results);
   if (isLoading) return <LoadingPage />;
 
   return (
@@ -38,6 +44,19 @@ export function PlatformUserDetails() {
               name: org.name,
               link: getPageUrl(PlatformRoute.OrganizationDetails, {
                 params: { id: org.id },
+              }),
+            }))}
+          />
+        </PageDetail>
+      ) : null}
+      {authenticators?.results?.length && readableAuthenticatorTypes?.length ? (
+        <PageDetail label={t('Authentication type', { count: readableAuthenticatorTypes?.length })}>
+          <LabelsCell
+            numLabels={3}
+            labelsWithLinks={authenticators.results.map((authenticator, index) => ({
+              name: readableAuthenticatorTypes[index],
+              link: getPageUrl(PlatformRoute.AuthenticatorDetails, {
+                params: { id: authenticator.id },
               }),
             }))}
           />
