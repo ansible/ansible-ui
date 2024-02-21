d95f21880d2223afd172b1f12cb4cfd562c1b35f
Vidya Nambiar <vnambiar@redhat.com>
2024-02-21 15:54:48 -0500

    created/modified appear last

--- platform/access/users/components/PlatformUserDetails.tsx
+++ platform/access/users/components/PlatformUserDetails.tsx
@@ -35,33 +35,37 @@ export function PlatformUserDetails() {
 
   return (
     <PageDetails>
-      <PageDetailsFromColumns columns={columns} item={user} />
-      {organizationsData?.results?.length ? (
-        <PageDetail label={t('Organization', { count: organizationsData.count })}>
-          <LabelsCell
-            numLabels={3}
-            labelsWithLinks={organizationsData?.results.map((org) => ({
-              name: org.name,
-              link: getPageUrl(PlatformRoute.OrganizationDetails, {
-                params: { id: org.id },
-              }),
-            }))}
-          />
-        </PageDetail>
-      ) : null}
-      {authenticators?.results?.length && readableAuthenticatorTypes?.length ? (
-        <PageDetail label={t('Authentication type', { count: readableAuthenticatorTypes?.length })}>
-          <LabelsCell
-            numLabels={3}
-            labelsWithLinks={authenticators.results.map((authenticator, index) => ({
-              name: readableAuthenticatorTypes[index],
-              link: getPageUrl(PlatformRoute.AuthenticatorDetails, {
-                params: { id: authenticator.id },
-              }),
-            }))}
-          />
-        </PageDetail>
-      ) : null}
+      <PageDetailsFromColumns columns={columns} item={user}>
+        {' '}
+        {organizationsData?.results?.length ? (
+          <PageDetail label={t('Organization', { count: organizationsData.count })}>
+            <LabelsCell
+              numLabels={3}
+              labelsWithLinks={organizationsData?.results.map((org) => ({
+                name: org.name,
+                link: getPageUrl(PlatformRoute.OrganizationDetails, {
+                  params: { id: org.id },
+                }),
+              }))}
+            />
+          </PageDetail>
+        ) : null}
+        {authenticators?.results?.length && readableAuthenticatorTypes?.length ? (
+          <PageDetail
+            label={t('Authentication type', { count: readableAuthenticatorTypes?.length })}
+          >
+            <LabelsCell
+              numLabels={3}
+              labelsWithLinks={authenticators.results.map((authenticator, index) => ({
+                name: readableAuthenticatorTypes[index],
+                link: getPageUrl(PlatformRoute.AuthenticatorDetails, {
+                  params: { id: authenticator.id },
+                }),
+              }))}
+            />
+          </PageDetail>
+        ) : null}
+      </PageDetailsFromColumns>
     </PageDetails>
   );
 }
