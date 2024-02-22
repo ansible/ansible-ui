c672389adf1641b03368a355099f08f6ce4811c9
vsanghishetty <vishali.kamenani@ymail.com>
2024-02-22 16:34:51 -0500

    adds new changes with extra_vars

--- frontend/awx/administration/management-jobs/hooks/useManagementJobRowActions.tsx
+++ frontend/awx/administration/management-jobs/hooks/useManagementJobRowActions.tsx
@@ -8,43 +8,47 @@ import { useManagementJobPrompt } from './useManagementJobPrompt';
 // import { useAwxActiveUser } from '../../../common/useAwxActiveUser';
 import { useLaunchManagementJob } from './useLaunchManagementJob';
 
-export function useManagementJobRowActions(managementJob: SystemJobTemplate) {
+export function useManagementJobRowActions() {
   const { t } = useTranslation();
-  const onManagementJobPrompt = useManagementJobPrompt(managementJob);
-  const { launchCleanupTokensAndSessions, launchOtherJobTypes } =
-    useLaunchManagementJob(managementJob);
+  const { launchCleanupTokensAndSessions } = useLaunchManagementJob();
+  const managementJobPrompt = useManagementJobPrompt();
+  //const launchManagementJob = useLaunchManagementJob(managementJob);
 
-  return useMemo<IPageAction<SystemJobTemplate>[]>(() => {
-    async function handleManagementJob(managementJob: SystemJobTemplate) {
-      const isPrompted = ['cleanup_activitystream', 'cleanup_jobs'].includes(
-        managementJob.job_type
-      );
-      console.log('Launching management job:', managementJob);
-      console.log('isPrompted:', isPrompted);
-      if (isPrompted) {
-        launchOtherJobTypes(managementJob);
-      } else if (
-        managementJob.job_type === 'cleanup_tokens' ||
-        managementJob.job_type === 'cleanup_sessions'
-      ) {
-        //console.log('Launching management job from cleanup sessions and tokens:', managementJob);
-        await launchCleanupTokensAndSessions(managementJob);
+  return useMemo<IPageAction<SystemJobTemplate>[]>(
+    () => {
+      async function handleManagementJob(managementJob: SystemJobTemplate) {
+        if (
+          managementJob.job_type === 'cleanup_activitystream' ||
+          managementJob.job_type === 'cleanup_jobs'
+        ) {
+          console.log(managementJob);
+          console.log('Launching PROMPT MODAL', managementJob.job_type);
+          managementJobPrompt(managementJob);
+        } else if (
+          managementJob.job_type === 'cleanup_tokens' ||
+          managementJob.job_type === 'cleanup_sessions'
+        ) {
+          console.log('Launching TOKEN AND SESSIONS', 'MANAGEMENT JOB TYPE', managementJob);
+          await launchCleanupTokensAndSessions(managementJob);
+        }
       }
-    }
 
-    const rowActions: IPageAction<SystemJobTemplate> = [
-      {
-        type: PageActionType.Button,
-        selection: PageActionSelection.Single,
-        icon: RocketIcon,
-        label: t('Launch management job'),
-        onClick: (managementJob: SystemJobTemplate) => handleManagementJob(managementJob),
-        // isDisabled: !activeUser?.is_superuser,
-        ouiaId: 'management-job-launch-button',
-        isDanger: false,
-        isPinned: true,
-      },
-    ];
-    return rowActions;
-  }, [onManagementJobPrompt, launchCleanupTokensAndSessions, launchOtherJobTypes, t]);
+      const rowActions: IPageAction<SystemJobTemplate> = [
+        {
+          type: PageActionType.Button,
+          selection: PageActionSelection.Single,
+          icon: RocketIcon,
+          label: t('Launch management job'),
+          onClick: (job: SystemJobTemplate) => handleManagementJob(job),
+          // isDisabled: !activeUser?.is_superuser,
+          ouiaId: 'management-job-launch-button',
+          isDanger: false,
+          isPinned: true,
+        },
+      ];
+      return rowActions;
+    },
+    //[onManagementJobPrompt, launchCleanupTokensAndSessions, launchOtherJobTypes, t]);
+    [managementJobPrompt, launchCleanupTokensAndSessions, t]
+  );
 }
