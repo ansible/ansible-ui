d31d3d8231e0212bf3b08da5d9e311f006410d64
Marliana Lara <mlara@redhat.com>
2024-02-26 10:08:43 -0500

    Fix: Save instance groups to job template (#1593)

--- frontend/awx/resources/templates/JobTemplateForm.cy.tsx
+++ frontend/awx/resources/templates/JobTemplateForm.cy.tsx
@@ -1,6 +1,9 @@
 import { CreateJobTemplate } from './TemplateForm';
+import { awxAPI } from '../../common/api/awx-utils';
+import type { InstanceGroup } from '../../interfaces/InstanceGroup';
 
 describe('Create job template ', () => {
+  const instanceGroups: Partial<InstanceGroup>[] = [{ id: 123, name: 'default_group' }];
   beforeEach(() => {
     cy.intercept(
       {
@@ -46,12 +49,9 @@ describe('Create job template ', () => {
       {
         method: 'GET',
         url: '/api/v2/instance_groups/*',
-        hostname: 'localhost',
       },
-      {
-        fixture: 'instance_groups.json',
-      }
-    );
+      { count: 1, results: instanceGroups }
+    ).as('getInstanceGroups');
     cy.intercept(
       {
         method: 'GET',
@@ -90,23 +90,56 @@ describe('Create job template ', () => {
       { fixture: 'project.json' }
     ).as('selectedProject');
   });
-  it('Create Template - Displays error message on internal server error', () => {
-    cy.mount(<CreateJobTemplate />);
-    cy.get('[data-cy="name"]').type('Test');
-  });
 
   it('Component renders', () => {
     cy.mount(<CreateJobTemplate />);
     cy.verifyPageTitle('Create Job Template');
   });
-  it('Validates properly', () => {
+
+  it('Should display field error messages on internal server error', () => {
+    cy.intercept(
+      { method: 'GET', url: '/api/v2/inventories/*' },
+      { statusCode: 500, message: 'Internal Server Error' }
+    );
+    cy.intercept(
+      { method: 'GET', url: '/api/v2/projects/*' },
+      { statusCode: 500, message: 'Internal Server Error' }
+    );
+    cy.mount(<CreateJobTemplate />);
+    cy.contains('Error loading inventories').should('be.visible');
+    cy.contains('Error loading projects').should('be.visible');
+  });
+
+  it('Should validate required form fields', () => {
     cy.mount(<CreateJobTemplate />);
     cy.clickButton(/^Create job template$/);
     ['Name', 'Inventory', 'Project', 'Playbook'].map((field) =>
       cy.contains(`${field} is required.`).should('be.visible')
     );
   });
-  it('Should update fields properly', () => {
+
+  it('Should send expected form data to API on save', () => {
+    cy.intercept('POST', '/api/v2/job_templates/', (req) => {
+      expect(req.body).to.contain({
+        inventory: 1,
+        job_type: 'check',
+        name: 'Test',
+        playbook: 'hello_world.yml',
+        project: 6,
+      });
+      return req.reply({ statusCode: 200, body: { id: 1000, type: 'job' } });
+    });
+    cy.intercept('GET', awxAPI`/job_templates/1000/instance_groups/`, {
+      count: 0,
+      results: [],
+    });
+    cy.intercept('POST', awxAPI`/job_templates/1000/instance_groups/`, (req) => {
+      expect(req.body).to.contain({
+        id: instanceGroups[0].id,
+      });
+      return req.reply({ statusCode: 204 });
+    });
+
     cy.mount(<CreateJobTemplate />);
     cy.get('[data-cy="name"]').type('Test');
     cy.get('button[aria-describedby="job_type-form-group"]').click();
@@ -114,16 +147,19 @@ describe('Create job template ', () => {
     cy.selectDropdownOptionByResourceName('inventory', 'Demo Inventory');
     cy.selectDropdownOptionByResourceName('project', 'Demo Project').as('ProjectInput');
     cy.selectDropdownOptionByResourceName('playbook', 'hello_world.yml');
-    cy.clickButton('Create job template');
-
-    cy.intercept('POST', '/api/v2/job_templates/', (req) => {
-      expect(req.body).to.contain({
-        inventory: 9,
-        job_type: 'check',
-        name: 'Test',
-        playbook: 'hello_world.yml',
-        project: 6,
+    cy.wait('@getInstanceGroups')
+      .its('response.body.results')
+      .then(() => {
+        cy.get(`[data-cy*="instance-group-select-form-group"]`).within(() => {
+          cy.get('button').eq(1).click();
+        });
+        cy.get('[data-ouia-component-type="PF5/ModalContent"]').within(() => {
+          cy.get('[data-ouia-component-id="simple-table"] tbody').within(() => {
+            cy.get('[data-cy="checkbox-column-cell"] input').click();
+          });
+          cy.clickButton(/^Confirm/);
+        });
       });
-    });
+    cy.clickButton('Create job template');
   });
 });
