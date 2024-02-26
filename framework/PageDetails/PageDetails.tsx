945c4030f58b59e2d7e9a3b364672e06d561ada3
James Talton <talton@gmail.com>
2024-02-26 14:25:17 -0500

    Fix scrolling of details pages by making PageDetails scroll by default
    Signed-off-by: James Talton <talton@gmail.com>

--- framework/PageDetails/PageDetails.tsx
+++ framework/PageDetails/PageDetails.tsx
@@ -2,6 +2,7 @@ import { Alert, DescriptionList, PageSection } from '@patternfly/react-core';
 import { ReactNode } from 'react';
 import styled from 'styled-components';
 import { useSettings } from '../Settings';
+import { Scrollable } from '../components/Scrollable';
 
 export function PageDetails(props: {
   children?: ReactNode;
@@ -17,45 +18,47 @@ export function PageDetails(props: {
   const numberOfColumns = props.numberOfColumns ? props.numberOfColumns : settings.formColumns;
   const isCompact = props.isCompact;
   return (
-    <PageSectionStyled variant="light" padding={{ default: 'noPadding' }}>
-      {alertPrompts &&
-        alertPrompts.length > 0 &&
-        alertPrompts.map((alertPrompt, i) => (
-          <Alert
-            style={{ margin: 12 }}
-            isInline
-            title={alertPrompt}
-            variant="warning"
-            key={i}
-            data-cy={alertPrompt}
-          ></Alert>
-        ))}
-      <DescriptionList
-        orientation={{
-          sm: orientation,
-          md: orientation,
-          lg: orientation,
-          xl: orientation,
-          '2xl': orientation,
-        }}
-        columnModifier={
-          numberOfColumns === 'multiple'
-            ? {
-                default: '1Col',
-                sm: '1Col',
-                md: '2Col',
-                lg: '2Col',
-                xl: '3Col',
-                '2xl': '3Col',
-              }
-            : undefined
-        }
-        style={{ maxWidth: 1200, padding: disablePadding ? undefined : 24 }}
-        isCompact={isCompact}
-      >
-        {props.children}
-      </DescriptionList>
-    </PageSectionStyled>
+    <Scrollable>
+      <PageSectionStyled variant="light" padding={{ default: 'noPadding' }}>
+        {alertPrompts &&
+          alertPrompts.length > 0 &&
+          alertPrompts.map((alertPrompt, i) => (
+            <Alert
+              style={{ margin: 12 }}
+              isInline
+              title={alertPrompt}
+              variant="warning"
+              key={i}
+              data-cy={alertPrompt}
+            ></Alert>
+          ))}
+        <DescriptionList
+          orientation={{
+            sm: orientation,
+            md: orientation,
+            lg: orientation,
+            xl: orientation,
+            '2xl': orientation,
+          }}
+          columnModifier={
+            numberOfColumns === 'multiple'
+              ? {
+                  default: '1Col',
+                  sm: '1Col',
+                  md: '2Col',
+                  lg: '2Col',
+                  xl: '3Col',
+                  '2xl': '3Col',
+                }
+              : undefined
+          }
+          style={{ maxWidth: 1200, padding: disablePadding ? undefined : 24 }}
+          isCompact={isCompact}
+        >
+          {props.children}
+        </DescriptionList>
+      </PageSectionStyled>
+    </Scrollable>
   );
 }
 
--- frontend/awx/access/credential-types/CredentialTypePage/CredentialTypeDetails.tsx
+++ frontend/awx/access/credential-types/CredentialTypePage/CredentialTypeDetails.tsx
@@ -1,13 +1,7 @@
 import { Label } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from 'react-router-dom';
-import {
-  DateTimeCell,
-  PageDetail,
-  PageDetails,
-  Scrollable,
-  useGetPageUrl,
-} from '../../../../../framework';
+import { DateTimeCell, PageDetail, PageDetails, useGetPageUrl } from '../../../../../framework';
 import { PageDetailCodeEditor } from '../../../../../framework/PageDetails/PageDetailCodeEditor';
 import { jsonToYaml } from '../../../../../framework/utils/codeEditorUtils';
 import { LastModifiedPageDetail } from '../../../../common/LastModifiedPageDetail';
@@ -43,51 +37,49 @@ export function CredentialTypeDetailInner(props: { credentialType: CredentialTyp
   }
 
   return (
-    <Scrollable>
-      <PageDetails>
-        <PageDetail label={t('Name')}>{renderCredentialTypeName(props.credentialType)}</PageDetail>
-        <PageDetail label={t('Description')}>{props.credentialType.description}</PageDetail>
-        <PageDetailCodeEditor
-          helpText={t('Input schema which defines a set of ordered fields for that type.')}
-          label={t('Input configuration')}
-          value={jsonToYaml(JSON.stringify(props.credentialType.inputs))}
-        />
-        <PageDetailCodeEditor
-          helpText={t(
-            'Environment variables or extra variables that specify the values a credential type can inject.'
-          )}
-          label={t('Injector configuration')}
-          value={jsonToYaml(JSON.stringify(props.credentialType.injectors))}
-        />
-        <PageDetail label={t('Created')}>
-          <DateTimeCell
-            value={props.credentialType.created}
-            author={props.credentialType.summary_fields?.created_by?.username}
-            onClick={() =>
-              history(
-                getPageUrl(AwxRoute.UserDetails, {
-                  params: {
-                    id: (props.credentialType.summary_fields?.created_by?.id ?? 0).toString(),
-                  },
-                })
-              )
-            }
-          />
-        </PageDetail>
-        <LastModifiedPageDetail
-          value={props.credentialType.modified}
-          author={props.credentialType.summary_fields?.modified_by?.username}
+    <PageDetails>
+      <PageDetail label={t('Name')}>{renderCredentialTypeName(props.credentialType)}</PageDetail>
+      <PageDetail label={t('Description')}>{props.credentialType.description}</PageDetail>
+      <PageDetailCodeEditor
+        helpText={t('Input schema which defines a set of ordered fields for that type.')}
+        label={t('Input configuration')}
+        value={jsonToYaml(JSON.stringify(props.credentialType.inputs))}
+      />
+      <PageDetailCodeEditor
+        helpText={t(
+          'Environment variables or extra variables that specify the values a credential type can inject.'
+        )}
+        label={t('Injector configuration')}
+        value={jsonToYaml(JSON.stringify(props.credentialType.injectors))}
+      />
+      <PageDetail label={t('Created')}>
+        <DateTimeCell
+          value={props.credentialType.created}
+          author={props.credentialType.summary_fields?.created_by?.username}
           onClick={() =>
             history(
               getPageUrl(AwxRoute.UserDetails, {
                 params: {
-                  id: (props.credentialType.summary_fields?.modified_by?.id ?? 0).toString(),
+                  id: (props.credentialType.summary_fields?.created_by?.id ?? 0).toString(),
                 },
               })
             )
           }
         />
-      </PageDetails>
-    </Scrollable>
+      </PageDetail>
+      <LastModifiedPageDetail
+        value={props.credentialType.modified}
+        author={props.credentialType.summary_fields?.modified_by?.username}
+        onClick={() =>
+          history(
+            getPageUrl(AwxRoute.UserDetails, {
+              params: {
+                id: (props.credentialType.summary_fields?.modified_by?.id ?? 0).toString(),
+              },
+            })
+          )
+        }
+      />
+    </PageDetails>
   );
 }
