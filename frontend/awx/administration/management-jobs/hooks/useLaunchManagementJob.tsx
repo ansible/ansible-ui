8298b6b08ad1602877103c190a18aeafd239d402
vsanghishetty <vishali.kamenani@ymail.com>
2024-02-27 11:37:51 -0500

    wip mgt jobs

--- frontend/awx/administration/management-jobs/hooks/useLaunchManagementJob.tsx
+++ frontend/awx/administration/management-jobs/hooks/useLaunchManagementJob.tsx
@@ -14,78 +14,22 @@ export function useLaunchManagementJob() {
   const alertToaster = usePageAlertToaster();
   const getJobOutputUrl = useGetJobOutputUrl();
 
-  const launchCleanupTokensAndSessions = async (managementJob: SystemJobTemplate) => {
-    const launchManagementJobEndpoint = getLaunchMgtJobEndpoint(managementJob);
-    //console.log(typeof launchManagementJobEndpoint, launchManagementJobEndpoint);
-
-    if (!launchManagementJobEndpoint) {
-      return Promise.reject(new Error('Unable to retrieve management job launch configuration'));
-      //throw new Error('Unable to retrieve management job launch configuration');
-    }
-
-    try {
-      const managementJobLaunch = await postRequest(launchManagementJobEndpoint, {});
-      console.log(
-        'Launching management launchCleanupTokensAndSessions:',
-        managementJob.job_type,
-        managementJobLaunch,
-        launchManagementJobEndpoint
-      );
-      navigate(getJobOutputUrl(managementJobLaunch as SystemJobTemplate));
-    } catch (error) {
-      alertToaster.addAlert({
-        variant: 'danger',
-        title: t('Failed to launch management job'),
-        children: error instanceof Error && error.message,
-        timeout: 5000,
-      });
-    }
-  };
-
-  const launchOtherJobTypes = async (managementJob: SystemJobTemplate) => {
-    const launchManagementJobEndpoint = getLaunchMgtJobEndpoint(managementJob);
-    console.log('Launch management endpoint:', launchManagementJobEndpoint);
-    console.log('Launching other management job types:', managementJob.job_type);
-    if (['cleanup_activitystream', 'cleanup_jobs'].includes(managementJob.job_type)) {
-      const managementJobLaunch = await postRequest(launchManagementJobEndpoint, {
-        extra_vars: { days: 3 },
-      });
-      navigate(getJobOutputUrl(managementJobLaunch as SystemJobTemplate));
-    }
-  };
-
-  return { launchCleanupTokensAndSessions, launchOtherJobTypes };
-
-  // const launchManagementJob = async (
-  //   managementJob: SystemJobTemplate,
-  //   extra_vars?: number = 30
-  // ) => {
+  // const launchCleanupTokensAndSessions = async (managementJob: SystemJobTemplate) => {
   //   const launchManagementJobEndpoint = getLaunchMgtJobEndpoint(managementJob);
-  //   console.log(launchManagementJobEndpoint);
+
   //   if (!launchManagementJobEndpoint) {
   //     return Promise.reject(new Error('Unable to retrieve management job launch configuration'));
   //   }
 
   //   try {
-  //     //const postData = extraVars ? { extra_vars: extra_vars } : {};
-  //     // const managementJobLaunch = await postRequest(launchManagementJobEndpoint, {});
-
-  //     // console.log('Launching management job:', managementJob.job_type, managementJobLaunch);
-
-  //     let targetUrl;
-  //     if (['cleanup_tokens', 'cleanup_sessions'].includes(managementJob.job_type)) {
-  //       const managementJobLaunch = await postRequest(launchManagementJobEndpoint, {});
-  //      // console.log('MANAGEMENT JOB LAUNCH T and S', managementJobLaunch, managementJob);
-  //       targetUrl = getJobOutputUrl(managementJobLaunch as SystemJobTemplate);
-  //     } else if (['cleanup_activitystream', 'cleanup_jobs'].includes(managementJob.job_type)) {
-  //       const managementJobLaunch = await postRequest(launchManagementJobEndpoint, {
-  //         extra_vars: extra_vars,
-  //       });
-  //      // console.log('MANAGEMENT JOB LAUNCH S and J', managementJob.job_type);
-  //      // console.log('MANAGEMENT JOB LAUNCH S and J', managementJobLaunch, managementJob);
-  //       targetUrl = getJobOutputUrl(managementJobLaunch as SystemJobTemplate);
-  //     }
-  //     navigate(targetUrl);
+  //     const managementJobLaunch = await postRequest(launchManagementJobEndpoint, {});
+  //     console.log(
+  //       'Launching management launchCleanupTokensAndSessions:',
+  //       managementJob.job_type,
+  //       managementJobLaunch,
+  //       launchManagementJobEndpoint
+  //     );
+  //     navigate(getJobOutputUrl(managementJobLaunch as SystemJobTemplate));
   //   } catch (error) {
   //     alertToaster.addAlert({
   //       variant: 'danger',
@@ -95,7 +39,60 @@ export function useLaunchManagementJob() {
   //     });
   //   }
   // };
-  // return launchManagementJob;
+
+  // const launchOtherJobTypes = async (managementJob: SystemJobTemplate) => {
+  //   const launchManagementJobEndpoint = getLaunchMgtJobEndpoint(managementJob);
+  //   if (['cleanup_activitystream', 'cleanup_jobs'].includes(managementJob.job_type)) {
+  //     const managementJobLaunch = await postRequest(launchManagementJobEndpoint, {
+  //       extra_vars: { days: 3 },
+  //     });
+  //     navigate(getJobOutputUrl(managementJobLaunch as SystemJobTemplate));
+  //   }
+  // };
+
+  // return { launchCleanupTokensAndSessions, launchOtherJobTypes };
+
+  const launchManagementJob = async (
+    managementJob: SystemJobTemplate,
+    extra_vars?: number = 30
+  ) => {
+    const launchManagementJobEndpoint = getLaunchMgtJobEndpoint(managementJob);
+    console.log(launchManagementJobEndpoint);
+    if (!launchManagementJobEndpoint) {
+      return Promise.reject(new Error('Unable to retrieve management job launch configuration'));
+    }
+
+    try {
+      // const postData = extraVars ? { extra_vars: extra_vars } : {};
+      // const managementJobLaunch = await postRequest(launchManagementJobEndpoint, {});
+
+      //console.log('Launching management job:', managementJob.job_type, managementJobLaunch);
+
+      let targetUrl;
+      if (['cleanup_tokens', 'cleanup_sessions'].includes(managementJob.job_type)) {
+        const managementJobLaunch = await postRequest(launchManagementJobEndpoint, {});
+        // console.log('MANAGEMENT JOB LAUNCH T and S', managementJobLaunch, managementJob);
+        targetUrl = getJobOutputUrl(managementJobLaunch as SystemJobTemplate);
+      } else if (['cleanup_activitystream', 'cleanup_jobs'].includes(managementJob.job_type)) {
+        const postData = extraVars ? { extra_vars: { days: extra_vars } } : {};
+        const managementJobLaunch = await postRequest(launchManagementJobEndpoint, {
+          postData,
+        });
+        // console.log('MANAGEMENT JOB LAUNCH S and J', managementJob.job_type);
+        // console.log('MANAGEMENT JOB LAUNCH S and J', managementJobLaunch, managementJob);
+        targetUrl = getJobOutputUrl(managementJobLaunch as SystemJobTemplate);
+      }
+      navigate(targetUrl);
+    } catch (error) {
+      alertToaster.addAlert({
+        variant: 'danger',
+        title: t('Failed to launch management job'),
+        children: error instanceof Error && error.message,
+        timeout: 5000,
+      });
+    }
+  };
+  return launchManagementJob;
 }
 
 export function getLaunchMgtJobEndpoint(managementJob: SystemJobTemplate) {
