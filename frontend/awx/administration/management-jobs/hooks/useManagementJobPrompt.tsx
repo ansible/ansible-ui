95977a736eb1bb82f66cb63308da80bf1cef191a
vsanghishetty <vishali.kamenani@ymail.com>
2024-03-04 16:13:40 -0500

    wip mgt jobs

--- frontend/awx/administration/management-jobs/hooks/useManagementJobPrompt.tsx
+++ frontend/awx/administration/management-jobs/hooks/useManagementJobPrompt.tsx
@@ -3,49 +3,50 @@ import React, { useState } from 'react';
 //import { SystemJobTemplate } from '../../interfaces/SystemJobTemplate';
 import { usePageDialogs } from '../../../../../framework';
 import { Button, Modal, NumberInput, ModalVariant } from '@patternfly/react-core';
-import { useLaunchManagementJob } from './useLaunchManagementJob';
+//import { useLaunchManagementJob } from './useLaunchManagementJob';
 import { TextContent, Text } from '@patternfly/react-core';
 import { SystemJobTemplate } from '../../../interfaces/SystemJobTemplate';
 
 export function useManagementJobPrompt() {
-  //const defaultDays = 30;
-  const [dataRetention, setDataRetention] = useState<number | ''>(3);
+  const defaultDays: number = 3;
+  const [dataRetention, setDataRetention] = useState(defaultDays);
   const { t } = useTranslation();
   const { pushDialog, popDialog } = usePageDialogs();
-  const launchManagementJob = useLaunchManagementJob();
-
-  const onMinus = () => {
-    const newValue = (dataRetention || 0) - 1;
-    setDataRetention(newValue);
-    console.log(dataRetention);
-  };
-
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const dataRetention = (event.target as HTMLInputElement).value;
-    setDataRetention(dataRetention === '' ? dataRetention : +dataRetention);
-    console.log(dataRetention);
-  };
-
-  const onPlus = () => {
-    const newValue = (dataRetention || 0) + 1;
-    setDataRetention(newValue);
-    console.log(dataRetention);
-  };
-
-  async function handleLaunch(
-    managementJob: SystemJobTemplate,
-    dataRetention?: number
-  ): Promise<void> {
-    await launchManagementJob(managementJob, dataRetention);
-  }
-
+  //const [isManagementPromptOpen, setIsManagementPromptOpen] = useState(false);
+  //const launchManagementJob = useLaunchManagementJob();
   const managementJobPrompt = (managementJob: SystemJobTemplate) => {
+    const onMinus = () => {
+      setDataRetention((prevDataRetention) => Math.max((prevDataRetention || 0) - 1, 0));
+    };
+
+    const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+      const value = event.currentTarget.value;
+      setDataRetention(parseInt(value) || 0);
+      console.log(dataRetention);
+    };
+
+    const onPlus = () => {
+      setDataRetention((prevDataRetention) => (prevDataRetention || 0) + 1);
+      console.log(dataRetention);
+    };
+    const handleCancel = () => {
+      popDialog();
+      setDataRetention(defaultDays);
+    };
+
+    const handleLaunch = () => {
+      console.log(dataRetention);
+      popDialog();
+      return dataRetention;
+    };
+
+    // const managementJobPrompt = (managementJob: SystemJobTemplate) => {
     const dialog = (
       <Modal
         title={t('Launch Management Job')}
         titleIconVariant="info"
         isOpen
-        width="35%"
+        width="25%"
         key="launch"
         onClose={popDialog}
         variant={ModalVariant.small}
@@ -57,6 +58,7 @@ export function useManagementJobPrompt() {
             value={dataRetention}
             onMinus={onMinus}
             onPlus={onPlus}
+            type="number"
             onChange={handleChange}
             inputName="input"
             inputAriaLabel="number input"
@@ -67,16 +69,11 @@ export function useManagementJobPrompt() {
           />
         </TextContent>
 
-        <Button
-          variant="primary"
-          onClick={() => {
-            void handleLaunch(managementJob, dataRetention);
-          }}
-        >
+        <Button variant="primary" onClick={() => handleLaunch(managementJob, dataRetention)}>
           {t('Launch')}
         </Button>
 
-        <Button key="cancel" variant="link" onClick={popDialog}>
+        <Button key="cancel" variant="link" onClick={handleCancel}>
           {t('Cancel')}
         </Button>
       </Modal>
