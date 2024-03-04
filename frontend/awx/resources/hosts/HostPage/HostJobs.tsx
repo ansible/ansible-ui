a0f9dae57c0b6c74f650a584a7c41ca5b78aa2bc
Maeve Hoffer <daphnemaeve@gmail.com>
2024-03-04 12:13:20 -0800

    Make JobsList reusable and add Host > Jobs List (#1745)

--- frontend/awx/resources/hosts/HostPage/HostJobs.tsx
+++ frontend/awx/resources/hosts/HostPage/HostJobs.tsx
@@ -1,9 +1,9 @@
 import { useParams } from 'react-router-dom';
 import { usePersistentFilters } from '../../../../common/PersistentFilters';
 import { JobsList } from '../../../views/jobs/JobsList';
-import { useHostsJobsColumns } from './hooks/useHostsJobsColumns';
+import { useHostsJobsColumns } from '../../inventories/inventoryHostsPage/hooks/useHostsJobsColumns';
 
-export function InventoryHostJobs() {
+export function HostJobs() {
   usePersistentFilters('inventories');
   const jobsColumns = useHostsJobsColumns();
   const { id = '' } = useParams<{ id: string }>();
