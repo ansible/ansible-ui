ff50530cdd9386ade651d4f6c28192c703be03f1
Jiří Jeřábek (Jiri Jerabek) <Jerabekjirka@email.cz>
2024-03-04 16:06:14 +0100

    [AAH-2525] Execution Environments > tab Images > action Manage tags (#1719)

--- frontend/hub/execution-environments/ExecutionEnvironmentPage/hooks/useImagesToolbarActions.tsx
+++ frontend/hub/execution-environments/ExecutionEnvironmentPage/hooks/useImagesToolbarActions.tsx
@@ -4,12 +4,16 @@ import { TrashIcon } from '@patternfly/react-icons';
 import { IPageAction, PageActionType, PageActionSelection } from '../../../../../framework';
 import { ExecutionEnvironmentImage as Image } from '../ExecutionEnvironmentImage';
 import { useDeleteImages } from './useDeleteImages';
+import { useExecutionEnvironmentManageTags } from './useExecutionEnvironmentManageTags';
+import { ExecutionEnvironment } from '../../ExecutionEnvironment';
 
 export function useImagesToolbarActions({
   id,
+  executionEnvironment,
   refresh,
 }: {
   id: string;
+  executionEnvironment: ExecutionEnvironment;
   refresh?: () => Promise<void>;
 }) {
   const deleteImages = useDeleteImages({
@@ -18,15 +22,21 @@ export function useImagesToolbarActions({
       void refresh?.();
     },
   });
-
   const { t } = useTranslation();
+  const executionEnvironmentManageTags = useExecutionEnvironmentManageTags(() => {
+    void refresh?.();
+  });
+
   return useMemo<IPageAction<Image>[]>(
     () => [
       {
         type: PageActionType.Button,
         selection: PageActionSelection.Single,
         label: t('Manage tags'),
-        onClick: () => {},
+        onClick: (image) => {
+          executionEnvironmentManageTags(executionEnvironment, image);
+        },
+        isHidden: () => !!executionEnvironment.pulp?.repository?.remote,
       },
       {
         type: PageActionType.Button,
@@ -45,6 +55,6 @@ export function useImagesToolbarActions({
         },
       },
     ],
-    [t, deleteImages]
+    [t, executionEnvironment, deleteImages, executionEnvironmentManageTags]
   );
 }
