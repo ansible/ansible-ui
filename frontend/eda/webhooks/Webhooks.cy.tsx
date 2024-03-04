7b760ebf443bec392ebafdccd0dccf9c5829adcf
lgalis <lgalis@redhat.com>
2024-03-04 10:15:22 -0500

    Add webhook test fixture, update test

--- frontend/eda/webhooks/Webhooks.cy.tsx
+++ frontend/eda/webhooks/Webhooks.cy.tsx
@@ -20,49 +20,99 @@ describe('Webhooks.cy.ts', () => {
         page: 1,
         results: [
           {
-            name: 'E2E Webhook N0vX',
-            description: 'This is a container registry webhook',
-            username: 'admin',
-            webhook_type: 'Container Registry',
-            id: 8,
-            created_at: '2023-07-28T18:29:28.512273Z',
-            modified_at: '2023-07-28T18:29:28.512286Z',
+            name: 'Webhook 1',
+            test_mode: false,
+            user: 'admin',
+            hmac_algorithm: 'sha256',
+            header_key: 'X-Hub-Signature-256',
+            hmac_signature_prefix: 'sha256=',
+            hmac_format: 'hex',
+            auth_type: 'hmac',
+            additional_data_headers: null,
+            id: 1,
+            url: 'https://ui.eda.local:8443/abc-def-123-34567/api/eda/v1/external_webhook/123a26f5-bddd-4737-a0e7-653949ccce0a/post/',
+            type: 'GitHub',
+            created_at: '2024-03-04T14:51:10.846070Z',
+            modified_at: '2024-03-04T14:51:10.846083Z',
+            test_content_type: '',
+            test_content: '',
+            test_error_message: '',
           },
           {
-            name: 'E2E Webhook aOHl',
-            description: 'This is a container registry webhook',
-            username: 'admin',
-            webhook_type: 'Container Registry',
-            id: 11,
-            created_at: '2023-07-28T18:32:34.992501Z',
-            modified_at: '2023-07-28T18:32:34.992522Z',
+            name: 'Webhook 2',
+            test_mode: false,
+            user: 'admin',
+            hmac_algorithm: 'sha256',
+            header_key: 'X-Gitlab-Token',
+            hmac_signature_prefix: '',
+            hmac_format: 'hex',
+            auth_type: 'token',
+            additional_data_headers: null,
+            id: 2,
+            url: 'https://ui.eda.local:8443/abc-def-123-34567/api/eda/v1/external_webhook/d0073c32-561f-428b-9461-6e783f3cad4d/post/',
+            type: 'GitLab',
+            created_at: '2024-03-04T14:51:28.010931Z',
+            modified_at: '2024-03-04T14:51:28.010942Z',
+            test_content_type: '',
+            test_content: '',
+            test_error_message: '',
           },
           {
-            name: 'E2E Webhook kpub',
-            description: 'This is a GitHub Webhook.',
-            username: 'admin',
-            webhook_type: 'GitHub Personal Access Token',
-            id: 13,
-            created_at: '2023-07-28T18:32:51.739715Z',
-            modified_at: '2023-07-28T18:32:51.739740Z',
+            name: 'Webhook 3',
+            test_mode: false,
+            user: 'admin',
+            hmac_algorithm: 'sha256',
+            header_key: 'Authorization',
+            hmac_signature_prefix: '',
+            hmac_format: 'hex',
+            auth_type: 'token',
+            additional_data_headers: null,
+            id: 3,
+            url: 'https://ui.eda.local:8443/abc-def-123-34567/api/eda/v1/external_webhook/6805007c-bad8-43ce-9afe-db20f1dbf594/post/',
+            type: 'Service Now',
+            created_at: '2024-03-04T14:51:45.682567Z',
+            modified_at: '2024-03-04T14:51:45.682577Z',
+            test_content_type: '',
+            test_content: '',
+            test_error_message: '',
           },
           {
-            name: 'E2E Webhook ZFca',
-            description: 'This is a container registry webhook',
-            username: 'admin',
-            webhook_type: 'Container Registry',
-            id: 30,
-            created_at: '2023-07-28T19:28:01.687027Z',
-            modified_at: '2023-07-28T19:28:01.687040Z',
+            name: 'Webhook 4',
+            test_mode: false,
+            user: 'admin',
+            hmac_algorithm: 'sha256',
+            header_key: 'X-Hub-Signature-256',
+            hmac_signature_prefix: 'sha256=',
+            hmac_format: 'hex',
+            auth_type: 'hmac',
+            additional_data_headers: null,
+            id: 4,
+            url: 'https://ui.eda.local:8443/abc-def-123-34567/api/eda/v1/external_webhook/123a26f5-bddd-4737-a0e7-653949ccce0a/post/',
+            type: 'GitHub',
+            created_at: '2024-03-04T14:51:10.846070Z',
+            modified_at: '2024-03-04T14:51:10.846083Z',
+            test_content_type: '',
+            test_content: '',
+            test_error_message: '',
           },
           {
-            name: 'E2E Webhook Y315',
-            description: 'This is a container registry webhook',
-            username: 'admin',
-            webhook_type: 'Container Registry',
-            id: 31,
-            created_at: '2023-07-28T19:28:01.767198Z',
-            modified_at: '2023-07-28T19:28:01.767210Z',
+            name: 'Webhook 5',
+            test_mode: false,
+            user: 'admin',
+            hmac_algorithm: 'sha256',
+            header_key: 'X-Gitlab-Token',
+            hmac_signature_prefix: '',
+            hmac_format: 'hex',
+            auth_type: 'token',
+            additional_data_headers: null,
+            id: 5,
+            url: 'https://ui.eda.local:8443/abc-def-123-34567/api/eda/v1/external_webhook/d0073c32-561f-428b-9461-6e783f3cad4d/post/',
+            type: 'GitLab',
+            created_at: '2024-03-04T14:51:28.010931Z',
+            modified_at: '2024-03-04T14:51:28.010942Z',
+            test_content_type: '',
+            test_content: '',
+            test_error_message: '',
           },
         ],
       }
@@ -73,23 +123,20 @@ describe('Webhooks.cy.ts', () => {
     cy.mount(<Webhooks />);
     cy.get('h1').should('contain', 'Webhooks');
     cy.get('tbody').find('tr').should('have.length', 10);
-    cy.contains(
-      /^Webhooks are utilized by EDA for authentication when launching rulebooks.$/
-    ).should('be.visible');
     cy.contains('th', 'Name');
-    cy.contains('th', 'Type');
+    cy.contains('th', 'Created');
   });
 
   it('Can delete a Webhook not in use', () => {
     cy.mount(<Webhooks />);
     cy.intercept(
-      { method: 'DELETE', url: edaAPI`/webhooks/100/` },
+      { method: 'DELETE', url: edaAPI`/webhooks/1/` },
       {
         statusCode: 204,
       }
     );
     cy.intercept(
-      { method: 'GET', url: edaAPI`/activations/?webhook_id=100` },
+      { method: 'GET', url: edaAPI`/activations/?webhook_id=1` },
       { count: 0, next: null, previous: null, page_size: 20, page: 1, results: [] }
     );
     cy.get('[data-cy="checkbox-column-cell"]').first().click();
@@ -100,64 +147,7 @@ describe('Webhooks.cy.ts', () => {
         'contain',
         `Yes, I confirm that I want to delete these`
       );
-      cy.contains('EDA Webhook 1');
-      cy.get('input[id="confirm"]').click();
-      cy.get('button').contains('Delete webhooks').click();
-    });
-    cy.get('[data-cy="status-column-cell"] > span').contains('Success');
-    cy.clickButton(/^Close$/);
-  });
-
-  it('can delete a Webhook in use', () => {
-    cy.mount(<Webhooks />);
-    cy.intercept(
-      { method: 'DELETE', url: edaAPI`/webhooks/100/?force=true` },
-      {
-        statusCode: 204,
-      }
-    );
-    cy.intercept(
-      { method: 'GET', url: edaAPI`/activations/?webhook_id=100` },
-      {
-        count: 1,
-        next: null,
-        previous: null,
-        page_size: 20,
-        page: 1,
-        results: [
-          {
-            id: 1,
-            name: 'Activation with webhook',
-            description: '',
-            is_enabled: true,
-            status: 'failed',
-            decision_environment_id: 11,
-            project_id: 4,
-            rulebook_id: 28,
-            extra_var_id: null,
-            restart_policy: 'on-failure',
-            restart_count: 0,
-            rulebook_name: 'basic_short.yml',
-            current_job_id: null,
-            rules_count: 0,
-            rules_fired_count: 0,
-            created_at: '2023-11-16T20:22:48.755916Z',
-            modified_at: '2023-11-16T20:22:51.382540Z',
-            status_message: 'Activation has failed',
-          },
-        ],
-      }
-    );
-    cy.get('[data-cy="checkbox-column-cell"]').first().click();
-    cy.get('[data-cy="actions-dropdown"]').first().click();
-    cy.get('[data-cy="delete-selected-webhooks"]').click();
-    cy.get('div[role="dialog"]').within(() => {
-      cy.get('.pf-v5-c-check__label').should(
-        'contain',
-        `Yes, I confirm that I want to delete these`
-      );
-      cy.contains('EDA Webhook 1');
-      cy.get('.pf-v5-c-alert__title').contains('The following webhooks are in use: EDA Webhook 1');
+      cy.contains('Webhook 1');
       cy.get('input[id="confirm"]').click();
       cy.get('button').contains('Delete webhooks').click();
     });
