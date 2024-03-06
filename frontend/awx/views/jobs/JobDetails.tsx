0d003eb0171c8f6b08b006f641073fc1dafe0d6d
resoluteCoder <resolutecoder@gmail.com>
2024-03-06 13:17:48 -0600

    implemented workflow approval job tab

--- frontend/awx/views/jobs/JobDetails.tsx
+++ frontend/awx/views/jobs/JobDetails.tsx
@@ -3,17 +3,29 @@ import { useParams } from 'react-router-dom';
 import { PageDetails, PageDetailsFromColumns } from '../../../../framework';
 import { useJobsColumns } from './hooks/useJobsColumns';
 import { useGetJob } from './JobPage';
+import { PageDetailCodeEditor } from '../../../../framework/PageDetails/PageDetailCodeEditor';
+import { useTranslation } from 'react-i18next';
+import { EmptyStateNoData } from '../../../../framework/components/EmptyStateNoData';
 
 export function JobDetails() {
-  const params = useParams<{ id: string; job_type: string }>();
-  const { job } = useGetJob(params.id, params.job_type);
+  const { t } = useTranslation();
+  const params = useParams<{ id: string; job_type: string; job_id: string }>();
+  const { job } = useGetJob(params.job_id ? params.job_id : params.id, params.job_type);
   const columns = useJobsColumns();
 
+  if (params.job_id && !job)
+    return <EmptyStateNoData title="" description={t('Workflow job deleted')} />;
   if (!job) return <Skeleton />;
 
   return (
     <PageDetails>
       <PageDetailsFromColumns columns={columns} item={job} />
+      <PageDetailCodeEditor
+        label={t`Extra Variables`}
+        showCopyToClipboard
+        data-cy="inventory-source-detail-variables"
+        value={job.extra_vars ?? ''}
+      />
     </PageDetails>
   );
 }
