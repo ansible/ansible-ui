f933f01d3a5aebeb3b37d4f10c67cdf8a4763cbb
lgalis <lgalis@redhat.com>
2024-03-08 15:27:11 -0500

    Add credential types

--- cypress/support/eda-commands.ts
+++ cypress/support/eda-commands.ts
@@ -16,7 +16,6 @@ import {
 } from '../../frontend/eda/interfaces/EdaRulebookActivation';
 import { EdaUser, EdaUserCreateUpdate } from '../../frontend/eda/interfaces/EdaUser';
 import {
-  CredentialTypeEnum,
   ImportStateEnum,
   RestartPolicyEnum,
   RoleDetail,
@@ -239,7 +238,7 @@ Cypress.Commands.add('pollEdaResults', (url: string) => {
 Cypress.Commands.add('createEdaCredential', () => {
   cy.requestPost<EdaCredentialCreate>(edaAPI`/eda-credentials/`, {
     name: 'E2E Credential ' + randomString(4),
-    credential_type_id: CredentialTypeEnum.ContainerRegistry,
+    credential_type_id: 1,
     secret: 'test token',
     description: 'This is a container registry credential',
     username: 'admin',
