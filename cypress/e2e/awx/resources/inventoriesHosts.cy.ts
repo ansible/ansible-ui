9fbb3ab82bccde5b375f9464fcb22c0956dc5daa
MilanPospisil <arkanus@seznam.cz>
2024-03-13 14:22:35 +0100

    Create function

--- cypress/e2e/awx/resources/inventoriesHosts.cy.ts
+++ cypress/e2e/awx/resources/inventoriesHosts.cy.ts
@@ -33,22 +33,28 @@ describe('inventory host', () => {
   });
 
   it('can create and delete a inventory host', () => {
-    const hostName = 'E2E Inventory host ' + randomString(4);
-    cy.get('[data-cy="empty-state-title"]').contains(
-      /^There are currently no hosts added to this inventory./
-    );
-    cy.clickButton(/^Create host$/);
-    cy.verifyPageTitle('Create Host');
-    cy.get('[data-cy="name"]').type(hostName);
-    cy.get('[data-cy="description"]').type('This is the description');
-    cy.getByDataCy('variables').type('test: true');
-    cy.clickButton(/^Create host/);
-    cy.hasDetail(/^Name$/, hostName);
-    //cy.hasDetail(/^Description$/, 'This is a description'); TODO: add after when description is fixed
-    cy.hasDetail(/^Variables$/, 'test: true');
-    cy.selectDetailsPageKebabAction('delete-host');
-    cy.get('[data-cy="empty-state-title"]').contains(
-      /^There are currently no hosts added to this inventory./
-    );
+    createAndDeleteHost(true);
   });
 });
+
+describe('standalone host', () => {});
+
+function createAndDeleteHost(inventory_host: boolean) {
+  const hostName = 'E2E Inventory host ' + randomString(4);
+  cy.get('[data-cy="empty-state-title"]').contains(
+    /^There are currently no hosts added to this inventory./
+  );
+  cy.clickButton(/^Create host$/);
+  cy.verifyPageTitle('Create Host');
+  cy.get('[data-cy="name"]').type(hostName);
+  cy.get('[data-cy="description"]').type('This is the description');
+  cy.getByDataCy('variables').type('test: true');
+  cy.clickButton(/^Create host/);
+  cy.hasDetail(/^Name$/, hostName);
+  //cy.hasDetail(/^Description$/, 'This is a description'); TODO: add after when description is fixed
+  cy.hasDetail(/^Variables$/, 'test: true');
+  cy.selectDetailsPageKebabAction('delete-host');
+  cy.get('[data-cy="empty-state-title"]').contains(
+    /^There are currently no hosts added to this inventory./
+  );
+}
