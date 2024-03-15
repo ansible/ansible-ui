d1ed35a9b171a7aec0db6810c04838a12ee283ce
prat98 <112966709+prat98@users.noreply.github.com>
2024-03-15 12:18:00 -0400

    Instances List Updates (#1819)

--- frontend/awx/administration/topology/Sidebar.tsx
+++ frontend/awx/administration/topology/Sidebar.tsx
@@ -8,22 +8,14 @@ export function InstanceDetailInner(props: {
   instance: Instance;
   instanceGroups: AwxItemsResponse<InstanceGroup> | undefined;
   instanceForks: number;
-  handleToggleInstance: (instance: Instance, isEnabled: boolean) => Promise<void>;
   handleInstanceForksSlider: (instance: Instance, value: number) => Promise<void>;
 }) {
-  const {
-    instance,
-    instanceGroups,
-    instanceForks,
-    handleToggleInstance,
-    handleInstanceForksSlider,
-  } = props;
+  const { instance, instanceGroups, instanceForks, handleInstanceForksSlider } = props;
   return (
     <InstanceDetailsTab
       numberOfColumns="single"
       instance={instance}
       instanceGroups={instanceGroups}
-      handleToggleInstance={handleToggleInstance}
       instanceForks={instanceForks}
       handleInstanceForksSlider={handleInstanceForksSlider}
     />
@@ -32,19 +24,13 @@ export function InstanceDetailInner(props: {
 
 export function InstanceDetailSidebar(props: { selectedId: string }) {
   const { selectedId } = props;
-  const {
-    instance,
-    instanceGroups,
-    handleToggleInstance,
-    handleInstanceForksSlider,
-    instanceForks,
-  } = useInstanceActions(selectedId);
+  const { instance, instanceGroups, handleInstanceForksSlider, instanceForks } =
+    useInstanceActions(selectedId);
 
   return instance ? (
     <InstanceDetailInner
       instance={instance}
       instanceGroups={instanceGroups ? instanceGroups : undefined}
-      handleToggleInstance={handleToggleInstance}
       handleInstanceForksSlider={handleInstanceForksSlider}
       instanceForks={instanceForks}
     />
