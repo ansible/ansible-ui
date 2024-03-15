f9dee791e218a959f76cfb2149933625e0f954d8
Martin Hradil <mhradil@redhat.com>
2024-03-15 20:31:19 +0000

    EdaUserDetails - fix loading state

--- frontend/eda/access/users/UserPage/EdaUserDetails.tsx
+++ frontend/eda/access/users/UserPage/EdaUserDetails.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useParams } from 'react-router-dom';
-import { LoadingPage } from '../../../../../framework/components/LoadingPage';
+import { LoadingState } from '../../../../../framework/components/LoadingState';
 import { UserDetails, UserDetailsType } from '../../../../common/access/UserDetails';
 import { useGet } from '../../../../common/crud/useGet';
 import { edaAPI } from '../../../common/eda-utils';
@@ -8,8 +8,8 @@ import { EdaUser } from '../../../interfaces/EdaUser';
 
 export function EdaUserDetails() {
   const params = useParams<{ id: string }>();
-  const { data: user } = useGet<EdaUser>(edaAPI`/users/${params.id ?? ''}/`);
-  if (!user) return <LoadingPage breadcrumbs tabs />;
+  const { data: user } = useGet<EdaUser>(params.id ? edaAPI`/users/${params.id}/` : null);
+  if (!user) return <LoadingState />;
 
   return (
     <>
