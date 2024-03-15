509d2a3963bb72d809689e8bca10a598f88cb3fb
MilanPospisil <arkanus@seznam.cz>
2024-03-15 14:52:20 +0100

    Actions

--- frontend/awx/resources/hosts/hooks/useHostsActions.tsx
+++ frontend/awx/resources/hosts/hooks/useHostsActions.tsx
@@ -36,7 +36,7 @@ export function useHostsActions(
     [onToggle]
   );
 
-  return useMemo<IPageAction<AwxHost>[]>(
+  let actions = useMemo<IPageAction<AwxHost>[]>(
     () => [
       {
         type: PageActionType.Switch,
@@ -87,4 +87,11 @@ export function useHostsActions(
     ],
     [t, handleToggleHost, pageNavigate, params.id, params.inventory_type, deleteHosts]
   );
+
+  if (params.inventory_type === 'smart_inventory')
+  {
+    return [];
+  }
+
+  return actions;
 }
