a6b24bc5cf38ad72b956f70e54e9b9064f274cf6
Salma Kochay <skochay330@gmail.com>
2024-03-18 13:13:32 -0400

    notifications list component test

---
+++ frontend/awx/administration/notifications/Notifications.cy.tsx
@@ -0,0 +1,153 @@
+import { ToolbarFilterType } from '../../../../framework';
+import * as useOptions from '../../../common/crud/useOptions';
+import { Notifications } from './Notifications';
+
+describe('Notifications.cy.tsx', () => {
+  describe('Error list', () => {
+    it('Displays error if notifications are not successfully loaded', () => {
+      cy.intercept({ method: 'GET', url: '/api/v2/notification_templates/*' }, { statusCode: 500 });
+      cy.mount(<Notifications />);
+      cy.contains('Error loading notifications');
+    });
+  });
+
+  describe('Non-empty list', () => {
+    it('Component renders', () => {
+      cy.intercept(
+        { method: 'GET', url: '/api/v2/notification_templates/*' },
+        { fixture: 'notification_templates.json' }
+      );
+      cy.mount(<Notifications />);
+      cy.verifyPageTitle('Notifications');
+      cy.get('table').find('tr').should('have.length', 2);
+    });
+
+    it('List has filter for Name', () => {
+      cy.intercept(
+        { method: 'GET', url: '/api/v2/notification_templates/*' },
+        { fixture: 'notification_templates.json' }
+      );
+      cy.mount(<Notifications />);
+      cy.intercept('/api/v2/notification_templates/?name__icontains=notification*').as(
+        'nameFilterRequest'
+      );
+      cy.filterTableByTextFilter('name', 'notification', { disableFilterSelection: true });
+      cy.wait('@nameFilterRequest');
+      cy.clearAllFilters();
+    });
+
+    it('Bulk deletion confirmation', () => {
+      cy.intercept(
+        { method: 'GET', url: '/api/v2/notification_templates/*' },
+        { fixture: 'notification_templates.json' }
+      );
+      cy.mount(<Notifications />);
+      cy.get('[type="checkbox"][id="select-all"]').check();
+      cy.clickToolbarKebabAction('delete-selected-notifications');
+      cy.contains('Delete notifications').should('be.visible');
+    });
+
+    it('Add notification template button is disabled if the user does not have the correct permissions', () => {
+      cy.intercept(
+        { method: 'GET', url: '/api/v2/notification_templates/*' },
+        { fixture: 'notification_templates.json' }
+      );
+      cy.mount(<Notifications />);
+      cy.get('[data-cy="add-notification-template"]').should('have.attr', 'aria-disabled', 'true');
+    });
+
+    it('Add notification template button is enabled if the user has the correct permissions', () => {
+      cy.stub(useOptions, 'useOptions').callsFake(() => ({
+        data: {
+          actions: {
+            POST: {
+              name: {
+                type: ToolbarFilterType.MultiText,
+                required: true,
+                label: 'Name',
+                max_length: 512,
+                help_text: 'Name of this notification template.',
+                filterable: true,
+              },
+            },
+          },
+        },
+      }));
+      cy.intercept(
+        { method: 'GET', url: '/api/v2/notification_templates/*' },
+        { fixture: 'notification_templates.json' }
+      );
+      cy.mount(<Notifications />);
+      cy.get('[data-cy="add-notification-template"]').should('have.attr', 'aria-disabled', 'false');
+    });
+
+    it('Copy notification template button is enabled if the user has the correct permissions', () => {
+      cy.intercept(
+        { method: 'GET', url: '/api/v2/notification_templates/*' },
+        { fixture: 'notification_templates.json' }
+      );
+      cy.mount(<Notifications />);
+      cy.get('[data-cy="copy-notification-template"]').should(
+        'have.attr',
+        'aria-disabled',
+        'false'
+      );
+    });
+
+    it('Edit notification template button is enabled if the user has the correct permissions', () => {
+      cy.intercept(
+        { method: 'GET', url: '/api/v2/notification_templates/*' },
+        { fixture: 'notification_templates.json' }
+      );
+      cy.mount(<Notifications />);
+      cy.get('[data-cy="edit-notification-template"]').should(
+        'have.attr',
+        'aria-disabled',
+        'false'
+      );
+    });
+  });
+
+  describe('Empty list', () => {
+    it('Empty state is displayed correctly for user with permission to add notification templates', () => {
+      cy.stub(useOptions, 'useOptions').callsFake(() => ({
+        data: {
+          actions: {
+            POST: {
+              name: {
+                type: ToolbarFilterType.MultiText,
+                required: true,
+                label: 'Name',
+                max_length: 512,
+                help_text: 'Name of this notification template.',
+                filterable: true,
+              },
+            },
+          },
+        },
+      }));
+      cy.intercept(
+        { method: 'GET', url: '/api/v2/notification_templates/*' },
+        { fixture: 'emptyList.json' }
+      );
+      cy.mount(<Notifications />);
+      cy.contains(/^No notifications found.$/);
+      cy.contains(/^Please add notification templates to populate this list.$/);
+      cy.contains('button', /^Add notification template$/).should('be.visible');
+    });
+
+    it('Empty state is displayed correctly for user without permission to add notifications templates', () => {
+      cy.stub(useOptions, 'useOptions').callsFake(() => ({ data: { actions: {} } }));
+      cy.intercept(
+        { method: 'GET', url: '/api/v2/notification_templates/*' },
+        { fixture: 'emptyList.json' }
+      );
+      cy.mount(<Notifications />);
+      cy.contains(/^You do not have permission to add notification templates.$/);
+      cy.contains(
+        /^Please contact your organization administrator if there is an issue with your access.$/
+      );
+      cy.contains('button', /^Add notification template$/).should('not.exist');
+    });
+  });
+});
--- frontend/awx/administration/notifications/Notifications.tsx
+++ frontend/awx/administration/notifications/Notifications.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { PageHeader, PageLayout, PageTable } from '../../../../framework';
+import { PageHeader, PageLayout, PageTable, usePageNavigate } from '../../../../framework';
 import { useAwxConfig } from '../../common/useAwxConfig';
 import { getDocsBaseUrl } from '../../common/util/getDocsBaseUrl';
 import { ActivityStreamIcon } from '../../common/ActivityStreamIcon';
@@ -10,6 +10,9 @@ import { NotificationTemplate } from '../../interfaces/NotificationTemplate';
 import { useAwxView } from '../../common/useAwxView';
 import { useNotificationsToolbarActions } from './hooks/useNotificationsToolbarActions';
 import { useNotificationsRowActions } from './hooks/useNotificationsRowActions';
+import { useOptions } from '../../../common/crud/useOptions';
+import { ActionsResponse, OptionsResponse } from '../../interfaces/OptionsResponse';
+import { AwxRoute } from '../../main/AwxRoutes';
 
 export function Notifications() {
   const { t } = useTranslation();
@@ -21,9 +24,17 @@ export function Notifications() {
     tableColumns,
   });
   const config = useAwxConfig();
+  const pageNavigate = usePageNavigate();
 
   const toolbarActions = useNotificationsToolbarActions(view.unselectItemsAndRefresh);
   const rowActions = useNotificationsRowActions(view.unselectItemsAndRefresh);
+
+  const notificationsOptions = useOptions<OptionsResponse<ActionsResponse>>(
+    awxAPI`/notification_templates/`
+  ).data;
+  const canAddNotificationTemplate = Boolean(
+    notificationsOptions && notificationsOptions.actions && notificationsOptions.actions['POST']
+  );
 
   return (
     <PageLayout>
@@ -42,8 +53,26 @@ export function Notifications() {
         toolbarActions={toolbarActions}
         rowActions={rowActions}
         errorStateTitle={t('Error loading notifications')}
-        emptyStateTitle={t('No notifications found')}
-        emptyStateDescription={t('Please add notification templates to populate this list.')}
+        emptyStateTitle={
+          canAddNotificationTemplate
+            ? t('No notifications found.')
+            : t('You do not have permission to add notification templates.')
+        }
+        emptyStateDescription={
+          canAddNotificationTemplate
+            ? t('Please add notification templates to populate this list.')
+            : t(
+                'Please contact your organization administrator if there is an issue with your access.'
+              )
+        }
+        emptyStateButtonText={
+          canAddNotificationTemplate ? t('Add notification template') : undefined
+        }
+        emptyStateButtonClick={
+          canAddNotificationTemplate
+            ? () => pageNavigate(AwxRoute.AddNotificationTemplate)
+            : undefined
+        }
         {...view}
       />
     </PageLayout>
