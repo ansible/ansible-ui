96728e1469d52c69845195ffc26047bb1d9d2202
Maeve Hoffer <dhoffer@redhat.com>
2024-03-18 16:05:00 -0700

    remove unnecessary credentialType interface

--- frontend/awx/access/credential-types/hooks/useCredentialTypesFilters.tsx
+++ frontend/awx/access/credential-types/hooks/useCredentialTypesFilters.tsx
@@ -3,12 +3,11 @@ import {
   useModifiedByToolbarFilter,
 } from '../../../common/awx-toolbar-filters';
 import { useDynamicToolbarFilters } from '../../../common/useDynamicFilters';
-import { CredentialType } from '../../../interfaces/CredentialType';
 
 export function useCredentialTypesFilters() {
   const createdByToolbarFilter = useCreatedByToolbarFilter();
   const modifiedByToolbarFilter = useModifiedByToolbarFilter();
-  const toolbarFilters = useDynamicToolbarFilters<CredentialType>({
+  const toolbarFilters = useDynamicToolbarFilters({
     optionsPath: 'credential_types',
     preSortedKeys: ['name'],
     preFilledValueKeys: ['name'],
