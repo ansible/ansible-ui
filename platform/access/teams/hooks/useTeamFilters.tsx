34fbd8d8ac74790bfe275783e292477fe0ba87b9
James Talton <talton@gmail.com>
2024-03-19 13:14:37 -0400

    Update team and user filters
    Signed-off-by: James Talton <talton@gmail.com>

--- platform/access/teams/hooks/useTeamFilters.tsx
+++ platform/access/teams/hooks/useTeamFilters.tsx
@@ -1,38 +1,14 @@
 import { useMemo } from 'react';
-import { IToolbarFilter, ToolbarFilterType } from '../../../../framework';
-import { IToolbarAsyncMultiSelectFilter } from '../../../../framework/PageToolbar/PageToolbarFilters/ToolbarAsyncMultiSelectFilter';
-import { AsyncQueryLabel } from '../../../../framework/components/AsyncQueryLabel';
+import { IToolbarFilter } from '../../../../framework';
 import { useNameToolbarFilter } from '../../../../frontend/awx/common/awx-toolbar-filters';
-import { gatewayAPI } from '../../../api/gateway-api-utils';
-import { useQueryPlatformOptions } from '../../../common/useQueryPlatformOptions';
-import { PlatformOrganization } from '../../../interfaces/PlatformOrganization';
+import { usePlatformOrganizationsFilter } from '../../organizations/hooks/usePlatformOrganizationsFilter';
 
 export function useTeamFilters() {
   const nameToolbarFilter = useNameToolbarFilter();
-  const queryOptions = useQueryPlatformOptions<PlatformOrganization, 'name', 'id'>({
-    url: gatewayAPI`/organizations/`,
-    labelKey: 'name',
-    valueKey: 'id',
-    orderQuery: 'order_by',
-  });
-  const organizationFilter = useMemo(() => {
-    const filter: IToolbarAsyncMultiSelectFilter = {
-      key: 'organization',
-      query: 'organization',
-      label: 'Organization',
-      type: ToolbarFilterType.AsyncMultiSelect,
-      queryOptions,
-      placeholder: 'Select organizations',
-      queryPlaceholder: 'Loading organizations...',
-      queryErrorText: 'Failed to load organizations.',
-      queryLabel: (id: string) => <AsyncQueryLabel id={id} url={gatewayAPI`/organizations/`} />,
-    };
-    return filter;
-  }, [queryOptions]);
-
+  const organizationsFilter = usePlatformOrganizationsFilter();
   const toolbarFilters = useMemo<IToolbarFilter[]>(
-    () => [nameToolbarFilter, organizationFilter],
-    [nameToolbarFilter, organizationFilter]
+    () => [nameToolbarFilter, organizationsFilter],
+    [nameToolbarFilter, organizationsFilter]
   );
   return toolbarFilters;
 }
