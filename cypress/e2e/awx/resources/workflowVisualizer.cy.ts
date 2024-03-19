a092e635681806e6fed982850883263dab161248
akus062381 <sarah.akus@gmail.com>
2024-03-19 17:33:42 -0400

    add prompt on launch node test

--- cypress/e2e/awx/resources/workflowVisualizer.cy.ts
+++ cypress/e2e/awx/resources/workflowVisualizer.cy.ts
@@ -43,15 +43,15 @@ describe('Workflow Visualizer', () => {
     }).then((wfjt) => (workflowJobTemplate = wfjt));
   });
 
-  afterEach(() => {
-    cy.deleteAwxWorkflowJobTemplate(workflowJobTemplate, { failOnStatusCode: false });
-  });
+  // afterEach(() => {
+  //   cy.deleteAwxWorkflowJobTemplate(workflowJobTemplate, { failOnStatusCode: false });
+  // });
 
-  after(function () {
-    cy.deleteAwxInventory(inventory, { failOnStatusCode: false });
-    cy.deleteAwxInventorySource(inventorySource, { failOnStatusCode: false });
-    cy.deleteAwxJobTemplate(jobTemplate, { failOnStatusCode: false });
-  });
+  // after(function () {
+  //   cy.deleteAwxInventory(inventory, { failOnStatusCode: false });
+  //   cy.deleteAwxInventorySource(inventorySource, { failOnStatusCode: false });
+  //   cy.deleteAwxJobTemplate(jobTemplate, { failOnStatusCode: false });
+  // });
 
   describe('Workflow Visualizer Create and Edit Nodes', () => {
     it('should render a workflow visualizer view with multiple nodes present', () => {
@@ -493,6 +493,58 @@ describe('Workflow Visualizer', () => {
             .then((jobId: string) => {
               cy.url().should('contain', `/jobs/workflow/${jobId}/output`);
             });
+        });
+    });
+
+    it('Can configure the prompt on launch values of a node, launch the job, and view the output screen', function () {
+      let projectNode: WorkflowNode;
+      let jobTemplateNode: WorkflowNode;
+      cy.createAwxWorkflowVisualizerProjectNode(workflowJobTemplate, project)
+        .then((projNode) => {
+          projectNode = projNode;
+          cy.createAwxWorkflowVisualizerJobTemplateNode(workflowJobTemplate, jobTemplate).then(
+            (jtNode) => {
+              jobTemplateNode = jtNode;
+              cy.createWorkflowJTSuccessNodeLink(projectNode, jobTemplateNode);
+            }
+          );
+        })
+        .then(() => {
+          cy.visit(`/templates/job_template/${jobTemplate?.id}/details`);
+          cy.getByDataCy('edit-template').click();
+          cy.contains('Edit Job Template').should('be.visible');
+          cy.getByDataCy('ask_variables_on_launch').click();
+          cy.getByDataCy('Submit').click();
+          cy.visit(`/templates/workflow_job_template/${workflowJobTemplate?.id}/visualizer`);
+          cy.contains('Workflow Visualizer').should('be.visible');
+          cy.get(`g[data-id=${jobTemplateNode.id}] .pf-topology__node__action-icon`).click({
+            force: true,
+          });
+          cy.get('li[data-cy="edit-node"]').click();
+          cy.contains('Edit step').should('be.visible');
+          cy.getByDataCy('Submit').click();
+          cy.getByDataCy('wizard-nav-item-nodePromptsStep').click();
+          cy.get('[class="view-lines monaco-mouse-cursor-text"]').type('foo: bar');
+          cy.getByDataCy('Submit').click();
+          cy.get('.scrollable-inner').scrollIntoView({ offset: { top: 150, left: 0 } });
+          cy.getByDataCy('extra-vars').should('contain', 'foo: bar');
+          cy.getByDataCy('wizard-next').click();
+          cy.getByDataCy('workflow-visualizer-toolbar-save').click();
+          cy.get('[data-cy="alert-toaster"]').should(
+            'have.text',
+            'Success alert:Successfully saved workflow visualizer'
+          );
+          cy.get('[data-cy="alert-toaster"]').within(() => {
+            cy.get('[class="pf-v5-c-alert__action"]').click();
+          });
+          cy.getByDataCy('workflow-visualizer-toolbar-kebab').click();
+          cy.getByDataCy('workflow-visualizer-toolbar-launch').click();
+          cy.contains('Output').should('be.visible');
+          cy.contains('Running').should('be.visible');
+          cy.contains('Unreachable').should('be.visible');
+          cy.get(`g[class*="node-label"]`).contains(jobTemplate.name).should('be.visible');
+          cy.get(`g[class*="node-label"]`).contains(project.name).should('be.visible');
+          cy.contains('Success').should('be.visible');
         });
     });
   });
