38642aa188e0b7c61de5010daddfb4b39c642256
lgalis <lgalis@redhat.com>
2024-03-21 18:48:15 -0400

    Add organization pages

--- frontend/eda/access/organizations/utils/getOrganizationByName.ts
+++ frontend/eda/access/organizations/utils/getOrganizationByName.ts
@@ -1,11 +1,11 @@
 import { requestGet } from '../../../../common/crud/Data';
-import { awxAPI } from '../../../common/api/awx-utils';
-import { AwxItemsResponse } from '../../../common/AwxItemsResponse';
-import { Organization } from '../../../interfaces/Organization';
+import { EdaItemsResponse } from '../../../common/EdaItemsResponse';
+import { EdaOrganization } from '../../../interfaces/EdaOrganization';
+import { edaAPI } from '../../../common/eda-utils';
 
 export async function getOrganizationByName(organizationName: string) {
-  const itemsResponse = await requestGet<AwxItemsResponse<Organization>>(
-    awxAPI`/organizations/?name=${organizationName}`
+  const itemsResponse = await requestGet<EdaItemsResponse<EdaOrganization>>(
+    edaAPI`/organizations/?name=${organizationName}`
   );
   if (itemsResponse.results.length >= 1) {
     return itemsResponse.results[0];
