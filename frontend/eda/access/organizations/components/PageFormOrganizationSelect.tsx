bd65c2553dc40a40e0b4c531046ee57e8d84af45
lgalis <lgalis@redhat.com>
2024-03-21 18:44:02 -0400

    Add organization pages

--- frontend/eda/access/organizations/components/PageFormOrganizationSelect.tsx
+++ frontend/eda/access/organizations/components/PageFormOrganizationSelect.tsx
@@ -2,8 +2,10 @@ import { FieldPath, FieldValues } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { PageFormSingleSelectEdaResource } from '../../../common/PageFormSingleSelectEdaResource';
 import { EdaOrganization } from '../../../interfaces/EdaOrganization';
-import { useOrganizationsColumns, useOrganizationsFilters } from '../Organizations';
+
 import { edaAPI } from '../../../common/eda-utils';
+import { useOrganizationColumns } from '../hooks/useOrganizationColumns';
+import { useOrganizationsFilters } from '../hooks/useOrganizationsFilters';
 
 /**
  * A form input for selecting an organization.
@@ -18,7 +20,7 @@ export function PageFormSelectOrganization<
   TFieldName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
 >(props: { name: TFieldName; isRequired?: boolean; isDisabled?: boolean; helperText?: string }) {
   const { t } = useTranslation();
-  const organizationColumns = useOrganizationsColumns({ disableLinks: true });
+  const organizationColumns = useOrganizationColumns();
   const organizationFilters = useOrganizationsFilters();
   return (
     <PageFormSingleSelectEdaResource<EdaOrganization, TFieldValues, TFieldName>
