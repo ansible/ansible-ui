c7411d3ed2aab6a74e1f71de91513a2d197954d6
Shaiah Emigh-Doyle <semighdo@redhat.com>
2024-03-23 18:01:11 -0400

    user for hub

--- frontend/hub/main/HubMasthead.tsx
+++ frontend/hub/main/HubMasthead.tsx
@@ -20,9 +20,11 @@ import { HubItemsResponse } from '../common/useHubView';
 import { HubRoute } from './HubRoutes';
 import GalaxyBrand from './galaxy-logo.svg';
 import { useHubProductVersionInfo } from './useHubProductVersionInfo';
+import { HubUser } from '../common/useHubContext';
 
 export function HubMasthead() {
   const { t } = useTranslation();
+  const { data: userInfo } = useGet<HubUser>(hubAPI`/_ui/v1/me/`);
   const versionInfo = useHubProductVersionInfo();
   const openAnsibleAboutModal = useAnsibleAboutModal();
   useHubNotifications();
