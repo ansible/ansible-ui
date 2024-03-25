5f5801ec3b96248d793daab0c57c875c1e370b8d
Adriana Cruz <acruzgon@redhat.com>
2024-03-25 11:05:53 -0600

    [Schedule form] Schedule Form Survey Step

--- frontend/awx/interfaces/Survey.ts
+++ frontend/awx/interfaces/Survey.ts
@@ -1,18 +1,18 @@
 export interface Survey {
   name: string;
   description: string;
-  spec: [
-    {
-      question_name: string;
-      question_description: string;
-      required: boolean;
-      type: string;
-      variable: string;
-      min: number;
-      max: number;
-      default: string;
-      choices: string;
-      new_question: boolean;
-    },
-  ];
+  spec: [Spec];
+}
+
+export interface Spec {
+  question_name: string;
+  question_description: string;
+  required: boolean;
+  type: string;
+  variable: string;
+  min: number;
+  max: number;
+  default: string;
+  choices: string[];
+  new_question: boolean;
 }
