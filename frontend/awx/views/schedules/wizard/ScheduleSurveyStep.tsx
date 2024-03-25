031de01b7b40d57ce8972e9299e924916747c401
Adriana Cruz <acruzgon@redhat.com>
2024-03-25 12:32:27 -0600

    fixing wording for min value in PageFormTextInput and fixing wizard component test

--- frontend/awx/views/schedules/wizard/ScheduleSurveyStep.tsx
+++ frontend/awx/views/schedules/wizard/ScheduleSurveyStep.tsx
@@ -27,7 +27,7 @@ export function ScheduleSurveyStep() {
   });
 
   return (
-    <PageFormSection singleColumn>
+    <PageFormSection>
       {survey_spec?.spec.map((element, index) =>
         element.type === 'text' ? (
           <PageFormTextInput
