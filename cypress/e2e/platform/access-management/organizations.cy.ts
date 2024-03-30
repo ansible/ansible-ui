bda93895f9b90145dd30966afdecada22e608a2a
Vishali Kamenani <95239214+vishsanghishetty@users.noreply.github.com>
2024-03-29 20:25:54 -0400

    Platform Org with teams and admins tab tests (#224)

--- cypress/e2e/platform/access-management/organizations.cy.ts
+++ cypress/e2e/platform/access-management/organizations.cy.ts
@@ -1,6 +1,8 @@
 import { randomString } from '../../../../framework/utils/random-string';
 import { gatewayV1API } from '../../../../platform/api/gateway-api-utils';
+import { PlatformUser } from '../../../../platform/interfaces/PlatformUser';
 import { PlatformOrganization } from '../../../../platform/interfaces/PlatformOrganization';
+import { PlatformTeam } from '../../../../platform/interfaces/PlatformTeam';
 
 describe('Organizations - create, edit and delete', () => {
   const organizationName = 'Platform E2E Organization ' + randomString(4);
@@ -123,6 +125,157 @@ describe('Organizations - create, edit and delete', () => {
         cy.clickButton(/^Close$/);
         cy.clickButton(/^Clear all filters$/);
       });
+    });
+  });
+});
+
+describe('Platform Teams - Users, Admins and Teams tabs', function () {
+  let organization: PlatformOrganization;
+
+  before(() => {
+    cy.platformLogin();
+  });
+
+  beforeEach(() => {
+    cy.createPlatformOrganization().then((org) => {
+      organization = org;
+    });
+
+    cy.navigateTo('platform', 'organizations');
+    cy.verifyPageTitle('Organizations');
+    cy.setTableView('table');
+  });
+
+  afterEach(() => {
+    cy.deletePlatformOrganization(organization, { failOnStatusCode: false });
+  });
+
+  // tests for tabs Roles, Users, Administrators, and Resource Access
+
+  // Users tab
+  it.skip('can add and remove users to the team via the organization users tab', function () {
+    // 1. creates a team with global organization and the users createdUser1 and createdUser2
+    // 2. add user createdUser3 to the team from the users tab
+    // 3. remove user createdUser1 from the team users tab list row items
+  });
+
+  // Administrators tab
+  it('can add and remove users as administrators to the organization from the administrators tab', function () {
+    // 1. creates an organization - createdPlatformOrg
+    // 2. add user createdUser1 as administrators to the organization from the Administrators tab
+    // 3. remove user createdUser2 as admin of the team from the Administrators tab list items
+
+    cy.createPlatformOrganization().then((createdPlatformOrg) => {
+      cy.createPlatformUser().then((createdUser1: PlatformUser) => {
+        cy.filterTableByTextFilter('name', createdPlatformOrg.name, {
+          disableFilterSelection: true,
+        });
+        cy.getByDataCy('name-column-cell').contains(createdPlatformOrg.name).click();
+        cy.clickTab('Administrators', true);
+
+        // search first user createdUser1 and add as admin
+        cy.intercept('GET', gatewayV1API`/users/?order_by=username&page=1&page_size=10`).as(
+          'getUsers'
+        );
+        cy.getByDataCy('add-administrators').click();
+
+        // wait for the users call to be done done and load the users in the add admin modal
+        cy.wait('@getUsers');
+        cy.searchAndDisplayResourceInModalPlatform(createdUser1.username);
+        cy.intercept(
+          'GET',
+          gatewayV1API`/users/?username__contains=${createdUser1.username}&order_by=username&page=1&page_size=10`
+        ).as('getUsersAfterSearch');
+
+        cy.wait('@getUsersAfterSearch');
+        cy.intercept(
+          'POST',
+          gatewayV1API`/organizations/${createdPlatformOrg.id.toString()}/admins/associate`
+        ).as('associateUser1AsAdmin');
+        cy.intercept(
+          'GET',
+          gatewayV1API`/organizations/${createdPlatformOrg.id.toString()}/users/?order_by=username&page=1&page_size=10`
+        ).as('getUsersList');
+        cy.intercept(
+          'GET',
+          gatewayV1API`/organizations/${createdPlatformOrg.id.toString()}/admins/?order_by=username&page=1&page_size=10`
+        ).as('getAdminsList');
+        cy.selectItemFromLookupModalPlatform();
+        cy.wait('@associateUser1AsAdmin')
+          .its('response')
+          .then((response) => {
+            expect(response?.statusCode).to.eql(204);
+            cy.wait('@getAdminsList')
+              .its('response.body.results')
+              .then((results: PlatformUser[]) => {
+                const admins = results?.map((user) => user.username);
+                expect(admins).to.have.lengthOf(1);
+                expect(admins[0]).to.equal(createdUser1.username);
+                cy.get('tr[data-cy^="row-id-"]').should('have.length', 1);
+              });
+          });
+        cy.intercept(
+          'POST',
+          gatewayV1API`/organizations/${createdPlatformOrg.id.toString()}/admins/disassociate`
+        ).as('disassociateUser1AsAdmin');
+        cy.clickTableRowKebabAction(createdUser1.username, 'remove-administrator', false);
+        cy.clickModalConfirmCheckbox();
+        cy.clickButton(/^Remove administrators/);
+        cy.wait('@disassociateUser1AsAdmin')
+          .its('response')
+          .then((response) => {
+            expect(response?.statusCode).to.eql(204);
+            cy.contains(/^Success$/);
+            cy.clickButton(/^Close$/);
+            cy.wait('@getAdminsList')
+              .its('response.body.results')
+              .then((results: PlatformUser[]) => {
+                const admins = results?.map((user) => user.username);
+                expect(admins).to.have.lengthOf(0);
+              });
+            cy.get('tr[data-cy^="row-id-"]').should('have.length', 0);
+          });
+        cy.deletePlatformUser(createdUser1, { failOnStatusCode: false });
+        cy.deletePlatformOrganization(createdPlatformOrg, { failOnStatusCode: false });
+      });
+    });
+  });
+
+  // Create team from teams tab
+  it('can create a team from the teams tab, add an organization and assert it is added', function () {
+    // 1. creates an organization - createdPlatformOrg
+    // 2. create a team - createdPlatformTeam from the Teams tab with the above organization
+    // 3. verify the created team createdPlatformTeam is created and is added to the organization createdPlatformOrg
+    cy.createPlatformOrganization().then((createdPlatformOrg) => {
+      cy.filterTableByTextFilter('name', createdPlatformOrg.name, {
+        disableFilterSelection: true,
+      });
+      cy.getByDataCy('name-column-cell').contains(createdPlatformOrg.name).click();
+      cy.clickTab('Teams', true);
+      const teamName = `E2E PlatformTeam ${randomString(4)}`;
+      cy.intercept('POST', gatewayV1API`/teams/`).as('createTeam');
+      cy.getByDataCy('create-team').click();
+      cy.get('[data-cy="name"]').type(teamName);
+      cy.singleSelectByDataCy('organization', `${createdPlatformOrg.name}`);
+      cy.clickButton(/^Create team$/);
+      cy.wait('@createTeam')
+        .its('response.body')
+        .then((createdPlatformTeam: PlatformTeam) => {
+          cy.verifyPageTitle(teamName);
+          cy.get('[data-cy="organization"]').should('have.text', createdPlatformOrg.name);
+          cy.clickPageAction('delete-team');
+          cy.intercept('DELETE', gatewayV1API`/teams/${createdPlatformTeam.id.toString()}/`).as(
+            'deleteTeam'
+          );
+          cy.get('#confirm').click();
+          cy.clickButton(/^Delete team/);
+          cy.wait('@deleteTeam')
+            .its('response')
+            .then((response) => {
+              expect(response?.statusCode).to.eql(204);
+            });
+        });
+      cy.deletePlatformOrganization(createdPlatformOrg, { failOnStatusCode: false });
     });
   });
 });
