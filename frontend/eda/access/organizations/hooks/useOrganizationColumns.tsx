c09b620c75f68cca09200a21fb8ebfd7c99c355e
lgalis <lgalis@users.noreply.github.com>
2024-04-02 13:31:34 -0400

    [AAP-19886] Add organizations (#1888)

---
+++ frontend/eda/access/organizations/hooks/useOrganizationColumns.tsx
@@ -0,0 +1,52 @@
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ColumnTableOption, ITableColumn, TextCell, useGetPageUrl } from '../../../../../framework';
+import { EdaOrganization } from '../../../interfaces/EdaOrganization';
+import { EdaRoute } from '../../../main/EdaRoutes';
+
+export function useOrganizationColumns() {
+  const { t } = useTranslation();
+  const getPageUrl = useGetPageUrl();
+  return useMemo<ITableColumn<EdaOrganization>[]>(
+    () => [
+      {
+        header: t('Name'),
+        cell: (organization) => (
+          <TextCell
+            text={organization.name}
+            to={getPageUrl(EdaRoute.OrganizationPage, {
+              params: { id: organization.id },
+            })}
+          />
+        ),
+        card: 'name',
+        list: 'name',
+      },
+      {
+        header: t('Description'),
+        type: 'description',
+        value: (organization) => organization.description,
+        table: ColumnTableOption.description,
+        card: 'description',
+        list: 'description',
+      },
+      {
+        header: t('Created'),
+        type: 'datetime',
+        value: (organization) => organization.created_at,
+        table: ColumnTableOption.expanded,
+        card: 'hidden',
+        list: 'secondary',
+      },
+      {
+        header: t('Last modified'),
+        type: 'datetime',
+        value: (organization) => organization.modified_at,
+        table: ColumnTableOption.expanded,
+        card: 'hidden',
+        list: 'secondary',
+      },
+    ],
+    [getPageUrl, t]
+  );
+}
