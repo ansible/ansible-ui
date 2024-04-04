d3c94c37a8fc40996cdbf980c52b5d53d408e3df
akus062381 <sarah.akus@gmail.com>
2024-04-03 20:22:24 -0400

    finish testing structure

--- cypress/e2e/awx/resources/inventoryHost.cy.ts
+++ cypress/e2e/awx/resources/inventoryHost.cy.ts
@@ -66,11 +66,11 @@ describe('Inventory Host Tab Tests', () => {
     });
 
     it.skip('can edit a host in the hosts tab list of an inventory', () => {
-      //1) Use the inventory created in beforeEach block, access the host tab of that inventory
-      //2) Use the host created in the beforeEach block
-      //3) Assert the original values the host shows on its details page
-      //4) Intercept the edit call
-      //5) Use the interception to assert the edited values
+      //1) Use the inventory created in beforeEach block, navigate to the host tab of that inventory
+      //........................(use the host created in beforeEach block)
+      //2) On the host list, assert the original values the host shows on its details page. Edit the host.
+      //3) Intercept the edit call
+      //4) Use the interception to assert the edited values
     });
 
     it.skip('can delete a single host from the hosts list tab of an inventory', () => {
@@ -91,30 +91,154 @@ describe('Inventory Host Tab Tests', () => {
   });
 
   describe('Inventory Host Details Page Tests', () => {
-    it.skip('can edit a host from the details screen by navigating from the host tab list of an inventory', () => {});
+    beforeEach(() => {
+      cy.createAwxOrganization().then((org) => {
+        organization = org;
+        cy.createAWXCredential({
+          kind: 'machine',
+          organization: organization.id,
+          credential_type: 1,
+        }).then((cred) => {
+          machineCredential = cred;
+        });
+        cy.createAwxExecutionEnvironment({ organization: organization.id }).then((ee) => {
+          executionEnvironment = ee;
+          cy.createInventoryHost(organization).then((result) => {
+            const { inventory, host } = result;
+            cy.log('Inventory', inventory); //remove this once it is written into the tests below
+            cy.log('Host', host); //remove this once it is written into the tests below
+            cy.log('EE', ee); //remove this once it is written into the tests below
+            cy.log('Machine Cred', machineCredential); //remove this once it is written into the tests below
+          });
+        });
+      });
+    });
 
-    it.skip('can delete a host from the details screen by navigating from the host tab list of an inventory', () => {});
-  });
+    it.skip('can edit a host from the details screen by navigating from the host tab list of an inventory', () => {
+      //1) Use the inventory created in beforeEach block, navigate to the host tab of that inventory,
+      //........................then to the details page of that host (use the host created in beforeEach block)
+      //2) On the host details page, assert the original values the host shows on its details page. Edit the host.
+      //3) Intercept the edit call
+      //4) Use the interception to assert the edited values
+    });
 
-  describe('Inventory Host Facts Tab Tests', () => {
-    it.skip("can view a host's facts on the facts tab of a host inside an inventory", () => {});
+    it.skip('can delete a host from the details screen by navigating from the host tab list of an inventory', () => {
+      //1) Use the inventory created in beforeEach block, access the host tab of that inventory, then visit the host details page
+      //2) Use the host created in the beforeEach block
+      //3) Assert the details on the host details page
+      //4) Delete the host, intercept the Delete call
+      //5) Assert that the host is not found in a search; assert the statusCode of the Delete call
+    });
+
+    it.skip("can view a host's facts on the facts tab of a host inside an inventory", () => {
+      //1) Use the inventory created in beforeEach block, access the host tab of that inventory
+      //2) Visit the host details page, then navigate to the host Facts tab
+      //3) Assert that no facts are showing
+      //4) Create a JT designed to populate the facts for the host, launch the job template
+      //5) Navigate back to the facts tab of that host and assert that the facts exist
+    });
   });
 
   describe('Inventory Host Groups Tab Tests', () => {
-    it.skip('can view a list of all groups associated with the host on the groups host tab of an inventory', () => {});
+    beforeEach(() => {
+      cy.createAwxOrganization().then((org) => {
+        organization = org;
+        cy.createAWXCredential({
+          kind: 'machine',
+          organization: organization.id,
+          credential_type: 1,
+        }).then((cred) => {
+          machineCredential = cred;
+        });
+        cy.createAwxExecutionEnvironment({ organization: organization.id }).then((ee) => {
+          executionEnvironment = ee;
+          cy.createInventoryHostGroup(organization).then((result) => {
+            const { inventory, host, group } = result;
+            cy.log('Inventory', inventory); //remove this once it is written into the tests below
+            cy.log('Host', host); //remove this once it is written into the tests below
+            cy.log('Group', group); //remove this once it is written into the tests below
+            cy.log('EE', ee); //remove this once it is written into the tests below
+            cy.log('Machine Cred', machineCredential); //remove this once it is written into the tests below
+          });
+        });
+      });
+    });
 
-    it.skip('can associate a group to the host on the host tab of an inventory', () => {});
+    it.skip('can view a list of all group(s) associated with the host on the groups host tab of an inventory', () => {
+      //1) Utilize the inventory, host, and group that were created in the beforeEach block- they are linked together
+      //2) Navigate to the host details page, then navigate to the Groups tab of that host
+      //3) Assert the presence of the expected group in the list
+    });
 
-    it.skip('can disassociate a single group from the host on the host tab of an inventory', () => {});
+    it.skip('can associate a group to the host on the host tab of an inventory', () => {
+      //1) Utilize the inventory, host, and group that were created in the beforeEach block- they are linked together
+      //2) Navigate to the host details tab, then the groups tab. Assert that only one group is showing
+      //3) Inside the inventory, create an additional group
+      //4) Associate the host and new group with each other
+      //5) Assert the presence of that new group on the Groups tab inside of the Host, assert that 2 groups are now showing
+    });
 
-    it.skip('can bulk disassociate multiple groups from the host on the host tab of an inventory', () => {});
+    it.skip('can disassociate a single group from the host on the host tab of an inventory', () => {
+      //1) Utilize the inventory, host, and group that were created in the beforeEach block- they are linked together
+      //2) Navigate to the host details tab, then the groups tab. Assert the one group showing there
+      //3) Disassociate that group from the host
+      //4) Assert that the groups tab inside the host now shows an empty list
+    });
 
-    it.skip('can run an ad-hoc command against the host on the groups tab of a host-inventory', () => {});
+    it.skip('can bulk disassociate multiple groups from the host on the host tab of an inventory', () => {
+      //1) Utilize the inventory, host, and group that were created in the beforeEach block- they are linked together
+      //2) Have the test create 2 additional groups and associate them to the host
+      //3) Navigate to the host details tab, then the groups tab. Assert the three groups showing there
+      //4) Disassociate all groups from the host
+      //5) Assert that the groups tab inside the host now shows an empty list
+    });
+
+    it.skip('can run an ad-hoc command against the host on the groups tab of a host-inventory from the host details page', () => {
+      //1) Use the inventory created in beforeEach block, access the host tab of that inventory, visit the host details page
+      //2) Use the host, EE, and credential created in the beforeEach block- these resources are needed to run a command against a host
+      //3) Assert redirect to the job output screen
+      //4) Navigate to the details page of the job and assert the values there match what was entered in the Run Command Wizard
+      //5) Navigate back to the Inventory -> Jobs Tab to assert that the Run Command job shows up there
+    });
   });
 
   describe('Inventory Host Jobs Tab Tests', () => {
-    it.skip('can launch a job template that uses an inventory with a particular host and view the job on the host jobs tab inside the inventory', () => {});
+    beforeEach(() => {
+      cy.createAwxOrganization().then((org) => {
+        organization = org;
+        cy.createAWXCredential({
+          kind: 'machine',
+          organization: organization.id,
+          credential_type: 1,
+        }).then((cred) => {
+          machineCredential = cred;
+        });
+        cy.createAwxExecutionEnvironment({ organization: organization.id }).then((ee) => {
+          executionEnvironment = ee;
+          cy.createInventoryHostGroup(organization).then((result) => {
+            const { inventory, host, group } = result;
+            cy.log('Inventory', inventory); //remove this once it is written into the tests below
+            cy.log('Host', host); //remove this once it is written into the tests below
+            cy.log('Group', group); //remove this once it is written into the tests below
+            cy.log('EE', ee); //remove this once it is written into the tests below
+            cy.log('Machine Cred', machineCredential); //remove this once it is written into the tests below
+          });
+        });
+      });
+    });
 
-    it.skip('can cancel a currently running job from the host jobs tab inside an inventory', () => {});
+    it.skip('can launch a job template that uses an inventory with a particular host and view the job on the host jobs tab inside the inventory', () => {
+      //1) Use the inventory and host created in beforeEach block
+      //2) Have the test create a job template that uses that inventory, launch the job template, wait for job to finish
+      //3) Navigate back to inventory -> host tab -> jobs tab -> assert presence of job in that list
+    });
+
+    it.skip('can cancel a currently running job from the host jobs tab inside an inventory', () => {
+      //1) Use the inventory and host created in beforeEach block
+      //2) Have the test create a job template that uses that inventory, utilize a playbook that will cause the job to be long running
+      //3) Launch the job template
+      //4) Navigate back to inventory -> host tab -> jobs tab -> assert presence of job in that list
+      //5) Cancel the job and assert that it has been canceled
+    });
   });
 });
