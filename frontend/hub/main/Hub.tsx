e3a5695d978337996dbae541763ec64128c31779
James Talton <talton@gmail.com>
2024-04-05 12:19:37 -0400

    Refactor routing in Awx, Eda, and Hub components
    Signed-off-by: James Talton <talton@gmail.com>

--- frontend/hub/main/Hub.tsx
+++ frontend/hub/main/Hub.tsx
@@ -1,6 +1,14 @@
 import { lazy } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 const Main = lazy(() => import('./HubMain'));
+
 document.body.innerHTML = '<div id="app"></div>';
+
 const root = createRoot(document.getElementById('app')!);
-root.render(<Main />);
+root.render(
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>
+);
