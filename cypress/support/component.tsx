53794682a0ac5114fbf350fd9ec331e8ed4e33a5
James Talton <talton@gmail.com>
2024-04-08 09:26:07 -0400

    fix component tests
    Signed-off-by: James Talton <talton@gmail.com>

--- cypress/support/component.tsx
+++ cypress/support/component.tsx
@@ -28,6 +28,7 @@ import { mount } from 'cypress/react18';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { SWRResponse } from 'swr';
 import { PageFramework } from '../../framework';
+import { AwxItemsResponse } from '../../frontend/awx/common/AwxItemsResponse';
 import { AwxActiveUserContext } from '../../frontend/awx/common/useAwxActiveUser';
 import { User } from '../../frontend/awx/interfaces/User';
 import '../../frontend/common/i18n';
@@ -79,7 +80,17 @@ Cypress.Commands.add('mount', (component, route, activeUserFixture) => {
     return mount(
       <MemoryRouter initialEntries={route?.initialEntries || ['/1']}>
         <PageFramework defaultRefreshInterval={60}>
-          <AwxActiveUserContext.Provider value={{ data: activeUser } as SWRResponse}>
+          <AwxActiveUserContext.Provider
+            value={
+              {
+                data: { count: 1, results: [activeUser] },
+                error: undefined,
+                mutate: () => Promise.resolve(),
+                isLoading: false,
+                isValidating: false,
+              } as SWRResponse<AwxItemsResponse<User>>
+            }
+          >
             <Page>
               <Routes>
                 <Route path={`${route?.path || '/:id/*'}`} element={component} />
