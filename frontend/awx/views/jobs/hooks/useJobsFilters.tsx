0c20e01e4de3359e52a8bd7ca6f543213eb4899d
Kia Lam <digitalanime@gmail.com>
2024-04-08 09:55:42 -0700

    Pass queryParams to useJobsFilters hook.

--- frontend/awx/views/jobs/hooks/useJobsFilters.tsx
+++ frontend/awx/views/jobs/hooks/useJobsFilters.tsx
@@ -1,15 +1,18 @@
+import { QueryParams } from '../../../common/useAwxView';
 import { useDynamicToolbarFilters } from '../../../common/useDynamicFilters';
 
-export function useJobsFilters() {
+export function useJobsFilters(queryParams: QueryParams = {}) {
   const toolBarFilters = useDynamicToolbarFilters({
     optionsPath: 'unified_jobs',
     preSortedKeys: ['name', 'description', 'status'],
     preFilledValueKeys: {
       name: {
         apiPath: 'unified_jobs',
+        queryParams: queryParams,
       },
       id: {
         apiPath: 'unified_jobs',
+        queryParams: queryParams,
       },
     },
   });
