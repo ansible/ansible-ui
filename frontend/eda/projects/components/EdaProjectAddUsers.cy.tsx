9d087ca8ce5682400146d950ac0cecdbe6cdef82
Vidya Nambiar <vnambiar@redhat.com>
2024-04-09 17:37:18 -0400

    component tests for adding users to project

--- frontend/eda/projects/components/EdaProjectAddUsers.cy.tsx
+++ frontend/eda/projects/components/EdaProjectAddUsers.cy.tsx
@@ -1,10 +1,10 @@
 import { edaAPI } from '../../common/eda-utils';
-import { EdaProjectAddTeams } from './EdaProjectAddTeams';
+import { EdaProjectAddUsers } from './EdaProjectAddUsers';
 
-describe('EdaProjectAddTeams', () => {
-  const component = <EdaProjectAddTeams />;
-  const path = '/projects/:id/team-access/add-teams';
-  const initialEntries = [`/projects/1/team-access/add-teams`];
+describe('EdaProjectAddUsers', () => {
+  const component = <EdaProjectAddUsers />;
+  const path = '/projects/:id/user-access/add-users';
+  const initialEntries = [`/projects/1/user-access/add-users`];
   const params = {
     path,
     initialEntries,
@@ -12,39 +12,39 @@ describe('EdaProjectAddTeams', () => {
 
   beforeEach(() => {
     cy.intercept('GET', edaAPI`/projects/*`, { fixture: 'edaProject.json' });
-    cy.intercept('GET', edaAPI`/teams/?order_by=name*`, { fixture: 'edaTeams.json' });
+    cy.intercept('GET', edaAPI`/users/?order_by=username*`, { fixture: 'edaUsers.json' });
     cy.intercept('GET', edaAPI`/role_definitions/?content_type__model=project*`, {
       fixture: 'edaProjectRoles.json',
     });
     cy.mount(component, params);
   });
   it('should render with correct steps', () => {
-    cy.get('[data-cy="wizard-nav"] li').eq(0).should('contain.text', 'Select team(s)');
+    cy.get('[data-cy="wizard-nav"] li').eq(0).should('contain.text', 'Select user(s)');
     cy.get('[data-cy="wizard-nav"] li').eq(1).should('contain.text', 'Select roles to apply');
     cy.get('[data-cy="wizard-nav"] li').eq(2).should('contain.text', 'Review');
-    cy.get('[data-cy="wizard-nav-item-teams"] button').should('have.class', 'pf-m-current');
-    cy.get('table tbody').find('tr').should('have.length', 4);
+    cy.get('[data-cy="wizard-nav-item-users"] button').should('have.class', 'pf-m-current');
+    cy.get('table tbody').find('tr').should('have.length', 6);
   });
-  it('can filter teams by name', () => {
-    cy.intercept(edaAPI`/teams/?name=Gal*`).as('nameFilterRequest');
+  it('can filter users by username', () => {
+    cy.intercept(edaAPI`/users/?name=demo*`).as('nameFilterRequest');
     cy.get('[data-cy="text-input"]').within(() => {
-      cy.get('input').clear().type('Gal', { delay: 0 });
+      cy.get('input').clear().type('demo', { delay: 0 });
     });
     cy.getByDataCy('apply-filter').click();
     cy.wait('@nameFilterRequest');
     cy.clearAllFilters();
   });
-  it('should validate that at least one team is selected for moving to next step', () => {
-    cy.get('table tbody').find('tr').should('have.length', 4);
+  it('should validate that at least one user is selected for moving to next step', () => {
+    cy.get('table tbody').find('tr').should('have.length', 6);
     cy.clickButton(/^Next$/);
-    cy.get('.pf-v5-c-alert__title').should('contain.text', 'Select at least one team.');
-    cy.selectTableRowByCheckbox('name', 'Demo', { disableFilter: true });
+    cy.get('.pf-v5-c-alert__title').should('contain.text', 'Select at least one user.');
+    cy.selectTableRowByCheckbox('username', 'demo-user', { disableFilter: true });
     cy.clickButton(/^Next$/);
-    cy.get('[data-cy="wizard-nav-item-teams"] button').should('not.have.class', 'pf-m-current');
+    cy.get('[data-cy="wizard-nav-item-users"] button').should('not.have.class', 'pf-m-current');
     cy.get('[data-cy="wizard-nav-item-roles"] button').should('have.class', 'pf-m-current');
   });
   it('should validate that at least one role is selected for moving to Review step', () => {
-    cy.selectTableRowByCheckbox('name', 'Demo', { disableFilter: true });
+    cy.selectTableRowByCheckbox('username', 'demo-user', { disableFilter: true });
     cy.clickButton(/^Next$/);
     cy.get('[data-cy="wizard-nav-item-roles"] button').should('have.class', 'pf-m-current');
     cy.clickButton(/^Next$/);
@@ -54,15 +54,15 @@ describe('EdaProjectAddTeams', () => {
     cy.get('[data-cy="wizard-nav-item-roles"] button').should('not.have.class', 'pf-m-current');
     cy.get('[data-cy="wizard-nav-item-review"] button').should('have.class', 'pf-m-current');
   });
-  it('should display selected team and role in the Review step', () => {
-    cy.selectTableRowByCheckbox('name', 'Demo', { disableFilter: true });
+  it('should display selected user and role in the Review step', () => {
+    cy.selectTableRowByCheckbox('username', 'demo-user', { disableFilter: true });
     cy.clickButton(/^Next$/);
     cy.selectTableRowByCheckbox('name', 'Project Admin', { disableFilter: true });
     cy.clickButton(/^Next$/);
     cy.get('[data-cy="wizard-nav-item-review"] button').should('have.class', 'pf-m-current');
-    cy.get('[data-cy="expandable-section-teams"]').should('contain.text', 'Teams');
-    cy.get('[data-cy="expandable-section-teams"]').should('contain.text', '1');
-    cy.get('[data-cy="expandable-section-teams"]').should('contain.text', 'Demo');
+    cy.get('[data-cy="expandable-section-users"]').should('contain.text', 'Users');
+    cy.get('[data-cy="expandable-section-users"]').should('contain.text', '1');
+    cy.get('[data-cy="expandable-section-users"]').should('contain.text', 'demo-user');
     cy.get('[data-cy="expandable-section-edaRoles"]').should('contain.text', 'Roles');
     cy.get('[data-cy="expandable-section-edaRoles"]').should('contain.text', '1');
     cy.get('[data-cy="expandable-section-edaRoles"]').should('contain.text', 'Project Admin');
@@ -72,11 +72,11 @@ describe('EdaProjectAddTeams', () => {
     );
   });
   it('should trigger bulk action dialog on submit', () => {
-    cy.intercept('POST', edaAPI`/role_team_assignments/`, {
+    cy.intercept('POST', edaAPI`/role_user_assignments/`, {
       statusCode: 201,
-      body: { team: 3, role_definition: 14, content_type: 'eda.project', object_id: 1 },
+      body: { user: 5, role_definition: 14, content_type: 'eda.project', object_id: 1 },
     }).as('createRoleAssignment');
-    cy.selectTableRowByCheckbox('name', 'Demo', { disableFilter: true });
+    cy.selectTableRowByCheckbox('username', 'demo-user', { disableFilter: true });
     cy.clickButton(/^Next$/);
     cy.selectTableRowByCheckbox('name', 'Project Admin', { disableFilter: true });
     cy.clickButton(/^Next$/);
@@ -85,7 +85,7 @@ describe('EdaProjectAddTeams', () => {
     // Bulk action modal is displayed with success
     cy.get('.pf-v5-c-modal-box').within(() => {
       cy.get('table tbody').find('tr').should('have.length', 1);
-      cy.get('table tbody').should('contain.text', 'Demo');
+      cy.get('table tbody').should('contain.text', 'demo-user');
       cy.get('table tbody').should('contain.text', 'Project Admin');
       cy.get('div.pf-v5-c-progress__description').should('contain.text', 'Success');
       cy.get('div.pf-v5-c-progress__status').should('contain.text', '100%');
