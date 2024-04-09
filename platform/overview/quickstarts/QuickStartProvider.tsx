62aef27a3906703986a1bde3bdb2ace0145a8601
James Talton <jtalton@redhat.com>
2024-04-09 13:32:58 -0400

    Platform Overview - Hide content if service is not available (#238)
    Signed-off-by: James Talton <talton@gmail.com>
    Co-authored-by: Vishali Kamenani <95239214+vishsanghishetty@users.noreply.github.com>

--- platform/overview/quickstarts/QuickStartProvider.tsx
+++ platform/overview/quickstarts/QuickStartProvider.tsx
@@ -1,15 +1,23 @@
 import { AllQuickStartStates, QuickStart, QuickStartContainer } from '@patternfly/quickstarts';
 import { ReactNode, useMemo, useState } from 'react';
+import { useHubService } from '../../main/GatewayServices';
 import { useFindingContentQuickStart } from './useFindingContentQuickStart';
 
 export function QuickStartProvider(props: { children: ReactNode }) {
+  // const awxService = useAwxService();
+  // const edaService = useEdaService();
+  const hubService = useHubService();
+
   const [activeQuickStartID, setActiveQuickStartID] = useState('');
   const [allQuickStartStates, setAllQuickStartStates] = useState<AllQuickStartStates>({});
-  const findingContentQuickStart = useFindingContentQuickStart();
-  const quickStarts = useMemo<QuickStart[]>(
-    () => [findingContentQuickStart],
-    [findingContentQuickStart]
-  );
+  const findingContentQuickStart: QuickStart | undefined = useFindingContentQuickStart();
+  const quickStarts = useMemo<QuickStart[]>(() => {
+    const quickStarts: QuickStart[] = [];
+    if (hubService) {
+      quickStarts.push(findingContentQuickStart);
+    }
+    return quickStarts;
+  }, [findingContentQuickStart, hubService]);
   return (
     <QuickStartContainer
       quickStarts={quickStarts}
