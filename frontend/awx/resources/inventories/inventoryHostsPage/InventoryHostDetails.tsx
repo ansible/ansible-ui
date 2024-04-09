37ad763f6dec050e01b16ec65242bb842f5990b1
MilanPospisil <arkanus@seznam.cz>
2024-04-09 14:17:08 +0200

    Fix Hosts detail inventory link

--- frontend/awx/resources/inventories/inventoryHostsPage/InventoryHostDetails.tsx
+++ frontend/awx/resources/inventories/inventoryHostsPage/InventoryHostDetails.tsx
@@ -42,6 +42,13 @@ export function InventoryHostDetails() {
   return <InventoryHostDetailsInner host={host} />;
 }
 
+function inventoryKindToType(kind: string) {
+  if (kind === '') return 'inventory';
+  if (kind === 'smart') return 'smart_inventory';
+  if (kind === 'constructed') return 'constructed_inventory';
+  return '';
+}
+
 export function InventoryHostDetailsInner(props: { host: AwxHost }) {
   const { t } = useTranslation();
   const pageNavigate = usePageNavigate();
@@ -68,7 +75,10 @@ export function InventoryHostDetailsInner(props: { host: AwxHost }) {
         <TextCell
           text={host.summary_fields?.inventory?.name}
           to={getPageUrl(AwxRoute.InventoryDetails, {
-            params: { id: host.summary_fields?.inventory?.id },
+            params: {
+              id: host.summary_fields?.inventory?.id,
+              inventory_type: inventoryKindToType(host.summary_fields?.inventory.kind),
+            },
           })}
         />
       </PageDetail>
