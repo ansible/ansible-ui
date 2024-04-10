586b2b27e406cd153e35ea1b4048053b92e556ec
MilanPospisil <arkanus@seznam.cz>
2024-04-10 13:18:15 +0200

    Notification type switcher

--- frontend/awx/administration/notifiers/NotifierForm.tsx
+++ frontend/awx/administration/notifiers/NotifierForm.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Button } from '@patternfly/react-core';
 import { useCallback, useEffect, useState } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { useForm, useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from 'react-router-dom';
 import {
@@ -24,6 +24,7 @@ import { useGet } from '../../../common/crud/useGet';
 import { AwxError } from '../../common/AwxError';
 import { PageFormSelectOrganization } from '../../access/organizations/components/PageFormOrganizationSelect';
 import { PageFormSingleSelect } from '../../../../framework/PageForm/Inputs/PageFormSingleSelect';
+import { PageFormWatch } from '../../../../framework/PageForm/Utils/PageFormWatch';
 
 export function EditNotifier() {
   return <NotifierForm mode={'edit'} />;
@@ -84,10 +85,11 @@ function NotifierForm(props: { mode: 'add' | 'edit' }) {
           />
           <PageFormSelectOrganization<NotificationTemplate> name="organization" isRequired />
           <PageFormSingleSelect
-            name="type"
-            id="type"
+            name="notification_type"
+            id="notification_type"
             label={t(`Type`)}
             placeholder={t('Choose a Notification Type')}
+            isRequired={true}
             options={[
               { value: 'email', label: t('Email') },
               { value: 'grafana', label: t('Grafana') },
@@ -100,8 +102,79 @@ function NotifierForm(props: { mode: 'add' | 'edit' }) {
               { value: 'webhook', label: t('Webhook') },
             ]}
           />
-
+          <PageFormWatch watch="notification_type">
+          {() =>
+            <TypeForm mode={mode}/>
+          }
+          </PageFormWatch>
       </AwxPageForm>
     </PageLayout>
   );
 }
+
+function TypeForm(props : {mode : 'add' | 'edit'})
+{
+  const context = useFormContext<NotificationTemplate>();
+  const notification_type = context.getValues().notification_type;
+ 
+  const {mode} = props;
+  switch (notification_type) {
+    case 'email':
+      return <EmailForm mode={mode} />;
+    case 'grafana':
+      return <GrafanaForm mode={mode} />;
+    case 'irc':
+      return <IRCForm mode={mode} />;
+    case 'mattermost':
+      return <MattermostForm mode={mode} />;
+    case 'pagerduty':
+      return <PagerdutyForm mode={mode} />;
+    case 'rocketchat':
+      return <RocketChatForm mode={mode} />;
+    case 'slack':
+      return <SlackForm mode={mode} />;
+    case 'twilio':
+      return <TwilioForm mode={mode} />;
+    case 'webhook':
+      return <WebhookForm mode={mode} />;
+    default:
+      return <></>;
+  }
+}
+
+function EmailForm(props: { mode: 'add' | 'edit' }) {
+  return <></>;
+}
+
+function GrafanaForm(props: { mode: 'add' | 'edit' }) {
+  return <></>;
+}
+
+function IRCForm(props: { mode: 'add' | 'edit' }) {
+  return <></>;
+}
+
+function MattermostForm(props: { mode: 'add' | 'edit' }) {
+  return <></>;
+}
+
+function PagerdutyForm(props: { mode: 'add' | 'edit' }) {
+  return <></>;
+}
+
+function RocketChatForm(props: { mode: 'add' | 'edit' }) {
+  return <></>;
+}
+
+function SlackForm(props: { mode: 'add' | 'edit' }) {
+  return <></>;
+}
+
+function TwilioForm(props: { mode: 'add' | 'edit' }) {
+  return <></>;
+}
+
+function WebhookForm(props: { mode: 'add' | 'edit' }) {
+  return <></>;
+}
+
