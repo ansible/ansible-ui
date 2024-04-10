d8bce1709d814986a492356eef17d7fe6e8dec7b
akus062381 <sarah.akus@gmail.com>
2024-04-10 14:45:20 -0400

    push another fix

--- cypress/e2e/eda/overview/eda-overview.cy.ts
+++ cypress/e2e/eda/overview/eda-overview.cy.ts
@@ -105,7 +105,7 @@ describe('EDA Overview', () => {
 });
 
 describe('overview checks when resources before any resources are created', () => {
-  before(() => {
+  beforeEach(() => {
     cy.edaLogin();
   });
 
