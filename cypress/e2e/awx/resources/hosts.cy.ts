f6c5de904f83ffdb78e406367b90e7383a38eb7a
Or Hochmann <orhochma@redhat.com>
2024-04-10 22:34:37 +0300

    try to fix this

--- cypress/e2e/awx/resources/hosts.cy.ts
+++ cypress/e2e/awx/resources/hosts.cy.ts
@@ -4,14 +4,12 @@ import { Organization } from '../../../../frontend/awx/interfaces/Organization';
 //**********8
 import { AwxUser } from '../../../../frontend/awx/interfaces/User';
 //**********8
-import { User } from '../../../../frontend/awx/interfaces/User';
 import { awxAPI } from '../../../support/formatApiPathForAwx';
-//**********8
 
 describe('host and inventory host', () => {
   let organization: Organization;
   let inventory: Inventory;
-  let user: User;
+  let user: AwxUser;
 
   before(() => {
     cy.awxLogin();
@@ -97,7 +95,7 @@ describe('host and inventory host', () => {
 
   // assisting functions
   function createAndCheckHost(inventory_host: boolean, inventory: string) {
-    const hostName = 'E2E Inventory host ' + randomString(4);
+    const hostName = 'E2E Inventory host 1' + randomString(4);
     if (inventory_host) {
       cy.get('[data-cy="empty-state-title"]').contains(
         /^There are currently no hosts added to this inventory./
@@ -113,7 +111,7 @@ describe('host and inventory host', () => {
       cy.contains('button', 'Browse').click();
       cy.contains('Select Inventory');
       cy.get(`[aria-label="Select Inventory"]`).within(() => {
-        cy.searchAndDisplayResource(inventory);
+        cy.filterTableBySingleSelect('name', inventory);
         cy.get(`[data-cy="checkbox-column-cell"] input`).click();
         cy.contains('button', 'Confirm').click();
       });
@@ -135,7 +133,7 @@ describe('host and inventory host', () => {
     } else {
       cy.visit('/infrastructure/hosts?page=1&perPage=10&sort=name');
     }
-    cy.searchAndDisplayResource(hostName);
+    cy.filterTableByMultiSelect('name', [hostName]);
     cy.get(`[data-cy='edit-host']`).click();
     cy.verifyPageTitle('Edit host');
     cy.get('[data-cy="description"]').clear().type('This is the description edited');
@@ -151,8 +149,7 @@ describe('host and inventory host', () => {
     } else {
       cy.visit('/infrastructure/hosts?page=1&perPage=10&sort=name');
     }
-
-    cy.searchAndDisplayResource(hostName);
+    cy.filterTableByMultiSelect('name', [hostName]);
     cy.get(`[data-cy="actions-column-cell"] [data-cy="actions-dropdown"]`).click();
     cy.get(`[data-cy="delete-host"]`).click();
     cy.clickModalConfirmCheckbox();
@@ -184,87 +181,3 @@ describe('host and inventory host', () => {
     deleteHost(inventory.id, inventory_host, hostName);
   }
 });
-//////////////
-
-function createAndCheckHost(inventory_host: boolean, inventory: string) {
-  const hostName = 'E2E Inventory host 1' + randomString(4);
-
-  if (inventory_host) {
-    cy.get('[data-cy="empty-state-title"]').contains(
-      /^There are currently no hosts added to this inventory./
-    );
-  }
-
-  // create
-  cy.clickButton(/^Create host$/);
-  cy.verifyPageTitle('Create Host');
-  cy.get('[data-cy="name"]').type(hostName);
-  cy.get('[data-cy="description"]').type('This is the description');
-
-  if (!inventory_host) {
-    cy.get(`[data-cy="inventory-id"]`).click();
-    cy.contains('button', 'Browse').click();
-    cy.contains('Select Inventory');
-    cy.get(`[aria-label="Select Inventory"]`).within(() => {
-      cy.filterTableBySingleSelect('name', inventory);
-      cy.get(`[data-cy="checkbox-column-cell"] input`).click();
-      cy.contains('button', 'Confirm').click();
-    });
-    cy.get(`[aria-label="Select Inventory"]`).should('not.exist');
-  }
-
-  cy.getByDataCy('variables').type('test: true');
-  cy.clickButton(/^Create host/);
-  cy.hasDetail(/^Name$/, hostName);
-  cy.hasDetail(/^Description$/, 'This is the description');
-  cy.hasDetail(/^Variables$/, 'test: true');
-
-  return hostName;
-}
-
-function editHost(inventoryID: number, inventory_host: boolean, hostName: string) {
-  if (inventory_host) {
-    cy.visit(
-      `/infrastructure/inventories/inventory/${inventoryID}/hosts/?page=1&perPage=10&sort=name`
-    );
-  } else {
-    cy.visit('/infrastructure/hosts?page=1&perPage=10&sort=name');
-  }
-  cy.filterTableByMultiSelect('name', [hostName]);
-
-  cy.get(`[data-cy='edit-host']`).click();
-  cy.verifyPageTitle('Edit host');
-  cy.get('[data-cy="description"]').clear().type('This is the description edited');
-  cy.get(`[data-cy='Submit']`).click();
-
-  cy.hasDetail(/^Description$/, 'This is the description edited');
-}
-
-function deleteHost(inventoryID: number, inventory_host: boolean, hostName: string) {
-  if (inventory_host) {
-    cy.visit(
-      `/infrastructure/inventories/inventory/${inventoryID}/hosts/?page=1&perPage=10&sort=name`
-    );
-  } else {
-    cy.visit('/infrastructure/hosts?page=1&perPage=10&sort=name');
-  }
-
-  cy.filterTableByMultiSelect('name', [hostName]);
-  cy.get(`[data-cy="actions-column-cell"] [data-cy="actions-dropdown"]`).click();
-  cy.get(`[data-cy="delete-host"]`).click();
-  cy.clickModalConfirmCheckbox();
-  cy.clickModalButton('Delete hosts');
-  cy.contains('button', 'Close').click();
-  cy.contains(/^No results found./);
-}
-
-function createAndEditAndDeleteHost(inventory_host: boolean, inventory: Inventory) {
-  //create
-  const hostName = createAndCheckHost(inventory_host, inventory.name);
-
-  // edit
-  editHost(inventory.id, inventory_host, hostName);
-  // delete
-  deleteHost(inventory.id, inventory_host, hostName);
-}
-///////
