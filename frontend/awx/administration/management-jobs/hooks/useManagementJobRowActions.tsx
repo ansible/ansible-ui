8463b13b39777aeaf49df7802bdcbfae1a2be4e2
Pratyush Bhandari <prbhanda@redhat.com>
2024-04-11 17:51:20 -0400

    Launch job or disp prompt based on job_type

--- frontend/awx/administration/management-jobs/hooks/useManagementJobRowActions.tsx
+++ frontend/awx/administration/management-jobs/hooks/useManagementJobRowActions.tsx
@@ -5,11 +5,35 @@ import { useTranslation } from 'react-i18next';
 import { IPageAction, PageActionSelection, PageActionType } from '../../../../../framework';
 import { SystemJobTemplate } from '../../../interfaces/SystemJobTemplate';
 import { useManagementJobsRetainDataModal } from '../components/ManagementJobsRetainDataModal';
+import { usePostRequest } from '../../../../common/crud/usePostRequest';
+import { awxAPI } from '../../../common/api/awx-utils';
+import { useGetJobOutputUrl } from '../../../views/jobs/useGetJobOutputUrl';
+import { useNavigate } from 'react-router-dom';
+import { UnifiedJob } from '../../../interfaces/UnifiedJob';
 
 export function useManagementJobRowActions() {
   const { t } = useTranslation();
 
   const openManagementJobsModal = useManagementJobsRetainDataModal();
+  const postRequest = usePostRequest();
+  const getJobOutputUrl = useGetJobOutputUrl();
+  const navigate = useNavigate();
+
+  const launchManagementJob = async (managementJob: SystemJobTemplate) => {
+    if (
+      managementJob.job_type === 'cleanup_activitystream' ||
+      managementJob.job_type === 'cleanup_jobs'
+    ) {
+      openManagementJobsModal({ id: managementJob.id });
+    } else {
+      const newJob = await postRequest(
+        awxAPI`/system_job_templates/${String(managementJob.id)}/launch/`,
+        {}
+      );
+      const jobUrl = getJobOutputUrl(newJob as unknown as UnifiedJob);
+      navigate(jobUrl);
+    }
+  };
 
   return useMemo<IPageAction<SystemJobTemplate>[]>(() => {
     const actions: IPageAction<SystemJobTemplate>[] = [
@@ -20,8 +44,7 @@ export function useManagementJobRowActions() {
         isPinned: true,
         icon: RocketIcon,
         label: t(`Launch Management Job`),
-        onClick: (managementJob: SystemJobTemplate) =>
-          openManagementJobsModal({ id: managementJob.id }),
+        onClick: launchManagementJob,
       },
     ];
     return actions;
