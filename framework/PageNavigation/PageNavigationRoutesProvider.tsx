46060a9a8b711100eb7bcb3fd7e68e70faefcecd
James Talton <jtalton@redhat.com>
2024-04-11 13:54:18 -0400

    Page navigation routes fix routes being available in modals (#2007)
    Signed-off-by: James Talton <talton@gmail.com>

--- framework/PageNavigation/PageNavigationRoutesProvider.tsx
+++ framework/PageNavigation/PageNavigationRoutesProvider.tsx
@@ -1,21 +1,32 @@
-import { ReactNode, createContext, useContext, useMemo } from 'react';
+import { ReactNode, createContext, useCallback, useContext, useMemo, useState } from 'react';
 import { PageNavigationItem } from './PageNavigationItem';
 
-const PageNavigationContext = createContext<{ [key: string]: string }>({});
+const PageNavigationContext = createContext<
+  [{ [key: string]: string }, (navigation: PageNavigationItem[]) => void]
+>([{}, () => {}]);
 
 /** Hook to get the PageNavigationContext which provides route information for navigation. */
 export function usePageNavigationRoutes() {
+  return useContext(PageNavigationContext)[0];
+}
+
+export function usePageNavigationRoutesContext() {
   return useContext(PageNavigationContext);
 }
 
 /** Provider for the PageNavigationContext which provides route information for navigation. */
-export function PageNavigationRoutesProvider(props: {
-  children: ReactNode;
-  navigation: PageNavigationItem[];
-}) {
-  const routes = useMemo(() => createNavigateToRoutes('', props.navigation), [props.navigation]);
+export function PageNavigationRoutesProvider(props: { children: ReactNode }) {
+  // const routes = useMemo(() => createNavigateToRoutes('', props.navigation), [props.navigation]);
+  const [routes, setRoutes] = useState<{ [key: string]: string }>({});
+  const setNavigation = useCallback((navigation: PageNavigationItem[]) => {
+    setRoutes(createNavigateToRoutes('', navigation));
+  }, []);
+  const value = useMemo<[{ [key: string]: string }, (navigation: PageNavigationItem[]) => void]>(
+    () => [routes, setNavigation],
+    [routes, setNavigation]
+  );
   return (
-    <PageNavigationContext.Provider value={routes}>{props.children}</PageNavigationContext.Provider>
+    <PageNavigationContext.Provider value={value}>{props.children}</PageNavigationContext.Provider>
   );
 }
 
