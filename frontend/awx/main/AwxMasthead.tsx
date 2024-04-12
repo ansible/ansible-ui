2e2849ed1e6340df77f29cf6046e048dc19398c2
James Talton <jtalton@redhat.com>
2024-04-12 16:44:58 -0400

    Active User State - Update to make more consistent (#2017)
    Signed-off-by: James Talton <talton@gmail.com>

--- frontend/awx/main/AwxMasthead.tsx
+++ frontend/awx/main/AwxMasthead.tsx
@@ -11,7 +11,6 @@ import { usePageNotifications } from '../../../framework/PageNotifications/PageN
 import { useAnsibleAboutModal } from '../../common/AboutModal';
 import { PageRefreshIcon } from '../../common/PageRefreshIcon';
 import { useGet } from '../../common/crud/useGet';
-import { useClearCache } from '../../common/useInvalidateCache';
 import { AwxItemsResponse } from '../common/AwxItemsResponse';
 import { awxAPI } from '../common/api/awx-utils';
 import { useAwxActiveUser } from '../common/useAwxActiveUser';
@@ -27,16 +26,14 @@ export function AwxMasthead() {
   const { t } = useTranslation();
   const versionInfo = useAwxProductVersionInfo();
   const openAnsibleAboutModal = useAnsibleAboutModal();
-  const { clearAllCache } = useClearCache();
   const config = useAwxConfig();
   const pageNavigate = usePageNavigate();
   const { activeAwxUser, refreshActiveAwxUser } = useAwxActiveUser();
   useAwxNotifications();
   const logout = useCallback(async () => {
     await fetch('/api/logout/');
-    clearAllCache();
     refreshActiveAwxUser?.();
-  }, [clearAllCache, refreshActiveAwxUser]);
+  }, [refreshActiveAwxUser]);
   return (
     <PageMasthead brand={<AwxBrand style={{ height: 60 }} />}>
       <ToolbarGroup variant="icon-button-group" style={{ flexGrow: 1 }}>
