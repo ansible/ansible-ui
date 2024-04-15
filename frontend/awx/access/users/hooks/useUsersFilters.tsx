dbd644e309a4c74f699ee8b7fe58481693110aa7
Maeve Hoffer <daphnemaeve@gmail.com>
2024-04-15 14:57:57 -0700

    Add dynamic filters to users page (#1982)

--- frontend/awx/access/users/hooks/useUsersFilters.tsx
+++ frontend/awx/access/users/hooks/useUsersFilters.tsx
@@ -1,31 +1,14 @@
-import { useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
-import { IToolbarFilter, ToolbarFilterType } from '../../../../../framework';
-import {
-  useFirstNameToolbarFilter,
-  useLastNameToolbarFilter,
-  useUsernameToolbarFilter,
-} from '../../../common/awx-toolbar-filters';
+import { useDynamicToolbarFilters } from '../../../common/useDynamicFilters';
 
 export function useUsersFilters() {
-  const { t } = useTranslation();
-  const usernameToolbarFilter = useUsernameToolbarFilter();
-  const firstnameByToolbarFilter = useFirstNameToolbarFilter();
-  const lastnameToolbarFilter = useLastNameToolbarFilter();
-  const toolbarFilters = useMemo<IToolbarFilter[]>(
-    () => [
-      usernameToolbarFilter,
-      firstnameByToolbarFilter,
-      lastnameToolbarFilter,
-      {
-        key: 'email',
-        label: t('Email'),
-        type: ToolbarFilterType.MultiText,
-        query: 'email__icontains',
-        comparison: 'contains',
+  const toolbarFilters = useDynamicToolbarFilters({
+    optionsPath: 'users',
+    preSortedKeys: ['username', 'first_name', 'last_name', 'email', 'id'],
+    preFilledValueKeys: {
+      id: {
+        apiPath: 'users',
       },
-    ],
-    [usernameToolbarFilter, firstnameByToolbarFilter, lastnameToolbarFilter, t]
-  );
+    },
+  });
   return toolbarFilters;
 }
