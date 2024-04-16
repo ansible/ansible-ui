35930253036dbfd56ca442e238d53e87023782e7
Salma Kochay <skochay330@gmail.com>
2024-04-16 12:26:52 -0400

    workflow approvals dynamic filters + test fixes

--- frontend/awx/administration/workflow-approvals/hooks/useWorkflowApprovalsFilters.tsx
+++ frontend/awx/administration/workflow-approvals/hooks/useWorkflowApprovalsFilters.tsx
@@ -1,36 +1,13 @@
-import { useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
-import { IToolbarFilter, ToolbarFilterType } from '../../../../../framework';
-import { useNameToolbarFilter } from '../../../common/awx-toolbar-filters';
+import { useDynamicToolbarFilters } from '../../../common/useDynamicFilters';
 
 export function useWorkflowApprovalsFilters() {
-  const { t } = useTranslation();
-  const nameToolbarFilter = useNameToolbarFilter();
-  const toolbarFilters = useMemo<IToolbarFilter[]>(
-    () => [
-      nameToolbarFilter,
-      {
-        type: ToolbarFilterType.MultiSelect,
-        key: 'status',
-        label: t('Status'),
-        query: 'status',
-        options: [
-          { label: t('Approved'), value: 'successful' },
-          { label: t('Denied'), value: 'failed' },
-          { label: t('Pending'), value: 'pending' },
-          { label: t('Canceled'), value: 'canceled' },
-        ],
-        placeholder: t('Filter by status'),
-      },
-      {
-        key: 'id',
-        label: t('ID'),
-        type: ToolbarFilterType.MultiText,
-        query: 'id',
-        comparison: 'equals',
-      },
-    ],
-    [nameToolbarFilter, t]
-  );
+  const toolbarFilters = useDynamicToolbarFilters({
+    optionsPath: 'workflow_approvals',
+    preFilledValueKeys: {
+      name: { apiPath: 'workflow_approvals' },
+      id: { apiPath: 'workflow_approvals' },
+    },
+    preSortedKeys: ['name', 'id'],
+  });
   return toolbarFilters;
 }
