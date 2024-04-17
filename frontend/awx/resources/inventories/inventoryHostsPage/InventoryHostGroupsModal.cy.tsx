b006c21b7b68d70f65967417547ad33c6174f23a
Kia Lam <digitalanime@gmail.com>
2024-04-17 13:07:12 -0700

    Fix inventory host groups modal component tests.

--- frontend/awx/resources/inventories/inventoryHostsPage/InventoryHostGroupsModal.cy.tsx
+++ frontend/awx/resources/inventories/inventoryHostsPage/InventoryHostGroupsModal.cy.tsx
@@ -30,67 +30,97 @@ describe('Inventory Host Groups List', () => {
     });
 
     it('Inventory Groups Add Modal Renders', () => {
+      cy.intercept(
+        {
+          method: 'OPTIONS',
+          url: '/api/v2/inventories/1/groups/',
+          hostname: 'localhost',
+        },
+        {
+          fixture: 'mock_options.json',
+        }
+      ).as('getFilterOptions');
       cy.mount(<InventoryHostGroupsAddModal {...props} />, {
         path: '/inventories/:inventory_type/:id/hosts/:host_id/*',
         initialEntries: ['/inventories/inventory/1/hosts/1/groups'],
       });
-      cy.get('tbody').find('tr').should('have.length', 10);
+      cy.get('tbody').find('tr').should('have.length', 4);
     });
 
     it('Filter Host Groups by name', () => {
+      cy.intercept(
+        {
+          method: 'OPTIONS',
+          url: '/api/v2/inventories/1/groups/',
+          hostname: 'localhost',
+        },
+        {
+          fixture: 'mock_options.json',
+        }
+      ).as('getFilterOptions');
+      cy.intercept(
+        {
+          method: 'GET',
+          url: '/api/v2/inventories/1/groups/*',
+          hostname: 'localhost',
+        },
+        {
+          fixture: 'groups.json',
+        }
+      ).as('getGroups');
+      cy.intercept('/api/v2/inventories/1/groups/?not__hosts=1&name=*').as('nameFilterRequest');
       cy.mount(<InventoryHostGroupsAddModal {...props} />, {
         path: '/inventories/:inventory_type/:id/hosts/:host_id/*',
         initialEntries: ['/inventories/inventory/1/hosts/1/groups'],
       });
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/v2/inventories/*/groups/?not__hosts=*&name__icontains=Related%20to%20group%201*',
-        },
-        {
-          fixture: 'group.json',
-        }
-      ).as('nameFilter');
-      cy.filterTableByTypeAndText(/^Name$/, 'Related to group 1');
-      cy.get('@nameFilter.all').should('have.length.least', 1);
+      cy.filterTableByMultiSelect('name', ['Related to group 1']);
+      cy.wait('@nameFilterRequest');
       cy.clearAllFilters();
     });
 
     it('Filter Host Groups by created by', () => {
+      cy.intercept(
+        {
+          method: 'OPTIONS',
+          url: '/api/v2/inventories/1/groups/',
+          hostname: 'localhost',
+        },
+        {
+          fixture: 'mock_options.json',
+        }
+      ).as('getFilterOptions');
+      cy.intercept(
+        '/api/v2/inventories/1/groups/?not__hosts=1&created_by__username__icontains=*'
+      ).as('createdByFilterRequest');
       cy.mount(<InventoryHostGroupsAddModal {...props} />, {
         path: '/inventories/:inventory_type/:id/hosts/:host_id/*',
         initialEntries: ['/inventories/inventory/1/hosts/1/groups'],
       });
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/v2/inventories/*/groups/?not__hosts=*&created_by__username__icontains=test*',
-        },
-        {
-          fixture: 'group.json',
-        }
-      ).as('createdByFilter');
-      cy.filterTableByTypeAndText(/^Created by$/, 'test');
-      cy.get('@createdByFilter.all').should('have.length.least', 1);
+      cy.filterTableByTextFilter('created-by', 'test');
+      cy.wait('@createdByFilterRequest');
       cy.clearAllFilters();
     });
 
     it('Filter Host Groups by modified by', () => {
+      cy.intercept(
+        {
+          method: 'OPTIONS',
+          url: '/api/v2/inventories/1/groups/',
+          hostname: 'localhost',
+        },
+        {
+          fixture: 'mock_options.json',
+        }
+      ).as('getFilterOptions');
+      cy.intercept(
+        '/api/v2/inventories/1/groups/?not__hosts=1&modified_by__username__icontains=*'
+      ).as('modifiedByFilterRequest');
       cy.mount(<InventoryHostGroupsAddModal {...props} />, {
         path: '/inventories/:inventory_type/:id/hosts/:host_id/*',
         initialEntries: ['/inventories/inventory/1/hosts/1/groups'],
       });
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/v2/inventories/*/groups/?not__hosts=*&modified_by__username__icontains=test*',
-        },
-        {
-          fixture: 'group.json',
-        }
-      ).as('modifiedByFilter');
-      cy.filterTableByTypeAndText(/^Modified by$/, 'test');
-      cy.get('@modifiedByFilter.all').should('have.length.least', 1);
+      cy.filterTableByTextFilter('modified-by', 'test');
+      cy.wait('@modifiedByFilterRequest');
       cy.clearAllFilters();
     });
 
