0b4a592fb5a4d17818bd3f38c153df9f03c1a5d4
Tomas Z <tznamena@redhat.com>
2024-04-17 20:03:45 -0400

    Add component unit test

--- frontend/awx/access/users/UserPage/UserTokens.tsx
+++ frontend/awx/access/users/UserPage/UserTokens.tsx
@@ -1,26 +1,23 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-// import { useTranslation } from 'react-i18next';
 import { usePageNavigate, PageNotImplemented } from '../../../../../framework';
 import { AwxRoute } from '../../../main/AwxRoutes';
 import { useAwxActiveUser } from '../../../common/useAwxActiveUser';
 
 export function UserTokens() {
   const params = useParams<{ id: string }>();
-  //const { t } = useTranslation();
+  const [showTokens, setShowTokens] = useState(true);
   const { activeAwxUser } = useAwxActiveUser();
   const pageNavigate = usePageNavigate();
 
   useEffect(() => {
     if (activeAwxUser?.id === undefined || activeAwxUser?.id.toString() !== params.id) {
+      setShowTokens(false);
       pageNavigate(AwxRoute.Users);
     }
-  });
+  }, [activeAwxUser?.id, params.id, setShowTokens, pageNavigate]);
 
-  return (
-    <>
-      <PageNotImplemented></PageNotImplemented>
-    </>
-  );
+  // not showing anything when user does not match. this helps to test the component.
+  return <> {showTokens && <PageNotImplemented></PageNotImplemented>} </>;
 }
