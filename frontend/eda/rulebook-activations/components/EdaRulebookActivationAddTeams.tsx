42b582e2ab0141e0142512bcb19237dc066b02d8
James Talton <jtalton@redhat.com>
2024-04-19 10:16:14 -0400

    Fix eda imports (#2115)
    Signed-off-by: James Talton <talton@gmail.com>

--- frontend/eda/rulebook-activations/components/EdaRulebookActivationAddTeams.tsx
+++ frontend/eda/rulebook-activations/components/EdaRulebookActivationAddTeams.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
 import {
   LoadingPage,
   PageHeader,
@@ -8,18 +9,17 @@ import {
   useGetPageUrl,
   usePageNavigate,
 } from '../../../../framework';
-import { EdaSelectTeamsStep } from '../../access/teams/components/steps/EdaSelectTeamsStep';
-import { EdaTeam } from '../../interfaces/EdaTeam';
-import { EdaSelectRolesStep } from '../../access/roles/components/EdaSelectRolesStep';
-import { useParams } from 'react-router-dom';
-import { useGet } from '../../../common/crud/useGet';
-import { EdaRulebookActivation } from '../../interfaces/EdaRulebookActivation';
-import { edaAPI } from '../../common/eda-utils';
-import { postRequest } from '../../../common/crud/Data';
 import { RoleAssignmentsReviewStep } from '../../../common/access/RolesWizard/steps/RoleAssignmentsReviewStep';
-import { EdaRbacRole } from '../../interfaces/EdaRbacRole';
-import { EdaRoute } from '../../main/EdaRoutes';
+import { postRequest } from '../../../common/crud/Data';
+import { useGet } from '../../../common/crud/useGet';
+import { EdaSelectRolesStep } from '../../access/common/EdaRolesWizardSteps/EdaSelectRolesStep';
+import { EdaSelectTeamsStep } from '../../access/common/EdaRolesWizardSteps/EdaSelectTeamsStep';
+import { edaAPI } from '../../common/eda-utils';
 import { useEdaBulkActionDialog } from '../../common/useEdaBulkActionDialog';
+import { EdaRbacRole } from '../../interfaces/EdaRbacRole';
+import { EdaRulebookActivation } from '../../interfaces/EdaRulebookActivation';
+import { EdaTeam } from '../../interfaces/EdaTeam';
+import { EdaRoute } from '../../main/EdaRoutes';
 
 interface WizardFormValues {
   teams: EdaTeam[];
