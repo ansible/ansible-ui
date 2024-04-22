63be058e1824dcdef105dec0423bc3141cff3ed1
James Talton <talton@gmail.com>
2024-04-22 14:42:19 -0400

    Update Login Screen to match UXD designs
    Signed-off-by: James Talton <talton@gmail.com>

--- frontend/common/Login.tsx
+++ frontend/common/Login.tsx
@@ -1,62 +1,121 @@
-import { Title, TitleSizes } from '@patternfly/react-core';
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Flex,
+  FlexItem,
+  Page,
+  Stack,
+  Text,
+  TextContent,
+} from '@patternfly/react-core';
+import { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { ErrorBoundary } from '../../framework/components/ErrorBoundary';
 import { useFrameworkTranslations } from '../../framework/useFrameworkTranslations';
-import background from '../../node_modules/@patternfly/patternfly/assets/images/pfbg_1200.jpg';
 import { LoginForm } from './LoginForm';
-import type { AuthOption } from './SocialAuthLogin';
+import { AuthOption } from './SocialAuthLogin';
 
-const Wrapper = styled.div`
-  min-height: 100dvh;
-  padding-block: 50px;
-  background-color: #444;
-  background-image: url(${background});
-  background-position: center;
-  background-size: cover;
-`;
-
-const Inner = styled.div`
-  max-inline-size: 550px;
-  margin-inline: auto;
-  padding: 3rem 3.5rem;
-  background-color: var(--pf-v5-global--BackgroundColor--100);
-
-  .pf-v5-theme-dark & {
-    background-color: var(--pf-v5-global--BackgroundColor--300);
-  }
-`;
-
-const Heading = styled(Title)`
-  margin-block-end: 2.5rem;
-`;
-
-type LoginProps = {
+export function Login(props: {
   hideInputs?: boolean;
   authOptions?: AuthOption[];
   apiUrl: string;
   onSuccess: () => void;
-};
-
-export function Login(props: LoginProps) {
+  loginDescription?: string;
+  icon?: ReactNode;
+  brand?: string;
+  product?: string;
+  productDescription?: string;
+  background?: ReactNode;
+}) {
   const { t } = useTranslation();
-  const productName = process.env.PRODUCT ?? 'Ansible';
   const [translations] = useFrameworkTranslations();
   return (
     <ErrorBoundary message={translations.errorText}>
-      <Wrapper>
-        <Inner>
-          <Heading headingLevel="h1" size={TitleSizes['2xl']}>
-            {t('Welcome to {{productName}}', { productName })}
-          </Heading>
-          <LoginForm
-            apiUrl={props.apiUrl}
-            authOptions={props.authOptions}
-            onSuccess={props.onSuccess}
-            hideInputs={props.hideInputs}
-          />
-        </Inner>
-      </Wrapper>
+      <Page style={{ overflow: 'auto' }}>
+        <LoginPageLayout>
+          <LoginPageBody>
+            <Card isLarge isRounded>
+              <CardHeader>
+                <TextContent>
+                  <CardTitle component="h1">{t('Log in to your account')}</CardTitle>
+                  {props.loginDescription && <Text component="p">{props.loginDescription}</Text>}
+                </TextContent>
+              </CardHeader>
+              <CardBody>
+                <LoginForm
+                  apiUrl={props.apiUrl}
+                  authOptions={props.authOptions}
+                  onSuccess={props.onSuccess}
+                  hideInputs={props.hideInputs}
+                />
+              </CardBody>
+            </Card>
+            <Flex direction={{ default: 'column' }} spaceItems={{ default: 'spaceItemsXl' }}>
+              <FlexItem>
+                <Flex
+                  spaceItems={{ default: 'spaceItemsMd' }}
+                  alignItems={{ default: 'alignItemsCenter' }}
+                >
+                  {props.icon}
+                  <Stack>
+                    {props.brand && <Brand>{props.brand}</Brand>}
+                    <Product>{props.product}</Product>
+                  </Stack>
+                </Flex>
+              </FlexItem>
+              {props.productDescription && <FlexItem>{props.productDescription}</FlexItem>}
+            </Flex>
+          </LoginPageBody>
+          {props.background && (
+            <LoginPageFooter>
+              <LoginPageDecoration>{props.background}</LoginPageDecoration>
+            </LoginPageFooter>
+          )}
+        </LoginPageLayout>
+      </Page>
     </ErrorBoundary>
   );
 }
+
+const Brand = styled.div`
+  font-size: xx-large;
+  font-weight: bold;
+  line-height: 1.15;
+  font-family: 'Red Hat Display', sans-serif;
+`;
+
+const Product = styled.h1`
+  font-size: xx-large;
+  line-height: 1.15;
+  font-family: 'Red Hat Display', sans-serif;
+`;
+
+const LoginPageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  align-items: stretch;
+`;
+
+const LoginPageBody = styled.div`
+  display: flex;
+  flex-grow: 1;
+  flex-wrap: wrap;
+  gap: 48px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoginPageFooter = styled.div`
+  display: flex;
+  justify-content: end;
+`;
+
+const LoginPageDecoration = styled.div`
+  display: flex;
+  width: 400px;
+`;
