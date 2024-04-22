edd38da89e35af39dd74f4efa9fa34aa1c2f71c7
Jiří Jeřábek (Jiri Jerabek) <Jerabekjirka@email.cz>
2024-04-22 13:47:35 +0200

    [AAP-8258] Update `TemplateJobs` for workflow job templates screen (#2078)

--- frontend/awx/resources/templates/TemplatePage/TemplateJobs.tsx
+++ frontend/awx/resources/templates/TemplatePage/TemplateJobs.tsx
@@ -3,12 +3,15 @@ import { usePersistentFilters } from '../../../../common/PersistentFilters';
 import { JobsList } from '../../../views/jobs/JobsList';
 import { useHostsJobsColumns } from '../../inventories/inventoryHostsPage/hooks/useHostsJobsColumns';
 
-export function TemplateJobs() {
+export function TemplateJobs(props: { resourceType: 'job_templates' | 'workflow_job_templates' }) {
+  const { resourceType } = props;
   usePersistentFilters('inventories');
   const jobsColumns = useHostsJobsColumns();
   const { id = '' } = useParams<{ id: string }>();
   const queryParams = {
-    job__job_template: id,
+    [resourceType === 'job_templates'
+      ? 'job__job_template'
+      : 'workflow_job__workflow_job_template']: id,
     not__launch_type: 'sync',
   };
   return <JobsList queryParams={queryParams} columns={jobsColumns} />;
