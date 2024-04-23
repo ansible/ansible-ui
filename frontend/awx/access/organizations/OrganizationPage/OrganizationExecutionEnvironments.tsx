46bba375dae63a3fd895f8d867ff57c4dc6abcdb
vsanghishetty <vishali.kamenani@ymail.com>
2024-04-23 09:55:40 -0400

    update to use existing component

--- frontend/awx/access/organizations/OrganizationPage/OrganizationExecutionEnvironments.tsx
+++ frontend/awx/access/organizations/OrganizationPage/OrganizationExecutionEnvironments.tsx
@@ -1,38 +1,10 @@
-import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
-import { PageTable, usePageNavigate } from '../../../../../framework';
 import { awxAPI } from '../../../common/api/awx-utils';
-import { useAwxView } from '../../../common/useAwxView';
-import { AwxRoute } from '../../../main/AwxRoutes';
-import { PlusCircleIcon } from '@patternfly/react-icons';
-import { ExecutionEnvironment } from '../../../interfaces/ExecutionEnvironment';
-import { useExecutionEnvironmentsColumns } from '../../../administration/execution-environments/hooks/useExecutionEnvironmentsColumns';
-import { useExecutionEnvironmentsFilters } from '../../../administration/execution-environments/hooks/useExecutionEnvironmentsFilters';
+import { ExecutionEnvironments } from '../../../administration/execution-environments/ExecutionEnvironments';
 
 export function OrganizationExecutionEnvironments() {
-  const params = useParams<{ id: string }>();
-  const { t } = useTranslation();
-  const pageHistory = usePageNavigate();
-  const toolbarFilters = useExecutionEnvironmentsFilters();
-  const tableColumns = useExecutionEnvironmentsColumns();
-  const view = useAwxView<ExecutionEnvironment>({
-    url: awxAPI`/organizations/${params.id ?? ''}/execution_environments/`,
-    toolbarFilters,
-    tableColumns,
-    disableQueryString: true,
-  });
+  const { id = '' } = useParams<{ id: string }>();
   return (
-    <PageTable<ExecutionEnvironment>
-      id="awx-execution-environments-table"
-      toolbarFilters={toolbarFilters}
-      tableColumns={tableColumns}
-      errorStateTitle={t('Error loading teams')}
-      emptyStateTitle={t('No execution environments yet')}
-      emptyStateDescription={t('To get started, create an execution environment.')}
-      emptyStateButtonIcon={<PlusCircleIcon />}
-      emptyStateButtonText={t('Create execution environment')}
-      emptyStateButtonClick={() => pageHistory(AwxRoute.CreateExecutionEnvironment)}
-      {...view}
-    />
+    <ExecutionEnvironments url={awxAPI`/execution_environments/`} executionEnvironmentId={id} />
   );
 }
