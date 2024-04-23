53051d96baec4f6a8e0eb129c62275078813ae64
vsanghishetty <vishali.kamenani@ymail.com>
2024-04-23 10:04:15 -0400

    makes changes

--- frontend/awx/access/organizations/OrganizationPage/OrganizationExecutionEnvironments.tsx
+++ frontend/awx/access/organizations/OrganizationPage/OrganizationExecutionEnvironments.tsx
@@ -1,10 +1,5 @@
-import { useParams } from 'react-router-dom';
-import { awxAPI } from '../../../common/api/awx-utils';
 import { ExecutionEnvironments } from '../../../administration/execution-environments/ExecutionEnvironments';
 
 export function OrganizationExecutionEnvironments() {
-  const { id = '' } = useParams<{ id: string }>();
-  return (
-    <ExecutionEnvironments url={awxAPI`/execution_environments/`} executionEnvironmentId={id} />
-  );
+  return <ExecutionEnvironments />;
 }
