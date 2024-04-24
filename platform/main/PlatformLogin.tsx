15cf3d41a51121fb4355939412722b9d3745d8a9
James Talton <jtalton@redhat.com>
2024-04-24 08:41:08 -0400

    Platform login screen (#266)
    Signed-off-by: James Talton <talton@gmail.com>

--- platform/main/PlatformLogin.tsx
+++ platform/main/PlatformLogin.tsx
@@ -1,20 +1,24 @@
 import { Page } from '@patternfly/react-core';
+import { t } from 'i18next';
 import { ReactNode } from 'react';
 import { mutate } from 'swr';
 import { LoadingState } from '../../framework/components/LoadingState';
 import { Login } from '../../frontend/common/Login';
 import { useGet } from '../../frontend/common/crud/useGet';
 import { gatewayAPI } from '../api/gateway-api-utils';
+import RedHatIcon from '../icons/redhat-icon.svg';
 import { UIAuth } from '../interfaces/UIAuth';
+// import LoginBackgroundDark from './LoginBackgroundDark.svg';
+// import LoginBackgroundLight from './LoginBackgroundLight.svg';
 import { usePlatformActiveUser } from './PlatformActiveUserProvider';
 
 export function PlatformLogin(props: { children: ReactNode }) {
-  const { activePlatformUser, refreshActivePlatformUser, activePlatformUserIsLoading } =
-    usePlatformActiveUser();
+  const { activePlatformUser, refreshActivePlatformUser } = usePlatformActiveUser();
   const { data: options } = useGet<UIAuth>(gatewayAPI`/ui_auth/`);
   const hideInputs = options ? !options.show_login_form : false;
+  // const { activeTheme } = usePageSettings();
 
-  if (activePlatformUserIsLoading) {
+  if (activePlatformUser === undefined) {
     return (
       <Page>
         <LoadingState />
@@ -32,6 +36,11 @@ export function PlatformLogin(props: { children: ReactNode }) {
         }}
         hideInputs={hideInputs}
         authOptions={options?.ssos}
+        loginDescription={t('Enter your single sign-on credentials.')}
+        icon={<RedHatIcon style={{ maxHeight: 64, maxWidth: 64 }} />}
+        brand={t('Red Hat')}
+        product={t('Ansible Automation Platform')}
+        // background={activeTheme === 'light' ? <LoginBackgroundLight /> : <LoginBackgroundDark />}
       />
     );
   }
