f3a60f5ceecde40d3dad75d9b064c28df270f9ef
MilanPospisil <arkanus@seznam.cz>
2024-04-25 15:50:42 +0200

    Notifications Form and Detail customize messages (#2126)

--- frontend/awx/interfaces/NotificationTemplate.ts
+++ frontend/awx/interfaces/NotificationTemplate.ts
@@ -1,5 +1,24 @@
 import { NotificationTemplate as SwaggerNotificationTemplate } from './generated-from-swagger/api';
 
+type NotificationType = {
+  message?: string | undefined;
+  body?: string | undefined;
+};
+
+type WorkflowApprovalMessages = {
+  denied?: NotificationType;
+  running?: NotificationType;
+  approved?: NotificationType;
+  timed_out?: NotificationType;
+};
+
+export type MessagesType = {
+  error?: NotificationType;
+  started?: NotificationType;
+  success?: NotificationType;
+  workflow_approval?: WorkflowApprovalMessages;
+};
+
 export interface NotificationTemplate
   extends Omit<
     SwaggerNotificationTemplate,
@@ -36,6 +55,7 @@ export interface NotificationTemplate
   notification_configuration: {
     [key: string]: number | string | boolean | string[] | { [key: string]: unknown };
   };
+  messages?: MessagesType | null;
   notification_type:
     | 'email'
     | 'grafana'
