7cc181213f98e8b137ea12c033df100cc3801600
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2024-04-26 10:00:23 -0400

    EDA User -> Roles Tab (#2167)

--- frontend/eda/access/interfaces/Assignment.ts
+++ frontend/eda/access/interfaces/Assignment.ts
@@ -10,5 +10,11 @@ export type Assignment = {
       description: string;
       managed: boolean;
     };
+    content_object: {
+      name: string;
+      id: number;
+    };
   };
+  object_id: string;
+  content_type: string;
 };
