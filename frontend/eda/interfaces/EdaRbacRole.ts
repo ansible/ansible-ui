3610e821ab7c5294a133e02d3548807b719c7b75
Michael Abashian <mabashian@users.noreply.github.com>
2024-04-30 12:56:08 -0400

    Update EDA roles list to support adding, editing and deleting custom roles (#2199)

--- frontend/eda/interfaces/EdaRbacRole.ts
+++ frontend/eda/interfaces/EdaRbacRole.ts
@@ -1,5 +1,18 @@
 import { RoleDefinition } from './generated/eda-api';
-export type EdaRbacRole = RoleDefinition;
+
+// extend the interface with additional properties
+export interface EdaRbacRole extends Omit<RoleDefinition, 'summary_fields'> {
+  summary_fields: {
+    created_by?: {
+      id: number;
+      username: string;
+    };
+    modified_by?: {
+      id: number;
+      username: string;
+    };
+  };
+}
 
 /*export interface EdaRbacRole {
   id: number;
