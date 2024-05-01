d2bd95e7c1821b8322a83805c51536714e154454
Sarah Akus <sarah.akus@gmail.com>
2024-05-01 15:18:38 -0400

    Controller -> Projects Test Scaffolding (#2210)

--- cypress.base.config.ts
+++ cypress.base.config.ts
@@ -4,7 +4,7 @@ const { DefinePlugin } = pkg;
 
 export const baseConfig: Cypress.ConfigOptions = {
   experimentalMemoryManagement: true,
-  numTestsKeptInMemory: 20,
+  numTestsKeptInMemory: 2,
   viewportWidth: 1600,
   viewportHeight: 1120,
   pageLoadTimeout: 120000,
