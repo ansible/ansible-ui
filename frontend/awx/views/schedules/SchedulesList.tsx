fd46a5d5aa70ef1d67cff370ff8ddc087e57570d
Adriana Cruz <88343229+acruzgon@users.noreply.github.com>
2024-05-01 13:05:19 -0600

    fixing Enable/Disable a Schedule from Details Page (#2212)

--- frontend/awx/views/schedules/SchedulesList.tsx
+++ frontend/awx/views/schedules/SchedulesList.tsx
@@ -43,7 +43,8 @@ export function SchedulesList(props: { sublistEndpoint?: string }) {
   const createUrl = useGetSchedulCreateUrl(apiEndPoint);
   const toolbarActions = useScheduleToolbarActions(view.unselectItemsAndRefresh, apiEndPoint);
   const rowActions = useSchedulesActions({
-    onScheduleToggleorDeleteCompleted: () => void view.refresh(),
+    onScheduleDeleteCompleted: () => void view.refresh(),
+    onScheduleToggleCompleted: () => void view.refresh(),
     sublistEndpoint: apiEndPoint,
   });
   return (
