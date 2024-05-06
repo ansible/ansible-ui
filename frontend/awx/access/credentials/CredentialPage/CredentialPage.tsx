94a17fa78811b5589eff5725e1ebca858aeddd2c
lgalis <lgalis@users.noreply.github.com>
2024-05-06 16:42:14 -0400

    Follow up to https://github.com/ansible/ansible-ui/pull/2232 - Remove duplicate tabs (#2245)

--- frontend/awx/access/credentials/CredentialPage/CredentialPage.tsx
+++ frontend/awx/access/credentials/CredentialPage/CredentialPage.tsx
@@ -51,8 +51,6 @@ export function CredentialPage() {
           { label: t('Details'), page: AwxRoute.CredentialDetails },
           { label: t('Access'), page: AwxRoute.CredentialAccess },
           { label: t('Job Templates'), page: AwxRoute.CredentialJobTemplates },
-          { label: t('Job Templates'), page: AwxRoute.CredentialJobTemplates },
-          { label: t('Job Templates'), page: AwxRoute.CredentialJobTemplates },
           { label: t('Team Access'), page: AwxRoute.CredentialTeamAccess },
           { label: t('User Access'), page: AwxRoute.CredentialUserAccess },
         ]}
