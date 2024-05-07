ab6ce93d05ff7ab4134a298f8532ca0e9fbad620
jerabekjiri <jerabekjirka@email.cz>
2024-05-07 17:36:24 +0200

    reuse sharedTemplateSurvey for wfjt test
    Issue: AAP-22947

--- cypress/e2e/awx/resources/workflowJobTemplateSurveys.cy.ts
+++ cypress/e2e/awx/resources/workflowJobTemplateSurveys.cy.ts
@@ -1,129 +1,153 @@
 import { Inventory } from '../../../../frontend/awx/interfaces/Inventory';
 import { WorkflowJobTemplate } from '../../../../frontend/awx/interfaces/WorkflowJobTemplate';
 import { Organization } from '../../../../frontend/awx/interfaces/Organization';
-// import { awxAPI } from '../../../support/formatApiPathForAwx';
+import { ReusableTemplateSurveyTestSuite } from './sharedTemplateSurvey';
 
 describe('Workflow Job Templates Surveys', function () {
   let inventory: Inventory;
   let workflowJobTemplate: WorkflowJobTemplate;
+  let reusableTemplateSurveyTestSuite: ReusableTemplateSurveyTestSuite;
 
   before(function () {
     cy.awxLogin();
   });
 
-  beforeEach(function () {
-    cy.createAwxInventory({ organization: (this.globalOrganization as Organization).id }).then(
-      (inv) => {
-        inventory = inv;
+  describe('WFJT Surveys: Create, Edit and Delete', function () {
+    const question = {
+      question_name: "Who's that?",
+      question_description: 'The person behind this.',
+      variable: 'who_is_that',
+      default: 'John Doe',
+      type: 'text',
+      label: 'Text',
+    };
 
-        cy.createAwxWorkflowJobTemplate({
-          organization: (this.globalOrganization as Organization).id,
-          inventory: inventory.id,
-        }).then((wfjt) => {
-          workflowJobTemplate = wfjt;
-        });
-      }
-    );
+    before(function () {
+      cy.createAwxInventory({ organization: (this.globalOrganization as Organization).id }).then(
+        (inv) => {
+          inventory = inv;
+
+          cy.createAwxWorkflowJobTemplate({
+            organization: (this.globalOrganization as Organization).id,
+            inventory: inventory.id,
+          }).then((wfjt) => {
+            workflowJobTemplate = wfjt;
+            reusableTemplateSurveyTestSuite = new ReusableTemplateSurveyTestSuite(
+              workflowJobTemplate
+            );
+
+            cy.visit(`/templates/workflow_job_template/${workflowJobTemplate.id}/survey`);
+          });
+        }
+      );
+    });
+
+    after(function () {
+      cy.deleteAwxWorkflowJobTemplate(workflowJobTemplate, { failOnStatusCode: false });
+      cy.deleteAwxInventory(inventory, { failOnStatusCode: false });
+    });
+
+    it('can create a required survey from surveys tab list of a WFJT, toggle survey on, and assert info on surveys list view', () =>
+      reusableTemplateSurveyTestSuite.canCreateSurvey(question));
+
+    it('can edit a WFJT survey from surveys list view and assert info on surveys list view', () =>
+      reusableTemplateSurveyTestSuite.canEditSurvey(question));
+
+    it('can delete a WFJT survey from the surveys list view and assert deletion', () =>
+      reusableTemplateSurveyTestSuite.canDeleteSurvey());
+
+    it('can create multiple surveys, assert order, change order, and assert new order, then bulk delete all surveys', () =>
+      reusableTemplateSurveyTestSuite.canCreateMultipleSurvey());
   });
 
-  afterEach(function () {
-    cy.deleteAwxWorkflowJobTemplate(workflowJobTemplate, { failOnStatusCode: false });
-    cy.deleteAwxInventory(inventory, { failOnStatusCode: false });
-  });
+  describe.skip('WFJT Surveys: Launch WFJT with Survey Enabled', function () {
+    before(function () {
+      cy.createAwxInventory({ organization: (this.globalOrganization as Organization).id }).then(
+        (inv) => {
+          inventory = inv;
 
-  describe('WFJT Surveys: Create and Edit', function () {
-    it.skip('can create a required survey from surveys tab list of a WFJT, toggle survey on, and assert info on surveys list view', function () {
-      //Use the WFJT created in the beforeEach block
-      //Assert the empty state survey list
-      //Assert creation of the survey and info showing on survey list
+          cy.createAwxWorkflowJobTemplate({
+            organization: (this.globalOrganization as Organization).id,
+            inventory: inventory.id,
+          }).then((wfjt) => {
+            workflowJobTemplate = wfjt;
+            reusableTemplateSurveyTestSuite = new ReusableTemplateSurveyTestSuite(
+              workflowJobTemplate
+            );
+
+            cy.visit(`/templates/workflow_job_template/${workflowJobTemplate.id}/survey`);
+          });
+        }
+      );
     });
 
-    it.skip('can edit a WFJT survey from surveys list view and assert info on surveys list view', function () {
-      //Use the WFJT created in the beforeEach block
-      //Assert survey creation and info showing on survey list
-      //Assert info on survey list after editing
-    });
-  });
-
-  describe('WFJT Surveys: Delete', function () {
-    it.skip('can delete a WFJT survey from the surveys list view and assert deletion', function () {
-      //Use the WFJT created in the beforeEach block
-      //Create survey, assert existence
-      //Delete and assert deletion
+    after(function () {
+      cy.deleteAwxWorkflowJobTemplate(workflowJobTemplate, { failOnStatusCode: false });
+      cy.deleteAwxInventory(inventory, { failOnStatusCode: false });
     });
 
-    it.skip('can create multiple surveys, assert order, change order, and assert new order, then bulk delete all surveys', function () {
-      //Use the WFJT created in the beforeEach block
-      //Create multiple surveys within the WFJT, assert existence and order of surveys
-      //Change order and assert new order
-      //Bulk delete and assert deletion
-    });
-  });
-
-  describe('WFJT Surveys: Launch WFJT with Survey Enabled', function () {
-    //For all tests in this section- consider creating a test that loops over the 7 survey types
-
-    it.skip('can create and enable a Text survey type, launch WFJT, view default survey answer, edit survey answer, complete launch, and assert survey answer on completed job', function () {
-      //Use the WFJT created in the beforeEach block
-      //Assert the type of survey created
-      //Assert info after editing
-      //Assert job output screen
-      //Wait for job completion
-      //Assert survey info on job details screen
-    });
-
-    it.skip('can create and enable a Text Area survey type, launch WFJT, view default survey answer, edit survey answer, complete launch, and assert survey answer on completed job', function () {
-      //Use the WFJT created in the beforeEach block
-      //Assert the type of survey created
-      //Assert info after editing
-      //Assert job output screen
-      //Wait for job completion
-      //Assert survey info on job details screen
-    });
-
-    it.skip('can create and enable a Password survey type, launch WFJT, view default survey answer, edit survey answer, complete launch, and assert survey answer on completed job', function () {
-      //Use the WFJT created in the beforeEach block
-      //Assert the type of survey created
-      //Assert info after editing
-      //Assert job output screen
-      //Wait for job completion
-      //Assert survey info on job details screen
-    });
-
-    it.skip('can create and enable a single select Multiple Choice survey type, launch WFJT, view default survey answer, edit survey answer, complete launch, and assert survey answer on completed job', function () {
-      //Use the WFJT created in the beforeEach block
-      //Assert the type of survey created
-      //Assert info after editing
-      //Assert job output screen
-      //Wait for job completion
-      //Assert survey info on job details screen
-    });
-
-    it.skip('can create and enable a multiple select Multiple Choice survey type, launch WFJT, view default survey answer, edit survey answer, complete launch, and assert survey answer on completed job', function () {
-      //Use the WFJT created in the beforeEach block
-      //Assert the type of survey created
-      //Assert info after editing
-      //Assert job output screen
-      //Wait for job completion
-      //Assert survey info on job details screen
-    });
-
-    it.skip('can create and enable a Integer survey type, launch WFJT, view default survey answer, edit survey answer, complete launch, and assert survey answer on completed job', function () {
-      //Use the WFJT created in the beforeEach block
-      //Assert the type of survey created
-      //Assert info after editing
-      //Assert job output screen
-      //Wait for job completion
-      //Assert survey info on job details screen
-    });
-
-    it.skip('can create and enable a Float survey type, launch WFJT, view default survey answer, edit survey answer, complete launch, and assert survey answer on completed job', function () {
-      //Use the WFJT created in the beforeEach block
-      //Assert the type of survey created
-      //Assert info after editing
-      //Assert job output screen
-      //Wait for job completion
-      //Assert survey info on job details screen
-    });
+    const surveyTypes = [
+      {
+        type: 'text',
+        label: 'Text',
+        question_name: 'Text Answer',
+        question_description: 'Text description.',
+        variable: 'text_answer',
+        default: 'default text answer',
+      },
+      {
+        type: 'textarea',
+        label: 'Textarea',
+        question_name: 'Textarea Answer',
+        question_description: 'Textarea description.',
+        variable: 'textarea_answer',
+        default: 'default textarea answer',
+      },
+      {
+        type: 'password',
+        label: 'Password',
+        question_name: 'Password Answer',
+        question_description: 'Password description.',
+        variable: 'password_answer',
+        default: 'default password answer',
+      },
+      {
+        type: 'integer',
+        label: 'Integer',
+        question_name: 'Integer Answer',
+        question_description: 'Integer description.',
+        variable: 'integer_answer',
+        max: 1338,
+        default: '1337',
+      },
+      {
+        type: 'float',
+        label: 'Float',
+        question_name: 'Float Answer',
+        question_description: 'Float description.',
+        variable: 'float_answer',
+        default: '13.37',
+      },
+      {
+        type: 'multiplechoice',
+        label: 'Multiple Choice (single select)',
+        question_name: 'Multiplechoice Answer',
+        question_description: 'multiplechoice description.',
+        variable: 'multiplechoice_answer',
+        choices: ['foo', 'bar', 'baz'],
+        default: 'bar',
+      },
+      {
+        type: 'multiselect',
+        label: 'Multiple Choice (multiple select)',
+        question_name: 'Multiselect Answer',
+        question_description: 'Multiselect description.',
+        variable: 'multiselect_answer',
+        choices: ['foo', 'bar', 'baz'],
+        default: 'foo\nbar',
+      },
+    ];
+    it('can create all 7 types of survey types, enable survey, launch WFJT, view default survey answer, complete launch, and assert survey answer on completed job', () =>
+      reusableTemplateSurveyTestSuite.canCreateAllSurveyTypes(surveyTypes));
   });
 });
