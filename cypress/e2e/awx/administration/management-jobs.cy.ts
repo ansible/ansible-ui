74cee5ee46b80d1303139ee0e6b59f12d7ac7fa6
vsanghishetty <vishali.kamenani@ymail.com>
2024-05-07 17:02:36 -0400

    updates test name

--- cypress/e2e/awx/administration/management-jobs.cy.ts
+++ cypress/e2e/awx/administration/management-jobs.cy.ts
@@ -88,7 +88,7 @@ describe('Management Jobs Page - List and Launch Jobs', () => {
   const managementJobsWithModal = ['Cleanup Activity Stream', 'Cleanup Job Details'];
   const rententionDays = '4';
   managementJobsWithModal.forEach((jobName) => {
-    it(`admin can launch management job: ${jobName} with retention days`, () => {
+    it(`admin can launch management job: ${jobName} with the retention days set`, () => {
       cy.intercept('GET', 'api/v2/system_job_templates/?order_by=name&page=1&page_size=10').as(
         'getManagementJobsListPage'
       );
