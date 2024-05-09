b47728b46e3fd19cfb45686e91169a3cd0cd3ed1
lgalis <lgalis@redhat.com>
2024-05-08 22:51:48 -0400

    Bulk delete credential types

--- frontend/eda/access/credential-types/hooks/useDeleteCredentialTypes.tsx
+++ frontend/eda/access/credential-types/hooks/useDeleteCredentialTypes.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { compareStrings } from '../../../../../framework';
 import { useNameColumn } from '../../../../common/columns';
@@ -16,27 +16,50 @@ export function useDeleteCredentialTypes(
   const confirmationColumns = useCredentialTypesColumns();
   const deleteActionNameColumn = useNameColumn({ disableLinks: true, disableSort: true });
   const actionColumns = useMemo(() => [deleteActionNameColumn], [deleteActionNameColumn]);
+
+  const cannotDeleteManagedCredentialType = (credentialType: EdaCredentialType) =>
+    credentialType.managed
+      ? t(`The credential type is provided by the system and is read-only.`)
+      : '';
   const bulkAction = useEdaBulkConfirmation<EdaCredentialType>();
-  return useCallback(
-    (credentialTypes: EdaCredentialType[]) => {
-      bulkAction({
-        title: t('Permanently delete credential types', { count: credentialTypes.length }),
-        confirmText: t('Yes, I confirm that I want to delete these {{count}} credential types.', {
-          count: credentialTypes.length,
-        }),
-        actionButtonText: t('Delete credential types', { count: credentialTypes.length }),
-        items: credentialTypes.sort((l, r) => compareStrings(l.name, r.name)),
-        keyFn: idKeyFn,
-        isDanger: true,
-        confirmationColumns,
-        actionColumns,
-        onComplete,
-        actionFn: (credentialType: EdaCredentialType, signal) => {
-          const url = edaAPI`/credential-types/${credentialType.id.toString()}/`;
-          return requestDelete(url, signal);
-        },
-      });
-    },
-    [actionColumns, bulkAction, confirmationColumns, onComplete, t]
-  );
+  const deleteCredentialTypes = (credentialTypes: EdaCredentialType[]) => {
+    const undeletableManagedCredentialTypes: EdaCredentialType[] = credentialTypes.filter(
+      (credentialType) => credentialType.managed
+    );
+
+    bulkAction({
+      title: t('Permanently delete credential types', { count: credentialTypes.length }),
+      confirmText: t('Yes, I confirm that I want to delete these {{count}} credential types.', {
+        count: credentialTypes.length,
+      }),
+      actionButtonText: t('Delete credential types', { count: credentialTypes.length }),
+      items: credentialTypes.sort((l, r) => compareStrings(l.name, r.name)),
+      alertPrompts: undeletableManagedCredentialTypes.length
+        ? [
+            ...(undeletableManagedCredentialTypes.length
+              ? [
+                  t(
+                    '{{count}} of the selected credential types cannot be deleted because they are read-only.',
+                    {
+                      count: undeletableManagedCredentialTypes.length,
+                    }
+                  ),
+                ]
+              : []),
+          ]
+        : undefined,
+      isItemNonActionable: (credentialType: EdaCredentialType) =>
+        cannotDeleteManagedCredentialType(credentialType),
+      keyFn: idKeyFn,
+      isDanger: true,
+      confirmationColumns,
+      actionColumns,
+      onComplete,
+      actionFn: (credentialType: EdaCredentialType, signal) => {
+        const url = edaAPI`/credential-types/${credentialType.id.toString()}/`;
+        return requestDelete(url, signal);
+      },
+    });
+  };
+  return deleteCredentialTypes;
 }
