e7a04e854164ef76879f3e3058f3bfc10badcbda
Pratyush Bhandari <prbhanda@redhat.com>
2024-05-10 11:25:42 -0400

    Fix e2e instance test

--- cypress/e2e/awx/administration/instances.cy.ts
+++ cypress/e2e/awx/administration/instances.cy.ts
@@ -271,10 +271,9 @@ describe('Instances: Peers', () => {
       cy.get('header').contains('Select Peer Addresses');
       cy.get('button').contains('Associate peer(s)').should('have.attr', 'aria-disabled', 'true');
       cy.filterTableBySingleText(instanceToAssociate.hostname, true);
-      cy.intercept('GET', awxAPI`/instances/${instance.id.toString()}/`).as('instanceA');
-      cy.intercept('GET', awxAPI`/instances/${instanceToAssociate.id.toString()}/`).as('instanceB');
+      cy.intercept('GET', awxAPI`/instances/${instanceToAssociate.id.toString()}/`).as('instanceA');
       cy.getByDataCy('checkbox-column-cell').find('input').click();
-      cy.wait(['@instanceA', '@instanceB']);
+      cy.wait('@instanceA');
       cy.get('button').contains('Associate peer(s)').click();
       cy.get('button').contains('Close').click();
     });
