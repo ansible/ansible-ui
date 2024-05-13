16133d1900f2edd57ca372e47d4979e16aa7a811
lgalis <lgalis@users.noreply.github.com>
2024-05-13 16:26:35 -0400

    Bulk delete for credential types (#2268)

--- frontend/eda/access/credential-types/hooks/useCredentialTypeActions.tsx
+++ frontend/eda/access/credential-types/hooks/useCredentialTypeActions.tsx
@@ -36,7 +36,6 @@ export function useCredentialTypeToolbarActions(
         selection: PageActionSelection.Multiple,
         icon: TrashIcon,
         label: t('Delete selected credential types'),
-        isDisabled: () => 'Not implemented',
         onClick: deleteCredentialTypes,
         isDanger: true,
       },
