9c94dbf1bcd68e9230c3abbd0001af656fb93fb9
MilanPospisil <arkanus@seznam.cz>
2024-05-13 11:12:54 +0200

    Jobs output expandable section (#2231)

---
+++ frontend/awx/views/jobs/JobOutput/JobEventEllipsis.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  border-radius: 1em;
+  background-color: white;
+  font-size: 0.6rem;
+  width: max-content;
+  padding: 0em 1em;
+  margin-left: auto;
+  margin-right: -0.3em;
+  color: black;
+`;
+
+export function JobEventEllipsis() {
+  return <Wrapper>...</Wrapper>;
+}
