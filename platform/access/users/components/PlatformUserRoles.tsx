20a70110504f17577589c5d599c2f5cc28777cce
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2024-05-14 10:52:07 -0400

    Use hook for checking if AWX/EDA are enabled (#287)

--- platform/access/users/components/PlatformUserRoles.tsx
+++ platform/access/users/components/PlatformUserRoles.tsx
@@ -1,20 +1,15 @@
 import { useMemo } from 'react';
 import { PageRoutedTabs } from '../../../../framework/PageTabs/PageRoutedTabs';
-import { useGatewayServices } from '../../../main/GatewayServices';
+import { useGatewayService } from '../../../main/GatewayServices';
 import { useTranslation } from 'react-i18next';
 import { PlatformRoute } from '../../../main/PlatformRoutes';
 import { useParams } from 'react-router-dom';
 
 export function PlatformUserRoles() {
-  const [gatewayServices, _] = useGatewayServices();
   const params = useParams<{ id: string }>();
   const { t } = useTranslation();
-  const awxService = gatewayServices.find(
-    (service) => service.summary_fields?.service_cluster?.service_type === 'controller'
-  );
-  const edaService = gatewayServices.find(
-    (service) => service.summary_fields?.service_cluster?.service_type === 'eda'
-  );
+  const awxService = useGatewayService('controller');
+  const edaService = useGatewayService('eda');
   const rolesTabs = useMemo(() => {
     return [
       ...(awxService
