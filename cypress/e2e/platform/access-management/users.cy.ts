2f0c50959f137e69f6be71bfc3c493bc46959df3
Vishali Kamenani <95239214+vishsanghishetty@users.noreply.github.com>
2024-05-16 11:10:57 -0400

    Platform Org-Users tab and Platform Users-Teams tab tests (#289)

--- cypress/e2e/platform/access-management/users.cy.ts
+++ cypress/e2e/platform/access-management/users.cy.ts
@@ -1,6 +1,8 @@
 import { randomString } from '../../../../framework/utils/random-string';
 import { gatewayV1API } from '../../../../platform/api/gateway-api-utils';
+import { PlatformTeam } from '../../../../platform/interfaces/PlatformTeam';
 import { PlatformUser } from '../../../../platform/interfaces/PlatformUser';
+import { PlatformOrganization } from '../../../../platform/interfaces/PlatformOrganization';
 
 describe('Users - create, edit and delete', () => {
   beforeEach(() => {
@@ -11,48 +13,41 @@ describe('Users - create, edit and delete', () => {
 
   it('edits a user from the list view and delete it from the ui', () => {
     cy.createPlatformUser().then((createdPlatformUser: PlatformUser) => {
-      cy.searchAndDisplayResourceByFilterOption(createdPlatformUser.username, 'username').then(
-        () => {
-          cy.clickTableRowAction('username', createdPlatformUser.username, 'edit-user', {
-            inKebab: true,
-          });
-          cy.verifyPageTitle('Edit user');
-          cy.get('[data-cy="username"]').clear().type(`edited-${createdPlatformUser.username}`);
-          cy.get('[data-cy="Submit"]').click();
-          cy.verifyPageTitle('Users');
-          cy.selectTableRowByCheckbox('username', `edited-${createdPlatformUser.username}`);
-          cy.clickToolbarKebabAction('delete-selected-users');
-          cy.get('#confirm').click();
-          cy.clickButton(/^Delete user/);
-          cy.clickButton('Close');
-        }
-      );
+      cy.clickTableRowAction('username', createdPlatformUser.username, 'edit-user', {
+        inKebab: true,
+      });
+      cy.verifyPageTitle('Edit user');
+      cy.get('[data-cy="username"]').clear().type(`edited-${createdPlatformUser.username}`);
+      cy.get('[data-cy="Submit"]').click();
+      cy.verifyPageTitle('Users');
+      cy.selectTableRowByCheckbox('username', `edited-${createdPlatformUser.username}`);
+      cy.clickToolbarKebabAction('delete-selected-users');
+      cy.get('#confirm').click();
+      cy.clickButton(/^Delete user/);
+      cy.clickButton('Close');
+      cy.clickButton(/^Clear all filters$/);
     });
   });
 
   it('edits a user from the details page and delete it from the ui', () => {
     cy.createPlatformUser().then((createdPlatformUser: PlatformUser) => {
-      cy.searchAndDisplayResourceByFilterOption(createdPlatformUser.username, 'username').then(
-        () => {
-          cy.clickTableRowLink('username', createdPlatformUser.username);
-          cy.contains('a[role="tab"]', 'Details').click();
-          cy.get('[data-cy="edit-user"]').click();
-          cy.verifyPageTitle('Edit user');
-          cy.get('[data-cy="username"]').clear().type(`edited-${createdPlatformUser.username}`);
-          cy.get('[data-cy="Submit"]').click();
-          cy.clickPageAction('delete-user');
-          cy.intercept('DELETE', gatewayV1API`/users/${createdPlatformUser.id.toString()}/`).as(
-            'deleteUser'
-          );
-          cy.get('#confirm').click();
-          cy.clickButton(/^Delete user/);
-          cy.wait('@deleteUser')
-            .its('response')
-            .then((response) => {
-              expect(response?.statusCode).to.eql(204);
-            });
-        }
+      cy.clickTableRowLink('username', createdPlatformUser.username);
+      cy.contains('a[role="tab"]', 'Details').click();
+      cy.get('[data-cy="edit-user"]').click();
+      cy.verifyPageTitle('Edit user');
+      cy.get('[data-cy="username"]').clear().type(`edited-${createdPlatformUser.username}`);
+      cy.get('[data-cy="Submit"]').click();
+      cy.clickPageAction('delete-user');
+      cy.intercept('DELETE', gatewayV1API`/users/${createdPlatformUser.id.toString()}/`).as(
+        'deleteUser'
       );
+      cy.get('#confirm').click();
+      cy.clickButton(/^Delete user/);
+      cy.wait('@deleteUser')
+        .its('response')
+        .then((response) => {
+          expect(response?.statusCode).to.eql(204);
+        });
     });
   });
 
@@ -68,6 +63,7 @@ describe('Users - create, edit and delete', () => {
           cy.contains(/^Success$/).should('be.visible');
           cy.containsBy('button', /^Close$/).click();
         });
+        cy.clickButton(/^Clear all filters$/);
       });
     });
   });
@@ -111,6 +107,7 @@ describe('User Types - creates users of type normal and system admin', () => {
         cy.clickButton(/^Delete user/);
         cy.clickButton('Close');
       });
+    cy.clickButton(/^Clear all filters$/);
   });
 
   it.skip('admin creates a normal user, log in as normal user, and verify auth type is set to local', () => {
@@ -147,7 +144,6 @@ describe('User Types - creates users of type normal and system admin', () => {
         cy.intercept('GET', gatewayV1API`/users/${createdNormalUser.id.toString()}/`).as(
           'normalUser'
         );
-        cy.searchAndDisplayResourceByFilterOption(createdNormalUser.username, 'username');
         cy.clickTableRowLink('username', createdNormalUser.username);
 
         cy.contains('a[role="tab"]', 'Details').click();
@@ -167,5 +163,42 @@ describe('User Types - creates users of type normal and system admin', () => {
           cy.deletePlatformUser(createdNormalUser, { failOnStatusCode: false });
         });
       });
+  });
+});
+
+describe('Users - Teams and Roles tab tests', () => {
+  let platformTeam: PlatformTeam;
+  beforeEach(function () {
+    cy.platformLogin();
+    cy.createPlatformTeam({
+      organization: (this.globalPlatformOrganization as PlatformOrganization).id,
+      users: [],
+    }).then((testPlatformTeam: PlatformTeam) => {
+      platformTeam = testPlatformTeam;
+    });
+    cy.navigateTo('platform', 'users');
+    cy.verifyPageTitle('Users');
+  });
+
+  it('should add and remove a team from teams tab', () => {
+    cy.createPlatformUser().then((createdPlatformUser: PlatformUser) => {
+      cy.clickTableRowLink('username', createdPlatformUser.username);
+      cy.clickTab('Teams', true);
+      cy.get('[data-cy="add-team(s)"]').click();
+      cy.getModal().within(() => {
+        cy.selectTableRowByCheckbox('name', platformTeam.name);
+        cy.getBy('#submit').click();
+      });
+      cy.getModal().should('not.exist');
+      cy.selectTableRowByCheckbox('name', platformTeam.name);
+      cy.clickToolbarKebabAction('remove-selected-teams');
+      cy.getModal().within(() => {
+        cy.get('#confirm').click();
+        cy.get('#submit').click();
+        cy.contains(/^Success$/).should('be.visible');
+        cy.containsBy('button', /^Close$/).click();
+      });
+      cy.clickButton(/^Clear all filters$/);
+    });
   });
 });
