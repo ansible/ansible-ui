da2aa1299b6eff200376bb9de5ddc6554b97933e
lgalis <lgalis@users.noreply.github.com>
2024-05-17 08:54:47 -0400

    [AAP-23477] AWX - Add user and team access to execution environments (#2331)
    Co-authored-by: Vishali Kamenani <95239214+vishsanghishetty@users.noreply.github.com>

---
+++ frontend/awx/administration/execution-environments/components/ExecutionEnvironmentAddTeams.tsx
@@ -0,0 +1,164 @@
+import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+import {
+  LoadingPage,
+  PageHeader,
+  PageLayout,
+  PageWizard,
+  PageWizardStep,
+  useGetPageUrl,
+  usePageNavigate,
+} from '../../../../../framework';
+import { RoleAssignmentsReviewStep } from '../../../../common/access/RolesWizard/steps/RoleAssignmentsReviewStep';
+import { postRequest } from '../../../../common/crud/Data';
+import { useGet } from '../../../../common/crud/useGet';
+import { AwxSelectRolesStep } from '../../../access/common/AwxRolesWizardSteps/AwxSelectRolesStep';
+import { AwxSelectTeamsStep } from '../../../access/common/AwxRolesWizardSteps/AwxSelectTeamsStep';
+import { awxErrorAdapter } from '../../../common/adapters/awxErrorAdapter';
+import { awxAPI } from '../../../common/api/awx-utils';
+import { useAwxBulkActionDialog } from '../../../common/useAwxBulkActionDialog';
+import { AwxRbacRole } from '../../../interfaces/AwxRbacRole';
+import { ExecutionEnvironment } from '../../../interfaces/ExecutionEnvironment';
+import { Team } from '../../../interfaces/Team';
+import { AwxRoute } from '../../../main/AwxRoutes';
+
+interface WizardFormValues {
+  teams: Team[];
+  awxRoles: AwxRbacRole[];
+}
+
+interface TeamRolePair {
+  team: Team;
+  role: AwxRbacRole;
+}
+
+export function ExecutionEnvironmentAddTeams() {
+  const { t } = useTranslation();
+  const getPageUrl = useGetPageUrl();
+  const params = useParams<{ id: string }>();
+  const pageNavigate = usePageNavigate();
+  const { data: executionenvironment, isLoading } = useGet<ExecutionEnvironment>(
+    awxAPI`/execution_environments/${params.id ?? ''}/`
+  );
+  const userProgressDialog = useAwxBulkActionDialog<TeamRolePair>();
+
+  if (isLoading || !executionenvironment) return <LoadingPage />;
+
+  const steps: PageWizardStep[] = [
+    {
+      id: 'teams',
+      label: t('Select team(s)'),
+      inputs: (
+        <AwxSelectTeamsStep
+          descriptionForTeamsSelection={t(
+            'Select the team(s) that you want to give access to {{executionenvironmentName}}.',
+            {
+              executionenvironmentName: executionenvironment?.name,
+            }
+          )}
+        />
+      ),
+      validate: (formData, _) => {
+        const { teams } = formData as { teams: Team[] };
+        if (!teams?.length) {
+          throw new Error(t('Select at least one team.'));
+        }
+      },
+    },
+    {
+      id: 'roles',
+      label: t('Select roles to apply'),
+      inputs: (
+        <AwxSelectRolesStep
+          contentType="executionenvironment"
+          fieldNameForPreviousStep="teams"
+          descriptionForRoleSelection={t('Choose roles to apply to {{executionenvironmentName}}.', {
+            executionenvironmentName: executionenvironment?.name,
+          })}
+        />
+      ),
+      validate: (formData, _) => {
+        const { awxRoles } = formData as { awxRoles: AwxRbacRole[] };
+        if (!awxRoles?.length) {
+          throw new Error(t('Select at least one role.'));
+        }
+      },
+    },
+    {
+      id: 'review',
+      label: t('Review'),
+      inputs: <RoleAssignmentsReviewStep />,
+    },
+  ];
+
+  const onSubmit = async (data: WizardFormValues) => {
+    const { teams, awxRoles } = data;
+    const items: TeamRolePair[] = [];
+    for (const team of teams) {
+      for (const role of awxRoles) {
+        items.push({ team, role });
+      }
+    }
+    return new Promise<void>((resolve) => {
+      userProgressDialog({
+        title: t('Add roles'),
+        keyFn: ({ team, role }) => `${team.id}_${role.id}`,
+        items,
+        actionColumns: [
+          { header: t('Team'), cell: ({ team }) => team.name },
+          { header: t('Role'), cell: ({ role }) => role.name },
+        ],
+        actionFn: ({ team, role }) =>
+          postRequest(awxAPI`/role_team_assignments/`, {
+            team: team.id,
+            role_definition: role.id,
+            content_type: 'awx.executionenvironment',
+            object_id: executionenvironment.id,
+          }),
+        onComplete: () => {
+          resolve();
+        },
+        onClose: () => {
+          pageNavigate(AwxRoute.ExecutionEnvironmentTeamAccess, {
+            params: { id: executionenvironment.id.toString() },
+          });
+        },
+      });
+    });
+  };
+
+  return (
+    <PageLayout>
+      <PageHeader
+        title={t('Add roles')}
+        breadcrumbs={[
+          { label: t('ExecutionEnvironments'), to: getPageUrl(AwxRoute.ExecutionEnvironments) },
+          {
+            label: executionenvironment?.name,
+            to: getPageUrl(AwxRoute.ExecutionEnvironmentDetails, {
+              params: { id: executionenvironment?.id },
+            }),
+          },
+          {
+            label: t('Team Access'),
+            to: getPageUrl(AwxRoute.ExecutionEnvironmentTeamAccess, {
+              params: { id: executionenvironment?.id },
+            }),
+          },
+          { label: t('Add roles') },
+        ]}
+      />
+      <PageWizard<WizardFormValues>
+        errorAdapter={awxErrorAdapter}
+        steps={steps}
+        onSubmit={onSubmit}
+        disableGrid
+        onCancel={() => {
+          pageNavigate(AwxRoute.ExecutionEnvironmentTeamAccess, {
+            params: { id: executionenvironment?.id },
+          });
+        }}
+      />
+    </PageLayout>
+  );
+}
