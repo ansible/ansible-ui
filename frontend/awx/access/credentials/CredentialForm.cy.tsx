020e54ba4981b8be3331eca0dd1bb2a9cc1683b1
Alex Corey <alex.swansboro@gmail.com>
2024-05-20 10:45:31 -0400

    uses field inputs values from api to assert on form

--- frontend/awx/access/credentials/CredentialForm.cy.tsx
+++ frontend/awx/access/credentials/CredentialForm.cy.tsx
@@ -1,14 +1,30 @@
 import { CreateCredential } from './CredentialForm';
-function assertOnSubFormField(formGroupName: string, labelText: string, required: boolean = false) {
-  cy.getByDataCy(`${formGroupName}-form-group`).scrollIntoView();
-  cy.getByDataCy(`${formGroupName}-form-group`)
-    .should('be.visible')
-    .within(() => {
-      cy.get('label').should('include.text', labelText);
-      if (required) {
-        cy.get('label').should('have.descendants', 'span.pf-v5-c-form__label-required');
+import credentialTypes from '../../../../cypress/fixtures/credentialTypes.json';
+function assertOnSubFormFields(credentialType: string | RegExp) {
+  const selectedType = credentialTypes.results.find((CT) => CT.name === credentialType);
+  cy.get('section[role="group"]').within(() => {
+    selectedType?.inputs.fields?.forEach(({ id, label, type }) => {
+      if (type === 'boolean') {
+        cy.getByDataCy(id).should('be.visible');
+        return;
       }
+      let newID = id.includes('_') ? id.split('_').join('-') : id;
+      if (id === 'become_method') {
+        newID = 'become_method';
+      }
+      const isRequired = selectedType?.inputs.required?.includes(id);
+
+      cy.getByDataCy(`${newID}-form-group`).scrollIntoView();
+      cy.getByDataCy(`${newID}-form-group`)
+        .should('be.visible')
+        .within(() => {
+          cy.get('label').should('include.text', label);
+          if (isRequired) {
+            cy.get('label').should('have.descendants', 'span.pf-v5-c-form__label-required');
+          }
+        });
     });
+  });
 }
 
 describe('Credential form', () => {
@@ -64,308 +80,147 @@ describe('Credential form', () => {
   });
   it('Create credential using Amazon Web Services', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Amazon Web Services');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('username', 'Access Key', true);
-      assertOnSubFormField('password', 'Secret Key', true);
-      assertOnSubFormField('security-token', 'STS Token');
-    });
+    assertOnSubFormFields('Amazon Web Services');
   });
   it('Create credential using Ansible Galaxy/Automation Hub API Token', () => {
     cy.selectSingleSelectOption(
       '[data-cy="credential_type"]',
       'Ansible Galaxy/Automation Hub API Token'
     );
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('url', 'Galaxy Server URL', true);
-      assertOnSubFormField('auth-url', 'Auth Server URL');
-      assertOnSubFormField('token', 'API Token');
-    });
+    assertOnSubFormFields('Ansible Galaxy/Automation Hub API Token');
   });
+
   it('Create credential using AWS Secrets Manager lookup', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'AWS Secrets Manager lookup');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('aws-access-key', 'AWS Access Key', true);
-      assertOnSubFormField('aws-secret-key', 'AWS Secret Key', true);
-    });
+    assertOnSubFormFields('AWS Secrets Manager lookup');
   });
   it('Create credential using Bitbucket Data Center HTTP Access Token', () => {
     cy.selectSingleSelectOption(
       '[data-cy="credential_type"]',
       'Bitbucket Data Center HTTP Access Token'
     );
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('token', 'Token', true);
-    });
+    assertOnSubFormFields('Bitbucket Data Center HTTP Access Token');
   });
   it('Create credential using Centrify Vault Credential Provider Lookup', () => {
     cy.selectSingleSelectOption(
       '[data-cy="credential_type"]',
       'Centrify Vault Credential Provider Lookup'
     );
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('url', 'Centrify Tenant URL', true);
-      assertOnSubFormField('client-id', 'Centrify API User', true);
-      assertOnSubFormField('client-password', 'Centrify API Password', true);
-      assertOnSubFormField('oauth-application-id', 'OAuth2 Application ID');
-      assertOnSubFormField('oauth-scope', 'OAuth2 Scope');
-    });
+
+    assertOnSubFormFields('Centrify Vault Credential Provider Lookup');
   });
   it('Create credential using Container Registry', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Container Registry');
-
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('host', 'Authentication URL', true);
-      assertOnSubFormField('username', 'Username');
-      assertOnSubFormField('password', 'Password or Token');
-    });
+    assertOnSubFormFields('Container Registry');
   });
   it('Create credential using CyberArk Central Credential Provider Lookup', () => {
     cy.selectSingleSelectOption(
       '[data-cy="credential_type"]',
       'CyberArk Central Credential Provider Lookup'
     );
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('url', 'CyberArk CCP URL', true);
-      assertOnSubFormField('webservice-id', 'Web Service ID');
-      assertOnSubFormField('app-id', 'Application ID', true);
-      assertOnSubFormField('client-key', 'Client Key');
-      assertOnSubFormField('client-cert', 'Client Certificate');
-      cy.getByDataCy('verify').should('be.visible');
-    });
+    assertOnSubFormFields('CyberArk Central Credential Provider Lookup');
   });
   it('Create credential using CyberArk Conjur Secrets Manager Lookup', () => {
     cy.selectSingleSelectOption(
       '[data-cy="credential_type"]',
       'CyberArk Conjur Secrets Manager Lookup'
     );
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('url', 'Conjur URL', true);
-      assertOnSubFormField('api-key', 'API Key', true);
-      assertOnSubFormField('account', 'Account', true);
-      assertOnSubFormField('username', 'Username', true);
-      assertOnSubFormField('cacert', 'Public Key Certificate');
-    });
+
+    assertOnSubFormFields('CyberArk Conjur Secrets Manager Lookup');
   });
   it('Create credential using GitHub Personal Access Token', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'GitHub Personal Access Token');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('token', 'Token', true);
-    });
+    assertOnSubFormFields('GitHub Personal Access Token');
   });
   it('Create credential using GitLab Personal Access Token', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'GitLab Personal Access Token');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('token', 'Token', true);
-    });
+    assertOnSubFormFields('GitLab Personal Access Token');
   });
   it('Create credential using Google Compute Engine', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Google Compute Engine');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('username', 'Service Account Email Address', true);
-      assertOnSubFormField('project', 'Project');
-      assertOnSubFormField('ssh-key-data', 'RSA Private Key ', true);
-      // Missing json file field for this credential type
-    });
+    assertOnSubFormFields('Google Compute Engine');
   });
   it('Create credential using GPG Public Key', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'GPG Public Key');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('gpg-public-key', 'GPG Public Key', true);
-    });
+    assertOnSubFormFields('GPG Public Key');
   });
   it('Create credential using HashiCorp Vault Secret Lookup', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'HashiCorp Vault Secret Lookup');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('url', 'Server URL', true);
-      assertOnSubFormField('token', 'Token');
-      assertOnSubFormField('cacert', 'CA Certificate');
-      assertOnSubFormField('role-id', 'AppRole role_id');
-      assertOnSubFormField('secret-id', 'AppRole secret_id');
-      assertOnSubFormField('client-cert-public', 'Client Certificate');
-      assertOnSubFormField('client-cert-private', 'Client Certificate Key');
-      assertOnSubFormField('client-cert-role', 'TLS Authentication Role');
-      assertOnSubFormField('namespace', 'Namespace name (Vault Enterprise only)');
-      assertOnSubFormField('kubernetes-role', 'Kubernetes role');
-      assertOnSubFormField('username', 'Username');
-      assertOnSubFormField('password', 'Password');
-      assertOnSubFormField('default-auth-path', 'Path to Auth');
-      assertOnSubFormField('api-version', 'API Version', true);
-    });
+    assertOnSubFormFields('HashiCorp Vault Secret Lookup');
   });
   it('Create credential using HashiCorp Vault Signed SSH', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'HashiCorp Vault Signed SSH');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('url', 'Server URL', true);
-      assertOnSubFormField('token', 'Token');
-      assertOnSubFormField('cacert', 'CA Certificate');
-      assertOnSubFormField('role-id', 'AppRole role_id');
-      assertOnSubFormField('secret-id', 'AppRole secret_id');
-      assertOnSubFormField('client-cert-public', 'Client Certificate');
-      assertOnSubFormField('client-cert-private', 'Client Certificate Key');
-      assertOnSubFormField('client-cert-role', 'TLS Authentication Role');
-      assertOnSubFormField('namespace', 'Namespace name (Vault Enterprise only)');
-      assertOnSubFormField('kubernetes-role', 'Kubernetes role');
-      assertOnSubFormField('username', 'Username');
-      assertOnSubFormField('password', 'Password');
-      assertOnSubFormField('default-auth-path', 'Path to Auth');
-    });
+    assertOnSubFormFields('HashiCorp Vault Signed SSH');
   });
   it('Create credential using Insights', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Insights');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('username', 'Username', true);
-      assertOnSubFormField('password', 'Password', true);
-    });
+    assertOnSubFormFields('Insights');
   });
   it('Create credential using Machine', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Machine');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('username', 'Username');
-      assertOnSubFormField('password', 'Password');
-      assertOnSubFormField('ssh-key-data', 'SSH Private Key');
-      assertOnSubFormField('ssh-public-key-data', 'Signed SSH Certificate');
-      assertOnSubFormField('ssh-key-unlock', 'Private Key Passphrase');
-      assertOnSubFormField('become_method', 'Privilege Escalation Method');
-      assertOnSubFormField('become-username', 'Privilege Escalation Username');
-      assertOnSubFormField('become-password', 'Privilege Escalation Password');
-    });
+    assertOnSubFormFields('Machine');
   });
   it('Create credential using Microsoft Azure Key Vault', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Microsoft Azure Key Vault');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('url', 'Vault URL (DNS Name)', true);
-      assertOnSubFormField('client', 'Client ID', true);
-      assertOnSubFormField('secret', 'Client Secret', true);
-      assertOnSubFormField('tenant', 'Tenant ID', true);
-      assertOnSubFormField('cloud-name', 'Cloud Environment');
-    });
+    assertOnSubFormFields('Microsoft Azure Key Vault');
   });
   it('Create credential using Microsoft Azure Resource Manager', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Microsoft Azure Resource Manager');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('subscription', 'Subscription ID', true);
-      assertOnSubFormField('username', 'Username');
-      assertOnSubFormField('password', 'Password');
-      assertOnSubFormField('client', 'Client ID');
-      assertOnSubFormField('secret', 'Client Secret');
-      assertOnSubFormField('tenant', 'Tenant ID');
-      assertOnSubFormField('cloud-environment', 'Azure Cloud Environment');
-    });
+    assertOnSubFormFields('Microsoft Azure Resource Manager');
   });
   it('Create credential using Network', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Network');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('username', 'Username', true);
-      assertOnSubFormField('password', 'Password');
-      assertOnSubFormField('ssh-key-data', 'SSH Private Key');
-      assertOnSubFormField('ssh-key-unlock', 'Private Key Passphrase');
-      assertOnSubFormField('authorize-password', 'Authorize Password');
-      cy.getByDataCy('authorize').should('be.visible');
-    });
+    assertOnSubFormFields('Network');
   });
   it('Create credential using OpenShift or Kubernetes API Bearer Token', () => {
     cy.selectSingleSelectOption(
       '[data-cy="credential_type"]',
       'OpenShift or Kubernetes API Bearer Token'
     );
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('host', 'OpenShift or Kubernetes API Endpoint', true);
-      assertOnSubFormField('bearer-token', 'API authentication bearer token', true);
-      assertOnSubFormField('ssl-ca-cert', 'Certificate Authority data');
-      cy.getByDataCy('verify_ssl').should('be.visible');
-    });
+    assertOnSubFormFields('OpenShift or Kubernetes API Bearer Token');
   });
   it('Create credential using Openstack', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'OpenStack');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('username', 'Username', true);
-      assertOnSubFormField('password', 'Password (API Key)', true);
-      assertOnSubFormField('host', 'Host (Authentication URL)', true);
-      assertOnSubFormField('project', 'Project (Tenant Name)', true);
-      assertOnSubFormField('project-domain-name', 'Project (Domain Name)');
-      assertOnSubFormField('domain', 'Domain Name');
-      assertOnSubFormField('region', 'Region Name');
-      cy.getByDataCy('verify_ssl').should('be.visible');
-    });
+    assertOnSubFormFields('OpenStack');
   });
   it('Create credential using Red Hat Ansible Automation Platform', () => {
     cy.selectSingleSelectOption(
       '[data-cy="credential_type"]',
       'Red Hat Ansible Automation Platform'
     );
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('host', 'Red Hat Ansible Automation Platform', true);
-      assertOnSubFormField('username', 'Username');
-      assertOnSubFormField('password', 'Password');
-      assertOnSubFormField('oauth-token', 'OAuth Token');
-      cy.getByDataCy('verify_ssl').should('be.visible');
-    });
+    assertOnSubFormFields('Red Hat Ansible Automation Platform');
   });
   it('Create credential using Red Hat Satellite 6', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Red Hat Satellite 6');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('host', 'Satellite 6 URL', true);
-      assertOnSubFormField('username', 'Username', true);
-      assertOnSubFormField('password', 'Password', true);
-    });
+    assertOnSubFormFields('Red Hat Satellite 6');
   });
   it('Create credential using Red Hat Virtualization', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Red Hat Virtualization');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('host', 'Host (Authentication URL)', true);
-      assertOnSubFormField('username', 'Username', true);
-      assertOnSubFormField('password', 'Password', true);
-      assertOnSubFormField('ca-file', 'CA File');
-    });
+    assertOnSubFormFields('Red Hat Virtualization');
   });
   it('Create credential using Source Control', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Source Control');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('username', 'Username');
-      assertOnSubFormField('password', 'Password');
-      assertOnSubFormField('ssh-key-data', 'SCM Private Key');
-      assertOnSubFormField('ssh-key-unlock', 'Private Key Passphrase');
-    });
+    assertOnSubFormFields('Source Control');
   });
   it('Create credential using Terraform Backend Configuration', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Terraform backend configuration');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('configuration', 'Backend configuration', true);
-      assertOnSubFormField('gce-credentials', 'Google Cloud Platform account credentials');
-    });
+    assertOnSubFormFields('Terraform backend configuration');
   });
   it('Create credential using Thycotic DevOps Secrets Vault', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Thycotic DevOps Secrets Vault');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('tenant', 'Tenant', true);
-      assertOnSubFormField('client-id', 'Client ID', true);
-      assertOnSubFormField('client-secret', 'Client Secret', true);
-      assertOnSubFormField('tld', 'Top-level Domain (TLD)');
-    });
+    assertOnSubFormFields('Thycotic DevOps Secrets Vault');
   });
   it('Create credential using Thycotic Secret Server', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'Thycotic Secret Server');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('server-url', 'Secret Server URL', true);
-      assertOnSubFormField('username', 'Username', true);
-      assertOnSubFormField('domain', 'Domain');
-      assertOnSubFormField('password', 'Password', true);
-    });
+    assertOnSubFormFields('Thycotic Secret Server');
   });
 
   it('Create credential using Vault', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', /^Vault$/);
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('vault-password', 'Vault Password', true);
-      assertOnSubFormField('vault-id', 'Vault Identifier');
-    });
+    assertOnSubFormFields(/^Vault$/);
   });
   it('Create credential using VMware vCenter', () => {
     cy.selectSingleSelectOption('[data-cy="credential_type"]', 'VMware vCenter');
-    cy.get('section[role="group"]').within(() => {
-      assertOnSubFormField('host', 'Center Host', true);
-      assertOnSubFormField('username', 'Username', true);
-      assertOnSubFormField('password', 'Password', true);
-    });
+    assertOnSubFormFields('VMware vCenter');
   });
 });
