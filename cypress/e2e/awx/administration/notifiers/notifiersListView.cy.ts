07b480988170a3a34f142bf847e46acad583e00c
MilanPospisil <arkanus@seznam.cz>
2024-05-22 10:48:41 +0200

    Fix notifier tests

--- cypress/e2e/awx/administration/notifiers/notifiersListView.cy.ts
+++ cypress/e2e/awx/administration/notifiers/notifiersListView.cy.ts
@@ -1,7 +1,7 @@
 import { NotificationTemplate } from '../../../../../frontend/awx/interfaces/NotificationTemplate';
 import { randomE2Ename } from '../../../../support/utils';
 
-describe.skip('Notifications: List View', () => {
+describe('Notifications: List View', () => {
   let notificationTemplate: NotificationTemplate;
 
   before(() => {
@@ -121,7 +121,7 @@ function testNotification(type: string) {
   const notificationName = randomE2Ename();
   const orgName = randomE2Ename();
   cy.createAwxOrganization(orgName).then(() => {
-    cy.navigateTo('awx', 'notification-templates');
+    cy.getByDataCy(`awx-notification-templates`).click({force : true});
     cy.get(`[data-cy="add-notifier"]`).click();
 
     fillBasicData(notificationName, type);
