bd5cd61f078494ef39a4472c8411b615f9f3b585
MilanPospisil <arkanus@seznam.cz>
2024-05-27 16:56:33 +0200

    Fix id and inventory type

--- frontend/awx/resources/inventories/inventoryHostsPage/hooks/useHostsGroupsColumns.tsx
+++ frontend/awx/resources/inventories/inventoryHostsPage/hooks/useHostsGroupsColumns.tsx
@@ -8,22 +8,21 @@ import { AwxRoute } from '../../../../main/AwxRoutes';
 export function useHostsGroupsColumns(options?: {
   disableSort?: boolean;
   disableLinks?: boolean;
-  inventory_type?: string;
+  useGroupInventory? : boolean;
 }) {
   const pageNavigate = usePageNavigate();
   const params = useParams<{ id: string; inventory_type: string }>();
-  const inventory_type = options?.inventory_type || params.inventory_type;
 
   const nameClick = useCallback(
-    (group: InventoryGroup) =>
+    (group: InventoryGroup) => {
       pageNavigate(AwxRoute.InventoryGroupDetails, {
         params: {
-          id: params.id,
+          id: options?.useGroupInventory === true ? group.summary_fields.inventory.id : params.id,
           group_id: group.id,
-          inventory_type,
+          inventory_type : options?.useGroupInventory === true ? kindToInventoryType(group.summary_fields.inventory.kind) : params.inventory_type,
         },
-      }),
-    [pageNavigate, params.id, inventory_type]
+      })},
+    [pageNavigate, params.id, params.inventory_type]
   );
   const nameColumn = useNameColumn({
     ...options,
@@ -33,4 +32,23 @@ export function useHostsGroupsColumns(options?: {
   const tableColumns = useMemo<ITableColumn<InventoryGroup>[]>(() => [nameColumn], [nameColumn]);
 
   return tableColumns;
+}
+
+function kindToInventoryType(kind : string)
+{
+  let inventory_type = '';
+
+  if (kind === '') {
+    inventory_type = 'inventory';
+  }
+
+  if (kind === 'smart') {
+    inventory_type = 'smart_inventory';
+  }
+
+  if (kind === 'constructed') {
+    inventory_type = 'constructed_inventory';
+  }
+
+  return inventory_type
 }
