5edd1b02d006f63d6b63f242678c02fd5e71b7bf
James Talton <jtalton@redhat.com>
2024-05-29 11:25:57 -0400

    genericErrorAdapter (#2353)
    Signed-off-by: James Talton <talton@gmail.com>

--- framework/PageForm/genericErrorAdapter.ts
+++ framework/PageForm/genericErrorAdapter.ts
@@ -1,31 +1,66 @@
-import { isRequestError } from '../../frontend/common/crud/RequestError';
-import { ErrorOutput, GenericErrorDetail, FieldErrorDetail } from './typesErrorAdapter';
+import { ErrorOutput, FieldErrorDetail, GenericErrorDetail } from './typesErrorAdapter';
 
 export const genericErrorAdapter = (error: unknown): ErrorOutput => {
+  const errorDetailKeys: string[] = ['json'];
+  const genericErrorKeys: string[] = ['non_field_errors'];
+
   const genericErrors: GenericErrorDetail[] = [];
   const fieldErrors: FieldErrorDetail[] = [];
 
-  if (isRequestError(error)) {
-    const typedBody = error.json as Record<string, unknown[]>;
-    for (const key in typedBody) {
-      const messages = typedBody[key];
+  switch (typeof error) {
+    case 'string':
+      genericErrors.push({ message: error });
+      break;
+    case 'object':
+      if (error === null) break;
 
-      // Check for non-field errors
-      if (key === 'non_field_errors' && Array.isArray(messages)) {
-        messages.forEach((message) => {
-          if (typeof message === 'string') {
-            genericErrors.push({ message });
+      if (Array.isArray(error)) {
+        for (const value of error) {
+          if (typeof value === 'string') {
+            genericErrors.push({ message: value });
           }
-        });
-      } else if (Array.isArray(messages)) {
-        const message = messages[0];
-        if (typeof message === 'string') {
-          fieldErrors.push({ name: key, message });
+        }
+        break;
+      }
+
+      for (const errorDetailsKey of errorDetailKeys) {
+        if (
+          errorDetailsKey in error &&
+          typeof (error as Record<string, unknown>)[errorDetailsKey] === 'object' &&
+          (error as Record<string, unknown>)[errorDetailsKey] !== null
+        ) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          for (const name in (error as Record<string, any>)[errorDetailsKey]) {
+            const value = (
+              (error as Record<string, unknown>)[errorDetailsKey] as Record<string, unknown>
+            )[name];
+            if (genericErrorKeys.includes(name)) {
+              if (typeof value === 'string') {
+                genericErrors.push({ message: value });
+              } else if (Array.isArray(value)) {
+                for (const message of value) {
+                  if (typeof message === 'string') {
+                    genericErrors.push({ message });
+                  }
+                }
+              }
+            } else {
+              if (typeof value === 'string') {
+                fieldErrors.push({ name, message: value });
+              } else if (Array.isArray(value)) {
+                for (const message of value) {
+                  if (typeof message === 'string') {
+                    fieldErrors.push({ name, message });
+                  }
+                }
+              }
+            }
+          }
+        } else if (error instanceof Error) {
+          genericErrors.push({ message: error.message });
         }
       }
-    }
-  } else if (error instanceof Error) {
-    genericErrors.push({ message: error.message });
+      break;
   }
 
   return { genericErrors, fieldErrors };
