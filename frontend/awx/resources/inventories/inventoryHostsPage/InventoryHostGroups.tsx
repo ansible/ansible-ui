539d32059f54fd0c05f9caf105e7f23c13d9f06b
MilanPospisil <arkanus@seznam.cz>
2024-05-30 08:49:58 +0200

    Add run command actions (#2391)

--- frontend/awx/resources/inventories/inventoryHostsPage/InventoryHostGroups.tsx
+++ frontend/awx/resources/inventories/inventoryHostsPage/InventoryHostGroups.tsx
@@ -32,7 +32,7 @@ export function InventoryHostGroups(props: { page: string }) {
     tableColumns,
   });
 
-  const toolbarActions = useHostsGroupsToolbarActions(view, inventoryId, hostId, isHostPage);
+  const toolbarActions = useHostsGroupsToolbarActions(view, inventoryId, hostId);
   const rowActions = useHostsGroupsActions(inventoryId);
 
   const openInventoryHostsGroupsAddModal = useInventoryHostGroupsAddModal();
--- frontend/awx/resources/inventories/inventoryHostsPage/hooks/useHostsGroupsToolbarActions.tsx
+++ frontend/awx/resources/inventories/inventoryHostsPage/hooks/useHostsGroupsToolbarActions.tsx
@@ -2,15 +2,9 @@ import { ButtonVariant } from '@patternfly/react-core';
 import { PlusCircleIcon } from '@patternfly/react-icons';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import {
-  IPageAction,
-  PageActionType,
-  PageActionSelection,
-  usePageNavigate,
-} from '../../../../../../framework';
+import { IPageAction, PageActionType, PageActionSelection } from '../../../../../../framework';
 import { useOptions } from '../../../../../common/crud/useOptions';
 import { awxAPI } from '../../../../common/api/awx-utils';
-import { AwxRoute } from '../../../../main/AwxRoutes';
 import { OptionsResponse, ActionsResponse } from '../../../../interfaces/OptionsResponse';
 import { InventoryGroup } from '../../../../interfaces/InventoryGroup';
 import { IAwxView } from '../../../../common/useAwxView';
@@ -21,20 +15,11 @@ import { useAssociateGroupsToHost } from './useAssociateGroupsToHost';
 export function useHostsGroupsToolbarActions(
   view: IAwxView<InventoryGroup>,
   inventoryId: string,
-  hostId: string,
-  hostPage: boolean
+  hostId: string
 ) {
   const { t } = useTranslation();
-  const pageNavigate = usePageNavigate();
 
   const disassociateGroups = useDisassociateGroups(view.unselectItemsAndRefresh, hostId);
-
-  const adhocOptions = useOptions<OptionsResponse<ActionsResponse>>(
-    awxAPI`/inventories/${inventoryId ?? ''}/ad_hoc_commands/`
-  ).data;
-  const canRunAdHocCommand = Boolean(
-    adhocOptions && adhocOptions.actions && adhocOptions.actions['POST']
-  );
 
   const groupOptions = useOptions<OptionsResponse<ActionsResponse>>(awxAPI`/groups/`).data;
   const canCreateGroup = Boolean(
@@ -66,21 +51,6 @@ export function useHostsGroupsToolbarActions(
                 'You do not have permission to create a host. Please contact your organization administrator if there is an issue with your access.'
               ),
       },
-      {
-        type: PageActionType.Button,
-        selection: PageActionSelection.None,
-        variant: ButtonVariant.secondary,
-        isHidden: () => hostPage,
-        isPinned: true,
-        label: t('Run Command'),
-        onClick: () => pageNavigate(AwxRoute.Inventories),
-        isDisabled: () =>
-          canRunAdHocCommand
-            ? undefined
-            : t(
-                'You do not have permission to run an ad hoc command. Please contact your organization administrator if there is an issue with your access.'
-              ),
-      },
       { type: PageActionType.Seperator },
       {
         type: PageActionType.Button,
@@ -99,10 +69,7 @@ export function useHostsGroupsToolbarActions(
       openInventoryHostsGroupsAddModal,
       associateGroups,
       canCreateGroup,
-      pageNavigate,
-      canRunAdHocCommand,
       hostId,
-      hostPage,
       inventoryId,
     ]
   );
