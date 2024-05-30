c209f3ce3554a2cd80a79f124666d783150369a6
MilanPospisil <arkanus@seznam.cz>
2024-05-30 11:49:00 +0200

    Prettier

--- frontend/awx/resources/inventories/inventoryHostsPage/hooks/useHostsGroupsToolbarActions.tsx
+++ frontend/awx/resources/inventories/inventoryHostsPage/hooks/useHostsGroupsToolbarActions.tsx
@@ -18,7 +18,7 @@ export function useHostsGroupsToolbarActions(
   view: IAwxView<InventoryGroup>,
   inventoryId: string,
   hostId: string,
-  type : 'standaloneHost' | 'inventoryHost',
+  type: 'standaloneHost' | 'inventoryHost'
 ) {
   const { t } = useTranslation();
 
@@ -35,64 +35,59 @@ export function useHostsGroupsToolbarActions(
   const params = useParams<{ id: string; inventory_type: string }>();
   const runCommandAction = useRunCommandAction<InventoryGroup>(params);
 
-  return useMemo<IPageAction<InventoryGroup>[]>(
-    () => {
-      
-      let arr : IPageAction<InventoryGroup>[] = [];
+  return useMemo<IPageAction<InventoryGroup>[]>(() => {
+    const arr: IPageAction<InventoryGroup>[] = [];
 
-      arr.push({
-        type: PageActionType.Button,
-        selection: PageActionSelection.None,
-        variant: ButtonVariant.primary,
-        isPinned: true,
-        icon: PlusCircleIcon,
-        label: t('Associate'),
-        onClick: () =>
-          openInventoryHostsGroupsAddModal({
-            onAdd: associateGroups,
-            inventoryId: inventoryId ?? '',
-            hostId: hostId ?? '',
-          }),
-        isDisabled: () =>
-          canCreateGroup
-            ? undefined
-            : t(
-                'You do not have permission to create a host. Please contact your organization administrator if there is an issue with your access.'
-              ),
-      });
+    arr.push({
+      type: PageActionType.Button,
+      selection: PageActionSelection.None,
+      variant: ButtonVariant.primary,
+      isPinned: true,
+      icon: PlusCircleIcon,
+      label: t('Associate'),
+      onClick: () =>
+        openInventoryHostsGroupsAddModal({
+          onAdd: associateGroups,
+          inventoryId: inventoryId ?? '',
+          hostId: hostId ?? '',
+        }),
+      isDisabled: () =>
+        canCreateGroup
+          ? undefined
+          : t(
+              'You do not have permission to create a host. Please contact your organization administrator if there is an issue with your access.'
+            ),
+    });
 
-      arr.push({ type: PageActionType.Seperator });
-    
-      if (type === 'inventoryHost')
-      {
-        arr.push(runCommandAction);
-      }
+    arr.push({ type: PageActionType.Seperator });
 
-      arr.push({ type: PageActionType.Seperator });
+    if (type === 'inventoryHost') {
+      arr.push(runCommandAction);
+    }
 
-      arr.push({
-        type: PageActionType.Button,
-        selection: PageActionSelection.Multiple,
-        label: t('Disassociate'),
-        isDisabled:
-          view.selectedItems.length === 0 ? t('Select at least one item from the list') : undefined,
-        onClick: disassociateGroups,
-        isPinned: true,
-      });
+    arr.push({ type: PageActionType.Seperator });
 
-      return arr;
-    },
-    [
-      t,
-      view.selectedItems.length,
-      disassociateGroups,
-      openInventoryHostsGroupsAddModal,
-      associateGroups,
-      canCreateGroup,
-      hostId,
-      inventoryId,
-      runCommandAction,
-      type
-    ]
-  );
+    arr.push({
+      type: PageActionType.Button,
+      selection: PageActionSelection.Multiple,
+      label: t('Disassociate'),
+      isDisabled:
+        view.selectedItems.length === 0 ? t('Select at least one item from the list') : undefined,
+      onClick: disassociateGroups,
+      isPinned: true,
+    });
+
+    return arr;
+  }, [
+    t,
+    view.selectedItems.length,
+    disassociateGroups,
+    openInventoryHostsGroupsAddModal,
+    associateGroups,
+    canCreateGroup,
+    hostId,
+    inventoryId,
+    runCommandAction,
+    type,
+  ]);
 }
