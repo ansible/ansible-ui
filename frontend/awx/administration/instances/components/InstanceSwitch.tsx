042e8e738dd1eba0b5baacc7a3b15fcce6e6c886
Pratyush Bhandari <prbhanda@redhat.com>
2024-06-03 15:40:27 -0400

    Use `PageActionSwitch` over regular pf `Switch`

--- frontend/awx/administration/instances/components/InstanceSwitch.tsx
+++ frontend/awx/administration/instances/components/InstanceSwitch.tsx
@@ -1,24 +1,34 @@
-import { Switch } from '@patternfly/react-core';
-import { t } from 'i18next';
+import { PageActionSwitch } from '../../../../../framework/PageActions/PageActionSwitch';
 import { useAwxActiveUser } from '../../../common/useAwxActiveUser';
 import { Instance } from '../../../interfaces/Instance';
 import { useInstanceActions } from '../hooks/useInstanceActions';
+import {
+  IPageActionSwitchSingle,
+  PageActionSelection,
+  PageActionType,
+} from '../../../../../framework';
+import { useTranslation } from 'react-i18next';
 
 export function InstanceSwitch(props: { instance: Instance }) {
+  const { t } = useTranslation();
   const { instance } = props;
   const { handleToggleInstance } = useInstanceActions(String(props.instance.id));
   const { activeAwxUser } = useAwxActiveUser();
 
-  return (
-    <Switch
-      id="enable-instance"
-      isDisabled={!activeAwxUser?.is_superuser}
-      label={t('Enabled')}
-      labelOff={t('Disabled')}
-      isChecked={instance?.enabled}
-      onChange={() => {
-        void handleToggleInstance(!instance?.enabled);
-      }}
-    />
-  );
+  const action: IPageActionSwitchSingle<Instance> = {
+    type: PageActionType.Switch,
+    onToggle: () => {
+      void handleToggleInstance(!instance?.enabled);
+    },
+    isSwitchOn: () => instance?.enabled,
+    isDisabled: (_instance) =>
+      !activeAwxUser?.is_superuser
+        ? t('You do not have permissions to toggle this instance')
+        : undefined,
+    ariaLabel: (isEnabled: boolean) => (isEnabled ? t('Enabled') : t('Disabled')),
+    selection: PageActionSelection.Single,
+    label: '',
+  };
+
+  return <PageActionSwitch action={action} selectedItem={instance} />;
 }
