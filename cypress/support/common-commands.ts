724992f3794c2be8ed5d802e6f235cd1b684de96
MilanPospisil <arkanus@seznam.cz>
2024-06-05 17:08:50 +0200

    Watch dog for poll command to prevent infinite loop 100 times

--- cypress/support/common-commands.ts
+++ cypress/support/common-commands.ts
@@ -196,9 +196,9 @@ Cypress.Commands.add('poll', function requestPoll<
     if (response !== undefined && check(response)) {
       cy.wrap(response);
     } else {
-      // call only 10 times, then fail
+      // call only 100 times, then fail
       const watchDog = (calledTimes || 0) + 1;
-      if (watchDog <= 10) {
+      if (watchDog <= 100) {
         cy.log('Calling cy.poll again for the ' + watchDog + ' time');
         cy.wait(1000).then(() => cy.poll<ResponseT>(fn, check, watchDog));
       } else {
