291043589255fd508d34fe445bbe5e131a6cbeca
MilanPospisil <arkanus@seznam.cz>
2024-06-05 12:45:44 +0200

    Uncomment

--- cypress/e2e/awx/administration/wfApprovals.cy.ts
+++ cypress/e2e/awx/administration/wfApprovals.cy.ts
@@ -109,7 +109,7 @@ describe('Workflow Approvals Tests', () => {
       .its('response.body')
       .then((response: WorkflowJob) => {
         expect(response.id).to.exist;
-        /*cy.pollFirstPendingWorkflowApprovalsForWorkflowJobID(response.id).then((approval) => {
+        cy.pollFirstPendingWorkflowApprovalsForWorkflowJobID(response.id).then((approval) => {
           cy.navigateTo('awx', 'workflow-approvals');
           cy.intercept('POST', awxAPI`/workflow_approvals/${approval.id.toString()}/approve/`).as(
             'WFaction'
@@ -143,7 +143,7 @@ describe('Workflow Approvals Tests', () => {
             .then((response) => {
               expect(response?.statusCode).to.eql(204);
             });
-        });*/
+        });
       });
   });
 
