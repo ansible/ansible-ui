8911984c2e4da94e40186999283b7490039f9cec
Ingrid Sena <10999758+fullsushidev@users.noreply.github.com>
2024-06-07 03:08:08 +0200

    Fix empty arrays

--- cypress/e2e/awx/resources/inventoriesConstructed.cy.ts
+++ cypress/e2e/awx/resources/inventoriesConstructed.cy.ts
@@ -6,10 +6,12 @@ import { awxAPI } from '../../../support/formatApiPathForAwx';
 
 describe('Constructed Inventories CRUD Tests', () => {
   let organization: Organization;
-  const arrayOfInventories = <Inventory[]>[];
-  const invNames = <string[]>[];
+  let inventoriesList: Inventory[] = [];
+  let invNames: string[] = [];
   let constructedInv: Inventory;
   let instanceGroup: InstanceGroup;
+  const invToDelete: Inventory[] = [];
+  const constrInvToDelete: Inventory[] = [];
 
   before(() => {
     cy.awxLogin();
@@ -24,40 +26,39 @@ describe('Constructed Inventories CRUD Tests', () => {
   });
 
   beforeEach(() => {
+    inventoriesList = [];
+    invNames = [];
     // creates 3 inventories
     for (let i = 0; i < 3; i++) {
       cy.createAwxInventory({ organization: organization.id }).then((inv) => {
-        arrayOfInventories.push(inv);
+        inventoriesList.push(inv);
       });
     }
-    arrayOfInventories.forEach((inv) => {
-      invNames.push(inv.name);
-    });
     cy.createAwxConstructedInventory(organization).then((constInv) => {
       constructedInv = constInv;
     });
   });
 
   afterEach(() => {
-    arrayOfInventories.forEach((inv) => {
-      cy.deleteAwxInventory(inv);
-    });
-    cy.deleteAwxConstructedInventory(constructedInv);
+    constrInvToDelete.push(constructedInv);
+    invToDelete.push(...inventoriesList);
   });
 
   after(() => {
+    constrInvToDelete.map((constrInventory) => cy.deleteAwxConstructedInventory(constrInventory));
+    invToDelete.map((inventory) => cy.deleteAwxInventory(inventory, { failOnStatusCode: false }));
     cy.deleteAwxInstanceGroup(instanceGroup);
     cy.deleteAwxOrganization(organization);
   });
 
   it('can create a constructed inventory using specific source_vars and limit and then delete that inventory', () => {
+    invNames = inventoriesList.map(({ name }) => String(name));
+    cy.intercept('POST', awxAPI`/constructed_inventories/`).as('createInv');
     const constInvName = 'E2E Constructed Inventory ' + randomString(4);
     // generates random values to be used during the test.
     const cacheTimeoutValue = generateRandom(0, 15);
     // which combination should be used to test verbosity from 1 to 5 - UI forces to use only 0-2
     const verbosityValue = generateRandom(0, 2);
-
-    cy.intercept('POST', awxAPI`/constructed_inventories/`).as('createInv');
 
     cy.navigateTo('awx', 'inventories');
     cy.getByDataCy('create-inventory').click();
@@ -134,13 +135,13 @@ describe('Constructed Inventories CRUD Tests', () => {
     // cy.waitForJobToProcessEvents(jobID.toString());
   });
 
-  it('can edit the input_inventories, verify the preservation of the order they were added in, and manually change the order', () => {
+  it.skip('can edit the input_inventories, verify the preservation of the order they were added in, and manually change the order', () => {
     //Create a constructed inventory in the beforeEach hook
     //Assert the original order of the input inventories
     //Assert the UI change to the order of input inventories
   });
 
-  it('shows a failed sync on the constructed inventory if the user sets strict to true and enters bad variables', () => {
+  it.skip('shows a failed sync on the constructed inventory if the user sets strict to true and enters bad variables', () => {
     //Create a constructed inventory in the beforeEach hook
     //Assert the original details of the inventory
     //Assert the user navigating to the edit constructed inventory form
