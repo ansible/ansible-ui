8976b862da3cc5584dec56be9db40ea4c60c316a
jerabekjiri <jerabekjirka@email.cz>
2024-06-07 21:38:10 +0200

    move to component tests
    Issue: AAP-23373

--- cypress/e2e/awx/administration/topology-view.cy.ts
+++ cypress/e2e/awx/administration/topology-view.cy.ts
@@ -1,10 +1,10 @@
 import { InstanceGroup } from '../../../../frontend/awx/interfaces/InstanceGroup';
 import { MeshVisualizer } from '../../../../frontend/awx/interfaces/MeshVisualizer';
-import { Instance } from '../../../../frontend/awx/interfaces/generated-from-swagger/api';
-import { awxAPI } from '../../../support/formatApiPathForAwx';
 import { AwxUser } from '../../../../frontend/awx/interfaces/User';
 import { Organization } from '../../../../frontend/awx/interfaces/Organization';
+import { Settings } from '../../../../frontend/awx/interfaces/Settings';
 import { randomString } from '../../../../framework/utils/random-string';
+import { awxAPI } from '../../../support/formatApiPathForAwx';
 
 describe('Topology view', () => {
   let user: AwxUser;
@@ -15,13 +15,10 @@ describe('Topology view', () => {
 
   beforeEach(() => {
     cy.intercept({ method: 'GET', url: awxAPI`/mesh_visualizer/` }).as('getMeshVisualizer');
-    cy.intercept({ method: 'GET', url: awxAPI`/instances/*/instance_groups/` }).as(
-      'getInstanceGroups'
-    );
     cy.intercept(
-      { method: 'PATCH', url: `api/v2/instances/1/` },
-      { fixture: 'instance_without_install_bundle.json' }
-    ).as('editInstance');
+      { method: 'GET', url: '/api/v2/instances/*/instance_groups/' },
+      { fixture: 'instance_groups.json' }
+    ).as('getInstanceGroups');
   });
 
   after(() => {
@@ -40,29 +37,6 @@ describe('Topology view', () => {
     cy.verifyPageTitle('Topology View');
   });
 
-  it('show sidebar when node is clicked', () => {
-    cy.navigateTo('awx', 'topology-view');
-    cy.wait('@getMeshVisualizer')
-      .its('response.body')
-      .then((data: MeshVisualizer) => {
-        cy.get(`[data-id="${data.nodes[0].id}"]`).click();
-        cy.get('[data-cy="mesh-viz-sidebar"]').should('be.visible');
-        cy.get('button[aria-label="Close"]').click();
-      });
-  });
-
-  it('navigate to instance detail when instance name is clicked from sidebar', () => {
-    cy.navigateTo('awx', 'topology-view');
-    cy.wait('@getMeshVisualizer')
-      .its('response.body')
-      .then((data: MeshVisualizer) => {
-        cy.get(`[data-id="${data.nodes[0].id}"]`).click();
-        cy.get('[data-cy="mesh-viz-sidebar"]').should('be.visible');
-        cy.get('[data-cy="name"] button').click();
-        cy.verifyPageTitle(`${data.nodes[0].hostname}`);
-      });
-  });
-
   it('navigate to instance group detail when instance group is clicked from sidebar', () => {
     cy.navigateTo('awx', 'topology-view');
     cy.wait('@getMeshVisualizer')
@@ -74,93 +48,9 @@ describe('Topology view', () => {
           .its('response.body.results')
           .then((instanceGroups: InstanceGroup[]) => {
             cy.clickLink(`${instanceGroups[0].name}`);
-            // TODO Instance groups detail page not yet implemented; check URL for now
             cy.url().should('contain', `/instance-groups/${instanceGroups[0].id}`);
+            cy.verifyPageTitle(instanceGroups[0].name);
           });
-      });
-  });
-
-  it('should show adjust instance forks when instance forks slider is adjusted', () => {
-    // Adjusting instance capacity while other concurrent tests are running could have unwanted
-    // side-effects affecting job runs. We should mock the response data in this scenario.
-    cy.intercept(
-      { method: 'GET', url: awxAPI`/instances/1/` },
-      { fixture: 'instance_without_install_bundle.json' }
-    ).as('getInstance');
-    cy.navigateTo('awx', 'topology-view');
-    cy.wait('@getMeshVisualizer')
-      .its('response.body')
-      .then((data: MeshVisualizer) => {
-        cy.get(`[data-id="${data.nodes[0].id}"]`).click();
-        cy.get('[data-cy="mesh-viz-sidebar"]').should('be.visible');
-        cy.wait('@getInstance')
-          .its('response.body')
-          .then((instance: Instance) => {
-            if (instance.enabled) {
-              cy.get('.pf-v5-c-slider__thumb').click({ multiple: true, force: true });
-              cy.get('.pf-v5-c-slider__thumb').type('{rightarrow}');
-              cy.wait('@editInstance')
-                .its('response')
-                .then((res) => {
-                  expect(res?.statusCode).to.eql(200);
-                });
-            }
-          });
-        cy.get('button[aria-label="Close"]').click();
-      });
-  });
-
-  it('should show no forks slider when instance has 0 cpu and memory capacity', () => {
-    // Adjusting instance capacity while other concurrent tests are running could have
-    // unwanted side-effects affecting job runs. We should mock the response data in this scenario.
-    cy.intercept(
-      { method: 'GET', url: awxAPI`/instances/1/` },
-      { fixture: 'instance_hop.json' }
-    ).as('getInstance');
-    cy.navigateTo('awx', 'topology-view');
-    cy.wait('@getMeshVisualizer')
-      .its('response.body')
-      .then((data: MeshVisualizer) => {
-        cy.get(`[data-id="${data.nodes[0].id}"]`).click();
-        cy.get('[data-cy="mesh-viz-sidebar"]').should('be.visible');
-        cy.wait('@getInstance')
-          .its('response.body')
-          .then((instance: Instance) => {
-            if (instance.enabled) {
-              cy.get('.pf-v5-c-slider__thumb').should('not.exist');
-            }
-          });
-        cy.get('button[aria-label="Close"]').click();
-      });
-  });
-
-  it('should render download icon for instance that has an install bundle', () => {
-    /**
-     Only certain node types have install bundles, however creating an instance via the
-     API requires that a user also manually download the respective install bundle and
-     SSH into the machine in order to run a few scripts to register the new instance(s)
-     and update the mesh.
-     See https://docs.ansible.com/automation-controller/4.4/html/administration/instances.html#add-an-instance.
-     In our current automated test environment this would be complicated to replicate so
-     a fixture with the requisite field is provided instead. This also means we can skip
-     the resources clean up step.
-    */
-    cy.intercept({ method: 'GET', url: awxAPI`/instances/1/` }, { fixture: 'instance.json' }).as(
-      'getInstanceWithInstallBundle'
-    );
-    cy.navigateTo('awx', 'topology-view');
-    cy.wait('@getMeshVisualizer')
-      .its('response.body')
-      .then((data: MeshVisualizer) => {
-        cy.get(`[data-id="${data.nodes[0].id}"]`).click();
-        cy.get('[data-cy="mesh-viz-sidebar"]').should('be.visible');
-        cy.get('[data-cy="download-bundle"]').should('be.visible');
-        // testing and verifying downloaded file contents requires extensive setup to work
-        // locally as well as in an automated test environment; it should be sufficient to
-        // assert that the download icon is a proper download link
-        cy.get('[data-cy="download-bundle"] a').should('have.attr', 'download');
-        cy.get('[data-cy="download-bundle"] a').should('have.attr', 'href');
-        cy.get('button[aria-label="Close"]').click();
       });
   });
 
@@ -194,25 +84,31 @@ describe('Topology view', () => {
   });
 
   it('will allow the user to select node and delete it', () => {
-    cy.awxLogin();
-    const node = 'E2EInstance' + randomString(4);
+    cy.awxRequestGet<Settings>(awxAPI`/settings/system/`).then((data) => {
+      if (!data?.IS_K8S) {
+        cy.log('TEST SKIPPED | IS_K8S: False');
+        return;
+      }
+      cy.awxLogin();
+      const node = 'E2EInstance' + randomString(4);
 
-    cy.createAwxInstance(node).then(() => {
-      cy.navigateTo('awx', 'topology-view');
-      cy.contains(node).click({ force: true });
-      cy.getByDataCy('mesh-viz-sidebar').within(() => {
-        cy.getByDataCy('name').contains(node).click();
+      cy.createAwxInstance(node).then(() => {
+        cy.navigateTo('awx', 'topology-view');
+        cy.contains(node).click({ force: true });
+        cy.getByDataCy('mesh-viz-sidebar').within(() => {
+          cy.getByDataCy('name').contains(node).click();
+        });
+
+        cy.url().should('include', '/infrastructure/instances/');
+        cy.getByDataCy('page-title').contains(node);
+
+        cy.clickKebabAction('actions-dropdown', 'remove-instance');
+        cy.clickModalConfirmCheckbox();
+        cy.clickButton('Remove instance');
+
+        cy.navigateTo('awx', 'topology-view');
+        cy.contains(node).should('not.exist');
       });
-
-      cy.url().should('include', '/infrastructure/instances/');
-      cy.getByDataCy('page-title').contains(node);
-
-      cy.clickKebabAction('actions-dropdown', 'remove-instance');
-      cy.clickModalConfirmCheckbox();
-      cy.clickButton('Remove instance');
-
-      cy.navigateTo('awx', 'topology-view');
-      cy.contains(node).should('not.exist');
     });
   });
 });
