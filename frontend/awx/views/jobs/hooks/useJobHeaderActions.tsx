f823c55003af00274058c183c99746ae3aced8ff
Alex Corey <acorey@redhat.com>
2024-06-11 19:52:02 -0400

    [AAP-24174] Adds relaunch with passwords (#2510)
    Co-authored-by: Marliana Lara <mlara@redhat.com>

--- frontend/awx/views/jobs/hooks/useJobHeaderActions.tsx
+++ frontend/awx/views/jobs/hooks/useJobHeaderActions.tsx
@@ -1,5 +1,5 @@
 import { ButtonVariant } from '@patternfly/react-core';
-import { DownloadIcon, MinusCircleIcon, RocketIcon, TrashIcon } from '@patternfly/react-icons';
+import { DownloadIcon, MinusCircleIcon, TrashIcon } from '@patternfly/react-icons';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { IPageAction, PageActionSelection, PageActionType } from '../../../../../framework';
@@ -7,9 +7,9 @@ import { UnifiedJob } from '../../../interfaces/UnifiedJob';
 import { isJobRunning } from '../jobUtils';
 import { useCancelJobs } from './useCancelJobs';
 import { useDeleteJobs } from './useDeleteJobs';
-import { useRelaunchJob } from './useRelaunchJob';
 import { TFunction } from 'i18next';
 import { useDownloadJobOutput } from './useDownloadJobOutput';
+import { useRelaunchOptions } from './useRelaunchOptions';
 
 export const cannotCancelJob = (job: UnifiedJob, t: TFunction<'translation', undefined>) => {
   if (!isJobRunning(job.status)) return t(`The job cannot be canceled because it is not running`);
@@ -23,8 +23,7 @@ export function useJobHeaderActions(onComplete: (jobs: UnifiedJob[]) => void) {
   const cancelJobs = useCancelJobs(onComplete);
   const deleteJobs = useDeleteJobs(onComplete);
   const downloadJobOutput = useDownloadJobOutput();
-  const relaunchJob = useRelaunchJob();
-
+  const relaunchOptions = useRelaunchOptions();
   return useMemo<IPageAction<UnifiedJob>[]>(() => {
     const cannotDeleteJob = (job: UnifiedJob) => {
       if (!job.summary_fields.user_capabilities.delete)
@@ -35,18 +34,7 @@ export function useJobHeaderActions(onComplete: (jobs: UnifiedJob[]) => void) {
     };
 
     const actions: IPageAction<UnifiedJob>[] = [
-      {
-        type: PageActionType.Button,
-        selection: PageActionSelection.Single,
-        variant: ButtonVariant.secondary,
-        isPinned: true,
-        icon: RocketIcon,
-        label: t(`Relaunch job`),
-        isHidden: (job: UnifiedJob) =>
-          !(job.type !== 'system_job' && job.summary_fields?.user_capabilities?.start) ||
-          (job.status === 'failed' && job.type === 'job'),
-        onClick: (job: UnifiedJob) => void relaunchJob(job),
-      },
+      ...relaunchOptions,
       {
         type: PageActionType.Button,
         selection: PageActionSelection.Single,
@@ -83,5 +71,5 @@ export function useJobHeaderActions(onComplete: (jobs: UnifiedJob[]) => void) {
       },
     ];
     return actions;
-  }, [cancelJobs, deleteJobs, relaunchJob, downloadJobOutput, t]);
+  }, [cancelJobs, deleteJobs, relaunchOptions, downloadJobOutput, t]);
 }
