db22a1af4bda6a5a855555ce822f5dc29d37660d
kialam <digitalanime@gmail.com>
2024-06-11 08:21:51 -0700

    Add GCE Upload component to CredentialsForm. (#2486)

--- framework/PageForm/Inputs/PageFormFileUpload.tsx
+++ framework/PageForm/Inputs/PageFormFileUpload.tsx
@@ -1,5 +1,5 @@
 import { FileUpload, FileUploadProps } from '@patternfly/react-core';
-import { useCallback, useState } from 'react';
+import { ReactNode, useCallback, useState } from 'react';
 import { Controller, FieldPathByValue, FieldValues, useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useID } from '../../hooks/useID';
@@ -13,6 +13,8 @@ export type PageFormFileUploadProps<
   name: TFieldName;
   placeholder?: string;
   validate?: (value: File) => string | undefined;
+  onInputChange?: (file: File) => Promise<void>;
+  additionalHelperText?: ReactNode;
 } & PageFormGroupProps &
   Omit<FileUploadProps, 'id'>;
 
@@ -21,8 +23,16 @@ export function PageFormFileUpload<
   TFieldValues extends FieldValues = FieldValues,
   TFieldName extends FieldPathByValue<TFieldValues, File> = FieldPathByValue<TFieldValues, File>,
 >(props: PageFormFileUploadProps<TFieldValues, TFieldName>) {
-  const { label, labelHelpTitle, labelHelp, additionalControls, helperText, isRequired, validate } =
-    props;
+  const {
+    label,
+    labelHelpTitle,
+    labelHelp,
+    additionalControls,
+    helperText,
+    isRequired,
+    validate,
+    onInputChange,
+  } = props;
   const { t } = useTranslation();
   const {
     control,
@@ -30,8 +40,20 @@ export function PageFormFileUpload<
   } = useFormContext<TFieldValues>();
   const [filename, setFilename] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [inputError, setInputError] = useState<Error | undefined>(undefined);
 
-  const handleFileInputChange = useCallback((_: unknown, file: File) => setFilename(file.name), []);
+  const handleFileInputChange = useCallback(
+    (_: unknown, file: File) => {
+      setFilename(file.name);
+      if (onInputChange) {
+        onInputChange(file).catch((error: Error) => {
+          setInputError(error);
+        });
+      }
+    },
+    [onInputChange]
+  );
+
   const handleFileReadStarted = (_fileHandle: File) => {
     setIsLoading(true);
   };
@@ -48,13 +70,16 @@ export function PageFormFileUpload<
           ? validate && isValidating
             ? t('Validating...')
             : error?.message
-          : undefined;
-        const handleClear = props.onClearClick
-          ? props.onClearClick
-          : (_event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-              setFilename('');
-              onChange(undefined);
-            };
+          : inputError?.message
+            ? inputError.message
+            : undefined;
+        const handleClear = (_event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+          setFilename('');
+          onChange(undefined);
+          if (props.onClearClick) {
+            props.onClearClick(_event);
+          }
+        };
         const handleFileReadFinished = (_fileHandle: File) => {
           setIsLoading(false);
           onChange(_fileHandle);
@@ -78,6 +103,37 @@ export function PageFormFileUpload<
             {props.icon && props.icon !== undefined ? (
               <div style={{ display: 'grid', gridTemplateColumns: '10fr 1fr' }}>
                 <FileUpload
+                  dropzoneProps={props.dropzoneProps}
+                  id={id}
+                  data-cy={id}
+                  type={props.type || 'dataURL'}
+                  value={value as string}
+                  hideDefaultPreview={props.hideDefaultPreview}
+                  filename={isLoading ? t('loading...') : filename}
+                  filenamePlaceholder={props.placeholder}
+                  onFileInputChange={handleFileInputChange}
+                  onDataChange={(_event, value: string) => handleTextOrDataChange(value)}
+                  onTextChange={(_event, value: string) => handleTextOrDataChange(value)}
+                  onReadStarted={(_event, _fileHandle: File) => handleFileReadStarted(_fileHandle)}
+                  onReadFinished={(_event, _fileHandle: File) =>
+                    handleFileReadFinished(_fileHandle)
+                  }
+                  onClearClick={handleClear}
+                  // isLoading={isLoading}
+                  allowEditingUploadedText={props.allowEditingUploadedText || false}
+                  // browseButtonText={t('Upload')}
+                  isReadOnly={props.isReadOnly || isSubmitting}
+                  validated={error || inputError ? 'error' : undefined}
+                  isDisabled={props.isDisabled}
+                  isClearButtonDisabled={props.isClearButtonDisabled}
+                />
+                {props.icon}
+                {props.additionalHelperText ? props.additionalHelperText : null}
+              </div>
+            ) : (
+              <>
+                <FileUpload
+                  dropzoneProps={props.dropzoneProps}
                   id={id}
                   data-cy={id}
                   type={props.type || 'dataURL'}
@@ -98,32 +154,9 @@ export function PageFormFileUpload<
                   // browseButtonText={t('Upload')}
                   isReadOnly={props.isReadOnly || isSubmitting}
                   validated={error ? 'error' : undefined}
-                  isDisabled={props.isDisabled}
-                  isClearButtonDisabled={props.isClearButtonDisabled}
                 />
-                {props.icon}
-              </div>
-            ) : (
-              <FileUpload
-                id={id}
-                data-cy={id}
-                type={props.type || 'dataURL'}
-                value={value as string}
-                hideDefaultPreview={props.hideDefaultPreview}
-                filename={isLoading ? t('loading...') : filename}
-                filenamePlaceholder={props.placeholder}
-                onFileInputChange={handleFileInputChange}
-                onDataChange={(_event, value: string) => handleTextOrDataChange(value)}
-                onTextChange={(_event, value: string) => handleTextOrDataChange(value)}
-                onReadStarted={(_event, _fileHandle: File) => handleFileReadStarted(_fileHandle)}
-                onReadFinished={(_event, _fileHandle: File) => handleFileReadFinished(_fileHandle)}
-                onClearClick={handleClear}
-                // isLoading={isLoading}
-                allowEditingUploadedText={props.allowEditingUploadedText || false}
-                // browseButtonText={t('Upload')}
-                isReadOnly={props.isReadOnly || isSubmitting}
-                validated={error ? 'error' : undefined}
-              />
+                {props.additionalHelperText ? props.additionalHelperText : null}
+              </>
             )}
           </PageFormGroup>
         );
