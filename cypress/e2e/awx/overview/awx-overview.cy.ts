f1dd9f0caf285407a2e343c27ec13143ceb09ff6
Marliana Lara <mlara@redhat.com>
2024-06-13 16:11:47 -0400

    Unskip awx overview tests

--- cypress/e2e/awx/overview/awx-overview.cy.ts
+++ cypress/e2e/awx/overview/awx-overview.cy.ts
@@ -3,8 +3,8 @@ import { Inventory } from '../../../../frontend/awx/interfaces/Inventory';
 import { Job } from '../../../../frontend/awx/interfaces/Job';
 import { Project } from '../../../../frontend/awx/interfaces/Project';
 import { awxAPI } from '../../../support/formatApiPathForAwx';
-// FLAKY_06_13_2024
-describe.skip('AWX Overview', () => {
+
+describe('AWX Overview', () => {
   before(() => {
     cy.awxLogin();
   });
