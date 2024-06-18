e52e6260745caa5908829f3c6f85740c369e9d7c
abakshi <abakshi@redhat.com>
2024-06-18 12:32:01 -0500

    fix tests

--- cypress/e2e/eda/Users/users-list.cy.ts
+++ cypress/e2e/eda/Users/users-list.cy.ts
@@ -17,8 +17,7 @@ describe('EDA Users List', () => {
       is_superuser: true,
     }).then((edaUser) => {
       cy.navigateTo('eda', 'users');
-      cy.setTablePageSize('100');
-      cy.clickTableRow(edaUser.username, false);
+      cy.clickTableRow(edaUser.username, true);
       cy.contains(edaUser.username).click();
       cy.verifyPageTitle(edaUser.username);
       cy.clickLink(/^Details$/);
@@ -34,9 +33,10 @@ describe('EDA Users List', () => {
         is_superuser: true,
       }).then((userB) => {
         cy.navigateTo('eda', 'users');
-        cy.selectTableRow(userA.username, false);
-        cy.selectTableRow(userB.username, false);
-        cy.clickToolbarKebabAction('delete-selected-users');
+        cy.selectTableRow(userA.username, true);
+        cy.clearAllFilters();
+        cy.selectTableRow(userB.username, true);
+        cy.clickToolbarKebabAction('delete-users');
         cy.intercept('DELETE', edaAPI`/users/${userA.id.toString()}/`).as('userA');
         cy.intercept('DELETE', edaAPI`/users/${userB.id.toString()}/`).as('userB');
         cy.clickModalConfirmCheckbox();
@@ -54,8 +54,8 @@ describe('EDA Users List', () => {
   it('deletes a User from kebab menu from the project details page', () => {
     cy.createEdaUser().then((edaUser) => {
       cy.navigateTo('eda', 'users');
-      cy.selectTableRow(edaUser.username, false);
-      cy.clickToolbarKebabAction('delete-selected-users');
+      cy.selectTableRow(edaUser.username, true);
+      cy.clickToolbarKebabAction('delete-users');
       cy.get('#confirm').click();
       cy.clickButton(/^Delete user/);
       cy.contains(/^Success$/);
