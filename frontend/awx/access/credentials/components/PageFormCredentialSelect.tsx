95967fdf30841cfaf6e2ade864b3958991e9ab77
Pratyush Bhandari <prbhanda@redhat.com>
2024-06-20 16:14:37 -0700

    Use new `PageFormMultiSelectAwxResource` component

--- frontend/awx/access/credentials/components/PageFormCredentialSelect.tsx
+++ frontend/awx/access/credentials/components/PageFormCredentialSelect.tsx
@@ -1,24 +1,19 @@
 import { ReactElement } from 'react';
-import { FieldPath, FieldValues, useFormContext } from 'react-hook-form';
+import { FieldPath, FieldValues } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { PageFormMultiInput } from '../../../../../framework/PageForm/Inputs/PageFormMultiInput';
-import { requestGet } from '../../../../common/crud/Data';
-import { AwxItemsResponse } from '../../../common/AwxItemsResponse';
 import { awxAPI } from '../../../common/api/awx-utils';
 import { Credential } from '../../../interfaces/Credential';
-import { useMultiSelectCredential } from '../hooks/useSelectCredential';
 import { PageFormSingleSelectAwxResource } from '../../../common/PageFormSingleSelectAwxResource';
 import { useCredentialsColumns } from '../hooks/useCredentialsColumns';
 import { useCredentialsFilters } from '../hooks/useCredentialsFilters';
 import { QueryParams } from '../../../common/useAwxView';
+import { PageFormMultiSelectAwxResource } from '../../../common/PageFormMultiSelectAwxResource';
 
-/** @deprecated */
 export function PageFormCredentialSelect<
   TFieldValues extends FieldValues = FieldValues,
   TFieldName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
 >(props: {
   name: TFieldName;
-  acceptableCredentialKinds?: string[];
   additionalControls?: ReactElement;
   credentialIdPath?: string;
   credentialPath?: string;
@@ -36,44 +31,25 @@ export function PageFormCredentialSelect<
   queryParams?: QueryParams;
 }) {
   const { t } = useTranslation();
-  const multiSelectCredential = useMultiSelectCredential(
-    true,
-    props.credentialType,
-    props?.acceptableCredentialKinds
-  );
 
   const credentialColumns = useCredentialsColumns({ disableLinks: true });
   const credentialFilters = useCredentialsFilters();
 
-  const { setValue } = useFormContext();
   return props.isMultiple ? (
-    <PageFormMultiInput<Credential>
-      {...props}
-      isDisabled={props.isDisabled ? true : false}
-      placeholder={props.placeholder ? props.placeholder : t('Add credentials')}
+    <PageFormMultiSelectAwxResource<Credential>
       name={props.name}
-      id="credential-select"
+      id={props.id ? props.id : 'credential'}
       label={props.label ? props.label : t('Credential')}
-      selectTitle={props.selectTitle ? props.selectTitle : t('Select credentials')}
-      selectOpen={multiSelectCredential}
-      validate={async (credentials: Credential[]) => {
-        if (!props.isRequired) {
-          return;
-        }
-        try {
-          const itemsResponse = await requestGet<AwxItemsResponse<Credential>>(
-            awxAPI`/credentials/?name=${credentials[0].name}`
-          );
-          if (itemsResponse.results.length === 0) return t('Credential not found.');
-          if (props.credentialPath) setValue(props.credentialPath, itemsResponse.results[0]);
-          if (props.credentialIdPath) setValue(props.credentialIdPath, itemsResponse.results[0].id);
-        } catch (err) {
-          if (err instanceof Error) return err.message;
-          else return 'Unknown error';
-        }
-        return undefined;
-      }}
+      placeholder={props.placeholder ? props.placeholder : t('Add credentials')}
+      queryPlaceholder={t('Loading credentials...')}
+      queryErrorText={t('Error loading credentials')}
       isRequired={props.isRequired}
+      isDisabled={props.isDisabled}
+      labelHelp={props.labelHelp}
+      url={awxAPI`/credentials/`}
+      tableColumns={credentialColumns}
+      toolbarFilters={credentialFilters}
+      queryParams={props.queryParams}
     />
   ) : (
     <PageFormSingleSelectAwxResource<Credential, TFieldValues, TFieldName>
