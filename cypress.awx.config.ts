27ba346db635cc99aa0cb6db24a494ab45b71dad
MilanPospisil <arkanus@seznam.cz>
2024-06-20 12:12:57 +0200

    Burn them all

--- cypress.awx.config.ts
+++ cypress.awx.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'cypress';
 import setValue from 'set-value';
 import { baseConfig } from './cypress.base.config';
 
-baseConfig.e2e!.specPattern =
-  'cypress/e2e/awx/resources/inventoryHost/inventoryHostConstructed.cy.ts';
+baseConfig.e2e!.specPattern = 'cypress/e2e/awx/**/*.cy.ts';
 baseConfig.e2e!.baseUrl = 'https://localhost:4101';
 baseConfig.component!.specPattern = 'frontend/awx/**/*.cy.{js,jsx,ts,tsx}';
 setValue(baseConfig, 'component.devServer.webpackConfig.devServer.port', 4201);
