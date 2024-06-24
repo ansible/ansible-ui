2ebdd268d7787e477282dcadc5713a2c40d54a86
jerabekjiri <jerabekjirka@email.cz>
2024-06-24 17:05:06 +0200

    rebuild 3

--- cypress/e2e/awx/resources/workflowJobTemplateSurveys.cy.ts
+++ cypress/e2e/awx/resources/workflowJobTemplateSurveys.cy.ts
@@ -10,7 +10,7 @@ describe('Workflow Job Templates Surveys', function () {
   let organization: Organization;
   let reusableTemplateSurveyTestSuite: ReusableTemplateSurveyTestSuite;
 
-  // rebuild 2
+  // rebuild 3
   describe('WFJT Surveys: Create, Edit and Delete', function () {
     const question = {
       question_name: "Who's that?",
