40d98f05334564648521baf822a1b84eff9ddb48
Alex Corey <alex.swansboro@gmail.com>
2024-06-25 16:54:15 -0400

    Updates select components in awx to use framework async select.  Removes unused form.  Adds missing resource icon to some form selects.

--- frontend/awx/resources/templates/components/PageFormJobTemplateSelect.tsx
+++ frontend/awx/resources/templates/components/PageFormJobTemplateSelect.tsx
@@ -1,12 +1,10 @@
-import { useCallback } from 'react';
-import { FieldPath, FieldPathValue, FieldValues, Path } from 'react-hook-form';
+import { FieldPath, FieldValues } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { PageFormAsyncSelect } from '../../../../../framework/PageForm/Inputs/PageFormAsyncSelect';
-import { requestGet } from '../../../../common/crud/Data';
-import { AwxItemsResponse } from '../../../common/AwxItemsResponse';
 import { awxAPI } from '../../../common/api/awx-utils';
 import { JobTemplate } from '../../../interfaces/JobTemplate';
-import { useSelectJobTemplate } from '../hooks/useSelectJobTemplate';
+import { PageFormSingleSelectAwxResource } from '../../../common/PageFormSingleSelectAwxResource';
+import { missingResources, useTemplateColumns } from '../hooks/useTemplateColumns';
+import { useTemplateFilters } from '../hooks/useTemplateFilters';
 
 export function PageFormJobTemplateSelect<
   TFieldValues extends FieldValues = FieldValues,
@@ -19,52 +17,43 @@ export function PageFormJobTemplateSelect<
 }) {
   const { t } = useTranslation();
   const { templateType = 'job_templates' } = props;
-  const openSelectDialog = useSelectJobTemplate();
-  const query = useCallback(async () => {
-    const response = await requestGet<AwxItemsResponse<JobTemplate>>(
-      awxAPI`/${templateType}/`.concat(`?page_size=200`)
-    );
-
-    return Promise.resolve({
-      total: response.count,
-      values: response.results as FieldPathValue<TFieldValues, Path<TFieldValues>>[],
-    });
-  }, [templateType]);
-
+  const tableColumns = useTemplateColumns();
+  const toolbarFilters = useTemplateFilters();
   return (
-    <PageFormAsyncSelect<TFieldValues>
+    <PageFormSingleSelectAwxResource<JobTemplate, TFieldValues, TFieldName>
       name={props.name}
+      missingResource={(template) =>
+        missingResources(template) ? t(`Resources are missing from this template.`) : ''
+      }
+      tableColumns={tableColumns}
+      toolbarFilters={toolbarFilters}
+      url={
+        templateType === 'job_templates'
+          ? awxAPI`/job_templates/`
+          : awxAPI`/workflow_job_templates/`
+      }
       id="job-template-select"
       label={
         props.templateType === 'workflow_job_templates'
           ? t('Workflow job template')
           : t('Job template')
       }
-      query={query}
-      valueToString={(value) => {
-        if (value && typeof value === 'string') {
-          return value;
-        }
-        return (value as JobTemplate)?.name ?? '';
-      }}
       placeholder={
         props.templateType === 'workflow_job_templates'
           ? t('Select a workflow job template')
           : t('Select job template')
       }
-      loadingPlaceholder={
+      queryPlaceholder={
         props.templateType === 'workflow_job_templates'
           ? t('Loading workflow job templates...')
           : t('Loading job templates...')
       }
-      loadingErrorText={
+      queryErrorText={
         props.templateType === 'workflow_job_templates'
           ? t('Error loading workflow job templates')
           : t('Error loading job templates')
       }
       isRequired={props.isRequired}
-      limit={200}
-      openSelectDialog={openSelectDialog}
     />
   );
 }
