50f618cbba64e2760966a7e18e921cee7843bf48
MilanPospisil <arkanus@seznam.cz>
2024-06-26 15:27:14 +0200

    Run command for inventory hosts regular

--- cypress/e2e/awx/resources/inventoryHost/inventoryHostRegular.cy.ts
+++ cypress/e2e/awx/resources/inventoryHost/inventoryHostRegular.cy.ts
@@ -5,6 +5,7 @@ import { AwxUser } from '../../../../../frontend/awx/interfaces/User';
 import { runCommand } from './runCommandFunction';
 import { randomString } from '../../../../../framework/utils/random-string';
 import { Project } from '../../../../../frontend/awx/interfaces/Project';
+import { launchHostJob } from '../../../../support/hostsfunctions';
 
 import {
   checkHostGroup,
@@ -24,6 +25,9 @@ describe('Inventory Host Tab Tests for regular inventory', () => {
   before(() => {
     cy.createAwxOrganization().then((org) => {
       organization = org;
+      cy.createAwxProject({ organization: organization.id }).then((proj) => {
+        project = proj;
+      });
       cy.createAwxInventory({ organization: organization.id }).then((inv) => {
         inventory = inv;
       });
@@ -79,10 +83,14 @@ describe('Inventory Host Tab Tests for regular inventory', () => {
     checkFactsInHost(inventory, 'inventory_host');
   });
 
-  it.skip('can launch a job template that uses an inventory with a particular host and view the job on the host jobs tab inside the inventory', () => {
+  it('can launch a job template that uses an inventory with a particular host and view the job on the host jobs tab inside the inventory', () => {
     //1) Use inventory and host
     //2) create a job template that uses that inventory, launch the job template, wait for job to finish
     //3) Navigate back to inventory -> host tab -> jobs tab -> assert presence of job in that list
+    cy.createInventoryHost(organization, '').then((result) => {
+      launchHostJob(result.inventory, result.host, organization.id, project.id, 'InventoryHost');
+      cy.deleteAwxInventory(result.inventory, { failOnStatusCode: false });
+    });
   });
 
   it('can cancel a currently running job from the host jobs tab inside an inventory', () => {
