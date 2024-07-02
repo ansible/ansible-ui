7d3b3f1c323222d51ac40e4019d2388eee4ede2e
James Talton <jtalton@redhat.com>
2024-07-02 11:23:58 -0400

    Separate out the AWX org, team and user cypress commands (#2632)
    Signed-off-by: James Talton <talton@gmail.com>

--- cypress/support/awx-commands.ts
+++ cypress/support/awx-commands.ts
@@ -25,8 +25,6 @@ import { Organization } from '../../frontend/awx/interfaces/Organization';
 import { Project } from '../../frontend/awx/interfaces/Project';
 import { Schedule } from '../../frontend/awx/interfaces/Schedule';
 import { Spec, Survey } from '../../frontend/awx/interfaces/Survey';
-import { Team } from '../../frontend/awx/interfaces/Team';
-import { AwxUser } from '../../frontend/awx/interfaces/User';
 import { WorkflowApproval } from '../../frontend/awx/interfaces/WorkflowApproval';
 import { WorkflowJobTemplate } from '../../frontend/awx/interfaces/WorkflowJobTemplate';
 import { WorkflowJobNode, WorkflowNode } from '../../frontend/awx/interfaces/WorkflowNode';
@@ -596,13 +594,6 @@ Cypress.Commands.add('clickPageAction', (dataCy: string) => {
   cy.getByDataCy(dataCy).click();
 });
 
-Cypress.Commands.add('createAwxOrganization', (orgName?: string, failOnStatusCode?: boolean) => {
-  cy.requestPost<Pick<Organization, 'name'>, Organization>(
-    awxAPI`/organizations/`,
-    { name: orgName ? orgName : 'E2E Organization ' + randomString(4) },
-    failOnStatusCode
-  );
-});
 Cypress.Commands.add(
   'createAWXCredential',
   (credential: SetRequired<Partial<Credential>, 'organization' | 'kind' | 'credential_type'>) => {
@@ -650,70 +641,6 @@ Cypress.Commands.add(
   ) => {
     if (credentialType?.id) {
       cy.requestDelete(awxAPI`/credential_types/${credentialType.id.toString()}`, options);
-    }
-  }
-);
-
-Cypress.Commands.add(
-  'deleteAwxOrganization',
-  (
-    organization: Organization,
-    options?: {
-      /** Whether to fail on response codes other than 2xx and 3xx */
-      failOnStatusCode?: boolean;
-    }
-  ) => {
-    if (!organization?.id) return;
-    cy.requestDelete(awxAPI`/organizations/${organization?.id.toString()}/`, options);
-  }
-);
-
-Cypress.Commands.add('createAwxTeam', (organization: Organization) => {
-  cy.requestPost<Pick<Team, 'name' | 'organization'>, Team>(awxAPI`/teams/`, {
-    name: 'E2E Team ' + randomString(4),
-    organization: organization.id,
-  });
-});
-
-Cypress.Commands.add(
-  'deleteAwxTeam',
-  (
-    team: Team,
-    options?: {
-      /** Whether to fail on response codes other than 2xx and 3xx */
-      failOnStatusCode?: boolean;
-    }
-  ) => {
-    if (team.id) {
-      cy.requestDelete(awxAPI`/teams/${team.id.toString()}/`, options);
-    }
-  }
-);
-
-Cypress.Commands.add('createAwxUser', (organization: Organization) => {
-  cy.requestPost<Omit<AwxUser, 'id' | 'auth' | 'summary_fields'>, AwxUser>(
-    awxAPI`/organizations/${organization.id.toString()}/users/`,
-    {
-      username: 'e2e-user-' + randomString(4),
-      is_superuser: false,
-      is_system_auditor: false,
-      password: 'pw',
-      user_type: 'normal',
-    }
-  ).then((user) => user);
-});
-
-Cypress.Commands.add(
-  'deleteAwxUser',
-  (
-    user: AwxUser,
-    options?: {
-      /** Whether to fail on response codes other than 2xx and 3xx */
-      failOnStatusCode?: boolean;
-    }
-  ) => {
-    if (user?.id) {
-      cy.requestDelete(awxAPI`/users/${user.id.toString()}/`, options);
     }
   }
 );
