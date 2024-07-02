f717dacfb07e269499bbcdde6572687eaad2710a
Martin Hradil <mhradil@redhat.com>
2024-07-02 19:45:43 +0000

    remove unused helper getGlobalAwxToken

--- cypress/support/awx-commands.ts
+++ cypress/support/awx-commands.ts
@@ -1238,10 +1238,7 @@ Cypress.Commands.add('createAwxToken', (awxToken?: Partial<AwxToken>) => {
   ).then((result) => JSON.parse(result.stdout) as AwxToken);
 });
 
-Cypress.Commands.add('getGlobalAwxToken', () => {
-  if (globalAwxToken) cy.wrap(globalAwxToken);
-  else cy.createAwxToken().then((awxToken) => (globalAwxToken = awxToken));
-});
+;
 
 Cypress.Commands.add(
   'deleteAwxToken',
