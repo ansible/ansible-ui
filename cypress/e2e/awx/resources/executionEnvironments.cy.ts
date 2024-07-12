846e7c37e7f7920d1c24241749b2010283a8e7b8
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2024-07-12 16:48:18 -0400

    Re-enable execution environment tests (#2695)

--- cypress/e2e/awx/resources/executionEnvironments.cy.ts
+++ cypress/e2e/awx/resources/executionEnvironments.cy.ts
@@ -4,7 +4,6 @@ import { ExecutionEnvironment } from '../../../../frontend/awx/interfaces/Execut
 import { Inventory } from '../../../../frontend/awx/interfaces/Inventory';
 import { Organization } from '../../../../frontend/awx/interfaces/Organization';
 import { Project } from '../../../../frontend/awx/interfaces/Project';
-import { Team } from '../../../../frontend/awx/interfaces/Team';
 import { AwxUser } from '../../../../frontend/awx/interfaces/User';
 import { awxAPI } from '../../../support/formatApiPathForAwx';
 
@@ -101,7 +100,7 @@ describe('Execution Environments', () => {
       cy.clickModalButton('Close');
     });
 
-    it.skip('can create a new EE associated to a particular org, then visit the EE tab inside the org to view the EE and assert info', () => {
+    it('can create a new EE associated to a particular org, then visit the EE tab inside the org to view the EE and assert info', () => {
       cy.getByDataCy('create-execution-environment').click();
       cy.getByDataCy('name').type(execEnvName);
       cy.getByDataCy('image').type(image);
@@ -129,12 +128,7 @@ describe('Execution Environments', () => {
         });
       cy.clickLink(awxOrganization.name);
       cy.verifyPageTitle(awxOrganization.name);
-      cy.url().then((currentUrl) => {
-        expect(
-          currentUrl.includes(`/access/organizations/${awxOrganization.id.toString()}/details`)
-        ).to.be.true;
-      });
-      cy.clickTab(/^Execution environments$/, true);
+      cy.clickTab(/^Execution Environments$/, true);
       cy.filterTableBySingleSelect('name', execEnvName);
       cy.clickTableRowKebabAction(execEnvName, 'delete-execution-environment', false);
       cy.clickModalConfirmCheckbox();
@@ -153,7 +147,7 @@ describe('Execution Environments', () => {
       cy.clickModalButton('Close');
     });
 
-    it.skip('can create a new EE associated to a particular org, assign admin access to a user in that org, and login as that user to assert access to the EE', () => {
+    it('can create a new EE associated to a particular org, assign access to a user in that org, and login as that user to assert access to the EE', () => {
       cy.getByDataCy('create-execution-environment').click();
       cy.getByDataCy('name').type(execEnvName);
       cy.getByDataCy('image').type(image);
@@ -180,6 +174,8 @@ describe('Execution Environments', () => {
             'aria-disabled',
             'true'
           );
+          cy.logout();
+          cy.login();
         });
     });
   });
@@ -314,7 +310,7 @@ describe('Execution Environments', () => {
       cy.deleteAwxInventory(inventory, { failOnStatusCode: false });
     });
 
-    it.skip('can create a new JT using the existing EE, visit the templates tab of the EE to view the JT, delete the JT and then delete the EE', function () {
+    it('can create a new JT using the existing EE, visit the templates tab of the EE to view the JT, delete the JT and then delete the EE', function () {
       const jtName = 'E2E Job Template EE ' + randomString(4);
       const execEnvName = 'E2E Execution Environment JT ' + randomString(4);
       const image = 'quay.io/ansible/awx-ee:latest';
@@ -397,152 +393,5 @@ describe('Execution Environments', () => {
           expect(response?.statusCode).to.eql(204);
         });
     });
-  });
-});
-
-describe('Execution Environments: User/Team access', () => {
-  let organization: Organization;
-  let user: AwxUser;
-  let team: Team;
-  let execEnv: ExecutionEnvironment;
-  const execEnvName = 'E2E Execution Environment Create' + randomString(4);
-  const image = 'quay.io/ansible/awx-ee:latest';
-
-  before(() => {
-    cy.createAwxOrganization().then((org) => {
-      organization = org;
-      cy.createAwxUser({ organization: organization.id }).then((testUser) => {
-        user = testUser;
-      });
-      cy.createAwxTeam({ organization: organization.id }).then((testTeam) => {
-        team = testTeam;
-      });
-      cy.createAwxExecutionEnvironment({
-        name: execEnvName,
-        organization: organization.id,
-        image: image,
-      }).then((createdEE) => {
-        execEnv = createdEE;
-      });
-    });
-  });
-
-  after(() => {
-    cy.deleteAwxExecutionEnvironment(execEnv, { failOnStatusCode: false });
-    cy.deleteAwxUser(user, { failOnStatusCode: false });
-    cy.deleteAwxTeam(team, { failOnStatusCode: false });
-    cy.deleteAwxOrganization(organization, { failOnStatusCode: false });
-  });
-
-  it.skip('Add a user role assignment from the User Access tab', () => {
-    cy.navigateTo('awx', 'execution-environments');
-    cy.verifyPageTitle('Execution Environments');
-    cy.intercept('POST', awxAPI`/role_user_assignments/`).as('userRoleAssignment');
-    cy.filterTableByMultiSelect('name', [execEnvName]);
-    cy.clickTableRowLink('name', execEnvName, { disableFilter: true });
-    cy.hasDetail('Name', execEnv.name);
-    cy.clickTab(/^User Access$/, true);
-
-    cy.getByDataCy('add-roles').click();
-    cy.verifyPageTitle('Add roles');
-    cy.getWizard().within(() => {
-      cy.contains('h1', 'Select user(s)').should('be.visible');
-      cy.selectTableRowByCheckbox('username', user.username);
-      cy.clickButton(/^Next/);
-      cy.contains('h1', 'Select roles to apply').should('be.visible');
-      cy.filterTableByTextFilter('name', 'ExecutionEnvironment Admin', {
-        disableFilterSelection: true,
-      });
-      cy.selectTableRowByCheckbox('name', 'ExecutionEnvironment Admin', {
-        disableFilter: true,
-      });
-      cy.clickButton(/^Next/);
-      cy.contains('h1', 'Review').should('be.visible');
-      cy.verifyReviewStepWizardDetails('users', [user.username], '1');
-      cy.verifyReviewStepWizardDetails(
-        'awxRoles',
-        ['ExecutionEnvironment Admin', 'Has all permissions to a single execution environment'],
-        '1'
-      );
-      cy.clickButton(/^Finish/);
-      cy.wait('@userRoleAssignment')
-        .its('response')
-        .then((response) => {
-          expect(response?.statusCode).to.eql(201);
-        });
-    });
-    cy.getModal().within(() => {
-      cy.clickButton(/^Close$/);
-    });
-    cy.getModal().should('not.exist');
-    cy.verifyPageTitle(execEnvName);
-
-    cy.getByDataCy('select-all').click();
-    cy.clickToolbarKebabAction('remove-selected-roles');
-    cy.contains('Remove role');
-    cy.clickModalConfirmCheckbox();
-    cy.clickButton(/^Remove role$/);
-    cy.clickButton(/^Close$/);
-  });
-
-  it.skip('Add a team role assignment from the Team Access tab', () => {
-    cy.navigateTo('awx', 'execution-environments');
-    cy.verifyPageTitle('Execution Environments');
-    cy.intercept('POST', awxAPI`/role_team_assignments/`).as('teamRoleAssignment');
-    cy.filterTableByMultiSelect('name', [execEnvName]);
-    cy.clickTableRowLink('name', execEnvName, { disableFilter: true });
-    cy.hasDetail('Name', execEnvName);
-    cy.clickTab(/^Team Access$/, true);
-
-    cy.getByDataCy('add-roles').click();
-    cy.verifyPageTitle('Add roles');
-    cy.getWizard().within(() => {
-      cy.contains('h1', 'Select team(s)').should('be.visible');
-      cy.filterTableByMultiSelect('name', [team.name]);
-      cy.selectTableRowByCheckbox('name', team.name, { disableFilter: true });
-      cy.clickButton(/^Next/);
-      cy.contains('h1', 'Select roles to apply').should('be.visible');
-      cy.filterTableByTextFilter('name', 'ExecutionEnvironment Admin', {
-        disableFilterSelection: true,
-      });
-      cy.selectTableRowByCheckbox('name', 'ExecutionEnvironment Admin', {
-        disableFilter: true,
-      });
-      cy.clickButton(/^Next/);
-      cy.contains('h1', 'Review').should('be.visible');
-      cy.verifyReviewStepWizardDetails('teams', [team.name], '1');
-      cy.verifyReviewStepWizardDetails(
-        'awxRoles',
-        ['ExecutionEnvironment Admin', 'Has all permissions to a single execution environment'],
-        '1'
-      );
-      cy.clickButton(/^Finish/);
-      cy.wait('@teamRoleAssignment')
-        .its('response')
-        .then((response) => {
-          expect(response?.statusCode).to.eql(201);
-        });
-    });
-    cy.getModal().within(() => {
-      cy.clickButton(/^Close$/);
-    });
-    cy.getModal().should('not.exist');
-    cy.verifyPageTitle(execEnvName);
-
-    cy.getByDataCy('select-all').click();
-    cy.clickToolbarKebabAction('remove-selected-roles');
-    cy.contains('Remove role');
-    cy.clickModalConfirmCheckbox();
-    cy.clickButton(/^Remove role$/);
-    cy.clickButton(/^Close$/);
-  });
-
-  it('User and Team Access tabs are not present for managed EEs', () => {
-    cy.navigateTo('awx', 'execution-environments');
-    cy.verifyPageTitle('Execution Environments');
-    cy.clickTableRowLink('name', 'Control Plane Execution Environment', { disableFilter: true });
-    cy.verifyPageTitle('Control Plane Execution Environment');
-    cy.contains('a[role="tab"]', 'User Access').should('not.exist');
-    cy.contains('a[role="tab"]', 'Team Access').should('not.exist');
   });
 });
