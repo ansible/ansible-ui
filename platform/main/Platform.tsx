34c80ce121300c0fbcb3494a77b94e4a88f6877e
James Talton <jtalton@redhat.com>
2024-07-15 13:55:25 -0400

    AAP-27090 Platform login screen is missing product name (#384)
    Signed-off-by: James Talton <talton@gmail.com>

--- platform/main/Platform.tsx
+++ platform/main/Platform.tsx
@@ -4,9 +4,7 @@ import { createRoot } from 'react-dom/client';
 const Main = lazy(() => import('./PlatformMain'));
 document.body.innerHTML = '<div id="app"></div>';
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.body.style.backgroundColor = 'rgb(27, 29, 33)';
-}
+document.body.style.backgroundColor = '#222';
 
 const root = createRoot(document.getElementById('app')!);
 
