4784d51cecf9b688cdbbf22156ecefae8fd6e583
kialam <digitalanime@gmail.com>
2024-07-18 19:54:06 -0700

    Move wfjt e2e test cases into workflowJobTemplates.cy.ts spec file. (#2758)

--- cypress/e2e/awx/resources/workflowJobTemplates.cy.ts
+++ cypress/e2e/awx/resources/workflowJobTemplates.cy.ts
@@ -1,4 +1,5 @@
 import { randomString } from '../../../../framework/utils/random-string';
+import { Credential } from '../../../../frontend/awx/interfaces/Credential';
 import { Inventory } from '../../../../frontend/awx/interfaces/Inventory';
 import { JobTemplate } from '../../../../frontend/awx/interfaces/JobTemplate';
 import { Label } from '../../../../frontend/awx/interfaces/Label';
@@ -6,7 +7,7 @@ import { Organization } from '../../../../frontend/awx/interfaces/Organization';
 import { Project } from '../../../../frontend/awx/interfaces/Project';
 import { WorkflowJobTemplate } from '../../../../frontend/awx/interfaces/WorkflowJobTemplate';
 import { awxAPI } from '../../../support/formatApiPathForAwx';
-import { Credential } from '../../../../frontend/awx/interfaces/Credential';
+import { randomE2Ename } from '../../../support/utils';
 
 describe('Workflow Job Templates Tests', () => {
   let organization: Organization;
@@ -567,6 +568,297 @@ describe('Workflow Job Templates Tests', () => {
         .then((jobId: string) => {
           /*there is a known React error, `create request error` happening due the output tab work in progress,but the test executes fine since there is no ui interaction here*/
           cy.waitForWorkflowJobStatus(jobId);
+        });
+    });
+  });
+
+  describe('Notifications Tab for Workflow Job Templates', () => {
+    const wfJobTemplateName = randomE2Ename();
+    const inventoryName = randomE2Ename();
+    let wfJobTemplate: WorkflowJobTemplate;
+    let inventory: Inventory;
+
+    beforeEach(() => {
+      cy.createAwxInventory(organization, { name: inventoryName }).then((inv) => {
+        inventory = inv;
+        cy.createAwxWorkflowJobTemplate({
+          organization: organization.id,
+          inventory: inventory.id,
+          name: wfJobTemplateName,
+        }).then((jt) => {
+          wfJobTemplate = jt;
+        });
+      });
+    });
+
+    afterEach(() => {
+      cy.deleteAwxWorkflowJobTemplate(wfJobTemplate, { failOnStatusCode: false });
+      cy.deleteAwxInventory(inventory, { failOnStatusCode: false });
+    });
+
+    it('can navigate to the Workflow Job Templates -> Notifications list and then to the details page of the Notification', () => {
+      const notificationName = randomE2Ename();
+      cy.createNotificationTemplate(notificationName, organization).then(() => {
+        moveToNotification('templates', wfJobTemplateName, notificationName);
+      });
+    });
+
+    it('can toggle the Workflow Job Templates -> Notification on and off for job approval', () => {
+      testToggle(
+        'templates',
+        wfJobTemplateName,
+        'Click to enable approval',
+        'Click to disable approval',
+        organization
+      );
+    });
+
+    it('can toggle the Workflow Job Templates -> Notification on and off for job start', () => {
+      testToggle(
+        'templates',
+        wfJobTemplateName,
+        'Click to enable start',
+        'Click to disable start',
+        organization
+      );
+    });
+
+    it('can toggle the Workflow Job Templates -> Notification on and off for job success', () => {
+      testToggle(
+        'templates',
+        wfJobTemplateName,
+        'Click to enable success',
+        'Click to disable success',
+        organization
+      );
+    });
+
+    it('can toggle the Workflow Job Templates -> Notification on and off for job failure', () => {
+      testToggle(
+        'templates',
+        wfJobTemplateName,
+        'Click to enable failure',
+        'Click to disable failure',
+        organization
+      );
+    });
+  });
+
+  function testToggle(
+    type: string,
+    typeEntityName: string,
+    type_enable: string,
+    type_disable: string,
+    awxOrganization: Organization
+  ) {
+    const notificationName = randomE2Ename();
+    cy.createNotificationTemplate(notificationName, awxOrganization).then(() => {
+      moveToNotificationList(type, typeEntityName);
+      filterNotification(notificationName);
+      cy.get(`[aria-label="${type_enable}"]`).click();
+
+      // reload page to check if the toggle is working and try to disable it
+      cy.get(`[aria-label="${type_disable}"]`, { timeout: 5000 }).click();
+
+      // check if it is disabled again
+      cy.get(`[aria-label="${type_enable}"]`, { timeout: 5000 });
+    });
+  }
+
+  // move to notification details page
+  // type - organization, workflow job templates, etc.
+  // typeEntityName - name of the resource
+  // notificationName - name of the notification
+  function moveToNotification(type: string, typeEntityName: string, notificationName: string) {
+    moveToNotificationList(type, typeEntityName);
+    // this may need to change in UIX, now UIX has obsolete filter
+    //cy.filterTableByMultiSelect('name', [notificationName]);
+    filterNotification(notificationName);
+    cy.get('[data-cy="name-column-cell"] a').click();
+    cy.contains(notificationName);
+  }
+
+  function filterNotification(notificationName: string) {
+    cy.get(`[aria-label="Type to filter"]`).type(notificationName);
+    cy.getByDataCy(`apply-filter`).click();
+    cy.get(`[aria-label="Simple table"] tr`).should('have.length', 2);
+  }
+
+  // move to notification list page
+  // type - organization, workflow job templates or such
+  // typeEntityName - name of the organization, or other type
+  function moveToNotificationList(type: string, typeEntityName: string) {
+    cy.navigateTo('awx', type);
+    cy.filterTableByMultiSelect('name', [typeEntityName]);
+    cy.get('[data-cy="name-column-cell"] a').click();
+    cy.contains(typeEntityName);
+    cy.contains(`a[role="tab"]`, 'Notifications').click();
+  }
+
+  describe('Schedules - Create schedule of resource type Workflow job template', () => {
+    let organization: Organization;
+    let inventory: Inventory;
+    let workflowTemplate: WorkflowJobTemplate;
+
+    beforeEach(() => {
+      cy.createAwxOrganization().then((o) => {
+        organization = o;
+        cy.createAwxInventory(organization).then((i) => {
+          inventory = i;
+          cy.createAwxWorkflowJobTemplate({
+            name: 'E2E Workflow Job Template ' + randomString(4),
+            organization: organization.id,
+            inventory: inventory.id,
+          }).then((wfjt) => {
+            workflowTemplate = wfjt;
+          });
+        });
+      });
+    });
+
+    afterEach(() => {
+      cy.deleteAwxWorkflowJobTemplate(workflowTemplate, { failOnStatusCode: false });
+      cy.deleteAwxInventory(inventory, { failOnStatusCode: false });
+      cy.deleteAwxOrganization(organization, { failOnStatusCode: false });
+    });
+
+    it('can create a simple schedule of resource type Workflow job template, then delete the schedule', () => {
+      cy.navigateTo('awx', 'schedules');
+      cy.verifyPageTitle('Schedules');
+      const scheduleName = 'E2E Simple Schedule WFJT' + randomString(4);
+      cy.getByDataCy('create-schedule').click();
+      cy.selectDropdownOptionByResourceName('schedule_type', 'Workflow job template');
+      cy.selectDropdownOptionByResourceName(
+        'workflow-job-template-select',
+        `${workflowTemplate.name}`
+      );
+      cy.getByDataCy('name').type(`${scheduleName}`);
+      cy.singleSelectByDataCy('timezone', 'Zulu');
+      cy.clickButton(/^Next$/);
+      cy.getByDataCy('interval').clear().type('100');
+      cy.selectDropdownOptionByResourceName('freq', 'Hourly');
+      cy.getByDataCy('count-form-group').type('17');
+      cy.getByDataCy('add-rule-button').click();
+      cy.get('tr[data-cy="row-id-1"]').within(() => {
+        cy.get('td[data-cy="rrule-column-cell"]').should(
+          'contains.text',
+          'RRULE:FREQ=HOURLY;INTERVAL=100;WKST=SU'
+        );
+      });
+      cy.clickButton(/^Next$/);
+      cy.clickButton(/^Next$/);
+      cy.get('tr[data-cy="row-id-1"]').should('be.visible');
+      cy.clickButton(/^Finish$/);
+      cy.verifyPageTitle(`${scheduleName}`);
+      cy.navigateTo('awx', 'schedules');
+      cy.filterTableBySingleSelect('name', scheduleName);
+      cy.getBy('tbody').within(() => {
+        cy.clickKebabAction('actions-dropdown', 'delete-schedule');
+      });
+      cy.intercept('DELETE', awxAPI`/schedules/*`).as('deleted');
+      cy.getModal().within(() => {
+        cy.getBy('input[id="confirm"]').click();
+        cy.getBy('[data-ouia-component-id="submit"]').click();
+        cy.wait('@deleted')
+          .its('response')
+          .then((response) => {
+            expect(response?.statusCode).to.eql(204);
+          });
+        cy.clickButton('Close');
+      });
+      cy.clickButton('Clear all filters');
+      const dataCy = 'name';
+      cy.get('#filter').click();
+      cy.document().its('body').find('#filter-search').type(dataCy.replaceAll('-', ' '));
+      cy.document()
+        .its('body')
+        .find('#filter-select')
+        .within(() => {
+          cy.getByDataCy(dataCy).click();
+        });
+
+      cy.getBy('#filter-input').click();
+      cy.document()
+        .its('body')
+        .find('.pf-v5-c-menu__content')
+        .within(() => {
+          cy.getByDataCy('search-input').type(scheduleName);
+          cy.contains('.pf-v5-c-menu__item-text', 'No results found').should('be.visible');
+        });
+      cy.deleteAwxWorkflowJobTemplate(workflowTemplate, { failOnStatusCode: false });
+    });
+  });
+
+  describe('Workflow template: Output and Details Screen', () => {
+    let workflowJobTemplate: WorkflowJobTemplate;
+    let jobTemplate: JobTemplate;
+    let inventory: Inventory;
+    let project: Project;
+
+    beforeEach(function () {
+      cy.createAwxProject(organization).then((p) => {
+        project = p;
+      });
+      cy.createAwxInventory(organization).then((i) => {
+        inventory = i;
+        cy.createAwxJobTemplate({
+          organization: organization.id,
+          project: project.id,
+          inventory: inventory.id,
+        }).then((jt) => {
+          jobTemplate = jt;
+          cy.createAwxWorkflowJobTemplate({
+            organization: organization.id,
+            inventory: inventory.id,
+          }).then((wfjt) => {
+            workflowJobTemplate = wfjt;
+            cy.createAwxWorkflowVisualizerJobTemplateNode(workflowJobTemplate, jobTemplate).then(
+              (jobTemplateNode) => {
+                cy.createAwxWorkflowVisualizerManagementNode(workflowJobTemplate, 2).then(
+                  (managementNode) => {
+                    cy.createWorkflowJTFailureNodeLink(jobTemplateNode, managementNode);
+                  }
+                );
+              }
+            );
+          });
+        });
+      });
+    });
+
+    afterEach(function () {
+      cy.deleteAwxWorkflowJobTemplate(workflowJobTemplate, { failOnStatusCode: false });
+      cy.deleteAwxJobTemplate(jobTemplate, { failOnStatusCode: false });
+      cy.deleteAwxInventory(inventory, { failOnStatusCode: false });
+    });
+
+    it('can launch a Workflow job, let it finish, and assert expected results on the output screen', () => {
+      cy.navigateTo('awx', 'templates');
+      cy.verifyPageTitle('Templates');
+      cy.filterTableBySingleSelect('name', workflowJobTemplate.name);
+      cy.intercept(
+        'POST',
+        awxAPI`/workflow_job_templates/${workflowJobTemplate.id.toString()}/launch/`
+      ).as('postLaunch');
+      cy.getByDataCy('launch-template').click();
+      cy.wait('@postLaunch')
+        .then((response) => {
+          expect(response?.response?.statusCode).to.eql(201);
+        })
+        .its('response.body')
+        .then((wfJob: WorkflowJobTemplate) => {
+          cy.waitForWorkflowJobStatus(wfJob.id.toString());
+          cy.verifyPageTitle(workflowJobTemplate.name);
+          cy.url().then((currentUrl) => {
+            expect(currentUrl.includes(`/jobs/workflow/${wfJob.id}/output`)).to.be.true;
+          });
+          cy.clickTab(/^Details$/, true);
+          cy.url().then((currentUrl) => {
+            expect(currentUrl.includes(`/jobs/workflow/${wfJob.id}/details`)).to.be.true;
+          });
+          cy.getByDataCy('name').should('contain', workflowJobTemplate.name);
+          cy.getByDataCy('type').should('contain', 'Workflow job');
+          cy.getByDataCy('inventory').should('contain', inventory.name);
         });
     });
   });
