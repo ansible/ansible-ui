f60c8e87bbc0ac33077c883ec525ee335c9bb222
James Talton <talton@gmail.com>
2024-07-19 15:43:07 -0400

    test
    Signed-off-by: James Talton <talton@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -74,7 +74,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run awx',
+    command: 'npm run docker:run:awx',
     url: 'https://localhost:4101',
     reuseExistingServer: !process.env.CI,
     timeout: 300 * 1000,
