2c7577938a2f5c0513ff1794ff07593e650dc96d
James Talton <talton@gmail.com>
2024-07-22 09:58:04 -0400

    fixes
    Signed-off-by: James Talton <talton@gmail.com>

--- playwright/commands/awx/awxLogin.ts
+++ playwright/commands/awx/awxLogin.ts
@@ -5,7 +5,7 @@ export async function awxLogin(page: Page) {
   // await page.goto(process.env.AWX_SERVER!);
   await page.goto('https://localhost:4101');
   await expect(page).toHaveTitle(/AWX/);
-  await page.fill('#pf-login-username-id', process.env.AWX_USENAME!);
+  await page.fill('#pf-login-username-id', process.env.AWX_USERNAME!);
   await page.fill('#pf-login-password-id', process.env.AWX_PASSWORD!);
   await page.click('button[type="submit"]');
   await page.click('button[aria-label="Close"]');
