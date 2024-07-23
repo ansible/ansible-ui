34fbd1336ac07335b21915b7d89ac3a1ba8fa16e
MilanPospisil <arkanus@seznam.cz>
2024-07-23 16:31:43 +0200

    Checks

--- frontend/common/label-group-wrapper.tsx
+++ frontend/common/label-group-wrapper.tsx
@@ -1,18 +1,20 @@
 import { LabelGroup as PFLabelGroup } from '@patternfly/react-core';
 
+type LabelGroupWrapperState = {
+  headingRef: { current: { offsetWidth: number; scrollWidth: number } };
+};
+
 // fix button without type when rendering "show more" in forms
 export class LabelGroupWrapper extends PFLabelGroup {
-  // @ts-expect-error Type '(e: any) => void' is not assignable to type '() => void'.
+  // @ts-expect-error Type '(e: React.MouseEvent<HTMLButtonElement>) => void' is not assignable to type '() => void'.
   toggleCollapse = (event: React.MouseEvent<HTMLButtonElement>) => {
     // Label isOverflowLabel renders a button, but not button type=button, breaks forms
     event.preventDefault();
 
     // original LabelGroup toggleCollapse, as of @patternfly/react-core 5.2.0, w/ ts fix
     this.setState((prevState) => {
-      // @ts-expect-error Property 'headingRef' is private and only accessible within class 'LabelGroup'.
-      const currentRef = (
-        this as { headingRef: { current: { offsetWidth: number; scrollWidth: number } } }
-      ).headingRef.current;
+      const obj = this as unknown as LabelGroupWrapperState;
+      const currentRef = obj.headingRef.current;
 
       return {
         isOpen: !prevState.isOpen,
