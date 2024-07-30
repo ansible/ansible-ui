6a4b4aac4569da27699809de28556c76496ea7c1
Stevenson Michel <smichel@smichel-thinkpadp1gen4i.rmtusky.csb>
2024-07-30 11:41:00 -0400

    Changed instances text

--- frontend/awx/administration/instances/Instances.tsx
+++ frontend/awx/administration/instances/Instances.tsx
@@ -21,10 +21,10 @@ export function Instances() {
         title={t('Instances')}
         titleHelpTitle={t('Instances')}
         titleHelp={t(
-          'Ansible node instances are dedicated for a particular purpose indicated by node type.'
+          'Ansible node instances dedicated for a particular purpose indicated by node type.'
         )}
         description={t(
-          'Ansible node instances are dedicated for a particular purpose indicated by node type.'
+          'Ansible node instances dedicated for a particular purpose indicated by node type.'
         )}
         headerActions={<ActivityStreamIcon type={'instance'} />}
       />
