13792ce99f8202a80da5af4a302e9b7fb43face1
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2024-08-05 16:26:35 -0400

    [AAP-16644] Hub Namespace - User/Team Access (#2877)
    Co-authored-by: nixocio <nixocio@gmail.com>
    Co-authored-by: vsanghishetty <vishali.kamenani@ymail.com>

---
+++ frontend/hub/access/common/HubRoleWizardSteps/HubSelectRolesStep.tsx
@@ -0,0 +1,78 @@
+import { useMemo } from 'react';
+import { ITableColumn, TextCell } from '../../../../../framework';
+import { useTranslation } from 'react-i18next';
+import { SelectRolesStep } from '../../../../common/access/RolesWizard/steps/SelectRolesStep';
+import { usePageWizard } from '../../../../../framework/PageWizard/PageWizardProvider';
+import { useHubRoleFilters } from '../hooks/useHubRoleFilters';
+import { HubRbacRole } from '../../../interfaces/expanded/HubRbacRole';
+import { useHubMultiSelectListView } from '../../../common/useHubMultiSelectListView';
+import { hubAPI } from '../../../common/api/formatPath';
+
+export function HubSelectRolesStep(props: {
+  contentType?: string;
+  fieldNameForPreviousStep?: string;
+  descriptionForRoleSelection?: string;
+  title?: string;
+}) {
+  const toolbarFilters = useHubRoleFilters();
+  const { t } = useTranslation();
+  const { wizardData } = usePageWizard();
+  const { resourceType } = wizardData as { [key: string]: unknown };
+  const { fieldNameForPreviousStep, title } = props;
+
+  const contentType = useMemo(() => {
+    return props.contentType ? props.contentType : (resourceType as string)?.split('.').pop() ?? '';
+  }, [props.contentType, resourceType]);
+
+  const descriptionForRoleSelection = useMemo(() => {
+    if (props.descriptionForRoleSelection) {
+      return props.descriptionForRoleSelection;
+    }
+    switch (resourceType as string) {
+      case 'galaxy.namespace':
+        return t('Select roles to apply to all of your selected namespaces.');
+      // TODO more resource types to be added later
+      default:
+        return t('Select roles to apply to all of your selected resources.');
+    }
+  }, [props.descriptionForRoleSelection, resourceType, t]);
+
+  const tableColumns: ITableColumn<HubRbacRole>[] = useMemo(() => {
+    return [
+      {
+        header: t('Name'),
+        cell: (role) => <TextCell text={role.name} />,
+        card: 'name',
+        list: 'name',
+        sort: 'name',
+      },
+      {
+        header: t('Description'),
+        cell: (role) => role.description && <TextCell text={role.description} />,
+        card: 'description',
+        list: 'description',
+      },
+    ];
+  }, [t]);
+
+  const view = useHubMultiSelectListView<HubRbacRole>(
+    {
+      url: hubAPI`/_ui/v2/role_definitions/`,
+      toolbarFilters,
+      tableColumns,
+      queryParams: { content_type__model: contentType },
+    },
+    'hubRoles'
+  );
+
+  return (
+    <SelectRolesStep
+      view={view}
+      tableColumns={tableColumns}
+      toolbarFilters={toolbarFilters}
+      fieldNameForPreviousStep={fieldNameForPreviousStep}
+      descriptionForRoleSelection={descriptionForRoleSelection}
+      title={title}
+    />
+  );
+}
