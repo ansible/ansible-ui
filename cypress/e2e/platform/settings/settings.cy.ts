2889afa079bbc62e38729e99320511a8348aa5a6
Sarah Akus <sarah.akus@gmail.com>
2024-08-09 10:06:53 -0400

    Add tagging to exclude eda tests from aaas test runs (#472)

--- cypress/e2e/platform/settings/settings.cy.ts
+++ cypress/e2e/platform/settings/settings.cy.ts
@@ -1,29 +1,37 @@
 import { ILicenseInfo } from '../../../../frontend/awx/interfaces/Config';
-describe('Settings', () => {
-  it('checks license compliance status', () => {
-    cy.intercept('GET', '/api/controller/v2/config').as('getConfig');
-    cy.wait('@getConfig')
-      .its('response.body.license_info')
-      .then((licenseObject: ILicenseInfo) => {
-        // If license compliant is false check for banner and possibility to Edit subscription
-        if (!licenseObject.compliant) {
-          cy.getByDataCy('subscription-grace-period-banner').should(
-            'include.text',
-            'Your subscription is out of compliance.'
-          );
-          cy.navigateTo('platform', 'subscription-details');
-          cy.get('[data-cy="status"]').should('have.text', 'Out of compliance');
-          cy.clickButton(/^Edit subscription/);
-          cy.contains('h1', 'Welcome to Red Hat Ansible Automation Platform!').should('be.visible');
-          cy.url().should('include', 'settings/subscription/wizard');
-        } else {
-          cy.get('[data-cy="subscription-grace-period-banner"]').should('not.exist');
-          cy.navigateTo('platform', 'subscription-details');
-          cy.get('[data-cy="status"]').should('have.text', 'Compliant');
-          cy.clickButton(/^Edit subscription/);
-          cy.contains('h1', 'Welcome to Red Hat Ansible Automation Platform!').should('be.visible');
-          cy.url().should('include', 'settings/subscription/wizard');
-        }
-      });
+import { tag } from '../../../support/tag';
+
+tag(['aaas-unsupported'], function () {
+  describe('Settings', () => {
+    it('checks license compliance status', () => {
+      cy.intercept('GET', '/api/controller/v2/config').as('getConfig');
+      cy.wait('@getConfig')
+        .its('response.body.license_info')
+        .then((licenseObject: ILicenseInfo) => {
+          // If license compliant is false check for banner and possibility to Edit subscription
+          if (!licenseObject.compliant) {
+            cy.getByDataCy('subscription-grace-period-banner').should(
+              'include.text',
+              'Your subscription is out of compliance.'
+            );
+            cy.navigateTo('platform', 'subscription-details');
+            cy.get('[data-cy="status"]').should('have.text', 'Out of compliance');
+            cy.clickButton(/^Edit subscription/);
+            cy.contains('h1', 'Welcome to Red Hat Ansible Automation Platform!').should(
+              'be.visible'
+            );
+            cy.url().should('include', 'settings/subscription/wizard');
+          } else {
+            cy.get('[data-cy="subscription-grace-period-banner"]').should('not.exist');
+            cy.navigateTo('platform', 'subscription-details');
+            cy.get('[data-cy="status"]').should('have.text', 'Compliant');
+            cy.clickButton(/^Edit subscription/);
+            cy.contains('h1', 'Welcome to Red Hat Ansible Automation Platform!').should(
+              'be.visible'
+            );
+            cy.url().should('include', 'settings/subscription/wizard');
+          }
+        });
+    });
   });
 });
