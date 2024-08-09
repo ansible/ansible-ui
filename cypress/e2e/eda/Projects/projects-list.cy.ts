e18c6be3477b1a1766ffa91e0064b9426715851c
Sarah Akus <sarah.akus@gmail.com>
2024-08-09 10:06:45 -0400

    Add tagging to exclude eda tests from aaas test run (#2919)

--- cypress/e2e/eda/Projects/projects-list.cy.ts
+++ cypress/e2e/eda/Projects/projects-list.cy.ts
@@ -1,56 +1,57 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /// <reference types="cypress" />
-
 import { edaAPI } from '../../../support/formatApiPathForEDA';
+import { tag } from '../../../support/tag';
 
-//Tests a user's ability to perform necessary actions on the Projects list in the EDA UI.
-describe('EDA Projects List', () => {
-  it('renders the EDA projects page', () => {
-    cy.navigateTo('eda', 'projects');
-    cy.verifyPageTitle('Projects');
-  });
-
-  it('renders the Project details page', () => {
-    cy.createEdaProject().then((edaProject) => {
+tag(['aaas-unsupported'], function () {
+  describe('EDA Projects List', () => {
+    it('renders the EDA projects page', () => {
       cy.navigateTo('eda', 'projects');
-      cy.clickTableRow(edaProject.name);
-      cy.verifyPageTitle(edaProject.name);
-      cy.clickLink(/^Details$/);
-      cy.verifyPageTitle(edaProject.name);
-      cy.deleteEdaProject(edaProject);
+      cy.verifyPageTitle('Projects');
     });
-  });
 
-  it('can filter the Projects list based on Name', () => {
-    cy.createEdaProject().then((edaProject) => {
-      cy.navigateTo('eda', 'projects');
-      cy.filterTableByText(edaProject.name);
-      cy.get('td[data-cy=name-column-cell]').should('contain', edaProject.name);
-      cy.deleteEdaProject(edaProject);
-    });
-  });
-
-  it('can bulk delete Projects from the Projects list', () => {
-    cy.createEdaProject().then((edaProject) => {
-      cy.createEdaProject().then((testProject) => {
+    it('renders the Project details page', () => {
+      cy.createEdaProject().then((edaProject) => {
         cy.navigateTo('eda', 'projects');
-        cy.selectTableRow(edaProject.name);
-        cy.clickButton(/^Clear all filters$/);
-        cy.selectTableRow(testProject.name);
-        cy.clickToolbarKebabAction('delete-projects');
-        cy.intercept('DELETE', edaAPI`/projects/${edaProject.id.toString()}/`).as('edaProject');
-        cy.intercept('DELETE', edaAPI`/projects/${testProject.id.toString()}/`).as('testProject');
-        cy.clickModalConfirmCheckbox();
-        cy.clickModalButton('Delete projects');
-        cy.wait('@edaProject').then((edaProject) => {
-          expect(edaProject?.response?.statusCode).to.eql(204);
+        cy.clickTableRow(edaProject.name);
+        cy.verifyPageTitle(edaProject.name);
+        cy.clickLink(/^Details$/);
+        cy.verifyPageTitle(edaProject.name);
+        cy.deleteEdaProject(edaProject);
+      });
+    });
+
+    it('can filter the Projects list based on Name', () => {
+      cy.createEdaProject().then((edaProject) => {
+        cy.navigateTo('eda', 'projects');
+        cy.filterTableByText(edaProject.name);
+        cy.get('td[data-cy=name-column-cell]').should('contain', edaProject.name);
+        cy.deleteEdaProject(edaProject);
+      });
+    });
+
+    it('can bulk delete Projects from the Projects list', () => {
+      cy.createEdaProject().then((edaProject) => {
+        cy.createEdaProject().then((testProject) => {
+          cy.navigateTo('eda', 'projects');
+          cy.selectTableRow(edaProject.name);
+          cy.clickButton(/^Clear all filters$/);
+          cy.selectTableRow(testProject.name);
+          cy.clickToolbarKebabAction('delete-projects');
+          cy.intercept('DELETE', edaAPI`/projects/${edaProject.id.toString()}/`).as('edaProject');
+          cy.intercept('DELETE', edaAPI`/projects/${testProject.id.toString()}/`).as('testProject');
+          cy.clickModalConfirmCheckbox();
+          cy.clickModalButton('Delete projects');
+          cy.wait('@edaProject').then((edaProject) => {
+            expect(edaProject?.response?.statusCode).to.eql(204);
+          });
+          cy.wait('@testProject').then((testProject) => {
+            expect(testProject?.response?.statusCode).to.eql(204);
+          });
+          cy.assertModalSuccess();
+          cy.clickButton(/^Close$/);
+          cy.clickButton(/^Clear all filters$/);
         });
-        cy.wait('@testProject').then((testProject) => {
-          expect(testProject?.response?.statusCode).to.eql(204);
-        });
-        cy.assertModalSuccess();
-        cy.clickButton(/^Close$/);
-        cy.clickButton(/^Clear all filters$/);
       });
     });
   });
