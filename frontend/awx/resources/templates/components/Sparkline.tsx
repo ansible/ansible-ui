17214ee259b6b647fb0ea84a32a91771da216cea
Maeve Hoffer <dhoffer@redhat.com>
2024-08-12 13:43:34 -0700

    add back url segments when needed

--- frontend/awx/resources/templates/components/Sparkline.tsx
+++ frontend/awx/resources/templates/components/Sparkline.tsx
@@ -13,6 +13,14 @@ const Wrapper = styled.div`
   flex-wrap: wrap;
 `;
 export const Sparkline = ({ jobs }: { jobs: SummaryFieldRecentJob[] | undefined }) => {
+  const JOB_TYPE_URL_SEGMENTS: { [char: string]: string } = {
+    job: 'playbook',
+    project_update: 'project',
+    system_job: 'management',
+    inventory_update: 'inventory',
+    ad_hoc_command: 'command',
+    workflow_job: 'workflow',
+  };
   const { t } = useTranslation();
   const getJobOutputUrl = useGetJobOutputUrl();
   const generateTooltip = (job: SummaryFieldRecentJob) => (
@@ -33,7 +41,14 @@ export const Sparkline = ({ jobs }: { jobs: SummaryFieldRecentJob[] | undefined 
 
   const statusIcons = jobs?.map((job) => (
     <Tooltip position="top" content={generateTooltip(job)} key={job.id}>
-      <Link aria-label={t(`View job ${job.id}`)} to={getJobOutputUrl(job as UnifiedJob)}>
+      <Link
+        aria-label={t(`View job ${job.id}`)}
+        to={
+          getJobOutputUrl(job as UnifiedJob)
+            ? getJobOutputUrl(job as UnifiedJob)
+            : `/jobs/${JOB_TYPE_URL_SEGMENTS[job.type]}/${job.id}/output`
+        }
+      >
         <StatusCell status={job.status} hideLabel={true} />
       </Link>
     </Tooltip>
