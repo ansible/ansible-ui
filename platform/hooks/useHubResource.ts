7e1d1e8f6ab24d3e8add3b6830a73027a4e33517
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2024-08-13 15:46:17 -0400

    Hook to query platform resources from Hub (#483)

---
+++ platform/hooks/useHubResource.ts
@@ -0,0 +1,29 @@
+import { requestGet } from '../../frontend/common/crud/Data';
+import { useGet } from '../../frontend/common/crud/useGet';
+import { hubAPI } from '../../frontend/hub/common/api/formatPath';
+import { PulpItemsResponse } from '../../frontend/hub/common/useHubView';
+import { PlatformResource } from '../interfaces/PlatformResource';
+
+export function useHubResource<T extends object>(url: string, platformResource?: PlatformResource) {
+  const { data, isLoading, error } = useGet<PulpItemsResponse<T>>(
+    hubAPI`/${url}?resource__ansible_id=${platformResource?.summary_fields?.resource?.ansible_id ?? ''}`
+  );
+  const { results } = data ?? {};
+
+  return {
+    resource: results && results.length ? results[0] : undefined,
+    isLoading,
+    error,
+  };
+}
+
+export async function getHubResource<T extends object>(
+  url: string,
+  platformResource?: PlatformResource
+) {
+  const data = await requestGet<PulpItemsResponse<T>>(
+    hubAPI`/${url}?resource__ansible_id=${platformResource?.summary_fields?.resource?.ansible_id ?? ''}`
+  );
+  const { results } = data ?? {};
+  return results && results.length ? results[0] : undefined;
+}
