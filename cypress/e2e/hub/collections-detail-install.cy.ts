712fa9b36f189cfd21132a5056229a71bbd75105
MilanPospisil <arkanus@seznam.cz>
2024-08-13 14:39:30 +0200

    Test install tab without download tarball

--- cypress/e2e/hub/collections-detail-install.cy.ts
+++ cypress/e2e/hub/collections-detail-install.cy.ts
@@ -3,65 +3,61 @@ import { Collections } from './constants';
 
 // cypress
 describe('collections-detail-install', () => {
+  let collectionName: string = '';
+  let namespaceName: string = '';
 
-    let collectionName : string = '';
-    let namespaceName : string = '';
+  function goToInstallTab() {
+    cy.navigateTo('hub', Collections.url);
+    cy.get(`[data-cy="table-view"] button`).click();
+    cy.filterTableBySingleText(collectionName);
+    cy.get(`[aria-label="Simple table"] tr`).should('have.length', 2);
+    cy.contains('a', collectionName).click();
+    cy.getByDataCy('collection-install-tab').click();
+  }
 
-    function goToInstallTab()
-    {
-        cy.navigateTo('hub', Collections.url);
-        cy.get(`[data-cy="table-view"] button`).click();
-        cy.filterTableBySingleText(collectionName);
-        cy.get(`[aria-label="Simple table"] tr`).should('have.length', 2);
-        cy.contains('a', collectionName).click();
-        cy.getByDataCy('collection-install-tab').click();
-    }
+  beforeEach(() => {
+    namespaceName = randomE2Ename();
+    collectionName = randomE2Ename();
 
-    beforeEach(() => {
-        namespaceName = randomE2Ename();
-        collectionName = randomE2Ename();
-
-        cy.createHubNamespace({ namespace: { name: namespaceName } }).then(() => {
-            cy.uploadCollection(collectionName, namespaceName, '1.0.0').then(() => {
-                cy.approveCollection(collectionName, namespaceName, '1.0.0');
-            });
-        });
+    cy.createHubNamespace({ namespace: { name: namespaceName } }).then(() => {
+      cy.uploadCollection(collectionName, namespaceName, '1.0.0').then(() => {
+        cy.approveCollection(collectionName, namespaceName, '1.0.0');
+      });
     });
+  });
 
-    it('can download tarball', () => {
-       goToInstallTab();
-        
-        cy.get('.body').contains('Install');
-        cy.get('.body').contains('License');
-        cy.get('.body').contains('Installation');
-    
-       // can go to documentation
-       cy.contains('a', 'Go to documentation').click();
-       cy.contains('Documentation (1)');
+  it('can download tarball', () => {
+    goToInstallTab();
 
-       goToInstallTab();
-    
-        /*
-         * This test needs some external library and custom command to test if the download had started.
-         * For now it fails if the button is not there.
-         */
-        // should be able to download the tarball
-        cy.contains('a', 'Distributions').click();
-        cy.contains('CLI configuration');
+    cy.contains('Install');
+    cy.contains('License');
+    cy.contains('Installation');
+    cy.contains('GPL-3.0-or-later');
 
-        goToInstallTab();
-    
-        // should have the correct tags
-        cy.contains('span','tools');
-    
-        // should have the correct ansible version
-        cy.contains('Ansible >=2');
-        
-        
-    });
+    // can go to documentation
+    cy.contains('a', 'Go to documentation').click();
+    cy.contains('Documentation (1)');
 
-    afterEach(() => {
-        cy.deleteHubCollectionByName(collectionName);
-    });
+    goToInstallTab();
+    cy.contains('a', 'Distributions').click();
+    cy.contains('CLI configuration');
 
+    goToInstallTab();
+
+    // should have the correct tags
+    cy.contains('span', 'tools');
+
+    // should have the correct ansible version
+    cy.contains('Ansible >=2');
+
+    // for now we test button is rendered
+    cy.contains('button', 'Download tarball');
+
+    cy.contains('button', 'Show signature').click();
+    cy.contains('BEGIN PGP SIGNATURE');
+  });
+
+  afterEach(() => {
+    cy.deleteHubCollectionByName(collectionName);
+  });
 });
