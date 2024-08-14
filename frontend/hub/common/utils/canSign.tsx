4a8ed90dc820824dc68d2ebf4dd73257aebc268b
Pratyush Bhandari <prbhanda@redhat.com>
2024-08-14 16:42:36 -0400

    Remove hasPermission check from canSign helpers

--- frontend/hub/common/utils/canSign.tsx
+++ frontend/hub/common/utils/canSign.tsx
@@ -1,22 +1,19 @@
 import { useHubContext } from '../useHubContext';
 
 export const useCanSignNamespace = () => {
-  const { featureFlags, hasPermission } = useHubContext();
+  const { featureFlags } = useHubContext();
   const { can_create_signatures } = featureFlags;
 
-  const canSign =
-    can_create_signatures &&
-    hasPermission('galaxy.change_namespace') &&
-    hasPermission('galaxy.upload_to_namespace');
+  const canSign = can_create_signatures;
 
   return canSign;
 };
 
 export const useCanSignEE = () => {
-  const { featureFlags, hasPermission } = useHubContext();
+  const { featureFlags } = useHubContext();
   const { container_signing } = featureFlags;
 
-  const canSign = container_signing && hasPermission('container.change_containernamespace');
+  const canSign = container_signing;
 
   return canSign;
 };
