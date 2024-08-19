d7f2294ee98d6d975190f9de4b1e4d4d14e111e7
lgalis <lgalis@redhat.com>
2024-08-19 18:41:23 -0400

    Update event streams columns and Test mode toggle

--- frontend/eda/event-streams/EventStreamPage/EventStreamPage.tsx
+++ frontend/eda/event-streams/EventStreamPage/EventStreamPage.tsx
@@ -1,7 +1,7 @@
-import { ButtonVariant } from '@patternfly/react-core';
+import { AlertProps, ButtonVariant } from '@patternfly/react-core';
 import { DropdownPosition } from '@patternfly/react-core/deprecated';
-import { PencilAltIcon, TrashIcon } from '@patternfly/react-icons';
-import { useMemo } from 'react';
+import { DatabaseIcon, PencilAltIcon, TaskIcon, TrashIcon } from '@patternfly/react-icons';
+import { useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import {
@@ -13,6 +13,7 @@ import {
   PageLayout,
   useGetPageUrl,
   usePageNavigate,
+  usePageAlertToaster,
 } from '../../../../framework';
 import { PageRoutedTabs } from '../../../common/PageRoutedTabs';
 import { useGet } from '../../../common/crud/useGet';
@@ -20,13 +21,41 @@ import { edaAPI } from '../../common/eda-utils';
 import { EdaEventStream } from '../../interfaces/EdaEventStream';
 import { EdaRoute } from '../../main/EdaRoutes';
 import { useDeleteEventStreams } from '../hooks/useDeleteEventStreams';
+import { usePatchRequest } from '../../../common/crud/usePatchRequest';
 
 export function EventStreamPage() {
   const { t } = useTranslation();
   const params = useParams<{ id: string }>();
   const pageNavigate = usePageNavigate();
   const { data: eventStream } = useGet<EdaEventStream>(edaAPI`/event-streams/${params.id ?? ''}/`);
+  const patchRequest = usePatchRequest();
+  const alertToaster = usePageAlertToaster();
 
+  const toggleEventStreamMode: (testMode: boolean, eventStream: EdaEventStream) => Promise<void> =
+    useCallback(
+      async (testMode, eventStream) => {
+        const alert: AlertProps = {
+          variant: 'success',
+          title: `${eventStream.name || ''} ${testMode ? t('switched to test mode') : t('switched to production mode')}.`,
+          timeout: 5000,
+        };
+        await patchRequest(
+          edaAPI`/event-streams/${eventStream?.id ? eventStream?.id.toString() : ''}/`,
+          {
+            test_mode: testMode,
+          }
+        )
+          .then(() => alertToaster.addAlert(alert))
+          .catch(() => {
+            alertToaster.addAlert({
+              variant: 'danger',
+              title: `${t('Failed to switch the mode for')} ${eventStream.name}`,
+              timeout: 5000,
+            });
+          });
+      },
+      [t, patchRequest, alertToaster]
+    );
   const deleteEventStreams = useDeleteEventStreams((deleted) => {
     if (deleted.length > 0) {
       pageNavigate(EdaRoute.EventStreams);
@@ -46,6 +75,22 @@ export function EventStreamPage() {
           pageNavigate(EdaRoute.EditEventStream, { params: { id: eventStream.id } }),
       },
       {
+        type: PageActionType.Button,
+        selection: PageActionSelection.Single,
+        icon: TaskIcon,
+        label: t('Switch to test mode'),
+        isHidden: (eventStream: EdaEventStream) => !!eventStream?.test_mode,
+        onClick: (eventStream: EdaEventStream) => toggleEventStreamMode(true, eventStream),
+      },
+      {
+        type: PageActionType.Button,
+        selection: PageActionSelection.Single,
+        icon: DatabaseIcon,
+        label: t('Switch to production mode'),
+        isHidden: (eventStream: EdaEventStream) => !eventStream?.test_mode,
+        onClick: (eventStream: EdaEventStream) => toggleEventStreamMode(false, eventStream),
+      },
+      {
         type: PageActionType.Seperator,
       },
       {
@@ -57,7 +102,7 @@ export function EventStreamPage() {
         isDanger: true,
       },
     ],
-    [deleteEventStreams, pageNavigate, t]
+    [deleteEventStreams, pageNavigate, t, toggleEventStreamMode]
   );
 
   const getPageUrl = useGetPageUrl();
