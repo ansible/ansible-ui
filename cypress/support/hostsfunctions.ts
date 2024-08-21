c7239cd5e5e917f3555e56f14404d803952f1149
James Talton <talton@gmail.com>
2024-08-21 12:51:17 -0400

    Select Dialog Fixes
    Signed-off-by: James Talton <talton@gmail.com>

--- cypress/support/hostsfunctions.ts
+++ cypress/support/hostsfunctions.ts
@@ -26,13 +26,14 @@ export function createAndCheckHost(host_type: string, inventory: string) {
   cy.getByDataCy('description').type('This is the description');
 
   if (host_type === 'stand_alone_host') {
-    cy.getByDataCy('inventory').click();
-    cy.contains('button', 'Browse').click();
-    cy.getModal().within(() => {
-      cy.filterTableBySingleSelect('name', inventory);
-      cy.get(`[data-cy="checkbox-column-cell"] input`).click();
-      cy.contains('button', 'Confirm').click();
-    });
+    cy.singleSelectByDataCy('inventory', inventory);
+    // cy.getByDataCy('inventory').click();
+    // cy.contains('button', 'Browse').click();
+    // cy.getModal().within(() => {
+    //   cy.filterTableBySingleSelect('name', inventory);
+    //   cy.get(`[data-cy="checkbox-column-cell"] input`).click();
+    //   cy.contains('button', 'Confirm').click();
+    // });
   }
 
   // after creation - verify data is currect
