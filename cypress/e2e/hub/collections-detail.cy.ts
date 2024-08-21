cb2cbf87d499ead8910b1178b3f5da3de4915e74
Zita Nemeckova <znemecko@redhat.com>
2024-08-21 16:40:33 +0200

    Skip test because it deletes wrong version of collection

--- cypress/e2e/hub/collections-detail.cy.ts
+++ cypress/e2e/hub/collections-detail.cy.ts
@@ -113,7 +113,7 @@ describe('Collections Details', () => {
     cy.deleteHubCollectionByName(collectionName);
   });
 
-  it('user can delete version from repository', () => {
+  it.skip('user can delete version from repository', () => {
     cy.uploadCollection(collectionName, namespace.name, '1.0.0');
     cy.uploadCollection(collectionName, namespace.name, '1.1.0');
     cy.approveCollection(collectionName, namespace.name, '1.0.0');
