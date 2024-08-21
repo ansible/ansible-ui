131684d922a473400971161c7468ab3f82b98afd
abakshi <abakshi@redhat.com>
2024-08-21 15:19:37 -0500

    add constant for downstream test

--- cypress/support/constants.ts
+++ cypress/support/constants.ts
@@ -2,6 +2,11 @@ import { EdaCredential } from '../../frontend/eda/interfaces/EdaCredential';
 import { EdaDecisionEnvironment } from '../../frontend/eda/interfaces/EdaDecisionEnvironment';
 import { EdaProject } from '../../frontend/eda/interfaces/EdaProject';
 import { EdaRulebookActivation } from '../../frontend/eda/interfaces/EdaRulebookActivation';
+import { Repository } from '../../frontend/hub/administration/repositories/Repository';
+import { HubNamespace } from '../../frontend/hub/namespaces/HubNamespace';
+import { HubRemote } from '../../frontend/hub/administration/remotes/Remotes';
+import { randomString } from '../../framework/utils/random-string';
+import { ContentTypeEnum } from '../../frontend/hub/interfaces/expanded/ContentType';
 
 export enum SERVER_NAME {
   AWX_SERVER = 'AWX Ansible Server',
@@ -55,5 +60,44 @@ export const user_team_access_tab_resources: AccessTabResource[] = [
     creation: () => cy.createEdaCredential() as Cypress.Chainable<ResourceObject>,
     deletion: (resourceObject) => cy.deleteEdaCredential(resourceObject as EdaCredential),
     role: 'Eda Credential Admin',
+  },
+];
+
+const testSignature: string = randomString(5, undefined, { isLowercase: true });
+function generateRemoteName(): string {
+  return `test-${testSignature}-remote-${randomString(5, undefined, { isLowercase: true })}`;
+}
+
+type ResourceObjectHub = HubNamespace | Repository | HubRemote;
+
+export interface HubResource {
+  name: string;
+  creation: (() => Cypress.Chainable<ResourceObjectHub>) | null;
+  deletion: ((resourceObjectHub: ResourceObjectHub) => Cypress.Chainable<void>) | null;
+  content_type: ContentTypeEnum;
+  permission: string;
+}
+
+export const hub_resources_roles_tab: HubResource[] = [
+  {
+    name: 'Repository',
+    creation: () => cy.createHubRepository() as Cypress.Chainable<ResourceObjectHub>,
+    deletion: (resourceObjectHub) => cy.deleteHubRepository(resourceObjectHub as Repository),
+    content_type: ContentTypeEnum.Repository,
+    permission: 'galaxy.view_ansiblerepository',
+  },
+  {
+    name: 'Remote',
+    creation: () => cy.createRemote(generateRemoteName()) as Cypress.Chainable<ResourceObjectHub>,
+    deletion: null,
+    content_type: ContentTypeEnum.CollectionRemote,
+    permission: 'galaxy.view_collectionremote',
+  },
+  {
+    name: 'Namespace',
+    creation: () => cy.createHubNamespace() as Cypress.Chainable<ResourceObjectHub>,
+    deletion: (resourceObject) => cy.deleteHubNamespace(resourceObject as HubNamespace),
+    content_type: ContentTypeEnum.Namespace,
+    permission: 'galaxy.view_namespace',
   },
 ];
