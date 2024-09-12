55d2069ca5263fa33e4461823b8cbae5eaa47f5b
Shaiah Emigh-Doyle <semighdo@redhat.com>
2024-09-12 11:04:06 -0400

    simplify logic for moving quickstarts to bottom of page

--- platform/overview/useManagedPlatformOverview.tsx
+++ platform/overview/useManagedPlatformOverview.tsx
@@ -6,10 +6,6 @@ import { useQuickStarts } from './quickstarts/useQuickStarts';
 
 type Resource = { id: string; name: string };
 
-function clearStorageKey() {
-  localStorage.removeItem('platform-dashboard');
-}
-
 export function useManagedPlatformOverview() {
   const hasAwx = useHasAwxService();
   const hasEda = useHasEdaService();
@@ -28,10 +24,10 @@ export function useManagedPlatformOverview() {
   );
 
   const resources: Resource[] = useMemo(() => {
-    const serviceResources: Resource[] = [];
+    const baseResources: Resource[] = [];
 
     if (hasAwx) {
-      serviceResources.push(
+      baseResources.push(
         { id: 'counts', name: t('Resource counts') },
         { id: 'job_activity', name: t('Job activity') },
         { id: 'recent_jobs', name: t('Recent jobs') },
@@ -41,56 +37,35 @@ export function useManagedPlatformOverview() {
     }
 
     if (hasEda) {
-      serviceResources.push(
+      baseResources.push(
         { id: 'recent-rulebook-activations', name: t('Recent rulebook activations') },
         { id: 'recent-rule-audits', name: t('Recent rule audits') },
         { id: 'recent-decision-environments', name: t('Recent decision environments') }
       );
     }
 
-    return serviceResources;
-  }, [t, hasAwx, hasEda]);
+    if (quickStarts.length > 0) {
+      baseResources.push({ id: 'quick-starts', name: t('Quick starts') });
+    }
 
-  const quickStartResources: Resource[] = useMemo(() => {
-    return quickStarts.length > 0 ? [{ id: 'quick-starts', name: t('Quick starts') }] : [];
-  }, [t, quickStarts]);
+    return baseResources;
+  }, [t, hasAwx, hasEda, quickStarts]);
 
-  const combinedResources = useMemo(() => {
-    return [...resources, ...quickStartResources];
-  }, [resources, quickStartResources]);
-
-  const sortedForModal = useMemo(() => {
-    const serviceResources = combinedResources.filter((item) => item.id !== 'quick-starts');
-    const quickStarts = combinedResources.filter((item) => item.id === 'quick-starts');
-    return [...serviceResources, ...quickStarts];
-  }, [combinedResources]);
-
-  const { openManageItems: originalOpenManageDashboard, managedItems: managedResources } =
+  const { openManageItems: openManageDashboard, managedItems: managedResources } =
     useManageItems<Resource>({
       id: 'platform-dashboard',
       title: t('Manage view'),
       description: t(
         'Hide or show the panels you want to see on the overview page by selecting or unselecting, respectively. The panels are ordered from top to bottom on the list. Use the draggable icon :: to re-order your view.'
       ),
-      items: sortedForModal,
+      items: resources,
       keyFn: (resources) => resources.id,
       columns,
       hideColumnHeaders: true,
     });
 
-  const sortedManagedResources = useMemo(() => {
-    const serviceResources = managedResources.filter((item) => item.id !== 'quick-starts');
-    const quickStarts = managedResources.filter((item) => item.id === 'quick-starts');
-    return [...serviceResources, ...quickStarts];
-  }, [managedResources]);
-
-  function openManageDashboard() {
-    clearStorageKey();
-    originalOpenManageDashboard();
-  }
-
   return {
     openManageDashboard,
-    managedResources: sortedManagedResources,
+    managedResources,
   };
 }
