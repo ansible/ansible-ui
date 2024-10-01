096fdcb4a3fc3e4e28d1cf49c952cadbe2dd0e8d
Keith Grant <keithjgrant@redhat.com>
2024-10-01 16:47:08 -0700

    fix auth mappings form layout

---
+++ platform/access/authenticators/components/steps/MapAttributesSubform.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useCallback } from 'react';
+import { Button } from '@patternfly/react-core';
+import { useFormContext, useFieldArray } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { PageFormSelect, PageFormTextInput } from '../../../../../framework';
+import { PageFormSection } from '../../../../../framework/PageForm/Utils/PageFormSection';
+import { MapFieldsGrid } from './MapFields';
+import { PlusCircleIcon, TrashIcon } from '@patternfly/react-icons';
+
+export function MapAttributesSubform(props: { mappingIndex: number }) {
+  const { mappingIndex } = props;
+  const { control } = useFormContext();
+  const {
+    fields: attributes,
+    append,
+    remove: deleteAttribute,
+  } = useFieldArray({
+    control,
+    name: `mappings.${mappingIndex}.attributes`,
+  });
+
+  const addAttribute = useCallback(() => {
+    append({
+      attribute: '',
+      comparison: 'contains',
+      value: '',
+    });
+  }, [append]);
+
+  useEffect(() => {
+    if (!attributes.length) {
+      addAttribute();
+    }
+  }, [attributes.length, addAttribute]);
+
+  return attributes.map((attribute, index) => (
+    <AttributeFields
+      key={attribute.id}
+      mapIndex={mappingIndex}
+      index={index}
+      addAttribute={index + 1 === attributes.length ? addAttribute : undefined}
+      deleteAttribute={attributes.length > 1 ? () => deleteAttribute(index) : undefined}
+    />
+  ));
+}
+
+interface AttributeFieldsProps {
+  mapIndex: number;
+  index: number;
+  addAttribute?: () => void;
+  deleteAttribute?: () => void;
+}
+export function AttributeFields(props: AttributeFieldsProps) {
+  const { mapIndex, index, deleteAttribute, addAttribute } = props;
+  const { t } = useTranslation();
+  // TODO: validate against duplicate criteria
+  return (
+    <PageFormSection singleColumn>
+      <MapFieldsGrid>
+        <PageFormTextInput
+          id={`mappings-${mapIndex}-attributes-${index}-attribute`}
+          name={`mappings.${mapIndex}.attributes.${index}.attribute`}
+          label={t('Attribute')}
+          isRequired
+          placeholder={t('Enter attribute')}
+        />
+        <PageFormSelect
+          id={`mappings-${mapIndex}-attributes-${index}-comparison`}
+          name={`mappings.${mapIndex}.attributes.${index}.comparison`}
+          label={t('Comparison')}
+          placeholderText={t('Select comparison')}
+          options={[
+            { value: 'contains', label: t('contains') },
+            { value: 'matches', label: t('matches') },
+            { value: 'ends_with', label: t('ends with') },
+            { value: 'equals', label: t('equals') },
+            { value: 'in', label: t('in') },
+          ]}
+          isRequired
+        />
+        <PageFormTextInput
+          id={`mappings-${mapIndex}-attributes-${index}-value`}
+          name={`mappings.${mapIndex}.attributes.${index}.value`}
+          label={t('Value')}
+          placeholder={t('Enter value')}
+          isRequired
+        />
+        {deleteAttribute ? (
+          <Button
+            icon={<TrashIcon />}
+            aria-label={t('Delete attribute')}
+            onClick={deleteAttribute}
+            style={{ gridColumn: 4 }}
+            variant="plain"
+          />
+        ) : null}
+        {addAttribute ? (
+          <Button
+            icon={<PlusCircleIcon />}
+            aria-label={t('Add attribute')}
+            onClick={addAttribute}
+            style={{ gridColumn: 5 }}
+            variant="plain"
+          />
+        ) : null}
+      </MapFieldsGrid>
+    </PageFormSection>
+  );
+}
--- platform/access/authenticators/components/steps/MapFields.tsx
+++ platform/access/authenticators/components/steps/MapFields.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useCallback } from 'react';
 import { Button, FormFieldGroup, FormFieldGroupHeader } from '@patternfly/react-core';
 import { TrashIcon } from '@patternfly/react-icons';
-import { useFormContext, useWatch, useFieldArray } from 'react-hook-form';
+import { useFormContext, useWatch } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { PageFormCheckbox, PageFormSelect, PageFormTextInput } from '../../../../../framework';
 import { PageFormCreatableSelect } from '../../../../../framework/PageForm/Inputs/PageFormCreatableSelect';
@@ -11,8 +10,29 @@ import { PageFormPlatformOrganizationNameSelect } from '../../../organizations/c
 import { PageFormPlatformTeamNameSelect } from '../../../organizations/components/PageFormPlatformTeamNameSelect';
 import { PageFormPlatformRoleNameSelect } from '../../../roles/components/PageFormPlatformRoleNameSelect';
 import type { AuthenticatorFormValues, AuthenticatorMapValues } from '../AuthenticatorForm';
+import { MapAttributesSubform } from './MapAttributesSubform';
 import { AuthenticatorMapType } from '../../../../interfaces/AuthenticatorMap';
 import { PlatformRole } from '../../../../interfaces/PlatformRole';
+import styled from 'styled-components';
+
+const FieldsWrapper = styled.div`
+  margin-right: 78px;
+
+  & > :not(:last-child) {
+    margin-bottom: var(--pf-v5-l-grid--m-gutter--GridGap);
+  }
+`;
+
+export const MapFieldsGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 24px 24px;
+  gap: var(--pf-v5-l-grid--m-gutter--GridGap);
+  align-items: end;
+
+  & > button {
+    justify-self: start;
+  }
+`;
 
 export function MapFields(props: {
   index: number;
@@ -84,194 +104,126 @@ export function MapFields(props: {
         />
       }
     >
-      <input
-        type="hidden"
-        {...register(`mappings.${index}.map_type`, { value: map.map_type })}
-        defaultValue={map.map_type}
-      />
-      <PageFormSection>
-        <PageFormTextInput
-          id={`mappings-${index}-name`}
-          name={`mappings.${index}.name`}
-          label={t('Name')}
-          isRequired
-          placeholder={t('Enter name')}
+      <FieldsWrapper>
+        <input
+          type="hidden"
+          {...register(`mappings.${index}.map_type`, { value: map.map_type })}
+          defaultValue={map.map_type}
         />
-        <PageFormSelect
-          id={`mappings-${index}-trigger`}
-          name={`mappings.${index}.trigger`}
-          label={t('Trigger')}
-          options={[
-            {
-              value: 'always',
-              label: t('Always'),
-            },
-            {
-              value: 'never',
-              label: t('Never'),
-            },
-            {
-              value: 'groups',
-              label: t('Groups'),
-            },
-            {
-              value: 'attributes',
-              label: t('Attributes'),
-            },
-          ]}
-          placeholderText={t('Select trigger')}
-          isRequired
-        />
-        <PageFormCheckbox name={`mappings.${index}.revoke`} label={t('Revoke')} />
-        <PageFormHidden watch={`mappings.${index}.trigger`} hidden={(value) => value !== 'groups'}>
-          <PageFormSelect
-            id={`mappings-${index}-groups-conditional`}
-            label={t('Operation')}
-            options={[
-              { value: 'or', label: t('or') },
-              { value: 'and', label: t('and') },
-            ]}
-            placeholderText={t('Select operation')}
-            name={`mappings.${index}.conditional`}
-            isRequired
-          />
-          <PageFormCreatableSelect
-            id={`mappings-${index}-groups-value`}
-            name={`mappings.${index}.groups_value`}
-            label={t('Groups')}
-            options={groupOptions}
-            isRequired
-            placeholderText={t('Select groups')}
-          />
-        </PageFormHidden>
-        <PageFormHidden
-          watch={`mappings.${index}.trigger`}
-          hidden={(value) => value !== 'attributes'}
-        >
-          <PageFormSelect
-            id={`mappings-${index}-attributes-conditional`}
-            name={`mappings.${index}.conditional`}
-            label={t('Operation')}
-            placeholderText={t('Select operation')}
-            options={[
-              { value: 'or', label: t('or') },
-              { value: 'and', label: t('and') },
-            ]}
-            isRequired
-          />
-          <AttributesSubform mappingIndex={index} />
-        </PageFormHidden>
-      </PageFormSection>
-      <PageFormSection>
-        <PageFormHidden
-          watch={`mappings.${index}.map_type`}
-          hidden={(value: string) => !['team', 'role'].includes(value)}
-        >
-          <PageFormPlatformTeamNameSelect
-            name={`mappings.${index}.team`}
-            isRequired={['team'].includes(mapType) || isTeamRoleSelected}
-          />
-        </PageFormHidden>
-        <PageFormHidden
-          watch={`mappings.${index}.map_type`}
-          hidden={(value: string) => !['team', 'organization', 'role'].includes(value)}
-        >
-          <PageFormPlatformOrganizationNameSelect
-            name={`mappings.${index}.organization`}
-            isRequired={
-              ['team', 'organization'].includes(mapType) || isOrgRoleSelected || isTeamRoleSelected
-            }
-          />
-          <PageFormPlatformRoleNameSelect
-            name={`mappings.${index}.role`}
-            contentType={roleContentType}
-            isRequired
-          />
-        </PageFormHidden>
-      </PageFormSection>
+        <PageFormSection singleColumn>
+          <MapFieldsGrid>
+            <PageFormTextInput
+              id={`mappings-${index}-name`}
+              name={`mappings.${index}.name`}
+              label={t('Name')}
+              isRequired
+              placeholder={t('Enter name')}
+            />
+            <PageFormSelect
+              id={`mappings-${index}-trigger`}
+              name={`mappings.${index}.trigger`}
+              label={t('Trigger')}
+              options={[
+                {
+                  value: 'always',
+                  label: t('Always'),
+                },
+                {
+                  value: 'never',
+                  label: t('Never'),
+                },
+                {
+                  value: 'groups',
+                  label: t('Groups'),
+                },
+                {
+                  value: 'attributes',
+                  label: t('Attributes'),
+                },
+              ]}
+              placeholderText={t('Select trigger')}
+              isRequired
+            />
+            <PageFormCheckbox name={`mappings.${index}.revoke`} label={t('Revoke')} />
+          </MapFieldsGrid>
+          <PageFormHidden
+            watch={`mappings.${index}.trigger`}
+            hidden={(value) => value !== 'groups'}
+          >
+            <MapFieldsGrid>
+              <PageFormSelect
+                id={`mappings-${index}-groups-conditional`}
+                label={t('Operation')}
+                options={[
+                  { value: 'or', label: t('or') },
+                  { value: 'and', label: t('and') },
+                ]}
+                placeholderText={t('Select operation')}
+                name={`mappings.${index}.conditional`}
+                isRequired
+              />
+              <PageFormCreatableSelect
+                id={`mappings-${index}-groups-value`}
+                name={`mappings.${index}.groups_value`}
+                label={t('Groups')}
+                options={groupOptions}
+                isRequired
+                placeholderText={t('Select groups')}
+              />
+            </MapFieldsGrid>
+          </PageFormHidden>
+          <PageFormHidden
+            watch={`mappings.${index}.trigger`}
+            hidden={(value) => value !== 'attributes'}
+          >
+            <MapFieldsGrid>
+              <PageFormSelect
+                id={`mappings-${index}-attributes-conditional`}
+                name={`mappings.${index}.conditional`}
+                label={t('Operation')}
+                placeholderText={t('Select operation')}
+                options={[
+                  { value: 'or', label: t('or') },
+                  { value: 'and', label: t('and') },
+                ]}
+                isRequired
+              />
+            </MapFieldsGrid>
+            <MapAttributesSubform mappingIndex={index} />
+          </PageFormHidden>
+        </PageFormSection>
+        <PageFormSection singleColumn>
+          <MapFieldsGrid>
+            <PageFormHidden
+              watch={`mappings.${index}.map_type`}
+              hidden={(value: string) => !['team', 'role'].includes(value)}
+            >
+              <PageFormPlatformTeamNameSelect
+                name={`mappings.${index}.team`}
+                isRequired={['team'].includes(mapType) || isTeamRoleSelected}
+              />
+            </PageFormHidden>
+            <PageFormHidden
+              watch={`mappings.${index}.map_type`}
+              hidden={(value: string) => !['team', 'organization', 'role'].includes(value)}
+            >
+              <PageFormPlatformOrganizationNameSelect
+                name={`mappings.${index}.organization`}
+                isRequired={
+                  ['team', 'organization'].includes(mapType) ||
+                  isOrgRoleSelected ||
+                  isTeamRoleSelected
+                }
+              />
+              <PageFormPlatformRoleNameSelect
+                name={`mappings.${index}.role`}
+                contentType={roleContentType}
+                isRequired
+              />
+            </PageFormHidden>
+          </MapFieldsGrid>
+        </PageFormSection>
+      </FieldsWrapper>
     </FormFieldGroup>
-  );
-}
-
-function AttributesSubform(props: { mappingIndex: number }) {
-  const { mappingIndex } = props;
-  const { control } = useFormContext();
-  const {
-    fields: attributes,
-    append,
-    remove: deleteAttribute,
-  } = useFieldArray({
-    control,
-    name: `mappings.${mappingIndex}.attributes`,
-  });
-
-  const addAttribute = useCallback(() => {
-    append({
-      attribute: '',
-      comparison: 'contains',
-      value: '',
-    });
-  }, [append]);
-
-  useEffect(() => {
-    if (!attributes.length) {
-      addAttribute();
-    }
-  }, [attributes.length, addAttribute]);
-
-  return attributes.map((attribute, index) => (
-    <AttributeFields
-      key={attribute.id}
-      mapIndex={mappingIndex}
-      index={index}
-      addAttribute={index + 1 === attributes.length ? addAttribute : undefined}
-      deleteAttribute={attributes.length > 1 ? () => deleteAttribute(index) : undefined}
-    />
-  ));
-}
-
-interface AttributeFieldsProps {
-  mapIndex: number;
-  index: number;
-  addAttribute?: () => void;
-  deleteAttribute?: () => void;
-}
-function AttributeFields(props: AttributeFieldsProps) {
-  const { mapIndex, index, deleteAttribute, addAttribute } = props;
-  const { t } = useTranslation();
-  // TODO: validate against duplicate criteria
-  return (
-    <PageFormSection>
-      <PageFormTextInput
-        id={`mappings-${mapIndex}-attributes-${index}-attribute`}
-        name={`mappings.${mapIndex}.attributes.${index}.attribute`}
-        label={t('Attribute')}
-        isRequired
-        placeholder={t('Enter attribute')}
-      />
-      <PageFormSelect
-        id={`mappings-${mapIndex}-attributes-${index}-comparison`}
-        name={`mappings.${mapIndex}.attributes.${index}.comparison`}
-        label={t('Comparison')}
-        placeholderText={t('Select comparison')}
-        options={[
-          { value: 'contains', label: t('contains') },
-          { value: 'matches', label: t('matches') },
-          { value: 'ends_with', label: t('ends with') },
-          { value: 'equals', label: t('equals') },
-          { value: 'in', label: t('in') },
-        ]}
-        isRequired
-      />
-      <PageFormTextInput
-        id={`mappings-${mapIndex}-attributes-${index}-value`}
-        name={`mappings.${mapIndex}.attributes.${index}.value`}
-        label={t('Value')}
-        placeholder={t('Enter value')}
-        isRequired
-      />
-      {deleteAttribute ? <Button onClick={deleteAttribute}>delete</Button> : <div />}
-      {addAttribute ? <Button onClick={addAttribute}>+</Button> : null}
-    </PageFormSection>
   );
 }
