b49e5cdc074b698a22e39a3bdaffdab689543459
Apurva <abakshi@redhat.com>
2024-10-22 12:08:23 -0500

    update users for upgrades testing (#816)
    Signed-off-by: James Talton <talton@gmail.com>
    Co-authored-by: James Talton <jtalton@redhat.com>
    Co-authored-by: nixocio <nixocio@gmail.com>

--- cypress/support/constants.ts
+++ cypress/support/constants.ts
@@ -90,187 +90,121 @@ export const hub_resources_roles_tab: HubResource[] = [
   },
 ];
 
+export const upgrade_users_list = [
+  'controller_ui_user_1',
+  'controller_ui_user_2',
+  'controller_ui_user_3',
+  'controller_ui_user_4',
+  'controller_ui_user_5',
+  'controller_ui_user_6',
+  'controller_ui_user_7',
+  'controller_ui_user_8',
+  'controller_ui_user_9',
+  'controller_ui_user_10',
+  'hub_ui_user_1',
+  'hub_ui_user_2',
+  'hub_ui_user_3',
+  'hub_ui_user_4',
+  'hub_ui_user_5',
+  'hub_ui_user_6',
+  'hub_ui_user_7',
+  'hub_ui_user_8',
+  'hub_ui_user_9',
+  'hub_ui_user_10',
+  'hub_keycloak_ui_user_1',
+  'hub_keycloak_ui_user_2',
+  'hub_keycloak_ui_user_3',
+  'ctlr_oidc_ui_user_1',
+  'ctlr_oidc_ui_user_2',
+  'ctlr_oidc_ui_user_3',
+  'ctlr_saml_ui_user_1',
+  'ctlr_saml_ui_user_2',
+  'ctlr_saml_ui_user_3',
+];
+
+export enum passwords {
+  local_user_password = '12345678pw',
+  sso_user_password = 'unpriv123',
+  ldap_user_password = 'Th1sP4ssd',
+}
+
 export enum UpgradeUserType {
-  eda = 'eda',
   hubKeycloak = 'hubKeycloak',
   hubLegacy = 'hubLegacy',
   hubLdap = 'hubLdap',
   controllerLdap = 'controllerLdap',
-  controllerSSO = 'controllerSSO',
+  controllerOIDC = 'controllerOIDC',
+  controllerSAML = 'controllerSAML',
   controllerLegacy = 'controllerLegacy',
 }
 
 export const usersForMigration: {
   [key in UpgradeUserType]: { username: string; password: string }[];
 } = {
-  eda: [
+  controllerLegacy: upgrade_users_list
+    .filter((username) => username.includes('controller_ui_user_'))
+    .map((username) => ({
+      username,
+      password: passwords.local_user_password,
+    })),
+  hubLegacy: upgrade_users_list
+    .filter((username) => username.includes('hub_ui_user_'))
+    .map((username) => ({
+      username,
+      password: passwords.local_user_password,
+    })),
+  controllerOIDC: upgrade_users_list
+    .filter((username) => username.includes('ctlr_oidc_ui_user_'))
+    .map((username) => ({
+      username,
+      password: passwords.sso_user_password,
+    })),
+  controllerSAML: upgrade_users_list
+    .filter((username) => username.includes('ctlr_saml_ui_user_'))
+    .map((username) => ({
+      username,
+      password: passwords.sso_user_password,
+    })),
+  hubKeycloak: upgrade_users_list
+    .filter((username) => username.includes('hub_keycloak_ui_user_'))
+    .map((username) => ({
+      username,
+      password: passwords.sso_user_password,
+    })),
+  controllerLdap: [
     {
-      username: 'eda_test_user_1',
-      password: 'ps12345678',
+      username: 'tower_all',
+      password: passwords.ldap_user_password,
     },
     {
-      username: 'eda_test_user_2',
-      password: 'ps12345678',
+      username: 'tower_1',
+      password: passwords.ldap_user_password,
     },
     {
-      username: 'eda_test_user_3',
-      password: 'ps12345678',
+      username: 'saml_user',
+      password: passwords.ldap_user_password,
     },
     {
-      username: 'eda_test_user_4',
-      password: 'ps12345678',
-    },
-    {
-      username: 'eda_test_user_5',
-      password: 'ps12345678',
-    },
-    {
-      username: 'eda_test_user_6',
-      password: 'ps12345678',
-    },
-    {
-      username: 'eda_test_user_7',
-      password: 'ps12345678',
-    },
-    {
-      username: 'eda_test_user_8',
-      password: 'ps12345678',
-    },
-    {
-      username: 'eda_test_user_9',
-      password: 'ps12345678',
-    },
-    {
-      username: 'eda_test_user_10',
-      password: 'ps12345678',
-    },
-  ],
-  hubKeycloak: [
-    // {
-    //   username: 'hub_ui_admin',
-    //   password: 'Th1sP4ssd',
-    // },
-    // {
-    //   username: 'hub_ui_user',
-    //   password: 'Th1sP4ssd',
-    // },
-  ],
-  hubLegacy: [
-    {
-      username: 'hub_ui_user_1',
-      password: '12345678pw',
-    },
-    {
-      username: 'hub_ui_user_2',
-      password: '12345678pw',
-    },
-    {
-      username: 'hub_ui_user_3',
-      password: '12345678pw',
-    },
-    {
-      username: 'hub_ui_user_4',
-      password: '12345678pw',
-    },
-    {
-      username: 'hub_ui_user_5',
-      password: '12345678pw',
-    },
-    {
-      username: 'hub_ui_user_6',
-      password: '12345678pw',
-    },
-    {
-      username: 'hub_ui_user_7',
-      password: '12345678pw',
-    },
-    {
-      username: 'hub_ui_user_8',
-      password: '12345678pw',
-    },
-    {
-      username: 'hub_ui_user_9',
-      password: '12345678pw',
-    },
-    {
-      username: 'hub_ui_user_10',
-      password: '12345678pw',
+      username: 'gbelcher',
+      password: passwords.ldap_user_password,
     },
   ],
   hubLdap: [
     {
-      username: 'awong',
-      password: 'Th1sP4ssd',
+      username: 'lobelcher',
+      password: passwords.ldap_user_password,
     },
     {
       username: 'hfarnsworth',
-      password: 'Th1sP4ssd',
-    },
-    {
-      username: 'lobelcher',
-      password: 'Th1sP4ssd',
+      password: passwords.ldap_user_password,
     },
     {
       username: 'tleela',
-      password: 'Th1sP4ssd',
-    },
-  ],
-  controllerLdap: [
-    {
-      username: 'tower_1',
-      password: 'Th1sP4ssd',
+      password: passwords.ldap_user_password,
     },
     {
-      username: 'hfarnsworth',
-      password: 'Th1sP4ssd',
-    },
-    {
-      username: 'lobelcher',
-      password: 'Th1sP4ssd',
-    },
-  ],
-  // controller sso
-  controllerSSO: [],
-  controllerLegacy: [
-    {
-      username: 'controller_ui_user_1',
-      password: '12345678pw',
-    },
-    {
-      username: 'controller_ui_user_2',
-      password: '12345678pw',
-    },
-    {
-      username: 'controller_ui_user_3',
-      password: '12345678pw',
-    },
-    {
-      username: 'controller_ui_user_4',
-      password: '12345678pw',
-    },
-    {
-      username: 'controller_ui_user_5',
-      password: '12345678pw',
-    },
-    {
-      username: 'controller_ui_user_6',
-      password: '12345678pw',
-    },
-    {
-      username: 'controller_ui_user_7',
-      password: '12345678pw',
-    },
-    {
-      username: 'controller_ui_user_8',
-      password: '12345678pw',
-    },
-    {
-      username: 'controller_ui_user_9',
-      password: '12345678pw',
-    },
-    {
-      username: 'controller_ui_user_10',
-      password: '12345678pw',
+      username: 'awong',
+      password: passwords.ldap_user_password,
     },
   ],
 };
