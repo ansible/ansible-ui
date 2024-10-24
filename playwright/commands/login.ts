022336178bcca603addc869cdce6565c5632e17a
James Talton <jtalton@redhat.com>
2024-10-24 15:34:23 -0400

    playwirght server url support (#863)
    Signed-off-by: James Talton <talton@gmail.com>

--- playwright/commands/login.ts
+++ playwright/commands/login.ts
@@ -1,11 +1,14 @@
 import { Page, expect } from '@playwright/test';
 
+export const platformUrl = process.env.PLATFORM_UI ?? 'https://localhost:4100';
+export const platformURL = new URL(platformUrl);
+
 /**
  * Logs into AWX.
  */
 export async function login(page: Page) {
   // Go to the AWX login page
-  await page.goto('https://localhost:4100');
+  await page.goto(process.env.PLATFORM_UI ?? platformUrl);
 
   // Verify we are on the AWX page
   await expect(page).toHaveTitle(/Ansible Automation Platform/);
