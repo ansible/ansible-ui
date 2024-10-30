93633c328ba65448f22aa7c14c8f4bacf572cf89
kialam <digitalanime@gmail.com>
2024-10-30 09:23:17 -0700

    Playwright add execution environments tests (#869)

---
+++ playwright/tests/execution-environment.spec.ts
@@ -0,0 +1,14 @@
+import { test } from '@playwright/test';
+import { setupAfter, setupBefore } from '../commands/setup';
+import {
+  createExecutionEnvironment,
+  deleteExecutionEnvironment,
+} from './execution-environment-utils';
+
+test.beforeEach(setupBefore);
+test.afterEach(setupAfter);
+
+test('create and delete an execution environment', async ({ page }) => {
+  const executionEnvName = await createExecutionEnvironment(page);
+  await deleteExecutionEnvironment(executionEnvName, page);
+});
