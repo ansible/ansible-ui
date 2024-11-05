a78d2caeb7916307fb7e3f0b52d4d7ef4a669dc0
Kersom <9053044+nixocio@users.noreply.github.com>
2024-11-05 15:32:59 -0500

    [AAP-35131] Fix remote-registries test (#893)

--- frontend/hub/administration/remote-registries/RemoteRegistries.tsx
+++ frontend/hub/administration/remote-registries/RemoteRegistries.tsx
@@ -56,6 +56,7 @@ export function RemoteRegistries() {
               icon={<PlusCircleIcon />}
               variant={ButtonVariant.primary}
               href={getPageUrl(HubRoute.CreateRemoteRegistry)}
+              data-cy="create-remote-registry"
             >
               {t('Create remote registry')}
             </ButtonLink>
