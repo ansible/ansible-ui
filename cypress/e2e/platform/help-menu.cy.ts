3465fdbf0bd17b3071659e277b36f96fa6c57368
Kersom <9053044+nixocio@users.noreply.github.com>
2024-11-06 15:24:30 -0500

    Fix help-menu-cy.ts (#897)

--- cypress/e2e/platform/help-menu.cy.ts
+++ cypress/e2e/platform/help-menu.cy.ts
@@ -3,24 +3,23 @@ describe('Platform Header Toolbar - Help Menu', () => {
     cy.visit('/');
 
     // Click on help-menu
-    cy.get('[data-cy="help-menu"]').click();
-
+    cy.get('#help-menu-menu-toggle').click();
     //Check the docs link
     cy.checkAnchorLinks('Documentation');
-    cy.get('[data-cy="masthead-documentation"]')
-      .should('have.attr', 'target', '_blank')
-      .should('have.attr', 'href')
-      .and(
-        'equal',
-        'https://access.redhat.com/documentation/en-us/red_hat_ansible_automation_platform'
-      );
-
+    cy.get('[data-cy="masthead-documentation"]').within(() => {
+      cy.get('a')
+        .should('have.attr', 'href')
+        .and(
+          'include',
+          'https://access.redhat.com/documentation/en-us/red_hat_ansible_automation_platform'
+        );
+    });
     // Check the quick starts link
     cy.get('[data-cy="masthead-quickstarts"]').click();
     cy.url().should('include', '/quickstarts');
 
     // Click on About to open the modal
-    cy.get('[data-cy="help-menu"]').click();
+    cy.get('#help-menu-menu-toggle').click();
     cy.get('[data-cy="masthead-about"]').click();
 
     cy.get('.pf-v5-c-about-modal-box').within(() => {
