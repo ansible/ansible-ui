8662c8275e131fe09f4e346e2cee3e81ae947948
James Talton <jtalton@redhat.com>
2024-11-11 20:52:00 -0500

    Run Playwright Github Action (#909)
    Signed-off-by: James Talton <talton@gmail.com>

--- playwright/commands/login.ts
+++ playwright/commands/login.ts
@@ -1,6 +1,8 @@
 import { Page, expect } from '@playwright/test';
 
-export const platformUI = process.env.PLATFORM_UI ?? 'https://localhost:4100';
+export const platformUI = process.env.PLATFORM_UI
+  ? process.env.PLATFORM_UI
+  : 'https://localhost:4100';
 export const platformURL = new URL(platformUI);
 
 /**
