d8bfaf1f92b5d85ce323223c0f52dcc0d7c2b76a
lgalis <lgalis@users.noreply.github.com>
2024-11-20 13:37:38 -0500

    [AAP-323707] Add the create button  on the Credentials list tab for a Credential Type (#944)

--- frontend/eda/access/credential-types/CredentialTypePage/CredentialTypeCredentials.tsx
+++ frontend/eda/access/credential-types/CredentialTypePage/CredentialTypeCredentials.tsx
@@ -1,8 +1,9 @@
-import { CubesIcon } from '@patternfly/react-icons';
+import { CubesIcon, PlusCircleIcon } from '@patternfly/react-icons';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 
-import { PageLayout, PageTable } from '@ansible/ansible-ui-framework';
+import { PageLayout, PageTable, useGetPageUrl } from '@ansible/ansible-ui-framework';
+import { useOptions } from '@ansible/common-ui/crud/useOptions';
 import { edaAPI } from '../../../common/eda-utils';
 import { useEdaView } from '../../../common/useEventDrivenView';
 import { EdaCredential } from '../../../interfaces/EdaCredential';
@@ -10,11 +11,16 @@ import { useCredentialActions } from '../../credentials/hooks/useCredentialActio
 import { useCredentialsActions } from '../../credentials/hooks/useCredentialsActions';
 import { useCredentialTypeCredentialsColumns } from '../hooks/useCredentialTypeCredentialsColumns';
 import { useCredentialTypeCredentialsFilters } from '../hooks/useCredentialTypeCredentialsFilters';
+import { PageTableEmptyState } from '../../../../../framework/PageTable/PageTableEmptyState';
+import { ButtonLink } from '../../../../../framework/components/ButtonLink';
+import { ButtonVariant } from '@patternfly/react-core';
+import { EdaRoute } from '../../../main/EdaRoutes';
+import { ActionsResponse, OptionsResponse } from '../../../interfaces/OptionsResponse';
 
 export function CredentialTypeCredentials() {
   const params = useParams<{ id: string }>();
   const { t } = useTranslation();
-
+  const getPageUrl = useGetPageUrl();
   const toolbarFilters = useCredentialTypeCredentialsFilters();
   const tableColumns = useCredentialTypeCredentialsColumns();
   const view = useEdaView<EdaCredential>({
@@ -24,7 +30,32 @@ export function CredentialTypeCredentials() {
     tableColumns,
   });
   const toolbarActions = useCredentialsActions(view);
+  const { data } = useOptions<OptionsResponse<ActionsResponse>>(edaAPI`/eda-credentials/`);
+  const canCreateCredential = Boolean(data && data.actions && data.actions['POST']);
   const rowActions = useCredentialActions(view);
+  const noCredentialsState = (
+    <PageTableEmptyState
+      title={t('There are currently no credentials of this type created for your organization.')}
+      description={t('Please create a credential by using the button below.')}
+    >
+      <ButtonLink
+        icon={<PlusCircleIcon />}
+        variant={ButtonVariant.primary}
+        href={getPageUrl(EdaRoute.CreateCredential)}
+      >
+        {t('Create credential')}
+      </ButtonLink>
+    </PageTableEmptyState>
+  );
+  const noRightsState = (
+    <PageTableEmptyState
+      icon={CubesIcon}
+      title={t('You do not have permission to create a credential.')}
+      description={t(
+        'Please contact your organization administrator if there is an issue with your access.'
+      )}
+    />
+  );
   return (
     <PageLayout>
       <PageTable
@@ -33,9 +64,7 @@ export function CredentialTypeCredentials() {
         toolbarFilters={toolbarFilters}
         rowActions={rowActions}
         errorStateTitle={t('Error loading credentials for this type')}
-        emptyStateTitle={t('No credentials for this type')}
-        emptyStateIcon={CubesIcon}
-        emptyStateDescription={t('No credentials for this type')}
+        emptyState={canCreateCredential ? noCredentialsState : noRightsState}
         {...view}
         defaultSubtitle={t('Credentials')}
       />
