5c0ec3a6a2ac69eae2414e930e8cf018f0ed3b43
prat98 <112966709+prat98@users.noreply.github.com>
2025-01-24 12:00:10 -0500

    [AAP-38320] Remove Deprecated PF Components in Analytics (#1127)

--- frontend/awx/analytics/components/Toolbar/Groups/index.ts
+++
@@ -1,4 +0,0 @@
-export { FilterCategoriesGroup } from './FiltersCategoriesGroup';
-export { QuickDateGroup } from './QuickDateGroup';
-export { SettingsPanel } from './SettingsPanel';
-export { SortByGroup } from './SortByGroup';
