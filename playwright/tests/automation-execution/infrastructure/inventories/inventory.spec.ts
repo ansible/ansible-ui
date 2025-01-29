ee9eb61c37bade3efda59583727f170a7447977f
prat98 <112966709+prat98@users.noreply.github.com>
2025-01-29 08:26:51 -0500

    [AAP-39477] Add test scaffolds and update helper function (#1142)

--- playwright/tests/automation-execution/infrastructure/inventories/inventory.spec.ts
+++ playwright/tests/automation-execution/infrastructure/inventories/inventory.spec.ts
@@ -9,3 +9,48 @@ test('inventory - create and delete', async ({ page }) => {
   const inventoryName = await createInventory({}, page);
   await deleteInventory(inventoryName, page);
 });
+
+//Vidya
+test.skip('inventory - can create an inventory, assert info on details page, and delete inventory', async () => {
+  //to-do
+});
+
+//Vidya
+test.skip('inventory - can edit an inventory from the list view and assert info on details page', async () => {
+  //to-do
+});
+
+//Vidya
+test.skip('inventory - can edit an inventory from the details view and assert info on details page', async () => {
+  //to-do
+});
+
+//Kersom
+test.skip('inventory - can copy an inventory on the details view and assert that the copy has been successful', async () => {
+  //to-do
+});
+
+//Kersom
+test.skip('inventory - can copy an inventory on the list view and assert that the copy has been successful', async () => {
+  //to-do
+});
+
+//Kersom
+test.skip('inventory - can delete an inventory from the inventory list row item', async () => {
+  //to-do
+});
+
+//Pratyush
+test.skip('inventory - can delete an inventory from the inventory list toolbar', async () => {
+  //to-do
+});
+
+//Pratyush
+test.skip('inventory - can bulk delete inventories from the list view and verify deletion', async () => {
+  //to-do
+});
+
+//Pratyush
+test.skip('inventory - can create, edit a smart inventory, assert info on details page, and delete inventory', async () => {
+  //to-do
+});
