dcb9cb60857f46f6ca0ab9209bd4d0d5fc64600c
James Talton <jtalton@redhat.com>
2025-02-04 16:31:04 -0500

    Cleanup framework test that was pulling in AWX specific resource (#1160)
    Signed-off-by: James Talton <talton@gmail.com>

--- framework/PageActions/PageActionSwitch.cy.tsx
+++ framework/PageActions/PageActionSwitch.cy.tsx
@@ -1,6 +1,4 @@
 import { t } from 'i18next';
-import instance from '../../cypress/fixtures/instance.json';
-import { Instance } from '../../frontend/awx/interfaces/Instance';
 import { IPageActionSwitchSingle, PageActionSelection, PageActionType } from './PageAction';
 import { PageActionSwitch } from './PageActionSwitch';
 
@@ -8,79 +6,75 @@ describe('PageActionSwitch', () => {
   const pageActionSwitchFlag = true;
 
   it('shows switch with the correct initial state', () => {
-    const action: IPageActionSwitchSingle<Instance> = {
+    const resource: { enabled: boolean } = { enabled: true };
+    const action: IPageActionSwitchSingle<{ enabled: boolean }> = {
       type: PageActionType.Switch,
       onToggle: () => {
         return !pageActionSwitchFlag;
       },
-      isSwitchOn: () => instance?.enabled,
+      isSwitchOn: (resource) => resource?.enabled,
       isDisabled: () => undefined,
       ariaLabel: (isEnabled: boolean) => (isEnabled ? t('Enabled') : t('Disabled')),
       selection: PageActionSelection.Single,
       label: '',
     };
-    cy.mount(<PageActionSwitch action={action} selectedItem={instance as unknown as Instance} />);
+    cy.mount(<PageActionSwitch action={action} selectedItem={resource} />);
     cy.getByDataCy('toggle-switch').click();
     cy.get('[data-cy="toggle-switch"]').within(() => {
-      cy.get('input[type="checkbox"]').should('have.prop', 'checked', instance.enabled);
+      cy.get('input[type="checkbox"]').should('have.prop', 'checked', resource.enabled);
     });
   });
 
   it('displays enabled tooltip content', () => {
-    const action: IPageActionSwitchSingle<Instance> = {
+    const resource: { enabled: boolean } = { enabled: true };
+    const action: IPageActionSwitchSingle<{ enabled: boolean }> = {
       type: PageActionType.Switch,
       onToggle: () => {
         return !pageActionSwitchFlag;
       },
-      isSwitchOn: () => instance?.enabled,
+      isSwitchOn: (resource) => resource?.enabled,
       isDisabled: () => undefined,
       ariaLabel: (isEnabled: boolean) => (isEnabled ? t('Enabled') : t('Disabled')),
       selection: PageActionSelection.Single,
       label: 'instance enabled',
     };
-    cy.mount(
-      <PageActionSwitch
-        action={action}
-        iconOnly={true}
-        selectedItem={instance as unknown as Instance}
-      />
-    );
+    cy.mount(<PageActionSwitch action={action} iconOnly={true} selectedItem={resource} />);
     cy.get('div.pf-v5-c-switch__toggle-icon').trigger('mouseenter');
     cy.hasTooltip('instance enabled');
   });
 
   it('switch is disabled when isDisabled is not undefined', () => {
-    const action: IPageActionSwitchSingle<Instance> = {
+    const resource: { enabled: boolean } = { enabled: true };
+    const action: IPageActionSwitchSingle<{ enabled: boolean }> = {
       type: PageActionType.Switch,
       onToggle: () => {
         return !pageActionSwitchFlag;
       },
-      isSwitchOn: () => instance?.enabled,
+      isSwitchOn: (resource) => resource?.enabled,
       isDisabled: () => 'instance disabled',
       ariaLabel: (isEnabled: boolean) => (isEnabled ? t('Enabled') : t('Disabled')),
       selection: PageActionSelection.Single,
       label: 'switch disabled',
     };
-    cy.mount(
-      <PageActionSwitch action={{ ...action }} selectedItem={instance as unknown as Instance} />
-    );
+    cy.mount(<PageActionSwitch action={{ ...action }} selectedItem={resource} />);
     cy.get('input[type="checkbox"]').should('be.disabled');
   });
 
   it('toggles switch state when clicked', () => {
-    const action: IPageActionSwitchSingle<Instance> = {
+    const resource: { enabled: boolean } = { enabled: true };
+    const action: IPageActionSwitchSingle<{ enabled: boolean }> = {
       type: PageActionType.Switch,
       onToggle: () => {
         return !pageActionSwitchFlag;
       },
-      isSwitchOn: () => instance?.enabled,
+      isSwitchOn: (resource) => resource?.enabled,
       isDisabled: () => undefined,
       ariaLabel: (isEnabled: boolean) => (isEnabled ? t('Enabled') : t('Disabled')),
       selection: PageActionSelection.Single,
       labelOff: 'instance disabled',
       label: 'instance enabled',
     };
-    cy.mount(<PageActionSwitch action={action} selectedItem={instance as unknown as Instance} />);
+    cy.mount(<PageActionSwitch action={action} selectedItem={resource} />);
     cy.get('span.pf-v5-c-switch__toggle').click();
     cy.contains('instance enabled');
     cy.get('span.pf-v5-c-switch__toggle').click();
@@ -88,37 +82,39 @@ describe('PageActionSwitch', () => {
   });
 
   it('displays tooltip when isDisabled is undefined', () => {
-    const action: IPageActionSwitchSingle<Instance> = {
+    const resource: { enabled: boolean } = { enabled: true };
+    const action: IPageActionSwitchSingle<{ enabled: boolean }> = {
       type: PageActionType.Switch,
       onToggle: () => {
         return !pageActionSwitchFlag;
       },
-      isSwitchOn: () => instance?.enabled,
+      isSwitchOn: (resource) => resource?.enabled,
       isDisabled: () => undefined,
       ariaLabel: (isEnabled: boolean) => (isEnabled ? t('Enabled') : t('Disabled')),
       selection: PageActionSelection.Single,
       tooltip: 'Tooltip message',
       label: '',
     };
-    cy.mount(<PageActionSwitch action={action} selectedItem={instance as unknown as Instance} />);
+    cy.mount(<PageActionSwitch action={action} selectedItem={resource} />);
     cy.get('span.pf-v5-c-switch__toggle').trigger('mouseenter');
     cy.hasTooltip('Tooltip message');
   });
 
   it('displays isDisabled text instead of tooltip prop text', () => {
-    const action: IPageActionSwitchSingle<Instance> = {
+    const resource: { enabled: boolean } = { enabled: true };
+    const action: IPageActionSwitchSingle<{ enabled: boolean }> = {
       type: PageActionType.Switch,
       onToggle: () => {
         return !pageActionSwitchFlag;
       },
-      isSwitchOn: () => instance?.enabled,
+      isSwitchOn: (resource) => resource?.enabled,
       isDisabled: () => 'This toggle is disabled',
       ariaLabel: (isEnabled: boolean) => (isEnabled ? t('Enabled') : t('Disabled')),
       selection: PageActionSelection.Single,
       tooltip: 'Tooltip message',
       label: '',
     };
-    cy.mount(<PageActionSwitch action={action} selectedItem={instance as unknown as Instance} />);
+    cy.mount(<PageActionSwitch action={action} selectedItem={resource} />);
     cy.get('span.pf-v5-c-switch__toggle').trigger('mouseenter');
     cy.hasTooltip('This toggle is disabled');
   });
