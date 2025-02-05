0cfc39dec20ef31a7e7fb55cd16bc84fda0395b5
Apurva <abakshi@redhat.com>
2025-02-05 12:18:48 -0600

    jt playwright create and delete (#1150)
    Co-authored-by: Keith Grant <keithjgrant@redhat.com>

--- playwright/tests/automation-execution/templates/job-template-utils.ts
+++ playwright/tests/automation-execution/templates/job-template-utils.ts
@@ -3,20 +3,39 @@ import { clickTableRowWithFilter } from '../../../commands/clickTableRow';
 import { createE2EName } from '../../../commands/createE2EName';
 import { navigateTo } from '../../../commands/navigateTo';
 import { selectTableFilter } from '../../../commands/selectTableFilter';
+import { clearTableFilters } from '../../../commands/clearTableFilters';
+import { filterTableBySelect } from '../../../commands/filterTableBySelect';
 
 export async function createJobTemplate(
-  options: { name?: string; inventoryName?: string; projectName?: string; labels?: string[] },
+  options: {
+    name?: string;
+    inventoryName?: string;
+    projectName?: string;
+    labels?: string[];
+    PromptOnLaunch?: boolean;
+  },
   page: Page
 ) {
   const jobTemplateName = options.name ?? createE2EName('job-template');
+  const jobTemplateDescription = 'This is a JT description';
+  const inventoryName = options.inventoryName ?? 'Demo Inventory';
   await navigateTo(page, 'Automation ExecutionAutomation Controller', 'Templates');
   await page.getByLabel('dropdown toggle', { exact: true }).click();
   await page.getByRole('menuitem', { name: 'Create job template' }).click();
   await page.getByPlaceholder('Enter job template name').fill(jobTemplateName);
+  await page.getByPlaceholder('Enter description').fill(jobTemplateDescription);
 
-  const inventoryName = options.inventoryName ?? 'Demo Inventory';
-  await page.getByLabel('Inventory * Prompt on launch').click();
-  await page.getByRole('option', { name: inventoryName }).click();
+  if (options.PromptOnLaunch) {
+    await page.getByPlaceholder('Select inventory').fill('');
+    await page.getByPlaceholder('Select inventory').press('Backspace');
+    await page.locator('#ask_inventory_on_launch').check();
+    await page.locator('#ask_execution_environment_on_launch').check();
+    await page.locator('#ask_credential_on_launch').check();
+    await page.locator('#ask_instance_groups_on_launch').check();
+  } else {
+    await page.getByLabel('Inventory * Prompt on launch').click();
+    await page.getByRole('option', { name: inventoryName }).click();
+  }
 
   const projectName = options.projectName ?? 'Demo Project';
   await page.locator('#project-select').click();
@@ -38,7 +57,12 @@ export async function createJobTemplate(
 
   await expect(page.getByRole('heading', { name: jobTemplateName, exact: true })).toBeVisible();
   await expect(page.locator('#name')).toContainText(jobTemplateName);
-  await expect(page.locator('#inventory')).toContainText(inventoryName);
+  await expect(page.locator('#description')).toContainText(jobTemplateDescription);
+  await expect(page.locator('#job-type')).toContainText('run');
+  await expect(page.locator('#organization')).toContainText('Default');
+  if (!options.PromptOnLaunch) {
+    await expect(page.locator('#inventory')).toContainText(inventoryName);
+  }
   await expect(page.locator('#project')).toContainText(projectName);
   // await expect(page.locator('#execution-environment')).toContainText(
   //   'Default execution environment'
@@ -50,16 +74,43 @@ export async function createJobTemplate(
 
 export async function runJobTemplate(
   jobTemplateName: string,
-  page: Page,
-  options?: { doNotWait?: boolean }
+  options: {
+    doNotWait?: boolean;
+    inventoryName?: string;
+    view?: 'list' | 'details';
+    PromptOnLaunch?: boolean;
+  },
+  page: Page
 ) {
+  const inventoryName = options?.inventoryName ?? 'Demo Inventory';
   await navigateTo(page, 'Automation Execution', 'Templates');
   await page.getByLabel('table view', { exact: true }).click();
-  await selectTableFilter('Name', page);
-  await page.getByRole('button', { name: 'Select name' }).click();
-  await page.getByLabel('Search input').fill(jobTemplateName);
-  await page.getByLabel(jobTemplateName).check();
-  await page.getByRole('row', { name: jobTemplateName }).getByLabel('Launch template').click();
+  if (options?.view === 'details') {
+    await clickTableRowWithFilter(jobTemplateName, page);
+    await expect(page.getByRole('main')).toContainText(jobTemplateName);
+    await page.locator('#launch-template').click();
+  } else {
+    await clearTableFilters(page);
+    await selectTableFilter('Name', page);
+    await filterTableBySelect(jobTemplateName, page);
+    await page.getByRole('row', { name: jobTemplateName }).getByLabel('Launch template').click();
+  }
+  if (options?.PromptOnLaunch) {
+    await expect(page.getByRole('heading', { name: 'Prompt on Launch', exact: true })).toBeVisible({
+      timeout: 30000,
+    });
+    await page.getByPlaceholder('Select inventory').click();
+    await page.getByRole('option', { name: inventoryName }).click();
+    await page.getByLabel('Execution environment').click();
+    await page.getByRole('option', { name: 'Control Plane Execution' }).click();
+    await page.getByLabel('Instance groups').click();
+    await page.getByLabel('default').check();
+    await page.getByRole('button', { name: 'Next' }).click();
+    await expect(page.locator('#inventory')).toContainText(inventoryName);
+    await expect(page.locator('#execution-environment')).toContainText('Control Plane Execution');
+    await expect(page.locator('#instance-groups')).toContainText('default');
+    await page.getByRole('button', { name: 'Finish' }).click();
+  }
   await expect(page.getByRole('main')).toContainText(jobTemplateName);
   if (!options?.doNotWait) {
     await expect(page.getByText('Success', { exact: true })).toBeVisible({ timeout: 60000 });
@@ -72,13 +123,26 @@ export async function runJobTemplate(
   await expect(page.locator('#job-template')).toContainText(jobTemplateName);
 }
 
-export async function deleteJobTemplate(jobTemplateName: string, page: Page) {
+export async function deleteJobTemplate(
+  jobTemplateName: string,
+  page: Page,
+  view?: 'list' | 'details'
+) {
   await navigateTo(page, 'Automation ExecutionAutomation Controller', 'Templates');
-  await clickTableRowWithFilter(jobTemplateName, page);
-
-  await expect(page.getByRole('heading', { name: jobTemplateName, exact: true })).toBeVisible();
-  await page.getByLabel('kebab dropdown toggle').click();
-  await page.getByRole('menuitem', { name: 'Delete template' }).click();
+  await page.getByLabel('table view', { exact: true }).click();
+  if (view === 'details') {
+    await clickTableRowWithFilter(jobTemplateName, page);
+    await expect(page.getByRole('heading', { name: jobTemplateName, exact: true })).toBeVisible();
+    await page.getByLabel('kebab dropdown toggle').click();
+    await page.getByRole('menuitem', { name: 'Delete template' }).click();
+  } else {
+    await clearTableFilters(page);
+    await selectTableFilter('Name', page);
+    await filterTableBySelect(jobTemplateName, page);
+    await page.getByRole('row', { name: jobTemplateName }).getByLabel('Select row').click();
+    await page.getByLabel('toolbar actions').click();
+    await page.getByRole('menuitem', { name: 'Delete template' }).click();
+  }
   await page.getByText('Yes, I confirm that I want to').click();
   await page.getByRole('button', { name: 'Delete template' }).click();
   await expect(
--- playwright/tests/automation-execution/templates/job-template.spec.ts
+++ playwright/tests/automation-execution/templates/job-template.spec.ts
@@ -1,13 +1,71 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { setupAfter, setupBefore } from '../../../commands/setup';
 import { createJobTemplate, deleteJobTemplate, runJobTemplate } from './job-template-utils';
+import { navigateTo } from '../../../commands/navigateTo';
+import { selectTableFilter } from '../../../commands/selectTableFilter';
+import { filterTableBySelect } from '../../../commands/filterTableBySelect';
+import { clearTableFilters } from '../../../commands/clearTableFilters';
 
 test.beforeEach(setupBefore({ path: '/execution/templates' }));
 test.afterEach(setupAfter);
 
-test('job template - create, run, and delete', { tag: ['@not_mock'] }, async ({ page }) => {
-  // if (page.mock.enabled) return; // Does not yet work in mock
+test('can create a job template and assert the information showing on the details page', async ({
+  page,
+}) => {
   const jobTemplateName = await createJobTemplate({}, page);
-  await runJobTemplate(jobTemplateName, page);
   await deleteJobTemplate(jobTemplateName, page);
+});
+
+test(
+  'can create a job template with prompted fields, launch from the list view, and complete launch via wizard',
+  { tag: ['@not_mock'] },
+  async ({ page }) => {
+    const jobTemplateName = await createJobTemplate({ PromptOnLaunch: true }, page);
+    await runJobTemplate(jobTemplateName, { PromptOnLaunch: true }, page);
+    await deleteJobTemplate(jobTemplateName, page);
+  }
+);
+
+test(
+  'can launch a job template from the details page launch button using the prompt on launch',
+  { tag: ['@not_mock'] },
+  async ({ page }) => {
+    const jobTemplateName = await createJobTemplate({ PromptOnLaunch: true }, page);
+    await runJobTemplate(jobTemplateName, { PromptOnLaunch: true, view: 'details' }, page);
+    await deleteJobTemplate(jobTemplateName, page);
+  }
+);
+
+test('can delete a job template from the list line item', async ({ page }) => {
+  const jobTemplateName = await createJobTemplate({}, page);
+  await deleteJobTemplate(jobTemplateName, page);
+});
+
+test('can delete a job template from the details page', async ({ page }) => {
+  const jobTemplateName = await createJobTemplate({}, page);
+  await deleteJobTemplate(jobTemplateName, page, 'details');
+});
+
+test('can bulk delete job templates from the list page', async ({ page }) => {
+  const jobTemplateName1 = await createJobTemplate({}, page);
+  const jobTemplateName2 = await createJobTemplate({}, page);
+  await navigateTo(page, 'Automation ExecutionAutomation Controller', 'Templates');
+  await page.getByLabel('table view', { exact: true }).click();
+  await selectTableFilter('Name', page);
+  await filterTableBySelect(jobTemplateName1, page);
+  await page.getByRole('row', { name: jobTemplateName1 }).getByLabel('Select row').click();
+  await clearTableFilters(page);
+  await selectTableFilter('Name', page);
+  await filterTableBySelect(jobTemplateName2, page);
+  await page.getByRole('row', { name: jobTemplateName2 }).getByLabel('Select row').click();
+  await page.getByLabel('toolbar actions').click();
+  await page.getByRole('menuitem', { name: 'Delete templates' }).click();
+  await page.getByText('Yes, I confirm that I want to').click();
+  await page.getByRole('button', { name: 'Delete template' }).click();
+  await expect(page.locator('[data-ouia-component-type="PF5/ModalContent"]')).toContainText(
+    'Success'
+  );
+  await expect(
+    page.getByRole('heading', { name: 'Automation Templates', exact: true })
+  ).toBeVisible();
 });
