821b5f46768b7edbe271e5fc2dc7a1681715447f
Vidya Nambiar <43621546+vidyanambiar@users.noreply.github.com>
2025-02-06 09:01:00 -0500

    [AAP-37190] Additional test coverage for Persona switcher (#1149)

--- playwright/tests/persona/persona-switcher.spec.ts
+++ playwright/tests/persona/persona-switcher.spec.ts
@@ -11,6 +11,11 @@ test('persona views', async ({ page }) => {
   await expect(page.locator('#platform-awx')).toContainText('Automation Execution');
   await expect(page.locator('#platform-eda')).toContainText('Automation Decisions');
   await expect(page.locator('#platform-access')).toContainText('Access Management');
+  await expect(page.locator('#platform-hub')).toContainText('Automation Content');
+  await expect(page.locator('#awx-analytics')).toContainText('Automation Analytics');
+  await expect(page.locator('#platform-lightspeed')).toContainText('Ansible Lightspeed');
+  await expect(page.locator('#awx-settings')).toContainText('Settings');
+  await expect(page.locator('#platform-quickstarts')).toContainText('QuickStarts');
   await page.getByRole('button', { name: 'Administration View' }).click();
 
   // Developer View
@@ -18,15 +23,29 @@ test('persona views', async ({ page }) => {
   await expect(page.locator('#platform-overview')).toContainText('Overview');
   await expect(page.locator('#platform-awx')).toContainText('Automation Execution');
   await expect(page.locator('#platform-eda')).toContainText('Automation Decisions');
-  await expect(page.locator('#platform-access')).toBeHidden();
+  await expect(page.locator('#platform-hub')).toContainText('Automation Content');
+  await expect(page.locator('#platform-lightspeed')).toContainText('Ansible Lightspeed');
+  await expect(page.locator('#awx-settings')).toContainText('Settings');
+  await expect(page.locator('#platform-quickstarts')).toContainText('QuickStarts');
   await page.getByRole('button', { name: 'Developer View' }).click();
+
+  await expect(page.locator('#platform-access')).toBeHidden();
+  await expect(page.locator('#awx-analytics')).toBeHidden();
 
   // Operator View
   await page.getByRole('menuitem', { name: 'Operator View The operator' }).click();
-  await expect(page.locator('#platform-awx')).toBeHidden();
-  await expect(page.locator('#platform-eda')).toBeHidden();
-  await expect(page.locator('#platform-access')).toBeHidden();
   await expect(page.locator('#awx-jobs')).toContainText('Jobs');
   await expect(page.locator('#awx-templates')).toContainText('Templates');
   await expect(page.locator('#awx-credentials')).toContainText('Credentials');
+  await expect(page.locator('#awx-settings-preferences')).toContainText('User Preferences');
+  await expect(page.locator('#platform-quickstarts')).toContainText('QuickStarts');
+
+  await expect(page.locator('#platform-awx')).toBeHidden();
+  await expect(page.locator('#platform-eda')).toBeHidden();
+  await expect(page.locator('#platform-access')).toBeHidden();
+  await expect(page.locator('#platform-overview')).toBeHidden();
+  await expect(page.locator('#platform-hub')).toBeHidden();
+  await expect(page.locator('#awx-analytics')).toBeHidden();
+  await expect(page.locator('#platform-lightspeed')).toBeHidden();
+  await expect(page.locator('#awx-settings')).toBeHidden();
 });
