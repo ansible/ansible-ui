5ac0b9842896591ac72de0fa1b4ad966083c2497
Keith Grant <keithjgrant@redhat.com>
2025-03-10 14:07:13 -0700

    [AAP-41345] Create queryString tests in vitest (#1273)

---
+++ vitest.workspace.ts
@@ -0,0 +1,10 @@
+import { defineWorkspace } from 'vitest/config';
+
+/* eslint-disable-next-line no-restricted-exports */
+export default defineWorkspace([
+  './platform/vite.config.ts',
+  './framework/vite.config.ts',
+  './frontend/eda/vite.config.ts',
+  './frontend/awx/vite.config.ts',
+  './frontend/hub/vite.config.ts',
+]);
