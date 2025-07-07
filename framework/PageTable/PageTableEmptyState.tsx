9301e34ab6dbcaf46599722aa7c615e76054d523
Salma Kochay <89094075+skochay330@users.noreply.github.com>
2025-07-07 14:23:45 -0400

    [AAP-48064] Update organization-teams empty state page (#1558)

--- framework/PageTable/PageTableEmptyState.tsx
+++ framework/PageTable/PageTableEmptyState.tsx
@@ -5,17 +5,20 @@ import {
   EmptyStateFooter,
 } from '@patternfly/react-core';
 import { PlusCircleIcon } from '@patternfly/react-icons';
-import { ComponentType, ReactNode } from 'react';
+import { ComponentType, CSSProperties, ReactNode } from 'react';
 
 export function PageTableEmptyState(props: {
   icon?: ComponentType;
   title: string;
   description?: string;
   children?: ReactNode;
+  style?: CSSProperties;
 }) {
   return (
     <EmptyState icon={props.icon ?? PlusCircleIcon} titleText={props.title} isFullHeight>
-      {props.description && <EmptyStateBody>{props.description}</EmptyStateBody>}
+      {props.description && (
+        <EmptyStateBody style={{ ...props.style }}>{props.description}</EmptyStateBody>
+      )}
       {props.children && (
         <EmptyStateFooter>
           <EmptyStateActions>{props.children}</EmptyStateActions>
