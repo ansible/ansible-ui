d3caa795360e590f718fd9a5951a2927ee43033c
Salma Kochay <89094075+skochay330@users.noreply.github.com>
2025-09-02 17:03:25 -0400

    [AAP-49265] OCP-A test fixes (#1784)

--- cypress/e2e/awx/infrastructure/instances/instances.cy.ts
+++ cypress/e2e/awx/infrastructure/instances/instances.cy.ts
@@ -128,7 +128,11 @@ describe('Instances K8S', () => {
       cy.url().should('include', `/infrastructure/instances/${instance.id}/details`);
       cy.get('input[aria-label="Enabled"]').should('exist');
       cy.getByDataCy('actions-dropdown').click();
+      cy.intercept('GET', awxAPI`/instances/*/`).as('editForm');
       cy.getByDataCy('edit-instance').click();
+      cy.wait('@editForm').then((response) => {
+        expect(response?.response?.statusCode).to.eql(200);
+      });
       cy.getByDataCy('enabled').click();
       cy.intercept('PATCH', awxAPI`/instances/*/`).as('editedInstance');
       cy.clickButton(/^Save instance$/);
