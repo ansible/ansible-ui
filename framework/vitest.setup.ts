b965ea82591437278bcc5e9eb85bdfba7779a0e5
Keith Grant <keithjgrant@redhat.com>
2025-10-10 12:54:38 -0700

    Configure vitest-preview (#1936)

--- framework/vitest.setup.ts
+++ framework/vitest.setup.ts
@@ -1,9 +1,6 @@
 // vitest.setup.ts
 import '@testing-library/jest-dom/vitest';
-import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
-import { mockI18n } from './vitest.common';
+import { mockI18n, enablePreview } from './vitest.common';
 
 mockI18n();
-
-afterEach(() => cleanup());
+enablePreview();
--- frontend/awx/vitest.setup.ts
+++ frontend/awx/vitest.setup.ts
@@ -1,9 +1,6 @@
 // vitest.setup.ts
 import '@testing-library/jest-dom/vitest';
-import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
-import { mockI18n } from '@ansible/ansible-ui-framework/vitest.common';
+import { mockI18n, enablePreview } from '@ansible/ansible-ui-framework/vitest.common';
 
 mockI18n();
-
-afterEach(() => cleanup());
+enablePreview();
