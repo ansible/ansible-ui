3e234d9370b3444591642a4755dece3d4249303f
Kersom <9053044+nixocio@users.noreply.github.com>
2025-10-30 13:59:50 -0400

    [AAP-47073] Add missing variables from template details (#2004)

--- frontend/awx/resources/templates/WorkflowJobTemplatePage/WorkflowJobTemplateDetails.tsx
+++ frontend/awx/resources/templates/WorkflowJobTemplatePage/WorkflowJobTemplateDetails.tsx
@@ -1,16 +1,18 @@
 import { LoadingPage, PageDetail, PageDetails, useGetPageUrl } from '@ansible/ansible-ui-framework';
 import { PageDetailCodeEditor } from '@ansible/ansible-ui-framework/PageDetails/PageDetailCodeEditor';
 import { LastModifiedPageDetail } from '@ansible/common-ui/LastModifiedPageDetail';
-import { useGetItem } from '@ansible/common-ui/crud/useGet';
+import { useGet, useGetItem } from '@ansible/common-ui/crud/useGet';
 import { Content, ContentVariants, Label, LabelGroup } from '@patternfly/react-core';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { AwxError } from '../../../common/AwxError';
 import { CredentialLabel } from '../../../common/CredentialLabel';
 import { UserDateDetail } from '../../../common/UserDateDetail';
+import { toTitleCase } from '../../../common/util/strings';
 import { awxAPI } from '../../../common/api/awx-utils';
 import { WorkflowJobTemplate } from '../../../interfaces/WorkflowJobTemplate';
 import { AwxRoute } from '../../../main/AwxRoutes';
+import { Sparkline } from '../components/Sparkline';
 import { WebhookService } from '../components/WebhookService';
 
 export function WorkflowJobTemplateDetails(props: {
@@ -25,6 +27,7 @@ export function WorkflowJobTemplateDetails(props: {
     error,
     refresh,
   } = useGetItem<WorkflowJobTemplate>(awxAPI`/workflow_job_templates/`, urlId);
+  const { data: webhookKey } = useGet<{ webhook_key: string }>(template?.related?.webhook_key);
   const getPageUrl = useGetPageUrl();
   const navigate = useNavigate();
   if (error) return <AwxError error={error} handleRefresh={refresh} />;
@@ -56,6 +59,14 @@ export function WorkflowJobTemplateDetails(props: {
         )}
       </PageDetail>
       <PageDetail label={t('Description')}>{template.description}</PageDetail>
+      <PageDetail label={t('Type')}>{toTitleCase(template.type)}</PageDetail>
+      <PageDetail
+        label={t('Activity')}
+        isEmpty={!summaryFields.recent_jobs?.length}
+        helpText={t('Recent job runs for this workflow job template.')}
+      >
+        <Sparkline jobs={summaryFields.recent_jobs} />
+      </PageDetail>
       <PageDetail label={t('Organization')} isEmpty={!summaryFields.organization}>
         <Link
           to={getPageUrl(AwxRoute.OrganizationDetails, {
@@ -65,7 +76,13 @@ export function WorkflowJobTemplateDetails(props: {
           {summaryFields.organization?.name}
         </Link>
       </PageDetail>
-      <PageDetail label={t('Inventory')} isEmpty={!summaryFields?.inventory}>
+      <PageDetail
+        label={t('Inventory')}
+        helpText={t(
+          'Select an inventory for the workflow. This inventory is applied to all workflow nodes that prompt for an inventory.'
+        )}
+        isEmpty={!summaryFields?.inventory}
+      >
         {summaryFields?.inventory ? (
           <Link
             to={getPageUrl(AwxRoute.InventoryDetails, {
@@ -79,12 +96,55 @@ export function WorkflowJobTemplateDetails(props: {
           </Link>
         ) : null}
       </PageDetail>
-
-      <PageDetail label={t('Webhook service')} isEmpty={!template.webhook_service}>
+      <PageDetail
+        label={t('Source control branch')}
+        helpText={t(
+          'Select a branch for the workflow. This branch is applied to all job template nodes that prompt for a branch.'
+        )}
+        isEmpty={!template.scm_branch}
+      >
+        {template.scm_branch}
+      </PageDetail>
+      <PageDetail
+        label={t('Limit')}
+        helpText={t(
+          'Provide a host pattern to further constrain the list of hosts that will be managed or affected by the playbook. Multiple patterns are allowed. Refer to Ansible documentation for more information and examples on patterns.'
+        )}
+        isEmpty={!template.limit}
+      >
+        {template.limit}
+      </PageDetail>
+      <PageDetail
+        label={t('Webhook service')}
+        helpText={t('Select a webhook service.')}
+        isEmpty={!template.webhook_service}
+      >
         <WebhookService service={template.webhook_service} />
       </PageDetail>
+      <PageDetail
+        label={t('Webhook URL')}
+        helpText={t(
+          'Webhook services can launch jobs with this workflow job template by making a POST request to this URL.'
+        )}
+        isEmpty={!webhookKey?.webhook_key}
+      >
+        {`${window.location.origin}${template.related.webhook_receiver}`}
+      </PageDetail>
+      <PageDetail
+        label={t('Webhook key')}
+        helpText={t('Webhook services can use this as a shared secret.')}
+        isEmpty={!webhookKey?.webhook_key}
+      >
+        {webhookKey?.webhook_key}
+      </PageDetail>
       {summaryFields.webhook_credential && (
-        <PageDetail label={t('Webhook credential')} isEmpty={!summaryFields.webhook_credential}>
+        <PageDetail
+          label={t('Webhook credential')}
+          helpText={t(
+            'Optionally select the credential to use to send status updates back to the webhook service.'
+          )}
+          isEmpty={!summaryFields.webhook_credential}
+        >
           <CredentialLabel credential={summaryFields?.webhook_credential} />
         </PageDetail>
       )}
@@ -104,25 +164,61 @@ export function WorkflowJobTemplateDetails(props: {
           )
         }
       />
-      <PageDetail label={t('Labels')} isEmpty={!summaryFields.labels?.results?.length}>
+      <PageDetail
+        label={t('Labels')}
+        helpText={t(
+          "Optional labels that describe this workflow job template, such as 'dev' or 'test'. Labels can be used to group and filter workflow job templates and completed jobs."
+        )}
+        isEmpty={!summaryFields.labels?.results?.length}
+      >
         <LabelGroup>
           {summaryFields.labels.results.map((label) => (
             <Label key={label.id}>{label.name}</Label>
           ))}
         </LabelGroup>
       </PageDetail>
-      <PageDetail label={t('Job tags')} isEmpty={!template.job_tags}>
+      <PageDetail
+        label={t('Job tags')}
+        helpText={t(
+          'Tags are useful when you have a large playbook, and you want to run a specific part of a play or task. Use commas to separate multiple tags. Refer to the documentation for details on the usage of tags.'
+        )}
+        isEmpty={!template.job_tags}
+      >
         <LabelGroup>
           {template.job_tags?.split(',').map((tag) => <Label key={tag}>{tag}</Label>)}
         </LabelGroup>
       </PageDetail>
-      <PageDetail label={t('Skip tags')} isEmpty={!template.skip_tags}>
+      <PageDetail
+        label={t('Skip tags')}
+        helpText={t(
+          'Skip tags are useful when you have a large playbook, and you want to skip specific parts of a play or task. Use commas to separate multiple tags. Refer to the documentation for details on the usage of tags.'
+        )}
+        isEmpty={!template.skip_tags}
+      >
         <LabelGroup>
           {template.skip_tags?.split(',').map((tag) => <Label key={tag}>{tag}</Label>)}
         </LabelGroup>
       </PageDetail>
-      <PageDetailCodeEditor label={t('Extra vars')} value={template.extra_vars} />
-      <PageDetail label={t('Enabled options')} isEmpty={!showOptionsField}>
+      <PageDetailCodeEditor
+        label={t('Variables')}
+        value={template.extra_vars}
+        helpText={t(
+          'Pass extra command line variables to the playbook. This is the -e or --extra-vars command line parameter for ansible-playbook. Provide key/value pairs using either YAML or JSON. Refer to the Ansible Controller documentation for example syntax.'
+        )}
+      />
+      <PageDetail
+        label={t('Enabled options')}
+        helpText={
+          <Trans>
+            <p>
+              Concurrent jobs: If enabled, simultaneous runs of this workflow job template will be
+              allowed.
+            </p>
+            <p>Webhooks: Enable Webhook for this workflow job template.</p>
+          </Trans>
+        }
+        isEmpty={!showOptionsField}
+      >
         <Content component={ContentVariants.ul}>
           {template.allow_simultaneous && (
             <Content component={ContentVariants.li}>{t`Concurrent jobs`}</Content>
