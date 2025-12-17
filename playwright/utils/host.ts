ba10d66a4052b0e889e1dbcb07810945eb293d5e
Marliana Lara <mlara@redhat.com>
2025-12-17 10:00:14 -0500

    [AAP-44295] Migrate host cypress tests  (#2209)

--- playwright/utils/host.ts
+++ playwright/utils/host.ts
@@ -1,16 +1,47 @@
 import { Page, expect } from '@playwright/test';
+import { awxAPI } from '../commands/apiClient';
 import { clickPageAction } from '../commands/clickPageAction';
 import { clickTableRow } from '../commands/clickTableRow';
 import { confirmAndAssertDeletion } from '../commands/confirmAndAssertDeletion';
 import { createE2EName } from '../commands/createE2EName';
 import { navigateTo } from '../commands/navigateTo';
+import { AwxHost as HostType } from '@ansible/awx-ui/interfaces/AwxHost';
 
 export interface CreateHostOptions {
   name?: string;
   inventoryName?: string;
+  description?: string;
+  variables?: string;
 }
 
 export const Host = {
+  api: {
+    create: async (
+      page: Page,
+      options: {
+        name?: string;
+        description?: string;
+        inventory: number;
+      }
+    ): Promise<HostType> => {
+      const host = await awxAPI.post<HostType>(page, 'hosts/', {
+        name: options.name ?? createE2EName('host', { noWhitespace: true }),
+        description: options.description ?? '',
+        inventory: options.inventory,
+        variables: 'ansible_connection: local',
+        enabled: true,
+      });
+
+      if (!host) {
+        throw new Error('Failed to create host: API returned null');
+      }
+
+      return host;
+    },
+    delete: async (page: Page, hostId: number): Promise<void> => {
+      await awxAPI.delete(page, `hosts/${hostId}/`);
+    },
+  },
   ui: {
     create: async (page: Page, options: CreateHostOptions = {}): Promise<string> => {
       const hostName = options.name ?? createE2EName('host');
@@ -23,6 +54,13 @@ export const Host = {
       await page.getByLabel('Search input').fill(inventoryName);
       await page.getByRole('option', { name: inventoryName }).click();
       await page.getByLabel('Enabled').click();
+      if (options.description) {
+        await page.getByRole('textbox', { name: 'Description' }).fill(options.description);
+      }
+      if (options.variables) {
+        await page.locator('.view-line').click();
+        await page.getByRole('textbox', { name: 'Editor content' }).fill(options.variables);
+      }
       await page.getByRole('button', { name: 'Create host' }).click();
 
       await expect(page.getByRole('heading', { name: hostName, exact: true })).toBeVisible();
