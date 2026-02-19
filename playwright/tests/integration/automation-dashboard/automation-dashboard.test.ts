bdef15b6d0e125948c70ee45cd463a1c94c90333
Kristijan Čagran <kristijan.cagran@xlab.si>
2026-02-19 18:12:25 +0100

    AAP-64648 XLAB Add upper statistic cards (#2313)

--- playwright/tests/integration/automation-dashboard/automation-dashboard.test.ts
+++ playwright/tests/integration/automation-dashboard/automation-dashboard.test.ts
@@ -13,6 +13,8 @@ test.beforeEach(async ({ page }) => {
   await row.getByRole('gridcell', { name: 'Disabled' }).locator('span').click();
   await expect(row.locator('input[type="checkbox"]')).toHaveAttribute('aria-label', 'Enabled');
   await page.getByRole('button', { name: 'Automation Analytics' }).click();
+  await page.getByTestId('awx-automation-dashboard').isVisible();
+  await navigateTo(page, 'Automation Analytics', 'Automation Dashboard');
 });
 
 test.afterEach(async ({ page }) => {
@@ -26,10 +28,42 @@ test.afterEach(setupAfter);
 
 test.describe('Automation Dashboard', () => {
   test('Automation dashboard view for System Administrator', async ({ page }) => {
-    await page.getByTestId('awx-automation-dashboard').isVisible();
-    await navigateTo(page, 'Automation Analytics', 'Automation Dashboard');
     await expect(
       page.getByTestId('page-title').filter({ hasText: 'Automation Dashboard' })
     ).toBeVisible();
+  });
+
+  test('Should display value cards on automation dashboard', async ({ page }) => {
+    await expect(
+      page
+        .getByTestId('successful-jobs-card')
+        .filter({ hasText: 'Total number of successful jobs' })
+    ).toBeVisible();
+    await expect(
+      page.getByTestId('failed-jobs-card').filter({ hasText: 'Total number of failed jobs' })
+    ).toBeVisible();
+    await expect(
+      page
+        .getByTestId('unique-hosts-card')
+        .filter({ hasText: 'Total number of unique hosts automated' })
+    ).toBeVisible();
+    await expect(
+      page.getByTestId('automation-hours-card').filter({ hasText: 'Total hours of automation' })
+    ).toBeVisible();
+  });
+
+  test('Should have correct link in value cards', async ({ page }) => {
+    const successfulJobsCard = page
+      .getByTestId('successful-jobs-card')
+      .filter({ hasText: 'Total number of successful jobs' });
+    await successfulJobsCard.getByRole('link', { name: 'See all successful jobs in AAP' }).click();
+    await expect(page).toHaveURL(new RegExp('/jobs\\?status=successful$'));
+
+    await navigateTo(page, 'Automation Analytics', 'Automation Dashboard');
+    const failedJobsCard = page
+      .getByTestId('failed-jobs-card')
+      .filter({ hasText: 'Total number of failed jobs' });
+    await failedJobsCard.getByRole('link', { name: 'See all failed jobs in AAP' }).click();
+    await expect(page).toHaveURL(new RegExp('/jobs\\?status=failed$'));
   });
 });
