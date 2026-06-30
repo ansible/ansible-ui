8d3a716d09e0077b32c7e5f2b94c888f618e03fc
Djebran Lezzoum <ldjebran@gmail.com>
2026-06-30 18:20:01 +0200

    [2.7] chore: update chatbot to v0.1.14 (#2730)
    Signed-off-by: Djebran Lezzoum <ldjebran@gmail.com>

--- platform/main/Platform.tsx
+++ platform/main/Platform.tsx
@@ -4,6 +4,12 @@ import { createRoot } from 'react-dom/client';
 const Main = lazy(() => import('./PlatformMain'));
 document.body.innerHTML = '<div id="app"></div>';
 
+const botNameEl = document.createElement('div');
+botNameEl.id = 'bot_name';
+botNameEl.hidden = true;
+botNameEl.textContent = 'Automation Intelligent Assistant';
+document.body.appendChild(botNameEl);
+
 document.body.style.backgroundColor = '#222';
 
 const root = createRoot(document.getElementById('app')!);
