d1ca38618f3ce57e9186bcb782ed5bc8d91cee82
Kersom <nixocio@gmail.com>
2026-09-09 13:47:52 -0400

    chore(framework): remove unused modules (#3542)

--- framework/PageForm/Utils/PageFormSetError.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-import { FieldValues, UseFormSetError, useFormContext } from 'react-hook-form';
-
-export function PageFormSetError<TFieldValues extends FieldValues = FieldValues>(props: {
-  children: (setError: UseFormSetError<TFieldValues>) => ReactNode;
-}) {
-  const { setError } = useFormContext();
-  return <>{props.children(setError)}</>;
-}
