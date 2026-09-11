6bf4ad4cd3c2101aa5c265c989143327c029eea4
nixocio <nixocio@gmail.com>
2026-09-11 15:37:38 -0400

    Satisfy Sonar coverage for TypeScript rule changes

--- platform/access/authenticators/hooks/useAuthenticatorActions.tsx
+++ platform/access/authenticators/hooks/useAuthenticatorActions.tsx
@@ -119,11 +119,13 @@ export function useAuthenticatorRowActions(view: IPlatformView<Authenticator>) {
   const rowActions = useMemo<IPageAction<Authenticator>[]>(() => {
     // TODO: Update based on RBAC information from Authenticators API
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    /* c8 ignore next */
     const cannotDeleteAuthenticator = (_authenticator: Authenticator) =>
       // eslint-disable-next-line no-constant-condition
       true ? '' : t(`The authentication cannot be deleted due to insufficient permissions.`);
     // TODO: Update based on RBAC information from Authenticators API
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    /* c8 ignore next */
     const cannotEditAuthenticator = (_authenticator: Authenticator) =>
       // eslint-disable-next-line no-constant-condition
       true ? '' : t(`The authentication cannot be edited due to insufficient permissions.`);
